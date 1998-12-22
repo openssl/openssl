c537fb08cd93c3c564478e6a238e502a7d17e216
Ralf S. Engelschall <rse@openssl.org>
1998-12-22 16:01:06 +0000

    OpenTLS ready

--- util/up_ver.pl
+++ util/up_ver.pl
@@ -60,7 +60,7 @@ foreach $file (@files)
 
 	while (<IN>)
 		{
-		if ((s/SSLeay \d\.\d.\d[^"]*(\"|\s)/SSLeay $ver $time\1/) ||
+		if ((s/(SSLeay|OpenTLS) \d\.\d.\d[^"]*(\"|\s)/\1 $ver $time\2/) ||
 			s/^(\#define\s+SSLEAY_VERSION_NUMBER\s+0x)[0-9a-zA-Z]+(.*)$/$1$xver$2/)
 			{
 			print STDERR " Done";
