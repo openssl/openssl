47696333505bf617936524364fb1f18291be4f82
Ulf Möller <ulf@openssl.org>
1999-05-06 11:45:47 +0000

    Add missing semicolon.
    Submitted by: Janez Jere <jj@void.si>

--- util/pl/BC-32.pl
+++ util/pl/BC-32.pl
@@ -18,9 +18,9 @@ $mlflags='';
 $out_def="out32";
 $tmp_def="tmp32";
 $inc_def="inc32";
-#enable max error messages, disable most common errors, and be quiet
+#enable max error messages, disable most common warnings
 $cflags="-j255 -w-aus -w-par -w-inl  -c -tWC -tWM -DWINDOWS -DWIN32 -DL_ENDIAN ";
-$dcflags='n'
+$dcflags='n';
 if ($debug)
 {
     $cflags.="-Od -y -v -vi- -D_DEBUG";
