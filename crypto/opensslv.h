06064bb512d9ccdb4b7aa8a6b55550439140dede
Bodo Möller <bodo@openssl.org>
1999-05-19 18:08:35 +0000

    Note that the numbering scheme used to be different.

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -9,6 +9,7 @@
  * 0.9.3	0x00903100
  * 0.9.3a	0x00903101
  * 1.2.3z	0x1020311a
+ * (Prior to 0.9.3-dev a different scheme was used: 0.9.2b is 0x0922.)
  */
 #define OPENSSL_VERSION_NUMBER	0x00903000L
 #define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.3-dev 19 May 1999"
