e90c7729467108ffd6f813ade011c7ec5ced9680
Ben Laurie <ben@openssl.org>
1999-05-19 17:36:40 +0000

    Switch to new version numbering scheme.

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -1,8 +1,17 @@
 #ifndef HEADER_OPENSSLV_H
 #define HEADER_OPENSSLV_H
 
-#define OPENSSL_VERSION_NUMBER	0x0923	/* Version 0.9.1c is 0913 */
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.2c 01 Apr 1999"
+/* Numeric release version identifier:
+ * MMNNFFRBB: major minor fix final beta/patch
+ * For example:
+ * 0.9.3-dev	0x00903000
+ * 0.9.3beta1	0x00903001
+ * 0.9.3	0x00903100
+ * 0.9.3a	0x00903101
+ * 1.2.3z	0x1020311a
+ */
+#define OPENSSL_VERSION_NUMBER	0x00903000L
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.3-dev 19 May 1999"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 #endif /* HEADER_OPENSSLV_H */
