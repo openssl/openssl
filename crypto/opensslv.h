6b6596202b54458b7ade4edbe39f1af74edd2db8
Ben Laurie <ben@openssl.org>
1999-05-20 19:33:46 +0000

    Prepare for a beta release.

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -11,8 +11,8 @@
  * 1.2.3z	0x1020311a
  * (Prior to 0.9.3-dev a different scheme was used: 0.9.2b is 0x0922.)
  */
-#define OPENSSL_VERSION_NUMBER	0x00903000L
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.3-dev 19 May 1999"
+#define OPENSSL_VERSION_NUMBER	0x00903001L
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.3beta1 20 May 1999"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 #endif /* HEADER_OPENSSLV_H */
