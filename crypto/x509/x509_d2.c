38899535f85784442395aeab921b25fc79266491
Bodo Möller <bodo@openssl.org>
1999-10-26 01:52:16 +0000

    Report an error from X509_STORE_load_locations when X509_LOOKUP_load_file or X509_LOOKUP_add_dir failed.

--- crypto/x509/x509_d2.c
+++ crypto/x509/x509_d2.c
@@ -89,13 +89,15 @@ int X509_STORE_load_locations(X509_STORE *ctx, const char *file,
 		{
 		lookup=X509_STORE_add_lookup(ctx,X509_LOOKUP_file());
 		if (lookup == NULL) return(0);
-		X509_LOOKUP_load_file(lookup,file,X509_FILETYPE_PEM);
+		if (X509_LOOKUP_load_file(lookup,file,X509_FILETYPE_PEM) != 1)
+		    return(0);
 		}
 	if (path != NULL)
 		{
 		lookup=X509_STORE_add_lookup(ctx,X509_LOOKUP_hash_dir());
 		if (lookup == NULL) return(0);
-		X509_LOOKUP_add_dir(lookup,path,X509_FILETYPE_PEM);
+		if (X509_LOOKUP_add_dir(lookup,path,X509_FILETYPE_PEM) != 1)
+		    return(0);
 		}
 	if ((path == NULL) && (file == NULL))
 		return(0);
