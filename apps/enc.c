f13b93d3b46d5fa0a2b0c4ec9854a3962a73d198
Bodo Möller <bodo@openssl.org>
2000-01-24 09:32:20 +0000

    RAND_pseudo_bytes is good enough for encryption IVs, we should not need RAND_bytes (and we cannot use the latter unless we load a seed file)

--- apps/enc.c
+++ apps/enc.c
@@ -448,11 +448,8 @@ bad:
 								"invalid hex salt value\n");
 							goto end;
 						}
-					} else if (RAND_bytes(salt, PKCS5_SALT_LEN) <= 0) {
-						BIO_printf(bio_err,
-							"prng not seeded\n");
+					} else if (RAND_pseudo_bytes(salt, PKCS5_SALT_LEN) <= 0)
 						goto end;
-					}
 					/* If -P option then don't bother writing */
 					if((printkey != 2)
 					   && (BIO_write(wbio,magic,
