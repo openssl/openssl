5921ea3bcf01cb8997ab823cb21a2f4390932a20
Richard Levitte <levitte@openssl.org>
2000-02-24 02:22:15 +0000

    0.9.5beta1

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -14,8 +14,8 @@
  * 1.2.3z	  0x1020311a
  * (Prior to 0.9.3-dev a different scheme was used: 0.9.2b is 0x0922.)
  */
-#define OPENSSL_VERSION_NUMBER	0x00905000L
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.5-dev 09 Aug 1999"
+#define OPENSSL_VERSION_NUMBER	0x00905001L
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.5beta1 24 Feb 2000"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 #endif /* HEADER_OPENSSLV_H */
