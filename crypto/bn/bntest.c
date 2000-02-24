0dd0cbf554f0211e49daed346a6a77587c681c22
Bodo Möller <bodo@openssl.org>
2000-02-24 20:13:27 +0000

    Do fflush(stdout) when there was an error.

--- crypto/bn/bntest.c
+++ crypto/bn/bntest.c
@@ -235,6 +235,7 @@ int main(int argc, char *argv[])
 	exit(0);
 err:
 	BIO_puts(out,"1\n"); /* make sure bc fails if we are piping to it */
+	fflush(stdout);
 	ERR_load_crypto_strings();
 	ERR_print_errors_fp(stderr);
 	exit(1);
