9da65c3280872e2228001fa3f0aabe4f94357c41
Richard Levitte <levitte@openssl.org>
2000-02-25 23:18:27 +0000

    Adjust all the old scripts to deal with the new location.

--- crypto/threads/ptest.bat
+++ crypto/threads/ptest.bat
@@ -1,4 +1,4 @@
 del mttest.exe
 
-purify cl /O2 -DWIN32 /MD -I..\out mttest.c /Femttest ..\out\ssl32.lib ..\out\crypt32.lib
+purify cl /O2 -DWIN32 /MD -I..\..\out mttest.c /Femttest ..\..\out\ssl32.lib ..\..\out\crypt32.lib
 
--- crypto/threads/purify.sh
+++ crypto/threads/purify.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /bin/rm -f mttest
-purify cc -DSOLARIS -I../include -g mttest.c -o mttest -L.. -lthread  -lssl -lcrypto -lnsl -lsocket
+purify cc -DSOLARIS -I../../include -g mttest.c -o mttest -L../.. -lthread  -lssl -lcrypto -lnsl -lsocket
 
