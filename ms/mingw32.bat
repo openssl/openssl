9f06706bf78991b760903a640286156ed1091ddb
Ulf Möller <ulf@openssl.org>
2000-03-10 17:02:12 +0000

    Don't generate asm files for no-asm.

--- ms/mingw32.bat
+++ ms/mingw32.bat
@@ -4,6 +4,10 @@
 perl Configure Mingw32 %1 %2 %3 %4 %5 %6 %7 %8
 
 @echo off
+
+perl -e "exit 1 if '%1' eq 'no-asm'"
+if errorlevel 1 goto noasm
+
 echo Generating x86 for GNU assember
 
 echo Bignum
@@ -56,25 +60,27 @@ cd crypto\rc5\asm
 perl rc5-586.pl gaswin > r5-win32.s
 cd ..\..\..
 
-@rem Makefile
+:noasm
+
+echo Generating makefile
 perl util\mkfiles.pl >MINFO
 perl util\mk1mf.pl gaswin Mingw32 >ms\mingw32a.mak
 perl util\mk1mf.pl gaswin Mingw32-files >ms\mingw32f.mak
-@rem DLL definition files
+echo Generating DLL definition files
 perl util\mkdef.pl 32 libeay >ms\libeay32.def
 if errorlevel 1 goto end
 perl util\mkdef.pl 32 ssleay >ms\ssleay32.def
 if errorlevel 1 goto end
 
-@rem Create files -- this can be skipped if using the GNU file utilities
+rem Create files -- this can be skipped if using the GNU file utilities
 make -f ms/mingw32f.mak
 echo You can ignore the error messages above
 
-@rem Build the libraries
+echo Building the libraries
 make -f ms/mingw32a.mak
 if errorlevel 1 goto end
 
-@rem Generate the DLLs and input libraries
+echo Generating the DLLs and input libraries
 dllwrap --dllname libeay32.dll --output-lib out/libeay32.a --def ms/libeay32.def out/libcrypto.a -lwsock32 -lgdi32
 if errorlevel 1 goto end
 dllwrap --dllname libssl32.dll --output-lib out/libssl32.a --def ms/ssleay32.def out/libssl.a out/libeay32.a
