e55818b9d3c472bfdbcdb6becd9d2c2201840f2f
Richard Levitte <levitte@openssl.org>
2000-03-19 09:35:19 +0000

    Change the notation and coding of the version to be able to contain both a patch level and a beta status.  IMHO, it also makes more sense to have beta status be part of the development status than to have it be an alternate name for patch levels under special conditions.

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -2,20 +2,31 @@
 #define HEADER_OPENSSLV_H
 
 /* Numeric release version identifier:
- * MMNNFFRBB: major minor fix final beta/patch
+ * MMNNFFPPS: major minor fix patch status
+ * The status nibble has one of the values 0 for development, 1 to e for betas
+ * 1 to 14, and f for release.  The patch level is exactly that.
  * For example:
  * 0.9.3-dev	  0x00903000
  * 0.9.3beta1	  0x00903001
  * 0.9.3beta2-dev 0x00903002
  * 0.9.3beta2     0x00903002 (same as ...beta2-dev)
- * 0.9.3	  0x00903100
- * 0.9.3a	  0x00903101
- * 0.9.4 	  0x00904100
- * 1.2.3z	  0x1020311a
+ * 0.9.3	  0x0090300f
+ * 0.9.3a	  0x0090301f
+ * 0.9.4 	  0x0090400f
+ * 1.2.3z	  0x102031af
+ *
+ * For continuity reasons (because 0.9.5 is already out, and is coded
+ * 0x00905100), between 0.9.5 and 0.9.6 the coding of the patch level
+ * part is slightly different, by setting the highest bit.  This means
+ * that 0.9.5a looks like this: 0x0090581f.  At 0.9.6, we can start
+ * with 0x0090600S...
+ *
  * (Prior to 0.9.3-dev a different scheme was used: 0.9.2b is 0x0922.)
+ * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
+ *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x00906000L
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.6-dev 28 Feb 2000"
+#define OPENSSL_VERSION_NUMBER	0x00905811L
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.5a beta1 (dev) 18 Mar 2000"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 #endif /* HEADER_OPENSSLV_H */
