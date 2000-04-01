7c6c052e682cb0b6154b7964eb579890ae07cac4
Richard Levitte <levitte@openssl.org>
2000-04-01 11:21:14 +0000

    Tagging has now been done, update to the next possible version (I keep a low profile, so we don't get discontinuity in the numbering...)

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,8 +25,8 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x0090581fL
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.5a 1 Apr 2000"
+#define OPENSSL_VERSION_NUMBER	0x00905820L
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.5b-dev 1 Apr 2000"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 #endif /* HEADER_OPENSSLV_H */
