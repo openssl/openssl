33399fdee225afa461d4ddffcaf751507f216671
Bodo Möller <bodo@openssl.org>
2000-05-21 14:10:05 +0000

    Avoid sprintf.

--- ssl/ssl_txt.c
+++ ssl/ssl_txt.c
@@ -81,7 +81,7 @@ int SSL_SESSION_print_fp(FILE *fp, SSL_SESSION *x)
 int SSL_SESSION_print(BIO *bp, SSL_SESSION *x)
 	{
 	unsigned int i;
-	char str[128],*s;
+	char *s;
 
 	if (x == NULL) goto err;
 	if (BIO_puts(bp,"SSL-Session:\n") <= 0) goto err;
@@ -93,36 +93,41 @@ int SSL_SESSION_print(BIO *bp, SSL_SESSION *x)
 		s="TLSv1";
 	else
 		s="unknown";
-	sprintf(str,"    Protocol  : %s\n",s);
-	if (BIO_puts(bp,str) <= 0) goto err;
+	if (BIO_printf(bp,"    Protocol  : %s\n",s) <= 0) goto err;
 
 	if (x->cipher == NULL)
 		{
 		if (((x->cipher_id) & 0xff000000) == 0x02000000)
-			sprintf(str,"    Cipher    : %06lX\n",x->cipher_id&0xffffff);
+			{
+			if (BIO_printf(bp,"    Cipher    : %06lX\n",x->cipher_id&0xffffff) <= 0)
+				goto err;
+			}
 		else
-			sprintf(str,"    Cipher    : %04lX\n",x->cipher_id&0xffff);
+			{
+			if (BIO_printf(bp,"    Cipher    : %04lX\n",x->cipher_id&0xffff) <= 0)
+				goto err;
+			}
 		}
 	else
-		sprintf(str,"    Cipher    : %s\n",(x->cipher == NULL)?"unknown":x->cipher->name);
-	if (BIO_puts(bp,str) <= 0) goto err;
+		{
+		if (BIO_printf(bp,"    Cipher    : %s\n",((x->cipher == NULL)?"unknown":x->cipher->name)) <= 0)
+			goto err;
+		}
 	if (BIO_puts(bp,"    Session-ID: ") <= 0) goto err;
 	for (i=0; i<x->session_id_length; i++)
 		{
-		sprintf(str,"%02X",x->session_id[i]);
-		if (BIO_puts(bp,str) <= 0) goto err;
+		if (BIO_printf(bp,"%02X",x->session_id[i]) <= 0) goto err;
 		}
 	if (BIO_puts(bp,"\n    Session-ID-ctx: ") <= 0) goto err;
 	for (i=0; i<x->sid_ctx_length; i++)
 		{
-		sprintf(str,"%02X",x->sid_ctx[i]);
-		if (BIO_puts(bp,str) <= 0) goto err;
+		if (BIO_printf(bp,"%02X",x->sid_ctx[i]) <= 0)
+			goto err;
 		}
 	if (BIO_puts(bp,"\n    Master-Key: ") <= 0) goto err;
 	for (i=0; i<(unsigned int)x->master_key_length; i++)
 		{
-		sprintf(str,"%02X",x->master_key[i]);
-		if (BIO_puts(bp,str) <= 0) goto err;
+		if (BIO_printf(bp,"%02X",x->master_key[i]) <= 0) goto err;
 		}
 	if (BIO_puts(bp,"\n    Key-Arg   : ") <= 0) goto err;
 	if (x->key_arg_length == 0)
@@ -132,8 +137,7 @@ int SSL_SESSION_print(BIO *bp, SSL_SESSION *x)
 	else
 		for (i=0; i<x->key_arg_length; i++)
 			{
-			sprintf(str,"%02X",x->key_arg[i]);
-			if (BIO_puts(bp,str) <= 0) goto err;
+			if (BIO_printf(bp,"%02X",x->key_arg[i]) <= 0) goto err;
 			}
 	if (x->compress_meth != 0)
 		{
@@ -142,32 +146,26 @@ int SSL_SESSION_print(BIO *bp, SSL_SESSION *x)
 		ssl_cipher_get_evp(x,NULL,NULL,&comp);
 		if (comp == NULL)
 			{
-			sprintf(str,"\n   Compression: %d",x->compress_meth);
-			if (BIO_puts(bp,str) <= 0) goto err;
+			if (BIO_printf(bp,"\n   Compression: %d",x->compress_meth) <= 0) goto err;
 			}
 		else
 			{
-			sprintf(str,"\n   Compression: %d (%s)",
-				comp->id,comp->method->name);
-			if (BIO_puts(bp,str) <= 0) goto err;
+			if (BIO_printf(bp,"\n   Compression: %d (%s)", comp->id,comp->method->name) <= 0) goto err;
 			}
 		}	
 	if (x->time != 0L)
 		{
-		sprintf(str,"\n    Start Time: %ld",x->time);
-		if (BIO_puts(bp,str) <= 0) goto err;
+		if (BIO_printf(bp, "\n    Start Time: %ld",x->time) <= 0) goto err;
 		}
 	if (x->timeout != 0L)
 		{
-		sprintf(str,"\n    Timeout   : %ld (sec)",x->timeout);
-		if (BIO_puts(bp,str) <= 0) goto err;
+		if (BIO_printf(bp, "\n    Timeout   : %ld (sec)",x->timeout) <= 0) goto err;
 		}
 	if (BIO_puts(bp,"\n") <= 0) goto err;
 
 	if (BIO_puts(bp, "    Verify return code: ") <= 0) goto err;
-	sprintf(str, "%ld (%s)\n", x->verify_result, 
-			X509_verify_cert_error_string(x->verify_result));
-	if (BIO_puts(bp,str) <= 0) goto err;
+	if (BIO_printf(bp, "%ld (%s)\n", x->verify_result,
+		X509_verify_cert_error_string(x->verify_result)) <= 0) goto err;
 		
 	return(1);
 err:
