15e98129b20fe433e814fe2f2679300c5f563301
Bodo Möller <bodo@openssl.org>
2000-05-31 16:44:22 +0000

    strtoul is not used anywhere.

--- e_os.h
+++ e_os.h
@@ -405,7 +405,6 @@ extern HINSTANCE _hInstance;
 
 #if defined(sun) && !defined(__svr4__) && !defined(__SVR4)
 #define memmove(s1,s2,b) bcopy((s2),(s1),(n))
-#define strtoul(s,e,b) ((unsigned long int)strtol((s),(e),(b)))
 #endif
 
 /***********************************************/
