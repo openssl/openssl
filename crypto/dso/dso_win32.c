8ba92cfe811f6fa10ecb12fe57294b9627fb9b4e
Geoff Thorpe <geoff@openssl.org>
2000-06-23 17:29:05 +0000

    Steve fixed up some strange errors introduced into dso_win32.c, and I'm just converting some spaces to tabs from his fix. :-)

--- crypto/dso/dso_win32.c
+++ crypto/dso/dso_win32.c
@@ -141,14 +141,14 @@ static int win32_load(DSO *dso, const char *filename)
 	if(p == NULL)
 		{
 		DSOerr(DSO_F_WIN32_LOAD,ERR_R_MALLOC_FAILURE);
-                FreeLibrary(h);
+		FreeLibrary(h);
 		return(0);
 		}
 	*p = h;
 	if(!sk_push(dso->meth_data, (char *)p))
 		{
 		DSOerr(DSO_F_WIN32_LOAD,DSO_R_STACK_ERROR);
-                FreeLibrary(h);
+		FreeLibrary(h);
 		OPENSSL_free(p);
 		return(0);
 		}
@@ -171,7 +171,7 @@ static int win32_unload(DSO *dso)
 		DSOerr(DSO_F_WIN32_UNLOAD,DSO_R_NULL_HANDLE);
 		return(0);
 		}
-        if(!FreeLibrary(*p))
+	if(!FreeLibrary(*p))
 		{
 		DSOerr(DSO_F_WIN32_UNLOAD,DSO_R_UNLOAD_FAILED);
 		/* We should push the value back onto the stack in
