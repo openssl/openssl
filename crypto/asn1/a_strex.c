469938cb4012472d3d99ed7ead258aef3a5bc029
Dr. Stephen Henson <steve@openssl.org>
2000-08-22 12:54:21 +0000

    Fixes to d2i_ASN1_OBJECT, ASN1_INTEGER_to_BN and a_strex.c

--- crypto/asn1/a_strex.c
+++ crypto/asn1/a_strex.c
@@ -96,7 +96,7 @@ int send_bio_chars(void *arg, const void *buf, int len)
 int send_fp_chars(void *arg, const void *buf, int len)
 {
 	if(!arg) return 1;
-	if(fwrite(buf, 1, len, arg) != len) return 0;
+	if(fwrite(buf, 1, len, arg) != (unsigned int)len) return 0;
 	return 1;
 }
 
@@ -123,7 +123,7 @@ static int do_esc_char(unsigned long c, unsigned char flags, char *do_quotes, ch
 		if(!io_ch(arg, tmphex, 6)) return -1;
 		return 6;
 	}
-	chtmp = c;
+	chtmp = (unsigned char)c;
 	if(chtmp > 0x7f) chflgs = flags & ASN1_STRFLGS_ESC_MSB;
 	else chflgs = char_type[chtmp] & flags;
 	if(chflgs & CHARTYPE_BS_ESC) {
@@ -200,12 +200,12 @@ static int do_buf(unsigned char *buf, int buflen,
 				 * otherwise each character will be > 0x7f and so the 
 				 * character will never be escaped on first and last.
 				 */
-				len = do_esc_char(utfbuf[i], flags | orflags, quotes, io_ch, arg);
+				len = do_esc_char(utfbuf[i], (unsigned char)(flags | orflags), quotes, io_ch, arg);
 				if(len < 0) return -1;
 				outlen += len;
 			}
 		} else {
-			len = do_esc_char(c, flags | orflags, quotes, io_ch, arg);
+			len = do_esc_char(c, (unsigned char)(flags | orflags), quotes, io_ch, arg);
 			if(len < 0) return -1;
 			outlen += len;
 		}
@@ -304,7 +304,7 @@ static int do_print_ex(char_io *io_ch, void *arg, unsigned long lflags, ASN1_STR
 	unsigned char flags;
 	quotes = 0;
 	/* Keep a copy of escape flags */
-	flags = lflags & ESC_FLAGS;
+	flags = (unsigned char)(lflags & ESC_FLAGS);
 
 	type = str->type;
 
