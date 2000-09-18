a3829b865056b9c3b912092cd9a5ba9bbbfdd48f
Richard Levitte <levitte@openssl.org>
2000-09-18 16:52:05 +0000

    ftime() is not supported on SGI. Reported by Steve Robb <steve@eu.c2.net>

--- apps/speed.c
+++ apps/speed.c
@@ -87,7 +87,7 @@
 #elif !defined(MSDOS) && (!defined(VMS) || defined(__DECC))
 # define TIMES
 #endif
-#if !defined(_UNICOS) && !defined(__OpenBSD__)
+#if !defined(_UNICOS) && !defined(__OpenBSD__) && !defined(sgi)
 # define TIMEB
 #endif
 
