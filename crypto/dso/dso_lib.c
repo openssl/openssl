02af65b347a47a11e8f90d4c52ce2f4dc30bb726
Bodo Möller <bodo@openssl.org>
2000-11-03 15:40:10 +0000

    avoid memory leak

--- crypto/dso/dso_lib.c
+++ crypto/dso/dso_lib.c
@@ -108,7 +108,7 @@ DSO *DSO_new_method(DSO_METHOD *meth)
 		}
 	memset(ret, 0, sizeof(DSO));
 	ret->meth_data = sk_new_null();
-	if((ret->meth_data = sk_new_null()) == NULL)
+	if(ret->meth_data == NULL)
 		{
 		/* sk_new doesn't generate any errors so we do */
 		DSOerr(DSO_F_DSO_NEW_METHOD,ERR_R_MALLOC_FAILURE);
