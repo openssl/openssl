c21c35e6a43d0798e29ad85e8c54fa6de45c12d7
Richard Levitte <levitte@openssl.org>
2000-12-02 21:16:13 +0000

    Add a comment to explain the purpose of bn_cmp_part_words().

--- crypto/bn/bn_lib.c
+++ crypto/bn/bn_lib.c
@@ -778,6 +778,13 @@ int bn_cmp_words(const BN_ULONG *a, const BN_ULONG *b, int n)
 	return(0);
 	}
 
+/* Here follows a specialised variants of bn_cmp_words().  It has the
+   property of performing the operation on arrays of different sizes.
+   The sizes of those arrays is expressed through cl, which is the
+   common length ( basicall, min(len(a),len(b)) ), and dl, which is the
+   delta between the two lengths, calculated as len(a)-len(b).
+   All lengths are the number of BN_ULONGs...  */
+
 int bn_cmp_part_words(const BN_ULONG *a, const BN_ULONG *b,
 	int cl, int dl)
 	{
