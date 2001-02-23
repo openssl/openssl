e3a91640739c4c016e234550ed913e7f7f6970f9
Geoff Thorpe <geoff@openssl.org>
2001-02-23 00:09:50 +0000

    I missed one.

--- apps/s_server.c
+++ apps/s_server.c
@@ -1605,7 +1605,7 @@ static int generate_session_id(const SSL *ssl, unsigned char *id,
 			(strlen(session_id_prefix) < *id_len) ?
 			strlen(session_id_prefix) : *id_len);
 		}
-	while(SSL_CTX_has_matching_session_id(ssl->ctx, id, *id_len) &&
+	while(SSL_has_matching_session_id(ssl, id, *id_len) &&
 		(++count < MAX_SESSION_ID_ATTEMPTS));
 	if(count >= MAX_SESSION_ID_ATTEMPTS)
 		return 0;
