f23478c3140637799d4aded503a7ecafbae8ba22
Dr. Stephen Henson <steve@openssl.org>
2001-03-01 13:32:11 +0000

    Fix bug in copy_email() which would not find emailAddress at start of subject name.

--- crypto/x509v3/v3_alt.c
+++ crypto/x509v3/v3_alt.c
@@ -318,7 +318,7 @@ static int copy_email(X509V3_CTX *ctx, GENERAL_NAMES *gens)
 	/* Now add any email address(es) to STACK */
 	i = -1;
 	while((i = X509_NAME_get_index_by_NID(nm,
-					 NID_pkcs9_emailAddress, i)) > 0) {
+					 NID_pkcs9_emailAddress, i)) >= 0) {
 		ne = X509_NAME_get_entry(nm, i);
 		email = M_ASN1_IA5STRING_dup(X509_NAME_ENTRY_get_data(ne));
 		if(!email || !(gen = GENERAL_NAME_new())) {
