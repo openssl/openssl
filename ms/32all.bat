bb6dcb54f98203b01759b005396d605eb08b43b3
Richard Levitte <levitte@openssl.org>
2001-03-15 06:51:32 +0000

    It is a good thing to prepare the .def files.

--- ms/32all.bat
+++ ms/32all.bat
@@ -6,6 +6,8 @@ perl util\mk1mf.pl %OPTS% debug VC-WIN32 >d32.mak
 perl util\mk1mf.pl %OPTS% VC-WIN32 >32.mak
 perl util\mk1mf.pl %OPTS% debug dll VC-WIN32 >d32dll.mak
 perl util\mk1mf.pl %OPTS% dll VC-WIN32 >32dll.mak
+perl util\mkdef.pl 32 libeay > ms\libeay32.def
+perl util\mkdef.pl 32 ssleay > ms\ssleay32.def
 
 nmake -f d32.mak
 @if errorlevel 1 goto end
