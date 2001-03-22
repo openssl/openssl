8d82218269eb649dd6ab7b125c2155702062fe0a
Dr. Stephen Henson <steve@openssl.org>
2001-03-22 13:49:15 +0000

    Fix bug in PKCS#7 decode routines when indefinite length encoding is used inside definite length encoding.

--- crypto/asn1/p7_lib.c
+++ crypto/asn1/p7_lib.c
@@ -307,6 +307,7 @@ PKCS7 *d2i_PKCS7(PKCS7 **a, unsigned char **pp, long length)
 			}
 		if (Tinf == (1|V_ASN1_CONSTRUCTED))
 			{
+			c.q=c.p;
 			if (!ASN1_check_infinite_end(&c.p,c.slen))
 				{
 				c.error=ERR_R_MISSING_ASN1_EOS;
@@ -314,6 +315,7 @@ PKCS7 *d2i_PKCS7(PKCS7 **a, unsigned char **pp, long length)
 				goto err;
 				}
 			}
+			c.slen-=(c.p-c.q);
 		}
 	else
 		ret->detached=1;
