91dc71f98dfa771463d5f0141a4a9e5143e5a6fd
Richard Levitte <levitte@openssl.org>
2001-04-26 08:26:18 +0000

    User OPENSSL_UNISTD instead of <unistd.h>. Spotted by Mark Crispin <MRC@Panda.COM>

--- crypto/uid.c
+++ crypto/uid.c
@@ -54,10 +54,11 @@
  */
 
 #include <openssl/crypto.h>
+#include <openssl/opensslconf.h>
 
 #if defined(__OpenBSD__) || (defined(__FreeBSD__) && __FreeBSD__ > 2)
 
-#include <unistd.h>
+#include OPENSSL_UNISTD
 
 int OPENSSL_issetugid(void)
 	{
@@ -73,7 +74,7 @@ int OPENSSL_issetugid(void)
 
 #else
 
-#include <unistd.h>
+#include OPENSSL_UNISTD
 #include <sys/types.h>
 
 int OPENSSL_issetugid(void)
