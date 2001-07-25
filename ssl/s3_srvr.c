7bc03ded122f6538fbcc321db987ba4338f5e278
Bodo Möller <bodo@openssl.org>
2001-07-25 10:04:14 +0000

    add a comment

--- ssl/s3_srvr.c
+++ ssl/s3_srvr.c
@@ -270,6 +270,10 @@ int ssl3_accept(SSL *s)
 				&& !(l & SSL_KRB5)
 #endif /* OPENSSL_NO_KRB5 */
 				)
+				/* option SSL_OP_EPHEMERAL_RSA sends temporary RSA key
+				 * even when forbidden by protocol specs
+				 * (handshake may fail as clients are not required to
+				 * be able to handle this) */
 				s->s3->tmp.use_rsa_tmp=1;
 			else
 				s->s3->tmp.use_rsa_tmp=0;
