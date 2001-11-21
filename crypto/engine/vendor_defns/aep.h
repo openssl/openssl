595241e17f16bc77e08bee4fe82aaa2e840bc82b
Richard Levitte <levitte@openssl.org>
2001-11-21 13:26:57 +0000

    inttypes.h apparently doesn't exist with VC++.  Therefore, use the built-in types __int8, __int16 and so on on that platform.

--- crypto/engine/vendor_defns/aep.h
+++ crypto/engine/vendor_defns/aep.h
@@ -9,7 +9,14 @@
  *
  */
 
+#ifdef WIN32
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+#else
 #include <inttypes.h>
+#endif
 
 /*Successful return value*/
 #define AEP_R_OK                                0x00000000
