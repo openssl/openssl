032c49b8b3bd732e1bfec6ac41cc3dad8663e503
Dr. Stephen Henson <steve@openssl.org>
2002-02-22 21:21:18 +0000

    non-Monolith fixes.
    Submitted by Andrew W. Gray <agray@iconsinc.com>

--- apps/req.c
+++ apps/req.c
@@ -181,6 +181,7 @@ int MAIN(int argc, char **argv)
 	unsigned long chtype = MBSTRING_ASC;
 #ifndef MONOLITH
 	MS_STATIC char config_name[256];
+	long errline;
 #endif
 
 	req_conf = NULL;
@@ -529,7 +530,7 @@ bad:
 		}
 	default_config_file=p;
 	config=NCONF_new(NULL);
-	i=NCONF_load(config, p);
+	i=NCONF_load(config, p, &errline);
 #endif
 
 	if (template != NULL)
