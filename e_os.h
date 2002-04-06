f60df86c899cc90d4d0d9d8a4d3591ac27218620
Richard Levitte <levitte@openssl.org>
2002-04-06 11:57:58 +0000

    Oops, the system macro for VMS is OPENSSL_SYS_VMS, not VMS

--- e_os.h
+++ e_os.h
@@ -160,7 +160,7 @@ extern "C" {
 #define closesocket(s)		MacSocket_close(s)
 #define readsocket(s,b,n)	MacSocket_recv((s),(b),(n),true)
 #define writesocket(s,b,n)	MacSocket_send((s),(b),(n))
-#elif defined(VMS)
+#elif defined(OPENSSL_SYS_VMS)
 #define get_last_socket_error() errno
 #define clear_socket_error()    errno=0
 #define ioctlsocket(a,b,c)      ioctl(a,b,c)
