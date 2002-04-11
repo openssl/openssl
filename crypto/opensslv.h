9dfcad117cb8eb9e8511709979572e8f548ea8bf
Richard Levitte <levitte@openssl.org>
2002-04-11 21:53:57 +0000

    Change the date to XX xxx XXXX in development versions.

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -26,7 +26,7 @@
  *  major minor fix final patch/beta)
  */
 #define OPENSSL_VERSION_NUMBER	0x00907000L
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7-dev 24 Sep 2000"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7-dev XX xxx XXXX"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 
