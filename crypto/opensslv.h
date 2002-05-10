69043fa13fc3119008a9b53795783bdcc486cb5d
Richard Levitte <levitte@openssl.org>
2002-05-10 01:02:28 +0000

    Merge recent changes from 0.9.6-stable.

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,8 +25,8 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x0090604fL
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.6d [engine] 9 May 2002"
+#define OPENSSL_VERSION_NUMBER	0x00906050L
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.6e-dev [engine] 9 May 2002"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 
