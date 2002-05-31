f918c330a938f667eb6070d5f199ac0ab897fac9
Richard Levitte <levitte@openssl.org>
2002-05-31 13:11:48 +0000

    For CFB and OFB modes, always create the encryption key.

--- crypto/evp/e_aes.c
+++ crypto/evp/e_aes.c
@@ -88,7 +88,9 @@ IMPLEMENT_BLOCK_CIPHER(aes_256, ks, AES, EVP_AES_KEY,
 static int aes_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key,
 		   const unsigned char *iv, int enc) {
 
-	if (enc) 
+	if ((ctx->cipher->flags & EVP_CIPH_MODE) == EVP_CIPH_CFB_MODE
+	    || (ctx->cipher->flags & EVP_CIPH_MODE) == EVP_CIPH_OFB_MODE
+	    || enc) 
 		AES_set_encrypt_key(key, ctx->key_len * 8, ctx->cipher_data);
 	else
 		AES_set_decrypt_key(key, ctx->key_len * 8, ctx->cipher_data);
