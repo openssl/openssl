d58a949aaee6a92c02e77fc5743a73cb9ceb3f5c
Richard Levitte <levitte@openssl.org>
2002-06-27 14:58:06 +0000

    Use underscores instead of dashes in temporary file names. This is due to weird Borland compilers. Part of PR 123

--- ms/do_masm.bat
+++ ms/do_masm.bat
@@ -3,54 +3,54 @@ echo Generating x86 for MASM assember
 
 echo Bignum
 cd crypto\bn\asm
-perl x86.pl win32 > bn-win32.asm
+perl x86.pl win32 > bn_win32.asm
 cd ..\..\..
 
 echo DES
 cd crypto\des\asm
-perl des-586.pl win32 > d-win32.asm
+perl des-586.pl win32 > d_win32.asm
 cd ..\..\..
 
 echo "crypt(3)"
 
 cd crypto\des\asm
-perl crypt586.pl win32 > y-win32.asm
+perl crypt586.pl win32 > y_win32.asm
 cd ..\..\..
 
 echo Blowfish
 
 cd crypto\bf\asm
-perl bf-586.pl win32 > b-win32.asm
+perl bf-586.pl win32 > b_win32.asm
 cd ..\..\..
 
 echo CAST5
 cd crypto\cast\asm
-perl cast-586.pl win32 > c-win32.asm
+perl cast-586.pl win32 > c_win32.asm
 cd ..\..\..
 
 echo RC4
 cd crypto\rc4\asm
-perl rc4-586.pl win32 > r4-win32.asm
+perl rc4-586.pl win32 > r4_win32.asm
 cd ..\..\..
 
 echo MD5
 cd crypto\md5\asm
-perl md5-586.pl win32 > m5-win32.asm
+perl md5-586.pl win32 > m5_win32.asm
 cd ..\..\..
 
 echo SHA1
 cd crypto\sha\asm
-perl sha1-586.pl win32 > s1-win32.asm
+perl sha1-586.pl win32 > s1_win32.asm
 cd ..\..\..
 
 echo RIPEMD160
 cd crypto\ripemd\asm
-perl rmd-586.pl win32 > rm-win32.asm
+perl rmd-586.pl win32 > rm_win32.asm
 cd ..\..\..
 
 echo RC5\32
 cd crypto\rc5\asm
-perl rc5-586.pl win32 > r5-win32.asm
+perl rc5-586.pl win32 > r5_win32.asm
 cd ..\..\..
 
 echo on
