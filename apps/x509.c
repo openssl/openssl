72a37c03beca1b1a28a9f5cb3d9b2ece76168b99
Lutz Jänicke <jaenicke@openssl.org>
2002-07-29 13:28:57 +0000

    Only use DSA-functions if available. Submitted by: "Hellan,Kim KHE" <KHE@kmd.dk> Reviewed by: PR: 167

--- apps/x509.c
+++ apps/x509.c
@@ -915,8 +915,10 @@ bad:
 
 				BIO_printf(bio_err,"Generating certificate request\n");
 
+#ifndef OPENSSL_NO_DSA
 		                if (pk->type == EVP_PKEY_DSA)
 		                        digest=EVP_dss1();
+#endif
 
 				rq=X509_to_X509_REQ(x,pk,digest);
 				EVP_PKEY_free(pk);
