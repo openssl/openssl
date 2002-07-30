2c3bd4778efbe3e4e2dc029fac887fc7d96cde54
Lutz Jänicke <jaenicke@openssl.org>
2002-07-30 10:52:36 +0000

    Next version of 0.9.6 would be 0.9.6f. Submitted by: Reviewed by: PR:

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,8 +25,8 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x0090605fL
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.6e 30 Jul 2002"
+#define OPENSSL_VERSION_NUMBER	0x00906060L
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.6f-dev XX xxx XXXX"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 
