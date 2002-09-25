413f2b2247bbe9214de17c3a4e98d438fb72e1d0
Lutz Jänicke <jaenicke@openssl.org>
2002-09-25 20:17:58 +0000

    Add missing brackets. Submitted by: "Chris Brook" <cbrook@v-one.com> Reviewed by: PR:

--- ssl/ssltest.c
+++ ssl/ssltest.c
@@ -405,7 +405,7 @@ int main(int argc, char *argv[])
 #ifndef OPENSSL_NO_DH
 			dhe1024=1;
 #else
-			fprintf(stderr,"ignoring -dhe1024, since I'm compiled without DH\n";
+			fprintf(stderr,"ignoring -dhe1024, since I'm compiled without DH\n");
 #endif
 			}
 		else if	(strcmp(*argv,"-dhe1024dsa") == 0)
@@ -413,7 +413,7 @@ int main(int argc, char *argv[])
 #ifndef OPENSSL_NO_DH
 			dhe1024dsa=1;
 #else
-			fprintf(stderr,"ignoring -dhe1024, since I'm compiled without DH\n";
+			fprintf(stderr,"ignoring -dhe1024, since I'm compiled without DH\n");
 #endif
 			}
 		else if	(strcmp(*argv,"-no_dhe") == 0)
