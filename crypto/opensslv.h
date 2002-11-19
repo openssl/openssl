8f89ae928b31e62aa5efdcb7c497693bac76bf52
Richard Levitte <levitte@openssl.org>
2002-11-19 11:37:03 +0000

    We now work with the development of 0.9.7 beta 5.

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,8 +25,8 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x00907004L
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7-beta4 19 Nov 2002"
+#define OPENSSL_VERSION_NUMBER	0x00907005L
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7-beta5-dev xx XXX 2002"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 
