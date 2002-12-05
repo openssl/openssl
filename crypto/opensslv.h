af0b30e77c5360cd90970da4565e31d209d7e46f
Richard Levitte <levitte@openssl.org>
2002-12-05 23:10:01 +0000

    Tagging has been done, time to move the branch to 0.9.7-beta6 development.

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,8 +25,8 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x00907005L
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7-beta5  5 Dec 2002"
+#define OPENSSL_VERSION_NUMBER	0x00907006L
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7-beta6-dev xx XXX 2002"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 
