54307ce955c7ebe31c131badbf6969338654902f
Richard Levitte <levitte@openssl.org>
2002-12-05 01:35:09 +0000

    Allow users to modify /MD to /MT. PR: 380

--- util/pl/VC-32.pl
+++ util/pl/VC-32.pl
@@ -91,7 +91,7 @@ if ($shlib)
 	{
 	$mlflags.=" $lflags /dll";
 #	$cflags =~ s| /MD| /MT|;
-	$lib_cflag=" -D_WINDLL -D_DLL";
+	$lib_cflag=" -D_WINDLL";
 	$out_def="out32dll";
 	$tmp_def="tmp32dll";
 	}
