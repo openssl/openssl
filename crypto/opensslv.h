9494e3cc406ddd4de357196948ac849b896328be
Richard Levitte <levitte@openssl.org>
2002-12-05 22:51:19 +0000

    Time to release 0.9.7-beta5. The tag will be OpenSSL_0_9_7-beta5.

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -26,7 +26,7 @@
  *  major minor fix final patch/beta)
  */
 #define OPENSSL_VERSION_NUMBER	0x00907005L
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7-beta5-dev xx XXX 2002"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7-beta5  5 Dec 2002"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 
