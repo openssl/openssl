631cdb9f690e78d71e417e9315ef68acc87053b7
Richard Levitte <levitte@openssl.org>
2002-12-05 22:44:12 +0000

    Small fault corrected

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,7 +25,7 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x00906080L
+#define OPENSSL_VERSION_NUMBER	0x0090608fL
 #define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.6h  5 Dec 2002"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
