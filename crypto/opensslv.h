fcf8f4d4530d56f36ead7aa1064b6912a0739f4e
Richard Levitte <levitte@openssl.org>
2002-12-17 14:24:51 +0000

    Time to release OpenSSL 0.9.7 beta6. The tag will be OpenSSL_0_9_7-beta6.

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -26,7 +26,7 @@
  *  major minor fix final patch/beta)
  */
 #define OPENSSL_VERSION_NUMBER	0x00907006L
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7-beta6-dev xx XXX 2002"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7-beta6 17 Dec 2002"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 
