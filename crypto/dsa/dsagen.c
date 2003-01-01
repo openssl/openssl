6983b4615c32d41d61e9a0e046a90ebe2bdce3e3
Richard Levitte <levitte@openssl.org>
2003-01-01 23:41:50 +0000

    Adjust the parameter lists in some not commonly used files. PR: 428

--- crypto/dsa/dsagen.c
+++ crypto/dsa/dsagen.c
@@ -103,7 +103,7 @@ main()
 		bio_err=BIO_new_fp(stderr,BIO_NOCLOSE);
 
 	memcpy(seed_buf,seed,20);
-	dsa=DSA_generate_parameters(1024,seed,20,&counter,&h,cb);
+	dsa=DSA_generate_parameters(1024,seed,20,&counter,&h,cb,bio_err);
 
 	if (dsa == NULL)
 		DSA_print(bio_err,dsa,0);
