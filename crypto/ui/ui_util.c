364c7cd6216bc9b5ad35cd556b8381c88a0f22e7
Richard Levitte <levitte@openssl.org>
2003-01-13 14:17:43 +0000

    UI_UTIL_read_pw() misinterpreted the values returned from UI functions. PR: 456

--- crypto/ui/ui_util.c
+++ crypto/ui/ui_util.c
@@ -78,12 +78,14 @@ int UI_UTIL_read_pw(char *buf,char *buff,int size,const char *prompt,int verify)
 	if (ui)
 		{
 		ok = UI_add_input_string(ui,prompt,0,buf,0,size-1);
-		if (ok == 0 && verify)
+		if (ok >= 0 && verify)
 			ok = UI_add_verify_string(ui,prompt,0,buff,0,size-1,
 				buf);
-		if (ok == 0)
+		if (ok >= 0)
 			ok=UI_process(ui);
 		UI_free(ui);
 		}
+	if (ok > 0)
+		ok = 0;
 	return(ok);
 	}
