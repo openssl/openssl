0c3426da8678d248c2ebfe02c84d6fdab122a21e
Richard Levitte <levitte@openssl.org>
2003-01-23 08:10:04 +0000

    Missing 0 broke FreeBSD build. PR: 470

--- crypto/engine/eng_cryptodev.c
+++ crypto/engine/eng_cryptodev.c
@@ -35,7 +35,7 @@
 
 #if (defined(__unix__) || defined(unix)) && !defined(USG)
 #include <sys/param.h>
-# if (OpenBSD >= 200112) || ((__FreeBSD_version >= 470101 && __FreeBSD_version < 50000) || __FreeBSD_version >= 50041)
+# if (OpenBSD >= 200112) || ((__FreeBSD_version >= 470101 && __FreeBSD_version < 500000) || __FreeBSD_version >= 500041)
 #  define HAVE_CRYPTODEV
 # endif
 # if (OpenBSD >= 200110)
