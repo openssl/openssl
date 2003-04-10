e352a9e6ecb320f849e41f8d349c2813729de6d3
Richard Levitte <levitte@openssl.org>
2003-04-10 20:30:41 +0000

    I forgot to change the status bits to release. This file will be retagged.

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,7 +25,7 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x009060a0L
+#define OPENSSL_VERSION_NUMBER	0x009060afL
 #define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.6j 10 Apr 2003"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
