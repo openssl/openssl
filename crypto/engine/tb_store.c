7f6af7d9db1a5e5f13dc0a0b03bea166ff24c3cb
Richard Levitte <levitte@openssl.org>
2003-05-01 20:15:35 +0000

    Get the year right...

--- crypto/engine/tb_store.c
+++ crypto/engine/tb_store.c
@@ -1,5 +1,5 @@
 /* ====================================================================
- * Copyright (c) 2000 The OpenSSL Project.  All rights reserved.
+ * Copyright (c) 2003 The OpenSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
