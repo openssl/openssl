4aae637f6c896405756245d0d80bd9b58ac5d07b
Richard Levitte <levitte@openssl.org>
2003-06-19 18:55:56 +0000

    We set the export flag for 512 *bit* keys, not 512 *byte* ones. PR: 587

--- crypto/x509/x509type.c
+++ crypto/x509/x509type.c
@@ -106,7 +106,8 @@ int X509_certificate_type(X509 *x, EVP_PKEY *pkey)
 		break;
 		}
 
-	if (EVP_PKEY_size(pk) <= 512)
+	if (EVP_PKEY_size(pk) <= 512/8)	/* /8 because it's 512 bits we look
+					   for, not bytes */
 		ret|=EVP_PKT_EXP;
 	if(pkey==NULL) EVP_PKEY_free(pk);
 	return(ret);
