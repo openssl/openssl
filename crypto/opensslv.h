fbe7cd3138da20204b7d9b895b59e86f2ab2269f
Mark J. Cox <mark@openssl.org>
2003-11-04 11:37:18 +0000

    Bump revision after tagging

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,8 +25,8 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x009060cfL
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.6l 04 Nov 2003"
+#define OPENSSL_VERSION_NUMBER	0x009060d0L
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.6m-dev xx XXX xxxx"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 
