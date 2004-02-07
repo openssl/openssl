ad5550209241f4c01881c1b786eabca99c696031
Andy Polyakov <appro@openssl.org>
2004-02-07 09:46:47 +0000

    Typo in crypto/bn/asm/x86_64.c, bn_div_words(). PR: 821

--- crypto/bn/asm/x86_64-gcc.c
+++ crypto/bn/asm/x86_64-gcc.c
@@ -142,7 +142,7 @@ void bn_sqr_words(BN_ULONG *r, BN_ULONG *a, int n)
 BN_ULONG bn_div_words(BN_ULONG h, BN_ULONG l, BN_ULONG d)
 {	BN_ULONG ret,waste;
 
-	asm ("divq	%3"
+	asm ("divq	%4"
 		: "=a"(ret),"=d"(waste)
 		: "a"(l),"d"(h),"g"(d)
 		: "cc");
