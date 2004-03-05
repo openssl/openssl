be21fe59aa11fe0fa3585c6d289b562a5f95555d
Dr. Stephen Henson <steve@openssl.org>
2004-03-05 23:45:08 +0000

    Call autoconfig code in pkcs7 utility.

--- apps/pkcs7.c
+++ apps/pkcs7.c
@@ -102,6 +102,9 @@ int MAIN(int argc, char **argv)
 		if ((bio_err=BIO_new(BIO_s_file())) != NULL)
 			BIO_set_fp(bio_err,stderr,BIO_NOCLOSE|BIO_FP_TEXT);
 
+	if (!load_config(bio_err, NULL))
+		goto end;
+
 	infile=NULL;
 	outfile=NULL;
 	informat=FORMAT_PEM;
