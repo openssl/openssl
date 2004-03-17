8ccf402239a07d23ab308c9dc11bfbe59bad3e65
Mark J. Cox <mark@openssl.org>
2004-03-17 11:40:44 +0000

    Fix null-pointer assignment in do_change_cipher_spec() revealed by using the Codenomicon TLS Test Tool (CAN-2004-0079)
    Prepare for 0.9.6m tagging and release
    
    Submitted by: Steven Henson
    Reviewed by: Joe Orton
    Approved by: Mark Cox

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,8 +25,8 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x009060d0L
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.6m-dev xx XXX xxxx"
+#define OPENSSL_VERSION_NUMBER	0x009060dfL
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.6m 17 Mar 2004"
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
 
