46b7624b8e3a376a55d613767c4f6ea41e638af2
Richard Levitte <levitte@openssl.org>
2004-06-28 12:23:40 +0000

    Make sure that the buffers are large enough to contain padding. PR: 904

--- apps/speed.c
+++ apps/speed.c
@@ -210,7 +210,11 @@
 #endif
 
 #undef BUFSIZE
-#define BUFSIZE	((long)1024*8+1)
+/* BUFSIZE needs to be one cipherblock larger than the largest number in the
+   lengths array (see below), to make space for padding when doing EVP tests.
+   1024 extra bytes may seem much, but hey, it doesn't hurt!
+							-- Richard Levitte */
+#define BUFSIZE	((long)1024*9+1)
 int run=0;
 
 static char ftime_used = 0, times_used = 0, gettimeofday_used = 0, getrusage_used = 0;
