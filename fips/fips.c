44dd6865b9da36937cc14b29e5e1f783677570a3
Dr. Stephen Henson <steve@openssl.org>
2004-08-05 18:11:43 +0000

    Stop compiler giving bogus shadow warning.

--- fips/fips.c
+++ fips/fips.c
@@ -166,7 +166,7 @@ static int FIPS_check_exe(const char *path)
 
 int FIPS_mode_set(int onoff,const char *path)
     {
-    void fips_set_mode(int onoff);
+    void fips_set_mode(int _onoff);
     int fips_set_owning_thread();
     int fips_clear_owning_thread();
     int ret = 0;
