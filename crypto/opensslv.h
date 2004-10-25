bfb7bac83b98d57e163a6d2eceda9073761ac708
Dr. Stephen Henson <steve@openssl.org>
2004-10-25 11:24:39 +0000

    Updates for 0.9.7e release.

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,11 +25,11 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x00907050L
+#define OPENSSL_VERSION_NUMBER	0x0090705FL
 #ifdef OPENSSL_FIPS
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7e-fips-dev XX xxx XXXX"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7e-fips 25 Oct 2004"
 #else
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7e-dev XX xxx XXXX"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7e 25 Oct 2004"
 #endif
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
