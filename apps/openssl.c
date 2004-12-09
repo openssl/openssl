5868130e7a31d21730d6a8698b7b6bf4a8aa72c2
Andy Polyakov <appro@openssl.org>
2004-12-09 18:00:26 +0000

    Eliminate dependency on UNICODE macro.

--- apps/openssl.c
+++ apps/openssl.c
@@ -239,7 +239,7 @@ int main(int Argc, char *Argv[])
 	if(getenv("OPENSSL_FIPS")) {
 #if defined(_WIN32)
 		char filename[MAX_PATH] = "";
-		GetModuleFileName( NULL, filename, MAX_PATH) ;
+		GetModuleFileNameA( NULL, filename, MAX_PATH) ;
 		p = filename;
 #else
 		p = Argv[0];
