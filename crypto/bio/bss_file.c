de421076a5e0cbf31268c8769f5ac0889bef79ba
Andy Polyakov <appro@openssl.org>
2004-12-27 21:27:46 +0000

    Minor cygwin update. PR: 949

--- crypto/bio/bss_file.c
+++ crypto/bio/bss_file.c
@@ -274,7 +274,7 @@ static long MS_CALLBACK file_ctrl(BIO *b, int cmd, long num, void *ptr)
 			ret=0;
 			break;
 			}
-#if defined(OPENSSL_SYS_MSDOS) || defined(OPENSSL_SYS_WINDOWS) || defined(OPENSSL_SYS_OS2)
+#if defined(OPENSSL_SYS_MSDOS) || defined(OPENSSL_SYS_WINDOWS) || defined(OPENSSL_SYS_OS2) || defined(OPENSSL_SYS_WIN32_CYGWIN)
 		if (!(num & BIO_FP_TEXT))
 			strcat(p,"b");
 		else
