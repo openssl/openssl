9d14506f292f36f78993dc813eda84008ecc0175
Andy Polyakov <appro@openssl.org>
2005-03-12 11:28:22 +0000

    Cygwin to use DSO_FLFCN and mingw to use DSO_WIN32 (required for FIPS).

--- crypto/dso/dso_win32.c
+++ crypto/dso/dso_win32.c
@@ -61,7 +61,7 @@
 #include "cryptlib.h"
 #include <openssl/dso.h>
 
-#if !defined(OPENSSL_SYS_WIN32) || defined(OPENSSL_SYS_WINCE)
+#if !defined(DSO_WIN32)
 DSO_METHOD *DSO_METHOD_win32(void)
 	{
 	return NULL;
