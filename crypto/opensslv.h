22e5a7935f23ec9568a79b024538580e54096e7b
Richard Levitte <levitte@openssl.org>
2005-04-11 15:10:07 +0000

    Prepare to release 0.9.7g. The tag till be OpenSSL_0_9_7g.

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,11 +25,11 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x00907070L
+#define OPENSSL_VERSION_NUMBER	0x0090707fL
 #ifdef OPENSSL_FIPS
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7g-fips-dev XX xxx XXXX"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7g-fips 11 Apr 2005"
 #else
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7g-dev XX xxx XXXX"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7g 11 Apr 2005"
 #endif
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
