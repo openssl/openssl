bc3cae7e7d1dd82b8b16d9bb63f754899e26b7ca
Dr. Stephen Henson <steve@openssl.org>
2005-04-12 13:31:14 +0000

    Include error library value in C error source files instead of fixing up at runtime.

--- crypto/err/err.c
+++ crypto/err/err.c
@@ -631,7 +631,8 @@ static void err_load_strings(int lib, ERR_STRING_DATA *str)
 	{
 	while (str->error)
 		{
-		str->error|=ERR_PACK(lib,0,0);
+		if (lib)
+			str->error|=ERR_PACK(lib,0,0);
 		ERRFN(err_set_item)(str);
 		str++;
 		}
@@ -647,7 +648,8 @@ void ERR_unload_strings(int lib, ERR_STRING_DATA *str)
 	{
 	while (str->error)
 		{
-		str->error|=ERR_PACK(lib,0,0);
+		if (lib)
+			str->error|=ERR_PACK(lib,0,0);
 		ERRFN(err_del_item)(str);
 		str++;
 		}
