1897c893022c75b314187d7bee3aef7a9026493c
Nils Larsch <nils@openssl.org>
2005-04-29 14:26:59 +0000

    avoid warnings when building on systems where sizeof(void *) > sizeof(int)

--- crypto/mem_clr.c
+++ crypto/mem_clr.c
@@ -68,7 +68,7 @@ void OPENSSL_cleanse(void *ptr, size_t len)
 	while(loop--)
 		{
 		*(p++) = cleanse_ctr;
-		cleanse_ctr += (17 + (unsigned char)((int)p & 0xF));
+		cleanse_ctr += (17 + (unsigned char)((unsigned long)p & 0xF));
 		}
 	if(memchr(ptr, cleanse_ctr, len))
 		cleanse_ctr += 63;
