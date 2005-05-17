2c4b354d32b1f34314f1a3ebeee0d0fcfb7505a0
Andy Polyakov <appro@openssl.org>
2005-05-17 00:07:13 +0000

    Disclaim 16-bit support.

--- ms/do_nasm.bat
+++ ms/do_nasm.bat
@@ -57,14 +57,9 @@ cd ..\..\..
 echo on
 
 perl util\mkfiles.pl >MINFO
-rem perl util\mk1mf.pl VC-MSDOS no-sock >ms\msdos.mak
-rem perl util\mk1mf.pl VC-W31-32 >ms\w31.mak
-perl util\mk1mf.pl dll VC-W31-32 >ms\w31dll.mak
 perl util\mk1mf.pl nasm VC-WIN32 >ms\nt.mak
 perl util\mk1mf.pl dll nasm VC-WIN32 >ms\ntdll.mak
 perl util\mk1mf.pl nasm BC-NT >ms\bcb.mak
 
-perl util\mkdef.pl 16 libeay > ms\libeay16.def
 perl util\mkdef.pl 32 libeay > ms\libeay32.def
-perl util\mkdef.pl 16 ssleay > ms\ssleay16.def
 perl util\mkdef.pl 32 ssleay > ms\ssleay32.def
