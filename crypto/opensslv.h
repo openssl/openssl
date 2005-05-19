032bb2a2c53aba207cf5d208ff3f2a0a3bd7b587
Richard Levitte <levitte@openssl.org>
2005-05-19 19:45:53 +0000

    Tagging of 0.9.8-beta1 is done, time to update the version numbers to the next beta (beta2).

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,11 +25,11 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x00908001L
+#define OPENSSL_VERSION_NUMBER	0x00908002L
 #ifdef OPENSSL_FIPS
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8-fips-beta1 19 May 2005"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8-fips-beta2-dev XX xxx XXXX"
 #else
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8-beta1 19 May 2005"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8-beta2-dev XX xxx XXXX"
 #endif
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
