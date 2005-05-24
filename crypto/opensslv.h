7d9e1321c7716d63501fe9d8ed6b0fecaa0168ec
Richard Levitte <levitte@openssl.org>
2005-05-24 03:57:12 +0000

    Forgot to change the version number itself.

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,7 +25,7 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x00908002L
+#define OPENSSL_VERSION_NUMBER	0x00908003L
 #ifdef OPENSSL_FIPS
 #define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8-fips-beta3 XX xxx XXXX"
 #else
