fcd12fc0f13ea2f578260aafe357460b90b5e9ff
Richard Levitte <levitte@openssl.org>
2005-06-06 00:39:18 +0000

    Time to release 0.9.8 beta 4. The tag will be OpenSSL_0_9_8-beta4

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -27,9 +27,9 @@
  */
 #define OPENSSL_VERSION_NUMBER	0x00908004L
 #ifdef OPENSSL_FIPS
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8-fips-beta4-dev XX xxx XXXX"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8-fips-beta4 06 Jun 2005"
 #else
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8-beta4-dev XX xxx XXXX"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8-beta4 06 Jun 2005"
 #endif
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
