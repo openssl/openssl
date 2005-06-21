09a7f2e734f20a69184cc7747b377358955fa7c0
Richard Levitte <levitte@openssl.org>
2005-06-21 05:49:47 +0000

    Prepare for a release of 0.9.8-beta6.  This is supposed to be the final beta. The tag will be OpenSSL_0_9_8-beta6

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -27,9 +27,9 @@
  */
 #define OPENSSL_VERSION_NUMBER	0x00908006L
 #ifdef OPENSSL_FIPS
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8-fips-beta6 XX xxx XXXX"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8-fips-beta6 21 Jun 2005"
 #else
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8-beta6 XX xxx XXXX"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8-beta6 21 Jun 2005"
 #endif
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
