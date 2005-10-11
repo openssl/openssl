49a305e7ef4de3f63d2d88f4a2c79f7dda440ec2
Mark J. Cox <mark@openssl.org>
2005-10-11 10:14:27 +0000

    Bump after tagging for 0.9.7h release

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,11 +25,11 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x0090708fL
+#define OPENSSL_VERSION_NUMBER	0x00907090L
 #ifdef OPENSSL_FIPS
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7h-fips 11 Oct 2005"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7i-fips-dev XX xxx XXXX"
 #else
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7h 11 Oct 2005"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7i-dev XX xxx XXXX"
 #endif
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
