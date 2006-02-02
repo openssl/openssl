0b8ed5de2d19bff1e39c0482e59d918078cd2119
Nils Larsch <nils@openssl.org>
2006-02-02 22:16:45 +0000

    fix if statement: call conn_state() if the BIO is not in the BIO_CONN_S_OK state

--- crypto/bio/bss_conn.c
+++ crypto/bio/bss_conn.c
@@ -469,7 +469,7 @@ static long conn_ctrl(BIO *b, int cmd, long num, void *ptr)
 		break;
 	case BIO_C_DO_STATE_MACHINE:
 		/* use this one to start the connection */
-		if (!(data->state != BIO_CONN_S_OK))
+		if (data->state != BIO_CONN_S_OK)
 			ret=(long)conn_state(b,data);
 		else
 			ret=1;
