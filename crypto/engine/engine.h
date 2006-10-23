08a638237d61382efe507dc27c34289d9605a964
Andy Polyakov <appro@openssl.org>
2006-10-23 07:30:19 +0000

    Allow for mingw cross-compile configuration.

--- crypto/engine/engine.h
+++ crypto/engine/engine.h
@@ -333,10 +333,10 @@ void ENGINE_load_gmp(void);
 void ENGINE_load_nuron(void);
 void ENGINE_load_sureware(void);
 void ENGINE_load_ubsec(void);
+void ENGINE_load_gost(void);
 #endif
 void ENGINE_load_cryptodev(void);
 void ENGINE_load_padlock(void);
-void ENGINE_load_gost(void);
 void ENGINE_load_builtin_engines(void);
 
 /* Get and set global flags (ENGINE_TABLE_FLAG_***) for the implementation
