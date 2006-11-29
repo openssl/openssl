9dc705a2ac604054bf061222cb0672488d4733b3
Bodo Möller <bodo@openssl.org>
2006-11-29 14:44:07 +0000

    fix support for receiving fragmented handshake messages

--- ssl/s3_srvr.c
+++ ssl/s3_srvr.c
@@ -662,9 +662,9 @@ static int ssl3_get_client_hello(SSL *s)
 	 */
 	if (s->state == SSL3_ST_SR_CLNT_HELLO_A)
 		{
-		s->first_packet=1;
 		s->state=SSL3_ST_SR_CLNT_HELLO_B;
 		}
+	s->first_packet=1;
 	n=ssl3_get_message(s,
 		SSL3_ST_SR_CLNT_HELLO_B,
 		SSL3_ST_SR_CLNT_HELLO_C,
@@ -673,6 +673,7 @@ static int ssl3_get_client_hello(SSL *s)
 		&ok);
 
 	if (!ok) return((int)n);
+	s->first_packet=0;
 	d=p=(unsigned char *)s->init_msg;
 
 	/* use version from inside client hello, not from record header
