54a2631eb3142df2639ede89c3c6e258966a5f2a
Nils Larsch <nils@openssl.org>
2006-12-06 16:52:55 +0000

    fix no-ssl2 build

--- ssl/s23_meth.c
+++ ssl/s23_meth.c
@@ -63,13 +63,21 @@
 static SSL_METHOD *ssl23_get_method(int ver);
 static SSL_METHOD *ssl23_get_method(int ver)
 	{
+#ifndef OPENSSL_NO_SSL2
 	if (ver == SSL2_VERSION)
 		return(SSLv2_method());
-	else if (ver == SSL3_VERSION)
+	else
+#endif
+#ifndef OPENSSL_NO_SSL3
+	if (ver == SSL3_VERSION)
 		return(SSLv3_method());
-	else if (ver == TLS1_VERSION)
+	else
+#endif
+#ifndef OPENSSL_NO_TLS1
+	if (ver == TLS1_VERSION)
 		return(TLSv1_method());
 	else
+#endif
 		return(NULL);
 	}
 
