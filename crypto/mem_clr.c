649ab2dcfa7e93110858ef6d0c172acfddc7f6b5
Andy Polyakov <appro@openssl.org>
2007-06-20 17:37:09 +0000

    Optimize OPENSSL_cleanse [from HEAD].

--- crypto/mem_clr.c
+++ crypto/mem_clr.c
@@ -64,12 +64,13 @@ unsigned char cleanse_ctr = 0;
 void OPENSSL_cleanse(void *ptr, size_t len)
 	{
 	unsigned char *p = ptr;
-	size_t loop = len;
+	size_t loop = len, ctr = cleanse_ctr;
 	while(loop--)
 		{
-		*(p++) = cleanse_ctr;
-		cleanse_ctr += (17 + (unsigned char)((unsigned long)p & 0xF));
+		*(p++) = (unsigned char)ctr;
+		ctr += (17 + ((size_t)p & 0xF));
 		}
-	if(memchr(ptr, cleanse_ctr, len))
-		cleanse_ctr += 63;
+	if(p=memchr(ptr, (unsigned char)ctr, len))
+		ctr += (63 + (size_t)p);
+	cleanse_ctr = (unsigned char)ctr;
 	}
