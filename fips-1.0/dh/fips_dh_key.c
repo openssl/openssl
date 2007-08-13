0a6e92a88ff02bebd04feec58903cbadcacaec15
Dr. Stephen Henson <steve@openssl.org>
2007-08-13 13:28:31 +0000

    Include modulus sanity checks.

--- fips-1.0/dh/fips_dh_key.c
+++ fips-1.0/dh/fips_dh_key.c
@@ -188,6 +188,12 @@ static int compute_key(unsigned char *key, const BIGNUM *pub_key, DH *dh)
 	if (ctx == NULL) goto err;
 	BN_CTX_start(ctx);
 	tmp = BN_CTX_get(ctx);
+
+	if (BN_num_bits(dh->p) > OPENSSL_DH_MAX_MODULUS_BITS)
+		{
+		DHerr(DH_F_COMPUTE_KEY,DH_R_MODULUS_TOO_LARGE);
+		goto err;
+		}
 	
 	if (dh->priv_key == NULL)
 		{
