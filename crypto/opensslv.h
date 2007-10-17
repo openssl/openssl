225aeb171e3b6c366b46eb4d5c865a2b95fe088a
Lutz Jänicke <jaenicke@openssl.org>
2007-10-17 07:46:49 +0000

    Work around inconsistent version numbering in 0.9.8f (release). The version code of the release should have been 09086f (6=f, f=release) but accidently it was marked "090870" (which would be "0.9.8g-dev").
    Therefore we now use "090871" for the development of 0.9.8g. Once
    0.9.8g is released, the problem will be "healed". We have never done
    beta releases for 0.9.x-stable patch releases, so 090871 would never
    be used in practice.
    
    PR: #1589

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,7 +25,7 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x00908070L
+#define OPENSSL_VERSION_NUMBER	0x00908071L
 #ifdef OPENSSL_FIPS
 #define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8g-fips-dev xx XXX xxxx"
 #else
