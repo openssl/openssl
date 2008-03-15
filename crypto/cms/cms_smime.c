fd47c36136c7ac9b86f14c69b23a7d2df4fce132
Dr. Stephen Henson <steve@openssl.org>
2008-03-15 00:02:23 +0000

    Return error if no cipher set for encrypted data type.
    Update CHANGES.

--- crypto/cms/cms_smime.c
+++ crypto/cms/cms_smime.c
@@ -227,6 +227,11 @@ CMS_ContentInfo *CMS_EncryptedData_encrypt(BIO *in, const EVP_CIPHER *cipher,
 					unsigned int flags)
 	{
 	CMS_ContentInfo *cms;
+	if (!cipher)
+		{
+		CMSerr(CMS_F_CMS_ENCRYPTEDDATA_ENCRYPT, CMS_R_NO_CIPHER);
+		return NULL;
+		}
 	cms = CMS_ContentInfo_new();
 	if (!cms)
 		return NULL;
