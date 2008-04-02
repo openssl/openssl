28a2759ab8255324c7837e979f8388740d95418d
Dr. Stephen Henson <steve@openssl.org>
2008-04-02 11:18:43 +0000

    Add RFC3394 compatible key wrap algorithm.

--- crypto/aes/aes.h
+++ crypto/aes/aes.h
@@ -130,6 +130,12 @@ void AES_bi_ige_encrypt(const unsigned char *in, unsigned char *out,
 			const AES_KEY *key2, const unsigned char *ivec,
 			const int enc);
 
+int AES_wrap_key(AES_KEY *key, const unsigned char *iv,
+		unsigned char *out,
+		const unsigned char *in, unsigned int inlen);
+int AES_unwrap_key(AES_KEY *key, const unsigned char *iv,
+		unsigned char *out,
+		const unsigned char *in, unsigned int inlen);
 
 #ifdef  __cplusplus
 }
