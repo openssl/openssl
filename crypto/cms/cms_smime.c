e6ef05d5f31ede3e4a27fd6fbf96880f141c6cdd
Dr. Stephen Henson <steve@openssl.org>
2008-04-18 11:18:20 +0000

    Make certs argument work in CMS_sign() add test case. PR:1664

--- crypto/cms/cms_smime.c
+++ crypto/cms/cms_smime.c
@@ -457,23 +457,23 @@ CMS_ContentInfo *CMS_sign(X509 *signcert, EVP_PKEY *pkey, STACK_OF(X509) *certs,
 	{
 	CMS_ContentInfo *cms;
 	int i;
+
 	cms = CMS_ContentInfo_new();
-	if (!cms)
+	if (!cms || !CMS_SignedData_init(cms))
 		goto merr;
+
 	if (pkey && !CMS_add1_signer(cms, signcert, pkey, NULL, flags))
 		{
 		CMSerr(CMS_F_CMS_SIGN, CMS_R_ADD_SIGNER_ERROR);
 		goto err;
 		}
+
 	for (i = 0; i < sk_X509_num(certs); i++)
 		{
 		X509 *x = sk_X509_value(certs, i);
 		if (!CMS_add1_cert(cms, x))
 			goto merr;
 		}
-	/* If no signer or certs initialize signedData */
-	if (!pkey && !i && !CMS_SignedData_init(cms))
-		goto merr;
 
 	if(!(flags & CMS_DETACHED))
 		CMS_set_detached(cms, 0);
