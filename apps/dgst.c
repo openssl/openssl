d13ea8e1844ee9e25b9a3c02831c8d749cb0abf0
Lutz Jänicke <jaenicke@openssl.org>
2008-05-19 07:43:41 +0000

    Provide information about "openssl dgst" -hmac option.

--- apps/dgst.c
+++ apps/dgst.c
@@ -229,6 +229,7 @@ int MAIN(int argc, char **argv)
 		BIO_printf(bio_err,"-keyform arg    key file format (PEM or ENGINE)\n");
 		BIO_printf(bio_err,"-signature file signature to verify\n");
 		BIO_printf(bio_err,"-binary         output in binary form\n");
+		BIO_printf(bio_err,"-hmac key       create hashed MAC with key\n");
 #ifndef OPENSSL_NO_ENGINE
 		BIO_printf(bio_err,"-engine e       use engine e, possibly a hardware device.\n");
 #endif
