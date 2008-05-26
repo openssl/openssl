b0118409a9550a5f5b7b5466bf8b5d57ffc5e843
Lutz Jänicke <jaenicke@openssl.org>
2008-05-26 06:21:10 +0000

    Reword comment to be much shorter to stop other people from complaining about "overcommenting"

--- ssl/ssl_rsa.c
+++ ssl/ssl_rsa.c
@@ -708,11 +708,7 @@ int SSL_CTX_use_certificate_chain_file(SSL_CTX *ctx, const char *file)
 	int ret=0;
 	X509 *x=NULL;
 
-	ERR_clear_error(); /* This function needs to check the error stack
-			      even if the return code(s) of called functions
-			      indicate success.
-			      The error stack must therefore be cleared
-			      before starting. */
+	ERR_clear_error(); /* clear error stack for SSL_CTX_use_certificate() */
 
 	in=BIO_new(BIO_s_file_internal());
 	if (in == NULL)
