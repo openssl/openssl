a29669d78d211b7bdc9690735938c1eb11ef6feb
Dr. Stephen Henson <steve@openssl.org>
2008-05-29 17:20:42 +0000

    Disable CryptoAPI engine compilation by default.

--- util/pl/VC-32.pl
+++ util/pl/VC-32.pl
@@ -283,6 +283,7 @@ sub do_lib_rule
 			{
 			$ex.=' unicows.lib' if ($FLAVOR =~ /NT/);
 			$ex.=' wsock32.lib gdi32.lib advapi32.lib user32.lib';
+			$ex.=' crypt32.lib';
 			$ex.=' bufferoverflowu.lib' if ($FLAVOR =~ /WIN64/);
 			}
 		$ex.=" $zlib_lib" if $zlib_opt == 1 && $target =~ /O_CRYPTO/;
