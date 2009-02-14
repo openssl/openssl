477fd4596feb89fe943130c8b1a4e37a0d3764bb
Dr. Stephen Henson <steve@openssl.org>
2009-02-14 21:49:38 +0000

    PR: 1835 Submitted by: Damien Miller <djm@mindrot.org> Approved by: steve@openssl.org
    Fix various typos.

--- crypto/asn1/a_bytes.c
+++ crypto/asn1/a_bytes.c
@@ -79,7 +79,7 @@ ASN1_STRING *d2i_ASN1_type_bytes(ASN1_STRING **a, const unsigned char **pp,
 
 	if (tag >= 32)
 		{
-		i=ASN1_R_TAG_VALUE_TOO_HIGH;;
+		i=ASN1_R_TAG_VALUE_TOO_HIGH;
 		goto err;
 		}
 	if (!(ASN1_tag2bit(tag) & type))
