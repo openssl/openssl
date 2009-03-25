15d3cd468071eff6d79bfb3d0699cb5f634db2fd
Dr. Stephen Henson <steve@openssl.org>
2009-03-25 12:08:14 +0000

    Aaargh.... wrong version number....

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,7 +25,7 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x009080b0L
+#define OPENSSL_VERSION_NUMBER	0x009080bfL
 #ifdef OPENSSL_FIPS
 #define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8k-fips 25 Mar 2009"
 #else
