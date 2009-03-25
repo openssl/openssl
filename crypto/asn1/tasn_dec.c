c60dca1f95457a9cb6deca7a4c3f81f99fe05b11
Dr. Stephen Henson <steve@openssl.org>
2009-03-25 10:42:34 +0000

    PR: 1868 Submitted by: Paolo Ganci <Paolo.Ganci@AdNovum.CH> Approved by: steve@openssl.org
    Don't set fields to NULL when freeing them up in ASN1 code. On some platforms
    with sizeof(long) < sizeof(char *) this can cause a crash.

--- crypto/asn1/tasn_dec.c
+++ crypto/asn1/tasn_dec.c
@@ -611,7 +611,6 @@ static int asn1_template_ex_d2i(ASN1_VALUE **val,
 
 	err:
 	ASN1_template_free(val, tt);
-	*val = NULL;
 	return 0;
 	}
 
@@ -758,7 +757,6 @@ static int asn1_template_noexp_d2i(ASN1_VALUE **val,
 
 	err:
 	ASN1_template_free(val, tt);
-	*val = NULL;
 	return 0;
 	}
 
