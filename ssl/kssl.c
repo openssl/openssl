caeb429055ad84c236e05018824d448b6377b78e
Dr. Stephen Henson <steve@openssl.org>
2009-04-16 16:43:18 +0000

    Update from 1.0.0-stable.

--- ssl/kssl.c
+++ ssl/kssl.c
@@ -68,11 +68,6 @@
 
 #include <openssl/opensslconf.h>
 
-#define _XOPEN_SOURCE 500 /* glibc2 needs this to declare strptime() */
-#include <time.h>
-#if 0 /* experimental */
-#undef _XOPEN_SOURCE /* To avoid clashes with anything else... */
-#endif
 #include <string.h>
 
 #define KRB5_PRIVATE	1
