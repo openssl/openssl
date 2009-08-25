8a04c6f8948e73cfe2edd6a9caa0a2d52300d979
Richard Levitte <levitte@openssl.org>
2009-08-25 07:10:40 +0000

    Include proper header files for time functions.
    Submitted by Arpadffy Zoltan <Zoltan.Arpadffy@scientificgames.se>

--- ssl/dtls1.h
+++ ssl/dtls1.h
@@ -62,6 +62,10 @@
 
 #include <openssl/buffer.h>
 #include <openssl/pqueue.h>
+#ifdef OPENSSL_SYS_VMS
+#include <resource.h>
+#include <sys/timeb.h>
+#endif
 #ifdef OPENSSL_SYS_WIN32
 /* Needed for struct timeval */
 #include <winsock.h>
