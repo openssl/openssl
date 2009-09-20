ca26ccb083c8a22ad7e3eca273b9eb87910da61b
Dr. Stephen Henson <steve@openssl.org>
2009-09-20 11:39:59 +0000

    add version info for VC-WIN64I too

--- util/mk1mf.pl
+++ util/mk1mf.pl
@@ -639,7 +639,7 @@ foreach (values %lib_nam)
 
 # hack to add version info on MSVC
 if (($platform eq "VC-WIN32") || ($platform eq "VC-WIN64A)
-				|| ($platform eq "VC-NT")) {
+	|| ($platform eq "VC-WIN64I") || ($platform eq "VC-NT")) {
     $rules.= <<"EOF";
 \$(OBJ_D)\\\$(CRYPTO).res: ms\\version32.rc
 	\$(RSC) /fo"\$(OBJ_D)\\\$(CRYPTO).res" /d CRYPTO ms\\version32.rc
@@ -831,7 +831,7 @@ sub do_defs
 		$ret.=$t;
 		}
 	# hack to add version info on MSVC
-	if ($shlib && (($platform eq "VC-WIN32") || ($platform eq "VC-WIN64A) || ($platform eq "VC-NT")))
+	if ($shlib && (($platform eq "VC-WIN32") || ($platfrom eq "VC-WIN64I") || ($platform eq "VC-WIN64A) || ($platform eq "VC-NT")))
 		{
 		if ($var eq "CRYPTOOBJ")
 			{ $ret.="\$(OBJ_D)\\\$(CRYPTO).res "; }
