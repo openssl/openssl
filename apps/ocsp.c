bc8c5fe58d8b69f25382beddb13cb13764823796
Dr. Stephen Henson <steve@openssl.org>
2009-09-30 21:35:26 +0000

    Free SSL_CTX after BIO

--- apps/ocsp.c
+++ apps/ocsp.c
@@ -1385,10 +1385,10 @@ OCSP_RESPONSE *process_responder(BIO *err, OCSP_REQUEST *req,
 	if (!resp)
 		BIO_printf(bio_err, "Error querying OCSP responsder\n");
 	end:
-	if (ctx)
-		SSL_CTX_free(ctx);
 	if (cbio)
 		BIO_free_all(cbio);
+	if (ctx)
+		SSL_CTX_free(ctx);
 	return resp;
 	}
 
