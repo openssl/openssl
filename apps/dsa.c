804196a418fc0ec6ceb936cc68057bf39312ead1
Dr. Stephen Henson <steve@openssl.org>
2009-10-01 00:26:07 +0000

    PR: 2061 Submitted by: Julia Lawall <julia@diku.dk> Approved by: steve@openssl.org
    Correct i2b_PVK_bio error handling in rsa.c, dsa.c

--- apps/dsa.c
+++ apps/dsa.c
@@ -351,7 +351,7 @@ bad:
 		BIO_printf(bio_err,"bad output format specified for outfile\n");
 		goto end;
 		}
-	if (!i)
+	if (i <= 0)
 		{
 		BIO_printf(bio_err,"unable to write private key\n");
 		ERR_print_errors(bio_err);
