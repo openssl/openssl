ad187f89053549cf3fe481ded227e61984fb0b61
Dr. Stephen Henson <steve@openssl.org>
2009-10-04 16:52:35 +0000

    Fix unitialized warnings

--- crypto/asn1/a_mbstr.c
+++ crypto/asn1/a_mbstr.c
@@ -93,7 +93,7 @@ int ASN1_mbstring_ncopy(ASN1_STRING **out, const unsigned char *in, int len,
 	int str_type;
 	int ret;
 	char free_out;
-	int outform, outlen;
+	int outform, outlen = 0;
 	ASN1_STRING *dest;
 	unsigned char *p;
 	int nchar;
