53e97e74334b7e46668640b3b4739650222c4d8d
Dr. Stephen Henson <steve@openssl.org>
2010-01-20 15:40:27 +0000

    update version for next beta if we have one...

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,11 +25,11 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x10000005L
+#define OPENSSL_VERSION_NUMBER	0x10000006L
 #ifdef OPENSSL_FIPS
-#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.0-fips-beta5 20 Jan 2010"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.0-fips-beta6-dev xx XXX xxxx"
 #else
-#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.0-beta5 20 Jan 2010"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.0-beta6-dev xx XXX xxxx"
 #endif
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
