714044cc0369c9672a5908bd003d2f4958a1ac66
Dr. Stephen Henson <steve@openssl.org>
2010-01-24 13:52:38 +0000

    oops revert test code from previous commit

--- ssl/s3_pkt.c
+++ ssl/s3_pkt.c
@@ -979,6 +979,7 @@ start:
 		(s->session != NULL) && (s->session->cipher != NULL))
 		{
 		s->s3->handshake_fragment_len = 0;
+
 		if ((s->s3->handshake_fragment[1] != 0) ||
 			(s->s3->handshake_fragment[2] != 0) ||
 			(s->s3->handshake_fragment[3] != 0))
