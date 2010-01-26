1cdb7854a513b29545135496a51dc02d00088f69
Dr. Stephen Henson <steve@openssl.org>
2010-01-26 18:07:41 +0000

    PR: 2138 Submitted by: Kevin Regan <k.regan@f5.com>
    Clear stat structure if -DPURIFY is set to avoid problems on some
    platforms which include unitialised fields.

--- crypto/rand/randfile.c
+++ crypto/rand/randfile.c
@@ -118,6 +118,14 @@ int RAND_load_file(const char *file, long bytes)
 	if (file == NULL) return(0);
 
 #ifndef OPENSSL_NO_POSIX_IO
+#ifdef PURIFY
+	/* struct stat can have padding and unused fields that may not be
+	 * initialized in the call to stat(). We need to clear the entire
+	 * structure before calling RAND_add() to avoid complaints from
+	 * applications such as Valgrind.
+	 */
+	memset(&sb, 0, sizeof(sb));
+#endif
 	if (stat(file,&sb) < 0) return(0);
 	RAND_add(&sb,sizeof(sb),0.0);
 #endif
