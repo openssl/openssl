aa7f5baad27729b819bced7040957dc401ed2b46
Dr. Stephen Henson <steve@openssl.org>
2010-02-03 18:19:05 +0000

    don't assume 0x is at start of string

--- crypto/perlasm/x86_64-xlate.pl
+++ crypto/perlasm/x86_64-xlate.pl
@@ -578,7 +578,7 @@ my %globals;
 				    my $last = pop(@arr);
 				    my $conv = sub  {	my $var=shift;
 							$var=~s/^(0b[0-1]+)/oct($1)/eig;
-							$var=~s/^0x([0-9a-f]+)/0$1h/ig if ($masm);
+							$var=~s/0x([0-9a-f]+)/0$1h/ig if ($masm);
 							if ($sz eq "D" && ($current_segment=~/.[px]data/ || $dir eq ".rva"))
 							{ $var=~s/([_a-z\$\@][_a-z0-9\$\@]*)/$nasm?"$1 wrt ..imagebase":"imagerel $1"/egi; }
 							$var;
