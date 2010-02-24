385a488c4313011ee7cf665d203af3f718a2a854
Dr. Stephen Henson <steve@openssl.org>
2010-02-24 15:24:19 +0000

    prevent warning

--- crypto/evp/e_aes.c
+++ crypto/evp/e_aes.c
@@ -99,9 +99,11 @@ IMPLEMENT_AES_CFBR(256,8)
 static int aes_counter (EVP_CIPHER_CTX *ctx, unsigned char *out,
 		const unsigned char *in, size_t len)
 {
+	unsigned int num;
 	AES_ctr128_encrypt (in,out,len,
 		&((EVP_AES_KEY *)ctx->cipher_data)->ks,
-		ctx->iv,ctx->buf,&ctx->num);
+		ctx->iv,ctx->buf,&num);
+	ctx->num = (size_t)num;
 	return 1;
 }
 
