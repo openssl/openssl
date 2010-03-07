49436b59b505d9e49ac06e4d544d748f78689036
Dr. Stephen Henson <steve@openssl.org>
2010-03-07 15:52:41 +0000

    oops, make EVP ctr mode work again

--- crypto/evp/e_aes.c
+++ crypto/evp/e_aes.c
@@ -100,6 +100,7 @@ static int aes_counter (EVP_CIPHER_CTX *ctx, unsigned char *out,
 		const unsigned char *in, size_t len)
 {
 	unsigned int num;
+	num = ctx->num;
 	AES_ctr128_encrypt (in,out,len,
 		&((EVP_AES_KEY *)ctx->cipher_data)->ks,
 		ctx->iv,ctx->buf,&num);
