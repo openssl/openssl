1eda14b44f4c1a958fbe2e9c4a6695eb08bd27c3
Dr. Stephen Henson <steve@openssl.org>
2010-03-27 18:28:24 +0000

    PR: 1813 Submitted by: Torsten Hilbrich <torsten.hilbrich@secunet.com>
    Fix memory leak when engine name cannot be loaded.

--- crypto/engine/eng_list.c
+++ crypto/engine/eng_list.c
@@ -412,6 +412,7 @@ ENGINE *ENGINE_by_id(const char *id)
 		return iterator;
 		}
 notfound:
+	ENGINE_free(iterator);
 	ENGINEerr(ENGINE_F_ENGINE_BY_ID,ENGINE_R_NO_SUCH_ENGINE);
 	ERR_add_error_data(2, "id=", id);
 	return NULL;
