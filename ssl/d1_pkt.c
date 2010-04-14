b7463c8818a3582ea54a7e2a86651c4e094df465
Dr. Stephen Henson <steve@openssl.org>
2010-04-14 00:03:27 +0000

    PR: 2228 Submitted By: Robin Seggelmann <seggelmann@fh-muenster.de>
    Fix DTLS buffer record MAC failure bug.

--- ssl/d1_pkt.c
+++ ssl/d1_pkt.c
@@ -196,6 +196,9 @@ dtls1_copy_record(SSL *s, pitem *item)
     s->packet_length = rdata->packet_length;
     memcpy(&(s->s3->rbuf), &(rdata->rbuf), sizeof(SSL3_BUFFER));
     memcpy(&(s->s3->rrec), &(rdata->rrec), sizeof(SSL3_RECORD));
+	
+	/* Set proper sequence number for mac calculation */
+	memcpy(&(s->s3->read_sequence[2]), &(rdata->packet[5]), 6);
     
     return(1);
     }
