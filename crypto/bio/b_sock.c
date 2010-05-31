eadfa019b3c82d07fb173eb34f09604c82e99a1f
Dr. Stephen Henson <steve@openssl.org>
2010-05-31 13:18:21 +0000

    fix PR#2261 in a different way

--- crypto/bio/b_sock.c
+++ crypto/bio/b_sock.c
@@ -731,7 +731,14 @@ again:
 #ifdef SO_REUSEADDR
 		err_num=get_last_socket_error();
 		if ((bind_mode == BIO_BIND_REUSEADDR_IF_UNUSED) &&
+#ifdef OPENSSL_SYS_WINDOWS
+			/* Some versions of Windows define EADDRINUSE to
+			 * a dummy value.
+			 */
+			(err_num == WSAEADDRINUSE))
+#else
 			(err_num == EADDRINUSE))
+#endif
 			{
 			client = server;
 			if (h == NULL || strcmp(h,"*") == 0)
