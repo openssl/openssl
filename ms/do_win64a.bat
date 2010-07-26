39438ff6e2d5a39ca5f260849e5f56844848b2c3
Andy Polyakov <appro@openssl.org>
2010-07-26 21:58:23 +0000

    ms/: update do_win64*.bat and remove redundant mingw32.bat.

--- ms/do_win64a.bat
+++ ms/do_win64a.bat
@@ -12,8 +12,8 @@ perl ms\uplink-x86_64.pl masm > ms\uptable.asm
 ml64 -c -Foms\uptable.obj ms\uptable.asm
 
 :proceed
-perl util\mk1mf.pl no-asm VC-WIN64A >ms\nt.mak
-perl util\mk1mf.pl dll no-asm VC-WIN64A >ms\ntdll.mak
+perl util\mk1mf.pl VC-WIN64A >ms\nt.mak
+perl util\mk1mf.pl dll VC-WIN64A >ms\ntdll.mak
 
 perl util\mkdef.pl 32 libeay > ms\libeay32.def
 perl util\mkdef.pl 32 ssleay > ms\ssleay32.def
