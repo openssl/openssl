0985473636b8bb998eb887c28489cfe5e57905a5
Andy Polyakov <appro@openssl.org>
2010-09-22 08:43:09 +0000

    sha1-mips.pl, mips-mont.pl: unify MIPS assembler modules in respect to ABI and binutils.

--- crypto/sha/asm/sha1-mips.pl
+++ crypto/sha/asm/sha1-mips.pl
@@ -14,12 +14,62 @@
 # vectorized Xupdate on MIPSIII/IV, but the goal was to code MIPS32-
 # compatible subroutine. There is room for minor optimization on
 # little-endian platforms...
-#
-# The code is somewhat IRIX-centric, i.e. is likely to require minor
-# adaptations for other OSes...
 
-for (@ARGV) {   $big_endian=1 if (/\-DB_ENDIAN/);
-                $big_endian=0 if (/\-DL_ENDIAN/);   }
+######################################################################
+# There is a number of MIPS ABI in use, O32 and N32/64 are most
+# widely used. Then there is a new contender: NUBI. It appears that if
+# one picks the latter, it's possible to arrange code in ABI neutral
+# manner. Therefore let's stick to NUBI register layout:
+#
+($zero,$at,$t0,$t1,$t2)=map("\$$_",(0..2,24,25));
+($a0,$a1,$a2,$a3,$a4,$a5,$a6,$a7)=map("\$$_",(4..11));
+($s0,$s1,$s2,$s3,$s4,$s5,$s6,$s7,$s8,$s9,$s10,$s11)=map("\$$_",(12..23));
+($gp,$tp,$sp,$fp,$ra)=map("\$$_",(3,28..31));
+#
+# The return value is placed in $a0. Following coding rules facilitate
+# interoperability:
+#
+# - never ever touch $tp, "thread pointer", former $gp;
+# - copy return value to $t0, former $v0 [or to $a0 if you're adapting
+#   old code];
+# - on O32 populate $a4-$a7 with 'lw $aN,4*N($sp)' if necessary;
+#
+# For reference here is register layout for N32/64 MIPS ABIs:
+#
+# ($zero,$at,$v0,$v1)=map("\$$_",(0..3));
+# ($a0,$a1,$a2,$a3,$a4,$a5,$a6,$a7)=map("\$$_",(4..11));
+# ($t0,$t1,$t2,$t3,$t8,$t9)=map("\$$_",(12..15,24,25));
+# ($s0,$s1,$s2,$s3,$s4,$s5,$s6,$s7)=map("\$$_",(16..23));
+# ($gp,$sp,$fp,$ra)=map("\$$_",(28..31));
+#
+$flavour = shift; # supported flavours are o32,n32,64,nubi32,nubi64
+
+if ($flavour =~ /64|n32/i) {
+	$PTR_ADD="dadd";	# incidentally works even on n32
+	$PTR_SUB="dsub";	# incidentally works even on n32
+	$REG_S="sd";
+	$REG_L="ld";
+	$PTR_SLL="dsll";	# incidentally works even on n32
+	$SZREG=8;
+} else {
+	$PTR_ADD="add";
+	$PTR_SUB="sub";
+	$REG_S="sw";
+	$REG_L="lw";
+	$PTR_SLL="sll";
+	$SZREG=4;
+}
+$SAVED_REGS_MASK = ($flavour =~ /nubi/i) ? 0x00fff008 : 0x00ff0000;
+#
+# <appro@openssl.org>
+#
+######################################################################
+
+for (@ARGV) {	$big_endian=1 if (/\-DB_ENDIAN/);
+		$big_endian=0 if (/\-DL_ENDIAN/);
+		$output=$_ if (/^\w[\w\-]*\.\w+$/);   }
+open STDOUT,">$output";
+
 if (!defined($big_endian))
             {   $big_endian=(unpack('L',pack('N',1))==1);   }
 
@@ -37,9 +87,9 @@ $B="\$2";
 $C="\$3";
 $D="\$7";
 $E="\$24";	@V=($A,$B,$C,$D,$E);
-$t0="\$25";	# jp,t9
-$t1="\$28";	# gp
-$t2="\$30";	# fp,s8
+$t0="\$25";
+$t1=$num;	# $num is offloaded to stack
+$t2="\$30";	# fp
 $K="\$31";	# ra
 
 $FRAMESIZE=16;
@@ -187,9 +237,6 @@ ___
 }
 
 $code=<<___;
-#include <asm.h>
-#include <regdef.h>
-
 .text
 
 .set	noat
@@ -198,22 +245,32 @@ $code=<<___;
 .globl	sha1_block_data_order
 .ent	sha1_block_data_order
 sha1_block_data_order:
-	.frame	sp,$FRAMESIZE*SZREG,zero
-	.mask	0xd0ff0000,-$FRAMESIZE*SZREG
+	.frame	$sp,$FRAMESIZE*$SZREG,$ra
+	.mask	0xd0000000|$SAVED_REGS_MASK,-$SZREG
 	.set	noreorder
-	PTR_SUB	sp,$FRAMESIZE*SZREG
-	REG_S	\$31,($FRAMESIZE-1)*SZREG(sp)
-	REG_S	\$30,($FRAMESIZE-2)*SZREG(sp)
-	REG_S	\$28,($FRAMESIZE-3)*SZREG(sp)
-	REG_S	\$23,($FRAMESIZE-4)*SZREG(sp)
-	REG_S	\$22,($FRAMESIZE-5)*SZREG(sp)
-	REG_S	\$21,($FRAMESIZE-6)*SZREG(sp)
-	REG_S	\$20,($FRAMESIZE-7)*SZREG(sp)
-	REG_S	\$19,($FRAMESIZE-8)*SZREG(sp)
-	REG_S	\$18,($FRAMESIZE-9)*SZREG(sp)
-	REG_S	\$17,($FRAMESIZE-10)*SZREG(sp)
-	REG_S	\$16,($FRAMESIZE-11)*SZREG(sp)
-
+	$PTR_SUB $sp,$FRAMESIZE*$SZREG
+	$REG_S	$ra,($FRAMESIZE-1)*$SZREG($sp)
+	$REG_S	$fp,($FRAMESIZE-2)*$SZREG($sp)
+	$REG_S	$s11,($FRAMESIZE-3)*$SZREG($sp)
+	$REG_S	$s10,($FRAMESIZE-4)*$SZREG($sp)
+	$REG_S	$s9,($FRAMESIZE-5)*$SZREG($sp)
+	$REG_S	$s8,($FRAMESIZE-6)*$SZREG($sp)
+	$REG_S	$s7,($FRAMESIZE-7)*$SZREG($sp)
+	$REG_S	$s6,($FRAMESIZE-8)*$SZREG($sp)
+	$REG_S	$s5,($FRAMESIZE-9)*$SZREG($sp)
+	$REG_S	$s4,($FRAMESIZE-10)*$SZREG($sp)
+___
+$code.=<<___ if ($flavour =~ /nubi/i);
+	$REG_S	$s3,($FRAMESIZE-11)*$SZREG($sp)
+	$REG_S	$s2,($FRAMESIZE-12)*$SZREG($sp)
+	$REG_S	$s1,($FRAMESIZE-13)*$SZREG($sp)
+	$REG_S	$s0,($FRAMESIZE-14)*$SZREG($sp)
+	$REG_S	$gp,($FRAMESIZE-15)*$SZREG($sp)
+___
+$code.=<<___;
+	$PTR_SLL $num,6
+	$PTR_ADD $num,$inp
+	$REG_S	$num,0($sp)
 	lw	$A,0($ctx)
 	lw	$B,4($ctx)
 	lw	$C,8($ctx)
@@ -246,6 +303,9 @@ $code.=<<___;
 ___
 for (;$i<80;$i++)	{ &BODY_20_39($i,@V); unshift(@V,pop(@V)); }
 $code.=<<___;
+	$PTR_ADD $inp,64
+	$REG_L	$num,0($sp)
+
 	addu	$A,$X[0]
 	addu	$B,$X[1]
 	sw	$A,0($ctx)
@@ -253,29 +313,38 @@ $code.=<<___;
 	addu	$D,$X[3]
 	sw	$B,4($ctx)
 	addu	$E,$X[4]
-	PTR_SUB	$num,1
 	sw	$C,8($ctx)
 	sw	$D,12($ctx)
 	sw	$E,16($ctx)
 	.set	noreorder
-	bnez	$num,.Loop
-	PTR_ADD	$inp,64
+	bne	$inp,$num,.Loop
+	nop
 
 	.set	noreorder
-	REG_L	\$31,($FRAMESIZE-1)*SZREG(sp)
-	REG_L	\$30,($FRAMESIZE-2)*SZREG(sp)
-	REG_L	\$28,($FRAMESIZE-3)*SZREG(sp)
-	REG_L	\$23,($FRAMESIZE-4)*SZREG(sp)
-	REG_L	\$22,($FRAMESIZE-5)*SZREG(sp)
-	REG_L	\$21,($FRAMESIZE-6)*SZREG(sp)
-	REG_L	\$20,($FRAMESIZE-7)*SZREG(sp)
-	REG_L	\$19,($FRAMESIZE-8)*SZREG(sp)
-	REG_L	\$18,($FRAMESIZE-9)*SZREG(sp)
-	REG_L	\$17,($FRAMESIZE-10)*SZREG(sp)
-	REG_L	\$16,($FRAMESIZE-11)*SZREG(sp)
-	jr	ra
-	PTR_ADD	sp,$FRAMESIZE*SZREG
+	$REG_L	$ra,($FRAMESIZE-1)*$SZREG($sp)
+	$REG_L	$fp,($FRAMESIZE-2)*$SZREG($sp)
+	$REG_L	$s11,($FRAMESIZE-3)*$SZREG($sp)
+	$REG_L	$s10,($FRAMESIZE-4)*$SZREG($sp)
+	$REG_L	$s9,($FRAMESIZE-5)*$SZREG($sp)
+	$REG_L	$s8,($FRAMESIZE-6)*$SZREG($sp)
+	$REG_L	$s7,($FRAMESIZE-7)*$SZREG($sp)
+	$REG_L	$s6,($FRAMESIZE-8)*$SZREG($sp)
+	$REG_L	$s5,($FRAMESIZE-9)*$SZREG($sp)
+	$REG_L	$s4,($FRAMESIZE-10)*$SZREG($sp)
+___
+$code.=<<___ if ($flavour =~ /nubi/i);
+	$REG_L	$s3,($FRAMESIZE-11)*$SZREG($sp)
+	$REG_L	$s2,($FRAMESIZE-12)*$SZREG($sp)
+	$REG_L	$s1,($FRAMESIZE-13)*$SZREG($sp)
+	$REG_L	$s0,($FRAMESIZE-14)*$SZREG($sp)
+	$REG_L	$gp,($FRAMESIZE-15)*$SZREG($sp)
+___
+$code.=<<___;
+	jr	$ra
+	$PTR_ADD $sp,$FRAMESIZE*$SZREG
 .end	sha1_block_data_order
+.rdata
+.asciiz	"SHA1 for MIPS, CRYPTOGAMS by <appro\@openssl.org>"
 ___
 print $code;
 close STDOUT;
