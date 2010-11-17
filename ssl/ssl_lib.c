ac7797a7225b2b014a1dd83db610ec4f4aff597a
Dr. Stephen Henson <steve@openssl.org>
2010-11-17 18:17:08 +0000

    oops, reinstate TLSv1 string

--- ssl/ssl_lib.c
+++ ssl/ssl_lib.c
@@ -2567,6 +2567,8 @@ const char *SSL_get_version(const SSL *s)
 	{
 	if (s->version == TLS1_1_VERSION)
 		return("TLSv1.1");
+	else if (s->version == TLS1_VERSION)
+		return("TLSv1");
 	else if (s->version == SSL3_VERSION)
 		return("SSLv3");
 	else if (s->version == SSL3_VERSION)
