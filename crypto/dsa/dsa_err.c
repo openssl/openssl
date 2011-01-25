c31945e6822450a894fa44ba26b553d3e8b9ac5b
Dr. Stephen Henson <steve@openssl.org>
2011-01-25 16:02:27 +0000

    recalculate DSA signature if r or s is zero (FIPS 186-3 requirement)

--- crypto/dsa/dsa_err.c
+++ crypto/dsa/dsa_err.c
@@ -105,6 +105,7 @@ static ERR_STRING_DATA DSA_str_reasons[]=
 {ERR_REASON(DSA_R_INVALID_DIGEST_TYPE)   ,"invalid digest type"},
 {ERR_REASON(DSA_R_MISSING_PARAMETERS)    ,"missing parameters"},
 {ERR_REASON(DSA_R_MODULUS_TOO_LARGE)     ,"modulus too large"},
+{ERR_REASON(DSA_R_NEED_NEW_SETUP_VALUES) ,"need new setup values"},
 {ERR_REASON(DSA_R_NO_PARAMETERS_SET)     ,"no parameters set"},
 {ERR_REASON(DSA_R_PARAMETER_ENCODING_ERROR),"parameter encoding error"},
 {0,NULL}
