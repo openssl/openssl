0912fae6ce910ea865f014f6f26fbe9928307128
Andy Polyakov <appro@openssl.org>
2011-02-12 16:46:10 +0000

    dso_dlfcn.c: make it work on Tru64 4.0 [from HEAD]. PR: 2316

--- crypto/dso/dso_dlfcn.c
+++ crypto/dso/dso_dlfcn.c
@@ -85,6 +85,7 @@ DSO_METHOD *DSO_METHOD_dlfcn(void)
 # define HAVE_DLINFO 1
 # if defined(_AIX) || defined(__CYGWIN__) || \
      defined(__SCO_VERSION__) || defined(_SCO_ELF) || \
+     (defined(__osf__) && !defined(RTLD_NEXT))     || \
      (defined(__OpenBSD__) && !defined(RTLD_SELF))
 #  undef HAVE_DLINFO
 # endif
