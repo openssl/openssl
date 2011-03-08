bc91494e064ebdcff68f987947f97e404fbca0b5
Dr. Stephen Henson <steve@openssl.org>
2011-03-08 19:07:26 +0000

    New SP 800-56A compliant version of DH_compute_key().

--- crypto/dh/dh.h
+++ crypto/dh/dh.h
@@ -202,6 +202,7 @@ int	DH_check(const DH *dh,int *codes);
 int	DH_check_pub_key(const DH *dh,const BIGNUM *pub_key, int *codes);
 int	DH_generate_key(DH *dh);
 int	DH_compute_key(unsigned char *key,const BIGNUM *pub_key,DH *dh);
+int	DH_compute_key_padded(unsigned char *key,const BIGNUM *pub_key,DH *dh);
 DH *	d2i_DHparams(DH **a,const unsigned char **pp, long length);
 int	i2d_DHparams(const DH *a,unsigned char **pp);
 #ifndef OPENSSL_NO_FP_API
