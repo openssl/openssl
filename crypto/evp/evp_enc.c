4fc02f1229e2b692ec52f01a38f8cba93c13ec84
Dr. Stephen Henson <steve@openssl.org>
2011-03-21 17:37:27 +0000

    Use a signed value to check return value of do_cipher().

--- crypto/evp/evp_enc.c
+++ crypto/evp/evp_enc.c
@@ -368,11 +368,11 @@ int EVP_EncryptFinal_ex(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl)
 
 	if (ctx->cipher->flags & EVP_CIPH_FLAG_CUSTOM_CIPHER)
 		{
-		i = ctx->cipher->do_cipher(ctx, out, NULL, 0);
-		if (i < 0)
+		ret = ctx->cipher->do_cipher(ctx, out, NULL, 0);
+		if (ret < 0)
 			return 0;
 		else 
-			*outl = i;
+			*outl = ret;
 		return 1;
 		}
 
