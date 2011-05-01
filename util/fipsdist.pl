1c6807c9d619243a86fcaa86143a08293474e8ea
Dr. Stephen Henson <steve@openssl.org>
2011-05-01 16:46:28 +0000

    Add ppc_cap.c to restricted tarball.

--- util/fipsdist.pl
+++ util/fipsdist.pl
@@ -17,6 +17,7 @@ foreach (@objlist) { $tarobjs{"$1.c"} = 1 if /([^\/]+).o$/};
 
 $tarobjs{"ncbc_enc.c"} = 1;
 $tarobjs{"mem_clr.c"} = 1;
+$tarobjs{"ppc_cap.c"} = 1;
 
 foreach (split / /, $ENV{LINKDIRS} ) { $cdirs{$_} = 1 };
 
