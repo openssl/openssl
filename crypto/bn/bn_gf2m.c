8d3cdd5b58bcd5c30811f4c7792cf09c9dc86d12
Dr. Stephen Henson <steve@openssl.org>
2011-05-05 14:47:38 +0000

    Fix warning of signed/unsigned comparison.

--- crypto/bn/bn_gf2m.c
+++ crypto/bn/bn_gf2m.c
@@ -368,7 +368,7 @@ int	BN_GF2m_mod(BIGNUM *r, const BIGNUM *a, const BIGNUM *p)
 	bn_check_top(a);
 	bn_check_top(p);
 	ret = BN_GF2m_poly2arr(p, arr, sizeof(arr)/sizeof(arr[0]));
-	if (!ret || ret > sizeof(arr)/sizeof(arr[0]))
+	if (!ret || ret > (int)(sizeof(arr)/sizeof(arr[0])))
 		{
 		BNerr(BN_F_BN_GF2M_MOD,BN_R_INVALID_LENGTH);
 		return 0;
