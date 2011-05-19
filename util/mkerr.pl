1a5538251f18520c292fd24b8eb386c4471a12f2
Dr. Stephen Henson <steve@openssl.org>
2011-05-19 17:56:12 +0000

    update date

--- util/mkerr.pl
+++ util/mkerr.pl
@@ -576,7 +576,7 @@ EOF
 	print OUT <<"EOF";
 /* $cfile */
 /* ====================================================================
- * Copyright (c) 1999-2010 The OpenSSL Project.  All rights reserved.
+ * Copyright (c) 1999-2011 The OpenSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
