2e75ed332fdc4d2599a8db40379c21dfb47c22b1
Andy Polyakov <appro@openssl.org>
2011-05-21 10:17:02 +0000

    ppccap.c: addenum to recent EC optimizations.

--- crypto/ppccap.c
+++ crypto/ppccap.c
@@ -22,7 +22,7 @@ int bn_mul_mont(BN_ULONG *rp, const BN_ULONG *ap, const BN_ULONG *bp, const BN_U
 	if (sizeof(size_t)==4)
 		{
 #if (defined(__APPLE__) && defined(__MACH__))
-		if ((OPENSSL_ppccap_P&PPC_FPU64))
+		if (num>=8 && (num&3)==0 && (OPENSSL_ppccap_P&PPC_FPU64))
 			return bn_mul_mont_fpu64(rp,ap,bp,np,n0,num);
 #else
 		/* boundary of 32 was experimentally determined on
