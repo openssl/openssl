1e368ab08fccdb824cb91bd7765880de0dbf2ea8
Dr. Stephen Henson <steve@openssl.org>
2011-05-25 14:43:47 +0000

    Fix the ECDSA timing attack mentioned in the paper at:
    	http://eprint.iacr.org/2011/232.pdf
    
    Thanks to the original authors Billy Bob Brumley and Nicola Tuveri for
    bringing this to our attention.

--- crypto/ecdsa/ecs_ossl.c
+++ crypto/ecdsa/ecs_ossl.c
@@ -144,6 +144,16 @@ static int ecdsa_sign_setup(EC_KEY *eckey, BN_CTX *ctx_in, BIGNUM **kinvp,
 			}
 		while (BN_is_zero(k));
 
+#ifdef ECDSA_POINT_MUL_NO_CONSTTIME
+		/* We do not want timing information to leak the length of k,
+		 * so we compute G*k using an equivalent scalar of fixed
+		 * bit-length. */
+
+		if (!BN_add(k, k, order)) goto err;
+		if (BN_num_bits(k) <= BN_num_bits(order))
+			if (!BN_add(k, k, order)) goto err;
+#endif /* def(ECDSA_POINT_MUL_NO_CONSTTIME) */
+
 		/* compute r the x-coordinate of generator * k */
 		if (!EC_POINT_mul(group, tmp_point, k, NULL, NULL, ctx))
 		{
