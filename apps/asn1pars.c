565c15363c0530f890f58283e5a43dbfe17ea7fd
Dr. Stephen Henson <steve@openssl.org>
2011-05-25 15:05:56 +0000

    PR: 2527 Submitted by: Marcus Meissner <meissner@suse.de> Reviewed by: steve
    Set cnf to NULL to avoid possible double free.

--- apps/asn1pars.c
+++ apps/asn1pars.c
@@ -408,6 +408,7 @@ static int do_generate(BIO *bio, char *genstr, char *genconf, BUF_MEM *buf)
 
 	atyp = ASN1_generate_nconf(genstr, cnf);
 	NCONF_free(cnf);
+	cnf = NULL;
 
 	if (!atyp)
 		return -1;
