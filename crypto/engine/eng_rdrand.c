f744bcfd7302534c18180d821bdab0b07bcc852b
Andy Polyakov <appro@openssl.org>
2011-08-14 08:30:56 +0000

    eng_rdrand.c: make it link in './config 386' case.

--- crypto/engine/eng_rdrand.c
+++ crypto/engine/eng_rdrand.c
@@ -57,7 +57,7 @@
 
 #if (defined(__i386)   || defined(__i386__)   || defined(_M_IX86) || \
      defined(__x86_64) || defined(__x86_64__) || \
-     defined(_M_AMD64) || defined (_M_X64)) && !defined(OPENSSL_NO_ASM)
+     defined(_M_AMD64) || defined (_M_X64)) && defined(OPENSSL_CPUID_OBJ)
 
 size_t OPENSSL_ia32_rdrand(void);
 
