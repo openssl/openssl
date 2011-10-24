482cdf24895ae454417866a22b8850cc53ef1235
Dr. Stephen Henson <steve@openssl.org>
2011-10-24 13:23:51 +0000

    typo

--- crypto/armcap.c
+++ crypto/armcap.c
@@ -30,7 +30,7 @@ unsigned int OPENSSL_rdtsc(void)
 	}
 
 #if defined(__GNUC__) && __GNUC__>=2
-void OPENSSL_cpuid_setup(void) __attribute__((constructor))
+void OPENSSL_cpuid_setup(void) __attribute__((constructor));
 #endif
 void OPENSSL_cpuid_setup(void)
 	{
