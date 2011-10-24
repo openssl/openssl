45e5f551acf65e1b2c36161e356637045cc5f793
Dr. Stephen Henson <steve@openssl.org>
2011-10-24 16:58:49 +0000

    Prepare for RC2.

--- fips/fips_locl.h
+++ fips/fips_locl.h
@@ -67,8 +67,8 @@ int fips_post_failed(int id, int subid, void *ex);
 int fips_post_corrupt(int id, int subid, void *ex);
 int fips_post_status(void);
 
-#define FIPS_MODULE_VERSION_NUMBER	0x20000001L
-#define FIPS_MODULE_VERSION_TEXT	"FIPS 2.0-rc1 unvalidated test module xx XXX xxxx"
+#define FIPS_MODULE_VERSION_NUMBER	0x20000002L
+#define FIPS_MODULE_VERSION_TEXT	"FIPS 2.0-rc2-dev unvalidated test module xx XXX xxxx"
 
 #ifdef  __cplusplus
 }
