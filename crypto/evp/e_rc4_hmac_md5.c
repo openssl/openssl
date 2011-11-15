cd7b854bbbfe368f09204bee105551e7f25a707c
Andy Polyakov <appro@openssl.org>
2011-11-15 12:39:48 +0000

    e_rc4_hmac_md5.c: make it work on darwin64, which is configured with RC4_CHAR.

--- crypto/evp/e_rc4_hmac_md5.c
+++ crypto/evp/e_rc4_hmac_md5.c
@@ -101,7 +101,8 @@ static int rc4_hmac_md5_init_key(EVP_CIPHER_CTX *ctx,
 #if	!defined(OPENSSL_NO_ASM) &&	( \
 	defined(__x86_64)	|| defined(__x86_64__)	|| \
 	defined(_M_AMD64)	|| defined(_M_X64)	|| \
-	defined(__INTEL__)		)
+	defined(__INTEL__)		) && \
+	!(defined(__APPLE__) && defined(__MACH__))
 #define	STITCHED_CALL
 #endif
 
