b3cebd5acfe28921bb00292440d121025bafba48
Dr. Stephen Henson <steve@openssl.org>
2012-01-04 19:20:49 +0000

    prepare for 0.9.8s release

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,11 +25,11 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x00908130L
+#define OPENSSL_VERSION_NUMBER	0x0090813fL
 #ifdef OPENSSL_FIPS
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8s-fips-dev xx XXX xxxx"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8s-fips 4 Jan 2012"
 #else
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8s xx XXX xxxx"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8s 4 Jan 2012"
 #endif
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
