c8e0b5d7b6bd13c6180c68eefb312f1de7b40584
Andy Polyakov <appro@openssl.org>
2012-01-15 13:42:50 +0000

    1.0.1-specific OPNESSL vs. OPENSSL typo. PR: 2613 Submitted by: Leena Heino

--- ssl/ssl_algs.c
+++ ssl/ssl_algs.c
@@ -90,7 +90,7 @@ int SSL_library_init(void)
 	EVP_add_cipher(EVP_aes_256_cbc());
 	EVP_add_cipher(EVP_aes_128_gcm());
 	EVP_add_cipher(EVP_aes_256_gcm());
-#if !defined(OPENSSL_NO_SHA) && !defined(OPNESSL_NO_SHA1)
+#if !defined(OPENSSL_NO_SHA) && !defined(OPENSSL_NO_SHA1)
 	EVP_add_cipher(EVP_aes_128_cbc_hmac_sha1());
 	EVP_add_cipher(EVP_aes_256_cbc_hmac_sha1());
 #endif
