f02f7c2c4aa19c8e70b1739fcdab749145d32805
Andy Polyakov <appro@openssl.org>
2012-01-21 12:18:29 +0000

    cryptlib.c: make even non-Windows builds "strtoull-agnostic" [from HEAD].

--- crypto/cryptlib.c
+++ crypto/cryptlib.c
@@ -698,7 +698,7 @@ void OPENSSL_cpuid_setup(void)
 #if defined(_WIN32)
 	if (!sscanf(env+off,"%I64i",&vec)) vec = strtoul(env+off,NULL,0);
 #else
-	vec = strtoull(env+off,NULL,0);
+	if (!sscanf(env+off,"%lli",(long long *)&vec)) vec = strtoul(env+off,NULL,0);
 #endif
 	if (off) vec = OPENSSL_ia32_cpuid()&~vec;
     }
