f0be325f885201430206559831f5e1c936be5df3
Dr. Stephen Henson <steve@openssl.org>
2012-02-28 14:47:36 +0000

    Fix memory leak cause by race condition when creating public keys.
    Thanks to Ivan Nestlerode <inestlerode@us.ibm.com> for reporting this bug.

--- crypto/asn1/x_pubkey.c
+++ crypto/asn1/x_pubkey.c
@@ -367,7 +367,16 @@ EVP_PKEY *X509_PUBKEY_get(X509_PUBKEY *key)
 		goto err;
 		}
 
-	key->pkey = ret;
+	/* Check to see if another thread set key->pkey first */
+	CRYPTO_w_lock(CRYPTO_LOCK_EVP_PKEY);
+	if (key->pkey)
+		{
+		EVP_PKEY_free(ret);
+		ret = key->pkey;
+		}
+	else
+		key->pkey = ret;
+	CRYPTO_w_unlock(CRYPTO_LOCK_EVP_PKEY);
 	CRYPTO_add(&ret->references, 1, CRYPTO_LOCK_EVP_PKEY);
 	return(ret);
 err:
