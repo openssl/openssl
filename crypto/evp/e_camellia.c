5b0e3daf50ef64fb36c864560881e0267170ce3c
Ben Laurie <ben@openssl.org>
2012-11-05 02:01:07 +0000

    Remove unused static function.

--- crypto/evp/e_camellia.c
+++ crypto/evp/e_camellia.c
@@ -378,6 +378,7 @@ static int camellia_cfb1_cipher(EVP_CIPHER_CTX *ctx,unsigned char *out,
 	return 1;
 }
 
+/*
 static int camellia_ctr_cipher (EVP_CIPHER_CTX *ctx, unsigned char *out,
 		const unsigned char *in, size_t len)
 {
@@ -393,6 +394,7 @@ static int camellia_ctr_cipher (EVP_CIPHER_CTX *ctx, unsigned char *out,
 	ctx->num = (size_t)num;
 	return 1;
 }
+*/
 
 BLOCK_CIPHER_generic_pack(NID_camellia,128,EVP_CIPH_FLAG_FIPS)
 BLOCK_CIPHER_generic_pack(NID_camellia,192,EVP_CIPH_FLAG_FIPS)
