66d9f2e5210b7f68c4ac9ffebedca031c05ec487
Dr. Stephen Henson <steve@openssl.org>
2012-12-03 03:33:44 +0000

    if no error code and -brief selected print out connection closed instead of read error

--- apps/s_client.c
+++ apps/s_client.c
@@ -1912,7 +1912,10 @@ printf("read=%d pending=%d peek=%d\n",k,SSL_pending(con),SSL_peek(con,zbuf,10240
 				break;
 			case SSL_ERROR_SYSCALL:
 				ret=get_last_socket_error();
-				BIO_printf(bio_err,"read:errno=%d\n",ret);
+				if (ret == 0 && c_brief)
+					BIO_puts(bio_err, "CONNECTION CLOSED BY SERVER\n");
+				else
+					BIO_printf(bio_err,"read:errno=%d\n",ret);
 				goto shut;
 			case SSL_ERROR_ZERO_RETURN:
 				BIO_printf(bio_c_out,"closed\n");
