7c770d572a719fa40fa9c82807a0bd3840baf4a0
Ben Laurie <ben@links.org>
2013-01-28 17:30:38 +0000

    Add and use a constant-time memcmp.
    This change adds CRYPTO_memcmp, which compares two vectors of bytes in
    an amount of time that's independent of their contents. It also changes
    several MAC compares in the code to use this over the standard memcmp,
    which may leak information about the size of a matching prefix.
    (cherry picked from commit 2ee798880a246d648ecddadc5b91367bee4a5d98)

--- crypto/cryptlib.c
+++ crypto/cryptlib.c
@@ -397,3 +397,16 @@ void OpenSSLDie(const char *file,int line,const char *assertion)
 #ifndef OPENSSL_FIPSCANISTER
 void *OPENSSL_stderr(void)	{ return stderr; }
 #endif
+
+int CRYPTO_memcmp(const void *in_a, const void *in_b, size_t len)
+	{
+	size_t i;
+	const unsigned char *a = in_a;
+	const unsigned char *b = in_b;
+	unsigned char x = 0;
+
+	for (i = 0; i < len; i++)
+		x |= a[i] ^ b[i];
+
+	return x;
+	}
