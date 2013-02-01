eeb486a5f41a6d71708f02bb32b07f023f5d5ea7
Andy Polyakov <appro@openssl.org>
2013-02-01 15:31:50 +0100

    ssl/*: remove SSL3_RECORD->orig_len to restore binary compatibility.
    Kludge alert. This is arranged by passing padding length in unused
    bits of SSL3_RECORD->type, so that orig_len can be reconstructed.
    (cherry picked from commit 8bfd4c659f180a6ce34f21c0e62956b362067fba)

--- ssl/ssl_locl.h
+++ ssl/ssl_locl.h
@@ -1277,7 +1277,7 @@ int ssl_parse_serverhello_use_srtp_ext(SSL *s, unsigned char *d, int len,int *al
 /* s3_cbc.c */
 void ssl3_cbc_copy_mac(unsigned char* out,
 		       const SSL3_RECORD *rec,
-		       unsigned md_size);
+		       unsigned md_size,unsigned orig_len);
 int ssl3_cbc_remove_padding(const SSL* s,
 			    SSL3_RECORD *rec,
 			    unsigned block_size,
