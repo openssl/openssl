2f297fce734aa1ba38836e1a75f65e0a48bfa0c7
Ben Laurie <ben@links.org>
2013-03-04 16:10:47 +0000

    Don't make CPUID stuff twice.

--- util/mk1mf.pl
+++ util/mk1mf.pl
@@ -877,7 +877,8 @@ sub fix_asm
 
 $lib_obj{CRYPTO} .= fix_asm($mf_md5_asm, 'crypto/md5');
 $lib_obj{CRYPTO} .= fix_asm($mf_bn_asm, 'crypto/bn');
-$lib_obj{CRYPTO} .= fix_asm($mf_cpuid_asm, 'crypto');
+# cpuid is included by the crypto dir
+#$lib_obj{CRYPTO} .= fix_asm($mf_cpuid_asm, 'crypto');
 # AES asm files end up included by the aes dir itself
 #$lib_obj{CRYPTO} .= fix_asm($mf_aes_asm, 'crypto/aes');
 $lib_obj{CRYPTO} .= fix_asm($mf_sha_asm, 'crypto/sha');
