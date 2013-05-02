b9e84f007f8985c60d66b42fa666c6fb7b3919a2
Dr. Stephen Henson <steve@openssl.org>
2013-05-02 12:18:46 +0100

    Reencode certificates in X509_sign_ctx.
    Reencode certificates in X509_sign_ctx as well as X509_sign.
    
    This was causing a problem in the x509 application when it modified an
    existing certificate.
    (cherry picked from commit c6d8adb8a45186617e0a8e2c09469bd164b92b31)

--- crypto/x509/x_all.c
+++ crypto/x509/x_all.c
@@ -97,6 +97,7 @@ int X509_sign(X509 *x, EVP_PKEY *pkey, const EVP_MD *md)
 
 int X509_sign_ctx(X509 *x, EVP_MD_CTX *ctx)
 	{
+	x->cert_info->enc.modified = 1;
 	return ASN1_item_sign_ctx(ASN1_ITEM_rptr(X509_CINF),
 		x->cert_info->signature,
 		x->sig_alg, x->signature, x->cert_info, ctx);
