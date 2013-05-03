cbd93a0636ea586e032bb55418a8d38773683be9
Dr. Stephen Henson <steve@openssl.org>
2013-05-03 12:31:47 +0100

    Reencode with X509_CRL_ctx_sign too. (cherry picked from commit 96940f4f2d0300c033379a87db0ff19e598c6264)

--- crypto/x509/x_all.c
+++ crypto/x509/x_all.c
@@ -124,6 +124,7 @@ int X509_CRL_sign(X509_CRL *x, EVP_PKEY *pkey, const EVP_MD *md)
 
 int X509_CRL_sign_ctx(X509_CRL *x, EVP_MD_CTX *ctx)
 	{
+	x->crl->enc.modified = 1;
 	return ASN1_item_sign_ctx(ASN1_ITEM_rptr(X509_CRL_INFO),
 		x->crl->sig_alg, x->sig_alg, x->signature, x->crl, ctx);
 	}
