d3890d3ba4c8874a65b2466a516574ddb7d87107
Trevor <trevp@trevp.net>
2013-05-15 19:04:47 -0700

    Finish removing OPENSSL_NO_SERVERINFO (s_server).

--- apps/s_server.c
+++ apps/s_server.c
@@ -318,10 +318,8 @@ static int cert_chain = 0;
 #ifndef OPENSSL_NO_TLSEXT
 static BIO *authz_in = NULL;
 static const char *s_authz_file = NULL;
-#ifndef OPENSSL_NO_SERVERINFO
 static BIO *serverinfo_in = NULL;
 static const char *s_serverinfo_file = NULL;
-#endif
 #endif
 
 #ifndef OPENSSL_NO_PSK
@@ -484,9 +482,7 @@ static void sv_usage(void)
 	BIO_printf(bio_err,"                 (default is %s)\n",TEST_CERT);
 	BIO_printf(bio_err," -authz arg   -  binary authz file for certificate\n");
 #ifndef OPENSSL_NO_TLSEXT
-#ifndef OPENSSL_NO_SERVERINFO
 	BIO_printf(bio_err," -serverinfo arg - PEM serverinfo file for certificate\n");
-#endif
 #endif
 	BIO_printf(bio_err," -crl_check    - check the peer certificate has not been revoked by its CA.\n" \
 	                   "                 The CRL(s) are appended to the certificate file\n");
@@ -1102,13 +1098,11 @@ int MAIN(int argc, char *argv[])
 			if (--argc < 1) goto bad;
 			s_authz_file = *(++argv);
 			}
-#ifndef OPENSSL_NO_SERVERINFO
 		else if	(strcmp(*argv,"-serverinfo") == 0)
 			{
 			if (--argc < 1) goto bad;
 			s_serverinfo_file = *(++argv);
 			}
-#endif
 #endif
 		else if	(strcmp(*argv,"-certform") == 0)
 			{
@@ -1869,10 +1863,8 @@ bad:
 #ifndef OPENSSL_NO_TLSEXT
 	if (s_authz_file != NULL && !SSL_CTX_use_authz_file(ctx, s_authz_file))
 		goto end;
-#ifndef OPENSSL_NO_SERVERINFO
 	if (s_serverinfo_file != NULL && !SSL_CTX_use_serverinfo_file(ctx, s_serverinfo_file))
 		goto end;
-#endif
 #endif
 #ifndef OPENSSL_NO_TLSEXT
 	if (ctx2 && !set_cert_key_stuff(ctx2,s_cert2,s_key2, NULL, build_chain))
@@ -2052,10 +2044,8 @@ end:
 		EVP_PKEY_free(s_key2);
 	if (authz_in != NULL)
 		BIO_free(authz_in);
-#ifndef OPENSSL_NO_SERVERINFO
 	if (serverinfo_in != NULL)
 		BIO_free(serverinfo_in);
-#endif
 #endif
 	ssl_excert_free(exc);
 	if (ssl_args)
