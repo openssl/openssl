6d84daa5d665e0de30c3d970cab65e8ade4d5b14
Ben Laurie <ben@links.org>
2013-06-12 14:18:33 +0100

    Add aesni-sha256-x86_64.

--- util/pl/unix.pl
+++ util/pl/unix.pl
@@ -70,7 +70,8 @@ $bf_enc_src="";
 	  'rc4-x86_64' => 'crypto/rc4',
 	  'rc4-md5-x86_64' => 'crypto/rc4',
 	  'ghash-x86_64' => 'crypto/modes',
-          'aesni-gcm-x86_64' => 'crypto/modes',
+	  'aesni-gcm-x86_64' => 'crypto/modes',
+	  'aesni-sha256-x86_64' => 'crypto/aes',
          );
 
 # If I were feeling more clever, these could probably be extracted
