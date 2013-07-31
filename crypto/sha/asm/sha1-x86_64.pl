006784378db1a8957fcf587ee1deaa5cf2ae76c7
Andy Polyakov <appro@openssl.org>
2013-07-31 23:50:15 +0200

    crypto/sha/asm/sha*-x86_64.pl: comply with Win64 ABI.

--- crypto/sha/asm/sha1-x86_64.pl
+++ crypto/sha/asm/sha1-x86_64.pl
@@ -806,7 +806,7 @@ $code.=<<___;
 	mov	%rdi,$ctx	# reassigned argument
 	mov	%rsi,$inp	# reassigned argument
 	mov	%rdx,$num	# reassigned argument
-	vzeroall
+	vzeroupper
 
 	shl	\$6,$num
 	add	$inp,$num
@@ -1096,7 +1096,7 @@ ___
 	&Xtail_avx(\&body_20_39);
 
 $code.=<<___;
-	vzeroall
+	vzeroupper
 
 	add	0($ctx),$A			# update context
 	add	4($ctx),@T[0]
