dece3209f299ebcd82414868ee39b2c6feb3be0a
Rob Stradling <rob@comodo.com>
2013-09-05 13:09:03 +0100

    Don't prefer ECDHE-ECDSA ciphers when the client appears to be Safari on OS X. OS X 10.8..10.8.3 has broken support for ECDHE-ECDSA ciphers.

--- ssl/ssl3.h
+++ ssl/ssl3.h
@@ -596,6 +596,14 @@ typedef struct ssl3_state_st
 	 * processed. */
 	unsigned char *alpn_selected;
 	unsigned alpn_selected_len;
+
+#ifndef OPENSSL_NO_EC
+	/* This is set to true if we believe that this is a version of Safari
+	 * running on OS X 10.6 or newer. We wish to know this because Safari
+	 * on 10.8 .. 10.8.3 has broken ECDHE-ECDSA support. */
+	char is_probably_safari;
+#endif	/* OPENSSL_NO_EC */
+
 #endif	/* OPENSSL_NO_TLSEXT */
 	} SSL3_STATE;
 
