937f125efc80d7a4e80a5a02ec0eae02ea0b55ac
Rob Stradling <rob@comodo.com>
2013-09-10 11:00:57 +0100

    Fix compilation with no-ec and/or no-tlsext.

--- ssl/s3_lib.c
+++ ssl/s3_lib.c
@@ -4021,15 +4021,15 @@ SSL_CIPHER *ssl3_choose_cipher(SSL *s, STACK_OF(SSL_CIPHER) *clnt,
 		ii=sk_SSL_CIPHER_find(allow,c);
 		if (ii >= 0)
 			{
+#if !defined(OPENSSL_NO_EC) && !defined(OPENSSL_NO_TLSEXT)
 			if ((alg_k & SSL_kEECDH) && (alg_a & SSL_aECDSA) && s->s3->is_probably_safari)
 				{
 				if (!ret) ret=sk_SSL_CIPHER_value(allow,ii);
+				continue;
 				}
-			else
-				{
-				ret=sk_SSL_CIPHER_value(allow,ii);
-				break;
-				}
+#endif
+			ret=sk_SSL_CIPHER_value(allow,ii);
+			break;
 			}
 		}
 	return(ret);
