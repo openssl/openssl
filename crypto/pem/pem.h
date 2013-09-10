033864842607895730d97baf4103da24f1207762
Ben Laurie <ben@links.org>
2013-09-10 17:58:44 +0100

    Constification.

--- crypto/pem/pem.h
+++ crypto/pem/pem.h
@@ -411,8 +411,8 @@ int	PEM_do_header (EVP_CIPHER_INFO *cipher, unsigned char *data,long *len,
 #ifndef OPENSSL_NO_BIO
 int	PEM_read_bio(BIO *bp, char **name, char **header,
 		unsigned char **data,long *len);
-int	PEM_write_bio(BIO *bp,const char *name,char *hdr,unsigned char *data,
-		long len);
+int	PEM_write_bio(BIO *bp,const char *name, const char *hdr,
+		      const unsigned char *data, long len);
 int PEM_bytes_read_bio(unsigned char **pdata, long *plen, char **pnm, const char *name, BIO *bp,
 	     pem_password_cb *cb, void *u);
 void *	PEM_ASN1_read_bio(d2i_of_void *d2i, const char *name, BIO *bp,
@@ -428,7 +428,8 @@ int	PEM_X509_INFO_write_bio(BIO *bp,X509_INFO *xi, EVP_CIPHER *enc,
 
 int	PEM_read(FILE *fp, char **name, char **header,
 		unsigned char **data,long *len);
-int	PEM_write(FILE *fp,char *name,char *hdr,unsigned char *data,long len);
+int	PEM_write(FILE *fp, const char *name, const char *hdr,
+		  const unsigned char *data, long len);
 void *  PEM_ASN1_read(d2i_of_void *d2i, const char *name, FILE *fp, void **x,
 		      pem_password_cb *cb, void *u);
 int	PEM_ASN1_write(i2d_of_void *i2d,const char *name,FILE *fp,
