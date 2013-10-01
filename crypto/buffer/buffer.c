b93916149d843df20e7e8e960813db81c7981863
Ben Laurie <ben@links.org>
2013-10-01 14:51:04 +0100

    Constification.

--- crypto/buffer/buffer.c
+++ crypto/buffer/buffer.c
@@ -179,7 +179,7 @@ int BUF_MEM_grow_clean(BUF_MEM *str, size_t len)
 	return(len);
 	}
 
-void BUF_reverse(unsigned char *out, unsigned char *in, size_t size)
+void BUF_reverse(unsigned char *out, const unsigned char *in, size_t size)
 	{
 	size_t i;
 	if (in)
