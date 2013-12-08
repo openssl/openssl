17694d6f157e2de928d3fcb7d2d120dd4c800bbb
Jonas Maebe <jonas.maebe@elis.ugent.be>
2013-12-08 22:45:58 +0100

    get_cert_by_subject: check for NULL when allocating hent
    Signed-off-by: Kurt Roeckx <kurt@openssl.org>
    Reviewed-by: Dr. Stephen Henson <steve@openssl.org>

--- crypto/x509/by_dir.c
+++ crypto/x509/by_dir.c
@@ -444,6 +444,13 @@ static int get_cert_by_subject(X509_LOOKUP *xl, int type, X509_NAME *name,
 			if (!hent)
 				{
 				hent = OPENSSL_malloc(sizeof(BY_DIR_HASH));
+				if (hent == NULL)
+					{
+					CRYPTO_w_unlock(CRYPTO_LOCK_X509_STORE);
+					X509err(X509_F_GET_CERT_BY_SUBJECT,ERR_R_MALLOC_FAILURE);
+					ok = 0;
+					goto finish;
+					}
 				hent->hash = h;
 				hent->suffix = k;
 				if (!sk_BY_DIR_HASH_push(ent->hashes, hent))
