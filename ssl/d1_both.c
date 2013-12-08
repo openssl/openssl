573dbcaee64b54428ff4ffaec32723e209ad3de9
Jonas Maebe <jonas.maebe@elis.ugent.be>
2013-12-08 23:29:17 +0100

    dtls1_process_heartbeat: check for NULL after allocating buffer

--- ssl/d1_both.c
+++ ssl/d1_both.c
@@ -1371,6 +1371,8 @@ dtls1_process_heartbeat(SSL *s)
 		 * payload, plus padding
 		 */
 		buffer = OPENSSL_malloc(write_length);
+		if (buffer == NULL)
+			return -1;
 		bp = buffer;
 
 		/* Enter response type, length and copy payload */
