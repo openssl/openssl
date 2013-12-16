4089bd6080d41450adab1e0ac0d63cfeab4a78e7
Dr. Stephen Henson <steve@openssl.org>
2013-12-16 14:29:20 +0000

    eCos ARMv4/5 support

--- fips/fips_canister.c
+++ fips/fips_canister.c
@@ -38,6 +38,7 @@ const void         *FIPS_text_end(void);
 				  defined(__mips__))	|| \
 	(defined(__APPLE__) /* verified on all MacOS X & iOS flavors */)|| \
 	(defined(_TMS320C6X))						|| \
+	(defined(__ECOS__))						|| \
 	(defined(_WIN32)      && defined(_MSC_VER))
 #  define FIPS_REF_POINT_IS_CROSS_COMPILER_AWARE
 # endif
