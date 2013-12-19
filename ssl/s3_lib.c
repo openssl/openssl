ca989269a2876bae79393bd54c3e72d49975fc75
Dr. Stephen Henson <steve@openssl.org>
2013-12-19 14:37:39 +0000

    Use version in SSL_METHOD not SSL structure.
    When deciding whether to use TLS 1.2 PRF and record hash algorithms
    use the version number in the corresponding SSL_METHOD structure
    instead of the SSL structure. The SSL structure version is sometimes
    inaccurate. Note: OpenSSL 1.0.2 and later effectively do this already.
    (CVE-2013-6449)

--- ssl/s3_lib.c
+++ ssl/s3_lib.c
@@ -4286,7 +4286,7 @@ need to go to SSL_ST_ACCEPT.
 long ssl_get_algorithm2(SSL *s)
 	{
 	long alg2 = s->s3->tmp.new_cipher->algorithm2;
-	if (TLS1_get_version(s) >= TLS1_2_VERSION &&
+	if (s->method->version == TLS1_2_VERSION &&
 	    alg2 == (SSL_HANDSHAKE_MAC_DEFAULT|TLS1_PRF))
 		return SSL_HANDSHAKE_MAC_SHA256 | TLS1_PRF_SHA256;
 	return alg2;
