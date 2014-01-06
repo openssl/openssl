11d31c00adca551fc853da36f98e078d83f8a680
Dr. Stephen Henson <steve@openssl.org>
2014-01-06 15:02:46 +0000

    Prepare for 1.0.0m-dev

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,11 +25,11 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x100000cfL
+#define OPENSSL_VERSION_NUMBER	0x100000d0L
 #ifdef OPENSSL_FIPS
-#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.0l-fips 6 Jan 2014"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.0m-fips-dev xx XXX xxxx"
 #else
-#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.0l 6 Jan 2014"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.0m-dev xx XXX xxxx"
 #endif
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
