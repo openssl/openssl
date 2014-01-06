197e0ea817ad64820789d86711d55ff50d71f631
Dr. Stephen Henson <steve@openssl.org>
2014-01-06 14:35:04 +0000

    Fix for TLS record tampering bug CVE-2013-4353

--- ssl/s3_both.c
+++ ssl/s3_both.c
@@ -210,7 +210,11 @@ static void ssl3_take_mac(SSL *s)
 	{
 	const char *sender;
 	int slen;
-
+	/* If no new cipher setup return immediately: other functions will
+	 * set the appropriate error.
+	 */
+	if (s->s3->tmp.new_cipher == NULL)
+		return;
 	if (s->state & SSL_ST_CONNECT)
 		{
 		sender=s->method->ssl3_enc->server_finished_label;
