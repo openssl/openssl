5df832293eb250a8208f8b24a36ad095c06ff4a7
Kaspar Brand <ossl-rt@velox.ch>
2014-01-16 13:49:38 +0000

    Omit initial status request callback check.
    PR#3178
    (cherry picked from commit eb85ee9a8867b605cd7fb427869d0e50caa80a3f)

--- ssl/t1_lib.c
+++ ssl/t1_lib.c
@@ -972,7 +972,7 @@ int ssl_parse_clienthello_tlsext(SSL *s, unsigned char **p, unsigned char *d, in
 			renegotiate_seen = 1;
 			}
 		else if (type == TLSEXT_TYPE_status_request &&
-		         s->version != DTLS1_VERSION && s->ctx->tlsext_status_cb)
+		         s->version != DTLS1_VERSION)
 			{
 		
 			if (size < 5) 
