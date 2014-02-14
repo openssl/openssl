f4d456408d9d7bca31f34765d1a05fbd9fa55826
Andy Polyakov <appro@openssl.org>
2014-02-14 17:24:12 +0100

    x86[_64]cpuid.pl: add low-level RDSEED.

--- crypto/x86cpuid.pl
+++ crypto/x86cpuid.pl
@@ -469,6 +469,18 @@ my $max = "ebp";
 	&ret	();
 &function_end_B("OPENSSL_ia32_rdrand");
 
+&function_begin_B("OPENSSL_ia32_rdseed");
+	&mov	("ecx",8);
+&set_label("loop");
+	&rdseed	("eax");
+	&jc	(&label("break"));
+	&loop	(&label("loop"));
+&set_label("break");
+	&cmp	("eax",0);
+	&cmove	("eax","ecx");
+	&ret	();
+&function_end_B("OPENSSL_ia32_rdseed");
+
 &initseg("OPENSSL_cpuid_setup");
 
 &hidden("OPENSSL_cpuid_setup");
