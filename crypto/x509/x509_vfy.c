bf2d129194d5a8f2ccf5ff5261ab755e4fc9f98e
Dr. Stephen Henson <steve@openssl.org>
2014-02-14 13:33:43 +0000

    Simplify X509_STORE_CTX_get1_chain (from master).

--- crypto/x509/x509_vfy.c
+++ crypto/x509/x509_vfy.c
@@ -2123,16 +2123,9 @@ STACK_OF(X509) *X509_STORE_CTX_get_chain(X509_STORE_CTX *ctx)
 
 STACK_OF(X509) *X509_STORE_CTX_get1_chain(X509_STORE_CTX *ctx)
 	{
-	int i;
-	X509 *x;
-	STACK_OF(X509) *chain;
-	if (!ctx->chain || !(chain = sk_X509_dup(ctx->chain))) return NULL;
-	for (i = 0; i < sk_X509_num(chain); i++)
-		{
-		x = sk_X509_value(chain, i);
-		CRYPTO_add(&x->references, 1, CRYPTO_LOCK_X509);
-		}
-	return chain;
+	if (!ctx->chain)
+		return NULL;
+	return X509_chain_up_ref(ctx->chain);
 	}
 
 X509 *X509_STORE_CTX_get0_current_issuer(X509_STORE_CTX *ctx)
