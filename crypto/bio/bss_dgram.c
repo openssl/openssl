972b0dc3500a26edb178a5446a0cdfb8f450f853
Andy Polyakov <appro@openssl.org>
2014-03-06 14:03:02 +0100

    bss_dgram.c,d1_lib.c: make it compile with mingw.
    Submitted by: Roumen Petrov

--- crypto/bio/bss_dgram.c
+++ crypto/bio/bss_dgram.c
@@ -1901,7 +1901,11 @@ static void get_current_time(struct timeval *t)
 
 	GetSystemTime(&st);
 	SystemTimeToFileTime(&st,&now.ft);
+#ifdef	__MINGW32__
+	now.ul -= 116444736000000000ULL;
+#else
 	now.ul -= 116444736000000000UI64;	/* re-bias to 1/1/1970 */
+#endif
 	t->tv_sec  = (long)(now.ul/10000000);
 	t->tv_usec = ((int)(now.ul%10000000))/10;
 #elif defined(OPENSSL_SYS_VMS)
