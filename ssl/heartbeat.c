81d0d4a03f56a02e5cce2cabb698a7d775bb61af
Soenke Ruempler <soenke@jimdo.com>
2014-04-11 18:21:36 +0200

    wip

--- ssl/heartbeat.c
+++ ssl/heartbeat.c
@@ -1,4 +1,5 @@
 #include "heartbeat.h"
+#include <openssl/ssl.h>
 
 int heartbeat_size(int payload, int padding)
 	{
@@ -20,3 +21,16 @@ void apply_msg_callback(SSL *s)
 			s, s->msg_callback_arg);
 	}
 
+}
+
+//-- int heartbeat_read_payload(SSL *s, unsigned char *pl, unsigned short hbtype, unsigned int payload) {
+//--     unsigned char *p = &s->s3->rrec.data[0];
+//-- 	/* Read type and payload length first */
+//-- 	if (1 + 2 + 16 > s->s3->rrec.length)
+//-- 		return 0; /* silently discard */
+//-- 	hbtype = *p++;
+//-- 	n2s(p, payload);
+//-- 	if (1 + 2 + payload + 16 > s->s3->rrec.length)
+//-- 		return 0; /* silently discard per RFC 6520 sec. 4 */
+//-- 	pl = p;
+//-- }
--- ssl/heartbeat.h
+++ ssl/heartbeat.h
@@ -7,5 +7,5 @@ int heartbeat_size(int payload, int padding);
 int heartbeat_size_std(int payload);
 
 void apply_msg_callback(SSL *s);
-
+void heartbeat_read_payload(SSL *s);
 #endif
