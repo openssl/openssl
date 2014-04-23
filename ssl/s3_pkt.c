725c5f1ad393a7bc344348d0ec7c268aaf2700a7
Ben Laurie <ben@links.org>
2014-04-23 07:24:03 +0100

    Fix use after free.

--- ssl/s3_pkt.c
+++ ssl/s3_pkt.c
@@ -1055,7 +1055,7 @@ start:
 				{
 				s->rstate=SSL_ST_READ_HEADER;
 				rr->off=0;
-				if (s->mode & SSL_MODE_RELEASE_BUFFERS)
+				if (s->mode & SSL_MODE_RELEASE_BUFFERS && s->s3->rbuf.left == 0)
 					ssl3_release_read_buffer(s);
 				}
 			}
