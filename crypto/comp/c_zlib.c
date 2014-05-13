1cab7d06ea110219a9b5f0a309254b86da6a0a46
David Brodski <bid@softing.com>
2014-05-13 18:06:27 +0200

    Fixed problem with multiple load-unload of comp zlib
    Signed-off-by: Rich Salz <rsalz@akamai.com>
    Reviewed-by: Tim Hudson <tjh@openssl.org>

--- crypto/comp/c_zlib.c
+++ crypto/comp/c_zlib.c
@@ -309,7 +309,9 @@ COMP_METHOD *COMP_zlib(void)
 void COMP_zlib_cleanup(void)
 {
 #ifdef ZLIB_SHARED
-    DSO_free(zlib_dso);
+    if (zlib_dso != NULL)
+        DSO_free(zlib_dso);
+    zlib_dso = NULL;
 #endif
 }
 
