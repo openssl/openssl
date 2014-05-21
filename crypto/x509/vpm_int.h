7643a1c882d14142ef394783cc7aa8ce7b187fb7
Viktor Dukhovni <viktor@dukhovni.org>
2014-05-21 10:57:44 +0100

    Fixes to host checking.
    Fixes to host checking wild card support and add support for
    setting host checking flags when verifying a certificate
    chain.

--- crypto/x509/vpm_int.h
+++ crypto/x509/vpm_int.h
@@ -62,6 +62,7 @@ struct X509_VERIFY_PARAM_ID_st
 	{
 	unsigned char *host;	/* If not NULL hostname to match */
 	size_t hostlen;
+	unsigned int hostflags;	/* Flags to control matching features */
 	unsigned char *email;	/* If not NULL email address to match */
 	size_t emaillen;
 	unsigned char *ip;	/* If not NULL IP address to match */
