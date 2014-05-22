d77501d8fd1a49ff82e2824ded6f341f4fc8307a
Ben Laurie <ben@links.org>
2014-05-22 10:11:40 +0100

    Check length first in BUF_strnlen().

--- crypto/buffer/buf_str.c
+++ crypto/buffer/buf_str.c
@@ -64,7 +64,7 @@ size_t BUF_strnlen(const char *str, size_t maxlen)
 	{
 	const char *p;
 
-	for (p = str; *p != '\0' && maxlen-- != 0; ++p)
+	for (p = str; maxlen-- != 0 && *p != '\0'; ++p)
 		;
 
 	return p - str;
