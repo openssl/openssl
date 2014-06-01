de8a5b523f38377c8e0d9098ebff880749156db6
Matt Caswell <matt@openssl.org>
2014-06-01 21:32:19 +0100

    Added SSLErr call for internal error in dtls1_buffer_record

--- ssl/d1_pkt.c
+++ ssl/d1_pkt.c
@@ -255,6 +255,7 @@ dtls1_buffer_record(SSL *s, record_pqueue *queue, unsigned char *priority)
 	/* insert should not fail, since duplicates are dropped */
 	if (pqueue_insert(queue->q, item) == NULL)
 		{
+		SSLerr(SSL_F_DTLS1_BUFFER_RECORD, ERR_R_INTERNAL_ERROR);
 		OPENSSL_free(rdata);
 		pitem_free(item);
 		return(0);
