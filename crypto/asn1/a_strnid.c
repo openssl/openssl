61e6e80fe5b99aecaeb1bc36f5781a4164747de4
Dr. Stephen Henson <steve@openssl.org>
2014-06-01 15:03:00 +0100

    Set default global mask to UTF8 only. (cherry picked from commit 3009244da47b989c4cc59ba02cf81a4e9d8f8431)

--- crypto/asn1/a_strnid.c
+++ crypto/asn1/a_strnid.c
@@ -75,7 +75,7 @@ static int table_cmp(const void *a, const void *b);
  * certain software (e.g. Netscape) has problems with them.
  */
 
-static unsigned long global_mask = 0xFFFFFFFFL;
+static unsigned long global_mask = B_ASN1_UTF8STRING;
 
 void ASN1_STRING_set_default_mask(unsigned long mask)
 {
