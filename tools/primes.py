72cfeeff1ce4985ea87637fcac4671510e1ee0b1
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-06-01 21:43:04 +0100

    Change the prime generator to output C.

--- tools/primes.py
+++ tools/primes.py
@@ -1,4 +1,6 @@
-primes = [2, 3, 5, 7, 11]
+primes = [2, 3, 5, 7, 11, 13]
+
+safe_prefix = 'safe_'
 
 for safe in (False, True):
     muliplier = 1 if not safe else 2
@@ -16,6 +18,22 @@ for safe in (False, True):
         if prime:
             offsets.append(x)
 
-    print(offsets)
-    print(len(offsets))
-    print(muliplier)
+    print('static const int %sprime_offsets[%d] = {' % (
+        safe_prefix if safe else '', len(offsets)))
+    print_buffer = '\t'
+    for offset in offsets:
+        if len(print_buffer) > 60:
+            print(print_buffer[:-1])
+            print_buffer = '\t'
+        print_buffer += '%d, ' % offset
+    print(print_buffer[:-2] + ' };')
+    print('static const int %sprime_offset_count = %d;' % (
+        safe_prefix if safe else '', len(offsets)))
+    print('static const int %sprime_multiplier = %d;' % (
+        safe_prefix if safe else '', muliplier))
+    print('static const int %sprime_multiplier_bits = %d;' % (
+        safe_prefix if safe else '', muliplier.bit_length()))
+
+    print('')
+
+print('static const int first_prime_index = %d;' % len(primes))
