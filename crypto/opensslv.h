4a1190beca317f4e4b8ac4a487a3fe3079534f30
Dr. Stephen Henson <steve@openssl.org>
2014-06-05 10:40:11 +0100

    Prepare for 0.9.8zb-dev

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,11 +25,11 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x009081afL
+#define OPENSSL_VERSION_NUMBER	0x009081b0L
 #ifdef OPENSSL_FIPS
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8za-fips 5 Jun 2014"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8zb-fips-dev xx XXX xxxx"
 #else
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8za 5 Jun 2014"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8zb-dev xx XXX xxxx"
 #endif
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
