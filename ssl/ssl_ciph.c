08db2cc9c14b397950b4e7f28ed68f060f0a9434
Matt Caswell <matt@openssl.org>
2014-06-22 23:22:33 +0100

    Revert "Fix off-by-one errors in ssl_cipher_get_evp()"
    This reverts commit 7e98378d155263bebbea3b3c13c1b6a780c8a6d3.
    
    Incorrect attribution.

--- ssl/ssl_ciph.c
+++ ssl/ssl_ciph.c
@@ -531,7 +531,7 @@ int ssl_cipher_get_evp(const SSL_SESSION *s, const EVP_CIPHER **enc,
 		break;
 		}
 
-	if ((i < 0) || (i >= SSL_ENC_NUM_IDX))
+	if ((i < 0) || (i > SSL_ENC_NUM_IDX))
 		*enc=NULL;
 	else
 		{
@@ -559,7 +559,7 @@ int ssl_cipher_get_evp(const SSL_SESSION *s, const EVP_CIPHER **enc,
 		i= -1;
 		break;
 		}
-	if ((i < 0) || (i >= SSL_MD_NUM_IDX))
+	if ((i < 0) || (i > SSL_MD_NUM_IDX))
 	{
 		*md=NULL; 
 		if (mac_pkey_type!=NULL) *mac_pkey_type = NID_undef;
