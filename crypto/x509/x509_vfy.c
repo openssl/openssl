c33a04dabdf5d943db925ed1917e636c3f8c2dab
Viktor Dukhovni <openssl-users@dukhovni.org>
2014-06-22 20:39:52 -0400

    Fix typo in last commit

--- crypto/x509/x509_vfy.c
+++ crypto/x509/x509_vfy.c
@@ -752,7 +752,7 @@ static int check_hosts(X509 *x, X509_VERIFY_PARAM_ID *id)
 	for (i = 0; i < n; ++i)
 		{
 		name = (unsigned char *)sk_OPENSSL_STRING_value(id->hosts, i);
-		if (X509_check_host(x, name, 0, id->hostflags)) > 0)
+		if (X509_check_host(x, name, 0, id->hostflags) > 0)
 			return 1;
 		}
 	return n == 0;
