44724beeadf95712a42a8b21dc71bf110e89a262
Dr. Stephen Henson <steve@openssl.org>
2014-06-29 13:51:30 +0100

    Fix memory leak.
    PR#2531

--- ssl/t1_lib.c
+++ ssl/t1_lib.c
@@ -3455,7 +3455,11 @@ static int tls_decrypt_ticket(SSL *s, const unsigned char *etick, int eticklen,
 		}
 	EVP_DecryptUpdate(&ctx, sdec, &slen, p, eticklen);
 	if (EVP_DecryptFinal(&ctx, sdec + slen, &mlen) <= 0)
+		{
+		EVP_CIPHER_CTX_cleanup(&ctx);
+		OPENSSL_free(sdec);
 		return 2;
+		}
 	slen += mlen;
 	EVP_CIPHER_CTX_cleanup(&ctx);
 	p = sdec;
