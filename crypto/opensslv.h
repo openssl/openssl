371d9a627b911feb569d4bca7ffef5b1b4778e20
Matt Caswell <matt@openssl.org>
2014-07-22 21:31:04 +0100

    Prepare for 1.0.2-beta3-dev
    Reviewed-by: Stephen Henson <steve@openssl.org>

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -25,11 +25,11 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x10002002L
+#define OPENSSL_VERSION_NUMBER	0x10002003L
 #ifdef OPENSSL_FIPS
-#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.2-beta2-fips 22 Jul 2014"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.2-beta3-fips-dev xx XXX xxxx"
 #else
-#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.2-beta2 22 Jul 2014"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.2-beta3-dev xx XXX xxxx"
 #endif
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
