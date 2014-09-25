b1620443f3784fa11ae3018361b4fe547dc926bb
Emilia Kasper <emilia@openssl.org>
2014-09-25 13:39:21 +0200

    Add missing tests
    Accidentally omitted from commit 455b65dfab0de51c9f67b3c909311770f2b3f801
    
    Reviewed-by: Kurt Roeckx <kurt@openssl.org>
    (cherry picked from commit fdc35a9d3e8cf4cfd9330d5df9883f42cf5648ad)

--- crypto/constant_time_test.c
+++ crypto/constant_time_test.c
@@ -201,14 +201,14 @@ static int test_eq_int(int a, int b)
 	unsigned int equal = constant_time_eq_int(a, b);
 	if (a == b && equal != CONSTTIME_TRUE)
 		{
-		fprintf(stderr, "Test failed for constant_time_select(%d, %d): "
+		fprintf(stderr, "Test failed for constant_time_eq_int(%d, %d): "
 			"expected %du(TRUE), got %du\n",
 			a, b, CONSTTIME_TRUE, equal);
 		return 1;
 		}
 	else if (a != b && equal != CONSTTIME_FALSE)
 		{
-		fprintf(stderr, "Test failed for constant_time_select(%d, %d): "
+		fprintf(stderr, "Test failed for constant_time_eq_int(%d, %d): "
 			"expected %du(FALSE), got %du\n",
 			a, b, CONSTTIME_FALSE, equal);
 		return 1;
@@ -221,14 +221,14 @@ static int test_eq_int_8(int a, int b)
 	unsigned char equal = constant_time_eq_int_8(a, b);
 	if (a == b && equal != CONSTTIME_TRUE_8)
 		{
-		fprintf(stderr, "Test failed for constant_time_select(%d, %d): "
+		fprintf(stderr, "Test failed for constant_time_eq_int_8(%d, %d): "
 			"expected %u(TRUE), got %u\n",
 			a, b, CONSTTIME_TRUE_8, equal);
 		return 1;
 		}
 	else if (a != b && equal != CONSTTIME_FALSE_8)
 		{
-		fprintf(stderr, "Test failed for constant_time_select(%d, %d): "
+		fprintf(stderr, "Test failed for constant_time_eq_int_8(%d, %d): "
 			"expected %u(FALSE), got %u\n",
 			a, b, CONSTTIME_FALSE_8, equal);
 		return 1;
@@ -300,7 +300,9 @@ int main(int argc, char *argv[])
 			{
 			d = signed_test_values[j];
 			num_failed += test_select_int(c, d);
-			num_all += 1;
+			num_failed += test_eq_int(c, d);
+			num_failed += test_eq_int_8(c, d);
+			num_all += 3;
 			}
 		}
 
