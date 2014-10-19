4aa7f7cc7569f05719486b86fd6ff5cff33202c5
Dr. Stephen Henson <steve@openssl.org>
2014-10-19 01:14:21 +0100

    remove FIPS module code from crypto/ecdh
    Reviewed-by: Tim Hudson <tjh@openssl.org>

--- crypto/ecdh/ech_ossl.c
+++ crypto/ecdh/ech_ossl.c
@@ -119,14 +119,6 @@ static int ecdh_compute_key(void *out, size_t outlen, const EC_POINT *pub_key,
 	size_t buflen, len;
 	unsigned char *buf=NULL;
 
-#ifdef OPENSSL_FIPS
-	if(FIPS_selftest_failed())
-		{
-		FIPSerr(FIPS_F_ECDH_COMPUTE_KEY,FIPS_R_FIPS_SELFTEST_FAILED);
-		return -1;
-		}
-#endif
-
 	if (outlen > INT_MAX)
 		{
 		ECDHerr(ECDH_F_ECDH_COMPUTE_KEY,ERR_R_MALLOC_FAILURE); /* sort of, anyway */
