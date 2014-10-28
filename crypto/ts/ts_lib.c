9905b75ab904d2e94112c89d02cfb81b910b9b33
Matt Caswell <matt@openssl.org>
2014-10-28 23:04:10 +0000

    Implement internally opaque bn access from ts
    Reviewed-by: Tim Hudson <tjh@openssl.org>

--- crypto/ts/ts_lib.c
+++ crypto/ts/ts_lib.c
@@ -69,19 +69,20 @@
 
 int TS_ASN1_INTEGER_print_bio(BIO *bio, const ASN1_INTEGER *num)
 	{
-	BIGNUM num_bn;
+	BIGNUM *num_bn;
 	int result = 0;
 	char *hex;
 
-	BN_init(&num_bn);
-	ASN1_INTEGER_to_BN(num, &num_bn);
-	if ((hex = BN_bn2hex(&num_bn))) 
+	num_bn = BN_new();
+	if(!num_bn) return -1;
+	ASN1_INTEGER_to_BN(num, num_bn);
+	if ((hex = BN_bn2hex(num_bn)))
 		{
 		result = BIO_write(bio, "0x", 2) > 0;
 		result = result && BIO_write(bio, hex, strlen(hex)) > 0;
 		OPENSSL_free(hex);
 		}
-	BN_free(&num_bn);
+	BN_free(num_bn);
 
 	return result;
 	}
