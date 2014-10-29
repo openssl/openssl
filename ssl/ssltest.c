821bee4333f4d4e72390f18b39a23e9a93d8b2d9
Dr. Stephen Henson <steve@openssl.org>
2014-10-29 12:51:31 +0000

    New option no-ssl3-method which removes SSLv3_*method
    When no-ssl3 is set only make SSLv3 disabled by default. Retain -ssl3
    options for s_client/s_server/ssltest.
    
    When no-ssl3-method is set SSLv3_*method() is removed and all -ssl3
    options.
    
    We should document this somewhere, e.g. wiki, FAQ or manual page.
    Reviewed-by: Emilia Käsper <emilia@openssl.org>
    
    (cherry picked from commit 3881d8106df732fc433d30446625dfa2396da42d)
    
    Conflicts:
    	util/mkdef.pl

--- ssl/ssltest.c
+++ ssl/ssltest.c
@@ -345,7 +345,7 @@ static void sv_usage(void)
 #ifndef OPENSSL_NO_SSL2
 	fprintf(stderr," -ssl2         - use SSLv2\n");
 #endif
-#ifndef OPENSSL_NO_SSL3
+#ifndef OPENSSL_NO_SSL3_METHOD
 	fprintf(stderr," -ssl3         - use SSLv3\n");
 #endif
 #ifndef OPENSSL_NO_TLS1
@@ -676,7 +676,7 @@ int main(int argc, char *argv[])
 			}
 		else if	(strcmp(*argv,"-ssl3") == 0)
 			{
-#ifdef OPENSSL_NO_SSL3
+#ifdef OPENSSL_NO_SSL3_METHOD
 			no_protocol = 1;
 #endif
 			ssl3 = 1;
