2e8d26bdc3d482301c98c69343c81648a45c0a0e
Matt Caswell <matt@openssl.org>
2014-11-17 23:09:05 +0000

    Added references to RFC 7027
    Reviewed-by: Dr. Stephen Henson <steve@openssl.org>

--- ssl/t1_lib.c
+++ ssl/t1_lib.c
@@ -312,7 +312,7 @@ static const unsigned char suiteb_curves[] =
 
 int tls1_ec_curve_id2nid(int curve_id)
 	{
-	/* ECC curves from RFC 4492 */
+	/* ECC curves from RFC 4492 and RFC 7027 */
 	if ((curve_id < 1) || ((unsigned int)curve_id >
 				sizeof(nid_list)/sizeof(nid_list[0])))
 		return 0;
@@ -321,7 +321,7 @@ int tls1_ec_curve_id2nid(int curve_id)
 
 int tls1_ec_nid2curve_id(int nid)
 	{
-	/* ECC curves from RFC 4492 */
+	/* ECC curves from RFC 4492 and RFC 7027 */
 	switch (nid)
 		{
 	case NID_sect163k1: /* sect163k1 (1) */
