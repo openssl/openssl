32b07f5a80d22b34cfcd6df76d425bed771b0146
Matt Caswell <matt@openssl.org>
2014-12-22 11:34:24 +0000

    Additional fix required for no-srtp to work
    RT3638
    
    Reviewed-by: Emilia Käsper <emilia@openssl.org>

--- ssl/d1_srtp.c
+++ ssl/d1_srtp.c
@@ -115,12 +115,12 @@
   Copyright (C) 2011, RTFM, Inc.
 */
 
-#ifndef OPENSSL_NO_SRTP
 
 #include <stdio.h>
 #include <openssl/objects.h>
 #include "ssl_locl.h"
 
+#ifndef OPENSSL_NO_SRTP
 
 static SRTP_PROTECTION_PROFILE srtp_known_profiles[]=
     {
