264212b643c621d7e89079c1d2b76f87beec7ceb
Kurt Roeckx <kurt@roeckx.be>
2015-01-02 12:27:57 +0100

    Make build reproducible
    It contained a date on when it was build.
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- crypto/cversion.c
+++ crypto/cversion.c
@@ -69,7 +69,11 @@ const char *SSLeay_version(int t)
 	if (t == SSLEAY_BUILT_ON)
 		{
 #ifdef DATE
+# ifdef OPENSSL_USE_BUILD_DATE
 		return(DATE);
+# else
+		return("built on: reproducible build, date unspecified");
+# endif
 #else
 		return("built on: date not available");
 #endif
