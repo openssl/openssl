e224c45317b8a78ff52a121aac30ffc1499410c8
Doug Hogan <doug@acyclic.org>
2015-01-07 18:21:01 -0800

    Avoid a double-free in an error path.
    Signed-off-by: Kurt Roeckx <kurt@roeckx.be>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (cherry picked from commit 1549a265209d449b6aefd2b49d7d39f7fbe0689b)

--- crypto/dh/dh_ameth.c
+++ crypto/dh/dh_ameth.c
@@ -258,6 +258,7 @@ static int dh_priv_encode(PKCS8_PRIV_KEY_INFO *p8, const EVP_PKEY *pkey)
     dplen = i2d_ASN1_INTEGER(prkey, &dp);
 
     ASN1_INTEGER_free(prkey);
+    prkey = NULL;
 
     if (!PKCS8_pkey_set0(p8, OBJ_nid2obj(NID_dhKeyAgreement), 0,
                          V_ASN1_SEQUENCE, params, dp, dplen))
