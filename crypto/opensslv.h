3a9a0321638ae13957b66baae6d4955597fc128d
Matt Caswell <matt@openssl.org>
2015-01-15 14:49:54 +0000

    Prepare for 1.0.1m-dev
    Reviewed-by: Stephen Henson <steve@openssl.org>

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -29,11 +29,11 @@ extern "C" {
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x100010cfL
+#define OPENSSL_VERSION_NUMBER	0x100010d0L
 #ifdef OPENSSL_FIPS
-#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.1l-fips 15 Jan 2015"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.1m-fips-dev xx XXX xxxx"
 #else
-#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.1l 15 Jan 2015"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.1m-dev xx XXX xxxx"
 #endif
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
