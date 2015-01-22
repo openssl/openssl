633e96de8a6507dc5a2173658a48c0d0b4a36321
Matt Caswell <matt@openssl.org>
2015-01-22 11:04:47 +0000

    Fix post-reformat errors preventing windows compilation
    Reviewed-by: Tim Hudson <tjh@openssl.org>

--- ssl/d1_lib.c
+++ ssl/d1_lib.c
@@ -528,7 +528,7 @@ static void get_current_time(struct timeval *t)
 # ifdef  __MINGW32__
     now.ul -= 116444736000000000ULL;
 # else
-    now.ul -= 116444736000000000U I64; /* re-bias to 1/1/1970 */
+    now.ul -= 116444736000000000UI64; /* re-bias to 1/1/1970 */
 # endif
     t->tv_sec = (long)(now.ul / 10000000);
     t->tv_usec = ((int)(now.ul % 10000000)) / 10;
