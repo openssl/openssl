4ac0329582829f5378d8078c8d314ad37db87736
Matt Caswell <matt@openssl.org>
2015-01-22 16:12:26 +0000

    Prepare for 1.0.2 release
    Reviewed-by: Stephen Henson <steve@openssl.org>

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -30,11 +30,11 @@ extern "C" {
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-# define OPENSSL_VERSION_NUMBER  0x10002004L
+# define OPENSSL_VERSION_NUMBER  0x1000200fL
 # ifdef OPENSSL_FIPS
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2-beta4-fips-dev xx XXX xxxx"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2-fips 22 Jan 2015"
 # else
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2-beta4-dev xx XXX xxxx"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2 22 Jan 2015"
 # endif
 # define OPENSSL_VERSION_PTEXT   " part of " OPENSSL_VERSION_TEXT
 
