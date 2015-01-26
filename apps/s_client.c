131d3fdfe2deae6c7ff180c561c088e3e11dd659
Matt Caswell <matt@openssl.org>
2015-01-26 16:46:49 +0000

    Remove explicit setting of read_ahead for DTLS. It never makes sense not to use read_ahead with DTLS because it doesn't work. Therefore read_ahead needs to be the default.
    Reviewed-by: Andy Polyakov <appro@openssl.org>
    (cherry picked from commit f4002412518703d07fee321d4c88ee0bbe1694fe)
    
    Conflicts:
    	apps/s_client.c
    	apps/s_server.c

--- apps/s_client.c
+++ apps/s_client.c
@@ -1149,12 +1149,6 @@ int MAIN(int argc, char **argv)
 
     if (clr)
         SSL_CTX_clear_options(ctx, clr);
-    /*
-     * DTLS: partial reads end up discarding unread UDP bytes :-( Setting
-     * read ahead solves this problem.
-     */
-    if (socket_type == SOCK_DGRAM)
-        SSL_CTX_set_read_ahead(ctx, 1);
 
 #if !defined(OPENSSL_NO_TLSEXT) && !defined(OPENSSL_NO_NEXTPROTONEG)
     if (next_proto.data)
