872f91c4036e35d292d423e751741ba76f8c5594
Rich Salz <rsalz@openssl.org>
2015-02-12 14:23:28 -0500

    RT3684: rand_egd needs stddef.h
    Reviewed-by: Andy Polyakov <appro@openssl.org>

--- crypto/rand/rand_egd.c
+++ crypto/rand/rand_egd.c
@@ -113,6 +113,7 @@ int RAND_egd_bytes(const char *path, int bytes)
 #else
 # include <openssl/opensslconf.h>
 # include OPENSSL_UNISTD
+# include <stddef.h>
 # include <sys/types.h>
 # include <sys/socket.h>
 # ifndef NO_SYS_UN_H
