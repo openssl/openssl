9fdbaf3a322689a58381c724e4f3497320a69581
Dr. Stephen Henson <steve@openssl.org>
2015-02-18 00:34:59 +0000

    Check public key is not NULL.
    CVE-2015-0288
    PR#3708
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (cherry picked from commit 28a00bcd8e318da18031b2ac8778c64147cd54f9)

--- crypto/x509/x509_req.c
+++ crypto/x509/x509_req.c
@@ -92,6 +92,8 @@ X509_REQ *X509_to_X509_REQ(X509 *x, EVP_PKEY *pkey, const EVP_MD *md)
         goto err;
 
     pktmp = X509_get_pubkey(x);
+    if (pktmp == NULL)
+        goto err;
     i = X509_REQ_set_pubkey(ret, pktmp);
     EVP_PKEY_free(pktmp);
     if (!i)
