35313768c77191a61bd6e34393d89e7b7cd78465
Dr. Stephen Henson <steve@openssl.org>
2015-02-26 19:23:38 +0000

    Make OpenSSL compile with no-rc4
    Reviewed-by: Matt Caswell <matt@openssl.org>

--- apps/dsa.c
+++ apps/dsa.c
@@ -113,7 +113,9 @@ int MAIN(int argc, char **argv)
     char *passin = NULL, *passout = NULL;
     int modulus = 0;
 
+#ifndef OPENSSL_NO_RC4
     int pvk_encr = 2;
+#endif
 
     apps_startup();
 
@@ -168,12 +170,14 @@ int MAIN(int argc, char **argv)
             engine = *(++argv);
         }
 # endif
+#ifndef OPENSSL_NO_RC4
         else if (strcmp(*argv, "-pvk-strong") == 0)
             pvk_encr = 2;
         else if (strcmp(*argv, "-pvk-weak") == 0)
             pvk_encr = 1;
         else if (strcmp(*argv, "-pvk-none") == 0)
             pvk_encr = 0;
+#endif
         else if (strcmp(*argv, "-noout") == 0)
             noout = 1;
         else if (strcmp(*argv, "-text") == 0)
