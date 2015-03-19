06cf4418cf45253afe401fd4270effd439103dca
Matt Caswell <matt@openssl.org>
2015-03-19 13:45:41 +0000

    Prepare for 1.0.0s-dev
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -26,11 +26,11 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-# define OPENSSL_VERSION_NUMBER  0x1000012fL
+# define OPENSSL_VERSION_NUMBER  0x10000130L
 # ifdef OPENSSL_FIPS
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.0r-fips 19 Mar 2015"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.0s-fips-dev xx XXX xxxx"
 # else
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.0r 19 Mar 2015"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.0s-dev xx XXX xxxx"
 # endif
 # define OPENSSL_VERSION_PTEXT   " part of " OPENSSL_VERSION_TEXT
 
