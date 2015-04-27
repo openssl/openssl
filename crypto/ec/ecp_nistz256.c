df6c736fbd0c1779df3464c8dfdaae723d65e8e5
Emilia Kasper <emilia@openssl.org>
2015-04-27 15:41:52 +0200

    NISTZ256: set Z_is_one to boolean 0/1 as is customary.
    Cosmetic, no real effect.
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (cherry picked from commit 4446044a793a9103a4bc70c0214005e6a4463767)

--- crypto/ec/ecp_nistz256.c
+++ crypto/ec/ecp_nistz256.c
@@ -1327,7 +1327,7 @@ static int ecp_nistz256_points_mul(const EC_GROUP *group,
         !ecp_nistz256_set_words(&r->Z, p.p.Z)) {
         goto err;
     }
-    r->Z_is_one = is_one(p.p.Z);
+    r->Z_is_one = is_one(p.p.Z) & 1;
 
     ret = 1;
 
