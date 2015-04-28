75862f7741d52651eefc2ae71e81a0d0e9d4c5ec
Matt Caswell <matt@openssl.org>
2015-04-28 15:19:50 +0100

    Sanity check the return from final_finish_mac
    The return value is checked for 0. This is currently safe but we should
    really check for <= 0 since -1 is frequently used for error conditions.
    Thanks to Kevin Wojtysiak (Int3 Solutions) and Paramjot Oberoi (Int3
    Solutions) for reporting this issue.
    
    Reviewed-by: Andy Polyakov <appro@openssl.org>
    (cherry picked from commit c427570e5098e120cbcb66e799f85c317aac7b91)
    
    Conflicts:
    	ssl/ssl_locl.h

--- ssl/ssl_locl.h
+++ ssl/ssl_locl.h
@@ -1230,7 +1230,6 @@ int dtls1_write_app_data_bytes(SSL *s, int type, const void *buf, int len);
 int dtls1_write_bytes(SSL *s, int type, const void *buf, int len);
 
 int dtls1_send_change_cipher_spec(SSL *s, int a, int b);
-int dtls1_send_finished(SSL *s, int a, int b, const char *sender, int slen);
 int dtls1_read_failed(SSL *s, int code);
 int dtls1_buffer_message(SSL *s, int ccs);
 int dtls1_retransmit_message(SSL *s, unsigned short seq,
