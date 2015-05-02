ccbfc0b94fd5075fd110912f3a5711980accc410
Gilles Khouzam <gillesk@microsoft.com>
2015-05-01 18:19:53 -0700

    Fixing Win8.1 caused by Windows.winmd reference got changed.
    Verified both Win10 and Win8.1

--- util/mk1mf.pl
+++ util/mk1mf.pl
@@ -748,13 +748,20 @@ foreach (values %lib_nam)
 	}
 
 # C++/CX extensions for winrt.obj, so we can generate entropy on the platform using these extensions.
-if ($platform eq "VC-WINPHONE" or $platform eq "VC-WINSTORE" or $platform eq "VC-WINUNIVERSAL") {
+if ($platform eq "VC-WINPHONE" or $platform eq "VC-WINSTORE") {
+    $rules.= <<"EOF";
+\$(OBJ_D)\\winrt.obj: \$(SRC_D)\\ms\\winrt.cpp
+	\$(CC)  /ZW:nostdlib /EHsc /ZW /Fo\$(OBJ_D)\\winrt.obj /FUPlatform.winmd  /FUWindows.winmd \$(LIB_CFLAGS) -c \$(SRC_D)\\ms\\winrt.cpp
+
+EOF
+} elsif($platform eq "VC-WINUNIVERSAL") {
     $rules.= <<"EOF";
 \$(OBJ_D)\\winrt.obj: \$(SRC_D)\\ms\\winrt.cpp
 	\$(CC)  /ZW:nostdlib /EHsc /ZW /Fo\$(OBJ_D)\\winrt.obj /FUPlatform.winmd  /FUwindows.foundation.foundationcontract.winmd /FUwindows.foundation.universalapicontract.winmd \$(LIB_CFLAGS) -c \$(SRC_D)\\ms\\winrt.cpp
 
 EOF
 }
+
 # hack to add version info on MSVC
 if (($platform eq "VC-WIN32") || ($platform eq "VC-WIN64A")
 	|| ($platform eq "VC-WIN64I") || ($platform eq "VC-NT")) {
