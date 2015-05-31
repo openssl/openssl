97025c94d7c68fd3fd9b3ed37dcc0df14091dfa1
Richard Levitte <levitte@openssl.org>
2015-05-31 17:47:31 +0200

    Add the macro OPENSSL_SYS_WIN64
    This is for consistency.
    Additionally, have its presence define OPENSSL_SYS_WINDOWS as well.
    
    Reviewed-by: Matt Caswell <matt@openssl.org>

--- include/openssl/e_os2.h
+++ include/openssl/e_os2.h
@@ -104,6 +104,12 @@ extern "C" {
 #     define OPENSSL_SYS_WIN32
 #    endif
 #   endif
+#   if defined(_WIN64) || defined(OPENSSL_SYS_WIN64)
+#    undef OPENSSL_SYS_UNIX
+#    if !defined(OPENSSL_SYS_WIN64)
+#     define OPENSSL_SYS_WIN64
+#    endif
+#   endif
 #   if defined(OPENSSL_SYS_WINNT)
 #    undef OPENSSL_SYS_UNIX
 #   endif
@@ -114,7 +120,7 @@ extern "C" {
 # endif
 
 /* Anything that tries to look like Microsoft is "Windows" */
-# if defined(OPENSSL_SYS_WIN32) || defined(OPENSSL_SYS_WINNT) || defined(OPENSSL_SYS_WINCE)
+# if defined(OPENSSL_SYS_WIN32) || defined(OPENSSL_SYS_WIN64) || defined(OPENSSL_SYS_WINNT) || defined(OPENSSL_SYS_WINCE)
 #  undef OPENSSL_SYS_UNIX
 #  define OPENSSL_SYS_WINDOWS
 #  ifndef OPENSSL_SYS_MSDOS
