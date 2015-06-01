1fdf36f5116f7e31e64e4027874e01eac3f0817d
Andy Polyakov <appro@openssl.org>
2015-06-01 22:51:33 +0200

    gcm.c: address linker warning about OPENSSL_ia32cap_P size mismatch.
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    (cherry picked from commit 75c4827dfe6f52ccd687a283e6108a88ba3b8ec4)

--- crypto/modes/gcm128.c
+++ crypto/modes/gcm128.c
@@ -694,7 +694,7 @@ static void gcm_gmult_1bit(u64 Xi[2], const u64 H[2])
          defined(_M_IX86)       || defined(_M_AMD64)    || defined(_M_X64))
 #  define GHASH_ASM_X86_OR_64
 #  define GCM_FUNCREF_4BIT
-extern unsigned int OPENSSL_ia32cap_P[2];
+extern unsigned int OPENSSL_ia32cap_P[];
 
 void gcm_init_clmul(u128 Htable[16], const u64 Xi[2]);
 void gcm_gmult_clmul(u64 Xi[2], const u128 Htable[16]);
