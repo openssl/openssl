1a9a506cfbb3a57215dae72aadab8943b977bcf7
Loganaden Velvindron <loganaden@gmail.com>
2015-07-31 13:20:16 -0400

    Clear BN-mont values when free'ing it.
    From a CloudFlare patch.
    
    Reviewed-by: Dr. Stephen Henson <steve@openssl.org>
    (cherry picked from commit 1a586b3942de1c0bd64203d09385d5e74f499d8d)

--- crypto/bn/bn_mont.c
+++ crypto/bn/bn_mont.c
@@ -361,9 +361,9 @@ void BN_MONT_CTX_free(BN_MONT_CTX *mont)
     if (mont == NULL)
         return;
 
-    BN_free(&(mont->RR));
-    BN_free(&(mont->N));
-    BN_free(&(mont->Ni));
+    BN_clear_free(&(mont->RR));
+    BN_clear_free(&(mont->N));
+    BN_clear_free(&(mont->Ni));
     if (mont->flags & BN_FLG_MALLOCED)
         OPENSSL_free(mont);
 }
