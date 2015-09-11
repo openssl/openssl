01d8ae61f236d24a5a04992866d94ec10f7d69a2
Dr. Stephen Henson <steve@openssl.org>
2015-09-11 16:13:52 +0100

    Use default field separator.
    If the field separator isn't specified through -nameopt then use
    XN_FLAG_SEP_CPLUS_SPC instead of printing nothing and returing an error.
    
    PR#2397
    
    Reviewed-by: Tim Hudson <tjh@openssl.org>
    (cherry picked from commit 03706afa30aeb4407287171a9d6f9a765395d0a2)

--- apps/apps.c
+++ apps/apps.c
@@ -1236,7 +1236,11 @@ int set_name_ex(unsigned long *flags, const char *arg)
         {"ca_default", XN_FLAG_MULTILINE, 0xffffffffL},
         {NULL, 0, 0}
     };
-    return set_multi_opts(flags, arg, ex_tbl);
+    if (set_multi_opts(flags, arg, ex_tbl) == 0)
+        return 0;
+    if ((*flags & XN_FLAG_SEP_MASK) == 0)
+        *flags |= XN_FLAG_SEP_CPLUS_SPC;
+    return 1;
 }
 
 int set_ext_copy(int *copy_type, const char *arg)
