6e2373cb58455ae88e1a25a4d238e294119cea63
Alessandro Ghedini <alessandro@ghedini.me>
2015-09-16 21:53:58 +0200

    Fix build on mingw
    When OPENSSL_EXPORT_VAR_AS_FUNCTION is defined, the static_ASN1_ITEM_start
    macro doesn't exist so the build fails. This problem was introduced in
    commit df2ee0e.

--- include/openssl/asn1t.h
+++ include/openssl/asn1t.h
@@ -102,6 +102,9 @@ extern "C" {
         { \
                 static const ASN1_ITEM local_it = {
 
+#  define static_ASN1_ITEM_start(itname) \
+        ASN1_ITEM_start(itname)
+
 #  define ASN1_ITEM_end(itname) \
                 }; \
         return &local_it; \
