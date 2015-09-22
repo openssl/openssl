d102d9df8625cb6f75c537b7f2a696bb5f305ff2
Matt Caswell <matt@openssl.org>
2015-09-22 11:12:50 +0100

    Implement write pipeline support in libssl
    Use the new pipeline cipher capability to encrypt multiple records being
    written out all in one go. Two new SSL/SSL_CTX parameters can be used to
    control how this works: max_pipelines and split_send_fragment.
    
    max_pipelines defines the maximum number of pipelines that can ever be used
    in one go for a single connection. It must always be less than or equal to
    SSL_MAX_PIPELINES (currently defined to be 32). By default only one
    pipeline will be used (i.e. normal non-parallel operation).
    
    split_send_fragment defines how data is split up into pipelines. The number
    of pipelines used will be determined by the amount of data provided to the
    SSL_write call divided by split_send_fragment. For example if
    split_send_fragment is set to 2000 and max_pipelines is 4 then:
    SSL_write called with 0-2000 bytes == 1 pipeline used
    SSL_write called with 2001-4000 bytes == 2 pipelines used
    SSL_write called with 4001-6000 bytes == 3 pipelines used
    SSL_write_called with 6001+ bytes == 4 pipelines used
    
    split_send_fragment must always be less than or equal to max_send_fragment.
    By default it is set to be equal to max_send_fragment. This will mean that
    the same number of records will always be created as would have been
    created in the non-parallel case, although the data will be apportioned
    differently. In the parallel case data will be spread equally between the
    pipelines.
    
    Reviewed-by: Tim Hudson <tjh@openssl.org>

--- ssl/record/rec_layer_d1.c
+++ ssl/record/rec_layer_d1.c
@@ -1039,7 +1039,7 @@ int do_dtls1_write(SSL *s, int type, const unsigned char *buf,
     SSL3_BUFFER *wb;
     SSL_SESSION *sess;
 
-    wb = &s->rlayer.wbuf;
+    wb = &s->rlayer.wbuf[0];
 
     /*
      * first check if there is a SSL3_BUFFER still being written out.  This
@@ -1128,7 +1128,7 @@ int do_dtls1_write(SSL *s, int type, const unsigned char *buf,
 
     /* first we compress */
     if (s->compress != NULL) {
-        if (!ssl3_do_compress(s)) {
+        if (!ssl3_do_compress(s, wr)) {
             SSLerr(SSL_F_DO_DTLS1_WRITE, SSL_R_COMPRESSION_FAILURE);
             goto err;
         }
@@ -1145,7 +1145,7 @@ int do_dtls1_write(SSL *s, int type, const unsigned char *buf,
      */
 
     if (mac_size != 0) {
-        if (s->method->ssl3_enc->mac(s,
+        if (s->method->ssl3_enc->mac(s, wr,
                 &(p[SSL3_RECORD_get_length(wr) + eivlen]), 1) < 0)
             goto err;
         SSL3_RECORD_add_length(wr, mac_size);
@@ -1158,7 +1158,7 @@ int do_dtls1_write(SSL *s, int type, const unsigned char *buf,
     if (eivlen)
         SSL3_RECORD_add_length(wr, eivlen);
 
-    if (s->method->ssl3_enc->enc(s, 1) < 1)
+    if (s->method->ssl3_enc->enc(s, wr, 1, 1) < 1)
         goto err;
 
     /* record length after mac and block padding */
