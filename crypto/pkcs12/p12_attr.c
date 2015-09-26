54c38b7f0dda668be82199b4e4aa56c1f6afe3ea
Dr. Stephen Henson <steve@openssl.org>
2015-09-26 12:26:51 +0100

    Make PKCS12 structures opaque
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- crypto/pkcs12/p12_attr.c
+++ crypto/pkcs12/p12_attr.c
@@ -59,6 +59,7 @@
 #include <stdio.h>
 #include "internal/cryptlib.h"
 #include <openssl/pkcs12.h>
+#include "p12_lcl.h"
 
 /* Add a local keyid to a safebag */
 
--- crypto/pkcs12/p12_crt.c
+++ crypto/pkcs12/p12_crt.c
@@ -59,6 +59,7 @@
 #include <stdio.h>
 #include "internal/cryptlib.h"
 #include <openssl/pkcs12.h>
+#include "p12_lcl.h"
 
 static int pkcs12_add_bag(STACK_OF(PKCS12_SAFEBAG) **pbags,
                           PKCS12_SAFEBAG *bag);
