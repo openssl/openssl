1b0eda13e77eb773057a1cc94bcb60ab8a38db29
Dmitry Belyavskiy <beldmit@gmail.com>
2015-09-30 22:21:50 +0300

    Alignment

--- ssl/ssl_ciph.c
+++ ssl/ssl_ciph.c
@@ -2055,7 +2055,7 @@ int ssl_cipher_get_cert_index(const SSL_CIPHER *c)
         return SSL_PKEY_DSA_SIGN;
     else if (alg_a & SSL_aRSA)
         return SSL_PKEY_RSA_ENC;
-		else if (alg_a & SSL_aGOST12)
+    else if (alg_a & SSL_aGOST12)
         return SSL_PKEY_GOST_EC;
     else if (alg_a & SSL_aGOST01)
         return SSL_PKEY_GOST01;
