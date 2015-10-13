8b7080b0b7f30669c0784d8aa73388f95bbd056b
Matt Caswell <matt@openssl.org>
2015-10-13 15:02:47 +0100

    Remove Obsolete engines
    There are a number of engines in the OpenSSL source code which are now
    obsolete. The following engines have been removed: 4758cca, aep, atalla,
    cswift, nuron, sureware.
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- crypto/engine/eng_all.c
+++ crypto/engine/eng_all.c
@@ -73,28 +73,10 @@ void ENGINE_load_builtin_engines(void)
     ENGINE_load_dynamic();
 #ifndef OPENSSL_NO_STATIC_ENGINE
 # ifndef OPENSSL_NO_HW
-#  ifndef OPENSSL_NO_HW_4758_CCA
-    ENGINE_load_4758cca();
-#  endif
 /*-
  * These engines have been disabled as they do not currently build
-#ifndef OPENSSL_NO_HW_AEP
-        ENGINE_load_aep();
-#endif
-#ifndef OPENSSL_NO_HW_ATALLA
-        ENGINE_load_atalla();
-#endif
-#ifndef OPENSSL_NO_HW_CSWIFT
-        ENGINE_load_cswift();
-#endif
 #ifndef OPENSSL_NO_HW_NCIPHER
         ENGINE_load_chil();
-#endif
-#ifndef OPENSSL_NO_HW_NURON
-        ENGINE_load_nuron();
-#endif
-#ifndef OPENSSL_NO_HW_SUREWARE
-        ENGINE_load_sureware();
 #endif
 #ifndef OPENSSL_NO_HW_UBSEC
         ENGINE_load_ubsec();
