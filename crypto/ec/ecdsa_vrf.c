b3084796d4244082f3c01361c677e69bb58818d1
Dr. Stephen Henson <steve@openssl.org>
2015-10-28 16:57:51 +0000

    Top level ECDSA sign/verify redirection.
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- crypto/ec/ecdsa_vrf.c
+++ crypto/ec/ecdsa_vrf.c
@@ -88,24 +88,10 @@ int ECDSA_do_verify(const unsigned char *dgst, int dgst_len,
 int ECDSA_verify(int type, const unsigned char *dgst, int dgst_len,
                  const unsigned char *sigbuf, int sig_len, EC_KEY *eckey)
 {
-    ECDSA_SIG *s;
-    const unsigned char *p = sigbuf;
-    unsigned char *der = NULL;
-    int derlen = -1;
-    int ret = -1;
-
-    s = ECDSA_SIG_new();
-    if (s == NULL)
-        return (ret);
-    if (d2i_ECDSA_SIG(&s, &p, sig_len) == NULL)
-        goto err;
-    /* Ensure signature uses DER and doesn't have trailing garbage */
-    derlen = i2d_ECDSA_SIG(s, &der);
-    if (derlen != sig_len || memcmp(sigbuf, der, derlen))
-        goto err;
-    ret = ECDSA_do_verify(dgst, dgst_len, s, eckey);
- err:
-    OPENSSL_clear_free(der, derlen);
-    ECDSA_SIG_free(s);
-    return (ret);
+    if (eckey->meth->verify)
+        return eckey->meth->verify(type, dgst, dgst_len, sigbuf, sig_len,
+                                   eckey);
+    ECerr(EC_F_ECDSA_VERIFY, EC_R_OPERATION_NOT_SUPPORTED);
+    return 0;
+    return 0;
 }
