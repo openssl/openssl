2688d99989902dea884632a8658f3abad0c26d16
Andy Polyakov <appro@openssl.org>
2015-11-14 00:10:19 +0100

    crypto/ppccap.c: add SIGILL-free processor capability detection code.
    Reviewed-by: Kurt Roeckx <kurt@openssl.org>

--- crypto/ppc_arch.h
+++ crypto/ppc_arch.h
@@ -3,8 +3,14 @@
 
 extern unsigned int OPENSSL_ppccap_P;
 
+/*
+ * Flags' usage can appear ambiguous, because they are set rather
+ * to reflect OpenSSL performance preferences than actual processor
+ * capabilities.
+ */
 # define PPC_FPU64       (1<<0)
 # define PPC_ALTIVEC     (1<<1)
 # define PPC_CRYPTO207   (1<<2)
+# define PPC_FPU         (1<<3)
 
 #endif
