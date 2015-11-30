32fd54a9a36c172cf4e5fe4b7af2ae1f1ce1bc0a
Richard Levitte <levitte@openssl.org>
2015-11-30 23:42:39 +0100

    Remove HMAC_CTX_cleanup and combine its functionality into EVP_MD_CTX_init
    This follows the same idea as the combination of EVP_MD_CTX_cleanup
    and EVP_MD_CTX_init into one function.
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- include/openssl/hmac.h
+++ include/openssl/hmac.h
@@ -70,9 +70,8 @@ extern "C" {
 
 size_t HMAC_size(HMAC_CTX *e);
 HMAC_CTX *HMAC_CTX_new(void);
-void HMAC_CTX_free(HMAC_CTX *ctx);
 int HMAC_CTX_init(HMAC_CTX *ctx);
-void HMAC_CTX_cleanup(HMAC_CTX *ctx);
+void HMAC_CTX_free(HMAC_CTX *ctx);
 
 #ifdef OPENSSL_USE_DEPRECATED
 
