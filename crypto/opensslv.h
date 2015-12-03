74743418dca737ff584530470c54d1a7061e7e0a
Matt Caswell <matt@openssl.org>
2015-12-03 15:00:54 +0000

    Prepare for 0.9.8zi-dev
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -26,11 +26,11 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-# define OPENSSL_VERSION_NUMBER  0x0090821fL
+# define OPENSSL_VERSION_NUMBER  0x00908220L
 # ifdef OPENSSL_FIPS
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 0.9.8zh-fips 3 Dec 2015"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 0.9.8zi-fips-dev xx XXX xxxx"
 # else
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 0.9.8zh 3 Dec 2015"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 0.9.8zi-dev xx XXX xxxx"
 # endif
 # define OPENSSL_VERSION_PTEXT   " part of " OPENSSL_VERSION_TEXT
 
