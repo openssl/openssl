6870fbb91f4150889bb258e4dfba2646dd0b9295
Joey Yandle <xoloki@gmail.com>
2015-12-20 18:44:11 -0800

    fix endif comment

--- crypto/rand/rand_win.c
+++ crypto/rand/rand_win.c
@@ -364,7 +364,7 @@ static void readscreen(void)
     /* Clean up */
     DeleteObject(hBitmap);
     ReleaseDC(NULL, hScrDC);
-# endif                         /* !OPENSSL_SYS_CYGWIN */
+# endif                         /* !OPENSSL_SYS_WIN32_CYGWIN */
 }
 
 #endif
