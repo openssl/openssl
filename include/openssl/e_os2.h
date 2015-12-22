80e0ecbf580e1455cdd6ccdb6f8f918295dca81c
Dr. Stephen Henson <steve@openssl.org>
2015-12-22 16:07:24 +0000

    Add ossl_inline
    Add macro ossl_inline for use in public headers where a portable inline
    is required. Change existing inline to use ossl_inline
    
    Reviewed-by: Kurt Roeckx <kurt@openssl.org>

--- include/openssl/e_os2.h
+++ include/openssl/e_os2.h
@@ -329,6 +329,25 @@ typedef unsigned __int64 uint64_t;
 #  endif
 # endif
 
+/* ossl_inline: portable inline definition usable in public headers */
+# if !defined(inline) && !defined(__cplusplus)
+#  if defined(__STDC_VERSION__) && __STDC_VERSION__>=199901L
+   /* just use inline */
+#   define ossl_inline inline
+#  elif defined(__GNUC__) && __GNUC__>=2
+#   define ossl_inline __inline__
+#  elif defined(_MSC_VER)
+  /*
+   * Visual Studio: inline is available in C++ only, however
+   * __inline is available for C, see
+   * http://msdn.microsoft.com/en-us/library/z8y1yy88.aspx
+   */
+#   define ossl_inline __inline
+#  else
+#   define ossl_inline
+#  endif
+# endif
+
 #ifdef  __cplusplus
 }
 #endif
