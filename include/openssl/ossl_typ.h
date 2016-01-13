e7fd6aee049c15813271a66d715a50a9a70ef7c9
Richard Levitte <levitte@openssl.org>
2016-01-13 21:29:24 +0100

    Remove spurious ;
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- include/openssl/ossl_typ.h
+++ include/openssl/ossl_typ.h
@@ -202,7 +202,7 @@ typedef struct ocsp_response_st OCSP_RESPONSE;
 typedef struct ocsp_responder_id_st OCSP_RESPID;
 
 #if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) \
-    && !defined(OPENSSL_SYS_VMS);
+    && !defined(OPENSSL_SYS_VMS)
 typedef intmax_t ossl_intmax_t;
 typedef uintmax_t ossl_uintmax_t;
 #else
