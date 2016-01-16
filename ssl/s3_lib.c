4d6fe78f65be650c84e14777c90e7a088f7a44ce
Viktor Dukhovni <openssl-users@dukhovni.org>
2016-01-16 12:57:24 -0500

    Empty SNI names are not valid
    While empty inputs to SSL_set1_host() clear the reference identifier
    list.
    
    (cherry-picked from 1.1.0-dev)
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- ssl/s3_lib.c
+++ ssl/s3_lib.c
@@ -3263,6 +3263,8 @@ long ssl3_ctrl(SSL *s, int cmd, long larg, void *parg)
 #ifndef OPENSSL_NO_TLSEXT
     case SSL_CTRL_SET_TLSEXT_HOSTNAME:
         if (larg == TLSEXT_NAMETYPE_host_name) {
+            size_t len;
+
             if (s->tlsext_hostname != NULL)
                 OPENSSL_free(s->tlsext_hostname);
             s->tlsext_hostname = NULL;
@@ -3270,7 +3272,8 @@ long ssl3_ctrl(SSL *s, int cmd, long larg, void *parg)
             ret = 1;
             if (parg == NULL)
                 break;
-            if (strlen((char *)parg) > TLSEXT_MAXLEN_host_name) {
+            len = strlen((char *)parg);
+            if (len == 0 || len > TLSEXT_MAXLEN_host_name) {
                 SSLerr(SSL_F_SSL3_CTRL, SSL_R_SSL3_EXT_INVALID_SERVERNAME);
                 return 0;
             }
