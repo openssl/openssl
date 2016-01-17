9391dc8cc3cbdddd1dfcacf51f2cda971609ee22
Rich Salz <rsalz@akamai.com>
2016-01-17 14:39:57 -0500

    Accessor update; fix API, document one.
    Reviewed-by: Dr. Stephen Henson <steve@openssl.org>

--- crypto/ts/ts_rsp_utils.c
+++ crypto/ts/ts_rsp_utils.c
@@ -398,17 +398,17 @@ int TS_STATUS_INFO_set_status(TS_STATUS_INFO *a, int i)
     return ASN1_INTEGER_set(a->status, i);
 }
 
-ASN1_INTEGER *TS_STATUS_INFO_get_status(TS_STATUS_INFO *a)
+ASN1_INTEGER *TS_STATUS_INFO_get0_status(TS_STATUS_INFO *a)
 {
     return a->status;
 }
 
-STACK_OF(ASN1_UTF8STRING) *TS_STATUS_INFO_get_text(TS_STATUS_INFO *a)
+STACK_OF(ASN1_UTF8STRING) *TS_STATUS_INFO_get0_text(TS_STATUS_INFO *a)
 {
     return a->text;
 }
 
-ASN1_BIT_STRING *TS_STATUS_INFO_get_failure_info(TS_STATUS_INFO *a)
+ASN1_BIT_STRING *TS_STATUS_INFO_get0_failure_info(TS_STATUS_INFO *a)
 {
     return a->failure_info;
 }
