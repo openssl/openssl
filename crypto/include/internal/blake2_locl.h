f972f324a859e99d465f343aa22100a804e5e8b6
Bill Cox <waywardgeek@google.com>
2016-01-18 04:54:22 -0800

    Reformatted BLAKE2 code to fit with OpenSSL style guidelines

--- crypto/include/internal/blake2_locl.h
+++ crypto/include/internal/blake2_locl.h
@@ -3,45 +3,86 @@
 
    Written in 2012 by Samuel Neves <sneves@dei.uc.pt>
 
-   To the extent possible under law, the author(s) have dedicated all copyright
-   and related and neighboring rights to this software to the public domain
-   worldwide. This software is distributed without any warranty.
-
-   You should have received a copy of the CC0 Public Domain Dedication along with
-   this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+   This version of BLAKE2 has been contributed under the OpenSSL license to the
+   OpenSSL project by the BKAKE2 authors.  More information about the BLAKE2
+   hash function can be found at https://blake2.net.
 */
-#pragma once
-#ifndef __BLAKE2_H__
-#define __BLAKE2_H__
+
+/* crypto/blake2/blake2_locl.h */
+/* ====================================================================
+ * Copyright (c) 1999-2016 The OpenSSL Project.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgment:
+ *    "This product includes software developed by the OpenSSL Project
+ *    for use in the OpenSSL Toolkit. (http://www.OpenSSL.org/)"
+ *
+ * 4. The names "OpenSSL Toolkit" and "OpenSSL Project" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For written permission, please contact
+ *    openssl-core@OpenSSL.org.
+ *
+ * 5. Products derived from this software may not be called "OpenSSL"
+ *    nor may "OpenSSL" appear in their names without prior written
+ *    permission of the OpenSSL Project.
+ *
+ * 6. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the OpenSSL Project
+ *    for use in the OpenSSL Toolkit (http://www.OpenSSL.org/)"
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE OpenSSL PROJECT ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OpenSSL PROJECT OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This product includes cryptographic software written by Eric Young
+ * (eay@cryptsoft.com).  This product includes software written by Tim
+ * Hudson (tjh@cryptsoft.com).
+ *
+ */
 
 #include <stddef.h>
 #include <stdint.h>
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
-
-  enum blake2s_constant
-  {
+enum blake2s_constant {
     BLAKE2S_BLOCKBYTES = 64,
     BLAKE2S_OUTBYTES   = 32,
     BLAKE2S_KEYBYTES   = 32,
     BLAKE2S_SALTBYTES  = 8,
     BLAKE2S_PERSONALBYTES = 8
-  };
+};
 
-  enum blake2b_constant
-  {
+enum blake2b_constant {
     BLAKE2B_BLOCKBYTES = 128,
     BLAKE2B_OUTBYTES   = 64,
     BLAKE2B_KEYBYTES   = 64,
     BLAKE2B_SALTBYTES  = 16,
     BLAKE2B_PERSONALBYTES = 16
-  };
+};
 
-#pragma pack(push, 1)
-  typedef struct __blake2s_param
-  {
+struct blake2s_param_st {
     uint8_t  digest_length; /* 1 */
     uint8_t  key_length;    /* 2 */
     uint8_t  fanout;        /* 3 */
@@ -53,20 +94,20 @@ extern "C" {
     /* uint8_t  reserved[0]; */
     uint8_t  salt[BLAKE2S_SALTBYTES]; /* 24 */
     uint8_t  personal[BLAKE2S_PERSONALBYTES];  /* 32 */
-  } blake2s_param;
+};
 
-  struct __blake2s_state
-  {
+typedef struct blake2s_param_st BLAKE2S_PARAM;
+
+struct blake2s_ctx_st {
     uint32_t h[8];
     uint32_t t[2];
     uint32_t f[2];
     uint8_t  buf[2 * BLAKE2S_BLOCKBYTES];
     size_t   buflen;
     uint8_t  last_node;
-  };
+};
 
-  typedef struct __blake2b_param
-  {
+struct blake2b_param_st {
     uint8_t  digest_length; /* 1 */
     uint8_t  key_length;    /* 2 */
     uint8_t  fanout;        /* 3 */
@@ -78,38 +119,15 @@ extern "C" {
     uint8_t  reserved[14];  /* 32 */
     uint8_t  salt[BLAKE2B_SALTBYTES]; /* 48 */
     uint8_t  personal[BLAKE2B_PERSONALBYTES];  /* 64 */
-  } blake2b_param;
+};
 
-  struct __blake2b_state
-  {
+typedef struct blake2b_param_st BLAKE2B_PARAM;
+
+struct blake2b_ctx_st {
     uint64_t h[8];
     uint64_t t[2];
     uint64_t f[2];
     uint8_t  buf[2 * BLAKE2B_BLOCKBYTES];
     size_t   buflen;
     uint8_t  last_node;
-  };
-
-  typedef struct __blake2sp_state
-  {
-    blake2s_state S[8][1];
-    blake2s_state R[1];
-    uint8_t buf[8 * BLAKE2S_BLOCKBYTES];
-    size_t  buflen;
-  } blake2sp_state;
-
-  typedef struct __blake2bp_state
-  {
-    blake2b_state S[4][1];
-    blake2b_state R[1];
-    uint8_t buf[4 * BLAKE2B_BLOCKBYTES];
-    size_t  buflen;
-  } blake2bp_state;
-#pragma pack(pop)
-
-#if defined(__cplusplus)
-}
-#endif
-
-#endif
-
+};
