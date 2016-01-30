7ab507495b86371756575d606af556b4fd74e27a
Dr. Stephen Henson <steve@openssl.org>
2016-01-30 02:51:01 +0000

    Add function to return internal enoding of X509_NAME.
    PR#4280
    
    Reviewed-by: Tim Hudson <tjh@openssl.org>

--- include/openssl/x509.h
+++ include/openssl/x509.h
@@ -873,6 +873,9 @@ ASN1_OBJECT *X509_NAME_ENTRY_get_object(X509_NAME_ENTRY *ne);
 ASN1_STRING *X509_NAME_ENTRY_get_data(X509_NAME_ENTRY *ne);
 int X509_NAME_ENTRY_set(const X509_NAME_ENTRY *ne);
 
+int X509_NAME_get0_der(const unsigned char **pder, size_t *pderlen,
+                       X509_NAME *nm);
+
 int X509v3_get_ext_count(const STACK_OF(X509_EXTENSION) *x);
 int X509v3_get_ext_by_NID(const STACK_OF(X509_EXTENSION) *x,
                           int nid, int lastpos);
