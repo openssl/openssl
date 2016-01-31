7a9a3895b2484b729eaa7bf122b039bb61433b75
FdaSilvaYY <fdasilvayy@gmail.com>
2016-01-31 19:49:39 +0100

    fix code indentation issue
    ... related to engine_ref_debug macro.
    
    Signed-off-by: Rich Salz <rsalz@akamai.com>
    Reviewed-by: Kurt Roeckx <kurt@openssl.org>

--- crypto/engine/eng_lib.c
+++ crypto/engine/eng_lib.c
@@ -71,8 +71,8 @@ ENGINE *ENGINE_new(void)
         return NULL;
     }
     ret->struct_ref = 1;
-    engine_ref_debug(ret, 0, 1)
-        CRYPTO_new_ex_data(CRYPTO_EX_INDEX_ENGINE, ret, &ret->ex_data);
+    engine_ref_debug(ret, 0, 1);
+    CRYPTO_new_ex_data(CRYPTO_EX_INDEX_ENGINE, ret, &ret->ex_data);
     return ret;
 }
 
