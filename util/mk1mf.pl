9868eac194140739b9fa211dfc25189ae9575528
Todd Short <tshort@akamai.com>
2016-02-04 11:41:25 -0500

    Add option to disable async
    Add no-async option to Configure that forces ASYNC_NULL.
    Related to RT1979
    An embedded system or replacement C library (e.g. musl or uClibc)
    may not support the *context APIs that are needed for async operation.
    
    Compiles with musl. Ran unit tests, async tests skipped as expected.
    
    Signed-off-by: Rich Salz <rsalz@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>

--- util/mk1mf.pl
+++ util/mk1mf.pl
@@ -142,6 +142,7 @@ and [options] can be one of
 	no-engine				- No engine
 	no-egd					- No EGD
 	no-hw					- No hw
+	no-async                                - No Async (use NULL)
 	nasm 					- Use NASM for x86 asm
 	nw-nasm					- Use NASM x86 asm for NetWare
 	nw-mwasm				- Use Metrowerks x86 asm for NetWare
@@ -296,6 +297,7 @@ $cflags.=" -DOPENSSL_NO_EC"   if $no_ec;
 $cflags.=" -DOPENSSL_NO_GOST" if $no_gost;
 $cflags.=" -DOPENSSL_NO_ENGINE"   if $no_engine;
 $cflags.=" -DOPENSSL_NO_HW"   if $no_hw;
+$cflags.=" -DOPENSSL_NO_ASYNC" if $no_async;
 $cflags.=" -DOPENSSL_FIPS"    if $fips;
 $cflags.=" -DOPENSSL_NO_JPAKE"    if $no_jpake;
 $cflags.=" -DOPENSSL_NO_EC2M"    if $no_ec2m;
@@ -1411,6 +1413,7 @@ sub read_options
 		"no-engine" => \$no_engine,
 		"no-egd" => 0,
 		"no-hw" => \$no_hw,
+		"no-async" => \$no_async,
 		"just-ssl" =>
 			[\$no_rc2, \$no_idea, \$no_des, \$no_bf, \$no_cast,
 			  \$no_md2, \$no_mdc2, \$no_dsa, \$no_dh,
