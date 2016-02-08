a6f04a70e97039a8b9ae89e7e9223eeae3d8d655
Rich Salz <rsalz@openssl.org>
2016-02-07 23:14:12 -0500

    more doc fixes
    dgst: using digest instead of specific digest commands
    the digest list specified in man dgst may be inaccurate, hence using
    digest and referring to the list in digest-commands
    
    'sha' as a digest name is no longer supported
    
    dgst,pkeyutl cmds help cleanup
    - In dgst, pkeyutl cmds, some options help was missing.
    - fixed a minor typo in openssl.pod, that fixes make install.
    - digest-commands was showing ‘sha’, which is not a supported digest
    anymore.
    
    Signed-off-by: Rich Salz <rsalz@openssl.org>
    Reviewed-by: Tim Hudson <tjh@openssl.org>

--- apps/progs.pl
+++ apps/progs.pl
@@ -72,7 +72,7 @@ foreach (@ARGV) {
 foreach (
 	"md2", "md4", "md5",
 	"md_ghost94",
-	"sha", "sha1", "sha224", "sha256", "sha384", "sha512",
+	"sha1", "sha224", "sha256", "sha384", "sha512",
 	"mdc2", "rmd160"
 ) {
         printf "#ifndef OPENSSL_NO_".uc($_)."\n" if ! /sha/;
