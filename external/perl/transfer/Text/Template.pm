c680f77fb181483d6d6ceee8c60eac6e568898c0
Richard Levitte <levitte@openssl.org>
2016-02-12 04:23:15 +0100

    Adjust transfer::Text::Template.pm for alternate directory name
    On VMS, periods in directory names weren't allowed.  To counter that,
    unpackers such as VMSTAR convert periods in directory names to
    underscores.  We need to count that in and add an alternative library
    path for Text::Template.
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- external/perl/transfer/Text/Template.pm
+++ external/perl/transfer/Text/Template.pm
@@ -5,9 +5,11 @@
 BEGIN {
     use File::Spec::Functions;
     use File::Basename;
-    use lib catdir(dirname(__FILE__), "..", "..",
-                   "Text-Template-1.46", "lib");
+    use lib catdir(dirname(__FILE__), "..", "..", "Text-Template-1.46", "lib");
+    # Some unpackers on VMS convert periods in directory names to underscores
+    use lib catdir(dirname(__FILE__), "..", "..", "Text-Template-1_46", "lib");
     use Text::Template;
+    shift @INC;                 # Takes away the effect of use lib
     shift @INC;                 # Takes away the effect of use lib
 }
 1;
