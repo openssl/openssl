1c6bc7a34a7482e65e9fa6aeb5c99dc8112f6622
Richard Levitte <levitte@openssl.org>
2016-02-15 16:14:46 +0100

    Correct deprecation of OPENSSL_config
    Reported in GH#684
    
    Reviewed-by: Matt Caswell <matt@openssl.org>

--- include/openssl/conf.h
+++ include/openssl/conf.h
@@ -137,9 +137,9 @@ int CONF_dump_fp(LHASH_OF(CONF_VALUE) *conf, FILE *out);
 #endif
 int CONF_dump_bio(LHASH_OF(CONF_VALUE) *conf, BIO *out);
 
-#if OPENSSL_API_COMPAT < 0x10100000L
-void OPENSSL_config(const char *config_name);
+DEPRECATEDIN_1_1_0(void OPENSSL_config(const char *config_name))
 
+#if OPENSSL_API_COMPAT < 0x10100000L
 # define OPENSSL_no_config() \
     OPENSSL_init_crypto(OPENSSL_INIT_NO_LOAD_CONFIG, NULL)
 #endif
