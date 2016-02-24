f36f317f02347ea759f6785ce3e5c0f04a25df57
J Mohan Rao Arisankala <mohan@barracuda.com>
2016-02-24 06:00:48 +0530

    explicit check for NULL

--- crypto/asn1/a_sign.c
+++ crypto/asn1/a_sign.c
@@ -243,12 +243,12 @@ int ASN1_item_sign_ctx(const ASN1_ITEM *it,
     type = EVP_MD_CTX_md(ctx);
     pkey = EVP_PKEY_CTX_get0_pkey(EVP_MD_CTX_pkey_ctx(ctx));
 
-    if (!type || !pkey) {
+    if (NULL == type || NULL == pkey) {
         ASN1err(ASN1_F_ASN1_ITEM_SIGN_CTX, ASN1_R_CONTEXT_NOT_INITIALISED);
         goto err;
     }
 
-    if (!pkey->ameth) {
+    if (NULL == pkey->ameth) {
         ASN1err(ASN1_F_ASN1_ITEM_SIGN_CTX, ASN1_R_DIGEST_AND_KEY_TYPE_NOT_SUPPORTED);
         goto err;
     }
