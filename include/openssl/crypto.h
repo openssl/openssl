3a1e770f45557e932a2d23a87cb2af901c476b28
Alessandro Ghedini <alessandro@ghedini.me>
2016-02-26 11:51:31 +0000

    Convert CRYPTO_LOCK_BIO to new multi-threading API

--- include/openssl/crypto.h
+++ include/openssl/crypto.h
@@ -181,7 +181,6 @@ extern "C" {
 # define CRYPTO_LOCK_RAND                18
 # define CRYPTO_LOCK_RAND2               19
 # define CRYPTO_LOCK_MALLOC              20
-# define CRYPTO_LOCK_BIO                 21
 # define CRYPTO_LOCK_READDIR             24
 # define CRYPTO_LOCK_RSA_BLINDING        25
 # define CRYPTO_LOCK_MALLOC2             27
