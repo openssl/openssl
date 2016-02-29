4b30636c6bff25389d48e3936e89018f41ee3a72
Rob Percival <robpercival@google.com>
2016-02-29 17:33:02 +0000

    CT policy validation
    Specifies a callback that will, in the future, be used by the SSL code to
    decide whether to abort a connection on Certificate Transparency grounds.

--- include/openssl/ossl_typ.h
+++ include/openssl/ossl_typ.h
@@ -204,6 +204,7 @@ typedef struct sct_st SCT;
 typedef struct sct_ctx_st SCT_CTX;
 typedef struct ctlog_st CTLOG;
 typedef struct ctlog_store_st CTLOG_STORE;
+typedef struct ct_policy_eval_ctx_st CT_POLICY_EVAL_CTX;
 
 #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L && \
     defined(INTMAX_MAX) && defined(UINTMAX_MAX)
--- test/recipes/80-test_ct.t
+++ test/recipes/80-test_ct.t
@@ -1,6 +1,9 @@
 #! /usr/bin/perl
 
+use OpenSSL::Test qw/:DEFAULT srctop_file/;
 use OpenSSL::Test::Simple;
 
-simple_test("test_ct", "ct_test");
+setup("test_ct");
+$ENV{CTLOG_FILE} = srctop_file("test", "ct", "log_list.conf");
+simple_test("test_ct", "ct_test", "ct");
 
