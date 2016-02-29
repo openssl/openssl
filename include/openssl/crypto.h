15457b620397d7fd671df6eea8055a18baf68699
Matt Caswell <matt@openssl.org>
2016-02-29 16:14:00 +0000

    Fix OPENSSL_INIT flags to avoid a clash.
    The 0x00010000L OPENSSL_INIT flag appeared twice.
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- include/openssl/crypto.h
+++ include/openssl/crypto.h
@@ -546,8 +546,8 @@ int CRYPTO_memcmp(const volatile void * volatile in_a,
 # define OPENSSL_INIT_ENGINE_CAPI            0x00002000L
 # define OPENSSL_INIT_ENGINE_PADLOCK         0x00004000L
 # define OPENSSL_INIT_ENGINE_DASYNC          0x00008000L
-# define OPENSSL_INIT_ENGINE_AFALG           0x00010000L
 /* OPENSSL_INIT flag 0x00010000 reserved for internal use */
+# define OPENSSL_INIT_ENGINE_AFALG           0x00020000L
 /* OPENSSL_INIT flag range 0xfff00000 reserved for OPENSSL_init_ssl() */
 /* Max OPENSSL_INIT flag value is 0x80000000 */
 
