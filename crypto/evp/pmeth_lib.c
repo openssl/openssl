232b108989b8526462c174fdeb254f8ca6123a8d
Alessandro Ghedini <alessandro@ghedini.me>
2016-03-02 23:58:27 +0000

    Implement HMAC-based extract-and-expand key derivation function
    This patch implements the HMAC-based Extract-and-Expand Key Derivation
    Function (HKDF) as defined in RFC 5869.
    
    It is required to implement the QUIC and TLS 1.3 protocols (among others).

--- crypto/evp/pmeth_lib.c
+++ crypto/evp/pmeth_lib.c
@@ -90,7 +90,8 @@ static const EVP_PKEY_METHOD *standard_methods[] = {
 #ifndef OPENSSL_NO_DH
     &dhx_pkey_meth,
 #endif
-    &tls1_prf_pkey_meth
+    &tls1_prf_pkey_meth,
+    &hkdf_pkey_meth
 };
 
 DECLARE_OBJ_BSEARCH_CMP_FN(const EVP_PKEY_METHOD *, const EVP_PKEY_METHOD *,
