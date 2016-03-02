b51ace667ceb2b3162acc4c2b68ce5f6ca98f1b0
Dr. Stephen Henson <steve@openssl.org>
2016-03-02 21:32:30 +0000

    add kdf.h to mkdef.pl
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- util/mkdef.pl
+++ util/mkdef.pl
@@ -295,6 +295,7 @@ $crypto.=" include/openssl/srp.h";
 $crypto.=" include/openssl/modes.h";
 $crypto.=" include/openssl/async.h";
 $crypto.=" include/openssl/ct.h";
+$crypto.=" include/openssl/kdf.h";
 
 my $symhacks="include/openssl/symhacks.h";
 
