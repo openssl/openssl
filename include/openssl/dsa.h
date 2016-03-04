c79a736f6bd65be83b9d648e60feaedb6f89e5d4
Dr. Stephen Henson <steve@openssl.org>
2016-03-04 02:39:50 +0000

    Make DSA_SIG opaque.
    This adds a new accessor function DSA_SIG_get0.
    The customisation of DSA_SIG structure initialisation has been removed this
    means that the 'r' and 's' components are automatically allocated when
    DSA_SIG_new() is called. Update documentation.
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- include/openssl/dsa.h
+++ include/openssl/dsa.h
@@ -121,10 +121,7 @@ extern "C" {
 /* typedef struct dsa_st DSA; */
 /* typedef struct dsa_method DSA_METHOD; */
 
-typedef struct DSA_SIG_st {
-    BIGNUM *r;
-    BIGNUM *s;
-} DSA_SIG;
+typedef struct DSA_SIG_st DSA_SIG;
 
 struct dsa_method {
     const char *name;
@@ -187,6 +184,7 @@ DSA_SIG *DSA_SIG_new(void);
 void DSA_SIG_free(DSA_SIG *a);
 int i2d_DSA_SIG(const DSA_SIG *a, unsigned char **pp);
 DSA_SIG *d2i_DSA_SIG(DSA_SIG **v, const unsigned char **pp, long length);
+void DSA_SIG_get0(BIGNUM **pr, BIGNUM **ps, DSA_SIG *sig);
 
 DSA_SIG *DSA_do_sign(const unsigned char *dgst, int dlen, DSA *dsa);
 int DSA_do_verify(const unsigned char *dgst, int dgst_len,
