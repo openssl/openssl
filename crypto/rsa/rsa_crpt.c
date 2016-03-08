0b1a07c8a70486534526d8967f03e32806da5661
Alessandro Ghedini <alessandro@ghedini.me>
2016-03-08 22:37:01 +0000

    Convert RSA blinding to new multi-threading API
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- crypto/rsa/rsa_crpt.c
+++ crypto/rsa/rsa_crpt.c
@@ -217,7 +217,9 @@ BN_BLINDING *RSA_setup_blinding(RSA *rsa, BN_CTX *in_ctx)
         RSAerr(RSA_F_RSA_SETUP_BLINDING, ERR_R_BN_LIB);
         goto err;
     }
-    CRYPTO_THREADID_current(BN_BLINDING_thread_id(ret));
+
+    BN_BLINDING_set_current_thread(ret);
+
  err:
     BN_CTX_end(ctx);
     if (ctx != in_ctx)
