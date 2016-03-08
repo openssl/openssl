8310d9e29b32a427c7d28a48b6d58a4b58b4b870
Rob Percival <robpercival@google.com>
2016-03-08 18:55:55 +0000

    Makes CTLOG_STORE_get0_log_by_id return const CTLOG*
    Reviewed-by: Emilia Käsper <emilia@openssl.org>
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- crypto/ct/ct_log.c
+++ crypto/ct/ct_log.c
@@ -334,14 +334,14 @@ EVP_PKEY *CTLOG_get0_public_key(const CTLOG *log)
  * Given a log ID, finds the matching log.
  * Returns NULL if no match found.
  */
-CTLOG *CTLOG_STORE_get0_log_by_id(const CTLOG_STORE *store,
-                                  const uint8_t *log_id,
-                                  size_t log_id_len)
+const CTLOG *CTLOG_STORE_get0_log_by_id(const CTLOG_STORE *store,
+                                        const uint8_t *log_id,
+                                        size_t log_id_len)
 {
     int i;
 
     for (i = 0; i < sk_CTLOG_num(store->logs); ++i) {
-        CTLOG *log = sk_CTLOG_value(store->logs, i);
+        const CTLOG *log = sk_CTLOG_value(store->logs, i);
         if (memcmp(log->log_id, log_id, log_id_len) == 0)
             return log;
     }
