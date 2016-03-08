60d1f8e506cc1732a6f17053858e61e16d79fce2
Benjamin Kaduk <bkaduk@akamai.com>
2016-03-08 16:44:57 -0600

    Avoid negative array index in BIO_debug_callback()
    BIO_snprintf() can return -1 on truncation (and overflow as of commit
    9cb177301fdab492e4cfef376b28339afe3ef663).  Though neither can
    realistically occur while printing a pointer and short fixed string into
    a buffer of length 256, the analysis to confirm that this the case goes
    somewhat far up the call chain, and not all static analyzers can
    successfully follow the chain of logic.
    
    It's easy enough to clamp the returned length to be nonnegative before
    continuing, which appeases the static analyzer and does not harm the
    subsequent code.
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- crypto/bio/bio_cb.c
+++ crypto/bio/bio_cb.c
@@ -77,6 +77,9 @@ long BIO_debug_callback(BIO *bio, int cmd, const char *argp,
 
     len = BIO_snprintf(buf,sizeof buf,"BIO[%p]: ",(void *)bio);
 
+    /* Ignore errors and continue printing the other information. */
+    if (len < 0)
+        len = 0;
     p = buf + len;
     p_maxlen = sizeof(buf) - len;
 
