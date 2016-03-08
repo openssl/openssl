87326458aa67c7452cd55e3deab8a258170a3665
Rich Salz <rsalz@openssl.org>
2016-03-08 14:02:54 -0500

    Remove some old ms/* files
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- ms/do_nt.bat
+++
@@ -1,7 +0,0 @@
-
-perl util\mkfiles.pl >MINFO
-perl util\mk1mf.pl no-asm VC-NT >ms\nt.mak
-perl util\mk1mf.pl dll no-asm VC-NT >ms\ntdll.mak
-
-perl util\mkdef.pl libcrypto NT > ms\libcrypto32.def
-perl util\mkdef.pl libssl NT > ms\libssl32.def
