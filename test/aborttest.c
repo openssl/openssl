9b65a22fe921cfbefb5efd1be23458485fa1d8b2
Richard Levitte <levitte@openssl.org>
2016-03-18 12:16:53 +0100

    Add a test to see that signals are caught as failures
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    Reviewed-by: Emilia Käsper <emilia@openssl.org>

---
+++ test/aborttest.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2016 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL licenses, (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * https://www.openssl.org/source/license.html
+ * or in the file LICENSE in the source distribution.
+ */
+
+#include <openssl/crypto.h>
+
+int main(int argc, char **argv)
+{
+    OPENSSL_die("Voluntary abort", __FILE__, __LINE__);
+    return 0;
+}
