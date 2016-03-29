b6ca88ab9d7b3e15b6ba541bc581bf018c453537
Todd Short <tshort@akamai.com>
2016-03-29 13:40:48 -0400

    Fixup SSL EX_DATA index
    The SSL, SSL_CTX, and SSL_SESSION indices were being referenced
    incorrectly in the "_get_ex_new_index" functions.
    
    Remove the STORE EX_DATA index; that functionality is gone.
    
    Reviewed-by: Emilia Käsper <emilia@openssl.org>
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- include/openssl/crypto.h
+++ include/openssl/crypto.h
@@ -218,9 +218,8 @@ DEFINE_STACK_OF(void)
 # define CRYPTO_EX_INDEX_ENGINE          10
 # define CRYPTO_EX_INDEX_UI              11
 # define CRYPTO_EX_INDEX_BIO             12
-# define CRYPTO_EX_INDEX_STORE           13
-# define CRYPTO_EX_INDEX_APP             14
-# define CRYPTO_EX_INDEX__COUNT          15
+# define CRYPTO_EX_INDEX_APP             13
+# define CRYPTO_EX_INDEX__COUNT          14
 
 /*
  * This is the default callbacks, but we can have others as well: this is
