877ca21e7a3c54792706bbb769e9af7d80b9d1b6
Richard Levitte <levitte@openssl.org>
2016-04-09 11:13:00 +0200

    VMS doesn't have directory separators in the same sense as Unix
    Don't add / in file specs on VMS.  The directory "separator" is part
    of the directory spec.
    
    Reviewed-by: Emilia Käsper <emilia@openssl.org>

--- test/ssl-tests/ssltests_base.pm
+++ test/ssl-tests/ssltests_base.pm
@@ -4,14 +4,16 @@
 
 package ssltests;
 
+my $dir_sep = $^O ne "VMS" ? "/" : "";
+
 our %base_server = (
-    "Certificate" => "\${ENV::TEST_CERTS_DIR}/servercert.pem",
-    "PrivateKey"  => "\${ENV::TEST_CERTS_DIR}/serverkey.pem",
+    "Certificate" => "\${ENV::TEST_CERTS_DIR}${dir_sep}servercert.pem",
+    "PrivateKey"  => "\${ENV::TEST_CERTS_DIR}${dir_sep}serverkey.pem",
     "CipherString" => "DEFAULT",
 );
 
 our %base_client = (
-    "VerifyCAFile" => "\${ENV::TEST_CERTS_DIR}/rootcert.pem",
+    "VerifyCAFile" => "\${ENV::TEST_CERTS_DIR}${dir_sep}rootcert.pem",
     "VerifyMode" => "Peer",
     "CipherString" => "DEFAULT",
 );
