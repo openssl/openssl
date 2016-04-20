a60ff4f9f0b1d43a04d1e8f4cd5733f3137cb458
Rich Salz <rsalz@openssl.org>
2016-04-19 22:10:43 -0400

    Copyright consolidation: perl files
    Add copyright to most .pl files
    This does NOT cover any .pl file that has other copyright in it.
    Most of those are Andy's but some are public domain.
    Fix typo's in some existing files.
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- crypto/bn/asm/ppc.pl
+++ crypto/bn/asm/ppc.pl
@@ -1,5 +1,11 @@
-#!/usr/bin/env perl
+#! /usr/bin/env perl
+# Copyright 2004-2016 The OpenSSL Project Authors. All Rights Reserved.
 #
+# Licensed under the OpenSSL license (the "License").  You may not use
+# this file except in compliance with the License.  You can obtain a copy
+# in the file LICENSE in the source distribution or at
+# https://www.openssl.org/source/license.html
+
 # Implemented as a Perl wrapper as we want to support several different
 # architectures with single file. We pick up the target based on the
 # file name we are asked to generate.
--- crypto/perlasm/sparcv9_modes.pl
+++ crypto/perlasm/sparcv9_modes.pl
@@ -1,4 +1,11 @@
-#!/usr/bin/env perl
+#! /usr/bin/env perl
+# Copyright 2012-2016 The OpenSSL Project Authors. All Rights Reserved.
+#
+# Licensed under the OpenSSL license (the "License").  You may not use
+# this file except in compliance with the License.  You can obtain a copy
+# in the file LICENSE in the source distribution or at
+# https://www.openssl.org/source/license.html
+
 
 # Specific modes implementations for SPARC Architecture 2011. There
 # is T4 dependency though, an ASI value that is not specified in the
