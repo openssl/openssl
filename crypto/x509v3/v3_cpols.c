620e7a8f06511d8db47c419703740d520968059e
Matt Caswell <matt@openssl.org>
2016-04-27 14:17:33 +0100

    Free an ASN1_OBJECT in an error path
    The r2i_certpol() function allocates an ASN1_OBJECT but can fail to free
    it in an error path.
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- crypto/x509v3/v3_cpols.c
+++ crypto/x509v3/v3_cpols.c
@@ -188,6 +188,7 @@ static STACK_OF(POLICYINFO) *r2i_certpol(X509V3_EXT_METHOD *method,
             pol = POLICYINFO_new();
             if (pol == NULL) {
                 X509V3err(X509V3_F_R2I_CERTPOL, ERR_R_MALLOC_FAILURE);
+                ASN1_OBJECT_free(pobj);
                 goto err;
             }
             pol->policyid = pobj;
