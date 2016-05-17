21dcbebc6e35419f1842f39a125374ea1ba45693
Rich Salz <rsalz@openssl.org>
2016-05-17 14:24:17 -0400

    Copyright consolidation 03/10
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- include/internal/comp.h
+++ include/internal/comp.h
@@ -1,9 +1,10 @@
 /*
- * Licensed under the OpenSSL licenses, (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Copyright 2016 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
  * https://www.openssl.org/source/license.html
- * or in the file LICENSE in the source distribution.
  */
 
 #include <openssl/comp.h>
