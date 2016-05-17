d2e9e320186f0917cc940f46bdf1a7e4120da9b0
Rich Salz <rsalz@openssl.org>
2016-05-17 14:51:26 -0400

    Copyright consolidation 07/10
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- crypto/dso/dso_locl.h
+++ crypto/dso/dso_locl.h
@@ -1,12 +1,12 @@
 /*
  * Copyright 2016 The OpenSSL Project Authors. All Rights Reserved.
  *
- * Licensed under the OpenSSL licenses (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *      https://www.openssl.org/source/license.html
- * or in the file LICENSE in the source distribution.
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
  */
+
 #include <stdio.h>
 #include "internal/cryptlib.h"
 #include "internal/dso.h"
