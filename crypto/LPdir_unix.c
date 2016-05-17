00dd302e175a41aae66683a206ee79f8ae118b1a
Rich Salz <rsalz@openssl.org>
2016-05-17 14:51:34 -0400

    Copyright consolidation 08/10
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- crypto/LPdir_unix.c
+++ crypto/LPdir_unix.c
@@ -1,4 +1,13 @@
 /*
+ * Copyright 2004-2016 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+/*
  * $LP: LPlib/source/LPdir_unix.c,v 1.11 2004/09/23 22:07:22 _cvs_levitte Exp
  * $
  */
