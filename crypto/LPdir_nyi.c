4f22f40507fea3f272637eb8e00cadf1f34b10d9
Rich Salz <rsalz@openssl.org>
2016-05-17 14:51:04 -0400

    Copyright consolidation 06/10
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- crypto/LPdir_nyi.c
+++ crypto/LPdir_nyi.c
@@ -1,4 +1,13 @@
 /*
+ * Copyright 2004-2016 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+/*
  * $LP: LPlib/source/LPdir_win.c,v 1.1 2004/06/14 10:07:56 _cvs_levitte Exp $
  */
 /*
