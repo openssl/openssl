b1322259d93cf6b6286f9febcd468b6a9f577d91
Rich Salz <rsalz@openssl.org>
2016-05-17 14:52:22 -0400

    Copyright consolidation 09/10
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- crypto/engine/eng_cryptodev.c
+++ crypto/engine/eng_cryptodev.c
@@ -1,4 +1,13 @@
 /*
+ * Copyright 2002-2016 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+/*
  * Copyright (c) 2002 Bob Beck <beck@openbsd.org>
  * Copyright (c) 2002 Theo de Raadt
  * Copyright (c) 2002 Markus Friedl
