c5d9162559c2e5bc30678fa77aad0ff3782bd48f
Rich Salz <rsalz@openssl.org>
2016-05-18 09:16:36 -0400

    Consolidate copyright for demos
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- demos/bio/sconnect.c
+++ demos/bio/sconnect.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright 1998-2016 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
 /*-
  * A minimal program to do SSL to a passed host and port.
  * It is actually using non-blocking IO but in a very simple manner
--- demos/smime/smenc.c
+++ demos/smime/smenc.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2007-2016 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
 /* Simple S/MIME encrypt example */
 #include <openssl/pem.h>
 #include <openssl/pkcs7.h>
