bc776510982b3768761d32c1160e79cb45a561c3
Richard Levitte <levitte@openssl.org>
2016-05-18 17:14:19 +0200

    Make it possible to have RFC2254 escapes with ASN1_STRING_print_ex()
    Also adds 'esc_2254' to the possible command line name options
    
    RT#1466
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- crypto/asn1/charmap.pl
+++ crypto/asn1/charmap.pl
@@ -21,6 +21,7 @@ my $NOESC_QUOTE	= 8;	# Not escaped if quoted
 my $PSTRING_CHAR = 0x10;	# Valid PrintableString character
 my $RFC2253_FIRST_ESC = 0x20; # Escaped with \ if first character
 my $RFC2253_LAST_ESC = 0x40;  # Escaped with \ if last character
+my $RFC2254_ESC = 0x400;	# Character escaped \XX
 
 for($i = 0; $i < 128; $i++) {
 	# Set the RFC2253 escape characters (control)
@@ -51,6 +52,14 @@ $arr[ord("\\")] |= $RFC2253_ESC;
 $arr[ord("<")] |= $NOESC_QUOTE | $RFC2253_ESC;
 $arr[ord(">")] |= $NOESC_QUOTE | $RFC2253_ESC;
 $arr[ord(";")] |= $NOESC_QUOTE | $RFC2253_ESC;
+
+# Remaining RFC2254 characters
+
+$arr[0] |= $RFC2254_ESC;
+$arr[ord("(")] |= $RFC2254_ESC;
+$arr[ord(")")] |= $RFC2254_ESC;
+$arr[ord("*")] |= $RFC2254_ESC;
+$arr[ord("\\")] |= $RFC2254_ESC;
 
 # Remaining PrintableString characters
 
@@ -86,7 +95,7 @@ print <<EOF;
  * Mask of various character properties
  */
 
-static const unsigned char char_type[] = {
+static const unsigned short char_type[] = {
 EOF
 
 print "   ";
