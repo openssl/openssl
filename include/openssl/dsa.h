1d454d58a80971eac941911965e894fdf1621cd8
TJ Saunders <tj@castaglia.org>
2016-06-09 14:53:23 -0700

    Forgot to update the setter argument ordering in the public headers.
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    Reviewed-by: Emilia Käsper <emilia@openssl.org>
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/1193)

--- include/openssl/dsa.h
+++ include/openssl/dsa.h
@@ -82,7 +82,7 @@ void DSA_SIG_free(DSA_SIG *a);
 int i2d_DSA_SIG(const DSA_SIG *a, unsigned char **pp);
 DSA_SIG *d2i_DSA_SIG(DSA_SIG **v, const unsigned char **pp, long length);
 void DSA_SIG_get0(BIGNUM **pr, BIGNUM **ps, const DSA_SIG *sig);
-int DSA_SIG_set0(BIGNUM *r, BIGNUM *s, DSA_SIG *sig);
+int DSA_SIG_set0(DSA_SIG *sig, BIGNUM *r, BIGNUM *s);
 
 DSA_SIG *DSA_do_sign(const unsigned char *dgst, int dlen, DSA *dsa);
 int DSA_do_verify(const unsigned char *dgst, int dgst_len,
