b02929802c1fc47daf268ec208ee113dcce3ed32
Emilia Kasper <emilia@openssl.org>
2016-06-13 17:46:12 +0200

    SSL test: only write out server2 when testing SNI
    The SNI tests introduced a redundant "server2" section into every test
    configuration. Copy this automatically from "server" unless testing SNI,
    to reduce noise in the generated confs.
    
    Also remove duplicate SSL_TEST_CTX_create (merge conflict error).
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- test/generate_ssl_tests.pl
+++ test/generate_ssl_tests.pl
@@ -43,12 +43,12 @@ sub print_templates {
     # Add the implicit base configuration.
     foreach my $test (@ssltests::tests) {
         $test->{"server"} = { (%ssltests::base_server, %{$test->{"server"}}) };
-	# use server values if server2 is not defined
+	# Do not emit an empty "server2" section.
 	if (defined $test->{"server2"}) {
-	    $test->{"server2"} = { (%ssltests::base_server, %{$test->{"server2"}}) };
-	} else {
-	    $test->{"server2"} = { (%ssltests::base_server, %{$test->{"server"}}) };
-	}
+            $test->{"server2"} = { (%ssltests::base_server, %{$test->{"server2"}}) };
+        } else {
+            $test->{"server2"} = { };
+        }
         $test->{"client"} = { (%ssltests::base_client, %{$test->{"client"}}) };
     }
 
@@ -98,8 +98,7 @@ sub print_templates {
 # Shamelessly copied from Configure.
 sub read_config {
     my $fname = shift;
-    open(INPUT, "< $fname")
-	or die "Can't open input file '$fname'!\n";
+    open(INPUT, "< $fname") or die "Can't open input file '$fname'!\n";
     local $/ = undef;
     my $content = <INPUT>;
     close(INPUT);
