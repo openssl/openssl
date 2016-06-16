fa4f9e01e4b77e4aefac106aafce796e8a97d71f
Richard Levitte <levitte@openssl.org>
2016-06-16 07:33:23 +0200

    ret, not err (master branch uses err)

--- apps/speed.c
+++ apps/speed.c
@@ -332,7 +332,7 @@ static double Time_F(int s)
         if (thr == NULL) {
             DWORD ret = GetLastError();
             BIO_printf(bio_err, "unable to CreateThread (%d)", ret);
-            TerminateProcess(GetCurrentProcess(), err);
+            TerminateProcess(GetCurrentProcess(), ret);
         }
         CloseHandle(thr);       /* detach the thread */
         while (!schlock)
