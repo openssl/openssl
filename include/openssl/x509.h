d0ba7bc8a0d577c73b942a99faa266f392e7f211
Richard Levitte <levitte@openssl.org>
2016-06-19 10:55:59 +0200

    make update
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    Reviewed-by: Stephen Henson <steve@openssl.org>

--- include/openssl/x509.h
+++ include/openssl/x509.h
@@ -1009,6 +1009,7 @@ void ERR_load_X509_strings(void);
 # define X509_F_ADD_CERT_DIR                              100
 # define X509_F_BUILD_CHAIN                               106
 # define X509_F_BY_FILE_CTRL                              101
+# define X509_F_CHECK_NAME_CONSTRAINTS                    149
 # define X509_F_CHECK_POLICY                              145
 # define X509_F_DANE_I2D                                  107
 # define X509_F_DIR_CTRL                                  102
