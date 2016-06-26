03cb37acec0c23a01bee4357cd59ec9f97e528ba
Alex Gaynor <alex.gaynor@gmail.com>
2016-06-26 10:37:03 -0400

    Add comment about X509_print
    Signed-off-by: Kurt Roeckx <kurt@roeckx.be>
    Reviewed-by: Emilia Kasper <emilia@openssl.org>
    
    GH: #1255

--- fuzz/x509.c
+++ fuzz/x509.c
@@ -19,6 +19,7 @@ int FuzzerTestOneInput(const uint8_t *buf, size_t len) {
     X509 *x509 = d2i_X509(NULL, &p, len);
     if (x509 != NULL) {
         BIO *bio = BIO_new(BIO_s_null());
+        /* This will load and print the public key as well as extensions */
         X509_print(bio, x509);
         BIO_free(bio);
 
