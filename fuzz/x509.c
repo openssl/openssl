a05b0bcf878c8c52824e8ee4bb6c1853fa23800b
Kurt Roeckx <kurt@roeckx.be>
2016-07-01 16:42:15 +0200

    Re-add x509 and crl fuzzer
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    
    GH: #1276

--- fuzz/x509.c
+++ fuzz/x509.c
@@ -12,6 +12,10 @@
 #include <openssl/bio.h>
 #include "fuzzer.h"
 
+int FuzzerInitialize(int *argc, char ***argv) {
+    return 1;
+}
+
 int FuzzerTestOneInput(const uint8_t *buf, size_t len) {
     const unsigned char *p = buf;
     unsigned char *der = NULL;
