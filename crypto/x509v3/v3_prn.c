a7ac2cc9ca44818d0237d9219dcca61ae90d09bc
FdaSilvaYY <fdasilvayy@gmail.com>
2016-07-08 00:43:32 +0200

    Enforce and explicit some const casting
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/1300)

--- crypto/x509v3/v3_prn.c
+++ crypto/x509v3/v3_prn.c
@@ -188,7 +188,7 @@ static int unknown_ext_print(BIO *out, const unsigned char *ext, int extlen,
     case X509V3_EXT_PARSE_UNKNOWN:
         return ASN1_parse_dump(out, ext, extlen, indent, -1);
     case X509V3_EXT_DUMP_UNKNOWN:
-        return BIO_dump_indent(out, (char *)ext, extlen, indent);
+        return BIO_dump_indent(out, (const char *)ext, extlen, indent);
 
     default:
         return 1;
