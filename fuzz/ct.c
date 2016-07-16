56a123cfc5924a903c48e2bad3dcffa43e24caa2
Kurt Roeckx <kurt@roeckx.be>
2016-07-16 13:41:33 +0200

    fuzzers: print and convert it back

--- fuzz/ct.c
+++ fuzz/ct.c
@@ -22,7 +22,17 @@ int FuzzerInitialize(int *argc, char ***argv) {
 
 int FuzzerTestOneInput(const uint8_t *buf, size_t len) {
     const uint8_t **pp = &buf;
+    unsigned char *der = NULL;
     STACK_OF(SCT) *scts = d2i_SCT_LIST(NULL, pp, len);
-    SCT_LIST_free(scts);
+    if (scts != NULL) {
+        BIO *bio = BIO_new(BIO_s_null());
+        SCT_LIST_print(scts, bio, 4, "\n", NULL);
+        BIO_free(bio);
+
+        i2d_SCT_LIST(scts, &der);
+        OPENSSL_free(der);
+
+        SCT_LIST_free(scts);
+    }
     return 0;
 }
