ef3ff1f87f90c7d2e59310189c3b1c2558e8c65b
Richard Levitte <levitte@openssl.org>
2016-07-19 21:01:11 +0200

    make update

--- include/openssl/engine.h
+++ include/openssl/engine.h
@@ -773,6 +773,8 @@ void ERR_load_ENGINE_strings(void);
 # define ENGINE_F_ENGINE_FINISH                           107
 # define ENGINE_F_ENGINE_GET_CIPHER                       185
 # define ENGINE_F_ENGINE_GET_DIGEST                       186
+# define ENGINE_F_ENGINE_GET_FIRST                        195
+# define ENGINE_F_ENGINE_GET_LAST                         196
 # define ENGINE_F_ENGINE_GET_NEXT                         115
 # define ENGINE_F_ENGINE_GET_PKEY_ASN1_METH               193
 # define ENGINE_F_ENGINE_GET_PKEY_METH                    192
@@ -784,6 +786,7 @@ void ERR_load_ENGINE_strings(void);
 # define ENGINE_F_ENGINE_LOAD_PUBLIC_KEY                  151
 # define ENGINE_F_ENGINE_LOAD_SSL_CLIENT_CERT             194
 # define ENGINE_F_ENGINE_NEW                              122
+# define ENGINE_F_ENGINE_PKEY_ASN1_FIND_STR               197
 # define ENGINE_F_ENGINE_REMOVE                           123
 # define ENGINE_F_ENGINE_SET_DEFAULT_STRING               189
 # define ENGINE_F_ENGINE_SET_ID                           129
