436dead2e2a157fa501a7538a77b6078391b477f
Dr. Stephen Henson <steve@openssl.org>
2016-08-02 23:53:41 +0100

    Calculate sequence length properly.
    Use correct length in old ASN.1 indefinite length sequence decoder
    (only used by SSL_SESSION).
    
    This bug was discovered by Hanno Böck using libfuzzer.
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- crypto/asn1/asn1_lib.c
+++ crypto/asn1/asn1_lib.c
@@ -328,7 +328,7 @@ int asn1_GetSequence(ASN1_const_CTX *c, long *length)
         return (0);
     }
     if (c->inf == (1 | V_ASN1_CONSTRUCTED))
-        c->slen = *length + *(c->pp) - c->p;
+        c->slen = *length;
     c->eos = 0;
     return (1);
 }
