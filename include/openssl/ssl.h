48593cb12afc9a8b9de8b3e25ee1097c4f6fe90d
Matt Caswell <matt@openssl.org>
2016-08-13 14:29:41 +0100

    Convert SSL_SESSION* functions to use const getters
    Reviewed-by: Viktor Dukhovni <viktor@openssl.org>
    Reviewed-by: Stephen Henson <steve@openssl.org>

--- include/openssl/ssl.h
+++ include/openssl/ssl.h
@@ -1400,7 +1400,7 @@ __owur const char *SSL_SESSION_get0_hostname(const SSL_SESSION *s);
 __owur const SSL_CIPHER *SSL_SESSION_get0_cipher(const SSL_SESSION *s);
 __owur int SSL_SESSION_has_ticket(const SSL_SESSION *s);
 __owur unsigned long SSL_SESSION_get_ticket_lifetime_hint(const SSL_SESSION *s);
-void SSL_SESSION_get0_ticket(const SSL_SESSION *s, unsigned char **tick,
+void SSL_SESSION_get0_ticket(const SSL_SESSION *s, const unsigned char **tick,
                             size_t *len);
 __owur int SSL_copy_session_id(SSL *to, const SSL *from);
 __owur X509 *SSL_SESSION_get0_peer(SSL_SESSION *s);
