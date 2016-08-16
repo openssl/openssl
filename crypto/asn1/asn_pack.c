203a5319c9d5e78979f5437d02ff693ee088a4c0
Dr. Stephen Henson <steve@openssl.org>
2016-08-16 20:14:02 +0100

    Constify ASN1_item_unpack().
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- crypto/asn1/asn_pack.c
+++ crypto/asn1/asn_pack.c
@@ -50,7 +50,7 @@ ASN1_STRING *ASN1_item_pack(void *obj, const ASN1_ITEM *it, ASN1_STRING **oct)
 
 /* Extract an ASN1 object from an ASN1_STRING */
 
-void *ASN1_item_unpack(ASN1_STRING *oct, const ASN1_ITEM *it)
+void *ASN1_item_unpack(const ASN1_STRING *oct, const ASN1_ITEM *it)
 {
     const unsigned char *p;
     void *ret;
