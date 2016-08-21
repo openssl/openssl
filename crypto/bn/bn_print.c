67e11f1d44b85758f01b4905d64c4c49476c1db5
Kazuki Yamaguchi <k@rhe.jp>
2016-08-22 02:36:36 +0900

    Fix overflow check in BN_bn2dec()
    Fix an off by one error in the overflow check added by 07bed46f332fc
    ("Check for errors in BN_bn2dec()").
    
    Reviewed-by: Stephen Henson <steve@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (cherry picked from commit 099e2968ed3c7d256cda048995626664082b1b30)

--- crypto/bn/bn_print.c
+++ crypto/bn/bn_print.c
@@ -141,14 +141,13 @@ char *BN_bn2dec(const BIGNUM *a)
         if (BN_is_negative(t))
             *p++ = '-';
 
-        i = 0;
         while (!BN_is_zero(t)) {
+            if (lp - bn_data >= bn_data_num)
+                goto err;
             *lp = BN_div_word(t, BN_DEC_CONV);
             if (*lp == (BN_ULONG)-1)
                 goto err;
             lp++;
-            if (lp - bn_data >= bn_data_num)
-                goto err;
         }
         lp--;
         /*
