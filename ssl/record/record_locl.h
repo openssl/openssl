8e6d03cac4c34dc089751f36120b69c512f77756
Matt Caswell <matt@openssl.org>
2016-09-06 09:24:19 +0100

    Convert record layer to use size_t
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- ssl/record/record_locl.h
+++ ssl/record/record_locl.h
@@ -38,7 +38,8 @@
 #define RECORD_LAYER_clear_first_record(rl)     ((rl)->is_first_record = 0)
 #define DTLS_RECORD_LAYER_get_r_epoch(rl)       ((rl)->d->r_epoch)
 
-__owur int ssl3_read_n(SSL *s, int n, int max, int extend, int clearold);
+__owur int ssl3_read_n(SSL *s, size_t n, size_t max, int extend, int clearold,
+                       size_t *read);
 
 void RECORD_LAYER_set_write_sequence(RECORD_LAYER *rl, const unsigned char *ws);
 DTLS1_BITMAP *dtls1_get_bitmap(SSL *s, SSL3_RECORD *rr,
