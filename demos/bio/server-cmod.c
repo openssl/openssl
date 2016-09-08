33dbfeef130b7071f893031be65ad1f330042aec
Richard Levitte <levitte@openssl.org>
2016-09-09 00:58:21 +0200

    Fixup BIO demos for OpenSSL 1.1.x
    Note: server-cmod doesn't seem to do things right...  from loading
    cmod.cnf, it tries to load libssl_conf.so.
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- demos/bio/server-cmod.c
+++ demos/bio/server-cmod.c
@@ -27,11 +27,6 @@ int main(int argc, char *argv[])
     SSL_CTX *ctx;
     int ret = 1, i;
 
-    SSL_load_error_strings();
-
-    /* Add ciphers and message digests */
-    OpenSSL_add_ssl_algorithms();
-
     if (CONF_modules_load_file("cmod.cnf", "testapp", 0) <= 0) {
         fprintf(stderr, "Error processing config file\n");
         goto err;
