b90348c7c57f2a6ba129fd2cb9a5889401fe3f91
Richard Levitte <levitte@openssl.org>
2016-09-09 09:32:00 +0200

    Fixup BIO demos for OpenSSL 1.1.x
    Call SSL_CTX_new() before doing any configuration.
    (or call OPENSSL_ssl_init())
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    (cherry picked from commit 54d72ef0f019db383f8e98342b9b77c5da8541e5)

--- demos/bio/server-cmod.c
+++ demos/bio/server-cmod.c
@@ -27,12 +27,12 @@ int main(int argc, char *argv[])
     SSL_CTX *ctx;
     int ret = 1, i;
 
+    ctx = SSL_CTX_new(TLS_server_method());
+
     if (CONF_modules_load_file("cmod.cnf", "testapp", 0) <= 0) {
         fprintf(stderr, "Error processing config file\n");
         goto err;
     }
-
-    ctx = SSL_CTX_new(TLS_server_method());
 
     if (SSL_CTX_config(ctx, "server") == 0) {
         fprintf(stderr, "Error configuring server.\n");
