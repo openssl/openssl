ceb7ea57f2b0e04723eabf28599d2a5aad1ac826
Richard Levitte <levitte@openssl.org>
2016-09-14 20:52:03 +0200

    Add copyright and license on apps/vms_term_sock.[ch]

--- apps/vms_term_sock.c
+++ apps/vms_term_sock.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2016 VMS Software, Inc. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
 #ifdef __VMS 
 #define OPENSSL_SYS_VMS
 #pragma message disable DOLLARID
