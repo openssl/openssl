07ebdfab079d1efc2d65e15f9f541fa1861ed4ff
Richard Levitte <levitte@openssl.org>
2016-09-14 20:52:03 +0200

    Add copyright and license on apps/vms_term_sock.[ch]
    Reviewed-by: Andy Polyakov <appro@openssl.org>
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    (cherry picked from commit 0d0723e813870c2e7a009ec47e933668a1548531)

--- apps/vms_term_sock.h
+++ apps/vms_term_sock.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2016 VMS Software, Inc. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
 #ifndef TERM_SOCK_H
 #define TERM_SOCK_H
 
