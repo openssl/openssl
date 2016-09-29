ac8cc3efb26fa91c4f29463044cfe9e7070ebc14
Matt Caswell <matt@openssl.org>
2016-09-29 14:26:36 +0100

    Remove tls12_copy_sigalgs_old()
    This was a temporary function needed during the conversion to WPACKET. All
    callers have now been converted to the new way of doing this so this
    function is no longer required.
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- ssl/t1_lib.c
+++ ssl/t1_lib.c
@@ -3320,26 +3320,6 @@ void ssl_set_sig_mask(uint32_t *pmask_a, SSL *s, int op)
         *pmask_a |= SSL_aECDSA;
 }
 
-/*
- * Old version of the tls12_copy_sigalgs function used by code that has not
- * yet been converted to WPACKET yet. It will be deleted once WPACKET conversion
- * is complete.
- * TODO - DELETE ME
- */
-size_t tls12_copy_sigalgs_old(SSL *s, unsigned char *out,
-                          const unsigned char *psig, size_t psiglen)
-{
-    unsigned char *tmpout = out;
-    size_t i;
-    for (i = 0; i < psiglen; i += 2, psig += 2) {
-        if (tls12_sigalg_allowed(s, SSL_SECOP_SIGALG_SUPPORTED, psig)) {
-            *tmpout++ = psig[0];
-            *tmpout++ = psig[1];
-        }
-    }
-    return tmpout - out;
-}
-
 int tls12_copy_sigalgs(SSL *s, WPACKET *pkt,
                        const unsigned char *psig, size_t psiglen)
 {
