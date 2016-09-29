da8f7ac4829ede1ed3c22de5a9ed52c83213e337
Matt Caswell <matt@openssl.org>
2016-09-29 18:08:34 +0100

    Remove the tls12_get_sigandhash_old() function
    This is no longer needed now that all messages use WPACKET
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- ssl/t1_lib.c
+++ ssl/t1_lib.c
@@ -3132,29 +3132,6 @@ int tls12_get_sigandhash(WPACKET *pkt, const EVP_PKEY *pk, const EVP_MD *md)
     return 1;
 }
 
-/*
- * Old version of the tls12_get_sigandhash function used by code that has not
- * yet been converted to WPACKET yet. It will be deleted once WPACKET conversion
- * is complete.
- * TODO - DELETE ME
- */
-int tls12_get_sigandhash_old(unsigned char *p, const EVP_PKEY *pk,
-                             const EVP_MD *md)
-{
-    int sig_id, md_id;
-    if (!md)
-        return 0;
-    md_id = tls12_find_id(EVP_MD_type(md), tls12_md, OSSL_NELEM(tls12_md));
-    if (md_id == -1)
-        return 0;
-    sig_id = tls12_get_sigid(pk);
-    if (sig_id == -1)
-        return 0;
-    p[0] = (unsigned char)md_id;
-    p[1] = (unsigned char)sig_id;
-    return 1;
-}
-
 int tls12_get_sigid(const EVP_PKEY *pk)
 {
     return tls12_find_id(EVP_PKEY_id(pk), tls12_sig, OSSL_NELEM(tls12_sig));
