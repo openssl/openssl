1db22cc5a5267bc16a74d5ac3f2a8b01f531c790
Matt Caswell <matt@openssl.org>
2016-10-12 10:56:29 +0100

    Rebase shim against latest boringssl code
    Numerous conflicts resolved. rebase was against commit 490469f850.
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- test/ossl_shim/test_config.h
+++ test/ossl_shim/test_config.h
@@ -16,15 +16,17 @@
 #define HEADER_TEST_CONFIG
 
 #include <string>
+#include <vector>
 
 
 struct TestConfig {
   int port = 0;
   bool is_server = false;
   bool is_dtls = false;
-  bool resume = false;
+  int resume_count = 0;
   bool fallback_scsv = false;
   std::string digest_prefs;
+  std::vector<int> signing_prefs;
   std::string key_file;
   std::string cert_file;
   std::string expected_server_name;
@@ -38,11 +40,13 @@ struct TestConfig {
   bool write_different_record_sizes = false;
   bool cbc_record_splitting = false;
   bool partial_write = false;
+  bool no_tls13 = false;
   bool no_tls12 = false;
   bool no_tls11 = false;
   bool no_tls1 = false;
   bool no_ssl3 = false;
   std::string expected_channel_id;
+  bool enable_channel_id = false;
   std::string send_channel_id;
   bool shim_writes_first = false;
   std::string host_name;
@@ -50,6 +54,7 @@ struct TestConfig {
   std::string expected_alpn;
   std::string expected_advertised_alpn;
   std::string select_alpn;
+  bool decline_alpn = false;
   bool expect_session_miss = false;
   bool expect_extended_master_secret = false;
   std::string psk;
@@ -97,12 +102,19 @@ struct TestConfig {
   bool renegotiate_freely = false;
   bool renegotiate_ignore = false;
   bool disable_npn = false;
-  int expect_server_key_exchange_hash = 0;
+  int expect_peer_signature_algorithm = 0;
   bool p384_only = false;
   bool enable_all_curves = false;
   bool use_sparse_dh_prime = false;
-  int expect_key_exchange_info = 0;
+  int expect_curve_id = 0;
+  int expect_dhe_group_size = 0;
   bool use_old_client_cert_callback = false;
+  int initial_timeout_duration_ms = 0;
+  bool use_null_client_ca_list = false;
+  bool send_alert = false;
+  bool peek_then_read = false;
+  bool enable_grease = false;
+  int max_cert_list = 0;
 };
 
 bool ParseConfig(int argc, char **argv, TestConfig *out_config);
