31bcec10723e55ba140fba73dae3aa51821b9310
Richard Levitte <levitte@openssl.org>
2016-10-18 22:02:30 +0200

    Fix config option 'no-deprecated'
    crypto/asn1/asn1_item_list.c needed including dh.h and rsa.h directly.
    The reason is that they are not included by x509.h when configured
    'no-deprecated'

--- crypto/asn1/asn1_item_list.c
+++ crypto/asn1/asn1_item_list.c
@@ -12,9 +12,11 @@
 #include <openssl/asn1.h>
 #include <openssl/asn1t.h>
 #include <openssl/cms.h>
+#include <openssl/dh.h>
 #include <openssl/ocsp.h>
 #include <openssl/pkcs7.h>
 #include <openssl/pkcs12.h>
+#include <openssl/rsa.h>
 #include <openssl/x509v3.h>
 
 #include "asn1_item_list.h"
