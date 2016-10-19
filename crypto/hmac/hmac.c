708e06c55d9fee5d59e4a4f409d115423ea1fa56
Matt Caswell <matt@openssl.org>
2016-10-19 16:29:01 +0100

    Ensure HMAC_size() handles errors correctly
    Reviewed-by: Rich Salz <rsalz@openssl.org>

--- crypto/hmac/hmac.c
+++ crypto/hmac/hmac.c
@@ -118,7 +118,10 @@ int HMAC_Final(HMAC_CTX *ctx, unsigned char *md, unsigned int *len)
 
 size_t HMAC_size(const HMAC_CTX *ctx)
 {
-    return EVP_MD_size((ctx)->md);
+    int size = EVP_MD_size((ctx)->md);
+    if (size < 0)
+        return 0;
+    return size;
 }
 
 HMAC_CTX *HMAC_CTX_new(void)
