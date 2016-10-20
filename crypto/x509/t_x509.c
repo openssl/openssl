676befbeb76ca19c9e7b2febfabda16bd26e00a0
Kurt Roeckx <kurt@roeckx.be>
2016-10-20 20:49:22 +0200

    Print the X509 version signed, and convert to unsigned for the hex version.
    Found by tis-interpreter
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    GH: #1754

--- crypto/x509/t_x509.c
+++ crypto/x509/t_x509.c
@@ -71,8 +71,13 @@ int X509_print_ex(BIO *bp, X509 *x, unsigned long nmflags,
     }
     if (!(cflag & X509_FLAG_NO_VERSION)) {
         l = X509_get_version(x);
-        if (BIO_printf(bp, "%8sVersion: %lu (0x%lx)\n", "", l + 1, l) <= 0)
-            goto err;
+        if (l >= 0 && l <= 2) {
+            if (BIO_printf(bp, "%8sVersion: %ld (0x%lx)\n", "", l + 1, (unsigned long)l) <= 0)
+                goto err;
+        } else {
+            if (BIO_printf(bp, "%8sVersion: Unknown (%ld)\n", "", l) <= 0)
+                goto err;
+        }
     }
     if (!(cflag & X509_FLAG_NO_SERIAL)) {
 
