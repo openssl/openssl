09b9ee04fb56bd8b8bae3c6421e192fb3f18ac73
Tomas Mraz <tmraz@fedoraproject.org>
2016-11-15 10:10:32 +0100

    Do not eat trailing '\n' in BIO_gets for fd BIO.

--- crypto/bio/bss_fd.c
+++ crypto/bio/bss_fd.c
@@ -207,8 +207,10 @@ static int fd_gets(BIO *bp, char *buf, int size)
     char *ptr = buf;
     char *end = buf + size - 1;
 
-    while ((ptr < end) && (fd_read(bp, ptr, 1) > 0) && (ptr[0] != '\n'))
-        ptr++;
+    while (ptr < end && fd_read(bp, ptr, 1) > 0) {
+        if (*ptr++ == '\n')
+           break;
+    }
 
     ptr[0] = '\0';
 
