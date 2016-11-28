00e85a25d9ac10c2a050030b73d7e7e3f61e79b1
Rich Salz <rsalz@openssl.org>
2016-11-28 15:33:40 -0500

    CRL critical extension bugfix
    More importantly, port CRL test from boringSSL crypto/x509/x509_test.cc
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/1775)

--- crypto/x509/x_crl.c
+++ crypto/x509/x_crl.c
@@ -213,7 +213,7 @@ static int crl_cb(int operation, ASN1_VALUE **pval, const ASN1_ITEM *it,
                 if ((nid == NID_issuing_distribution_point)
                     || (nid == NID_authority_key_identifier)
                     || (nid == NID_delta_crl))
-                    break;;
+                    continue;
                 crl->flags |= EXFLAG_CRITICAL;
                 break;
             }
