e61ec2d9babf30da3a4d94554e576bf10c1fc92c
Richard Levitte <levitte@openssl.org>
2016-12-05 23:15:58 +0100

    STORE 'file' scheme loader: add support for containers
    Containers are objects that are containers for a bunch of other
    objects with types we recognise but aren't readable in a stream.  Such
    containers are read and parsed, and their content is cached, to be
    served one object at a time.
    
    This extends the FILE_HANDLER type to include a function to destroy
    the cache and a function to simulate the EOF check.
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/3542)

--- include/openssl/storeerr.h
+++ include/openssl/storeerr.h
@@ -51,6 +51,7 @@ int ERR_load_OSSL_STORE_strings(void);
  */
 # define OSSL_STORE_R_AMBIGUOUS_CONTENT_TYPE              107
 # define OSSL_STORE_R_INVALID_SCHEME                      106
+# define OSSL_STORE_R_IS_NOT_A                            112
 # define OSSL_STORE_R_NOT_A_CERTIFICATE                   100
 # define OSSL_STORE_R_NOT_A_CRL                           101
 # define OSSL_STORE_R_NOT_A_KEY                           102
