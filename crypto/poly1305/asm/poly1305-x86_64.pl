7eaf002f3e6f387dd2376f3a643319f727fa2b2f
Andy Polyakov <appro@openssl.org>
2016-12-14 13:38:04 +0100

    poly1305/asm/poly1305-x86_64.pl: allow nasm to assemble AVX512 code. chacha/asm/chacha-x86_64.pl: refine nasm detection logic.

--- crypto/poly1305/asm/poly1305-x86_64.pl
+++ crypto/poly1305/asm/poly1305-x86_64.pl
@@ -66,8 +66,9 @@ if (`$ENV{CC} -Wa,-v -c -o /dev/null -x assembler /dev/null 2>&1`
 }
 
 if (!$avx && $win64 && ($flavour =~ /nasm/ || $ENV{ASM} =~ /nasm/) &&
-	   `nasm -v 2>&1` =~ /NASM version ([2-9]\.[0-9]+)/) {
-	$avx = ($1>=2.09) + ($1>=2.10);
+	   `nasm -v 2>&1` =~ /NASM version ([2-9]\.[0-9]+)(?:\.([0-9]+))?/) {
+	$avx = ($1>=2.09) + ($1>=2.10) + ($1>=2.12);
+	$avx += 1 if ($1==2.11 && $2>=8);
 }
 
 if (!$avx && $win64 && ($flavour =~ /masm/ || $ENV{ASM} =~ /ml64/) &&
