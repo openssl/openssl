1ef99268e9085b3d9db05448372cb58ad130bdf9
Ben Laurie <ben@links.org>
2016-12-27 15:25:10 +0000

    Partial SHA-1 verification (doesn't work yet, lss can't cope).

---
+++ proof/sha1-wrap.c
@@ -0,0 +1,10 @@
+#include <openssl/sha.h>
+#include <sym-api.h>
+
+int main() {
+  const size_t s = 100;
+  unsigned char *in = lss_fresh_array_uint8(s, 0, NULL);
+  unsigned char out[SHA_DIGEST_LENGTH];
+  SHA1(in, s, out);
+  lss_write_aiger_array_uint8(out, SHA_DIGEST_LENGTH, "sha1.aig");
+}
