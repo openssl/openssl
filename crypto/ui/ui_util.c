cf26e093a956d342151b7912045156f79a688d05
Richard Levitte <levitte@openssl.org>
2017-01-12 21:34:11 +0100

    [to be squashed] Make sure the ex_data index is valid

--- crypto/ui/ui_util.c
+++ crypto/ui/ui_util.c
@@ -139,6 +139,7 @@ UI_METHOD *UI_UTIL_wrap_read_pem_callback(pem_password_cb *cb, int rwflag)
 {
     struct pem_password_cb_data *data = NULL;
     UI_METHOD *ui_method = NULL;
+    int idx = 0;
 
     if ((data = OPENSSL_zalloc(sizeof(*data))) == NULL
         || (ui_method = UI_create_method("PEM password callback wrapper")) == NULL
@@ -146,9 +147,8 @@ UI_METHOD *UI_UTIL_wrap_read_pem_callback(pem_password_cb *cb, int rwflag)
         || UI_method_set_reader(ui_method, ui_read) < 0
         || UI_method_set_writer(ui_method, ui_write) < 0
         || UI_method_set_closer(ui_method, ui_close) < 0
-        || UI_method_set_ex_data(ui_method, RUN_ONCE(&get_index_once,
-                                                     ui_method_data_index),
-                                 data) < 0) {
+        || (idx = RUN_ONCE(&get_index_once, ui_method_data_index)) <= 0
+        || UI_method_set_ex_data(ui_method, idx, data) < 0) {
         UI_destroy_method(ui_method);
         OPENSSL_free(data);
         return NULL;
