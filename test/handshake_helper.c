ee5b6a42be3c0ca18145ace8793135fbb4768248
Dr. Stephen Henson <steve@openssl.org>
2017-01-13 15:20:42 +0000

    Add options to check TLS signing hashes
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/2235)

--- test/handshake_helper.c
+++ test/handshake_helper.c
@@ -1070,6 +1070,9 @@ static HANDSHAKE_RESULT *do_handshake_internal(
         EVP_PKEY_free(tmp_key);
     }
 
+    SSL_get_peer_signature_nid(client.ssl, &ret->server_sign_hash);
+    SSL_get_peer_signature_nid(server.ssl, &ret->client_sign_hash);
+
     ret->server_cert_type = peer_pkey_type(client.ssl);
     ret->client_cert_type = peer_pkey_type(server.ssl);
 
