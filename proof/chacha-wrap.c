30c98597d59b9df07c885ff460d208a101cdaee2
Ben Laurie <ben@links.org>
2017-01-29 16:02:07 +0000

    Broken stuff.

---
+++ proof/chacha-wrap.c
@@ -0,0 +1,12 @@
+#include <internal/chacha.h>
+#include <sym-api.h>
+
+int main() {
+  uint32_t *key = lss_fresh_array_uint32(8, 0, NULL);
+  const size_t s = 100;
+  unsigned char *in = lss_fresh_array_uint8(s, 0, NULL);
+  uint32_t *ctr = lss_fresh_array_uint32(4, 0, NULL);
+  unsigned char *out = malloc(s);
+  ChaCha20_ctr32(out, in, s, key, ctr);
+  lss_write_aiger_array_uint8(out, s, "chacha.aig");
+}
--- proof/sha256-wrap.c
+++ proof/sha256-wrap.c
@@ -5,6 +5,6 @@ int main() {
   const size_t s = 100;
   const unsigned char *in = lss_fresh_array_uint8(s, 0, NULL);
   unsigned char out[SHA_DIGEST_LENGTH];
-  SHA1(in, s, out);
+  SHA256(in, s, out);
   lss_write_aiger_array_uint8(out, SHA_DIGEST_LENGTH, "sha1.aig");
 }
