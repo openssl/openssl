93a77f9e2c66a4e63f732bb0d04695b4f951febb
Dr. Stephen Henson <steve@openssl.org>
2017-01-31 17:45:00 +0000

    Add function tls_choose_sigalg().
    New function tls_choose_sigalg(). This is a signature algorithm version
    of ssl3_choose_cipher(): it picks and sets the appropriate signature
    algorithm and certificate based on shared signature algorithms.
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/2339)

--- ssl/ssl_locl.h
+++ ssl/ssl_locl.h
@@ -1287,6 +1287,10 @@ typedef struct ssl3_state_st {
         unsigned char *psk;
         size_t psklen;
 # endif
+        /* Signature algorithm we actually use */
+        const SIGALG_LOOKUP *sigalg;
+        /* Index of certificate we use */
+        int cert_idx;
         /*
          * signature algorithms peer reports: e.g. supported signature
          * algorithms extension for server or as part of a certificate
@@ -2256,6 +2260,8 @@ __owur DH *ssl_get_auto_dh(SSL *s);
 __owur int ssl_security_cert(SSL *s, SSL_CTX *ctx, X509 *x, int vfy, int is_ee);
 __owur int ssl_security_cert_chain(SSL *s, STACK_OF(X509) *sk, X509 *ex,
                                    int vfy);
+
+int tls_choose_sigalg(SSL *s);
 
 __owur EVP_MD_CTX *ssl_replace_hash(EVP_MD_CTX **hash, const EVP_MD *md);
 void ssl_clear_hash_ctx(EVP_MD_CTX **hash);
