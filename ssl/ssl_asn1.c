bc931874e97db89284175e11761cee777b29928d
Bernd Edlinger <bernd.edlinger@hotmail.de>
2017-02-13 13:03:52 +0100

    Fix i2d_SSL_SESSION pp output parameter should point to end of asn1 data.
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/2607)
    (cherry picked from commit a0179d0afb621a0875ddcfd939719a9628ac4444)

--- ssl/ssl_asn1.c
+++ ssl/ssl_asn1.c
@@ -278,8 +278,8 @@ SSL_SESSION *d2i_SSL_SESSION(SSL_SESSION **a, const unsigned char **pp,
         goto err;
     }
 
-    p = as->cipher->data;
-    id = 0x03000000L | ((unsigned long)p[0] << 8L) | (unsigned long)p[1];
+    id = 0x03000000L | ((unsigned long)as->cipher->data[0] << 8L)
+                     | (unsigned long)as->cipher->data[1];
 
     ret->cipher = NULL;
     ret->cipher_id = id;
