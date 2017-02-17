389252a37e37d096d584899a4bfb47c7adce3710
FdaSilvaYY <fdasilvayy@gmail.com>
2017-02-17 23:13:26 +0100

    Fix a few more typos
    Reviewed-by: Andy Polyakov <appro@openssl.org>
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/3066)
    (cherry picked from commit 1ee4b98e695cd041da931c10fbdaf82f0ee0f268)

--- ssl/statem/statem_clnt.c
+++ ssl/statem/statem_clnt.c
@@ -2373,7 +2373,7 @@ static int tls_construct_cke_gost(SSL *s, unsigned char **p, int *len, int *al)
         dgst_nid = NID_id_GostR3411_2012_256;
 
     /*
-     * Get server sertificate PKEY and create ctx from it
+     * Get server certificate PKEY and create ctx from it
      */
     peer_cert = s->session->peer;
     if (!peer_cert) {
