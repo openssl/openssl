d5f94fd472deee3d6386cf494548c495090a9266
Richard Levitte <levitte@openssl.org>
2017-03-05 21:51:18 +0100

    Add a platform specific configuration checker
    For each platform, we may need to perform some basic checks to see
    that available tools perform as we expect them.
    
    For the moment, the added checkers test that Perl gives the expected
    path format.  This should help MingW users to see if they run an
    appropriate Perl implementation, for example.

---
+++ Configurations/windows-checker.pm
@@ -0,0 +1,21 @@
+#! /usr/bin/perl
+
+use Config;
+
+# Check that the perl implementation file modules generate paths that
+# we expect for the platform
+use File::Spec::Functions qw(:DEFAULT rel2abs);
+
+if (rel2abs('.') !~ m|\\|) {
+    die <<EOF;
+
+******************************************************************************
+This perl version doesn't produce Windows like paths (with backward slash
+directory separators).  Please use an implementation that does.
+
+This Perl version: $Config{version} for $Config{archname}
+******************************************************************************
+EOF
+}
+
+1;
