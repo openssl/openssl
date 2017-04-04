e7a069766b2a51dd83c7727cdbc0fed9530d76c0
rashok <raja.ashok.85@gmail.com>
2017-04-05 02:25:57 +0530

    Additional check to handle BAD SSL_write retry, in master branch

--- ssl/record/rec_layer_s3.c
+++ ssl/record/rec_layer_s3.c
@@ -356,7 +356,7 @@ int ssl3_write_bytes(SSL *s, int type, const void *buf_, size_t len,
      * promptly send beyond the end of the users buffer ... so we trap and
      * report the error in a way the user will notice
      */
-    if (len < s->rlayer.wnum) {
+    if ((len < s->rlayer.wnum) || ((tot != 0) && (len < (tot + s->rlayer.wnum)))) {
         SSLerr(SSL_F_SSL3_WRITE_BYTES, SSL_R_BAD_LENGTH);
         return -1;
     }
