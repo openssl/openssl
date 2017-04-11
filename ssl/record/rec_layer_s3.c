2b4f565506c978a1328d481ee7b7951a734f053c
rashok <raja.ashok.85@gmail.com>
2017-04-11 08:57:11 +0530

    Compilation warning fix

--- ssl/record/rec_layer_s3.c
+++ ssl/record/rec_layer_s3.c
@@ -369,7 +369,7 @@ int ssl3_write_bytes(SSL *s, int type, const void *buf_, int len)
      * report the error in a way the user will notice
      */
     if (((unsigned int)len < s->rlayer.wnum) 
-        || ((wb->left != 0) && (len < (s->rlayer.wnum + s->rlayer.wpend_tot)))) {
+        || ((wb->left != 0) && ((unsigned int)len < (s->rlayer.wnum + s->rlayer.wpend_tot)))) {
         SSLerr(SSL_F_SSL3_WRITE_BYTES, SSL_R_BAD_LENGTH);
         return -1;
     }
