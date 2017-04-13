b95c1fd0577aa4c6988e9103ff94046c052207c9
Pauli <paul.dale@oracle.com>
2017-04-13 11:40:35 +1000

    Split the CAST tests up.

--- test/casttest.c
+++ test/casttest.c
@@ -16,13 +16,7 @@
 #include "test_main.h"
 #include "testutil.h"
 
-#ifdef OPENSSL_NO_CAST
-int main(int argc, char *argv[])
-{
-    printf("No CAST support\n");
-    return (0);
-}
-#else
+#ifndef OPENSSL_NO_CAST
 # include <openssl/cast.h>
 
 static unsigned char k[16] = {
@@ -40,8 +34,6 @@ static unsigned char c[3][8] = {
     {0xEB, 0x6A, 0x71, 0x1A, 0x2C, 0x02, 0x27, 0x1B},
     {0x7A, 0xC8, 0x16, 0xD1, 0x6E, 0x9B, 0x30, 0x2E},
 };
-
-static unsigned char out[80];
 
 static unsigned char in_a[16] = {
     0x01, 0x23, 0x45, 0x67, 0x12, 0x34, 0x56, 0x78,
@@ -63,27 +55,33 @@ static unsigned char c_b[16] = {
     0x80, 0xAC, 0x05, 0xB8, 0xE8, 0x3D, 0x69, 0x6E
 };
 
-static int cast_test(void)
+static int cast_test_vector(int z)
+{
+    int testresult = 1;
+    CAST_KEY key;
+    unsigned char out[80];
+
+    CAST_set_key(&key, k_len[z], k);
+    CAST_ecb_encrypt(in, out, &key, CAST_ENCRYPT);
+    if (!TEST_mem_eq(out, sizeof(c[z]), c[z], sizeof(c[z]))) {
+        TEST_info("CAST_ENCRYPT iteration %d failed (len=%d)", z, k_len[z]);
+        testresult = 0;
+    }
+
+    CAST_ecb_encrypt(out, out, &key, CAST_DECRYPT);
+    if (!TEST_mem_eq(out, sizeof(in), in, sizeof(in))) {
+        TEST_info("CAST_DECRYPT iteration %d failed (len=%d)", z, k_len[z]);
+        testresult = 0;
+    }
+    return testresult;
+}
+
+static int cast_test_iterations(void)
 {
     long l;
-    int z, testresult = 1;
+    int testresult = 1;
     CAST_KEY key, key_b;
     unsigned char out_a[16], out_b[16];
-
-    for (z = 0; z < 3; z++) {
-        CAST_set_key(&key, k_len[z], k);
-        CAST_ecb_encrypt(in, out, &key, CAST_ENCRYPT);
-        if (!TEST_mem_eq(out, sizeof(c[z]), c[z], sizeof(c[z]))) {
-            TEST_info("CAST_ENCRYPT iteration %d failed (len=%d)", z, k_len[z]);
-            testresult = 0;
-        }
-
-        CAST_ecb_encrypt(out, out, &key, CAST_DECRYPT);
-        if (!TEST_mem_eq(out, sizeof(in), in, sizeof(in))) {
-            TEST_info("CAST_DECRYPT iteration %d failed (len=%d)", z, k_len[z]);
-            testresult = 0;
-        }
-    }
 
     memcpy(out_a, in_a, sizeof(in_a));
     memcpy(out_b, in_b, sizeof(in_b));
@@ -107,5 +105,8 @@ static int cast_test(void)
 
 void register_tests(void)
 {
-    ADD_TEST(cast_test);
+#ifndef OPENSSL_NO_CAST
+    ADD_ALL_TESTS(cast_test_vector, OSSL_NELEM(k_len));
+    ADD_TEST(cast_test_iterations);
+#endif
 }
