1d9704c73a00b3ebc83d1d03a292b4fd2c8f6ffd
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2017-04-16 22:23:22 +0900

    Don't nullify s->ext.ocsp.resp on 2nd or later Certificate in TLSv1.3
    In tls_parse_stoc_status_request function, the OCSP in the only first
    certificate in the chain is parsed for TLSv1.3.  But
    init_status_request function is already called for every certificate
    in the chain, and it nullifies s->ext.ocsp.resp.  This means that if
    server sends 2 certificates in the chain, s->ext.ocsp.resp ends up
    being NULL.
    
    This patch fixes the issue by delaying initialization of
    s->ext.ocsp.resp, and makes sure that it is called when necessary.
    Specifically, for TLSv1.3, it is not called for 2nd or later
    certificate.

--- ssl/statem/extensions.c
+++ ssl/statem/extensions.c
@@ -857,14 +857,6 @@ static int init_status_request(SSL *s, unsigned int context)
 {
     if (s->server) {
         s->ext.status_type = TLSEXT_STATUSTYPE_nothing;
-    } else {
-        /*
-         * Ensure we get sensible values passed to tlsext_status_cb in the event
-         * that we don't receive a status message
-         */
-        OPENSSL_free(s->ext.ocsp.resp);
-        s->ext.ocsp.resp = NULL;
-        s->ext.ocsp.resp_len = 0;
     }
 
     return 1;
