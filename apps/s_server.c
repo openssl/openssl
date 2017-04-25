2e71d24724c0261eca4d53a109f8b865f6210089
Richard Levitte <levitte@openssl.org>
2017-04-25 15:35:09 +0200

    Add guards around one of use of IPPROTO_SCTP where it was missing

--- apps/s_server.c
+++ apps/s_server.c
@@ -2199,10 +2199,12 @@ static int sv_body(int s, int stype, int prot, unsigned char *context)
             /* want to do MTU discovery */
             BIO_ctrl(sbio, BIO_CTRL_DGRAM_MTU_DISCOVER, 0, NULL);
 
+# ifndef OPENSSL_NO_SCTP
         if (prot != IPPROTO_SCTP) {
             /* Turn on cookie exchange. Not necessary for SCTP */
             SSL_set_options(con, SSL_OP_COOKIE_EXCHANGE);
         }
+# endif
     } else
 #endif
         sbio = BIO_new_socket(s, BIO_NOCLOSE);
