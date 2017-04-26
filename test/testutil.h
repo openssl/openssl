6a119c51716ea420615c9df4a2b678eb78090e52
Bernd Edlinger <bernd.edlinger@hotmail.de>
2017-04-26 19:29:49 +0200

    Simplify dataflow in TEST_true/TEST_false to prevent false positive GCC-7 warnings in clienthellotest.c

--- test/testutil.h
+++ test/testutil.h
@@ -328,8 +328,8 @@ void test_info_c90(const char *desc, ...) PRINTF_FORMAT(1, 2);
 # define TEST_mem_eq(a, m, b, n) test_mem_eq(__FILE__, __LINE__, #a, #b, a, m, b, n)
 # define TEST_mem_ne(a, m, b, n) test_mem_ne(__FILE__, __LINE__, #a, #b, a, m, b, n)
 
-# define TEST_true(a)         test_true(__FILE__, __LINE__, #a, (a) != 0)
-# define TEST_false(a)        test_false(__FILE__, __LINE__, #a, (a) != 0)
+# define TEST_true(a)  ((a) == 0 ? test_true(__FILE__, __LINE__, #a, 0), 0 : 1)
+# define TEST_false(a) ((a) != 0 ? test_false(__FILE__, __LINE__, #a, 1), 0 : 1)
 
 /*
  * TEST_error(desc, ...) prints an informative error message in the standard
