74a011ebb5e9028ef18982d737a434a8ff926a95
Richard Levitte <levitte@openssl.org>
2017-05-11 20:20:07 +0200

    Cleanup - use e_os2.h rather than stdint.h
    Not exactly everywhere, but in those source files where stdint.h is
    included conditionally, or where it will be eventually
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/3447)

--- include/openssl/bio.h
+++ include/openssl/bio.h
@@ -20,11 +20,7 @@
 # include <openssl/crypto.h>
 
 # ifndef OPENSSL_NO_SCTP
-#  ifndef OPENSSL_SYS_VMS
-#   include <stdint.h>
-#  else
-#   include <inttypes.h>
-#  endif
+#  include <openssl/e_os2.h>
 # endif
 
 #ifdef  __cplusplus
