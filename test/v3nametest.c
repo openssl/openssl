4adbbd1a67d5e0076a3f77518767a16d007dbc90
Thijs Wenker <me@thoys.nl>
2017-05-29 02:55:43 +0200

    CLA: Trivial - Fix certificate version number in test
    The version number 3 means version 4, while 2 means version 3. Since this is the v3nametest, version 3 should be used.

--- test/v3nametest.c
+++ test/v3nametest.c
@@ -251,7 +251,7 @@ static X509 *make_cert()
 
     if (!TEST_ptr(crt = X509_new()))
         return NULL;
-    if (!TEST_true(X509_set_version(crt, 3))) {
+    if (!TEST_true(X509_set_version(crt, 2))) {
         X509_free(crt);
         return NULL;
     }
