675787a088c76d8891d90de12b55186a2de748bb
Dr. Stephen Henson <steve@openssl.org>
2017-06-14 16:37:06 +0100

    Ed25519 support for mkcert.sh
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/3585)

--- test/certs/mkcert.sh
+++ test/certs/mkcert.sh
@@ -49,6 +49,7 @@ key() {
         ec)  args=("${args[@]}" -pkeyopt "ec_paramgen_curve:$bits")
                args=("${args[@]}" -pkeyopt ec_param_enc:named_curve);;
         dsa)  args=(-paramfile "$bits");;
+        ed25519)  ;;
         *) printf "Unsupported key algorithm: %s\n" "$alg" >&2; return 1;;
         esac
         stderr_onerror \
