bcdf00d0bc50d5455677dfa462399d4229286a61
Rich Salz <rsalz@openssl.org>
2017-06-15 13:15:26 -0400

    Standardize Levitte's dual-license
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/3689)

--- crypto/LPdir_nyi.c
+++ crypto/LPdir_nyi.c
@@ -8,6 +8,9 @@
  */
 
 /*
+ * This file is dual-licensed and is also avialable under the following
+ * terms:
+ *
  * Copyright (c) 2004, Richard Levitte <richard@levitte.org>
  * All rights reserved.
  *
