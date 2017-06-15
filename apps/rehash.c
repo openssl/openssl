1f4629e88e7692feec41ced43da88ef8975682e7
Rich Salz <rsalz@openssl.org>
2017-06-15 12:03:40 -0400

    Cleanup some copyright stuff
    Remove some incorrect copyright references.
    Move copyright to standard place
    Add OpenSSL copyright where missing.
    Remove copyrighted file that we don't use any more
    Remove Itanium assembler for RC4 and MD5 (assembler versions of old and
    weak algorithms for an old chip)
    Standardize apps/rehash copyright comment; approved by Timo
    Put dual-copyright notice on mkcert
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/3691)

--- apps/rehash.c
+++ apps/rehash.c
@@ -1,16 +1,11 @@
 /*
  * Copyright 2015-2017 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright (c) 2013-2014 Timo Ter‰s <timo.teras@gmail.com>
  *
  * Licensed under the OpenSSL license (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
  * in the file LICENSE in the source distribution or at
  * https://www.openssl.org/source/license.html
- */
-
-/*
- * C implementation based on the original Perl and shell versions
- *
- * Copyright (c) 2013-2014 Timo Ter√§s <timo.teras@iki.fi>
  */
 
 #include "apps.h"
--- crypto/mem_sec.c
+++ crypto/mem_sec.c
@@ -1,15 +1,11 @@
 /*
  * Copyright 2015-2017 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2004-2014, Akamai Technologies. All Rights Reserved.
  *
  * Licensed under the OpenSSL license (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
  * in the file LICENSE in the source distribution or at
  * https://www.openssl.org/source/license.html
- */
-
-/*
- * Copyright 2004-2014, Akamai Technologies. All Rights Reserved.
- * This file is distributed under the terms of the OpenSSL license.
  */
 
 /*
