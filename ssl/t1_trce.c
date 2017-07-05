8f81476145f75851a5f894e857ceb781aa979b99
Matt Caswell <matt@openssl.org>
2017-07-05 11:31:51 +0100

    Update SSL_trace() to know about ticket_nonce
    Reviewed-by: Ben Kaduk <kaduk@mit.edu>
    (Merged from https://github.com/openssl/openssl/pull/3852)

--- ssl/t1_trce.c
+++ ssl/t1_trce.c
@@ -1341,6 +1341,9 @@ static int ssl_print_ticket(BIO *bio, int indent, SSL *s,
         msg += 4;
         BIO_indent(bio, indent + 2, 80);
         BIO_printf(bio, "ticket_age_add=%u\n", ticket_age_add);
+        if (!ssl_print_hexbuf(bio, indent + 2, "ticket_nonce", 1, &msg,
+                              &msglen))
+            return 0;
     }
     if (!ssl_print_hexbuf(bio, indent + 2, "ticket", 2, &msg, &msglen))
         return 0;
