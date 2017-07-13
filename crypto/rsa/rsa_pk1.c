ff0426cc94df5e6dd25b8cfd6f9f7c840264a400
Emilia Kasper <emilia@openssl.org>
2017-07-13 18:30:56 +0200

    Remove resolved TODO
    Fixed in 5b8fa431ae8eb5a18ba913494119e394230d4b70
    
    [ci skip]
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/3924)

--- crypto/rsa/rsa_pk1.c
+++ crypto/rsa/rsa_pk1.c
@@ -226,8 +226,6 @@ int RSA_padding_check_PKCS1_type_2(unsigned char *to, int tlen,
      * We can't continue in constant-time because we need to copy the result
      * and we cannot fake its length. This unavoidably leaks timing
      * information at the API boundary.
-     * TODO(emilia): this could be addressed at the call site,
-     * see BoringSSL commit 0aa0767340baf925bda4804882aab0cb974b2d26.
      */
     if (!good) {
         mlen = -1;
