8b4bd0eceb5200fe9efea8b1494036db26d38898
Andy Polyakov <appro@openssl.org>
2017-07-23 16:44:27 +0200

    sha/asm/keccak1600-avx512.pl: recover piece of commentary [to be squashed].
    [skip ci]

--- crypto/sha/asm/keccak1600-avx512.pl
+++ crypto/sha/asm/keccak1600-avx512.pl
@@ -81,7 +81,20 @@
 # [4][2] [3][1] [2][0] [1][4] [0][3]>1.0.4.3.2>[1][4] [0][3] [4][2] [3][1] [2][0]
 # [4][3] [3][2] [2][1] [1][0] [0][4]>0.4.3.2.1>[0][4] [4][3] [3][2] [2][1] [1][0]
 #
-# Now intra-register permutations yield initial straight linear layout.
+# Now intra-register permutations yield initial [almost] straight
+# linear layout:
+#
+# [4][4] [3][3] [2][2] [1][1] [0][0]
+##[0][4] [0][3] [0][2] [0][1] [0][0]
+# [3][4] [2][3] [1][2] [0][1] [4][0]
+##[2][3] [2][2] [2][1] [2][0] [2][4]
+# [2][4] [1][3] [0][2] [4][1] [3][0]
+##[4][2] [4][1] [4][0] [4][4] [4][3]
+# [1][4] [0][3] [4][2] [3][1] [2][0]
+##[1][1] [1][0] [1][4] [1][3] [1][2]
+# [0][4] [4][3] [3][2] [2][1] [1][0]
+##[3][0] [3][4] [3][3] [3][2] [3][1]
+#
 # This means that odd round Chi is performed in less suitable layout,
 # with a number of additional permutations. But overall it turned to be
 # a win. Permutations are fastest possible on Knights Landing and they
