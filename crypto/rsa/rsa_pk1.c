57ad215615071a7dc578e390de61ae163b15df9d
Bernd Edlinger <bernd.edlinger@hotmail.de>
2017-07-31 20:38:26 +0200

    Fix an information leak in the RSA padding check code. The memory blocks contain secret data and must be cleared before returning to the system heap.
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/4063)

--- crypto/rsa/rsa_pk1.c
+++ crypto/rsa/rsa_pk1.c
@@ -264,8 +264,10 @@ int RSA_padding_check_PKCS1_type_2(unsigned char *to, int tlen,
     memcpy(to, em + msg_index, mlen);
 
  err:
-    if (em != NULL)
+    if (em != NULL) {
+        OPENSSL_cleanse(em, num);
         OPENSSL_free(em);
+    }
     if (mlen == -1)
         RSAerr(RSA_F_RSA_PADDING_CHECK_PKCS1_TYPE_2,
                RSA_R_PKCS_DECODING_ERROR);
