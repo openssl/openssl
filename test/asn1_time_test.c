3a97e080dd229c40bcd5d738439dd9a1a000952d
Richard Levitte <levitte@openssl.org>
2017-08-17 21:22:43 +0200

    [to be squashed] Restore ptm, as Windows has a signed time_t, but negative values are an error

--- test/asn1_time_test.c
+++ test/asn1_time_test.c
@@ -285,16 +285,22 @@ int setup_tests(void)
      * if |t| is unsigned.
      */
     time_t t = -1;
+    /*
+     * On some platforms, |time_t| is signed, but a negative value is an
+     * error, and using it with gmtime() or localtime() generates a NULL.
+     * If that is the case, we can't perform tests on negative values.
+     */
+    struct tm *ptm = localtime(&t);
 
     ADD_ALL_TESTS(test_table_pos, OSSL_NELEM(tbl_testdata_pos));
-    if (!(t > 0)) {
+    if (!(t > 0) && ptm != NULL) {
         TEST_info("Adding negative-sign time_t tests");
         ADD_ALL_TESTS(test_table_neg, OSSL_NELEM(tbl_testdata_neg));
     }
     if (sizeof(time_t) > sizeof(uint32_t)) {
         TEST_info("Adding 64-bit time_t tests");
         ADD_ALL_TESTS(test_table_pos_64bit, OSSL_NELEM(tbl_testdata_pos_64bit));
-        if (!(t > 0)) {
+        if (!(t > 0) && ptm != NULL) {
             TEST_info("Adding negative-sign 64-bit time_t tests");
             ADD_ALL_TESTS(test_table_neg_64bit, OSSL_NELEM(tbl_testdata_neg_64bit));
         }
