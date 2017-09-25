6efcd28f7754f4a7da6466299885912095290bc8
Pauli <paul.dale@oracle.com>
2017-09-25 12:04:42 +1000

    Add a CRYPTO_atomic_read call which allows an int variable to be read in an atomic fashion.
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/4414)

--- crypto/threads_none.c
+++ crypto/threads_none.c
@@ -125,6 +125,12 @@ int CRYPTO_atomic_add(int *val, int amount, int *ret, CRYPTO_RWLOCK *lock)
     return 1;
 }
 
+int CRYPTO_atomic_read(int *val, int *ret, CRYPTO_RWLOCK *lock)
+{
+    *ret  = *val;
+    return 1;
+}
+
 int openssl_init_fork_handlers(void)
 {
     return 0;
