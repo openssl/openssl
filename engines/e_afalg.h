4eeed96a69b102fe7d5d9a1315d852a333e5aa91
JitendraLulla <lullajd@yahoo.com>
2017-10-04 16:56:55 +0530

    updated:extending afalg with aes-cbc-192 and aes-cbc-256

--- engines/e_afalg.h
+++ engines/e_afalg.h
@@ -43,39 +43,9 @@
 # define AES_KEY_SIZE_128 16
 # define AES_KEY_SIZE_192 24
 # define AES_KEY_SIZE_256 32
-
 # define AES_IV_LEN       16
 
 # define MAX_INFLIGHTS 1
-
-#define DECLARE_AES_CBC(key_size)   \
-const EVP_CIPHER *afalg_aes_##key_size##_cbc(void)  \
-{   \
-    if (_hidden_aes_##key_size##_cbc == NULL \
-        && ((_hidden_aes_##key_size##_cbc =  \
-            EVP_CIPHER_meth_new(NID_aes_##key_size##_cbc,    \
-                                AES_BLOCK_SIZE, \
-                                AES_KEY_SIZE_##key_size)) == NULL  \
-            || !EVP_CIPHER_meth_set_iv_length(_hidden_aes_##key_size##_cbc,  \
-                                            AES_IV_LEN)\
-            || !EVP_CIPHER_meth_set_flags(_hidden_aes_##key_size##_cbc, \
-                                            EVP_CIPH_CBC_MODE | \
-                                            EVP_CIPH_FLAG_DEFAULT_ASN1) \
-            || !EVP_CIPHER_meth_set_init(_hidden_aes_##key_size##_cbc,   \
-                                            afalg_cipher_init)  \
-            || !EVP_CIPHER_meth_set_do_cipher(_hidden_aes_##key_size##_cbc,  \
-                                            afalg_do_cipher)    \
-            || !EVP_CIPHER_meth_set_cleanup(_hidden_aes_##key_size##_cbc,    \
-                                            afalg_cipher_cleanup)   \
-            || !EVP_CIPHER_meth_set_impl_ctx_size(_hidden_aes_##key_size##_cbc,\
-                                            sizeof(afalg_ctx))))    \
-    {   \
-        EVP_CIPHER_meth_free(_hidden_aes_##key_size##_cbc);  \
-        _hidden_aes_##key_size##_cbc = NULL; \
-    }   \
-    return _hidden_aes_##key_size##_cbc; \
-}
-
 
 typedef enum {
     MODE_UNINIT = 0,
