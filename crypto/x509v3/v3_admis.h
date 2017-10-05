10bcdda30ae2d2a0f4f556a759510b45d13c9b68
Rich Salz <rsalz@openssl.org>
2017-10-06 00:52:12 +0100

    Add accessors for AdmissionSyntax
    Based on code from Matthias Ballreich, Steve Henson, and Wolf Tobias.

--- crypto/x509v3/v3_admis.h
+++ crypto/x509v3/v3_admis.h
@@ -14,44 +14,30 @@
 extern "C" {
 #endif
 
-typedef struct NamingAuthority_st {
+struct NamingAuthority_st {
     ASN1_OBJECT* namingAuthorityId;
     ASN1_IA5STRING* namingAuthorityUrl;
     ASN1_STRING* namingAuthorityText;          /* i.e. DIRECTORYSTRING */
-} NAMING_AUTHORITY;
+};
 
-typedef struct ProfessionInfo_st {
+struct ProfessionInfo_st {
     NAMING_AUTHORITY* namingAuthority;
     STACK_OF(ASN1_STRING)* professionItems;    /* i.e. DIRECTORYSTRING */
     STACK_OF(ASN1_OBJECT)* professionOIDs;
     ASN1_PRINTABLESTRING* registrationNumber;
     ASN1_OCTET_STRING* addProfessionInfo;
-} PROFESSION_INFO;
+};
 
-typedef struct Admissions_st {
+struct Admissions_st {
     GENERAL_NAME* admissionAuthority;
     NAMING_AUTHORITY* namingAuthority;
     STACK_OF(PROFESSION_INFO)* professionInfos;
-} ADMISSIONS;
+};
 
-typedef struct AdmissionSyntax_st {
+struct AdmissionSyntax_st {
     GENERAL_NAME* admissionAuthority;
     STACK_OF(ADMISSIONS)* contentsOfAdmissions;
-} ADMISSION_SYNTAX;
-
-DECLARE_ASN1_ITEM(ADMISSIONS)
-DECLARE_ASN1_ITEM(NAMING_AUTHORITY)
-DECLARE_ASN1_ITEM(PROFESSION_INFO)
-DECLARE_ASN1_ITEM(ADMISSION_SYNTAX)
-
-DECLARE_ASN1_FUNCTIONS(NAMING_AUTHORITY)
-DECLARE_ASN1_FUNCTIONS(PROFESSION_INFO)
-DECLARE_ASN1_FUNCTIONS(ADMISSIONS)
-DECLARE_ASN1_FUNCTIONS(ADMISSION_SYNTAX)
-
-DEFINE_STACK_OF(ADMISSIONS)
-DEFINE_STACK_OF(PROFESSION_INFO)
-DEFINE_STACK_OF(ASN1_STRING)
+};
 
 #ifdef  __cplusplus
 }
