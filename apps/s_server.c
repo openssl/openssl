6233ab6bbcefeadd15f7135918ee66a4fd46b8d3
Kaoru Toda <kunnpuu@gmail.com>
2017-10-22 01:24:10 +0900

    Change to explicit context != NULL.

--- apps/s_server.c
+++ apps/s_server.c
@@ -2213,9 +2213,9 @@ static int sv_body(int s, int stype, int prot, unsigned char *context)
         SSL_set_tlsext_debug_arg(con, bio_s_out);
     }
 
-    if (context
-        && !SSL_set_session_id_context(con,
-                                       context, strlen((char *)context))) {
+    if (context != NULL
+        && !SSL_set_session_id_context(con, context,
+                                       strlen((char *)context))) {
         BIO_printf(bio_err, "Error setting session id context\n");
         ret = -1;
         goto err;
