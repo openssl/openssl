20fbb41f62dc17bcd3895971c0ceb4d163d359ca
Shawn Landden <slandden@gmail.com>
2017-10-21 14:37:14 -0700

    Revert "Add OpenSSL copyright to .pl files"
    This reverts commit 6aa36e8e5a062e31543e7796f0351ff9628832ce.
    
    This commit inadvertantly removed the cryptogram license that all the
    assembly routines were licensed under (differn't from the general
    OpenSSL license, but compatible).
    
     Conflicts:
    	crypto/bn/asm/rsaz-avx2.pl
    	crypto/bn/asm/rsaz-x86_64.pl
    	crypto/ec/asm/ecp_nistz256-avx2.pl
    	crypto/ec/asm/ecp_nistz256-x86_64.pl
    	crypto/rc4/asm/rc4-ia64.pl
    	crypto/sha/asm/sha512-armv8.pl
    	util/incore

--- crypto/sha/asm/sha512-armv8.pl
+++ crypto/sha/asm/sha512-armv8.pl
@@ -1,11 +1,5 @@
-#! /usr/bin/env perl
-# Copyright 2014-2016 The OpenSSL Project Authors. All Rights Reserved.
+#!/usr/bin/env perl
 #
-# Licensed under the OpenSSL license (the "License").  You may not use
-# this file except in compliance with the License.  You can obtain a copy
-# in the file LICENSE in the source distribution or at
-# https://www.openssl.org/source/license.html
-
 # ====================================================================
 # Written by Andy Polyakov <appro@openssl.org> for the OpenSSL
 # project. The module is, however, dual licensed under OpenSSL and
