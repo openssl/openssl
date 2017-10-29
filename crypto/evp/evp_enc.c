5a3d24ad5af3a2fee1d5381bda5329c07b6328d8
Kurt Roeckx <kurt@roeckx.be>
2017-10-29 15:13:43 +0100

    Only reset the ctx when a cipher is given
    This restores the 1.0.2 behaviour
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    Reviewed-by: Benjamin Kaduk <bkaduk@akamai.com>
    GH: #4613
    (cherry picked from commit ffd23209933ea0ad5543f15ca6303d63d8dac826)

--- crypto/evp/evp_enc.c
+++ crypto/evp/evp_enc.c
@@ -50,7 +50,8 @@ void EVP_CIPHER_CTX_free(EVP_CIPHER_CTX *ctx)
 int EVP_CipherInit(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *cipher,
                    const unsigned char *key, const unsigned char *iv, int enc)
 {
-    EVP_CIPHER_CTX_reset(ctx);
+    if (cipher != NULL)
+        EVP_CIPHER_CTX_reset(ctx);
     return EVP_CipherInit_ex(ctx, cipher, NULL, key, iv, enc);
 }
 
