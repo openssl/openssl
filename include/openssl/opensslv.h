382253938fd95d8ac65c56ba74b5cf95b05f6ebf
Matt Caswell <matt@openssl.org>
2017-11-02 14:30:01 +0000

    Prepare for 1.1.0h-dev
    Reviewed-by: Andy Polyakov <appro@openssl.org>

--- include/openssl/opensslv.h
+++ include/openssl/opensslv.h
@@ -39,11 +39,11 @@ extern "C" {
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-# define OPENSSL_VERSION_NUMBER  0x1010007fL
+# define OPENSSL_VERSION_NUMBER  0x10100080L
 # ifdef OPENSSL_FIPS
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.0g-fips  2 Nov 2017"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.0h-fips-dev  xx XXX xxxx"
 # else
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.0g  2 Nov 2017"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.0h-dev  xx XXX xxxx"
 # endif
 
 /*-
