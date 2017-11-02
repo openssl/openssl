95aec441c0a8d4caa4590f9ba2827a131134e8d1
Matt Caswell <matt@openssl.org>
2017-11-02 14:34:50 +0000

    Prepare for 1.0.2n-dev
    Reviewed-by: Andy Polyakov <appro@openssl.org>

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -30,11 +30,11 @@ extern "C" {
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-# define OPENSSL_VERSION_NUMBER  0x100020dfL
+# define OPENSSL_VERSION_NUMBER  0x100020e0L
 # ifdef OPENSSL_FIPS
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2m-fips  2 Nov 2017"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2n-fips-dev  xx XXX xxxx"
 # else
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2m  2 Nov 2017"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2n-dev  xx XXX xxxx"
 # endif
 # define OPENSSL_VERSION_PTEXT   " part of " OPENSSL_VERSION_TEXT
 
