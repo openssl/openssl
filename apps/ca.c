29b1795f72d071d7f75fda5b5df5e8f8b7581a4f
Pavel Kopyl <p.kopyl@samsung.com>
2017-11-03 18:18:59 +0300

    Fix memory leaks in CA related functions.

--- apps/ca.c
+++ apps/ca.c
@@ -937,10 +937,13 @@ end_of_options:
             if (j > 0) {
                 total_done++;
                 BIO_printf(bio_err, "\n");
-                if (!BN_add_word(serial, 1))
+                if (!BN_add_word(serial, 1)) {
+                    X509_free(x);
                     goto end;
+                }
                 if (!sk_X509_push(cert_sk, x)) {
                     BIO_printf(bio_err, "Memory allocation failure\n");
+                    X509_free(x);
                     goto end;
                 }
             }
@@ -1859,7 +1862,7 @@ static int do_body(X509 **xret, EVP_PKEY *pkey, X509 *x509,
     irow = NULL;
     ok = 1;
  end:
-    if (irow != NULL) {
+    if (irow != NULL || ok <= 0) {
         for (i = 0; i < DB_NUMBER; i++)
             OPENSSL_free(row[i]);
         OPENSSL_free(irow);
