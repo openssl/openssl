e22ce0d07496c53cd100c6d46a54d7a46c13bba9
Pavel Kopyl <p.kopyl@samsung.com>
2017-11-03 18:18:59 +0300

    Fix memory leaks in CA related functions.
    (cherry picked from commit aebd0e5ca12d1ba0b229a4121a54afa5ea2d8aa1)
    
    Reviewed-by: Andy Polyakov <appro@openssl.org>
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/6237)

--- crypto/conf/conf_api.c
+++ crypto/conf/conf_api.c
@@ -205,10 +205,14 @@ CONF_VALUE *_CONF_new_section(CONF *conf, const char *section)
 
     vv = lh_CONF_VALUE_insert(conf->data, v);
     OPENSSL_assert(vv == NULL);
+    if (lh_CONF_VALUE_error(conf->data) > 0)
+        goto err;
     return v;
 
  err:
     sk_CONF_VALUE_free(sk);
+    if (v != NULL)
+        OPENSSL_free(v->section);
     OPENSSL_free(v);
     return NULL;
 }
