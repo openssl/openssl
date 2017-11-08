4258d879d2d6c8865eb3f28090b88f3c86980354
Matt Caswell <matt@openssl.org>
2017-11-08 11:18:00 +0000

    Remove TLSv1.3 specific write transition for ClientHello
    Since we no longer do version negotiation during the processing of an HRR
    we do not need the TLSv1.3 specific write transition for ClientHello

--- ssl/statem/statem_clnt.c
+++ ssl/statem/statem_clnt.c
@@ -391,10 +391,6 @@ static WRITE_TRAN ossl_statem_client13_write_transition(SSL *s)
                  ERR_R_INTERNAL_ERROR);
         return WRITE_TRAN_ERROR;
 
-    case TLS_ST_CW_CLNT_HELLO:
-        /* We only hit this in the case of HelloRetryRequest */
-        return WRITE_TRAN_FINISHED;
-
     case TLS_ST_CR_FINISHED:
         if (s->early_data_state == SSL_EARLY_DATA_WRITE_RETRY
                 || s->early_data_state == SSL_EARLY_DATA_FINISHED_WRITING)
