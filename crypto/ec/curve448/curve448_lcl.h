4ea41daa073af5fb45dacc7e380248e3a108c63d
Matt Caswell <matt@openssl.org>
2017-11-27 11:32:03 +0000

    Add Ed448 tests
    Reviewed-by: Bernd Edlinger <bernd.edlinger@hotmail.de>
    (Merged from https://github.com/openssl/openssl/pull/5105)

--- crypto/ec/curve448/curve448_lcl.h
+++ crypto/ec/curve448/curve448_lcl.h
@@ -5,3 +5,23 @@ int X448(uint8_t out_shared_key[56], const uint8_t private_key[56],
 
 void X448_public_from_private(uint8_t out_public_value[56],
                               const uint8_t private_key[56]);
+
+int ED448_sign(uint8_t *out_sig, const uint8_t *message, size_t message_len,
+               const uint8_t public_key[56], const uint8_t private_key[56],
+               const uint8_t *context, size_t context_len);
+
+int ED448_verify(const uint8_t *message, size_t message_len,
+                 const uint8_t signature[112], const uint8_t public_key[56],
+                 const uint8_t *context, size_t context_len);
+
+int ED448ph_sign(uint8_t *out_sig, const uint8_t *message, size_t message_len,
+                 const uint8_t public_key[56], const uint8_t private_key[56],
+                 const uint8_t *context, size_t context_len);
+
+
+int ED448ph_verify(const uint8_t *message, size_t message_len,
+                   const uint8_t signature[112], const uint8_t public_key[56],
+                   const uint8_t *context, size_t context_len);
+
+void ED448_public_from_private(uint8_t out_public_key[56],
+                               const uint8_t private_key[56]);
