cf2f89d80a5b99e7fa23aa4d1ff1d15456057bda
Matt Caswell <matt@openssl.org>
2017-11-28 18:23:51 +0000

    Fix the ED448 key lengths
    Unlike X448 the key lengths for ED448 are 57 bytes (as opposed to 56)

--- crypto/ec/curve448/curve448_lcl.h
+++ crypto/ec/curve448/curve448_lcl.h
@@ -7,20 +7,20 @@ void X448_public_from_private(uint8_t out_public_value[56],
                               const uint8_t private_key[56]);
 
 int ED448_sign(uint8_t *out_sig, const uint8_t *message, size_t message_len,
-               const uint8_t public_key[56], const uint8_t private_key[56],
+               const uint8_t public_key[57], const uint8_t private_key[57],
                const uint8_t *context, size_t context_len);
 
 int ED448_verify(const uint8_t *message, size_t message_len,
-                 const uint8_t signature[112], const uint8_t public_key[56],
+                 const uint8_t signature[114], const uint8_t public_key[57],
                  const uint8_t *context, size_t context_len);
 
 int ED448ph_sign(uint8_t *out_sig, const uint8_t hash[64],
-                 const uint8_t public_key[56], const uint8_t private_key[56],
+                 const uint8_t public_key[57], const uint8_t private_key[57],
                  const uint8_t *context, size_t context_len);
 
-int ED448ph_verify(const uint8_t hash[64], const uint8_t signature[112],
-                   const uint8_t public_key[56], const uint8_t *context,
+int ED448ph_verify(const uint8_t hash[64], const uint8_t signature[114],
+                   const uint8_t public_key[57], const uint8_t *context,
                    size_t context_len);
 
-int ED448_public_from_private(uint8_t out_public_key[56],
-                              const uint8_t private_key[56]);
+int ED448_public_from_private(uint8_t out_public_key[57],
+                              const uint8_t private_key[57]);
