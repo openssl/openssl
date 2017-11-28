6ea71cbabe96c5382dda51553271570535d94cbd
Matt Caswell <matt@openssl.org>
2017-11-28 09:53:58 +0000

    Add tests for Ed448ph
    Reviewed-by: Bernd Edlinger <bernd.edlinger@hotmail.de>
    (Merged from https://github.com/openssl/openssl/pull/5105)

--- crypto/ec/curve448/curve448_lcl.h
+++ crypto/ec/curve448/curve448_lcl.h
@@ -1,4 +1,7 @@
 #include "curve448utils.h"
+#include "shake.h"
+
+#define decaf_ed448_prehash_ctx_t   decaf_shake256_ctx_t
 
 int X448(uint8_t out_shared_key[56], const uint8_t private_key[56],
          const uint8_t peer_public_value[56]);
@@ -14,14 +17,13 @@ int ED448_verify(const uint8_t *message, size_t message_len,
                  const uint8_t signature[112], const uint8_t public_key[56],
                  const uint8_t *context, size_t context_len);
 
-int ED448ph_sign(uint8_t *out_sig, const uint8_t *message, size_t message_len,
+int ED448ph_sign(uint8_t *out_sig, const uint8_t hash[64],
                  const uint8_t public_key[56], const uint8_t private_key[56],
                  const uint8_t *context, size_t context_len);
 
-
-int ED448ph_verify(const uint8_t *message, size_t message_len,
-                   const uint8_t signature[112], const uint8_t public_key[56],
-                   const uint8_t *context, size_t context_len);
+int ED448ph_verify(const uint8_t hash[64], const uint8_t signature[112],
+                   const uint8_t public_key[56], const uint8_t *context,
+                   size_t context_len);
 
 void ED448_public_from_private(uint8_t out_public_key[56],
                                const uint8_t private_key[56]);
