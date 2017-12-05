aea06898ad91ee56c046e425649b969d442f270a
林胜恩 <shengen.lin@baishancloud.com>
2017-12-05 12:00:29 +0000

    remove copyright info

--- include/openssl/evp.h
+++ include/openssl/evp.h
@@ -1,6 +1,5 @@
 /*
  * Copyright 1995-2017 The OpenSSL Project Authors. All Rights Reserved.
- * Copyright 2017 BaishanCloud. All Rights Reserved.
  *
  * Licensed under the OpenSSL license (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
