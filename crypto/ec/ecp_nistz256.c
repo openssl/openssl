3c7d0945b655a71a026bd0d855943037d8ce2bec
Richard Levitte <levitte@openssl.org>
2018-01-09 05:49:01 +0100

    Update copyright years on all files merged since Jan 1st 2018
    Reviewed-by: Tim Hudson <tjh@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/5038)

--- crypto/ec/ecp_nistz256.c
+++ crypto/ec/ecp_nistz256.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2014-2018 The OpenSSL Project Authors. All Rights Reserved.
  * Copyright (c) 2014, Intel Corporation. All Rights Reserved.
  * Copyright (c) 2015, CloudFlare, Inc.
  *
