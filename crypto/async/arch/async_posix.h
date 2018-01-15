ed54856765976841faa4fc41e8a09d14f0724dee
Richard Levitte <levitte@openssl.org>
2018-01-15 19:04:17 +0100

    Simplify Cygwin checks, part 1
    Because OPENSSL_SYS_CYGWIN will keep OPENSSL_SYS_UNIX defined, there's
    no point having checks of this form:
    
        #if (defined(OPENSSL_SYS_UNIX) || defined(OPENSSL_SYS_CYGWIN))
    
    Reviewed-by: Andy Polyakov <appro@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/5060)
    
    (cherry picked from commit 6ce66ce073b19af46fd94f4eefdb7c4772d7dd6f)

--- crypto/async/arch/async_posix.h
+++ crypto/async/arch/async_posix.h
@@ -11,7 +11,7 @@
 #define OPENSSL_ASYNC_ARCH_ASYNC_POSIX_H
 #include <openssl/e_os2.h>
 
-#if (defined(OPENSSL_SYS_UNIX) || defined(OPENSSL_SYS_CYGWIN)) \
+#if defined(OPENSSL_SYS_UNIX) \
     && defined(OPENSSL_THREADS) && !defined(OPENSSL_NO_ASYNC) \
     && !defined(__ANDROID__) && !defined(__OpenBSD__)
 
