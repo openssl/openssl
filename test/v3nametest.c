193366d4d35b94cc9e57336c47a59e7f4987c2e2
Richard Levitte <levitte@openssl.org>
2018-01-22 14:39:34 +0100

    fixup! Reduce the use of e_os.h in test programs

--- test/v3nametest.c
+++ test/v3nametest.c
@@ -8,10 +8,16 @@
  */
 
 #include <string.h>
-#include "internal/nelem.h"
+
+#include <openssl/e_os2.h>
 #include <openssl/x509.h>
 #include <openssl/x509v3.h>
+#include "internal/nelem.h"
 #include "testutil.h"
+
+#ifdef OPENSSL_SYS_WINDOWS
+# define strcasecmp _stricmp
+#endif
 
 static const char *const names[] = {
     "a", "b", ".", "*", "@",
