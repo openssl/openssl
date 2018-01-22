d19bf137ba24ce15ceb1e37e0093f1b7242340f1
Todd Short <tshort@akamai.com>
2018-01-22 14:30:24 -0500

    Fix error-path memory leak in asn_mime.c
    Reviewed-by: Bernd Edlinger <bernd.edlinger@hotmail.de>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/5142)
    
    (cherry picked from commit a26dd465b21d8def440c16b6bd90227b03e12e02)

--- crypto/asn1/asn_mime.c
+++ crypto/asn1/asn_mime.c
@@ -473,6 +473,7 @@ ASN1_VALUE *SMIME_read_ASN1(BIO *bio, BIO **bcont, const ASN1_ITEM *it)
         if (!(hdr = mime_hdr_find(headers, "content-type")) || !hdr->value) {
             sk_MIME_HEADER_pop_free(headers, mime_hdr_free);
             ASN1err(ASN1_F_SMIME_READ_ASN1, ASN1_R_NO_SIG_CONTENT_TYPE);
+            sk_BIO_pop_free(parts, BIO_vfree);
             return NULL;
         }
 
