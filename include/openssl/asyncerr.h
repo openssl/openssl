0f585c0ed61d45f26aa436c3c7d99385ac8cccfd
Richard Levitte <levitte@openssl.org>
2018-02-07 19:51:55 +0100

    fixup! make update ERROR_REBUILD=-rebuild

--- include/openssl/asyncerr.h
+++ include/openssl/asyncerr.h
@@ -11,35 +11,30 @@
 #ifndef HEADER_ASYNCERR_H
 # define HEADER_ASYNCERR_H
 
-# include <openssl/opensslconf.h>
-
-# ifndef OPENSSL_NO_ASYNC
-
-#  ifdef  __cplusplus
+# ifdef  __cplusplus
 extern "C" {
-#  endif
+# endif
 int ERR_load_ASYNC_strings(void);
-#  ifdef  __cplusplus
+# ifdef  __cplusplus
 }
-#  endif
+# endif
 
 /*
  * ASYNC function codes.
  */
-#  define ASYNC_F_ASYNC_CTX_NEW                            100
-#  define ASYNC_F_ASYNC_INIT_THREAD                        101
-#  define ASYNC_F_ASYNC_JOB_NEW                            102
-#  define ASYNC_F_ASYNC_PAUSE_JOB                          103
-#  define ASYNC_F_ASYNC_START_FUNC                         104
-#  define ASYNC_F_ASYNC_START_JOB                          105
+# define ASYNC_F_ASYNC_CTX_NEW                            100
+# define ASYNC_F_ASYNC_INIT_THREAD                        101
+# define ASYNC_F_ASYNC_JOB_NEW                            102
+# define ASYNC_F_ASYNC_PAUSE_JOB                          103
+# define ASYNC_F_ASYNC_START_FUNC                         104
+# define ASYNC_F_ASYNC_START_JOB                          105
 
 /*
  * ASYNC reason codes.
  */
-#  define ASYNC_R_FAILED_TO_SET_POOL                       101
-#  define ASYNC_R_FAILED_TO_SWAP_CONTEXT                   102
-#  define ASYNC_R_INIT_FAILED                              105
-#  define ASYNC_R_INVALID_POOL_SIZE                        103
+# define ASYNC_R_FAILED_TO_SET_POOL                       101
+# define ASYNC_R_FAILED_TO_SWAP_CONTEXT                   102
+# define ASYNC_R_INIT_FAILED                              105
+# define ASYNC_R_INVALID_POOL_SIZE                        103
 
-# endif
 #endif
