8a0beb5b9410fd806b684acce6afa85df85910fe
Rich Salz <rsalz@openssl.org>
2018-02-10 18:04:19 -0500

    Remove RMD160 assembler

--- crypto/ripemd/rmd_dgst.c
+++ crypto/ripemd/rmd_dgst.c
@@ -11,12 +11,7 @@
 #include "rmd_locl.h"
 #include <openssl/opensslv.h>
 
-#ifdef RMD160_ASM
-void ripemd160_block_x86(RIPEMD160_CTX *c, unsigned long *p, size_t num);
-# define ripemd160_block ripemd160_block_x86
-#else
 void ripemd160_block(RIPEMD160_CTX *c, unsigned long *p, size_t num);
-#endif
 
 int RIPEMD160_Init(RIPEMD160_CTX *c)
 {
--- crypto/ripemd/rmd_locl.h
+++ crypto/ripemd/rmd_locl.h
@@ -12,16 +12,6 @@
 #include <openssl/opensslconf.h>
 #include <openssl/ripemd.h>
 
-/*
- * DO EXAMINE COMMENTS IN crypto/md5/md5_locl.h & crypto/md5/md5_dgst.c
- * FOR EXPLANATIONS ON FOLLOWING "CODE."
- */
-#ifdef RMD160_ASM
-# if defined(__i386) || defined(__i386__) || defined(_M_IX86)
-#  define ripemd160_block_data_order ripemd160_block_asm_data_order
-# endif
-#endif
-
 void ripemd160_block_data_order(RIPEMD160_CTX *c, const void *p, size_t num);
 
 #define DATA_ORDER_IS_LITTLE_ENDIAN
