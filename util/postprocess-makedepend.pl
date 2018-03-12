f1a4de30b24c88199004a91a88fabda1964f5021
Richard Levitte <levitte@openssl.org>
2018-03-12 09:18:44 +0100

    Streamline dependency generation
    It seems that only gcc -MMD produces dependency files that are "sane"
    for our needs.  For all other methods, some post processing is needed:
    
    - 'makedepend' (Unix) insists that object files are located in the
      same spot as the source file.
    - 'cl /Zs /showIncludes' (Visual C) has "Note: including file: " where
      we'd like to see the object.
    - 'CC/DECC' (VMS) insists that the object file is located in the
      current directory, i.e. it strips away all directory information.
    
    So far, we've managed this (except for the VMS case) with individual
    uncommented perl command lines directly in the build file template.
    We're now collecting these diverse hacks into one perl script that
    takes an argument to tell what kind of input to expect and that
    massages whatever it gets on STDIN and outputs the result on STDOUT.

---
+++ util/postprocess-makedepend.pl
@@ -0,0 +1,75 @@
+#! /usr/bin/env perl
+# Copyright 2018 The OpenSSL Project Authors. All Rights Reserved.
+#
+# Licensed under the OpenSSL license (the "License").  You may not use
+# this file except in compliance with the License.  You can obtain a copy
+# in the file LICENSE in the source distribution or at
+# https://www.openssl.org/source/license.html
+
+my $producer = $ARGV[0];
+
+while (<STDIN>) {
+    if ($producer eq 'makedepend') {
+
+        # makedepend, in its infinite wisdom, wants to have the object file in
+        # the same directory as the source file.  This doesn't work too well
+        # with out-of-source-tree builds, so we must resort to tricks to get
+        # things right.  The trick is to call makedepend with an extra suffix
+        # that contains the desired object file path, like this:
+        #
+        #     makedepend -f- -o"|dir/foo.o" -- $(CFLAGS) -- ../somewhere/foo.c
+        #
+        # The result will look something like this:
+        #
+        #     ../somewhere/foo|dir/foo.o: deps...
+        #
+        # Which is easy to massage by removing everything up to the first |
+
+        # Remove everything up to the first |
+        s/^.*\|//;
+        # Also, remove any dependency that starts with a /, because those are
+        # typically system headers
+        s/ \/(\\.|[^ ])*//;
+        # Finally, remove all empty lines or comment lines
+        $_ = undef if (/: *$/ || /^(#.*| *)$/);
+        $_.="\n" unless !defined($_) or /\R$/g;
+
+    } elsif ($producer eq 'VMS C') {
+
+        # current versions of DEC / Compaq / HP / VSI C strips away all
+        # directory information from the object file, so we must insert it
+        # back. Just to be safe against future changes, we check that there
+        # really is no directory information.
+
+        my $directory = $ARGV[1];
+
+        # The pattern for target and dependencies will always take this form:
+        #
+        #     target SPACE : SPACE deps
+        #
+        # This is so a volume delimiter (a : without any spaces around it)
+        # won't get mixed up with the target / deps delimiter.  We use this
+        # fact in the regexp below to make sure we do look at the target.
+        s/^/$directory/ unless /^\S+[:<\]]\S+\s+:/;
+
+    } elsif ($producer eq 'Windows C') {
+
+        # For the moment, we only know of one native Windows C compiler, and
+        # that's Visual C.  With that compiler, the flags /Zs /showIncludes
+        # give us the necessary output to be able to create dependencies that
+        # nmake (or any 'make' implementation) should be able to read, with a
+        # bit of help.  The output we're interested in looks like this (it
+        # always starts the same)
+        #
+        #     Note: including file: {whatever header file}
+        #
+        # So all we really have to do is to is to replace the start of the line
+        # with an object file specification, given to us as $ARGV[1].
+
+        my $object = $ARGV[1];
+        s/^Note: including file: */${object}: /;
+
+    }
+} continue {
+    print or die "$!\n";
+}
