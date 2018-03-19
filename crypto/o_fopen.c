ab50cf0db0782f891a9c8bda19bacef5d7bcc258
Andy Polyakov <appro@openssl.org>
2018-03-19 22:37:28 +0100

    crypto/o_fopen.c: revert more [to be squashed].

--- crypto/o_fopen.c
+++ crypto/o_fopen.c
@@ -47,7 +47,7 @@ FILE *openssl_fopen(const char *filename, const char *mode)
         if (MultiByteToWideChar(CP_UTF8, flags,
                                 filename, len_0, wfilename, sz) &&
             MultiByteToWideChar(CP_UTF8, 0, mode, strlen(mode) + 1,
-                                wmode, sizeof(wmode) / sizeof(wmode[0])) &&
+                                wmode, OSSL_NELEM(wmode)) &&
             (file = _wfopen(wfilename, wmode)) == NULL &&
             (errno == ENOENT || errno == EBADF)
             ) {
