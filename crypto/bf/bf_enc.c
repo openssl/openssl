4a196c01e04c17d9349c32f0d6e8041141bc4108
Arthur Williams <echocode@protonmail.com>
2018-03-20 16:56:00 +0000

    Modified the BF_encrypt and BF_decrypt
    Modified the BF_encrypt and BF_decrypt to allow any value to be used for BF_ROUNDS. The code is also a lot better syntaxually, as it coverts 40 copy and pasted lines into 2 for loops.

--- crypto/bf/bf_enc.c
+++ crypto/bf/bf_enc.c
@@ -16,10 +16,6 @@
  * SECURITY WORKSHOP, CAMBRIDGE, U.K., DECEMBER 9-11, 1993)
  */
 
-#if (BF_ROUNDS != 16) && (BF_ROUNDS != 20)
-# error If you set BF_ROUNDS to some value other than 16 or 20, you will have \
-to modify the code.
-#endif
 
 void BF_encrypt(BF_LONG *data, const BF_KEY *key)
 {
@@ -32,28 +28,20 @@ void BF_encrypt(BF_LONG *data, const BF_KEY *key)
     r = data[1];
 
     l ^= p[0];
-    BF_ENC(r, l, s, p[1]);
-    BF_ENC(l, r, s, p[2]);
-    BF_ENC(r, l, s, p[3]);
-    BF_ENC(l, r, s, p[4]);
-    BF_ENC(r, l, s, p[5]);
-    BF_ENC(l, r, s, p[6]);
-    BF_ENC(r, l, s, p[7]);
-    BF_ENC(l, r, s, p[8]);
-    BF_ENC(r, l, s, p[9]);
-    BF_ENC(l, r, s, p[10]);
-    BF_ENC(r, l, s, p[11]);
-    BF_ENC(l, r, s, p[12]);
-    BF_ENC(r, l, s, p[13]);
-    BF_ENC(l, r, s, p[14]);
-    BF_ENC(r, l, s, p[15]);
-    BF_ENC(l, r, s, p[16]);
-# if BF_ROUNDS == 20
-    BF_ENC(r, l, s, p[17]);
-    BF_ENC(l, r, s, p[18]);
-    BF_ENC(r, l, s, p[19]);
-    BF_ENC(l, r, s, p[20]);
-# endif
+
+    int i;
+    for (i = 1; i < BF_ROUNDS + 1; i++)
+    {
+        if (i % 2 == 1) /* if i is odd */
+        {
+            BF_ENC(r, l, s, p[i]);
+        }
+        else
+        {
+            BF_ENC(l, r, s, p[i]);
+        }
+    }
+
     r ^= p[BF_ROUNDS + 1];
 
     data[1] = l & 0xffffffffU;
@@ -71,28 +59,19 @@ void BF_decrypt(BF_LONG *data, const BF_KEY *key)
     r = data[1];
 
     l ^= p[BF_ROUNDS + 1];
-#  if BF_ROUNDS == 20
-    BF_ENC(r, l, s, p[20]);
-    BF_ENC(l, r, s, p[19]);
-    BF_ENC(r, l, s, p[18]);
-    BF_ENC(l, r, s, p[17]);
-#  endif
-    BF_ENC(r, l, s, p[16]);
-    BF_ENC(l, r, s, p[15]);
-    BF_ENC(r, l, s, p[14]);
-    BF_ENC(l, r, s, p[13]);
-    BF_ENC(r, l, s, p[12]);
-    BF_ENC(l, r, s, p[11]);
-    BF_ENC(r, l, s, p[10]);
-    BF_ENC(l, r, s, p[9]);
-    BF_ENC(r, l, s, p[8]);
-    BF_ENC(l, r, s, p[7]);
-    BF_ENC(r, l, s, p[6]);
-    BF_ENC(l, r, s, p[5]);
-    BF_ENC(r, l, s, p[4]);
-    BF_ENC(l, r, s, p[3]);
-    BF_ENC(r, l, s, p[2]);
-    BF_ENC(l, r, s, p[1]);
+
+    int i;
+    for (i = BF_ROUNDS; i > 0; i++)
+    {
+        if (1 % 2 == 1) /* if i is odd */
+        {
+            BF_ENC(l, r, s, p[i]);
+        }
+        else
+        {
+            BF_ENC(r, l, s, p[i]);
+        }
+    }
     r ^= p[0];
 
     data[1] = l & 0xffffffffU;
@@ -107,11 +86,13 @@ void BF_cbc_encrypt(const unsigned char *in, unsigned char *out, long length,
     register long l = length;
     BF_LONG tin[2];
 
-    if (encrypt) {
+    if (encrypt) 
+    {
         n2l(ivec, tout0);
         n2l(ivec, tout1);
         ivec -= 8;
-        for (l -= 8; l >= 0; l -= 8) {
+        for (l -= 8; l >= 0; l -= 8) 
+        {
             n2l(in, tin0);
             n2l(in, tin1);
             tin0 ^= tout0;
@@ -124,7 +105,8 @@ void BF_cbc_encrypt(const unsigned char *in, unsigned char *out, long length,
             l2n(tout0, out);
             l2n(tout1, out);
         }
-        if (l != -8) {
+        if (l != -8) 
+        {
             n2ln(in, tin0, tin1, l + 8);
             tin0 ^= tout0;
             tin1 ^= tout1;
@@ -138,11 +120,14 @@ void BF_cbc_encrypt(const unsigned char *in, unsigned char *out, long length,
         }
         l2n(tout0, ivec);
         l2n(tout1, ivec);
-    } else {
+    } 
+    else 
+    {
         n2l(ivec, xor0);
         n2l(ivec, xor1);
         ivec -= 8;
-        for (l -= 8; l >= 0; l -= 8) {
+        for (l -= 8; l >= 0; l -= 8) 
+        {
             n2l(in, tin0);
             n2l(in, tin1);
             tin[0] = tin0;
@@ -155,7 +140,8 @@ void BF_cbc_encrypt(const unsigned char *in, unsigned char *out, long length,
             xor0 = tin0;
             xor1 = tin1;
         }
-        if (l != -8) {
+        if (l != -8) 
+        {
             n2l(in, tin0);
             n2l(in, tin1);
             tin[0] = tin0;
