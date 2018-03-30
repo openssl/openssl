e25ac9ba9679ec5b877dfd66161703f070a57d71
Matt Caswell <matt@openssl.org>
2018-03-30 14:33:55 +0100

    GOST MAC algorithms don't support EVP_PKEY_new_raw_private_key()
    We should use the old EVP_PKEY_new_mac_key() instead.

--- ssl/t1_enc.c
+++ ssl/t1_enc.c
@@ -257,7 +257,7 @@ int tls1_change_cipher_state(SSL *s, int which)
 
     if (!(EVP_CIPHER_flags(c) & EVP_CIPH_FLAG_AEAD_CIPHER)) {
         /* TODO(size_t): Convert this function */
-        mac_key = EVP_PKEY_new_raw_private_key(mac_type, NULL, mac_secret,
+        mac_key = EVP_PKEY_new_mac_key(mac_type, NULL, mac_secret,
                                                (int)*mac_secret_size);
         if (mac_key == NULL
             || EVP_DigestSignInit(mac_ctx, NULL, m, NULL, mac_key) <= 0) {
