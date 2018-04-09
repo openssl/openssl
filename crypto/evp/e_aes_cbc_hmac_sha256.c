eec0ac99cc346107998ab4c3e073860157b5162b
scauligi <scauligi@eng.ucsd.edu>
2018-04-08 21:55:28 -0700

    Simplify unnecessary computation
    The removed code caps "maxpad" at at most 255 without leaking its value
    via side channels. However, the value of "maxpad" is computed entirely
    from data that would already be known to any attacker, and so the bit
    hacker serves only to needlessly obfuscate the code.
    
    CLA: trivial

--- crypto/evp/e_aes_cbc_hmac_sha256.c
+++ crypto/evp/e_aes_cbc_hmac_sha256.c
@@ -537,8 +537,7 @@ static int aesni_cbc_hmac_sha256_cipher(EVP_CIPHER_CTX *ctx,
             /* figure out payload length */
             pad = out[len - 1];
             maxpad = len - (SHA256_DIGEST_LENGTH + 1);
-            maxpad |= (255 - maxpad) >> (sizeof(maxpad) * 8 - 8);
-            maxpad &= 255;
+            maxpad = maxpad > 255 ? 255 : maxpad;
 
             mask = constant_time_ge(maxpad, pad);
             ret &= mask;
