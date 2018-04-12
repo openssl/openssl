2fb9900ca4b50964032174e7458d87c9a36f4817
Matthias Kraft <Matthias.Kraft@softwareag.com>
2018-04-12 12:25:27 +0200

    openssl#5668: corrections after compiling with -qinfo=all:als.
    The ongoing discussion about casting or not in PR #5626 had me compiling
    again with above mentioned flags. Indeed the compiler had to say something
    about it and I did these changes to silence it again.

--- crypto/dso/dso_dlfcn.c
+++ crypto/dso/dso_dlfcn.c
@@ -352,11 +352,14 @@ static int dladdr(void *addr, Dl_info *dl)
 
     do {
         this_ldi = next_ldi;
-        if (((addr >= this_ldi->ldinfo_textorg)
-             && (addr < (this_ldi->ldinfo_textorg + this_ldi->ldinfo_textsize)))
-            || ((addr >= this_ldi->ldinfo_dataorg)
-                && (addr <
-                    (this_ldi->ldinfo_dataorg + this_ldi->ldinfo_datasize)))) {
+        if ((((uintptr_t)addr >= (uintptr_t)this_ldi->ldinfo_textorg)
+             && ((uintptr_t)addr <
+                 ((uintptr_t)this_ldi->ldinfo_textorg +
+                     this_ldi->ldinfo_textsize)))
+            || (((uintptr_t)addr >= (uintptr_t)this_ldi->ldinfo_dataorg)
+                && ((uintptr_t)addr <
+                    ((uintptr_t)this_ldi->ldinfo_dataorg +
+                        this_ldi->ldinfo_datasize)))) {
             found = 1;
             /*
              * Ignoring the possibility of a member name and just returning
@@ -367,7 +370,8 @@ static int dladdr(void *addr, Dl_info *dl)
                  OPENSSL_strdup(this_ldi->ldinfo_filename)) == NULL)
                 errno = ENOMEM;
         } else {
-            next_ldi = (char *)this_ldi + this_ldi->ldinfo_next;
+            next_ldi =
+                (struct ld_info *)((uintptr_t)this_ldi + this_ldi->ldinfo_next);
         }
     } while (this_ldi->ldinfo_next && !found);
     OPENSSL_free((void *)ldinfos);
@@ -395,7 +399,7 @@ static int dlfcn_pathbyaddr(void *addr, char *path, int sz)
         len = (int)strlen(dli.dli_fname);
         if (sz <= 0) {
 #  ifdef _AIX
-            OPENSSL_free(dli.dli_fname);
+            OPENSSL_free((void *)dli.dli_fname);
 #  endif
             return len + 1;
         }
@@ -404,7 +408,7 @@ static int dlfcn_pathbyaddr(void *addr, char *path, int sz)
         memcpy(path, dli.dli_fname, len);
         path[len++] = 0;
 #  ifdef _AIX
-        OPENSSL_free(dli.dli_fname);
+        OPENSSL_free((void *)dli.dli_fname);
 #  endif
         return len;
     }
