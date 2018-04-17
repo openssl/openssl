8c9e466c2ccd06c27abd952146c4cd045efafd36
Steve Linsell <stevenx.linsell@intel.com>
2018-04-17 13:29:09 +0100

    Fix fibre stack to prevent memory corruption if stack overflows.

--- crypto/async/arch/async_posix.c
+++ crypto/async/arch/async_posix.c
@@ -13,9 +13,14 @@
 #ifdef ASYNC_POSIX
 
 # include <stddef.h>
+# include <string.h>
 # include <unistd.h>
+# include <sys/mman.h>
 
-#define STACKSIZE       32768
+# define STACKSIZE     32768
+# ifndef PAGE_SIZE
+#  define PAGE_SIZE    4096
+# endif
 
 int ASYNC_is_capable(void)
 {
@@ -34,25 +39,98 @@ void async_local_cleanup(void)
 
 int async_fibre_makecontext(async_fibre *fibre)
 {
+    size_t pagesize;
+    size_t stackallocsize;
+    size_t stacksize = STACKSIZE;
+    char *stackallocaddr = NULL;
+
+# if defined(_SC_PAGE_SIZE) || defined (_SC_PAGESIZE)
+    {
+#  if defined(_SC_PAGE_SIZE)
+        long tmppgsize = sysconf(_SC_PAGE_SIZE);
+#  else
+        long tmppgsize = sysconf(_SC_PAGESIZE);
+#  endif
+        if (tmppgsize < 1)
+            pagesize = PAGE_SIZE;
+        else
+            pagesize = (size_t)tmppgsize;
+    }
+# else
+    pagesize = PAGE_SIZE;
+# endif
+    if (pagesize > stacksize)
+        stacksize = pagesize;
+
+    stackallocsize = stacksize + 2 * pagesize;
+
     fibre->env_init = 0;
     if (getcontext(&fibre->fibre) == 0) {
-        fibre->fibre.uc_stack.ss_sp = OPENSSL_malloc(STACKSIZE);
-        if (fibre->fibre.uc_stack.ss_sp != NULL) {
-            fibre->fibre.uc_stack.ss_size = STACKSIZE;
+        if (posix_memalign((void **)&stackallocaddr,
+                           pagesize,
+                           stackallocsize) == 0) {
+            fibre->fibre.uc_stack.ss_sp = stackallocaddr + pagesize;
+            fibre->fibre.uc_stack.ss_size = stacksize;
             fibre->fibre.uc_link = NULL;
+            /* Make a best effort to create guard pages, lock the
+               stack into memory and prevent it getting dumped
+               on a core dump. */
+            mprotect(stackallocaddr,
+                     pagesize,
+                     PROT_NONE);
+            mprotect(stackallocaddr + pagesize + stacksize,
+                     pagesize,
+                     PROT_NONE);
+            mlock(fibre->fibre.uc_stack.ss_sp, stacksize);
+# ifdef MADV_DONTDUMP
+            madvise(fibre->fibre.uc_stack.ss_sp, stacksize, MADV_DONTDUMP);
+# endif
             makecontext(&fibre->fibre, async_start_func, 0);
             return 1;
         }
-    } else {
-        fibre->fibre.uc_stack.ss_sp = NULL;
     }
+    fibre->fibre.uc_stack.ss_sp = NULL;
     return 0;
 }
 
 void async_fibre_free(async_fibre *fibre)
 {
-    OPENSSL_free(fibre->fibre.uc_stack.ss_sp);
-    fibre->fibre.uc_stack.ss_sp = NULL;
+    size_t pagesize;
+    size_t stacksize = fibre->fibre.uc_stack.ss_size;
+    char *stackallocaddr = NULL;
+
+# if defined(_SC_PAGE_SIZE) || defined (_SC_PAGESIZE)
+    {
+#  if defined(_SC_PAGE_SIZE)
+        long tmppgsize = sysconf(_SC_PAGE_SIZE);
+#  else
+        long tmppgsize = sysconf(_SC_PAGESIZE);
+#  endif
+        if (tmppgsize < 1)
+            pagesize = PAGE_SIZE;
+        else
+            pagesize = (size_t)tmppgsize;
+    }
+# else
+    pagesize = PAGE_SIZE;
+# endif
+
+    if (fibre->fibre.uc_stack.ss_sp) {
+        stackallocaddr = (char *)fibre->fibre.uc_stack.ss_sp - pagesize;
+        /* Make a best effort to reverse the stack setup and guard pages.
+           Purposely not checking return values as there is nothing we
+           can do if they fail. */
+        madvise(fibre->fibre.uc_stack.ss_sp, stacksize, MADV_NORMAL);
+        munlock(fibre->fibre.uc_stack.ss_sp, stacksize);
+        mprotect(stackallocaddr,
+                 pagesize,
+                 PROT_READ|PROT_WRITE);
+        mprotect(stackallocaddr + pagesize + stacksize,
+                 pagesize,
+                 PROT_READ|PROT_WRITE);
+        free(stackallocaddr);
+        fibre->fibre.uc_stack.ss_sp = NULL;
+    }
 }
 
 #endif
