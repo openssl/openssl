2add645dfc364cfc609e6138de1bf43c217bb557
Matt Caswell <matt@openssl.org>
2018-04-18 11:07:18 +0100

    Fix ocsp app exit code
    If we run the ocsp command line app and the responder returns a
    non-successful status code then the app should exit with a failure code.
    
    Based on an original patch by Tatsuhiro Tsujikawa.
    
    Fixes #2387
    
    Reviewed-by: Rich Salz <rsalz@openssl.org>
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/5999)

--- apps/ocsp.c
+++ apps/ocsp.c
@@ -639,7 +639,6 @@ int ocsp_main(int argc, char **argv)
                    OCSP_response_status_str(i), i);
         if (ignore_err)
             goto redo_accept;
-        ret = 0;
         goto end;
     }
 
