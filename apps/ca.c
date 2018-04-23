25642ad29e6a2c15c10ceb5e4f029638f73a879e
Richard Levitte <levitte@openssl.org>
2018-04-23 10:22:56 +0200

    Fix openssl ca, to correctly make output file binary when using -spkac
    On Unix, this doesn't matter, but on other platforms, it may.
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/6050)

--- apps/ca.c
+++ apps/ca.c
@@ -722,8 +722,12 @@ end_of_options:
 
     /*****************************************************************/
     if (req || gencrl) {
-        /* FIXME: Is it really always text? */
-        Sout = bio_open_default(outfile, 'w', FORMAT_TEXT);
+        if (spkac_file != NULL) {
+            output_der = 1;
+            batch = 1;
+        }
+        Sout = bio_open_default(outfile, 'w',
+                                output_der ? FORMAT_ASN1 : FORMAT_TEXT);
         if (Sout == NULL)
             goto end;
     }
@@ -876,10 +880,6 @@ end_of_options:
                 if (!sk_X509_push(cert_sk, x)) {
                     BIO_printf(bio_err, "Memory allocation failure\n");
                     goto end;
-                }
-                if (outfile) {
-                    output_der = 1;
-                    batch = 1;
                 }
             }
         }
