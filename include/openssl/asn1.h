ebdeeb363f03848cea5028e63c6be294a34a8426
Dr. Matthias St. Pierre <Matthias.St.Pierre@ncp-e.com>
2018-04-26 20:36:41 +0200

    a_strex.c: prevent out of bound read in do_buf()
    which is used for ASN1_STRING_print_ex*() and X509_NAME_print_ex*().
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/6117)

--- include/openssl/asn1.h
+++ include/openssl/asn1.h
@@ -953,6 +953,7 @@ int ERR_load_ASN1_strings(void);
 # define ASN1_F_D2I_AUTOPRIVATEKEY                        207
 # define ASN1_F_D2I_PRIVATEKEY                            154
 # define ASN1_F_D2I_PUBLICKEY                             155
+# define ASN1_F_DO_BUF                                    142
 # define ASN1_F_DO_TCREATE                                222
 # define ASN1_F_I2D_ASN1_BIO_STREAM                       211
 # define ASN1_F_I2D_DSA_PUBKEY                            161
