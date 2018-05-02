777c075c2e9cd9b3f66beaff0b0422c27f3cfa80
Richard Levitte <levitte@openssl.org>
2018-05-02 09:17:09 +0200

    fixup! Add APIs for custom X509_LOOKUP_METHOD creation
    Note that these are code style fixes, and that some cases were not
    possible to have conform because of long symbols

--- crypto/x509/x509_meth.c
+++ crypto/x509/x509_meth.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 1995-2017 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2018 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the OpenSSL license (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
@@ -44,9 +44,8 @@ void X509_LOOKUP_meth_free(X509_LOOKUP_METHOD *method)
     OPENSSL_free(method);
 }
 
-int X509_LOOKUP_meth_set_new_item(
-    X509_LOOKUP_METHOD *method,
-    int (*new_item) (X509_LOOKUP *ctx))
+int X509_LOOKUP_meth_set_new_item(X509_LOOKUP_METHOD *method,
+                                  int (*new_item) (X509_LOOKUP *ctx))
 {
     method->new_item = new_item;
     return 1;
@@ -72,9 +71,8 @@ void (*X509_LOOKUP_meth_get_free(const X509_LOOKUP_METHOD* method))
     return method->free;
 }
 
-int X509_LOOKUP_meth_set_init(
-    X509_LOOKUP_METHOD *method,
-    int (*init) (X509_LOOKUP *ctx))
+int X509_LOOKUP_meth_set_init(X509_LOOKUP_METHOD *method,
+                              int (*init) (X509_LOOKUP *ctx))
 {
     method->init = init;
     return 1;
@@ -113,8 +111,7 @@ X509_LOOKUP_ctrl_fn X509_LOOKUP_meth_get_ctrl(const X509_LOOKUP_METHOD *method)
     return method->ctrl;
 }
 
-int X509_LOOKUP_meth_set_get_by_subject(
-    X509_LOOKUP_METHOD *method,
+int X509_LOOKUP_meth_set_get_by_subject(X509_LOOKUP_METHOD *method,
     X509_LOOKUP_get_by_subject_fn get_by_subject)
 {
     method->get_by_subject = get_by_subject;
@@ -128,23 +125,21 @@ X509_LOOKUP_get_by_subject_fn X509_LOOKUP_meth_get_get_by_subject(
 }
 
 
-int X509_LOOKUP_meth_set_get_by_issuer_serial(
-    X509_LOOKUP_METHOD *method,
+int X509_LOOKUP_meth_set_get_by_issuer_serial(X509_LOOKUP_METHOD *method,
     X509_LOOKUP_get_by_issuer_serial_fn get_by_issuer_serial)
 {
     method->get_by_issuer_serial = get_by_issuer_serial;
     return 1;
 }
 
-X509_LOOKUP_get_by_issuer_serial_fn X509_LOOKUP_meth_get_get_by_issuer_serial(
-    const X509_LOOKUP_METHOD *method)
+X509_LOOKUP_get_by_issuer_serial_fn
+    X509_LOOKUP_meth_get_get_by_issuer_serial(const X509_LOOKUP_METHOD *method)
 {
     return method->get_by_issuer_serial;
 }
 
 
-int X509_LOOKUP_meth_set_get_by_fingerprint(
-    X509_LOOKUP_METHOD *method,
+int X509_LOOKUP_meth_set_get_by_fingerprint(X509_LOOKUP_METHOD *method,
     X509_LOOKUP_get_by_fingerprint_fn get_by_fingerprint)
 {
     method->get_by_fingerprint = get_by_fingerprint;
@@ -157,9 +152,8 @@ X509_LOOKUP_get_by_fingerprint_fn X509_LOOKUP_meth_get_get_by_fingerprint(
     return method->get_by_fingerprint;
 }
 
-int X509_LOOKUP_meth_set_get_by_alias(
-    X509_LOOKUP_METHOD *method,
-    X509_LOOKUP_get_by_alias_fn get_by_alias)
+int X509_LOOKUP_meth_set_get_by_alias(X509_LOOKUP_METHOD *method,
+                                      X509_LOOKUP_get_by_alias_fn get_by_alias)
 {
     method->get_by_alias = get_by_alias;
     return 1;
