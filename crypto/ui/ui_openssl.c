a6dbdde4139f503cb8a661b8a18b3b19fe25915d
Richard Levitte <levitte@openssl.org>
2018-05-02 14:18:55 +0200

    UI console: Restore tty settings, do not force ECHO after prompt
    The Console UI method always set echo on after prompting without
    echo.  However, echo might not have been on originally, so just
    restore the original TTY settings.
    
    Fixes #2373

--- crypto/ui/ui_openssl.c
+++ crypto/ui/ui_openssl.c
@@ -190,8 +190,8 @@ static int read_string(UI *ui, UI_STRING *uis);
 static int write_string(UI *ui, UI_STRING *uis);
 
 static int open_console(UI *ui);
-static int echo_console(UI *ui);
 static int noecho_console(UI *ui);
+static int echo_console(UI *ui);
 static int close_console(UI *ui);
 
 /*
@@ -503,7 +503,6 @@ static int echo_console(UI *ui)
 {
 # if defined(TTY_set) && !defined(OPENSSL_SYS_VMS)
     memcpy(&(tty_new), &(tty_orig), sizeof(tty_orig));
-    tty_new.TTY_FLAGS |= ECHO;
 # endif
 
 # if defined(TTY_set) && !defined(OPENSSL_SYS_VMS)
@@ -513,7 +512,7 @@ static int echo_console(UI *ui)
 # ifdef OPENSSL_SYS_VMS
     if (is_a_tty) {
         tty_new[0] = tty_orig[0];
-        tty_new[1] = tty_orig[1] & ~TT$M_NOECHO;
+        tty_new[1] = tty_orig[1];
         tty_new[2] = tty_orig[2];
         status = sys$qiow(0, channel, IO$_SETMODE, &iosb, 0, 0, tty_new, 12,
                           0, 0, 0, 0);
@@ -534,7 +533,6 @@ static int echo_console(UI *ui)
 # if defined(_WIN32) && !defined(_WIN32_WCE)
     if (is_a_tty) {
         tty_new = tty_orig;
-        tty_new |= ENABLE_ECHO_INPUT;
         SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), tty_new);
     }
 # endif
