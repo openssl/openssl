a8858e8a2a481c2771ca16f1252cf4431b87bd57
Kurt Roeckx <kurt@roeckx.be>
2018-05-09 18:32:02 +0200

    rsaz_avx2_eligible doesn't take parameters

--- crypto/bn/rsaz_exp.h
+++ crypto/bn/rsaz_exp.h
@@ -28,7 +28,7 @@ void RSAZ_1024_mod_exp_avx2(BN_ULONG result[16],
                             const BN_ULONG exponent[16],
                             const BN_ULONG m_norm[16], const BN_ULONG RR[16],
                             BN_ULONG k0);
-int rsaz_avx2_eligible();
+int rsaz_avx2_eligible(void);
 
 void RSAZ_512_mod_exp(BN_ULONG result[8],
                       const BN_ULONG base_norm[8], const BN_ULONG exponent[8],
