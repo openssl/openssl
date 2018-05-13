ee4107a1cf29d63fa524b9c937fd23ef010ce743
Richard Levitte <levitte@openssl.org>
2018-05-13 21:49:22 +0200

    fixup! UI console: Restore tty settings, do not force ECHO after prompt

--- crypto/ui/ui_openssl.c
+++ crypto/ui/ui_openssl.c
@@ -318,8 +318,8 @@ static int read_string(UI *ui, UI_STRING *uis);
 static int write_string(UI *ui, UI_STRING *uis);
 
 static int open_console(UI *ui);
-static int noecho_console(UI *ui);
 static int echo_console(UI *ui);
+static int noecho_console(UI *ui);
 static int close_console(UI *ui);
 
 static UI_METHOD ui_openssl = {
