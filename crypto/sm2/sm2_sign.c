e6f2494376644bca9b1bc234e2fe0576b31df9ad
Jack Lloyd <jack.lloyd@ribose.com>
2018-05-17 18:57:13 -0400

    Fix bracket indentation

--- crypto/sm2/sm2_sign.c
+++ crypto/sm2/sm2_sign.c
@@ -28,44 +28,44 @@ static BIGNUM *SM2_compute_msg_hash(const EVP_MD *digest,
     BIGNUM *e = NULL;
 
     if (za == NULL)
-        {
+    {
         SM2err(SM2_F_SM2_COMPUTE_MSG_HASH, ERR_R_MALLOC_FAILURE);
         goto done;
-        }
+    }
 
     if (hash == NULL)
-        {
+    {
         SM2err(SM2_F_SM2_COMPUTE_MSG_HASH, ERR_R_MALLOC_FAILURE);
         goto done;
-        }
+    }
 
     if (SM2_compute_userid_digest(za, digest, user_id, key) == 0)
         goto done;
 
     if (EVP_DigestInit(hash, digest) == 0)
-        {
+    {
         SM2err(SM2_F_SM2_COMPUTE_MSG_HASH, ERR_R_EVP_LIB);
         goto done;
-        }
+    }
 
     if (EVP_DigestUpdate(hash, za, md_size) == 0)
-        {
+    {
         SM2err(SM2_F_SM2_COMPUTE_MSG_HASH, ERR_R_EVP_LIB);
         goto done;
-        }
+    }
 
     if (EVP_DigestUpdate(hash, msg, msg_len) == 0)
-        {
+    {
         SM2err(SM2_F_SM2_COMPUTE_MSG_HASH, ERR_R_EVP_LIB);
         goto done;
-        }
+    }
 
     /* reuse za buffer to hold H(ZA || M) */
     if (EVP_DigestFinal(hash, za, NULL) == 0)
-        {
+    {
         SM2err(SM2_F_SM2_COMPUTE_MSG_HASH, ERR_R_EVP_LIB);
         goto done;
-        }
+    }
 
     e = BN_bin2bn(za, md_size, NULL);
 
@@ -94,17 +94,17 @@ ECDSA_SIG *SM2_sig_gen(const EC_KEY *key, const BIGNUM *e)
 
     kG = EC_POINT_new(group);
     if (kG == NULL)
-        {
+    {
         SM2err(SM2_F_SM2_SIG_GEN, ERR_R_MALLOC_FAILURE);
         goto done;
-        }
+    }
 
     ctx = BN_CTX_new();
     if (ctx == NULL)
-        {
+    {
         SM2err(SM2_F_SM2_SIG_GEN, ERR_R_MALLOC_FAILURE);
         goto done;
-        }
+    }
 
     BN_CTX_start(ctx);
 
@@ -114,41 +114,41 @@ ECDSA_SIG *SM2_sig_gen(const EC_KEY *key, const BIGNUM *e)
     tmp = BN_CTX_get(ctx);
 
     if (tmp == NULL)
-        {
+    {
         SM2err(SM2_F_SM2_SIG_GEN, ERR_R_BN_LIB);
         goto done;
-        }
+    }
 
     /* These values are returned and so should not be allocated out of the context */
     r = BN_new();
     s = BN_new();
 
     if (r == NULL || s == NULL)
-        {
+    {
         SM2err(SM2_F_SM2_SIG_GEN, ERR_R_MALLOC_FAILURE);
         goto done;
-        }
+    }
 
     for (;;) {
         BN_priv_rand_range(k, order);
 
         if (EC_POINT_mul(group, kG, k, NULL, NULL, ctx) == 0)
-            {
+        {
             SM2err(SM2_F_SM2_SIG_GEN, ERR_R_EC_LIB);
             goto done;
-            }
+        }
 
         if (EC_POINT_get_affine_coordinates_GFp(group, kG, x1, NULL, ctx) == 0)
-            {
+        {
             SM2err(SM2_F_SM2_SIG_GEN, ERR_R_EC_LIB);
             goto done;
-            }
+        }
 
         if (BN_mod_add(r, e, x1, order, ctx) == 0)
-            {
+        {
             SM2err(SM2_F_SM2_SIG_GEN, ERR_R_BN_LIB);
             goto done;
-            }
+        }
 
         /* try again if r == 0 or r+k == n */
         if (BN_is_zero(r))
@@ -160,42 +160,42 @@ ECDSA_SIG *SM2_sig_gen(const EC_KEY *key, const BIGNUM *e)
             continue;
 
         if(BN_add(s, dA, BN_value_one()) == 0)
-           {
-           SM2err(SM2_F_SM2_SIG_GEN, ERR_R_BN_LIB);
-           goto done;
-           }
+        {
+            SM2err(SM2_F_SM2_SIG_GEN, ERR_R_BN_LIB);
+            goto done;
+        }
 
         if(BN_mod_inverse(s, s, order, ctx) == 0)
-           {
-           SM2err(SM2_F_SM2_SIG_GEN, ERR_R_BN_LIB);
-           goto done;
-           }
+        {
+            SM2err(SM2_F_SM2_SIG_GEN, ERR_R_BN_LIB);
+            goto done;
+        }
 
         if(BN_mod_mul(tmp, dA, r, order, ctx) == 0)
-           {
-           SM2err(SM2_F_SM2_SIG_GEN, ERR_R_BN_LIB);
-           goto done;
-           }
+        {
+            SM2err(SM2_F_SM2_SIG_GEN, ERR_R_BN_LIB);
+            goto done;
+        }
 
         if(BN_sub(tmp, k, tmp) == 0)
-           {
-           SM2err(SM2_F_SM2_SIG_GEN, ERR_R_BN_LIB);
-           goto done;
-           }
+        {
+            SM2err(SM2_F_SM2_SIG_GEN, ERR_R_BN_LIB);
+            goto done;
+        }
 
         if(BN_mod_mul(s, s, tmp, order, ctx) == 0)
-           {
-           SM2err(SM2_F_SM2_SIG_GEN, ERR_R_BN_LIB);
-           goto done;
-           }
+        {
+            SM2err(SM2_F_SM2_SIG_GEN, ERR_R_BN_LIB);
+            goto done;
+        }
 
         sig = ECDSA_SIG_new();
 
         if (sig == NULL)
-            {
+        {
             SM2err(SM2_F_SM2_SIG_GEN, ERR_R_MALLOC_FAILURE);
             goto done;
-            }
+        }
 
          /* takes ownership of r and s */
         ECDSA_SIG_set0(sig, r, s);
@@ -231,17 +231,17 @@ int SM2_sig_verify(const EC_KEY *key, const ECDSA_SIG *sig, const BIGNUM *e)
 
     ctx = BN_CTX_new();
     if (ctx == NULL)
-        {
+    {
         SM2err(SM2_F_SM2_SIG_VERIFY, ERR_R_MALLOC_FAILURE);
         goto done;
-        }
+    }
 
     pt = EC_POINT_new(group);
     if (pt == NULL)
-        {
+    {
         SM2err(SM2_F_SM2_SIG_VERIFY, ERR_R_MALLOC_FAILURE);
         goto done;
-        }
+    }
 
     BN_CTX_start(ctx);
 
@@ -249,10 +249,10 @@ int SM2_sig_verify(const EC_KEY *key, const ECDSA_SIG *sig, const BIGNUM *e)
     x1 = BN_CTX_get(ctx);
 
     if (x1 == NULL)
-       {
-       SM2err(SM2_F_SM2_SIG_VERIFY, ERR_R_MALLOC_FAILURE);
-       goto done;
-       }
+    {
+        SM2err(SM2_F_SM2_SIG_VERIFY, ERR_R_MALLOC_FAILURE);
+        goto done;
+    }
 
     /*
        B1: verify whether r' in [1,n-1], verification failed if not
@@ -267,46 +267,46 @@ int SM2_sig_verify(const EC_KEY *key, const ECDSA_SIG *sig, const BIGNUM *e)
     ECDSA_SIG_get0(sig, &r, &s);
 
     if ((BN_cmp(r, BN_value_one()) < 0) || (BN_cmp(s, BN_value_one()) < 0))
-        {
+    {
         SM2err(SM2_F_SM2_SIG_VERIFY, SM2_R_BAD_SIGNATURE);
         goto done;
-        }
+    }
 
     if ((BN_cmp(order, r) <= 0) || (BN_cmp(order, s) <= 0))
-        {
+    {
         SM2err(SM2_F_SM2_SIG_VERIFY, SM2_R_BAD_SIGNATURE);
         goto done;
-        }
+    }
 
     if (BN_mod_add(t, r, s, order, ctx) == 0)
-        {
+    {
         SM2err(SM2_F_SM2_SIG_VERIFY, ERR_R_BN_LIB);
         goto done;
-        }
+    }
 
     if (BN_is_zero(t) == 1)
-        {
+    {
         SM2err(SM2_F_SM2_SIG_VERIFY, SM2_R_BAD_SIGNATURE);
         goto done;
-        }
+    }
 
     if (EC_POINT_mul(group, pt, s, EC_KEY_get0_public_key(key), t, ctx) == 0)
-        {
+    {
         SM2err(SM2_F_SM2_SIG_VERIFY, ERR_R_EC_LIB);
         goto done;
-        }
+    }
 
     if (EC_POINT_get_affine_coordinates_GFp(group, pt, x1, NULL, ctx) == 0)
-        {
+    {
         SM2err(SM2_F_SM2_SIG_VERIFY, ERR_R_EC_LIB);
         goto done;
-        }
+    }
 
     if (BN_mod_add(t, e, x1, order, ctx) == 0)
-        {
+    {
         SM2err(SM2_F_SM2_SIG_VERIFY, ERR_R_BN_LIB);
         goto done;
-        }
+    }
 
     if (BN_cmp(r, t) == 0)
         ret = 1;
@@ -362,10 +362,10 @@ int SM2_sign(int type, const unsigned char *dgst, int dgstlen,
     int ret = -1;
 
     if (type != NID_sm3 || dgstlen != 32)
-       {
-       SM2err(SM2_F_SM2_SIGN, SM2_R_INVALID_DIGEST_TYPE);
-       goto done;
-       }
+    {
+        SM2err(SM2_F_SM2_SIGN, SM2_R_INVALID_DIGEST_TYPE);
+        goto done;
+    }
 
     e = BN_bin2bn(dgst, dgstlen, NULL);
 
@@ -392,29 +392,29 @@ int SM2_verify(int type, const unsigned char *dgst, int dgstlen,
     int ret = -1;
 
     if (type != NID_sm3)
-       {
-       SM2err(SM2_F_SM2_VERIFY, SM2_R_INVALID_DIGEST_TYPE);
-       goto done;
-       }
+    {
+        SM2err(SM2_F_SM2_VERIFY, SM2_R_INVALID_DIGEST_TYPE);
+        goto done;
+    }
 
     s = ECDSA_SIG_new();
     if (s == NULL)
-        {
+    {
         SM2err(SM2_F_SM2_VERIFY, ERR_R_MALLOC_FAILURE);
         goto done;
-        }
+    }
     if (d2i_ECDSA_SIG(&s, &p, sig_len) == NULL)
-        {
+    {
         SM2err(SM2_F_SM2_VERIFY, SM2_R_INVALID_ENCODING);
         goto done;
-        }
+    }
     /* Ensure signature uses DER and doesn't have trailing garbage */
     derlen = i2d_ECDSA_SIG(s, &der);
     if (derlen != sig_len || memcmp(sig, der, derlen) != 0)
-        {
+    {
         SM2err(SM2_F_SM2_VERIFY, SM2_R_INVALID_ENCODING);
         goto done;
-        }
+    }
 
     e = BN_bin2bn(dgst, dgstlen, NULL);
 
