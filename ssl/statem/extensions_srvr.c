f649aaa44bd2feff15a8ecccc09aa99ad2022d9e
Benjamin Kaduk <bkaduk@akamai.com>
2018-05-30 09:49:29 -0500

    Normalize SNI hostname handling for SSL and SSL_SESSION
    In particular, adhere to the rule that we must not modify any
    property of an SSL_SESSION object once it is (or might be) in
    a session cache.  Such modifications are thread-unsafe and have
    been observed to cause crashes at runtime.
    
    To effect this change, standardize on the property that
    SSL_SESSION->ext.hostname is set only when that SNI value
    has been negotiated by both parties for use with that session.
    For session resumption this is trivially the case, so only new
    handshakes are affected.
    
    On the client, the new semantics are that the SSL->ext.hostname is
    for storing the value configured by the caller, and this value is
    used when constructing the ClientHello.  On the server, SSL->ext.hostname
    is used to hold the value received from the client.  Only if the
    SNI negotiation is successful will the hostname be stored into the
    session object; the server can do this after it sends the ServerHello,
    and the client after it has received and processed the ServerHello.
    
    This obviates the need to remove the hostname from the session object
    in case of failed negotiation (a change that was introduced in commit
    9fb6cb810b769abbd60f11ef6e936a4e4456b19d in order to allow TLS 1.3
    early data when SNI was present in the ClientHello but not the session
    being resumed), which was modifying cached sessions in certain cases.
    (In TLS 1.3 we always produce a new SSL_SESSION object for new
    connections, even in the case of resumption, so no TLS 1.3 handshakes
    were affected.)

--- ssl/statem/extensions_srvr.c
+++ ssl/statem/extensions_srvr.c
@@ -142,9 +142,11 @@ int tls_parse_ctos_server_name(SSL *s, PACKET *pkt, unsigned int context,
             return 0;
         }
 
-        OPENSSL_free(s->session->ext.hostname);
-        s->session->ext.hostname = NULL;
-        if (!PACKET_strndup(&hostname, &s->session->ext.hostname)) {
+        /* Store the requested SNI in the SSL as temporary storage.
+         * If we accept it, it will get stored in the SSL_SESSION as well. */
+        OPENSSL_free(s->ext.hostname);
+        s->ext.hostname = NULL;
+        if (!PACKET_strndup(&hostname, &s->ext.hostname)) {
             SSLfatal(s, SSL_AD_INTERNAL_ERROR, SSL_F_TLS_PARSE_CTOS_SERVER_NAME,
                      ERR_R_INTERNAL_ERROR);
             return 0;
@@ -156,7 +158,7 @@ int tls_parse_ctos_server_name(SSL *s, PACKET *pkt, unsigned int context,
          * TODO(openssl-team): if the SNI doesn't match, we MUST
          * fall back to a full handshake.
          */
-        s->servername_done = s->session->ext.hostname
+        s->servername_done = (s->session->ext.hostname != NULL)
             && PACKET_equal(&hostname, s->session->ext.hostname,
                             strlen(s->session->ext.hostname));
 
@@ -1286,7 +1288,7 @@ EXT_RETURN tls_construct_stoc_server_name(SSL *s, WPACKET *pkt,
                                           size_t chainidx)
 {
     if (s->hit || s->servername_done != 1
-            || s->session->ext.hostname == NULL)
+            || s->ext.hostname == NULL)
         return EXT_RETURN_NOT_SENT;
 
     if (!WPACKET_put_bytes_u16(pkt, TLSEXT_TYPE_server_name)
