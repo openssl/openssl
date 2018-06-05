e82dc06b17b086e606a00fb2d16ffdd5c099f8da
Matt Caswell <matt@openssl.org>
2018-06-05 14:49:42 +0100

    Tidy up the Binary ECC Lambda Projective Co-ordinates patch
    Bring into current coding style and practices.

--- crypto/ec/ec2_lambda.c
+++ crypto/ec/ec2_lambda.c
@@ -1,4 +1,3 @@
-/* crypto/ec/ec2_lambda.c */
 /**
  * Binary curve arithmetic using lambda-projective coordinates.
  * Only supports short binary curve coeffs a=0 and a=1.
@@ -35,6 +34,7 @@ const EC_METHOD *EC_GF2m_lambda_method(void)
         ec_GF2m_simple_group_set_curve,
         ec_GF2m_simple_group_get_curve,
         ec_GF2m_simple_group_get_degree,
+        ec_group_simple_order_bits,
         ec_GF2m_simple_group_check_discriminant,
         ec_GF2m_simple_point_init,
         ec_GF2m_simple_point_finish,
@@ -55,9 +55,8 @@ const EC_METHOD *EC_GF2m_lambda_method(void)
         ec_GF2m_lambda_make_affine,
         ec_GF2m_simple_points_make_affine,
         0 /* mul defaults to ec_wNAF_mul */ ,
-        /* next two functions defined in ec2_mult.c */
-        ec_GF2m_precompute_mult,
-        ec_GF2m_have_precompute_mult,
+        0,
+        0,
         ec_GF2m_simple_field_mul,
         ec_GF2m_simple_field_sqr,
         ec_GF2m_simple_field_div,
@@ -79,38 +78,36 @@ const EC_METHOD *EC_GF2m_lambda_method(void)
 int ec_GF2m_lambda_dbl(const EC_GROUP *group, EC_POINT *r, const EC_POINT *a,
                        BN_CTX *ctx)
 {
-    if (EC_POINT_is_at_infinity(group, a)) {
-        return EC_POINT_set_to_infinity(group, r);
-    }
-
+    BIGNUM *t0, *t1, *t2, *t3;
     int ret = 0;
+
+    if (EC_POINT_is_at_infinity(group, a))
+        return EC_POINT_set_to_infinity(group, r);
+
     BN_CTX_start(ctx);
 
-    do {
-        BIGNUM *t0, *t1, *t2, *t3;
-        t0 = BN_CTX_get(ctx);
-        t1 = BN_CTX_get(ctx);
-        t2 = BN_CTX_get(ctx);
-        t3 = BN_CTX_get(ctx);
-        if (t3 == NULL) break;
-        if (!group->meth->field_sqr(group, t0, a->Y, ctx)) break;
-        if (!group->meth->field_mul(group, t3, a->Z, a->Y, ctx)) break;
-        if (!group->meth->field_sqr(group, t1, a->Z, ctx)) break;
-        if (!group->meth->field_mul(group, t2, a->X, a->Z, ctx)) break;
-        if (!BN_GF2m_add(t0, t3, t0)) break;
-        if (BN_is_one(group->a)) {
-            if (!BN_GF2m_add(t0, t0, t1)) break;
-        }
-        if (!group->meth->field_sqr(group, r->X, t0, ctx)) break;
-        if (!group->meth->field_mul(group, r->Z, t1, t0, ctx)) break;
-        if (!group->meth->field_mul(group, t0, t3, t0, ctx)) break;
-        if (!group->meth->field_sqr(group, t2, t2, ctx)) break;
-        if (!BN_GF2m_add(t0, r->Z, t0)) break;
-        if (!BN_GF2m_add(t0, t0, r->X)) break;
-        if (!BN_GF2m_add(r->Y, t0, t2)) break;
-        ret = 1;
-    } while(0);
+    t0 = BN_CTX_get(ctx);
+    t1 = BN_CTX_get(ctx);
+    t2 = BN_CTX_get(ctx);
+    t3 = BN_CTX_get(ctx);
+    if (t3 == NULL
+            || !group->meth->field_sqr(group, t0, a->Y, ctx)
+            || !group->meth->field_mul(group, t3, a->Z, a->Y, ctx)
+            || !group->meth->field_sqr(group, t1, a->Z, ctx)
+            || !group->meth->field_mul(group, t2, a->X, a->Z, ctx)
+            || !BN_GF2m_add(t0, t3, t0)
+            || (BN_is_one(group->a) && !BN_GF2m_add(t0, t0, t1))
+            || !group->meth->field_sqr(group, r->X, t0, ctx)
+            || !group->meth->field_mul(group, r->Z, t1, t0, ctx)
+            || !group->meth->field_mul(group, t0, t3, t0, ctx)
+            || !group->meth->field_sqr(group, t2, t2, ctx)
+            || !BN_GF2m_add(t0, r->Z, t0)
+            || !BN_GF2m_add(t0, t0, r->X)
+            || !BN_GF2m_add(r->Y, t0, t2))
+        goto done;
+    ret = 1;
 
+ done:
     r->Z_is_one = 0;
     BN_CTX_end(ctx);
     return ret;
@@ -125,48 +122,50 @@ int ec_GF2m_lambda_dbl(const EC_GROUP *group, EC_POINT *r, const EC_POINT *a,
  * @param b input point in lambda-affine
  * @param r output point in lambda-proj
  */
-int ec_GF2m_lambda_add_mixed(const EC_GROUP *group, EC_POINT *r, const EC_POINT *a,
-                       const EC_POINT *b, BN_CTX *ctx)
+static int ec_GF2m_lambda_add_mixed(const EC_GROUP *group, EC_POINT *r,
+                                    const EC_POINT *a, const EC_POINT *b,
+                                    BN_CTX *ctx)
 {
+    BIGNUM *t0, *t1, *t2, *t3;
     int ret = 0;
     BN_CTX_start(ctx);
 
-    do {
-        BIGNUM *t0, *t1, *t2, *t3;
-        t0 = BN_CTX_get(ctx);
-        t1 = BN_CTX_get(ctx);
-        t2 = BN_CTX_get(ctx);
-        t3 = BN_CTX_get(ctx);
-        if (t3 == NULL) break;
-        if (!group->meth->field_mul(group, t1, b->Y, a->Z, ctx)) break;
-        if (!BN_GF2m_add(t2, t1, a->Y)) break;
-        if (!group->meth->field_mul(group, t1, b->X, a->Z, ctx)) break;
-        if (!BN_GF2m_add(t0, t1, a->X)) break;
-        if (BN_is_zero(t0)) {
-            if (BN_is_zero(t2)) {
-                /* a and b are the same in lambda-affine, so double */
-                ret = ec_GF2m_lambda_dbl(group, r, b, ctx);
-            }
-            else {
-                /* b = -a, so infty */
-                ret = EC_POINT_set_to_infinity(group, r);
-            }
-            break;
-        }
-        if (!group->meth->field_mul(group, t1, t1, t2, ctx)) break;
-        if (!group->meth->field_mul(group, t3, a->X, t2, ctx)) break;
-        if (!group->meth->field_sqr(group, t0, t0, ctx)) break;
-        if (!group->meth->field_mul(group, r->X, t1, t3, ctx)) break;
-        if (!BN_GF2m_add(t3, a->Z, a->Y)) break;
-        if (!group->meth->field_mul(group, t2, t0, t2, ctx)) break;
-        if (!BN_GF2m_add(t0, t0, t1)) break;
-        if (!group->meth->field_mul(group, t3, t3, t2, ctx)) break;
-        if (!group->meth->field_sqr(group, t0, t0, ctx)) break;
-        if (!group->meth->field_mul(group, r->Z, a->Z, t2, ctx)) break;
-        if (!BN_GF2m_add(r->Y, t3, t0)) break;
-        ret = 1;
-    } while(0);
+    t0 = BN_CTX_get(ctx);
+    t1 = BN_CTX_get(ctx);
+    t2 = BN_CTX_get(ctx);
+    t3 = BN_CTX_get(ctx);
+    if (t3 == NULL
+            || !group->meth->field_mul(group, t1, b->Y, a->Z, ctx)
+            || !BN_GF2m_add(t2, t1, a->Y)
+            || !group->meth->field_mul(group, t1, b->X, a->Z, ctx)
+            || !BN_GF2m_add(t0, t1, a->X))
+        goto done;
 
+    if (BN_is_zero(t0)) {
+        if (BN_is_zero(t2)) {
+            /* a and b are the same in lambda-affine, so double */
+            ret = ec_GF2m_lambda_dbl(group, r, b, ctx);
+        } else {
+            /* b = -a, so infty */
+            ret = EC_POINT_set_to_infinity(group, r);
+        }
+    } else {
+        if (!group->meth->field_mul(group, t1, t1, t2, ctx)
+                || !group->meth->field_mul(group, t3, a->X, t2, ctx)
+                || !group->meth->field_sqr(group, t0, t0, ctx)
+                || !group->meth->field_mul(group, r->X, t1, t3, ctx)
+                || !BN_GF2m_add(t3, a->Z, a->Y)
+                || !group->meth->field_mul(group, t2, t0, t2, ctx)
+                || !BN_GF2m_add(t0, t0, t1)
+                || !group->meth->field_mul(group, t3, t3, t2, ctx)
+                || !group->meth->field_sqr(group, t0, t0, ctx)
+                || !group->meth->field_mul(group, r->Z, a->Z, t2, ctx)
+                || !BN_GF2m_add(r->Y, t3, t0))
+            goto done;
+        ret = 1;
+    }
+
+ done:
     BN_CTX_end(ctx);
     return ret;
 }
@@ -180,53 +179,56 @@ int ec_GF2m_lambda_add_mixed(const EC_GROUP *group, EC_POINT *r, const EC_POINT 
  * @param b input point in lambda-affine/proj
  * @param r output point in lambda-proj
  */
-int ec_GF2m_lambda_add_proj(const EC_GROUP *group, EC_POINT *r, const EC_POINT *a,
-                       const EC_POINT *b, BN_CTX *ctx)
+static int ec_GF2m_lambda_add_proj(const EC_GROUP *group, EC_POINT *r,
+                                   const EC_POINT *a, const EC_POINT *b,
+                                   BN_CTX *ctx)
 {
+    BIGNUM *t0, *t1, *t2, *t3, *t4, *t5;
     int ret = 0;
     BN_CTX_start(ctx);
 
-    do {
-        BIGNUM *t0, *t1, *t2, *t3, *t4, *t5;
-        t0 = BN_CTX_get(ctx);
-        t1 = BN_CTX_get(ctx);
-        t2 = BN_CTX_get(ctx);
-        t3 = BN_CTX_get(ctx);
-        t4 = BN_CTX_get(ctx);
-        t5 = BN_CTX_get(ctx);
-        if (t5 == NULL) break;
-        if (!group->meth->field_mul(group, t3, a->X, b->Z, ctx)) break;
-        if (!group->meth->field_mul(group, t2, b->X, a->Z, ctx)) break;
-        if (!group->meth->field_mul(group, t0, b->Y, a->Z, ctx)) break;
-        if (!group->meth->field_mul(group, t1, b->Z, a->Y, ctx)) break;
-        if (!BN_GF2m_add(t0, t0, t1)) break;
-        if (!BN_GF2m_add(t1, t2, t3)) break;
-        if (BN_is_zero(t1)) {
-            if (BN_is_zero(t0)) {
-                /* a and b are the same in lambda-affine, so double */
-                ret = ec_GF2m_lambda_dbl(group, r, a, ctx);
-            }
-            else {
-                /* b = -a, so infty */
-                ret = EC_POINT_set_to_infinity(group, r);
-            }
-            break;
-        }
-        if (!BN_GF2m_add(t4, a->Z, a->Y)) break;
-        if (!group->meth->field_mul(group, t2, t2, t0, ctx)) break;
-        if (!group->meth->field_sqr(group, t5, t1, ctx)) break;
-        if (!group->meth->field_mul(group, t3, t3, t0, ctx)) break;
-        if (!group->meth->field_mul(group, t1, t5, t0, ctx)) break;
-        if (!group->meth->field_mul(group, r->X, t2, t3, ctx)) break;
-        if (!group->meth->field_mul(group, t0, b->Z, t1, ctx)) break;
-        if (!BN_GF2m_add(t1, t5, t2)) break;
-        if (!group->meth->field_mul(group, t4, t4, t0, ctx)) break;
-        if (!group->meth->field_sqr(group, t1, t1, ctx)) break;
-        if (!group->meth->field_mul(group, r->Z, a->Z, t0, ctx)) break;
-        if (!BN_GF2m_add(r->Y, t4, t1)) break;
-        ret = 1;
-    } while(0);
+    t0 = BN_CTX_get(ctx);
+    t1 = BN_CTX_get(ctx);
+    t2 = BN_CTX_get(ctx);
+    t3 = BN_CTX_get(ctx);
+    t4 = BN_CTX_get(ctx);
+    t5 = BN_CTX_get(ctx);
+    if (t5 == NULL
+            || !group->meth->field_mul(group, t3, a->X, b->Z, ctx)
+            || !group->meth->field_mul(group, t2, b->X, a->Z, ctx)
+            || !group->meth->field_mul(group, t0, b->Y, a->Z, ctx)
+            || !group->meth->field_mul(group, t1, b->Z, a->Y, ctx)
+            || !BN_GF2m_add(t0, t0, t1)
+            || !BN_GF2m_add(t1, t2, t3))
+        goto done;
 
+    if (BN_is_zero(t1)) {
+        if (BN_is_zero(t0)) {
+            /* a and b are the same in lambda-affine, so double */
+            ret = ec_GF2m_lambda_dbl(group, r, a, ctx);
+        } else {
+            /* b = -a, so infty */
+            ret = EC_POINT_set_to_infinity(group, r);
+        }
+    } else {
+        if (!BN_GF2m_add(t4, a->Z, a->Y)
+                || !group->meth->field_mul(group, t2, t2, t0, ctx)
+                || !group->meth->field_sqr(group, t5, t1, ctx)
+                || !group->meth->field_mul(group, t3, t3, t0, ctx)
+                || !group->meth->field_mul(group, t1, t5, t0, ctx)
+                || !group->meth->field_mul(group, r->X, t2, t3, ctx)
+                || !group->meth->field_mul(group, t0, b->Z, t1, ctx)
+                || !BN_GF2m_add(t1, t5, t2)
+                || !group->meth->field_mul(group, t4, t4, t0, ctx)
+                || !group->meth->field_sqr(group, t1, t1, ctx)
+                || !group->meth->field_mul(group, r->Z, a->Z, t0, ctx)
+                || !BN_GF2m_add(r->Y, t4, t1))
+            goto done;
+
+        ret = 1;
+    }
+
+ done:
     BN_CTX_end(ctx);
     return ret;
 }
@@ -242,6 +244,7 @@ int ec_GF2m_lambda_add_proj(const EC_GROUP *group, EC_POINT *r, const EC_POINT *
 int ec_GF2m_lambda_add(const EC_GROUP *group, EC_POINT *r, const EC_POINT *a,
                        const EC_POINT *b, BN_CTX *ctx)
 {
+    int ret = 0;
 
     if (EC_POINT_is_at_infinity(group, a)) {
         if (!EC_POINT_copy(r, b))
@@ -255,20 +258,14 @@ int ec_GF2m_lambda_add(const EC_GROUP *group, EC_POINT *r, const EC_POINT *a,
         return 1;
     }
 
-    int ret = 0;
-
-    if (a == b) {
+    if (a == b)
         ret = ec_GF2m_lambda_dbl(group, r, a, ctx);
-    }
-    else if (b->Z_is_one) {
+    else if (b->Z_is_one)
         ret = ec_GF2m_lambda_add_mixed(group,r,a,b,ctx);
-    }
-    else if (a->Z_is_one) {
+    else if (a->Z_is_one)
         ret = ec_GF2m_lambda_add_mixed(group,r,b,a,ctx);
-    }
-    else {
+    else
         ret = ec_GF2m_lambda_add_proj(group,r,a,b,ctx);
-    }
 
     r->Z_is_one = 0;
     return ret;
@@ -301,26 +298,28 @@ int ec_GF2m_lambda_point_set_affine_coordinates(const EC_GROUP *group,
                                                 const BIGNUM *x,
                                                 const BIGNUM *y, BN_CTX *ctx)
 {
+    int ret = 0;
+
     if (x == NULL || y == NULL) {
-        ECerr(EC_F_EC_GF2M_SIMPLE_POINT_SET_AFFINE_COORDINATES,
+        ECerr(EC_F_EC_GF2M_LAMBDA_POINT_SET_AFFINE_COORDINATES,
               ERR_R_PASSED_NULL_PARAMETER);
         return 0;
     }
 
-    int ret = 0;
+    if (!BN_copy(point->X, x))
+        goto done;
+    BN_set_negative(point->X, 0);
+    if (!group->meth->field_div(group, point->Y, y, point->X, ctx)
+            || !BN_GF2m_add(point->Y, point->Y, point->X))
+        goto done;
+    BN_set_negative(point->Y, 0);
+    if (!BN_one(point->Z))
+        goto done;
+    BN_set_negative(point->Z, 0);
+    point->Z_is_one = 1;
+    ret = 1;
 
-    do {
-        if (!BN_copy(point->X, x)) break;
-        BN_set_negative(point->X, 0);
-        if (!group->meth->field_div(group, point->Y, y, point->X, ctx)) break;
-        if (!BN_GF2m_add(point->Y, point->Y, point->X)) break;
-        BN_set_negative(point->Y, 0);
-        if (!BN_one(point->Z)) break;
-        BN_set_negative(point->Z, 0);
-        point->Z_is_one = 1;
-        ret = 1;
-    } while(0);
-
+ done:
     return ret;
 }
 
@@ -338,31 +337,34 @@ int ec_GF2m_lambda_point_get_affine_coordinates(const EC_GROUP *group,
                                                 BIGNUM *x, BIGNUM *y,
                                                 BN_CTX *ctx)
 {
+    int ret = 0;
+    EC_POINT *r;
+
     if (EC_POINT_is_at_infinity(group, point)) {
-        ECerr(EC_F_EC_GF2M_SIMPLE_POINT_GET_AFFINE_COORDINATES,
+        ECerr(EC_F_EC_GF2M_LAMBDA_POINT_GET_AFFINE_COORDINATES,
               EC_R_POINT_AT_INFINITY);
         return 0;
     }
 
-    int ret = 0;
-    EC_POINT *r;
+    r = EC_POINT_new(group);
+    if (r == NULL
+            || !EC_POINT_copy(r, point)
+            || !EC_POINT_make_affine(group, r, ctx))
+        goto done;
+    if (x != NULL) {
+        if (!BN_copy(x, r->X))
+            goto done;
+        BN_set_negative(x, 0);
+    }
+    if (y != NULL) {
+        if (!BN_GF2m_add(y, r->Y, r->X)
+                || !group->meth->field_mul(group, y, y, point->X, ctx))
+            goto done;
+        BN_set_negative(y, 0);
+    }
+    ret = 1;
 
-    do {
-        if ((r = EC_POINT_new(group)) == NULL) break;
-        if (!EC_POINT_copy(r, point)) break;
-        if (!EC_POINT_make_affine(group, r, ctx)) break;
-        if (x != NULL) {
-            if (!BN_copy(x, r->X)) break;
-            BN_set_negative(x, 0);
-        }
-        if (y != NULL) {
-            if (!BN_GF2m_add(y, r->Y, r->X)) break;
-            if (!group->meth->field_mul(group, y, y, point->X, ctx)) break;
-            BN_set_negative(y, 0);
-        }
-        ret = 1;
-    } while(0);
-
+ done:
     EC_POINT_free(r);
     return ret;
 }
@@ -373,23 +375,25 @@ int ec_GF2m_lambda_point_get_affine_coordinates(const EC_GROUP *group,
  *
  * @param point input/output point
  */
-int ec_GF2m_lambda_make_affine(const EC_GROUP *group, EC_POINT *point, BN_CTX *ctx)
+int ec_GF2m_lambda_make_affine(const EC_GROUP *group, EC_POINT *point,
+                               BN_CTX *ctx)
 {
+    int ret = 0;
+
     /* infty -> infty */
     if (EC_POINT_is_at_infinity(group, point))
         return 1;
 
-    int ret = 0;
+    if (!BN_GF2m_mod_inv_arr(point->Z, point->Z, group->poly, ctx)
+            || !group->meth->field_mul(group, point->X, point->X, point->Z, ctx)
+            || !group->meth->field_mul(group, point->Y, point->Y, point->Z, ctx)
+            || !BN_one(point->Z))
+        goto done;
 
-    do {
-        if (!BN_GF2m_mod_inv_arr(point->Z, point->Z, group->poly, ctx)) break;
-        if (!group->meth->field_mul(group, point->X, point->X, point->Z, ctx)) break;
-        if (!group->meth->field_mul(group, point->Y, point->Y, point->Z, ctx)) break;
-        if (!BN_one(point->Z)) break;
-        point->Z_is_one = 1;
-        ret = 1;
-    } while(0);
+    point->Z_is_one = 1;
+    ret = 1;
 
+ done:
     return ret;
 }
 
@@ -402,35 +406,35 @@ int ec_GF2m_lambda_make_affine(const EC_GROUP *group, EC_POINT *point, BN_CTX *c
 int ec_GF2m_lambda_is_on_curve(const EC_GROUP *group, const EC_POINT *point,
                                BN_CTX *ctx)
 {
+    int ret = 0;
+    BIGNUM *t1, *t2, *t3;
+
     /* infty always on curve */
     if (EC_POINT_is_at_infinity(group, point))
         return 1;
 
-    int ret = 0;
     BN_CTX_start(ctx);
+    t1 = BN_CTX_get(ctx);
+    t2 = BN_CTX_get(ctx);
+    t3 = BN_CTX_get(ctx);
 
-    do {
-        BIGNUM *t1, *t2, *t3;
-        t1 = BN_CTX_get(ctx);
-        t2 = BN_CTX_get(ctx);
-        t3 = BN_CTX_get(ctx);
-        if (t3 == NULL) break;
-        if (!group->meth->field_sqr(group, t1, point->Y, ctx)) break;
-        if (!group->meth->field_mul(group, t2, point->Y, point->Z, ctx)) break;
-        if (!BN_GF2m_add(t1, t1, t2)) break;
-        if (!group->meth->field_sqr(group, t2, point->Z, ctx)) break;
-        if (BN_is_one(group->a)) {
-            if (!BN_GF2m_add(t1, t1, t2)) break;
-        }
-        if (!group->meth->field_sqr(group, t3, point->X, ctx)) break;
-        if (!BN_GF2m_add(t1, t1, t3)) break;
-        if (!group->meth->field_mul(group, t1, t1, t3, ctx)) break;
-        if (!group->meth->field_sqr(group, t2, t2, ctx)) break;
-        if (!group->meth->field_mul(group, t2, t2, group->b, ctx)) break;
-        if (!BN_GF2m_add(t1, t1, t2)) break;
-        ret = BN_is_zero(t1);
-    } while(0);
+    if (t3 == NULL
+            || !group->meth->field_sqr(group, t1, point->Y, ctx)
+            || !group->meth->field_mul(group, t2, point->Y, point->Z, ctx)
+            || !BN_GF2m_add(t1, t1, t2)
+            || !group->meth->field_sqr(group, t2, point->Z, ctx)
+            || (BN_is_one(group->a) && !BN_GF2m_add(t1, t1, t2))
+            || !group->meth->field_sqr(group, t3, point->X, ctx)
+            || !BN_GF2m_add(t1, t1, t3)
+            || !group->meth->field_mul(group, t1, t1, t3, ctx)
+            || !group->meth->field_sqr(group, t2, t2, ctx)
+            || !group->meth->field_mul(group, t2, t2, group->b, ctx)
+            || !BN_GF2m_add(t1, t1, t2))
+        goto done;
 
+    ret = BN_is_zero(t1);
+
+ done:
     BN_CTX_end(ctx);
     return ret;
 }
