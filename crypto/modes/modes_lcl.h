298431d3baad63e9b883adbcd2a0cc82ea5e0e5b
Mingtao Yang <mingtao@fb.com>
2018-06-06 09:34:18 -0700

    modes/ocb128.c: Reset nonce dependent variables on setiv
    Upon a call to CRYPTO_ocb128_setiv, either directly on an OCB_CTX or
    indirectly with EVP_CTRL_AEAD_SET_IVLEN, resets the nonce dependent
    variables on the OCB_CTX.

--- crypto/modes/modes_lcl.h
+++ crypto/modes/modes_lcl.h
@@ -178,12 +178,13 @@ struct ocb128_context {
     OCB_BLOCK l_dollar;
     OCB_BLOCK *l;
     /* Must be reset for each session */
-    u64 blocks_hashed;
-    u64 blocks_processed;
-    OCB_BLOCK tag;
-    OCB_BLOCK offset_aad;
-    OCB_BLOCK sum;
-    OCB_BLOCK offset;
-    OCB_BLOCK checksum;
+    struct {
+        u64 blocks_hashed;
+        u64 blocks_processed;
+        OCB_BLOCK offset_aad;
+        OCB_BLOCK sum;
+        OCB_BLOCK offset;
+        OCB_BLOCK checksum;
+    } sess;
 };
 #endif                          /* OPENSSL_NO_OCB */
