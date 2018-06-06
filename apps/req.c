4bc894164bc028dd3bebadfbecea3dbd96720c08
Matt Caswell <matt@openssl.org>
2018-06-06 09:51:12 +0100

    Fix no-dsa
    Broken by 0336df2fa.

--- apps/req.c
+++ apps/req.c
@@ -523,11 +523,13 @@ int req_main(int argc, char **argv)
                        "         Your key size is %ld! Larger key size may behave not as expected.\n",
                        OPENSSL_RSA_MAX_MODULUS_BITS, newkey);
 
+#ifndef OPENSSL_NO_DSA
         if (pkey_type == EVP_PKEY_DSA && newkey > OPENSSL_DSA_MAX_MODULUS_BITS)
             BIO_printf(bio_err,
                        "Warning: It is not recommended to use more than %d bit for DSA keys.\n"
                        "         Your key size is %ld! Larger key size may behave not as expected.\n",
                        OPENSSL_DSA_MAX_MODULUS_BITS, newkey);
+#endif
 
         if (genctx == NULL) {
             genctx = set_keygen_ctx(NULL, &pkey_type, &newkey,
