e3bd8603b062e7c2bd6336cd357cdaaa546578c6
Gilles Khouzam <gillesk@microsoft.com>
2018-06-18 15:40:22 -0700

    Fixup code from remove of C++/CX code
    Remove the need to update the LIBPATH environment variable since we don't use C++/CX anymore.
    
    Change the winrt.cpp to spaces for better consistency with the reset of the project.

--- ms/setVSvars.bat
+++ ms/setVSvars.bat
@@ -211,10 +211,6 @@ exit /b
 	call:setVar _VS15VC VisualStudio15VC
 	call:setVar _WKITS10 WindowsKits10.0
 	call:setVar _WKITS10VER WindowsKits10Version
-	for /D %%f in ("%_WKITS10%\references\%_WKITS10VER%.0\windows.foundation.foundationcontract\*") do set LIBPATH=%LIBPATH%;%%f\
-	for /D %%f in ("%_WKITS10%\references\%_WKITS10VER%.0\windows.foundation.universalapicontract\*") do set LIBPATH=%LIBPATH%;%%f\
-	for /D %%f in ("%_WKITS10%\references\windows.foundation.foundationcontract\*") do set LIBPATH=%LIBPATH%;%%f\
-	for /D %%f in ("%_WKITS10%\references\windows.foundation.universalapicontract\*") do set LIBPATH=%LIBPATH%;%%f\
 	goto :eof
 
 :end
--- ms/winrt.cpp
+++ ms/winrt.cpp
@@ -9,9 +9,9 @@
 #if defined(WINAPI_FAMILY)
 extern "C"
 {
-	unsigned entropyRT(BYTE *buffer, unsigned len);
-	void RAND_add(const void *buf,int num,double entropy);
-	int RAND_poll(void);
+    unsigned entropyRT(BYTE *buffer, unsigned len);
+    void RAND_add(const void *buf,int num,double entropy);
+    int RAND_poll(void);
 }
 #endif
 
@@ -24,26 +24,26 @@ extern "C"
 // easy way to get to the API.
 
 unsigned entropyRT(BYTE *buffer, unsigned len)
-	{
-	unsigned bytesLeft = len;
-	BYTE* buffer_pos = buffer;
-	GUID guid;
-	while (bytesLeft > 0)
-		{
-		if (SUCCEEDED(CoCreateGuid(&guid)))
-			{
-			unsigned copy = (bytesLeft > sizeof(guid)) ? sizeof(guid) : bytesLeft;
-			memcpy(buffer_pos, &guid, copy);
-			buffer_pos += copy;
-			bytesLeft -= copy;
-			}
-		else
-			{
-			break;
-			}
-		}
-	return len - bytesLeft;
-	}
+{
+    unsigned bytesLeft = len;
+    BYTE* buffer_pos = buffer;
+    GUID guid;
+    while (bytesLeft > 0)
+    {
+        if (SUCCEEDED(CoCreateGuid(&guid)))
+        {
+            unsigned copy = (bytesLeft > sizeof(guid)) ? sizeof(guid) : bytesLeft;
+            memcpy(buffer_pos, &guid, copy);
+            buffer_pos += copy;
+            bytesLeft -= copy;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return len - bytesLeft;
+}
 
 #else
 #include <roapi.h>
@@ -53,84 +53,84 @@ unsigned entropyRT(BYTE *buffer, unsigned len)
 
 unsigned entropyRT(BYTE *buffer, unsigned len)
 {
-	using ABI::Windows::Storage::Streams::IBuffer;
-	using ABI::Windows::Security::Cryptography::ICryptographicBufferStatics;
-	using Windows::Storage::Streams::IBufferByteAccess;
+    using ABI::Windows::Storage::Streams::IBuffer;
+    using ABI::Windows::Security::Cryptography::ICryptographicBufferStatics;
+    using Windows::Storage::Streams::IBufferByteAccess;
 
-	unsigned ArrayLength = 0;
+    unsigned ArrayLength = 0;
 
-	if (SUCCEEDED(RoInitialize(RO_INIT_MULTITHREADED)))
-	{
-		const wchar_t* CryptographicBufferName =
-			RuntimeClass_Windows_Security_Cryptography_CryptographicBuffer;
-		UINT32 CryptographicBufferNameLength =
-			static_cast<UINT32>(wcslen(CryptographicBufferName));
-		HSTRING_HEADER CryptographicBufferNameObjectHeader = { 0 };
-		HSTRING CryptographicBufferNameObject = nullptr;
-		ICryptographicBufferStatics* pCryptographicBuffer = nullptr;
-		IBuffer* pBuffer = nullptr;
-		IBufferByteAccess* pBufferByteAccess = nullptr;
-		byte* RawBuffer = nullptr;
+    if (SUCCEEDED(RoInitialize(RO_INIT_MULTITHREADED)))
+    {
+        const wchar_t* CryptographicBufferName =
+            RuntimeClass_Windows_Security_Cryptography_CryptographicBuffer;
+        UINT32 CryptographicBufferNameLength =
+            static_cast<UINT32>(wcslen(CryptographicBufferName));
+        HSTRING_HEADER CryptographicBufferNameObjectHeader = { 0 };
+        HSTRING CryptographicBufferNameObject = nullptr;
+        ICryptographicBufferStatics* pCryptographicBuffer = nullptr;
+        IBuffer* pBuffer = nullptr;
+        IBufferByteAccess* pBufferByteAccess = nullptr;
+        byte* RawBuffer = nullptr;
 
-		do
-		{
-			if (FAILED(WindowsCreateStringReference(
-				CryptographicBufferName,
-				CryptographicBufferNameLength,
-				&CryptographicBufferNameObjectHeader,
-				&CryptographicBufferNameObject)))
-				break;
+        do
+        {
+            if (FAILED(WindowsCreateStringReference(
+                CryptographicBufferName,
+                CryptographicBufferNameLength,
+                &CryptographicBufferNameObjectHeader,
+                &CryptographicBufferNameObject)))
+                break;
 
-			if (FAILED(RoGetActivationFactory(
-				CryptographicBufferNameObject,
-				IID_INS_ARGS(&pCryptographicBuffer))))
-				break;
+            if (FAILED(RoGetActivationFactory(
+                CryptographicBufferNameObject,
+                IID_INS_ARGS(&pCryptographicBuffer))))
+                break;
 
-			if (FAILED(pCryptographicBuffer->GenerateRandom(len, &pBuffer)))
-				break;
+            if (FAILED(pCryptographicBuffer->GenerateRandom(len, &pBuffer)))
+                break;
 
-			if (FAILED(pBuffer->QueryInterface(&pBufferByteAccess)))
-				break;
+            if (FAILED(pBuffer->QueryInterface(&pBufferByteAccess)))
+                break;
 
-			if (FAILED(pBufferByteAccess->Buffer(&RawBuffer)))
-				break;
+            if (FAILED(pBufferByteAccess->Buffer(&RawBuffer)))
+                break;
 
-			if (nullptr == RawBuffer)
-				break;
+            if (nullptr == RawBuffer)
+                break;
 
-			if (FAILED(pBuffer->get_Length(&ArrayLength)))
-				break;
+            if (FAILED(pBuffer->get_Length(&ArrayLength)))
+                break;
 
-			// Make sure not to overflow the copy
-			ArrayLength = (ArrayLength > len) ? len : ArrayLength;
-			memcpy(buffer, RawBuffer, ArrayLength);
+            // Make sure not to overflow the copy
+            ArrayLength = (ArrayLength > len) ? len : ArrayLength;
+            memcpy(buffer, RawBuffer, ArrayLength);
 
-		} while (false);
+        } while (false);
 
 
-		if (nullptr != pBufferByteAccess)
-			pBufferByteAccess->Release();
+        if (nullptr != pBufferByteAccess)
+            pBufferByteAccess->Release();
 
-		if (nullptr != pBuffer)
-			pBuffer->Release();
+        if (nullptr != pBuffer)
+            pBuffer->Release();
 
-		if (nullptr != pCryptographicBuffer)
-			pCryptographicBuffer->Release();
+        if (nullptr != pCryptographicBuffer)
+            pCryptographicBuffer->Release();
 
-		RoUninitialize();
-	}
+        RoUninitialize();
+    }
 
-	return ArrayLength;
+    return ArrayLength;
 }
 #endif
 
 int RAND_poll(void)
-	{
-	BYTE buf[60];
-	unsigned collected = entropyRT(buf , sizeof(buf));
-	RAND_add(buf, collected, collected);
-	return 1;
-	}
+{
+    BYTE buf[60];
+    unsigned collected = entropyRT(buf , sizeof(buf));
+    RAND_add(buf, collected, collected);
+    return 1;
+}
 
 #if defined(OPENSSL_WINAPP)
 extern "C"
@@ -138,226 +138,226 @@ extern "C"
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-	//no dynamic handle load
+    //no dynamic handle load
 #if WINAPI_FAMILY==WINAPI_FAMILY_PHONE_APP
-	void* LoadLibraryA(
-					  const char* lpFileName
-					  )
-		{
-		return NULL;
-		}
+    void* LoadLibraryA(
+                      const char* lpFileName
+                      )
+        {
+        return NULL;
+        }
 #endif
-	HMODULE winrt_GetModuleHandle(
-								 const wchar_t* lpModuleName
-								 )
-		{
-		return NULL;
-		}
-	//no log for phone
-	int RegisterEventSource(
-						   _In_  LPCTSTR lpUNCServerName,
-						   _In_  LPCTSTR lpSourceName
-						   )
-		{
-		return NULL;
-		}
+    HMODULE winrt_GetModuleHandle(
+                                 const wchar_t* lpModuleName
+                                 )
+        {
+        return NULL;
+        }
+    //no log for phone
+    int RegisterEventSource(
+                           _In_  LPCTSTR lpUNCServerName,
+                           _In_  LPCTSTR lpSourceName
+                           )
+        {
+        return NULL;
+        }
 
-	int ReportEvent(
-				   _In_  HANDLE hEventLog,
-				   _In_  WORD wType,
-				   _In_  WORD wCategory,
-				   _In_  DWORD dwEventID,
-				   _In_  PSID lpUserSid,
-				   _In_  WORD wNumStrings,
-				   _In_  DWORD dwDataSize,
-				   _In_  LPCTSTR *lpStrings,
-				   _In_  LPVOID lpRawData
-				   )
-		{
-		return 0;
-		}
-	int MessageBox(
-				  _In_opt_  HWND hWnd,
-				  _In_opt_  LPCTSTR lpText,
-				  _In_opt_  LPCTSTR lpCaption,
-				  _In_      UINT uType
-				  )
-		{
-		return 0;
-		}
+    int ReportEvent(
+                   _In_  HANDLE hEventLog,
+                   _In_  WORD wType,
+                   _In_  WORD wCategory,
+                   _In_  DWORD dwEventID,
+                   _In_  PSID lpUserSid,
+                   _In_  WORD wNumStrings,
+                   _In_  DWORD dwDataSize,
+                   _In_  LPCTSTR *lpStrings,
+                   _In_  LPVOID lpRawData
+                   )
+        {
+        return 0;
+        }
+    int MessageBox(
+                  _In_opt_  HWND hWnd,
+                  _In_opt_  LPCTSTR lpText,
+                  _In_opt_  LPCTSTR lpCaption,
+                  _In_      UINT uType
+                  )
+        {
+        return 0;
+        }
 
-	HDC GetDC(
-			 _In_  HWND hWnd
-			 )
-		{
-		return NULL;
-		}
+    HDC GetDC(
+             _In_  HWND hWnd
+             )
+        {
+        return NULL;
+        }
 
-	HDC CreateDC(
-				LPCTSTR lpszDriver,
-				_In_  LPCTSTR lpszDevice,
-				LPCTSTR lpszOutput,
-				_In_  const DEVMODE *lpInitData
-				)
-		{
-		return NULL;
-		}
+    HDC CreateDC(
+                LPCTSTR lpszDriver,
+                _In_  LPCTSTR lpszDevice,
+                LPCTSTR lpszOutput,
+                _In_  const DEVMODE *lpInitData
+                )
+        {
+        return NULL;
+        }
 
-	BOOL winrt_GetVersionEx(
-							_Inout_  LPOSVERSIONINFO lpVersionInfo
-							)
-		{
-		return 0;
-		}
-	int GetObject(
-				 _In_   HGDIOBJ hgdiobj,
-				 _In_   int cbBuffer,
-				 _Out_  LPVOID lpvObject
-				 )
-		{
-		return 0;
-		}
-	int MoveFile(
-				_In_  LPCTSTR lpExistingFileName,
-				_In_  LPCTSTR lpNewFileName
-				)
-		{
-		return 0;
-		}
-	int WINAPI GetProcessWindowStation(void)
-		{
-		return NULL;
-		}
-	BOOL WINAPI GetUserObjectInformationW(
-										 _In_       HANDLE hObj,
-										 _In_       int nIndex,
-										 _Out_opt_  PVOID pvInfo,
-										 _In_       DWORD nLength,
-										 _Out_opt_  LPDWORD lpnLengthNeeded
-										 )
-		{
-		return 0;
-		}
+    BOOL winrt_GetVersionEx(
+                            _Inout_  LPOSVERSIONINFO lpVersionInfo
+                            )
+        {
+        return 0;
+        }
+    int GetObject(
+                 _In_   HGDIOBJ hgdiobj,
+                 _In_   int cbBuffer,
+                 _Out_  LPVOID lpvObject
+                 )
+        {
+        return 0;
+        }
+    int MoveFile(
+                _In_  LPCTSTR lpExistingFileName,
+                _In_  LPCTSTR lpNewFileName
+                )
+        {
+        return 0;
+        }
+    int WINAPI GetProcessWindowStation(void)
+        {
+        return NULL;
+        }
+    BOOL WINAPI GetUserObjectInformationW(
+                                         _In_       HANDLE hObj,
+                                         _In_       int nIndex,
+                                         _Out_opt_  PVOID pvInfo,
+                                         _In_       DWORD nLength,
+                                         _Out_opt_  LPDWORD lpnLengthNeeded
+                                         )
+        {
+        return 0;
+        }
 #ifndef STD_ERROR_HANDLE
-	int WINAPI GetStdHandle(
-						   _In_  DWORD nStdHandle
-						   )
-		{
-		return 0;
-		}
+    int WINAPI GetStdHandle(
+                           _In_  DWORD nStdHandle
+                           )
+        {
+        return 0;
+        }
 #endif
-	BOOL DeregisterEventSource(
-							  _Inout_  HANDLE hEventLog
-							  )
-		{
-		return 0;
-		}
-	char *winrt_getenv(
-					  const char *varname
-					  )
-		{
-		//hardcoded environmental variables used for the appx testing application for store/phone
-		if (!strcmp(varname, "OPENSSL_CONF"))
-			{
-			return "./openssl.cnf";
-			}
-		return 0;
-		}
-	int winrt_setenv(const char *envname, const char *envval, int overwrite)
-		{
-		return -1;
-		}
-	void WINAPI GlobalMemoryStatus(
-								  _Out_  LPMEMORYSTATUS lpBuffer
-								  )
-		{
-		return;
-		}
-	HDC CreateCompatibleDC(
-						  HDC hdc
-						  )
-		{
-		return NULL;
-		}
-	int GetDeviceCaps(
-					 _In_  HDC hdc,
-					 _In_  int nIndex
-					 )
-		{
-		return 0;
-		}
-	HBITMAP CreateCompatibleBitmap(
-								  _In_  HDC hdc,
-								  _In_  int nWidth,
-								  _In_  int nHeight
-								  )
-		{
-		return 0;
-		}
-	HGDIOBJ SelectObject(
-						_In_  HDC hdc,
-						_In_  HGDIOBJ hgdiobj
-						)
-		{
-		return 0;
-		}
-	BOOL BitBlt(
-			   _In_  HDC hdcDest,
-			   _In_  int nXDest,
-			   _In_  int nYDest,
-			   _In_  int nWidth,
-			   _In_  int nHeight,
-			   _In_  HDC hdcSrc,
-			   _In_  int nXSrc,
-			   _In_  int nYSrc,
-			   _In_  DWORD dwRop
-			   )
-		{
-		return 0;
-		}
-	LONG GetBitmapBits(
-					  _In_   HBITMAP hbmp,
-					  _In_   LONG cbBuffer,
-					  _Out_  LPVOID lpvBits
-					  )
-		{
-		return 0;
-		}
-	BOOL DeleteObject(
-					 _In_  HGDIOBJ hObject
-					 )
-		{
-		return 0;
-		}
-	int _getch(void)
-		{
-		return 0;
-		}
-	int _kbhit()
-		{
-		return 0;
-		}
+    BOOL DeregisterEventSource(
+                              _Inout_  HANDLE hEventLog
+                              )
+        {
+        return 0;
+        }
+    char *winrt_getenv(
+                      const char *varname
+                      )
+        {
+        //hardcoded environmental variables used for the appx testing application for store/phone
+        if (!strcmp(varname, "OPENSSL_CONF"))
+            {
+            return "./openssl.cnf";
+            }
+        return 0;
+        }
+    int winrt_setenv(const char *envname, const char *envval, int overwrite)
+        {
+        return -1;
+        }
+    void WINAPI GlobalMemoryStatus(
+                                  _Out_  LPMEMORYSTATUS lpBuffer
+                                  )
+        {
+        return;
+        }
+    HDC CreateCompatibleDC(
+                          HDC hdc
+                          )
+        {
+        return NULL;
+        }
+    int GetDeviceCaps(
+                     _In_  HDC hdc,
+                     _In_  int nIndex
+                     )
+        {
+        return 0;
+        }
+    HBITMAP CreateCompatibleBitmap(
+                                  _In_  HDC hdc,
+                                  _In_  int nWidth,
+                                  _In_  int nHeight
+                                  )
+        {
+        return 0;
+        }
+    HGDIOBJ SelectObject(
+                        _In_  HDC hdc,
+                        _In_  HGDIOBJ hgdiobj
+                        )
+        {
+        return 0;
+        }
+    BOOL BitBlt(
+               _In_  HDC hdcDest,
+               _In_  int nXDest,
+               _In_  int nYDest,
+               _In_  int nWidth,
+               _In_  int nHeight,
+               _In_  HDC hdcSrc,
+               _In_  int nXSrc,
+               _In_  int nYSrc,
+               _In_  DWORD dwRop
+               )
+        {
+        return 0;
+        }
+    LONG GetBitmapBits(
+                      _In_   HBITMAP hbmp,
+                      _In_   LONG cbBuffer,
+                      _Out_  LPVOID lpvBits
+                      )
+        {
+        return 0;
+        }
+    BOOL DeleteObject(
+                     _In_  HGDIOBJ hObject
+                     )
+        {
+        return 0;
+        }
+    int _getch(void)
+        {
+        return 0;
+        }
+    int _kbhit()
+        {
+        return 0;
+        }
 
 # if !defined(NTDDI_VERSION) || (NTDDI_VERSION < NTDDI_WIN10_RS4)
-	BOOL WINAPI FlushConsoleInputBuffer(
-									   _In_  HANDLE hConsoleInput
-									   )
-		{
-		return 0;
-		}
+    BOOL WINAPI FlushConsoleInputBuffer(
+                                       _In_  HANDLE hConsoleInput
+                                       )
+        {
+        return 0;
+        }
 #endif
-	BOOL DeleteDC(
-				 _In_  HDC hdc
-				 )
-		{
-		return 0;
-		}
-	int winrt_GetTickCount(void)
-		{
-		LARGE_INTEGER t;
-		return(int) (QueryPerformanceCounter(&t) ? t.QuadPart : 0);
-		}
-	void *OPENSSL_UplinkTable [26]= {0};
+    BOOL DeleteDC(
+                 _In_  HDC hdc
+                 )
+        {
+        return 0;
+        }
+    int winrt_GetTickCount(void)
+        {
+        LARGE_INTEGER t;
+        return(int) (QueryPerformanceCounter(&t) ? t.QuadPart : 0);
+        }
+    void *OPENSSL_UplinkTable [26]= {0};
 } //extern C
 
 #endif /*defined(OPENSSL_WINAPP)*/
