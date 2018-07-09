2a70c317417df67f04413a6ff3da573e9d4ebec9
Richard Levitte <levitte@openssl.org>
2018-07-09 21:07:25 +0200

    Make 'with_fallback' use 'use' instead of 'require'
    This enables us to require module versions, and to fall back to a
    bundled version if the system version is too low.

--- util/perl/with_fallback.pm
+++ util/perl/with_fallback.pm
@@ -8,15 +8,17 @@
 package with_fallback;
 
 sub import {
+    shift;
+
     use File::Basename;
     use File::Spec::Functions;
     foreach (@_) {
-	eval "require $_";
+	eval "use $_";
 	if ($@) {
 	    unshift @INC, catdir(dirname(__FILE__),
                                  "..", "..", "external", "perl");
 	    my $transfer = "transfer::$_";
-	    eval "require $transfer";
+	    eval "use $transfer";
 	    shift @INC;
 	    warn $@ if $@;
 	}
