502a172cd192d39d9bf5f84db5e7abf08ddef3a1
Andy Polyakov <appro@openssl.org>
2018-07-12 19:15:26 +0200

    bn/bn_lcl.h,bn_nist.c: addres strict warnings with -DBN_DEBUG.

--- crypto/bn/bn_nist.c
+++ crypto/bn/bn_nist.c
@@ -254,7 +254,7 @@ static void nist_cp_bn_0(BN_ULONG *dst, const BN_ULONG *src, int top, int max)
     int i;
 
 #ifdef BN_DEBUG
-    assert(top <= max);
+    (void)ossl_assert(top <= max);
 #endif
     for (i = 0; i < top; i++)
         dst[i] = src[i];
