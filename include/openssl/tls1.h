43a0f2733a943799060ea275516fcce00d89eb38
Matt Caswell <matt@openssl.org>
2018-07-30 09:13:14 +0100

    Fix some TLSv1.3 alert issues
    Ensure that the certificate required alert actually gets sent (and doesn't
    get translated into handshake failure in TLSv1.3).
    
    Ensure that proper reason codes are given for the new TLSv1.3 alerts.
    
    Remove an out of date macro for TLS13_AD_END_OF_EARLY_DATA. This is a left
    over from an earlier TLSv1.3 draft that is no longer used.
    
    Fixes #6804
    
    Reviewed-by: Tim Hudson <tjh@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/6809)

--- include/openssl/tls1.h
+++ include/openssl/tls1.h
@@ -70,7 +70,6 @@ extern "C" {
 # define TLS1_AD_USER_CANCELLED          90
 # define TLS1_AD_NO_RENEGOTIATION        100
 /* TLSv1.3 alerts */
-# define TLS13_AD_END_OF_EARLY_DATA      1
 # define TLS13_AD_MISSING_EXTENSION      109 /* fatal */
 # define TLS13_AD_CERTIFICATE_REQUIRED   116 /* fatal */
 /* codes 110-114 are from RFC3546 */
