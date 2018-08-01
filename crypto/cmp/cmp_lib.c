533598b0ba31be683ed81551a37a049a14e8baf3
David von Oheimb <David.von.Oheimb@siemens.com>
2018-08-01 17:23:14 +0200

    add TODO to cmp_lib.c on pushing add_error_txt() upstrem to crypto/err

--- crypto/cmp/cmp_lib.c
+++ crypto/cmp/cmp_lib.c
@@ -45,6 +45,7 @@
 #include "cmp_int.h"
 
 
+/* TODO DvO push this functions upstream to crypto/err (PR #add_error_txt) */
 /*
  * Appends text to the extra error data field of the last error message in
  * OpenSSL's error queue, after adding the given separator string. Note that,
