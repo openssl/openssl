a0f443a05dd68b9949b39b3310a595babcae4624
Rich Salz <rsalz@openssl.org>
2018-08-07 15:28:59 -0400

    Increase CT_NUMBER values
    Also add build-time errors to keep them in sync.
    Thanks to GitHub user YuDudysheva for reporting this.
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/6874)
    (cherry picked from commit b5ee517794cf546dc7e3d5a82b400955a7381053)

--- include/openssl/tls1.h
+++ include/openssl/tls1.h
@@ -883,7 +883,13 @@ SSL_CTX_callback_ctrl(ssl,SSL_CTRL_SET_TLSEXT_TICKET_KEY_CB,(void (*)(void))cb)
  * when correcting this number, correct also SSL3_CT_NUMBER in ssl3.h (see
  * comment there)
  */
-# define TLS_CT_NUMBER                   9
+# define TLS_CT_NUMBER                   10
+
+# if defined(SSL3_CT_NUMBER)
+#  if TLS_CT_NUMBER != SSL3_CT_NUMBER
+#    error "SSL/TLS CT_NUMBER values do not match"
+#  endif
+# endif
 
 # define TLS1_FINISH_MAC_LENGTH          12
 
