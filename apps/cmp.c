openssl/openssl

commit 884f23b8b720a439da5023d8c82c47b7929568e4

remove remaining ssl.h references from crypto/cmp; exporting CMP_add_error_txt() for use in cmp.c

apps/cmp.c
@@ -1958,14 +1958,41 @@ static int read_write_req_resp(OSSL_CMP_CTX *ctx, const OSSL_CMP_MSG *req,
     return ret;
 }
 
-static BIO *tls_http_cb(OSSL_CMP_CTX *ctx, BIO *hbio, int connect)
+const char *tls_error_hint(unsigned long err)
+{
+    switch(ERR_GET_REASON(err)) {
+/*  case 0x1408F10B: */ /* xSL_F_SSL3_GET_RECORD */
+    case SSL_R_WRONG_VERSION_NUMBER:
+/*  case 0x140770FC: */ /* xSL_F_SSL23_GET_SERVER_HELLO */
+    case SSL_R_UNKNOWN_PROTOCOL:
+         return "The server does not support (a recent version of) TLS";
+/*  case 0x1407E086: */ /* xSL_F_SSL3_GET_SERVER_HELLO */
+/*  case 0x1409F086: */ /* xSL_F_SSL3_WRITE_PENDING */
+/*  case 0x14090086: */ /* xSL_F_SSL3_GET_SERVER_CERTIFICATE */
+/*  case 0x1416F086: */ /* xSL_F_TLS_PROCESS_SERVER_CERTIFICATE */
+    case SSL_R_CERTIFICATE_VERIFY_FAILED:
+        return "Cannot authenticate server via its TLS certificate, likely due to mismatch with our trusted TLS certs or missing revocation status";
+/*  case 0x14094418: */ /* xSL_F_SSL3_READ_BYTES */
+    case SSL_AD_REASON_OFFSET+TLS1_AD_UNKNOWN_CA:
+        return "Server did not accept our TLS certificate, likely due to mismatch with server's trust anchor or missing revocation status";
+    case SSL_AD_REASON_OFFSET+SSL3_AD_HANDSHAKE_FAILURE:
+        return "Server requires our TLS certificate but did not receive one";
+    default: /* no error or no hint available for error */
+        return NULL;
+    }
+}
+
+static BIO *tls_http_cb(OSSL_CMP_CTX *ctx, BIO *hbio, unsigned long detail)
 {
     SSL_CTX *ssl_ctx = OSSL_CMP_CTX_get_http_cb_arg(ctx);
     BIO *sbio = NULL;
-    if (connect) {
+    if (detail == 1) { /* connecting */
         sbio = BIO_new_ssl(OSSL_CMP_CTX_get_http_cb_arg(ctx), 1/* client */);
         hbio = sbio != NULL ? BIO_push(sbio, hbio): NULL;
-    } else {
+    } else { /* disconnecting */
+        const char *hint = tls_error_hint(detail);
+        if (hint != NULL)
+            OSSL_CMP_add_error_data(hint);
         /* as a workaround for OpenSSL double free, do not pop the sbio, but
            rely on BIO_free_all() done by OSSL_CMP_MSG_http_perform() */
     }

crypto/cmp/cmp_ctx.c
@@ -18,7 +18,6 @@
 #include <openssl/x509.h>
 #include <openssl/x509v3.h>
 #include <openssl/err.h>
-#include <openssl/ssl.h>
 #include <openssl/crypto.h>
 #include <string.h>
 #include <stdio.h>

crypto/cmp/cmp_http.c
@@ -20,7 +20,6 @@
 #include <openssl/asn1t.h>
 #include <openssl/cmp.h>
 #include <openssl/ocsp.h>
-#include <openssl/ssl.h>
 #include <openssl/err.h>
 #include <openssl/bio.h>
 #include <openssl/buffer.h>
@@ -220,46 +219,12 @@ static void add_conn_error_hint(const OSSL_CMP_CTX *ctx, unsigned long errdetail
     char buf[200];
     snprintf(buf, 200, "connecting to '%s' port %d", ctx->serverName,
              ctx->serverPort);
-    CMP_add_error_data(buf);
+    OSSL_CMP_add_error_data(buf);
     if (errdetail == 0) {
         snprintf(buf, 200, "server has disconnected%s",
                  ctx->http_cb_arg != NULL ? " violating the protocol" :
                                ", likely because it requires the use of TLS");
-        CMP_add_error_data(buf);
-    } else {
-# if 0
-        CMP_add_error_data(ERR_lib_error_string(errdetail));
-        CMP_add_error_data(ERR_func_error_string(errdetail));
-        CMP_add_error_data(ERR_reason_error_string(errdetail));
-# endif
-        switch(ERR_GET_REASON(errdetail)) {
-    /*  case 0x1408F10B: */ /* xSL_F_SSL3_GET_RECORD */
-        case SSL_R_WRONG_VERSION_NUMBER:
-    /*  case 0x140770FC: */ /* xSL_F_SSL23_GET_SERVER_HELLO */
-        case SSL_R_UNKNOWN_PROTOCOL:
-            CMP_add_error_data(
-                       "The server does not support (a recent version of) TLS");
-            break;
-    /*  case 0x1407E086: */ /* xSL_F_SSL3_GET_SERVER_HELLO */
-    /*  case 0x1409F086: */ /* xSL_F_SSL3_WRITE_PENDING */
-    /*  case 0x14090086: */ /* xSL_F_SSL3_GET_SERVER_CERTIFICATE */
-    /*  case 0x1416F086: */ /* xSL_F_TLS_PROCESS_SERVER_CERTIFICATE */
-        case SSL_R_CERTIFICATE_VERIFY_FAILED:
-            CMP_add_error_data(
-"Cannot authenticate server via its TLS certificate, likely due to mismatch with our trusted TLS certs or missing revocation status");
-            break;
-    /*  case 0x14094418: */ /* xSL_F_SSL3_READ_BYTES */
-        case SSL_AD_REASON_OFFSET+TLS1_AD_UNKNOWN_CA:
-            CMP_add_error_data(
-"Server did not accept our TLS certificate, likely due to mismatch with server's trust anchor or missing revocation status");
-            break;
-        case SSL_AD_REASON_OFFSET+40:
-            CMP_add_error_data(
-                 "Server requires our TLS certificate but did not receive one");
-            break;
-        default:
-            break;
-        }
+        OSSL_CMP_add_error_data(buf);
     }
 }
 
@@ -438,9 +403,9 @@ int OSSL_CMP_MSG_http_perform(OSSL_CMP_CTX *ctx, const OSSL_CMP_MSG *req,
             add_conn_error_hint(ctx, ERR_peek_error());
     }
 
-    if (ctx->http_cb && (*ctx->http_cb)(ctx, hbio, 0) == NULL)
+    if (ctx->http_cb && (*ctx->http_cb)(ctx, hbio, ERR_peek_error()) == NULL)
         err = CMP_R_OUT_OF_MEMORY;
-    BIO_free_all(hbio); /* also frees any BIOs linked with hbio
+    BIO_free_all(hbio); /* also frees any (e.g., SSL/TLS) BIOs linked with hbio
        and, like BIO_reset(hbio), calls SSL_shutdown() to notify/alert peer */
 
     return err;

crypto/cmp/cmp_int.h
@@ -696,10 +696,6 @@ DECLARE_ASN1_FUNCTIONS(CMP_PROTECTEDPART)
 X509_EXTENSIONS *CMP_exts_dup(X509_EXTENSIONS *extin);
 
 /* from cmp_lib.c */
-void CMP_add_error_txt(const char *separator, const char *txt);
-# define CMP_add_error_data(txt) CMP_add_error_txt(":", txt)
-# define CMP_add_error_line(txt) CMP_add_error_txt("\n", txt)
-
 OSSL_CMP_PKIFREETEXT *CMP_PKIFREETEXT_push_str(OSSL_CMP_PKIFREETEXT *ft,
                                                const char *text);
 

crypto/cmp/cmp_lib.c
@@ -45,14 +45,14 @@
 #include "cmp_int.h"
 
 
-/* TODO DvO push this functions upstream to crypto/err (PR #add_error_txt) */
+/* TODO DvO push this function upstream to crypto/err (PR #add_error_txt) */
 /*
  * Appends text to the extra error data field of the last error message in
  * OpenSSL's error queue, after adding the given separator string. Note that,
  * in contrast, ERR_add_error_data() simply overwrites the previous contents
  * of the error data.
  */
-void CMP_add_error_txt(const char *separator, const char *txt)
+void OSSL_CMP_add_error_txt(const char *separator, const char *txt)
 {
     const char *file;
     int line;

crypto/cmp/cmp_ses.c
@@ -203,7 +203,7 @@ static int send_receive_check(OSSL_CMP_CTX *ctx, const OSSL_CMP_MSG *req,
             CMPerr(func, not_received);
         else {
             CMPerr(func, CMP_R_ERROR_SENDING_REQUEST);
-            CMP_add_error_data(type_string);
+            OSSL_CMP_add_error_data(type_string);
         }
         *rep = NULL;
         return 0;
@@ -534,8 +534,7 @@ static int cert_response(OSSL_CMP_CTX *ctx, long rid, OSSL_CMP_MSG **resp,
             goto retry; /* got rp/cp/kup which might still indicate 'waiting' */
         } else {
             CMPerr(func, not_received);
-            ERR_add_error_data(1,
-                             "received 'waiting' pkistatus but polling failed");
+            ERR_add_error_data(1, "received 'waiting' pkistatus but polling failed");
             *resp = NULL;
             return 0;
         }
@@ -545,7 +544,7 @@ static int cert_response(OSSL_CMP_CTX *ctx, long rid, OSSL_CMP_MSG **resp,
         return 0;
     if ((ctx->newClCert = get_cert_status(ctx, (*resp)->body->type,
                                           crep)) == NULL) {
-        CMP_add_error_data("cannot extract certificate from response");
+        OSSL_CMP_add_error_data("cannot extract certificate from response");
         return 0;
     }
 
@@ -612,7 +611,7 @@ static int cert_response(OSSL_CMP_CTX *ctx, long rid, OSSL_CMP_MSG **resp,
         CMPerr(func, CMP_R_CERTIFICATE_NOT_ACCEPTED);
         ERR_add_error_data(1, "rejecting newly enrolled cert");
         if (txt != NULL)
-            CMP_add_error_txt("; ", txt);
+            OSSL_CMP_add_error_txt("; ", txt);
         return 0;
     }
     return ret;

crypto/cmp/cmp_vfy.c
@@ -249,7 +249,7 @@ void put_cert_verify_err(int func)
         CMPerr(func, CMP_R_POTENTIALLY_INVALID_CERTIFICATE);
         if (len > 0) {
             str[len-1] = '\0'; /* replace last '\n', terminating str */
-            CMP_add_error_line(str);
+            OSSL_CMP_add_error_line(str);
         }
         BIO_free(cert_verify_err_bio);
         cert_verify_err_bio = NULL;
@@ -331,9 +331,9 @@ static void add_name_mismatch_data(const char *error_prefix,
     char *expected = X509_NAME_oneline(expected_name, NULL, 0);
     char *actual = actual_name != NULL ? X509_NAME_oneline(actual_name, NULL, 0)
                                        : "(none)";
-    CMP_add_error_txt("", error_prefix);
-    CMP_add_error_txt("\n   actual = ", actual);
-    CMP_add_error_txt("\n expected = ", expected);
+    OSSL_CMP_add_error_txt("", error_prefix);
+    OSSL_CMP_add_error_txt("\n   actual = ", actual);
+    OSSL_CMP_add_error_txt("\n expected = ", expected);
     OPENSSL_free(expected);
     OPENSSL_free(actual);
 }
@@ -348,7 +348,7 @@ static int check_kid(X509 *cert, const ASN1_OCTET_STRING *skid, int fn)
         if (ckid == NULL) {
             if (fn != 0)
                 CMPerr(fn, CMP_R_UNEXPECTED_SENDER);
-            CMP_add_error_line(" missing Subject Key Identifier in certificate");
+            OSSL_CMP_add_error_line(" missing Subject Key Identifier in certificate");
             return 0;
         }
         if (ASN1_OCTET_STRING_cmp(ckid, skid) != 0) {
@@ -357,13 +357,13 @@ static int check_kid(X509 *cert, const ASN1_OCTET_STRING *skid, int fn)
 #endif
             if (fn != 0)
                 CMPerr(fn, CMP_R_UNEXPECTED_SENDER);
-            CMP_add_error_line(" certificate Subject Key Identifier does not match senderKID:");
+            OSSL_CMP_add_error_line(" certificate Subject Key Identifier does not match senderKID:");
 #ifdef hex_to_string
             str = OPENSSL_buf2hexstr(ckid->data, ckid->length);
-            CMP_add_error_txt("\n   actual = ", str);
+            OSSL_CMP_add_error_txt("\n   actual = ", str);
             OPENSSL_free(str);
             str = OPENSSL_buf2hexstr(skid->data, skid->length);
-            CMP_add_error_txt("\n expected = ", str);
+            OSSL_CMP_add_error_txt("\n expected = ", str);
             OPENSSL_free(str);
 #endif
             return 0;
@@ -391,7 +391,7 @@ static int cert_acceptable(X509 *cert, const OSSL_CMP_MSG *msg,
 
     /* TODO better also check revocation state with OCSP/CRLs if avaialble */
     if (OSSL_CMP_expired(X509_get0_notAfter(cert), vpm)) {
-        CMP_add_error_data(" expired");
+        OSSL_CMP_add_error_data(" expired");
         return 0;
     }
 
@@ -400,7 +400,7 @@ static int cert_acceptable(X509 *cert, const OSSL_CMP_MSG *msg,
 
         /* enforce that the right subject DN is there */
         if (name == NULL) {
-            CMP_add_error_data(" missing subject in certificate");
+            OSSL_CMP_add_error_data(" missing subject in certificate");
             return 0;
         }
         if (X509_NAME_cmp(name, sender_name) != 0) {
@@ -437,8 +437,8 @@ static int find_acceptable_certs(STACK_OF(X509) *certs,
         X509 *cert = sk_X509_value(certs, i);
         char *str = X509_NAME_oneline(X509_get_subject_name(cert), NULL, 0);
 
-        CMP_add_error_line("  considering cert with subject");
-        CMP_add_error_txt(" = ", str);
+        OSSL_CMP_add_error_line("  considering cert with subject");
+        OSSL_CMP_add_error_txt(" = ", str);
         OPENSSL_free(str);
 
         if (cert_acceptable(cert, msg, ts) &&
@@ -485,9 +485,9 @@ static STACK_OF(X509) *find_server_cert(const X509_STORE *ts,
     if (!find_acceptable_certs(untrusted, msg, ts, found_certs))
         goto oom;
 
-    CMP_add_error_line(sk_X509_num(found_certs) ?
-                       "found at least one matching server cert" :
-                       "no matching server cert found");
+    OSSL_CMP_add_error_line(sk_X509_num(found_certs) ?
+                            "found at least one matching server cert" :
+                            "no matching server cert found");
     return found_certs;
 oom:
     sk_X509_pop_free(found_certs, X509_free);
@@ -555,7 +555,7 @@ static X509 *find_srvcert(OSSL_CMP_CTX *ctx, const OSSL_CMP_MSG *msg)
         char *sname = X509_NAME_oneline(sender->d.directoryName, NULL, 0);
         (void)ERR_set_mark();
         CMPerr(CMP_F_FIND_SRVCERT, CMP_R_NO_VALID_SERVER_CERT_FOUND);
-        CMP_add_error_txt("\ntrying to match msg sender name = ", sname);
+        OSSL_CMP_add_error_txt("\ntrying to match msg sender name = ", sname);
         OPENSSL_free(sname);
 
         /* release any cached cert, which is no more acceptable */
@@ -723,15 +723,15 @@ int OSSL_CMP_validate_msg(OSSL_CMP_CTX *ctx, const OSSL_CMP_MSG *msg)
             if (!X509_find_by_issuer_and_serial(msg->extraCerts,
                                 X509_get_issuer_name(scrt),
                                 (ASN1_INTEGER *)X509_get0_serialNumber(scrt))) {
-                CMP_add_error_line(" certificate used for signature verification attempt was not found in extraCerts");
+                OSSL_CMP_add_error_line(" certificate used for signature verification attempt was not found in extraCerts");
             }
 
             if (msg->header->senderKID == NULL)
-                CMP_add_error_line(" no senderKID in CMP header; risk that correct server cert could not be identified");
+                OSSL_CMP_add_error_line(" no senderKID in CMP header; risk that correct server cert could not be identified");
             else /* server cert should match senderKID in header */
                 if (!check_kid(scrt, msg->header->senderKID, 0))
                     /* here this can only happen if ctx->srvCert has been set */
-                    CMP_add_error_line(" for senderKID in CMP header there is no matching Subject Key Identifier in context-provided server cert");
+                    OSSL_CMP_add_error_line(" for senderKID in CMP header there is no matching Subject Key Identifier in context-provided server cert");
         } else {
             CMPerr(CMP_F_OSSL_CMP_VALIDATE_MSG, CMP_R_NO_SUITABLE_SERVER_CERT);
         }

doc/man3/CMP_CTX.pod
@@ -435,12 +435,18 @@ to a structure containing arguments, previously set by
 OSSL_CMP_CTX_set_transfer_cb_arg().
 
 OSSL_CMP_CTX_set_http_cb() sets the optional http connect/disconnect callback
-function, which may modify the HTTP BIO given in the B<hbio> argument
+function, which has the prototype
+
+typedef BIO *(*OSSL_cmp_http_cb_t) (OSSL_CMP_CTX *ctx, BIO *hbio,
+                                    unsigned long detail);
+
+It may modify the HTTP BIO given in the B<hbio> argument
 used by OSSL_CMP_MSG_http_perform().
-On connect, the B<connect> argument is 1, and on disconnect it is 0.
+On connect the B<detail> argument is 1.
+On disconnect it is 0 if no error occurred or else the last error code.
 For instance, on connect a TLS BIO may be prepended to implement HTTPS,
-and on disconnect any cleanup may be done.
-The callback function may return NULL to indicate an error.
+and on disconnect some error diagnostics and/or cleanup may be done.
+The callback function should return NULL to indicate failure.
 It may make use of a custom defined argument stored in the ctx
 by means of OSSL_CMP_CTX_set_http_cb_arg(),
 which may be retrieved again through OSSL_CMP_CTX_get_http_cb_arg().

commit 4a7d9a840ef8ba1c4fb5e9261b5334e9243434a7

fix minor build issue introduced in commit 884f23b8b720

apps/cmp.c
@@ -1958,7 +1958,7 @@ static int read_write_req_resp(OSSL_CMP_CTX *ctx, const OSSL_CMP_MSG *req,
     return ret;
 }
 
-const char *tls_error_hint(unsigned long err)
+static const char *tls_error_hint(unsigned long err)
 {
     switch(ERR_GET_REASON(err)) {
 /*  case 0x1408F10B: */ /* xSL_F_SSL3_GET_RECORD */