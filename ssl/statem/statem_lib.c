582585236e8ff8d35f4358a8c527b8456d2e40c1
Jacob Brown <jacob@gnu.org>
2018-08-19 12:44:41 -0500

    Fixing ssl_get_client_min_max_version() not checking if version is disabled or not

--- ssl/statem/statem_lib.c
+++ ssl/statem/statem_lib.c
@@ -1015,6 +1015,22 @@ int ssl_get_client_min_max_version(const SSL *s, int *min_version,
          * versions they don't want.  If not, then easy to fix, just return
          * ssl_method_error(s, s->method)
          */
+#define CHECK_REQUESTED_VERSION_IS_NOT_DISABLED
+#ifdef CHECK_REQUESTED_VERSION_IS_NOT_DISABLED
+    {
+        int found = 0;
+        for (vent = SSL_IS_DTLS(s) ? dtls_version_table : tls_version_table; vent->version != 0; ++vent) {
+            // Note, When disabled, the value version may be in the table still, but cmeth and smeth will be NULL pointers.
+            if ((s->version == vent->version) && (vent->cmeth != NULL) && (vent->smeth != NULL)) {
+                found = 1;
+                break;
+            }
+        }
+        if (!found) {
+            return SSL_R_NO_PROTOCOLS_AVAILABLE;
+        }
+    }
+#endif //CHECK_REQUESTED_VERSION_IS_NOT_DISABLED
         *min_version = *max_version = s->version;
         return 0;
     case TLS_ANY_VERSION:
