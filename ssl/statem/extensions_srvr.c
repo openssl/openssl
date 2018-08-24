2ce2b71594b6c3eb9b71b1a327ef54ea88d22a1a
Matt Caswell <matt@openssl.org>
2018-08-24 16:16:28 +0100

    Send a NewSessionTicket after using an external PSK
    Treat a connection using an external PSK like we would a resumption and
    send a single NewSessionTicket afterwards.
    
    Fixes #6941

--- ssl/statem/extensions_srvr.c
+++ ssl/statem/extensions_srvr.c
@@ -1160,6 +1160,7 @@ int tls_parse_ctos_psk(SSL *s, PACKET *pkt, unsigned int context, X509 *x,
             ext = 1;
             if (id == 0)
                 s->ext.early_data_ok = 1;
+            s->ext.ticket_expected = 1;
         } else {
             uint32_t ticket_age = 0, now, agesec, agems;
             int ret;
@@ -1235,6 +1236,7 @@ int tls_parse_ctos_psk(SSL *s, PACKET *pkt, unsigned int context, X509 *x,
             SSL_SESSION_free(sess);
             sess = NULL;
             s->ext.early_data_ok = 0;
+            s->ext.ticket_expected = 0;
             continue;
         }
         break;
