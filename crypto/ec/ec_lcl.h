fff1da43be2236995cdf5ef2f3e2a51be232ba85
Nicola Tuveri <nic.tuv@gmail.com>
2018-09-07 00:44:36 +0300

    Access `group->mont_data` conditionally in EC_GROUP_set_generator()
    It appears that, in FIPS mode, `ec_precompute_mont_data()` always failed
    but the error was ignored until commit e3ab8cc from #6810.
    
    The actual problem lies in the fact that access to the `mont_data` field
    of an `EC_GROUP` struct should always be guarded by an
    `EC_GROUP_VERSION(group)` check to avoid OOB accesses, because `group`
    might come from the FIPS module, which does not define the `mont_data`
    field inside the EC_GROUP structure.
    
    This commit adds the required check before any access to
    `group->mont_data` in `EC_GROUP_set_generator()`.
    
    Fixes #7127
    
    Reviewed-by: Tim Hudson <tjh@openssl.org>
    Reviewed-by: Matthias St. Pierre <Matthias.St.Pierre@ncp-e.com>
    (Merged from https://github.com/openssl/openssl/pull/7135)

--- crypto/ec/ec_lcl.h
+++ crypto/ec/ec_lcl.h
@@ -214,7 +214,7 @@ struct ec_group_st {
     int asn1_flag;              /* flag to control the asn1 encoding */
     /*
      * Kludge: upper bit of ans1_flag is used to denote structure
-     * version. Is set, then last field is present. This is done
+     * version. If set, then last field is present. This is done
      * for interoperation with FIPS code.
      */
 #define EC_GROUP_ASN1_FLAG_MASK 0x7fffffff
@@ -549,7 +549,6 @@ void ec_GFp_nistp_points_make_affine_internal(size_t num, void *point_array,
 void ec_GFp_nistp_recode_scalar_bits(unsigned char *sign,
                                      unsigned char *digit, unsigned char in);
 #endif
-int ec_precompute_mont_data(EC_GROUP *);
 
 #ifdef ECP_NISTZ256_ASM
 /** Returns GFp methods using montgomery multiplication, with x86-64 optimized
