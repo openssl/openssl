4ee2a66d82e2851423fa95445ecd54eba3f70c9e
Nicola Tuveri <nic.tuv@gmail.com>
2018-09-07 00:44:36 +0300

    Access `group->mont_data` conditionally in EC_GROUP_set_generator()
    It appears that, in FIPS mode, `ec_precompute_mont_data()` always failed
    but the error was ignored until commit e3ab8cc from #6810.
    
    The actual problem lies in the fact that access to the `mont_data` field
    of an `EC_GROUP` struct should always be guarded by an
    `EC_GROUP_VERSION(group)` check to avoid OOB accesses, because `group`
    might come from the FIPS module, which does not define the `mont_data`
    field inside the EC_GROUP structure.
    
    This commit adds the required check before any access to
    `group->mont_data` in `EC_GROUP_set_generator()`.
    
    Fixes #7127

--- crypto/ec/ec_lib.c
+++ crypto/ec/ec_lib.c
@@ -318,17 +318,25 @@ int EC_GROUP_set_generator(EC_GROUP *group, const EC_POINT *generator,
     } else
         BN_zero(&group->cofactor);
 
-    /*
-     * Some groups have an order with
-     * factors of two, which makes the Montgomery setup fail.
-     * |group->mont_data| will be NULL in this case.
+    /*-
+     * Access to the `mont_data` field of an EC_GROUP struct should always be
+     * guarded by an EC_GROUP_VERSION(group) check to avoid OOB accesses, as the
+     * group might come from the FIPS module, which does not define the
+     * `mont_data` field inside the EC_GROUP structure.
      */
-    if (BN_is_odd(&group->order)) {
-        return ec_precompute_mont_data(group);
+    if (EC_GROUP_VERSION(group)) {
+        /*-
+         * Some groups have an order with
+         * factors of two, which makes the Montgomery setup fail.
+         * |group->mont_data| will be NULL in this case.
+         */
+        if (BN_is_odd(&group->order))
+            return ec_precompute_mont_data(group);
+
+        BN_MONT_CTX_free(group->mont_data);
+        group->mont_data = NULL;
     }
 
-    BN_MONT_CTX_free(group->mont_data);
-    group->mont_data = NULL;
     return 1;
 }
 
