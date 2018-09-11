1212818eb07add297fe562eba80ac46a9893781e
Matt Caswell <matt@openssl.org>
2018-09-11 13:22:14 +0100

    Update copyright year
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/7176)

--- crypto/camellia/camellia.c
+++ crypto/camellia/camellia.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2016 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2006-2018 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the OpenSSL license (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
