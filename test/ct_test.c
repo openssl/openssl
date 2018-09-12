622fc2e051bc3c61a09d08f1fafdb45f6f8499b0
Matt Caswell <matt@openssl.org>
2018-09-12 16:49:19 +0100

    Add an explicit cast to time_t
    Caused a compilation failure in some environments
    
    Fixes #7204
    
    Reviewed-by: Viktor Dukhovni <viktor@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/7205)
    
    (cherry picked from commit bc278f30f0b766bfb82426c641dc1d51ace4a994)

--- test/ct_test.c
+++ test/ct_test.c
@@ -542,8 +542,8 @@ static int test_default_ct_policy_eval_ctx_time_is_now()
 {
     int success = 0;
     CT_POLICY_EVAL_CTX *ct_policy_ctx = CT_POLICY_EVAL_CTX_new();
-    const time_t default_time = CT_POLICY_EVAL_CTX_get_time(ct_policy_ctx) /
-            1000;
+    const time_t default_time =
+        (time_t)(CT_POLICY_EVAL_CTX_get_time(ct_policy_ctx) / 1000);
     const time_t time_tolerance = 600;  /* 10 minutes */
 
     if (fabs(difftime(time(NULL), default_time)) > time_tolerance) {
