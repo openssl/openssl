1fd6afb571e85fbc37ffb522646e7ec2c6e4a11e
Bernd Edlinger <bernd.edlinger@hotmail.de>
2018-09-13 18:25:37 +0200

    Create the .rnd file it it does not exist
    It's a bit annoying, since some commands try to read a .rnd file,
    and print an error message if the file does not exist.
    
    But previously a .rnd file was created on exit, and that does no longer
    happen.
    
    Fixed by continuing in app_RAND_load_conf regardless of the error in
    RAND_load_file.
    
    If the random number generator is still not initalized on exit, the
    function RAND_write_file will fail and no .rnd file would be created.
    
    Remove RANDFILE from openssl.cnf
    
    Reviewed-by: Kurt Roeckx <kurt@roeckx.be>
    (Merged from https://github.com/openssl/openssl/pull/7217)
    
    (cherry picked from commit 0f58220973a02248ca5c69db59e615378467b9c8)

--- apps/app_rand.c
+++ apps/app_rand.c
@@ -26,7 +26,6 @@ void app_RAND_load_conf(CONF *c, const char *section)
     if (RAND_load_file(randfile, -1) < 0) {
         BIO_printf(bio_err, "Can't load %s into RNG\n", randfile);
         ERR_print_errors(bio_err);
-        return;
     }
     if (save_rand_file == NULL)
         save_rand_file = OPENSSL_strdup(randfile);
