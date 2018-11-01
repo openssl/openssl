892e6d981573e5ce33cb0b51b32d5ece62e7262a
Antoine Salon <asalon@vmware.com>
2018-11-01 11:56:55 -0700

    Added SRP_VBASE_add0_user()
    Signed-off-by: Antoine Salon <asalon@vmware.com>

--- include/openssl/srp.h
+++ include/openssl/srp.h
@@ -75,6 +75,7 @@ SRP_VBASE *SRP_VBASE_new(char *seed_key);
 void SRP_VBASE_free(SRP_VBASE *vb);
 int SRP_VBASE_init(SRP_VBASE *vb, char *verifier_file);
 
+int SRP_VBASE_add0_user(SRP_VBASE *vb, SRP_user_pwd *user_pwd);
 /* This method ignores the configured seed and fails for an unknown user. */
 DEPRECATEDIN_1_1_0(SRP_user_pwd *SRP_VBASE_get_by_user(SRP_VBASE *vb, char *username))
 /* NOTE: unlike in SRP_VBASE_get_by_user, caller owns the returned pointer.*/
