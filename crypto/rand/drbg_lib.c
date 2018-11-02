c40c1ef4f3c3a6a4d7878bbf8b13742a5bffd963
Bernd Edlinger <bernd.edlinger@hotmail.de>
2018-11-02 11:46:38 +0100

    Fix error handling in RAND_DRBG_uninstantiate
    Reviewed-by: Matthias St. Pierre <Matthias.St.Pierre@ncp-e.com>
    Reviewed-by: Paul Dale <paul.dale@oracle.com>
    (Merged from https://github.com/openssl/openssl/pull/7519)

--- crypto/rand/drbg_lib.c
+++ crypto/rand/drbg_lib.c
@@ -374,6 +374,7 @@ int RAND_DRBG_instantiate(RAND_DRBG *drbg,
 int RAND_DRBG_uninstantiate(RAND_DRBG *drbg)
 {
     if (drbg->meth == NULL) {
+        drbg->state = DRBG_ERROR;
         RANDerr(RAND_F_RAND_DRBG_UNINSTANTIATE,
                 RAND_R_NO_DRBG_IMPLEMENTATION_SELECTED);
         return 0;
