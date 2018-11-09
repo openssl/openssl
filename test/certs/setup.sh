d9f467e45a8a5849b9acb9c865fed8da7af3143b
Matt Caswell <matt@openssl.org>
2018-11-09 11:40:33 +0000

    fixup! Don't negotiate TLSv1.3 if our EC cert isn't TLSv1.3 capable

--- test/certs/setup.sh
+++ test/certs/setup.sh
@@ -366,6 +366,6 @@ REQMASK=MASK:0x800 ./mkcert.sh req badalt7-key "O = Bad NC Test Certificate 7" \
 ./mkcert.sh genee PSS-SHA256 ee-key ee-pss-sha256-cert ca-key ca-cert \
     -sha256 -sigopt rsa_padding_mode:pss -sigopt rsa_pss_saltlen:digest
 
-./mkcert.sh genee "Server ECDSA brainpoolP256r1 cert" \
-    server-ecdsa-brainpoolP256r1-key server-ecdsa-brainpoolP256r1-cert rootkey \
-    rootcert
+OPENSSL_KEYALG=ec OPENSSL_KEYBITS=brainpoolP256r1 ./mkcert.sh genee \
+    "Server ECDSA brainpoolP256r1 cert" server-ecdsa-brainpoolP256r1-key \
+    server-ecdsa-brainpoolP256r1-cert rootkey rootcert
