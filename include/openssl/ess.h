5418ae0e147253a07781415101ee0996642c4426
Antonio Iacono <antiac@gmail.com>
2018-12-09 05:59:38 -0500

    Re-licensing and ifndef changed

--- include/openssl/ess.h
+++ include/openssl/ess.h
@@ -1,7 +1,7 @@
 /*
  * Copyright 2018 The OpenSSL Project Authors. All Rights Reserved.
  *
- * Licensed under the OpenSSL license (the "License").  You may not use
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
  * in the file LICENSE in the source distribution or at
  * https://www.openssl.org/source/license.html
@@ -12,7 +12,7 @@
 
 # include <openssl/opensslconf.h>
 
-# if !defined(OPENSSL_NO_CMS) || !defined(OPENSSL_NO_TS)
+# ifndef OPENSSL_NO_CMS
 
 # ifdef  __cplusplus
 extern "C" {
