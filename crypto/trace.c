1759dee81d5125416517284498415c46d9c3a6d9
Richard Levitte <levitte@openssl.org>
2018-12-11 23:58:29 +0100

    Add generic trace and debug hook API
    The idea is that the application shall be able to register callbacks
    to print tracing and debugging output as it sees fit.
    
    OpenSSL internals, on the other hand, want to print thoses texts using
    normal printing routines, such as BIO_printf() or BIO_dump().
    
    Internally, this is solved by creating BIOs that simply forward
    received text to the appropriate application provided callback.

---
+++ crypto/trace.c
@@ -0,0 +1,214 @@
+/*
+ * Copyright 2018 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <openssl/bio.h>
+#include <openssl/crypto.h>
+#include <openssl/trace.h>
+#include "internal/bio.h"
+#include "internal/nelem.h"
+
+/*-
+ * INTERNAL BIO IMPLEMENTATION
+ *
+ * For our own flexibility, we have an internal BIO whose only job is
+ * to call the application provided hooks for trace and debug output.
+ */
+static int hookwrite(BIO *h, const char *buf, size_t num, size_t *written);
+static int hookputs(BIO *h, const char *str);
+static const BIO_METHOD hook_method = {
+    BIO_TYPE_SOURCE_SINK,
+    "memory buffer",
+    hookwrite,
+    NULL,                        /* old write */
+    NULL,                        /* read_ex */
+    NULL,                        /* read */
+    hookputs,
+    NULL,                        /* gets */
+    NULL,                        /* ctrl */
+    NULL,                        /* create */
+    NULL,                        /* free */
+    NULL,                        /* callback_ctrl */
+};
+
+/* This structure connects the BIO to the corresponding hook function */
+struct bio_hook_st {
+    OSSL_tracer_fn hook;
+    void *hookdata;
+};
+
+static int hookwrite(BIO *h, const char *buf, size_t num, size_t *written)
+{
+    struct bio_hook_st *biodata = (struct bio_hook_st *)BIO_get_data(h);
+    size_t cnt = biodata->hook(buf, num, biodata->hookdata);
+
+    *written = cnt;
+    return cnt != 0;
+}
+
+static int hookputs(BIO *h, const char *str)
+{
+    size_t written;
+
+    if (hookwrite(h, str, strlen(str), &written))
+        return (int)written;
+
+    return EOF;
+}
+
+/* Helper struct and macro to get name string to number mapping */
+struct namenum_st {
+    const char * const name;
+    const int num;
+};
+#define DEFNAME(typename)       { #typename, OSSL_DEBUG_##typename }
+
+/*-
+ * TRACE
+ */
+
+static const struct namenum_st tracenames[] = {
+    DEFNAME(DEFAULT),
+};
+
+int OSSL_trace_get_type(const char *name)
+{
+    size_t i;
+
+    for (i = 0; i < OSSL_NELEM(tracenames); i++)
+        if (strcasecmp(name, tracenames[i].name) == 0)
+            return tracenames[i].num;
+    return -1;                   /* or should that be 0, i.e. DEFAULT? */
+}
+
+/* We store one trace BIO for each trace type */
+static struct bio_hook_st trace_data[OSSL_TRACE_NUM] = { { NULL, NULL }, };
+static BIO *trace_bio[OSSL_TRACE_NUM] = { NULL, };
+
+void OSSL_trace_set(int type, OSSL_tracer_fn fn, void *hookdata)
+{
+    if (trace_data[type].hook != NULL && fn == NULL) {
+        BIO_free(trace_bio[type]);
+        trace_bio[type] = NULL;
+    }
+
+    trace_data[type].hook = fn;
+    trace_data[type].hookdata = hookdata;
+
+    if (trace_bio[type] == NULL
+        && fn != NULL
+        && (trace_bio[type] = BIO_new(&hook_method)) != NULL)
+        BIO_set_data(trace_bio[type], &trace_data[type]);
+}
+
+int OSSL_trace_is_set(int type)
+{
+    return OSSL_trace_bio(type) != NULL;
+}
+
+BIO *OSSL_trace_bio(int type)
+{
+    if (trace_bio[type] != NULL)
+        return trace_bio[type];
+    return trace_bio[OSSL_TRACE_DEFAULT];
+}
+
+int OSSL_trace(int type, char *fmt, ...)
+{
+    va_list args;
+    int ret;
+
+    va_start(args, fmt);
+    ret = OSSL_vtrace(type, fmt, args);
+    va_end(args);
+
+    return ret;
+}
+
+int OSSL_vtrace(int type, char *fmt, va_list args)
+{
+    BIO *bio = OSSL_trace_bio(type);
+
+    if (bio != NULL)
+        return BIO_vprintf(bio, fmt, args) >= 0;
+    return 1;
+}
+
+/*-
+ * DEBUG
+ */
+
+static const struct namenum_st debugnames[] = {
+    DEFNAME(DEFAULT),
+};
+
+int OSSL_debug_get_type(const char *name)
+{
+    size_t i;
+
+    for (i = 0; i < OSSL_NELEM(debugnames); i++)
+        if (strcasecmp(name, debugnames[i].name) == 0)
+            return debugnames[i].num;
+    return -1;                   /* or should that be 0, i.e. DEFAULT? */
+}
+
+/* We store one trace BIO for each debug type */
+static struct bio_hook_st debug_data[OSSL_DEBUG_NUM] = { { NULL, NULL }, };
+static BIO *debug_bio[OSSL_DEBUG_NUM] = { NULL, };
+
+void OSSL_debug_set(int type, OSSL_tracer_fn fn, void *hookdata)
+{
+    if (debug_data[type].hook != NULL && fn == NULL) {
+        BIO_free(debug_bio[type]);
+        debug_bio[type] = NULL;
+    }
+
+    debug_data[type].hook = fn;
+    debug_data[type].hookdata = hookdata;
+
+    if (debug_bio[type] == NULL
+        && fn != NULL
+        && (debug_bio[type] = BIO_new(&hook_method)) != NULL)
+        BIO_set_data(debug_bio[type], &debug_data[type]);
+}
+
+int OSSL_debug_is_set(int type)
+{
+    return OSSL_debug_bio(type) != NULL;
+}
+
+BIO *OSSL_debug_bio(int type)
+{
+    if (debug_bio[type] != NULL)
+        return debug_bio[type];
+    return debug_bio[OSSL_DEBUG_DEFAULT];
+}
+
+int OSSL_debug(int type, char *fmt, ...)
+{
+    va_list args;
+    int ret = 0;
+
+    va_start(args, fmt);
+    ret = OSSL_vdebug(type, fmt, args);
+    va_end(args);
+
+    return ret;
+}
+
+int OSSL_vdebug(int type, char *fmt, va_list args)
+{
+    BIO *bio = OSSL_debug_bio(type);
+
+    if (bio != NULL)
+        return BIO_vprintf(bio, fmt, args) >= 0;
+    return 1;
+}
---
+++ include/openssl/trace.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2018 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#ifndef OSSL_TRACE_H
+# define OSSL_TRACE_H
+
+# include <stdarg.h>
+
+# include <openssl/bio.h>
+
+# ifdef  __cplusplus
+extern "C" {
+# endif
+
+/*
+ * OSSL_tracer_fn is the type of the function that the application defines.
+ * It MUST return the number of bytes written, or 0 on error (in other words,
+ * it can never write zero bytes).
+ *
+ * The data to write will always be text, and may be several lines.
+ */
+typedef size_t (*OSSL_tracer_fn)(const char *buf, size_t cnt, void *hookdata);
+
+/*
+ * The DEBUG and TRACE types or "channels" that the application can register
+ * a hook for.  The DEFAULT type works as a single fallback.
+ */
+# define OSSL_DEBUG_DEFAULT             0 /* The fallback */
+# define OSSL_DEBUG_NUM                 1
+
+# define OSSL_TRACE_DEFAULT             0 /* The fallback */
+# define OSSL_TRACE_NUM                 1
+
+/* Functions to get a type number from its name */
+int OSSL_trace_get_type(const char *name);
+int OSSL_debug_get_type(const char *name);
+
+/* Functions to associate a hook with a TRACE and DEBUG type / "channel" */
+void OSSL_trace_set(int type, OSSL_tracer_fn fn, void *hookdata);
+void OSSL_debug_set(int type, OSSL_tracer_fn fn, void *hookdata);
+
+/*
+ * Functions to check that a type / "channel" has a hook registered.
+ * These are used within OpenSSL libraries to avoid unnecessary work.
+ */
+int OSSL_trace_is_set(int type);
+int OSSL_debug_is_set(int type);
+
+/*
+ * Each type / "channel" is implemented as a BIO.  These functions are used to
+ * get that BIO, which can then be used with any chose BIO output function.
+ */
+BIO *OSSL_trace_bio(int type);
+BIO *OSSL_debug_bio(int type);
+
+/*
+ * Convenience functions that are shortcuts for
+ * BIO_printf(BIO_trace_bio(type), fmt, ...),
+ * BIO_vprintf(BIO_trace_bio(type), fmt, args),
+ * BIO_printf(BIO_debug_bio(type), fmt, ...),
+ * BIO_vprintf(BIO_debug_bio(type), fmt, args),
+ */
+int OSSL_trace(int type, char *fmt, ...);
+int OSSL_vtrace(int type, char *fmt, va_list args);
+int OSSL_debug(int type, char *fmt, ...);
+int OSSL_vdebug(int type, char *fmt, va_list args);
+
+#endif
