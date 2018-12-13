9ef073a37998874f16d0af9c088a43dd210a77da
Richard Levitte <levitte@openssl.org>
2018-12-13 12:35:48 +0100

    Adapt BN_CTX_DEBUG to the new generic trace API
    Co-authored-by: Dr. Matthias St. Pierre <Matthias.St.Pierre@ncp-e.com>

--- crypto/trace.c
+++ crypto/trace.c
@@ -130,6 +130,7 @@ static const struct trace_category_st trace_categories[] = {
     TRACE_CATEGORY_(PKCS12_KEYGEN),
     TRACE_CATEGORY_(PKCS12_DECRYPT),
     TRACE_CATEGORY_(X509V3_POLICY),
+    TRACE_CATEGORY_(BN_CTX),
 };
 
 const char *OSSL_trace_get_category_name(int num)
