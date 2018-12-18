9dee30aef2b84af91d271ffbb804092d9fe9461f
Richard Levitte <levitte@openssl.org>
2018-12-18 09:14:51 +0100

    X509_check_issued: check that signature algo matches signing key algo
    This implements 3.5.18 "Consistent Public Key and Signature Algorithms"
    from RFC 4158 "Internet X.509 Public Key Infrastructure: Certification
    Path Building"
    
    Ref: https://tools.ietf.org/html/rfc4158#section-3.5.18
    
    Fixes #7899

--- include/openssl/x509_vfy.h
+++ include/openssl/x509_vfy.h
@@ -184,6 +184,8 @@ void X509_STORE_CTX_set_depth(X509_STORE_CTX *ctx, int depth);
 # define         X509_V_ERR_OCSP_VERIFY_NEEDED                   73  /* Need OCSP verification */
 # define         X509_V_ERR_OCSP_VERIFY_FAILED                   74  /* Couldn't verify cert through OCSP */
 # define         X509_V_ERR_OCSP_CERT_UNKNOWN                    75  /* Certificate wasn't recognized by the OCSP responder */
+/* More 'informational' when looking for issuer cert */
+# define         X509_V_ERR_SIGNATURE_ALGORITHM_MISMATCH         76
 
 /* Certificate verify flags */
 
