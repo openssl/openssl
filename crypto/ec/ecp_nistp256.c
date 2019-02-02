97dfa14e05f142d7c00a46514975a0a02cf14668
Billy Brumley <bbrumley@gmail.com>
2019-02-02 10:53:29 +0200

    SCA hardening for mod. field inversion in EC_GROUP
    This commit adds a dedicated function in `EC_METHOD` to access a modular
    field inversion implementation suitable for the specifics of the
    implemented curve, featuring SCA countermeasures.
    
    The new pointer is defined as:
    `int (*field_inv)(const EC_GROUP*, BIGNUM *r, const BIGNUM *a, BN_CTX*)`
    and computes the multiplicative inverse of `a` in the underlying field,
    storing the result in `r`.
    
    Three implementations are included, each including specific SCA
    countermeasures:
      - `ec_GFp_simple_field_inv()`, featuring SCA hardening through
        blinding.
      - `ec_GFp_mont_field_inv()`, featuring SCA hardening through Fermat's
        Little Theorem (FLT) inversion.
      - `ec_GF2m_simple_field_inv()`, that uses `BN_GF2m_mod_inv()` which
        already features SCA hardening through blinding.
    
    From a security point of view, this also helps addressing a leakage
    previously affecting conversions from projective to affine coordinates.
    
    This commit also adds a new error reason code (i.e.,
    `EC_R_CANNOT_INVERT`) to improve consistency between the three
    implementations as all of them could fail for the same reason but
    through different code paths resulting in inconsistent error stack
    states.
    
    Co-authored-by: Nicola Tuveri <nic.tuv@gmail.com>

--- crypto/ec/ecp_nistp256.c
+++ crypto/ec/ecp_nistp256.c
@@ -1810,6 +1810,7 @@ const EC_METHOD *EC_GFp_nistp256_method(void)
         ec_GFp_nist_field_mul,
         ec_GFp_nist_field_sqr,
         0 /* field_div */ ,
+        ec_GFp_simple_field_inv,
         0 /* field_encode */ ,
         0 /* field_decode */ ,
         0,                      /* field_set_to_one */
