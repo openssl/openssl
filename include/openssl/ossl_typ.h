d64b62998beceef8a8b8e1558862eb1c671d677a
Richard Levitte <levitte@openssl.org>
2019-02-06 17:42:50 +0100

    Add an OpenSSL library context
    The context builds on CRYPTO_EX_DATA, allowing it to be dynamically
    extended with new data from the different parts of libcrypto.
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/8225)

--- include/openssl/ossl_typ.h
+++ include/openssl/ossl_typ.h
@@ -179,6 +179,8 @@ typedef struct ct_policy_eval_ctx_st CT_POLICY_EVAL_CTX;
 typedef struct ossl_store_info_st OSSL_STORE_INFO;
 typedef struct ossl_store_search_st OSSL_STORE_SEARCH;
 
+typedef struct openssl_ctx_st OPENSSL_CTX;
+
 #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L && \
     defined(INTMAX_MAX) && defined(UINTMAX_MAX)
 typedef intmax_t ossl_intmax_t;
