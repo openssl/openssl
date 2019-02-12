d62cfb9174ada486159a5b50c5b8161e2185c195
Richard Levitte <levitte@openssl.org>
2019-02-12 11:37:43 +0100

    Move libapps headers into their own directory
    This got triggered by test/testutil.h including ../apps/opt.h.
    
    Some compilers do all inclusions from the directory of the C file
    being compiled, so when a C file includes a header file with a
    relative file spec, and that header file also includes another header
    file with a relative file spec, the compiler no longer follows.
    
    As a specific example, test/testutil/basic_output.c included
    ../testutil.h.  Fine so far, but then, test/testutil.h includes
    ../apps/opt.h, and the compiler ends up trying to include (seen from
    the source top) test/apps/opt.h rather than apps/opt.h, and fails.
    
    The solution could have been to simply add apps/ as an inclusion
    directory.  However, that directory also has header files that have
    nothing to do with libapps, so we take this a bit further, create
    apps/include and move libapps specific headers there, and then add
    apps/include as inclusion directory in the build.info files where
    needed.

--- test/testutil.h
+++ test/testutil.h
@@ -15,7 +15,7 @@
 #include <openssl/err.h>
 #include <openssl/e_os2.h>
 #include <openssl/bn.h>
-#include "../apps/opt.h"
+#include "opt.h"
 
 /*-
  * Simple unit tests should implement setup_tests().
