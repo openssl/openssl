41bffb9da082e49378ef0c027beac75399315bb9
Eneas U de Queiroz <cote2004-github@yahoo.com>
2019-02-15 15:51:59 -0200

    engines/e_devcrypto: fixes logic in close_devcrypto
    Call close(cfd) before setting cfd = -1.
    
    Signed-off-by: Eneas U de Queiroz <cote2004-github@yahoo.com>
    
    Reviewed-by: Paul Dale <paul.dale@oracle.com>
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/8213)

--- engines/e_devcrypto.c
+++ engines/e_devcrypto.c
@@ -1183,10 +1183,13 @@ static int open_devcrypto(void)
 
 static int close_devcrypto(void)
 {
+    int ret;
+
     if (cfd < 0)
         return 1;
+    ret = close(cfd);
     cfd = -1;
-    if (close(cfd) == 0) {
+    if (ret != 0) {
         fprintf(stderr, "Error closing /dev/crypto: %s\n", strerror(errno));
         return 0;
     }
