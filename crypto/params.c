4539ebccc6c118ea0b3e5598ef2dd86e6b5e02c8
Pauli <paul.dale@oracle.com>
2019-02-22 12:21:33 +1000

    Numeric OSSL_PARAM helper functions.
    Provide a number of functions to allow parameters to be set and retrieved
    in a type safe manner.  Functions are provided for many integral types
    plus float and double.
    
    All of the integer functions will widen and narrow the parameter data as
    required.  This permits a degree of malleability in the parameter definition.
    For example a type can be changed from a thirty two bit integer to a sixty
    four bit one without changing application code.  The fast path case where
    the types match is dealt with directly, so it is only when the types don't
    match that there is a performance impact.
    
    The two pairs of real functions accept all real and integer types and
    automaticly convert as required.  There could be some loss of precision
    when converting wide integral types to reals, but the magnitude will
    remain correct.  Unlike the integer functions which accept any width input,
    the real functions require standard integer widths.
    
    A pair of functions is available for BIGNUMs.  These accept any unsigned
    integer input and convert to/from a BIGNUM.
    
    Finally, some utility macros are defined to allow OSSL_PARAM definition
    arrays to be more simplify specified.  There is one macro for each native
    integer type and both floating point types.

---
+++ crypto/params.c
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2019 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright (c) 2019, Oracle and/or its affiliates.  All rights reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#include <string.h>
+#include <openssl/params.h>
+
+#define SET_RETURN_SIZE(p, sz) \
+    if (p->return_size != NULL) \
+        *p->return_size = (sz)
+
+const OSSL_PARAM *OSSL_PARAM_locate(const OSSL_PARAM *p, const char *key)
+{
+    if (p != NULL && key != NULL)
+        for (; p->key != NULL; p++)
+            if (strcmp(key, p->key) == 0)
+                return p;
+    return NULL;
+}
+
+#define PARAM_INT_GET(to, from) \
+    case sizeof(from): \
+        if (sizeof(to) >= sizeof(from)) { \
+            *val = (to)*(const from *)p->buffer; \
+            return 1; \
+        } \
+        break
+
+#define PARAM_INT_SET(to, from) \
+    case sizeof(to): \
+        SET_RETURN_SIZE(p, sizeof(to)); \
+        if (sizeof(to) >= sizeof(from)) { \
+            *(to *)p->buffer = (to)val; \
+            return 1; \
+        } \
+        break
+
+#define PARAM_INT(name, type, pre) \
+    int OSSL_PARAM_get_##name(const OSSL_PARAM *p, type *val) \
+    { \
+        if (val == NULL \
+            || p == NULL \
+            || (p->data_type != OSSL_PARAM_INTEGER \
+                && p->data_type != OSSL_PARAM_UNSIGNED_INTEGER)) \
+            return 0; \
+        switch (p->buffer_size) { \
+        PARAM_INT_GET(type, pre ## 32_t); \
+        PARAM_INT_GET(type, pre ## 64_t); \
+        } \
+        return 0; \
+    } \
+    int OSSL_PARAM_set_##name(const OSSL_PARAM *p, type val) \
+    { \
+        if (p == NULL) \
+            return 0; \
+        SET_RETURN_SIZE(p, 0); \
+        if (p->data_type == OSSL_PARAM_INTEGER \
+            || p->data_type == OSSL_PARAM_UNSIGNED_INTEGER) \
+            switch (p->buffer_size) { \
+            PARAM_INT_SET(pre ## 32_t, type); \
+            PARAM_INT_SET(pre ## 64_t, type); \
+            } \
+        return 0; \
+    }
+
+PARAM_INT(int, int, int)
+PARAM_INT(uint, unsigned int, uint)
+PARAM_INT(long, long int, int)
+PARAM_INT(ulong, unsigned long int, uint)
+PARAM_INT(int32, int32_t, int)
+PARAM_INT(uint32, uint32_t, uint)
+PARAM_INT(int64, int64_t, int)
+PARAM_INT(uint64, uint64_t, uint)
+PARAM_INT(size_t, size_t, uint)
+
+int OSSL_PARAM_get_BN(const OSSL_PARAM *p, BIGNUM **val)
+{
+    BIGNUM *b;
+    size_t size;
+
+    if (val == NULL || p == NULL)
+        return 0;
+
+    if (p->data_type == OSSL_PARAM_UNSIGNED_INTEGER) {
+        size = p->return_size != NULL ? *p->return_size : p->buffer_size;
+        b = BN_native2bn(p->buffer, (int)size, *val);
+        if (b != NULL) {
+            *val = b;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int OSSL_PARAM_set_BN(const OSSL_PARAM *p, const BIGNUM *val)
+{
+    const size_t bytes = (size_t)BN_num_bytes(val);
+
+    if (p == NULL)
+        return 0;
+
+    if (val != NULL && p->data_type == OSSL_PARAM_UNSIGNED_INTEGER) {
+        SET_RETURN_SIZE(p, bytes);
+        return p->buffer_size >= bytes
+               && BN_bn2nativepad(val, p->buffer, bytes) >= 0;
+    }
+    SET_RETURN_SIZE(p, 0);
+    return 0;
+}
+
+int OSSL_PARAM_get_double(const OSSL_PARAM *p, double *val)
+{
+#define CASE(type) \
+    case sizeof(type): \
+        *val = (double)(*(type *)p->buffer); \
+        return 1
+
+    if (val == NULL || p == NULL)
+        return 0;
+
+    switch (p->data_type) {
+    case OSSL_PARAM_REAL:
+        switch (p->buffer_size) {
+        CASE(double);
+        }
+        break;
+    case OSSL_PARAM_INTEGER:
+        switch (p->buffer_size) {
+        CASE(int32_t);
+        CASE(int64_t);
+        }
+        break;
+    case OSSL_PARAM_UNSIGNED_INTEGER:
+        switch (p->buffer_size) {
+        CASE(uint32_t);
+        CASE(uint64_t);
+        }
+    }
+    return 0;
+#undef CASE
+}
+
+int OSSL_PARAM_set_double(const OSSL_PARAM *p, double val)
+{
+#define CASE(type) \
+    case sizeof(type): \
+        *(type *)p->buffer = (type)val; \
+        SET_RETURN_SIZE(p, sizeof(type)); \
+        return 1
+
+    if (p == NULL)
+        return 0;
+
+    SET_RETURN_SIZE(p, sizeof(double));
+    switch (p->data_type) {
+    case OSSL_PARAM_REAL:
+        switch (p->buffer_size) {
+        CASE(double);
+        }
+        break;
+    case OSSL_PARAM_INTEGER:
+        switch (p->buffer_size) {
+        CASE(int32_t);
+        CASE(int64_t);
+        }
+        break;
+    case OSSL_PARAM_UNSIGNED_INTEGER:
+        switch (p->buffer_size) {
+        CASE(uint32_t);
+        CASE(uint64_t);
+        }
+        break;
+    }
+    return 0;    
+#undef CASE
+}
