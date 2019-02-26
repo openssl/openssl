f27f22d5506480c42b82cd9248ec9657d6a6b769
Matt Caswell <matt@openssl.org>
2019-02-26 14:07:28 +0000

    Update copyright year

--- crypto/evp/evp_test.c
+++ crypto/evp/evp_test.c
@@ -1,6 +1,6 @@
 /* Written by Ben Laurie, 2001 */
 /*
- * Copyright (c) 2001 The OpenSSL Project.  All rights reserved.
+ * Copyright (c) 2001-2019 The OpenSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
