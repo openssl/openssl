fcfd8c7af531c969b2e41106cf33c379d8512e0f
Rich Salz <rsalz@akamai.com>
2019-02-28 17:06:25 -0500

    Add some tests

--- test/params_test.c
+++ test/params_test.c
@@ -12,20 +12,37 @@
 #include "internal/nelem.h"
 #include <openssl/params.h>
 
+#define IVAL    22
+#define U64VAL  0xDEADBEEF
+
 static int test_params(void)
 {
     size_t sz;
     int i;
+    uint64_t u64 = U64VAL, alt64;
     const OSSL_PARAM params[] = {
+        OSSL_PARAM_uint64("uv", &u64),
         OSSL_PARAM_int("a", &i),
         OSSL_PARAM_size_t("b", &sz),
+        { NULL }
     };
     int r = 0;
 
-    if (!TEST_true(OSSL_PARAM_set_int(params, "a", 22))
-        || !TEST_int_eq(i, 22)
-        || TEST_true(OSSL_PARAM_get_size_t(params, "a", &sz)))
+    /* Set and get. */
+    if (!TEST_true(OSSL_PARAM_set_int(params, "a", IVAL))
+            || !TEST_int_eq(i, IVAL))
         goto err;
+
+    /* Auto-set. */
+    if (!TEST_true(OSSL_PARAM_get_uint64(params, "uv", &alt64))
+            || !TEST_true(alt64 == u64))
+        goto err;
+
+    /* Type mismatches. */
+    if (!TEST_false(OSSL_PARAM_get_uint64(params, "a", &alt64))
+            || !TEST_false(OSSL_PARAM_get_int(params, "uv", &i)))
+        goto err;
+
     r = 1;
 err:
     return r;
