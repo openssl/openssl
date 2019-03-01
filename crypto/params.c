089aa29ef9c602fd8a2b55a51107d4e1bae72e38
Rich Salz <rsalz@akamai.com>
2019-03-01 12:11:34 -0500

    Set error code on PARAM failures

--- crypto/params.c
+++ crypto/params.c
@@ -11,12 +11,16 @@
 #include <string.h>
 #include <openssl/bn.h>
 #include <openssl/params.h>
+#include <openssl/err.h>
+#include <openssl/cryptoerr.h>
 
 static const OSSL_PARAM *OSSL_PARAM_locate(const OSSL_PARAM *p, const char *key)
 {
     for (; p->key != NULL; p++)
         if (strcmp(key, p->key) == 0)
             return p;
+    CRYPTOerr(CRYPTO_F_OSSL_PARAM_LOCATE, CRYPTO_R_PARAM_NOT_FOUND);
+    ERR_add_error_data(2, "param name=", key);
     return NULL;
 }
 
@@ -31,6 +35,7 @@ static size_t OSSL_PARAM_get_width(int type)
     case OSSL_PARAM_LONG:       return sizeof(long);
     case OSSL_PARAM_ULONG:      return sizeof(unsigned long);
     case OSSL_PARAM_SIZET:      return sizeof(size_t);
+    case OSSL_PARAM_DOUBLE:     return sizeof(double);
     }
     return -1;
 }
@@ -46,8 +51,10 @@ static int OSSL_PARAM_get_int_common(const OSSL_PARAM *p, const char *key,
 
     /* Type safety. */
     width = OSSL_PARAM_get_width(type);
-    if (p->data_type != type || p->buffer_size != width)
+    if (p->data_type != type || p->buffer_size != width) {
+        CRYPTOerr(CRYPTO_F_OSSL_PARAM_GET_INT_COMMON, CRYPTO_R_TYPE_MISMATCH);
         return 0;
+    }
 
     memcpy(val, p->buffer, width);
     return 1;
@@ -63,8 +70,10 @@ static int OSSL_PARAM_set_int_common(const OSSL_PARAM *p, const char *key,
 
     /* Type safety. */
     width = OSSL_PARAM_get_width(type);
-    if (p->data_type != type || p->buffer_size != width)
+    if (p->data_type != type || p->buffer_size != width) {
+        CRYPTOerr(CRYPTO_F_OSSL_PARAM_SET_INT_COMMON, CRYPTO_R_TYPE_MISMATCH);
         return 0;
+    }
 
     memcpy(p->buffer, val, width);
     if (p->return_size != NULL)
@@ -90,16 +99,26 @@ PARAM_INT(long, long int, OSSL_PARAM_LONG)
 PARAM_INT(ulong, unsigned long int, OSSL_PARAM_ULONG)
 PARAM_INT(size_t, size_t, OSSL_PARAM_SIZET)
 
+/*
+ * In the get/set double functions, "width" is always sizeof(double) but
+ * having parallel construction with the integer functions above seems
+ * worthwhile.
+ */
+
 int OSSL_PARAM_get_double(const OSSL_PARAM *p, const char *key, double *val)
 {
+    size_t width;
     double *dp;
 
     if ((p = OSSL_PARAM_locate(p, key)) == NULL)
         return 0;
 
     /* Type safety. */
-    if (p->data_type != OSSL_PARAM_DOUBLE || p->buffer_size != sizeof(double))
+    width = OSSL_PARAM_get_width(OSSL_PARAM_DOUBLE);
+    if (p->data_type != OSSL_PARAM_DOUBLE || p->buffer_size != width) {
+        CRYPTOerr(CRYPTO_F_OSSL_PARAM_GET_DOUBLE, CRYPTO_R_TYPE_MISMATCH);
         return 0;
+    }
 
     dp = (double *)p->buffer;
     *val = *dp;
@@ -108,18 +127,22 @@ int OSSL_PARAM_get_double(const OSSL_PARAM *p, const char *key, double *val)
 
 int OSSL_PARAM_set_double(const OSSL_PARAM *p, const char *key, double val)
 {
+    size_t width;
     double *dp;
 
     if ((p = OSSL_PARAM_locate(p, key)) == NULL)
         return 0;
 
     /* Type safety. */
-    if (p->data_type != OSSL_PARAM_DOUBLE || p->buffer_size != sizeof(double))
+    width = OSSL_PARAM_get_width(OSSL_PARAM_DOUBLE);
+    if (p->data_type != OSSL_PARAM_DOUBLE || p->buffer_size != width) {
+        CRYPTOerr(CRYPTO_F_OSSL_PARAM_SET_DOUBLE, CRYPTO_R_TYPE_MISMATCH);
         return 0;
+    }
     dp = (double *)p->buffer;
     *dp = val;
     if (p->return_size != NULL)
-        *p->return_size = sizeof(double);
+        *p->return_size = width;
     return 1;
 }
 
@@ -131,12 +154,13 @@ int OSSL_PARAM_get_BN(const OSSL_PARAM *p, const char *key, BIGNUM **val)
         return 0;
 
     /* Type safety. */
-    if (p->data_type != OSSL_PARAM_BIGNUM)
+    if (p->data_type != OSSL_PARAM_BIGNUM) {
+        CRYPTOerr(CRYPTO_F_OSSL_PARAM_GET_BN, CRYPTO_R_TYPE_MISMATCH);
         return 0;
+    }
 
     if ((b = BN_native2bn(p->buffer, (int)p->buffer_size, *val)) == NULL)
         return 0;
-
     *val = b;
     if (p->return_size != NULL)
         *p->return_size = BN_num_bytes(b);
@@ -152,13 +176,14 @@ int OSSL_PARAM_set_BN(const OSSL_PARAM *p, const char *key, const BIGNUM *val)
         return 0;
 
     /* Type safety. */
-    if (p->data_type != OSSL_PARAM_BIGNUM)
+    if (p->data_type != OSSL_PARAM_BIGNUM || p->buffer_size < bytes) {
+        CRYPTOerr(CRYPTO_F_OSSL_PARAM_SET_BN, CRYPTO_R_TYPE_MISMATCH);
         return 0;
+    }
 
     if (p->buffer_size < bytes
             || (r = BN_bn2nativepad(val, p->buffer, bytes)) < 0)
         return 0;
-
     if (p->return_size != NULL)
         *p->return_size = r;
     return 1;
