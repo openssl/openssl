e3e51afef0a3190a4a4fe725e65c66e51cd054a1
Rich Salz <rsalz@akamai.com>
2019-03-04 18:26:05 -0500

    Remove symhacks from crypto; change mkerr

--- include/openssl/crypto.h
+++ include/openssl/crypto.h
@@ -24,7 +24,6 @@
 # include <openssl/opensslv.h>
 # include <openssl/ossl_typ.h>
 # include <openssl/opensslconf.h>
-# include <openssl/symhacks.h>
 # include <openssl/cryptoerr.h>
 
 # ifdef CHARSET_EBCDIC
