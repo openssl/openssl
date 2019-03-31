31b6ed76dfd53529b74e79830c81372d0b756929
Richard Levitte <levitte@openssl.org>
2019-03-31 16:15:02 +0200

    Rework DSO API conditions and configuration option
    'no-dso' is meaningless, as it doesn't get any macro defined.
    Therefore, we remove all checks of OPENSSL_NO_DSO.  However, there may
    be some odd platforms with no DSO scheme.  For those, we generate the
    internal macro DSO_NONE aand use it.
    
    Reviewed-by: Paul Dale <paul.dale@oracle.com>
    (Merged from https://github.com/openssl/openssl/pull/#8622)

--- include/internal/dsoerr.h
+++ include/internal/dsoerr.h
@@ -17,11 +17,9 @@
 
 # include <openssl/opensslconf.h>
 
-# ifndef OPENSSL_NO_DSO
-
-#  ifdef  __cplusplus
+# ifdef  __cplusplus
 extern "C"
-#  endif
+# endif
 int ERR_load_DSO_strings(void);
 
 /*
@@ -83,5 +81,4 @@ int ERR_load_DSO_strings(void);
 #  define DSO_R_UNLOAD_FAILED                              107
 #  define DSO_R_UNSUPPORTED                                108
 
-# endif
 #endif
