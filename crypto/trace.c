03a2e417dd084ec4ab9575db0b37925e6aeece7c
Richard Levitte <levitte@openssl.org>
2019-04-05 11:25:40 +0200

    fixup! Convert the ENGINE_CONF trace calls to use CONF instead

--- crypto/trace.c
+++ crypto/trace.c
@@ -124,7 +124,7 @@ static const struct trace_category_st trace_categories[] = {
     TRACE_CATEGORY_(INIT),
     TRACE_CATEGORY_(TLS),
     TRACE_CATEGORY_(TLS_CIPHER),
-    TRACE_CATEGORY_(ENGINE_CONF),
+    TRACE_CATEGORY_(CONF),
     TRACE_CATEGORY_(ENGINE_TABLE),
     TRACE_CATEGORY_(ENGINE_REF_COUNT),
     TRACE_CATEGORY_(PKCS5V2),
@@ -132,7 +132,6 @@ static const struct trace_category_st trace_categories[] = {
     TRACE_CATEGORY_(PKCS12_DECRYPT),
     TRACE_CATEGORY_(X509V3_POLICY),
     TRACE_CATEGORY_(BN_CTX),
-    TRACE_CATEGORY_(CONF),
 };
 
 const char *OSSL_trace_get_category_name(int num)
