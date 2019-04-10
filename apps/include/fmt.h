ee215c7eea91f193d4765127eb31332758753058
Jakub Wilk <jwilk@jwilk.net>
2019-04-10 12:00:20 +0200

    Fix typos
    CLA: trivial
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/8714)

--- apps/include/fmt.h
+++ apps/include/fmt.h
@@ -32,8 +32,8 @@
 # define FORMAT_PKCS12   6
 # define FORMAT_SMIME   (7 | B_FORMAT_TEXT)
 # define FORMAT_ENGINE   8                      /* Not really a file format */
-# define FORMAT_PEMRSA  (9 | B_FORMAT_TEXT)     /* PEM RSAPubicKey format */
-# define FORMAT_ASN1RSA  10                     /* DER RSAPubicKey format */
+# define FORMAT_PEMRSA  (9 | B_FORMAT_TEXT)     /* PEM RSAPublicKey format */
+# define FORMAT_ASN1RSA  10                     /* DER RSAPublicKey format */
 # define FORMAT_MSBLOB   11                     /* MS Key blob format */
 # define FORMAT_PVK      12                     /* MS PVK file format */
 # define FORMAT_HTTP     13                     /* Download using HTTP */
