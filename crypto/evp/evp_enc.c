cd1c0aacdc32660539fe4a5549a79797a784c85d
Matt Caswell <matt@openssl.org>
2019-04-19 16:21:10 +0100

    Fix EVP_CIPHER_CTX_rand_key()
    Make sure we use the the correct key length in EVP_CIPHER_CTX_rand_key().
    Now that ciphers may come from providers we need to make sure we ask the
    provider for the value if appropriate.

--- crypto/evp/evp_enc.c
+++ crypto/evp/evp_enc.c
@@ -947,7 +947,7 @@ int EVP_CIPHER_CTX_rand_key(EVP_CIPHER_CTX *ctx, unsigned char *key)
 {
     if (ctx->cipher->flags & EVP_CIPH_RAND_KEY)
         return EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_RAND_KEY, 0, key);
-    if (RAND_priv_bytes(key, ctx->key_len) <= 0)
+    if (RAND_priv_bytes(key, EVP_CIPHER_CTX_key_length(ctx)) <= 0)
         return 0;
     return 1;
 }
