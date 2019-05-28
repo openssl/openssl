14df04ce2ef2192ea3dbe3f263c3cd26e330e5d2
Richard Levitte <richard@levitte.org>
2019-05-28 14:41:38 +0200

    Update copyright year

--- crypto/ec/ecp_oct.c
+++ crypto/ec/ecp_oct.c
@@ -5,7 +5,7 @@
  * OpenSSL project.
  */
 /* ====================================================================
- * Copyright (c) 1998-2002 The OpenSSL Project.  All rights reserved.
+ * Copyright (c) 1998-2019 The OpenSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
--- crypto/ecdh/ech_ossl.c
+++ crypto/ecdh/ech_ossl.c
@@ -14,7 +14,7 @@
  *
  */
 /* ====================================================================
- * Copyright (c) 1998-2003 The OpenSSL Project.  All rights reserved.
+ * Copyright (c) 1998-2019 The OpenSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
