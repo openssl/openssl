d2066ef265c15cb4f529d6baf85865252411280e
Richard Levitte <richard@levitte.org>
2019-05-28 14:49:38 +0200

    Update copyright year

--- crypto/evp/e_aria.c
+++ crypto/evp/e_aria.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2017-2019 The OpenSSL Project Authors. All Rights Reserved.
  * Copyright (c) 2017, Oracle and/or its affiliates.  All rights reserved.
  *
  * Licensed under the OpenSSL license (the "License").  You may not use
