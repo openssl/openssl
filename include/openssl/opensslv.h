9c2539da8264cadfc5c5fb8568ee1602f8b4b31e
Richard Levitte <richard@levitte.org>
2019-05-28 14:33:04 +0200

    Prepare for 1.1.0k release
    Reviewed-by: Matt Caswell <matt@openssl.org>

--- include/openssl/opensslv.h
+++ include/openssl/opensslv.h
@@ -39,11 +39,11 @@ extern "C" {
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-# define OPENSSL_VERSION_NUMBER  0x101000b0L
+# define OPENSSL_VERSION_NUMBER  0x101000bfL
 # ifdef OPENSSL_FIPS
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.0k-fips-dev  xx XXX xxxx"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.0k-fips  28 May 2019"
 # else
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.0k-dev  xx XXX xxxx"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.0k  28 May 2019"
 # endif
 
 /*-
