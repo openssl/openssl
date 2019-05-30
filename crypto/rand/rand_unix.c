8d28a0fc129a089249568c5d70e63df91d3f6b18
Dr. Matthias St. Pierre <Matthias.St.Pierre@ncp-e.com>
2019-05-30 18:52:39 +0200

    drbg: add fork id to additional data on UNIX systems
    Provides a little extra fork-safety on UNIX systems, adding to the
    fact that all DRBGs reseed automatically when the fork_id changes.
    
    Reviewed-by: Paul Dale <paul.dale@oracle.com>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/9802)

--- crypto/rand/rand_unix.c
+++ crypto/rand/rand_unix.c
@@ -699,6 +699,7 @@ int rand_pool_add_nonce_data(RAND_POOL *pool)
 int rand_pool_add_additional_data(RAND_POOL *pool)
 {
     struct {
+        int fork_id;
         CRYPTO_THREAD_ID tid;
         uint64_t time;
     } data;
@@ -708,9 +709,11 @@ int rand_pool_add_additional_data(RAND_POOL *pool)
 
     /*
      * Add some noise from the thread id and a high resolution timer.
+     * The fork_id adds some extra fork-safety.
      * The thread id adds a little randomness if the drbg is accessed
      * concurrently (which is the case for the <master> drbg).
      */
+    data.fork_id = openssl_get_fork_id();
     data.tid = CRYPTO_THREAD_get_current_id();
     data.time = get_timer_bits();
 
