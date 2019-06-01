36105df3ace384a67314dc1d60eecaa37f8175f1
Richard Levitte <levitte@openssl.org>
2019-06-01 14:05:45 +0200

    Move GMAC to providers

--- providers/common/include/internal/provider_algs.h
+++ providers/common/include/internal/provider_algs.h
@@ -70,6 +70,7 @@ extern const OSSL_DISPATCH aria128gcm_functions[];
 extern const OSSL_DISPATCH blake2bmac_functions[];
 extern const OSSL_DISPATCH blake2smac_functions[];
 extern const OSSL_DISPATCH cmac_functions[];
+extern const OSSL_DISPATCH gmac_functions[];
 
 /* Key management */
 extern const OSSL_DISPATCH dh_keymgmt_functions[];
