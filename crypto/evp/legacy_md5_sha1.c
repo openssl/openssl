498ff8be97bfe7f61e3a12bbcce8c3839d36b92a
Richard Levitte <levitte@openssl.org>
2019-06-04 13:58:43 +0200

    Move MD5-SHA1 digest completely to the default provider
    This leaves minimal implementations of EVP_md5_sha1, which is now only
    there to provide a name for implicit fetches.

---
+++ crypto/evp/legacy_md5_sha1.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2015-2019 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#include <openssl/opensslconf.h>
+
+#ifndef OPENSSL_NO_MD5
+
+# include <openssl/obj_mac.h>
+# include "crypto/evp.h"
+
+static const EVP_MD md5_sha1_md = {
+    NID_md5_sha1,
+    NID_md5_sha1,
+};
+
+const EVP_MD *EVP_md5_sha1(void)
+{
+    return &md5_sha1_md;
+}
+
+#endif /* OPENSSL_NO_MD5 */
