f50958b8c80ae267c02b2d534da8bde091010707
Richard Levitte <levitte@openssl.org>
2019-06-04 13:58:43 +0200

    Move MD5-SHA1 digest completely to the default provider
    This leaves minimal implementations of EVP_md5_sha1, which is now only
    there to provide a name for implicit fetches.
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/9076)

--- crypto/evp/legacy_md5_sha1.c
+++ crypto/evp/legacy_md5_sha1.c
@@ -7,47 +7,26 @@
  * https://www.openssl.org/source/license.html
  */
 
+#include <openssl/opensslconf.h>
+
+#include "internal/md5_sha1.h"   /* diverse MD5_SHA1 macros */
+
 #ifndef OPENSSL_NO_MD5
 
-# include <string.h>
-# include <openssl/evp.h>
 # include <openssl/obj_mac.h>
 # include "crypto/evp.h"
-# include "internal/md5_sha1.h"
-
-static int init(EVP_MD_CTX *ctx)
-{
-    return md5_sha1_init(EVP_MD_CTX_md_data(ctx));
-}
-
-static int update(EVP_MD_CTX *ctx, const void *data, size_t count)
-{
-    return md5_sha1_update(EVP_MD_CTX_md_data(ctx), data, count);
-}
-
-static int final(EVP_MD_CTX *ctx, unsigned char *md)
-{
-    return md5_sha1_final(md, EVP_MD_CTX_md_data(ctx));
-}
-
-static int ctrl(EVP_MD_CTX *ctx, int cmd, int mslen, void *ms)
-{
-    return md5_sha1_ctrl(EVP_MD_CTX_md_data(ctx), cmd, mslen, ms);
-}
 
 static const EVP_MD md5_sha1_md = {
     NID_md5_sha1,
     NID_md5_sha1,
     MD5_SHA1_DIGEST_LENGTH,
     0,
-    init,
-    update,
-    final,
+    NULL,
+    NULL,
+    NULL,
     NULL,
     NULL,
     MD5_SHA1_CBLOCK,
-    sizeof(EVP_MD *) + sizeof(MD5_SHA1_CTX),
-    ctrl
 };
 
 const EVP_MD *EVP_md5_sha1(void)
