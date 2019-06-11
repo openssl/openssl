53f5469604b5295a93316ec75f5da6aaf299d892
Shane Lontis <shane.lontis@oracle.com>
2019-06-11 18:19:20 +1000

    Fix app opt compile failure due to missing <inttypes.h>
    opt.c uses functions that are only available if inttypes.h exists.
    It now checks a define which is unavailable if
    inttypes.h is included. The include is done automagically inside e_os2.h.
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/8986)

--- apps/include/opt.h
+++ apps/include/opt.h
@@ -317,7 +317,8 @@ int opt_int(const char *arg, int *result);
 int opt_ulong(const char *arg, unsigned long *result);
 int opt_long(const char *arg, long *result);
 #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L && \
-    defined(INTMAX_MAX) && defined(UINTMAX_MAX)
+    defined(INTMAX_MAX) && defined(UINTMAX_MAX) && \
+    !defined(OPENSSL_NO_INTTYPES_H)
 int opt_imax(const char *arg, intmax_t *result);
 int opt_umax(const char *arg, uintmax_t *result);
 #else
