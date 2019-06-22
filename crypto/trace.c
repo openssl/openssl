1d12afc310e8334e731d2c7acf1c3f1d0395cd03
Richard Levitte <levitte@openssl.org>
2019-06-22 10:08:05 +0200

    crypto/trace.c: Remove unexisting categories
    Reverts an inadvertent change from commit
    fe26f798526c14a3f8c9bb55d0eaf8eaa0d086e1
    
    Fixes #9220
    
    Reviewed-by: Matthias St. Pierre <Matthias.St.Pierre@ncp-e.com>
    (Merged from https://github.com/openssl/openssl/pull/9218)

--- crypto/trace.c
+++ crypto/trace.c
@@ -132,8 +132,6 @@ static const struct trace_category_st trace_categories[] = {
     TRACE_CATEGORY_(PKCS12_DECRYPT),
     TRACE_CATEGORY_(X509V3_POLICY),
     TRACE_CATEGORY_(BN_CTX),
-    TRACE_CATEGORY_(PROV),
-    TRACE_CATEGORY_(FETCH),
 };
 
 const char *OSSL_trace_get_category_name(int num)
