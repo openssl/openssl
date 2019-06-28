f3cd4e35a2996eb293b19b329aba9e931946c3e1
Dmitry Belyavskiy <beldmit@gmail.com>
2019-06-28 21:38:34 +0300

    fixup! Travis build

--- crypto/punycode.c
+++ crypto/punycode.c
@@ -290,25 +290,25 @@ enum punycode_status punycode_decode(punycode_uint input_length,
 static int codepoint2utf8(unsigned char *out, unsigned long utf)
 {
     if (utf <= 0x7F) {
-        // Plain ASCII
+        /* Plain ASCII */
         out[0] = (unsigned char)utf;
         out[1] = 0;
         return 1;
     } else if (utf <= 0x07FF) {
-        // 2-byte unicode
+        /* 2-byte unicode */
         out[0] = (unsigned char)(((utf >> 6) & 0x1F) | 0xC0);
         out[1] = (unsigned char)(((utf >> 0) & 0x3F) | 0x80);
         out[2] = 0;
         return 2;
     } else if (utf <= 0xFFFF) {
-        // 3-byte unicode
+        /* 3-byte unicode */
         out[0] = (unsigned char)(((utf >> 12) & 0x0F) | 0xE0);
         out[1] = (unsigned char)(((utf >> 6) & 0x3F) | 0x80);
         out[2] = (unsigned char)(((utf >> 0) & 0x3F) | 0x80);
         out[3] = 0;
         return 3;
     } else if (utf <= 0x10FFFF) {
-        // 4-byte unicode
+        /* 4-byte unicode */
         out[0] = (unsigned char)(((utf >> 18) & 0x07) | 0xF0);
         out[1] = (unsigned char)(((utf >> 12) & 0x3F) | 0x80);
         out[2] = (unsigned char)(((utf >> 6) & 0x3F) | 0x80);
@@ -316,7 +316,7 @@ static int codepoint2utf8(unsigned char *out, unsigned long utf)
         out[4] = 0;
         return 4;
     } else {
-        // error - use replacement character
+        /* error - use replacement character */
         out[0] = (unsigned char)0xEF;
         out[1] = (unsigned char)0xBF;
         out[2] = (unsigned char)0xBD;
