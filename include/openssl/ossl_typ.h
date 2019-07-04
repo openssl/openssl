470c37f9d3dd2ef73aee3a6138e0303c4c9c1442
Dr. Matthias St. Pierre <Matthias.St.Pierre@ncp-e.com>
2019-07-04 13:40:36 +0200

    add <openssl/ossl_typ.h> compatibility header

---
+++ include/openssl/ossl_typ.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2001-2018 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#ifndef OPENSSL_TYPES_H
+/*
+ * The original <openssl/ossl_typ.h> was renamed to <openssl/types.h>
+ *
+ * This header file only exists for compatibility reasons with older
+ * applications which #include <openssl/types.h>.
+ */
+# include <openssl/types.h>
+#endif
