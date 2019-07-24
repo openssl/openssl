13ee76d37f9cc12bcded9091c0b0b577cb9ee75e
Richard Levitte <levitte@openssl.org>
2019-07-24 22:19:37 +0200

    HMAC: use EVP_MD_flags(), not EVP_MD_meth_get_flags()
    The latter should only be ussed with legacy methods.

--- crypto/hmac/hmac.c
+++ crypto/hmac/hmac.c
@@ -39,7 +39,7 @@ int HMAC_Init_ex(HMAC_CTX *ctx, const void *key, int len,
      * The HMAC construction is not allowed to be used with the
      * extendable-output functions (XOF) shake128 and shake256.
      */
-    if ((EVP_MD_meth_get_flags(md) & EVP_MD_FLAG_XOF) != 0)
+    if ((EVP_MD_flags(md) & EVP_MD_FLAG_XOF) != 0)
         return 0;
 
     if (key != NULL) {
