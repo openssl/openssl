e5d4233fbd07eac52227c7ec5f479a46f15914bf
Richard Levitte <levitte@openssl.org>
2019-07-26 18:11:55 +0200

    Deprecate ERR_get_state()
    Internally, we still need this function, so we make it internal and
    then add a new ERR_get_state() that simply calls the internal variant,
    unless it's "removed" by configuration.
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/9462)

--- fuzz/x509.c
+++ fuzz/x509.c
@@ -19,7 +19,7 @@
 int FuzzerInitialize(int *argc, char ***argv)
 {
     OPENSSL_init_crypto(OPENSSL_INIT_LOAD_CRYPTO_STRINGS, NULL);
-    ERR_get_state();
+    ERR_clear_error();
     CRYPTO_free_ex_index(0, -1);
     FuzzerSetRand();
     return 1;
--- include/openssl/err.h
+++ include/openssl/err.h
@@ -333,7 +333,7 @@ int ERR_load_ERR_strings(void);
 
 DEPRECATEDIN_1_1_0(void ERR_remove_thread_state(void *))
 DEPRECATEDIN_1_0_0(void ERR_remove_state(unsigned long pid))
-ERR_STATE *ERR_get_state(void);
+DEPRECATEDIN_3(ERR_STATE *ERR_get_state(void))
 
 int ERR_get_next_error_library(void);
 
