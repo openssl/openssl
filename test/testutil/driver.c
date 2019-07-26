e9a5932d04f6b7dd25b39a8ff9dc162d64a78c22
Pauli <paul.dale@oracle.com>
2019-07-26 12:56:01 +1000

    Add weak platform independent PRNG to test framework.
    Implement the GNU C library's random(3) pseudorandom number generator.
    The algorithm is described: https://www.mscs.dal.ca/~selinger/random/
    
    The rationale is to make the tests repeatable across differing platforms with
    different underlying implementations of the random(3) library call.
    
    More specifically: when executing tests with random ordering.
    
    [extended tests]
    
    Reviewed-by: Bernd Edlinger <bernd.edlinger@hotmail.de>
    (Merged from https://github.com/openssl/openssl/pull/9463)

--- test/testutil/driver.c
+++ test/testutil/driver.c
@@ -114,7 +114,7 @@ static void set_seed(int s)
         seed = (int)time(NULL);
     test_printf_stdout("%*s# RAND SEED %d\n", subtest_level(), "", seed);
     test_flush_stdout();
-    srand(seed);
+    test_random_seed(seed);
 }
 
 
@@ -326,7 +326,7 @@ int run_tests(const char *test_prog_name)
         permute[i] = i;
     if (seed != 0)
         for (i = num_tests - 1; i >= 1; i--) {
-            j = rand() % (1 + i);
+            j = test_random() % (1 + i);
             ii = permute[j];
             permute[j] = permute[i];
             permute[i] = ii;
@@ -373,7 +373,7 @@ int run_tests(const char *test_prog_name)
                 jstep = 1;
             else
                 do
-                    jstep = rand() % all_tests[i].num;
+                    jstep = test_random() % all_tests[i].num;
                 while (jstep == 0 || gcd(all_tests[i].num, jstep) != 1);
 
             for (jj = 0; jj < all_tests[i].num; jj++) {
