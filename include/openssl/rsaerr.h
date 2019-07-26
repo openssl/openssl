ffc2b6373aabcdcfbb0ac725a00a907834202c4f
David von Oheimb <David.von.Oheimb@siemens.com>
2019-07-26 11:03:12 +0200

    make RSA and DSA operations throw MISSING_PRIVATE_KEY if needed, adapt ECDSA
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Nicola Tuveri <nic.tuv@gmail.com>
    (Merged from https://github.com/openssl/openssl/pull/9466)
    
    (cherry picked from commit 7408f6759f1b0100438ca236ea8f549454aaf2d5)

--- include/openssl/rsaerr.h
+++ include/openssl/rsaerr.h
@@ -130,6 +130,7 @@ int ERR_load_RSA_strings(void);
 # define RSA_R_KEY_PRIME_NUM_INVALID                      165
 # define RSA_R_KEY_SIZE_TOO_SMALL                         120
 # define RSA_R_LAST_OCTET_INVALID                         134
+# define RSA_R_MISSING_PRIVATE_KEY                        179
 # define RSA_R_MGF1_DIGEST_NOT_ALLOWED                    152
 # define RSA_R_MODULUS_TOO_LARGE                          105
 # define RSA_R_MP_COEFFICIENT_NOT_INVERSE_OF_R            168
