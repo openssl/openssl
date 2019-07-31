ca33a43fe21ace99a7e689f40a90e3ad42e794a0
joe2018Outlookcom <joe_2018@Outlook.com>
2019-07-31 13:46:02 +0800

    Fix warning C4164 in MSVC.
    Fix: crypto\whrlpool\wp_block.c(90) : warning C4164: '_rotl64' : intrinsic function not declared.
    Fixes #9487
    
    Reviewed-by: Paul Dale <paul.dale@oracle.com>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/9488)
    
    (cherry picked from commit 0c789f59f117ccbb30ffc621216ba776117c7c61)

--- crypto/whrlpool/wp_block.c
+++ crypto/whrlpool/wp_block.c
@@ -87,6 +87,7 @@ typedef unsigned long long u64;
 #ifndef PEDANTIC
 # if defined(_MSC_VER)
 #  if defined(_WIN64)            /* applies to both IA-64 and AMD64 */
+#   include <stdlib.h>
 #   pragma intrinsic(_rotl64)
 #   define ROTATE(a,n) _rotl64((a),n)
 #  endif
