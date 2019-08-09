3992dc834e0abe24048529640958c3989a8b7415
Matt Caswell <matt@openssl.org>
2019-08-09 14:08:35 +0100

    Remove GCM key length work around
    Now that EVP_CIPHER_CTX_set_key_length() can handle ciphers that don't
    implement the OSSL_CIPHER_PARAM_KEYLEN parameter for setting, we can
    remove the special case GCM work around for this.

--- providers/common/ciphers/gcm.c
+++ providers/common/ciphers/gcm.c
@@ -209,25 +209,6 @@ static int gcm_ctx_set_params(void *vctx, const OSSL_PARAM params[])
         }
     }
 
-    /*
-     * TODO(3.0) Temporary solution to address fuzz test crash, which will be
-     * reworked once the discussion in PR #9510 is resolved. i.e- We need a
-     * general solution for handling missing parameters inside set_params and
-     * get_params methods.
-     */
-    p = OSSL_PARAM_locate_const(params, OSSL_CIPHER_PARAM_KEYLEN);
-    if (p != NULL) {
-        int keylen;
-
-        if (!OSSL_PARAM_get_int(p, &keylen)) {
-            PROVerr(0, PROV_R_FAILED_TO_GET_PARAMETER);
-            return 0;
-        }
-        /* The key length can not be modified for gcm mode */
-        if (keylen != (int)ctx->keylen)
-            return 0;
-    }
-
     return 1;
 }
 
