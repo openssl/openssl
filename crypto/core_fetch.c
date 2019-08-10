d2e5212f05d83a5b50358fe285de8d247f67f5b2
Richard Levitte <levitte@openssl.org>
2019-08-10 07:25:19 +0200

    fixup! crypto/core_fetch.c: before contructing a method, check if it already exists

--- crypto/core_fetch.c
+++ crypto/core_fetch.c
@@ -39,10 +39,11 @@ static void ossl_method_construct_this(OSSL_PROVIDER *provider,
      * to copy a property definition and paste that wherever a property
      * is being queried.
      */
-    if (data->mcm->get(data->libctx, NULL, data->operation_id,
-                       algo->algorithm_name, algo->property_definition,
-                       data->mcm_data) != NULL)
-        return;
+    if ((method = data->mcm->get(data->libctx, NULL, data->operation_id,
+                                 algo->algorithm_name,
+                                 algo->property_definition,
+                                 data->mcm_data)) != NULL)
+        goto end;
 
     if ((method = data->mcm->construct(algo->algorithm_name,
                                        algo->implementation, provider,
@@ -74,6 +75,7 @@ static void ossl_method_construct_this(OSSL_PROVIDER *provider,
                    algo->algorithm_name, algo->property_definition,
                    data->mcm_data);
 
+ end:
     /* refcnt-- because we're dropping the reference */
     data->mcm->destruct(method, data->mcm_data);
 }
