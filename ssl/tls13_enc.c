df08303be7604c607d7b98688cbbbcba07c8ff4c
Todd Short <tshort@akamai.com>
2019-08-15 12:47:51 -0400

    fixup! Use proper secrets for handshake

--- ssl/tls13_enc.c
+++ ssl/tls13_enc.c
@@ -790,10 +790,12 @@ int tls13_change_cipher_state(SSL *s, int which)
         }
     } else if (label == client_application_traffic)
         memcpy(s->client_app_traffic_secret, secret, hashlen);
+#ifndef OPENSSL_NO_QUIC
     else if (label == client_handshake_traffic)
         memcpy(s->client_hand_traffic_secret, secret, hashlen);
     else if (label == server_handshake_traffic)
         memcpy(s->server_hand_traffic_secret, secret, hashlen);
+#endif
 
     if (!ssl_log_secret(s, log_label, secret, hashlen)) {
         /* SSLfatal() already called */
