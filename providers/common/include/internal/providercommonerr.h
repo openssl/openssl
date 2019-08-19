c916f7a529a82ba9e72e37998ff586bcd650757d
Shane Lontis <shane.lontis@oracle.com>
2019-08-19 17:08:27 +1000

    fixup! add aes_ccm to provider

--- providers/common/include/internal/providercommonerr.h
+++ providers/common/include/internal/providercommonerr.h
@@ -28,14 +28,19 @@ int ERR_load_PROV_strings(void);
 #  define PROV_F_AES_BLOCK_FINAL                           0
 #  define PROV_F_AES_BLOCK_UPDATE                          0
 #  define PROV_F_AES_CIPHER                                0
-#  define PROV_F_AES_GET_CTX_PARAMS                        0
-#  define PROV_F_AES_SET_CTX_PARAMS                        0
 #  define PROV_F_AES_DINIT                                 0
 #  define PROV_F_AES_DUPCTX                                0
 #  define PROV_F_AES_EINIT                                 0
+#  define PROV_F_AES_GET_CTX_PARAMS                        0
 #  define PROV_F_AES_INIT_KEY                              0
+#  define PROV_F_AES_SET_CTX_PARAMS                        0
 #  define PROV_F_AES_STREAM_UPDATE                         0
 #  define PROV_F_AES_T4_INIT_KEY                           0
+#  define PROV_F_BLAKE2_MAC_INIT                           0
+#  define PROV_F_BLAKE2_MAC_SET_PARAMS                     0
+#  define PROV_F_GMAC_SET_PARAMS                           0
+#  define PROV_F_KMAC_SET_PARAMS                           0
+#  define PROV_F_POLY1305_SET_PARAMS                       0
 #  define PROV_F_PROV_AES_KEY_GENERIC_INIT                 0
 #  define PROV_F_TRAILINGDATA                              0
 #  define PROV_F_UNPADBLOCK                                0
@@ -51,13 +56,19 @@ int ERR_load_PROV_strings(void);
 # define PROV_R_FAILED_TO_SET_PARAMETER                   104
 # define PROV_R_INVALID_AAD                               108
 # define PROV_R_INVALID_CUSTOM_LENGTH                     111
+# define PROV_R_INVALID_DATA                              115
+# define PROV_R_INVALID_IVLEN                             116
 # define PROV_R_INVALID_IV_LENGTH                         109
+# define PROV_R_INVALID_KEYLEN                            117
 # define PROV_R_INVALID_KEY_LENGTH                        105
 # define PROV_R_INVALID_SALT_LENGTH                       112
 # define PROV_R_INVALID_TAG                               110
+# define PROV_R_INVALID_TAGLEN                            118
 # define PROV_R_NOT_XOF_OR_INVALID_LENGTH                 113
 # define PROV_R_NO_KEY_SET                                114
 # define PROV_R_OUTPUT_BUFFER_TOO_SMALL                   106
+# define PROV_R_TAG_NOTSET                                119
+# define PROV_R_TAG_NOT_NEEDED                            120
 # define PROV_R_WRONG_FINAL_BLOCK_LENGTH                  107
 
 #endif
