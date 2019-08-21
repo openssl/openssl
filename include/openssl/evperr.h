a76a20b150ea508ce1e9f6c9a94f75452b298035
Pauli <paul.dale@oracle.com>
2019-08-21 18:53:07 +1000

    KDF provider conversion error updates - generated

--- include/openssl/evperr.h
+++ include/openssl/evperr.h
@@ -67,6 +67,7 @@ int ERR_load_EVP_strings(void);
 #  define EVP_F_EVP_ENCRYPTUPDATE                          0
 #  define EVP_F_EVP_KDF_CTRL                               0
 #  define EVP_F_EVP_KDF_CTRL_STR                           0
+#  define EVP_F_EVP_KDF_CTX_DUP                            0
 #  define EVP_F_EVP_KDF_CTX_NEW                            0
 #  define EVP_F_EVP_KDF_CTX_NEW_ID                         0
 #  define EVP_F_EVP_KEYEXCH_FETCH                          0
