027cf069052e68503847cac8069f5ddd4d7977c8
Shane Lontis <shane.lontis@oracle.com>
2019-09-15 18:18:10 +1000

    Fix Coverity CID:1453685 'unreachable code' in aes_xts code.

--- providers/common/ciphers/cipher_aes_xts.c
+++ providers/common/ciphers/cipher_aes_xts.c
@@ -176,7 +176,6 @@ static int aes_xts_cipher(void *vctx, unsigned char *out, size_t *outl,
     else if (CRYPTO_xts128_encrypt(&ctx->xts, ctx->base.iv, in, out, inl,
                                    ctx->base.enc))
         return 0;
-    return 1;
 
     *outl = inl;
     return 1;
