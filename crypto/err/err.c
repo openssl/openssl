4ea0317bf498e598147ab8afa17632bc4c5c93b1
Dr. David von Oheimb <David.von.Oheimb@siemens.com>
2019-09-19 15:02:50 +0200

    make default values stored by ERR_get_error_all() and friends more consistent and document them

--- crypto/err/err.c
+++ crypto/err/err.c
@@ -533,36 +533,22 @@ static unsigned long get_error_values(ERR_GET_ACTION g,
         es->err_buffer[i] = 0;
     }
 
-    if (file != NULL && line != NULL) {
-        if (es->err_file[i] == NULL) {
-            *file = "NA";
-            *line = 0;
-        } else {
-            *file = es->err_file[i];
-            *line = es->err_line[i];
-        }
-    }
-
-    if (func != NULL) {
+    if (file != NULL)
+        *file = es->err_file[i];
+    if (line != NULL)
+        *line = es->err_line[i];
+    if (func != NULL)
         *func = es->err_func[i];
-        if (*func == NULL)
-            *func = "N/A";
-    }
-
+    if (flags != NULL)
+        *flags = es->err_data_flags[i];
     if (data == NULL) {
         if (g == EV_POP) {
             err_clear_data(es, i, 0);
         }
     } else {
-        if (es->err_data[i] == NULL) {
-            *data = "";
-            if (flags != NULL)
-                *flags = 0;
-        } else {
-            *data = es->err_data[i];
-            if (flags != NULL)
-                *flags = es->err_data_flags[i];
-        }
+        *data = es->err_data[i];
+         if (*data == NULL && flags != NULL)
+             *flags = 0;
     }
     return ret;
 }
