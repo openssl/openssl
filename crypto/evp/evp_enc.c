5579268d88e137bc56ff58d6a395c8ceaab9a1ef
Shane Lontis <shane.lontis@oracle.com>
2019-09-26 13:07:26 +1000

    fixup! Add rc5 ciphers to default provider

--- crypto/evp/evp_enc.c
+++ crypto/evp/evp_enc.c
@@ -273,6 +273,7 @@ int EVP_CipherInit_ex(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *cipher,
         case NID_rc5_ecb:
         case NID_rc5_cfb64:
         case NID_rc5_ofb64:
+            break;
         default:
             goto legacy;
         }
