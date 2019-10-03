61cc715240d2d3f9511ca88043a3e9797c11482f
Richard Levitte <levitte@openssl.org>
2019-10-03 08:28:31 +0200

    Define AESNI_ASM if AESNI assembler is included, and use it
    Because we have cases where basic assembler support isn't present, but
    AESNI asssembler support is, we need a separate macro that indicates
    that, and use it.
    
    Reviewed-by: Tomas Mraz <tmraz@fedoraproject.org>
    Reviewed-by: Paul Dale <paul.dale@oracle.com>
    (Merged from https://github.com/openssl/openssl/pull/10080)

--- crypto/evp/e_aes_cbc_hmac_sha1.c
+++ crypto/evp/e_aes_cbc_hmac_sha1.c
@@ -33,7 +33,7 @@ typedef struct {
 
 #define NO_PAYLOAD_LENGTH       ((size_t)-1)
 
-#if     defined(AES_ASM) &&     ( \
+#if     defined(AESNI_ASM) &&     ( \
         defined(__x86_64)       || defined(__x86_64__)  || \
         defined(_M_AMD64)       || defined(_M_X64)      )
 
