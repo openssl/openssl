c0848e5e416abc7e4a54c8d1bd5c6508bf9cb219
Dr. Matthias St. Pierre <Matthias.St.Pierre@ncp-e.com>
2019-10-03 14:52:41 +0200

    squash! Update dgst.c to show a list of message digests  [master only]
    Add deprecation for version 3.0

--- apps/enc.c
+++ apps/enc.c
@@ -51,7 +51,9 @@ typedef enum OPTION_choice {
 const OPTIONS enc_options[] = {
     {"help", OPT_HELP, '-', "Display this summary"},
     {"list", OPT_LIST, '-', "List ciphers"},
+#if !OPENSSL_API_3
     {"ciphers", OPT_LIST, '-', "Alias for -list"},
+#endif
     {"in", OPT_IN, '<', "Input file"},
     {"out", OPT_OUT, '>', "Output file"},
     {"pass", OPT_PASS, 's', "Passphrase source"},
