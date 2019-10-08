cc747562e8a7024267ed68e05a307d1b6de913c8
Moreno Carullo <moreno.carullo@nozominetworks.com>
2019-10-08 16:37:19 +0100

    First, initial implementation with integrity-only ciphersuites for TLS 1.3.

--- include/openssl/tls1.h
+++ include/openssl/tls1.h
@@ -613,6 +613,10 @@ __owur int SSL_check_chain(SSL *s, X509 *x, EVP_PKEY *pk, STACK_OF(X509) *chain)
 # define TLS1_3_CK_AES_128_CCM_SHA256                     0x03001304
 # define TLS1_3_CK_AES_128_CCM_8_SHA256                   0x03001305
 
+/* draft-camwinget-tls-ts13-macciphersuites-02 */
+# define TLS1_3_CK_MACCIPHERS_SHA256_SHA256                             0x0300C0B4
+# define TLS1_3_CK_MACCIPHERS_SHA384_SHA384                             0x0300C0B5
+
 /* Aria ciphersuites from RFC6209 */
 # define TLS1_CK_RSA_WITH_ARIA_128_GCM_SHA256             0x0300C050
 # define TLS1_CK_RSA_WITH_ARIA_256_GCM_SHA384             0x0300C051
@@ -692,6 +696,8 @@ __owur int SSL_check_chain(SSL *s, X509 *x, EVP_PKEY *pk, STACK_OF(X509) *chain)
 # define TLS1_3_RFC_CHACHA20_POLY1305_SHA256             "TLS_CHACHA20_POLY1305_SHA256"
 # define TLS1_3_RFC_AES_128_CCM_SHA256                   "TLS_AES_128_CCM_SHA256"
 # define TLS1_3_RFC_AES_128_CCM_8_SHA256                 "TLS_AES_128_CCM_8_SHA256"
+# define TLS1_3_RFC_MACCIPHERS_SHA256_SHA256             "TLS_SHA256_SHA256"
+# define TLS1_3_RFC_MACCIPHERS_SHA384_SHA384             "TLS_SHA384_SHA384"
 # define TLS1_RFC_ECDHE_ECDSA_WITH_NULL_SHA              "TLS_ECDHE_ECDSA_WITH_NULL_SHA"
 # define TLS1_RFC_ECDHE_ECDSA_WITH_DES_192_CBC3_SHA      "TLS_ECDHE_ECDSA_WITH_3DES_EDE_CBC_SHA"
 # define TLS1_RFC_ECDHE_ECDSA_WITH_AES_128_CBC_SHA       "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA"
@@ -1098,6 +1104,10 @@ __owur int SSL_check_chain(SSL *s, X509 *x, EVP_PKEY *pk, STACK_OF(X509) *chain)
 # define TLS1_TXT_ECDHE_PSK_WITH_CHACHA20_POLY1305         "ECDHE-PSK-CHACHA20-POLY1305"
 # define TLS1_TXT_DHE_PSK_WITH_CHACHA20_POLY1305           "DHE-PSK-CHACHA20-POLY1305"
 # define TLS1_TXT_RSA_PSK_WITH_CHACHA20_POLY1305           "RSA-PSK-CHACHA20-POLY1305"
+
+/* draft-camwinget-tls-ts13-macciphersuites-02 */
+# define TLS1_3_TXT_MACCIPHERS_SHA256_SHA256               "SHA256-SHA256"
+# define TLS1_3_TXT_MACCIPHERS_SHA384_SHA384               "SHA384-SHA384"
 
 /* Aria ciphersuites from RFC6209 */
 # define TLS1_TXT_RSA_WITH_ARIA_128_GCM_SHA256             "ARIA128-GCM-SHA256"
