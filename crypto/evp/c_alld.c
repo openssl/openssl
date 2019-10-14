a4ba325363d769453884451c320c4a088d0e8211
Richard Levitte <levitte@openssl.org>
2019-10-14 16:05:54 +0200

    fixup! Move MD2, MD4 and MD5 digests completely to the providers

--- crypto/evp/c_alld.c
+++ crypto/evp/c_alld.c
@@ -16,9 +16,19 @@
 
 void openssl_add_all_digests_int(void)
 {
+#ifndef OPENSSL_NO_MD4
+   EVP_add_digest(EVP_md4());
+#endif
+#ifndef OPENSSL_NO_MD5
+    EVP_add_digest(EVP_md5());
+    EVP_add_digest_alias(SN_md5, "ssl3-md5");
+#endif
     EVP_add_digest(EVP_sha1());
     EVP_add_digest_alias(SN_sha1, "ssl3-sha1");
     EVP_add_digest_alias(SN_sha1WithRSAEncryption, SN_sha1WithRSA);
+#if !defined(OPENSSL_NO_MDC2) && !defined(OPENSSL_NO_DES)
+    EVP_add_digest(EVP_mdc2());
+#endif
 #ifndef OPENSSL_NO_RMD160
     EVP_add_digest(EVP_ripemd160());
     EVP_add_digest_alias(SN_ripemd160, "ripemd");
