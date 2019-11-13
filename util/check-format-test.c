2b1dcf0b8cda44b27e3e6ad40afded876ff7de51
Dr. David von Oheimb <David.von.Oheimb@siemens.com>
2019-11-13 11:00:06 +0100

    add tag '*@' in test input wherever check-format.pl should complain

--- util/check-format-test.c
+++ util/check-format-test.c
@@ -10,65 +10,63 @@
  */
 
 /* This demonstrates/tests cases where check-format.pl should complain */
-/* In total, 55 formattiing issues should be reported for this file */
 
 /* For each of the following set of lines the tool should complain */
-/* tab character: 	 */
-/* cr character:  */
-/* non-printable ASCII character:  */
-/* non-ascii character: ä */
-/* whitespace at EOL: */ 
-// end-of-line comment style not allowed for C90
-/* comment start: /* inside intra-line comment */
-/* multi-line comment with text on first line
- * comment start: /* inside multi-line comment
-  * indent off by 1 in multi-line comment
- multi-line comment with text on last line */
-*/ /* comment end outside comment */
-/* over-loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong line */
- #define X      /* indent of '#' (preprocessor directive) off by 1 */
-# define X      /* indent off by 1 in preprocessor directive */
-typedef struct s  { /* double space, flagged unless sloppy_space */
-     enum {     /* indent off by 1 */
-          x = 1, /* indent off by -1 or 1 */
-           y,z  /* no space after first comma */
-    } type ;    /* space before ; */
-   union {      /* indent off by -1 */
-        struct{ T m; } n; /* no space before { */
-    }p;         /* no space after } */
-    };          /* indent off by 4 */
-fun() {         /* opening brace at end of function definition header */
-    if(cond)) { /* too many closing parens */
-        stmt;   /* single-line statement in braces */
-}}}             /* too many closing braces */
-#endif          /* too many #endif */
- int f(int a,   /* normal indent off by 1 */
-      int b,    /* hanging indent off by -1, flagged unless sloppy_expr */
-        int c)  /* hanging indent off by 1 */
-{ int           /* text after opening brace */
-    x = 0);     /* 1 too many closing paren */
-   g(a,         /* normal indent off by -1 */
-    b,          /* hanging indent off by -1, flagged unless sloppy_expr */
-# define M(X) X /* macro indent off by 1, does not disturb surrounding C code */
-      c,        /* hanging indent off by 1 */
-   d);          /* hanging indent off by -2 */
-    if(e+       /* no space before + */
-       g*= 2    /* no space before *= */
-       h %2     /* no space after % */
-        && 1)   /* indent off by 1 or -3 */
-       cmd;     /* indent off by -1 */
-    while( e2)  /* space after ( */
-         cmd2;  /* indent off by 1 */
-    switch(e ) {/* space before ) */
-   case 1:      /* case indent off by -1 */
-     default:   /* default indent off by 1 */
-}               /* indent off by -4 */
-  label:        /* label indent off by 1*/
-x; }            /* text before closing brace */
-/* Here the tool should stop complaining apart from those three issues at EOF */
+/*@ tab character: 	 */
+/*@ cr character:  */
+/*@ non-printable ASCII character:  */
+/*@ non-ascii character: ä */
+/*@ whitespace at EOL: */ 
+// /*@ end-of-line comment style not allowed for C90 */
+/*@ comment start: /* inside intra-line comment */
+/*@ multi-line comment with text on first line
+ *@ comment start: /* inside multi-line comment
+  *@ indent off by 1 in multi-line comment
+ *@ multi-line comment with text on last line */
+*/ /*@ comment end outside comment */
+/*@ line is 1 column tooooooooooooooooooooooooooooooooooooooooooooooooooo wide */
+ #define X          /*@ indent of '#' (preprocessor directive) off by 1 */
+# define X          /*@ indent off by 1 in preprocessor directive */
+typedef struct s  { /*@ double space, flagged unless sloppy_space */
+     enum {         /*@ indent off by 1 */
+          x = 1,    /*@ indent off by -1 or 1 */
+           y,z      /*@ no space after first comma */
+    } type ;        /*@ space before ; */
+   union {          /*@ indent off by -1 */
+        struct{ T m; } n; /*@ no space before { */
+    }p;             /*@ no space after } */
+    };              /*@ indent off by 4 */
+fun() {             /*@ opening brace at end of function definition header */
+    if(cond)) {     /*@ too many closing parens */
+        stmt;       /*@ single-line statement in braces */
+}}}                 /*@ too many closing braces */
+#endif              /*@ too many #endif */
+ int f(int a,       /*@ normal indent off by 1 */
+      int b,        /*@ hanging indent off by -1, flagged unless sloppy_expr */
+        int c)      /*@ hanging indent off by 1 */
+{ int               /*@ text after opening brace */
+    x = 0);         /*@ 1 too many closing paren */
+   g(a,             /*@ normal indent off by -1 */
+    b,              /*@ hanging indent off by -1, flagged unless sloppy_expr */
+# define M(X) X     /*@ macro indent off by 1 */
+      c,            /*@ hanging indent off by 1 */
+   d);              /*@ hanging indent off by -2 */
+    if(e+           /*@ no space before + */
+       g*= 2        /*@ no space before *= */
+       h %2         /*@ no space after % */
+        && 1)       /*@ indent off by 1 or -3 */
+       cmd;         /*@ indent off by -1 */
+    while( e2)      /*@ space after ( */
+         cmd2;      /*@ indent off by 1 */
+    switch(e ) {    /*@ space before ) */
+   case 1:          /*@ case indent off by -1 */
+     default:       /*@ default indent off by 1 */
+}                   /*@ indent off by -4 */
+  label:            /*@ label indent off by 1*/
+x; }                /*@ text before closing brace */
+/* Here the tool should stop complaining apart from the below issues at EOF */
 
-
-{ /* unclosed brace */
-#if /* unclosed #if */
-/* empty line follows just before EOF: */
+{                   /*@ unclosed brace */
+#if                 /*@ unclosed #if */
+                    /*@ empty line follows just before EOF: */
 
