984cff6ba52655cf8242b3ef6b4e97a5e8b33266
Richard Levitte <levitte@openssl.org>
2019-11-14 12:09:42 +0100

    UI_UTIL_wrap_read_pem_callback(): when |cb| is NULL, use PEM_def_callback
    Fixes #10444
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/10447)
    
    (cherry picked from commit 72a5412b4858cc7c5627a121f78685a2a4065521)

--- crypto/ui/ui_util.c
+++ crypto/ui/ui_util.c
@@ -8,6 +8,7 @@
  */
 
 #include <string.h>
+#include <openssl/pem.h>         /* PEM_def_callback() */
 #include "internal/thread_once.h"
 #include "ui_local.h"
 
@@ -156,7 +157,7 @@ UI_METHOD *UI_UTIL_wrap_read_pem_callback(pem_password_cb *cb, int rwflag)
         return NULL;
     }
     data->rwflag = rwflag;
-    data->cb = cb;
+    data->cb = cb != NULL ? cb : PEM_def_callback;
 
     return ui_method;
 }
