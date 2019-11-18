264b789bc240e6d0f642b274b6ab3cf8470e4aef
Richard Levitte <levitte@openssl.org>
2019-11-18 02:01:13 +0100

    PROV SERIALIZER: add support for writing DSA keys and parameters
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/10394)

--- providers/implementations/serializers/serializer_local.h
+++ providers/implementations/serializers/serializer_local.h
@@ -34,11 +34,24 @@ OSSL_OP_keymgmt_importkey_fn *ossl_prov_get_importkey(const OSSL_DISPATCH *fns);
 
 OSSL_OP_keymgmt_importkey_fn *ossl_prov_get_rsa_importkey(void);
 OSSL_OP_keymgmt_importkey_fn *ossl_prov_get_dh_importkey(void);
+OSSL_OP_keymgmt_importkey_fn *ossl_prov_get_dsa_importkey(void);
 
 int ossl_prov_prepare_dh_params(const void *dh, int nid,
                                 ASN1_STRING **pstr, int *pstrtype);
 int ossl_prov_dh_pub_to_der(const void *dh, unsigned char **pder);
 int ossl_prov_dh_priv_to_der(const void *dh, unsigned char **pder);
+
+int ossl_prov_prepare_dsa_params(const void *dsa, int nid,
+                                ASN1_STRING **pstr, int *pstrtype);
+/*
+ * Special variant of ossl_prov_prepare_dsa_params() that requires all
+ * three parameters (P, Q and G) to be set.  This is used when serializing
+ * the public key.
+ */
+int ossl_prov_prepare_all_dsa_params(const void *dsa, int nid,
+                                     ASN1_STRING **pstr, int *pstrtype);
+int ossl_prov_dsa_pub_to_der(const void *dsa, unsigned char **pder);
+int ossl_prov_dsa_priv_to_der(const void *dsa, unsigned char **pder);
 
 int ossl_prov_print_labeled_bignum(BIO *out, const char *label,
                                    const BIGNUM *n);
@@ -51,6 +64,14 @@ enum dh_print_type {
 };
 
 int ossl_prov_print_dh(BIO *out, DH *dh, enum dh_print_type type);
+
+enum dsa_print_type {
+    dsa_print_priv,
+    dsa_print_pub,
+    dsa_print_params
+};
+
+int ossl_prov_print_dsa(BIO *out, DSA *dsa, enum dsa_print_type type);
 
 int ossl_prov_write_priv_der_from_obj(BIO *out, const void *obj, int obj_nid,
                                       int (*p2s)(const void *obj, int nid,
