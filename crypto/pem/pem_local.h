27418171b8e470530a353c4576e06af47625f12d
Richard Levitte <levitte@openssl.org>
2019-11-27 15:18:50 +0100

    fixup! SERIALIZER: add hooks in PEM_write_bio_ and PEM_write_fp_ routines

--- crypto/pem/pem_local.h
+++ crypto/pem/pem_local.h
@@ -38,8 +38,8 @@
     }                                                                   \
     if (enc != NULL) {                                                  \
         ret = 0;                                                        \
-        if (OSSL_SERIALIZER_CTX_set_cipher_name(ctx,                    \
-                                                EVP_CIPHER_name(enc))) { \
+        if (OSSL_SERIALIZER_CTX_set_cipher(ctx, EVP_CIPHER_name(enc),   \
+                                           NULL)) {                     \
             ret = 1;                                                    \
             if (kstr != NULL                                            \
                 && !OSSL_SERIALIZER_CTX_set_passphrase(ctx, kstr, klen)) \
