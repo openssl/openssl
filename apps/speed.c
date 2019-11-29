c5abbca711b850f2c13250577940ea570a364026
Matt Caswell <matt@openssl.org>
2019-11-29 12:02:54 +0000

    Deprecate the AES_ige_*() functions
    These functions were already partially deprecated. Now we do it fully.

--- apps/speed.c
+++ apps/speed.c
@@ -17,6 +17,9 @@
 #define EdDSA_SECONDS   10
 #define SM2_SECONDS     10
 
+/* We need to use some deprecated APIs */
+#define OPENSSL_INTERNAL_USE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
