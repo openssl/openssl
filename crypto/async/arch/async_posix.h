386909f6c392bccb81357b94d040b4a10b537fb6
H.J. Lu <hongjiu.lu@intel.com>
2019-12-13 16:46:07 -0800

    Use swapcontext for Intel CET
    Use swapcontext for Intel CET to swap shadow stack when switching
    context.  Verified with
    
    $ CC="gcc -Wl,-z,cet-report=error" ./Configure shared linux-x86_64 -fcf-protection
    $ make
    $ make test

--- crypto/async/arch/async_posix.h
+++ crypto/async/arch/async_posix.h
@@ -25,17 +25,27 @@
 #  define ASYNC_POSIX
 #  define ASYNC_ARCH
 
+#  ifdef __CET__
+#    define USE_SWAPCONTEXT
+#  endif
 #  include <ucontext.h>
-#  include <setjmp.h>
+#    ifndef USE_SWAPCONTEXT
+#      include <setjmp.h>
+#    endif
 
 typedef struct async_fibre_st {
     ucontext_t fibre;
+#    ifndef USE_SWAPCONTEXT
     jmp_buf env;
     int env_init;
+#    endif
 } async_fibre;
 
 static ossl_inline int async_fibre_swapcontext(async_fibre *o, async_fibre *n, int r)
 {
+#    ifdef USE_SWAPCONTEXT
+    swapcontext(&o->fibre, &n->fibre);
+#    else
     o->env_init = 1;
 
     if (!r || !_setjmp(o->env)) {
@@ -44,6 +54,7 @@ static ossl_inline int async_fibre_swapcontext(async_fibre *o, async_fibre *n, i
         else
             setcontext(&n->fibre);
     }
+#    endif
 
     return 1;
 }
