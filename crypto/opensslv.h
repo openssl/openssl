12ad22dd16ffe47f8cde3cddb84a160e8cdb3e30
Matt Caswell <matt@openssl.org>
2019-12-20 13:10:12 +0000

    Prepare for 1.0.2v-dev
    Reviewed-by: Paul Yang <kaishen.yy@antfin.com>

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -30,11 +30,11 @@ extern "C" {
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-# define OPENSSL_VERSION_NUMBER  0x1000215fL
+# define OPENSSL_VERSION_NUMBER  0x10002160L
 # ifdef OPENSSL_FIPS
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2u-fips  20 Dec 2019"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2v-fips-dev  xx XXX xxxx"
 # else
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2u  20 Dec 2019"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2v-dev  xx XXX xxxx"
 # endif
 # define OPENSSL_VERSION_PTEXT   " part of " OPENSSL_VERSION_TEXT
 
