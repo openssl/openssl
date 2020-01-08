b2be1ea2c37cc9ac1c44e34f50df2b2207abdbdd
Pauli <paul.dale@oracle.com>
2020-01-09 09:46:13 +1000

    md2: fix preprocessor indentation
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Tomas Mraz <tmraz@fedoraproject.org>
    (Merged from https://github.com/openssl/openssl/pull/10790)

--- include/openssl/md2.h
+++ include/openssl/md2.h
@@ -19,15 +19,15 @@
 # include <openssl/opensslconf.h>
 
 # ifndef OPENSSL_NO_MD2
-# include <stddef.h>
-# ifdef  __cplusplus
+#  include <stddef.h>
+#  ifdef  __cplusplus
 extern "C" {
-# endif
+#  endif
 
 typedef unsigned char MD2_INT;
 
-# define MD2_DIGEST_LENGTH       16
-# define MD2_BLOCK               16
+#  define MD2_DIGEST_LENGTH       16
+#  define MD2_BLOCK               16
 
 typedef struct MD2state_st {
     unsigned int num;
@@ -42,9 +42,9 @@ int MD2_Update(MD2_CTX *c, const unsigned char *data, size_t len);
 int MD2_Final(unsigned char *md, MD2_CTX *c);
 unsigned char *MD2(const unsigned char *d, size_t n, unsigned char *md);
 
-# ifdef  __cplusplus
+#  ifdef  __cplusplus
 }
-# endif
+#  endif
 # endif
 
 #endif
