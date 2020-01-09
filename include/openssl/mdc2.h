5d032bc3c2c507ead87ef1134bb47d296f7ed50e
Pauli <paul.dale@oracle.com>
2020-01-09 10:48:01 +1000

    mdc2: fix preprocessor indentation

--- include/openssl/mdc2.h
+++ include/openssl/mdc2.h
@@ -18,15 +18,15 @@
 
 # include <openssl/opensslconf.h>
 
-#ifndef OPENSSL_NO_MDC2
-# include <stdlib.h>
-# include <openssl/des.h>
-# ifdef  __cplusplus
+# ifndef OPENSSL_NO_MDC2
+#  include <stdlib.h>
+#  include <openssl/des.h>
+#  ifdef  __cplusplus
 extern "C" {
-# endif
+#  endif
 
-# define MDC2_BLOCK              8
-# define MDC2_DIGEST_LENGTH      16
+#  define MDC2_BLOCK              8
+#  define MDC2_DIGEST_LENGTH      16
 
 typedef struct mdc2_ctx_st {
     unsigned int num;
@@ -40,9 +40,9 @@ int MDC2_Update(MDC2_CTX *c, const unsigned char *data, size_t len);
 int MDC2_Final(unsigned char *md, MDC2_CTX *c);
 unsigned char *MDC2(const unsigned char *d, size_t n, unsigned char *md);
 
-# ifdef  __cplusplus
+#  ifdef  __cplusplus
 }
-# endif
+#  endif
 # endif
 
 #endif
