ea99def89894526c9287c141c467da5485107d27
Pauli <paul.dale@oracle.com>
2020-01-14 09:03:55 +1000

    rc4: fix preprocessor indentation

--- include/openssl/rc4.h
+++ include/openssl/rc4.h
@@ -19,10 +19,10 @@
 # include <openssl/opensslconf.h>
 
 # ifndef OPENSSL_NO_RC4
-# include <stddef.h>
-#ifdef  __cplusplus
+#  include <stddef.h>
+#  ifdef  __cplusplus
 extern "C" {
-#endif
+#  endif
 
 typedef struct rc4_key_st {
     RC4_INT x, y;
@@ -34,9 +34,9 @@ void RC4_set_key(RC4_KEY *key, int len, const unsigned char *data);
 void RC4(RC4_KEY *key, size_t len, const unsigned char *indata,
          unsigned char *outdata);
 
-# ifdef  __cplusplus
+#  ifdef  __cplusplus
 }
-# endif
+#  endif
 # endif
 
 #endif
