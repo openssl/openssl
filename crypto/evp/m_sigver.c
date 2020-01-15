1c6f5f5823b5848700deeeb0745c0ed9b8be128e
Richard Levitte <levitte@openssl.org>
2020-01-15 10:46:15 +0100

    fixup! EVP: Add evp_pkey_make_provided() and refactor around it

--- crypto/evp/m_sigver.c
+++ crypto/evp/m_sigver.c
@@ -80,7 +80,7 @@ static int do_sigver_init(EVP_MD_CTX *ctx, EVP_PKEY_CTX **pctx,
     if (provkey == NULL)
         goto legacy;
     EVP_KEYMGMT_up_ref(tmp_keymgmt);
-    EVP_KEYMGMT_free(ctx->keymgmt);
+    EVP_KEYMGMT_free(locpctx->keymgmt);
     locpctx->keymgmt = tmp_keymgmt;
 
     if (locpctx->keymgmt->query_operation_name != NULL)
