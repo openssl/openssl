5ce93e8710d3acc1a13a7c2bf96e5e5226f8714d
Matt Caswell <matt@openssl.org>
2020-01-17 17:39:19 +0000

    Detect EOF while reading in libssl
    If we hit an EOF while reading in libssl then we will report an error
    back to the application (SSL_ERROR_SYSCALL) but errno will be 0. We add
    an error to the stack (which means we instead return SSL_ERROR_SSL) and
    therefore give a hint as to what went wrong.
    
    Contains a partial fix for #10880

--- crypto/bio/bss_sock.c
+++ crypto/bio/bss_sock.c
@@ -101,6 +101,8 @@ static int sock_read(BIO *b, char *out, int outl)
         if (ret <= 0) {
             if (BIO_sock_should_retry(ret))
                 BIO_set_retry_read(b);
+            else if (ret == 0)
+                b->flags |= BIO_FLAGS_IN_EOF;
         }
     }
     return ret;
@@ -150,6 +152,9 @@ static long sock_ctrl(BIO *b, int cmd, long num, void *ptr)
     case BIO_CTRL_DUP:
     case BIO_CTRL_FLUSH:
         ret = 1;
+        break;
+    case BIO_CTRL_EOF:
+        ret = (b->flags & BIO_FLAGS_IN_EOF) != 0 ? 1 : 0;
         break;
     default:
         ret = 0;
