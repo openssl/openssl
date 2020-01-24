6d53ad6b5cf726d92860e973d7bc8c1930762086
Matt Caswell <matt@openssl.org>
2020-01-24 16:07:51 +0000

    Teach more BIOs how to handle BIO_CTRL_EOF
    Reviewed-by: Dmitry Belyavskiy <beldmit@gmail.com>
    (Merged from https://github.com/openssl/openssl/pull/10907)

--- crypto/bio/bss_conn.c
+++ crypto/bio/bss_conn.c
@@ -313,6 +313,8 @@ static int conn_read(BIO *b, char *out, int outl)
         if (ret <= 0) {
             if (BIO_sock_should_retry(ret))
                 BIO_set_retry_read(b);
+            else if (ret == 0)
+                b->flags |= BIO_FLAGS_IN_EOF;
         }
     }
     return ret;
@@ -491,6 +493,9 @@ static long conn_ctrl(BIO *b, int cmd, long num, void *ptr)
             fptr = (BIO_info_cb **)ptr;
             *fptr = data->info_callback;
         }
+        break;
+    case BIO_CTRL_EOF:
+        ret = (b->flags & BIO_FLAGS_IN_EOF) != 0 ? 1 : 0;
         break;
     default:
         ret = 0;
