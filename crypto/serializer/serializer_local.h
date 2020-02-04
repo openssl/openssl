454519287b9c692b2969fed862f07d314792ef26
Richard Levitte <levitte@openssl.org>
2020-02-04 18:48:56 +0100

    fixup! Redesign the KEYMGMT libcrypto <-> provider interface - the basics

--- crypto/serializer/serializer_local.h
+++ crypto/serializer/serializer_local.h
@@ -32,7 +32,6 @@ struct ossl_serializer_ctx_st {
     OSSL_SERIALIZER *ser;
     void *serctx;
 
-    /* One of OSSL_KEYMGMT_WANT_PARAMETERS and OSSL_KEYMGMT_WANT_KEY */
     int selection;
 
     /*
