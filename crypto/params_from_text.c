b7b523d93f4e23da691c5f4e724e4df7ed17b25d
Richard Levitte <levitte@openssl.org>
2020-02-09 13:33:05 +0100

    Refactor OSSL_PARAM_allocate_from_text() for better flexibility
    OSSL_PARAM_allocate_from_text() and OSSL_PARAM_construct_from_text()
    were "do-it-all" kind of functions, which didn't allow much
    flexibility in code that wants to adapt to situations such as the key
    not being found in the descriptor OSSL_PARAM array.
    
    We therefore split the functionality to make key lookup and value
    processing two different functions, thus resulting in the following
    functions:
    
    - OSSL_PARAM_parse_locate_const(), which parses the key and sets flags
      accordingly, but otherwise works like OSSL_PARAM_locate_const().
    - OSSL_PARAM_construct_from_text(), which doesn't look up the key any
      more, but rather only parses the given value, with direction from a
      template OSSL_PARAM item, which could come from a call of
      OSSL_PARAM_parse_locate_const().
    - OSSL_PARAM_allocate_from_text(), which is modified exactly like
      OSSL_PARAM_construct_from_text().
    
    With this split, callers can also ignore OSSL_PARAM_parse_locate_const()
    completely, and work with an descriptor OSSL_PARAM item of their own
    choosing.

--- crypto/params_from_text.c
+++ crypto/params_from_text.c
@@ -12,6 +12,8 @@
 #include <openssl/err.h>
 #include <openssl/params.h>
 
+#define FLAG_HEX               1
+
 /*
  * When processing text to params, we're trying to be smart with numbers.
  * Instead of handling each specific separate integer type, we use a bignum
@@ -20,31 +22,15 @@
  * (if the size can be arbitrary, then we give whatever we have)
  */
 
-static int prepare_from_text(const OSSL_PARAM *paramdefs, const char *key,
-                             const char *value, size_t value_n,
+static int prepare_from_text(const OSSL_PARAM *template,
+                             const char *value, size_t value_n, int flags,
                              /* Output parameters */
-                             const OSSL_PARAM **paramdef, int *ishex,
                              size_t *buf_n, BIGNUM **tmpbn)
 {
-    const OSSL_PARAM *p;
-
-    /*
-     * ishex is used to translate legacy style string controls in hex format
-     * to octet string parameters.
-     */
-    *ishex = strncmp(key, "hex", 3) == 0;
-
-    if (*ishex)
-        key += 3;
-
-    p = *paramdef = OSSL_PARAM_locate_const(paramdefs, key);
-    if (p == NULL)
-        return 0;
-
-    switch (p->data_type) {
+    switch (template->data_type) {
     case OSSL_PARAM_INTEGER:
     case OSSL_PARAM_UNSIGNED_INTEGER:
-        if (*ishex)
+        if ((flags & FLAG_HEX) != 0)
             BN_hex2bn(tmpbn, value);
         else
             BN_dec2bn(tmpbn, value);
@@ -60,7 +46,7 @@ static int prepare_from_text(const OSSL_PARAM *paramdefs, const char *key,
          * it by subtracting 1 here and inverting the bytes in
          * construct_from_text() below.
          */
-        if (p->data_type == OSSL_PARAM_INTEGER && BN_is_negative(*tmpbn)
+        if (template->data_type == OSSL_PARAM_INTEGER && BN_is_negative(*tmpbn)
             && !BN_sub_word(*tmpbn, 1)) {
             return 0;
         }
@@ -71,25 +57,25 @@ static int prepare_from_text(const OSSL_PARAM *paramdefs, const char *key,
          * TODO(v3.0) is this the right way to do this?  This code expects
          * a zero data size to simply mean "arbitrary size".
          */
-        if (p->data_size > 0) {
-            if (*buf_n >= p->data_size) {
+        if (template->data_size > 0) {
+            if (*buf_n >= template->data_size) {
                 CRYPTOerr(0, CRYPTO_R_TOO_SMALL_BUFFER);
                 /* Since this is a different error, we don't break */
                 return 0;
             }
             /* Change actual size to become the desired size. */
-            *buf_n = p->data_size;
+            *buf_n = template->data_size;
         }
         break;
     case OSSL_PARAM_UTF8_STRING:
-        if (*ishex) {
+        if ((flags & FLAG_HEX) != 0) {
             CRYPTOerr(0, ERR_R_PASSED_INVALID_ARGUMENT);
             return 0;
         }
         *buf_n = strlen(value) + 1;
         break;
     case OSSL_PARAM_OCTET_STRING:
-        if (*ishex) {
+        if ((flags & FLAG_HEX) != 0) {
             *buf_n = strlen(value) >> 1;
         } else {
             *buf_n = value_n;
@@ -100,15 +86,15 @@ static int prepare_from_text(const OSSL_PARAM *paramdefs, const char *key,
     return 1;
 }
 
-static int construct_from_text(OSSL_PARAM *to, const OSSL_PARAM *paramdef,
-                               const char *value, size_t value_n, int ishex,
+static int construct_from_text(OSSL_PARAM *to, const OSSL_PARAM *template,
+                               const char *value, size_t value_n, int flags,
                                void *buf, size_t buf_n, BIGNUM *tmpbn)
 {
     if (buf == NULL)
         return 0;
 
     if (buf_n > 0) {
-        switch (paramdef->data_type) {
+        switch (template->data_type) {
         case OSSL_PARAM_INTEGER:
         case OSSL_PARAM_UNSIGNED_INTEGER:
             /*
@@ -127,7 +113,7 @@ static int construct_from_text(OSSL_PARAM *to, const OSSL_PARAM *paramdef,
              * Because we did the first part on the BIGNUM itself, we can just
              * invert all the bytes here and be done with it.
              */
-            if (paramdef->data_type == OSSL_PARAM_INTEGER
+            if (template->data_type == OSSL_PARAM_INTEGER
                 && BN_is_negative(tmpbn)) {
                 unsigned char *cp;
                 size_t i = buf_n;
@@ -140,7 +126,7 @@ static int construct_from_text(OSSL_PARAM *to, const OSSL_PARAM *paramdef,
             strncpy(buf, value, buf_n);
             break;
         case OSSL_PARAM_OCTET_STRING:
-            if (ishex) {
+            if ((flags & FLAG_HEX) != 0) {
                 size_t l = 0;
 
                 if (!OPENSSL_hexstr2buf_ex(buf, buf_n, &l, value))
@@ -152,7 +138,8 @@ static int construct_from_text(OSSL_PARAM *to, const OSSL_PARAM *paramdef,
         }
     }
 
-    *to = *paramdef;
+    to->key = template->key;
+    to->data_type = template->data_type;
     to->data = buf;
     to->data_size = buf_n;
     to->return_size = 0;
@@ -160,23 +147,37 @@ static int construct_from_text(OSSL_PARAM *to, const OSSL_PARAM *paramdef,
     return 1;
 }
 
-int OSSL_PARAM_allocate_from_text(OSSL_PARAM *to,
-                                  const OSSL_PARAM *paramdefs,
-                                  const char *key, const char *value,
-                                  size_t value_n)
+#define FLAG_ISHEX             1
+
+const OSSL_PARAM *OSSL_PARAM_parse_locate_const(const OSSL_PARAM *params,
+                                                const char *key,
+                                                int *flags)
 {
-    const OSSL_PARAM *paramdef = NULL;
-    int ishex = 0;
+    if (params == NULL || key == NULL || flags == NULL)
+        return NULL;
+
+    *flags = 0;
+    if (strncmp(key, "hex", 3) == 0) {
+        *flags |= FLAG_ISHEX;
+        key += 3;
+    }
+
+    return OSSL_PARAM_locate_const(params, key);
+}
+
+int OSSL_PARAM_allocate_from_text(OSSL_PARAM *to, const OSSL_PARAM *template,
+                                  const char *value, size_t value_n, int flags)
+{
     void *buf = NULL;
     size_t buf_n = 0;
     BIGNUM *tmpbn = NULL;
     int ok = 0;
 
-    if (to == NULL || paramdefs == NULL)
+    if (to == NULL || template == NULL)
         return 0;
 
-    if (!prepare_from_text(paramdefs, key, value, value_n,
-                           &paramdef, &ishex, &buf_n, &tmpbn))
+    if (!prepare_from_text(template, value, value_n, flags,
+                           &buf_n, &tmpbn))
         return 0;
 
     if ((buf = OPENSSL_zalloc(buf_n > 0 ? buf_n : 1)) == NULL) {
@@ -184,7 +185,7 @@ int OSSL_PARAM_allocate_from_text(OSSL_PARAM *to,
         return 0;
     }
 
-    ok = construct_from_text(to, paramdef, value, value_n, ishex,
+    ok = construct_from_text(to, template, value, value_n, flags,
                              buf, buf_n, tmpbn);
     BN_free(tmpbn);
     if (!ok)
--- include/openssl/params.h
+++ include/openssl/params.h
@@ -89,10 +89,10 @@ OSSL_PARAM OSSL_PARAM_construct_octet_ptr(const char *key, void **buf,
                                           size_t bsize);
 OSSL_PARAM OSSL_PARAM_construct_end(void);
 
-int OSSL_PARAM_allocate_from_text(OSSL_PARAM *to,
-                                  const OSSL_PARAM *paramdefs,
-                                  const char *key, const char *value,
-                                  size_t value_n);
+const OSSL_PARAM *OSSL_PARAM_parse_locate_const(const OSSL_PARAM *params,
+                                                const char *key, int *flags);
+int OSSL_PARAM_allocate_from_text(OSSL_PARAM *to, const OSSL_PARAM *template,
+                                  const char *value, size_t value_n, int flags);
 
 int OSSL_PARAM_get_int(const OSSL_PARAM *p, int *val);
 int OSSL_PARAM_get_uint(const OSSL_PARAM *p, unsigned int *val);
