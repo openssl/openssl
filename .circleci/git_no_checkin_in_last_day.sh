29ebb242ecea1725df6545a13857b6f6fa0e83db
Douglas Stebila <dstebila@uwaterloo.ca>
2020-02-29 19:27:09 -0500

    Triggering build

--- .circleci/git_no_checkin_in_last_day.sh
+++ .circleci/git_no_checkin_in_last_day.sh
@@ -5,21 +5,21 @@
 
 r=`git log --name-only --since="1 day ago" -n 2`
 if [ "x$r" == "x" ]; then 
-	echo "No openssl commit in the last day. Checking liboqs now."
-	if [ -d oqs_test/tmp/liboqs ]; then
-        	cd oqs_test/tmp/liboqs
-        	r=`git log --name-only --since="1 day ago" -n 2`
-        	if [ "x$r" == "x" ]; then 
-         		echo "Also no liboqs commit in the last day. No build/test required. Exiting."
-         		exit 0
-        	else
-               		echo "liboqs commit found. Should test."
-         		exit 1
-        	fi
-	else
-		echo "Shouldn't happen (liboqs not checked out?). Testing recommended."
-        	exit 1
-	fi
+    echo "No openssl commit in the last day. Checking liboqs now."
+    if [ -d oqs_test/tmp/liboqs ]; then
+            cd oqs_test/tmp/liboqs
+            r=`git log --name-only --since="1 day ago" -n 2`
+            if [ "x$r" == "x" ]; then 
+                echo "Also no liboqs commit in the last day. No build/test required. Exiting."
+                exit 0
+            else
+                echo "liboqs commit found. Should test."
+                exit 1
+            fi
+    else
+        echo "Shouldn't happen (liboqs not checked out?). Testing recommended."
+        exit 1
+    fi
 else
-	exit 1
+    exit 1
 fi
