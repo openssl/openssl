ecdc20c22d65768a3f31afffe1a0ac54e66e38bf
Richard Levitte <levitte@openssl.org>
2020-03-04 15:16:08 +0100

    fixup! util/perl/OpenSSL/config.pm: Rework determining compiler information

--- util/perl/OpenSSL/config.pm
+++ util/perl/OpenSSL/config.pm
@@ -284,7 +284,8 @@ sub determine_compiler_settings {
     foreach my $pair (List::Util::pairs @cc_version) {
         # Try to get the version number.
         # Failure gets us undef or an empty string
-        my $v = $pair->value()->();
+        my ( $k, $v ) = @$pair;
+        $v = $v->();
 
         # If we got a version number, process it
         if ($v) {
