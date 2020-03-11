b4842a969a5426d7a1fbf6f9f7509f8b5644ff75
Douglas Stebila <dstebila@uwaterloo.ca>
2020-03-11 09:28:56 -0400

    Don't skip build if running in circleci local

--- .circleci/git_no_checkin_in_last_day.sh
+++ .circleci/git_no_checkin_in_last_day.sh
@@ -2,6 +2,12 @@
 
 # Detects whether there has been a Git commit in the last day on this
 # branch. Returns 1 if there has been a commit, 0 if there has not.
+# Always returns 1 if running in circleci local
+
+if [ "x${CIRCLECI}" == "xtrue" ] && [ "x${CIRCLE_BUILD_NUM}" == "x" ]; then
+	echo "Running in circleci local"
+	exit 1
+fi
 
 r=`git log --name-only --since="1 day ago" -n 2`
 if [ "x$r" == "x" ]; then 
