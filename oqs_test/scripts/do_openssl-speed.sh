fc3fac06940b7eebc07c4e9a8a96caeffc7a8e91
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2020-03-11 09:26:52 +0100

    simplified OSX oqssig speed testing

--- oqs_test/scripts/do_openssl-speed.sh
+++ oqs_test/scripts/do_openssl-speed.sh
@@ -18,6 +18,12 @@ apps/openssl speed -seconds 1 oqskem
 if [ $? -ne 0 ]; then
    exit -1
 fi
-# Test all SIGs (disabling cleanup because of picnic-induced memory glitch):
-OPENSSL_NO_CLEANUP=1 apps/openssl speed -seconds 1 oqssig
+# Test all SIGs 
+if [ `uname` == "Darwin" ]; then
+# On OSX, only test one alg that doesn't cause memory problems:
+   apps/openssl speed -seconds 1 dilithium2
+else
+# Disabling cleanup on Linux because of picnic-induced memory glitch:
+   OPENSSL_NO_CLEANUP=1 apps/openssl speed -seconds 1 oqssig
+fi
 
