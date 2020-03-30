4f77706443df4d615782197dff1653743201dcf7
Diego F. Aranha <dfaranha@gmail.com>
2020-03-30 14:06:28 +0200

    Replace group->order with group->field.

--- crypto/ec/ec_mult.c
+++ crypto/ec/ec_mult.c
@@ -426,7 +426,7 @@ static int ec_mul_consttime(const EC_GROUP *group, EC_POINT *r,
 
         /* first randomize r->Z to blind s. */
         do {
-            if (!BN_rand(&r->Z, BN_num_bits(&group->order), 0, 0)) {
+            if (!BN_rand(&r->Z, BN_num_bits(&group->field), 0, 0)) {
                 ECerr(EC_F_EC_MUL_CONSTTIME, ERR_R_BN_LIB);
                 goto err;
             }
@@ -453,7 +453,7 @@ static int ec_mul_consttime(const EC_GROUP *group, EC_POINT *r,
 
         /* blinding: now rerandomize r->Z to make r a blinded copy of s. */
         do {
-            if (!BN_rand(&r->Z, BN_num_bits(&group->order), 0, 0)) {
+            if (!BN_rand(&r->Z, BN_num_bits(&group->field), 0, 0)) {
                 ECerr(EC_F_EC_MUL_CONSTTIME, ERR_R_BN_LIB);
                 goto err;
             }
