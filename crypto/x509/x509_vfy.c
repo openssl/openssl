950746058e931362962195ccf47bcb05536cfba8
Tomas Mraz <tmraz@fedoraproject.org>
2020-04-02 17:52:18 +0200

    fixup! Set X509_V_ERR_INVALID_EXTENSION error for invalid basic constraints

--- crypto/x509/x509_vfy.c
+++ crypto/x509/x509_vfy.c
@@ -511,7 +511,7 @@ static int check_chain_extensions(X509_STORE_CTX *ctx)
         }
         if ((x->ex_flags & EXFLAG_CA) == 0
             && x->ex_pathlen != -1
-            && ctx->param->flags & X509_V_FLAG_X509_STRICT) {
+            && (ctx->param->flags & X509_V_FLAG_X509_STRICT)) {
             ctx->error = X509_V_ERR_INVALID_EXTENSION;
             ret = 0;
         }
