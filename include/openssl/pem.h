3cf30379d87d30e1f23ffa6edac994fa1e0bccad
Matt Caswell <matt@openssl.org>
2020-04-06 18:21:50 +0100

    Teach PEM_read_bio_PrivateKey about libctx
    Now that d2i_PrivateKey_ex() and other similar functions exist we should
    use it when loading a PEM PrivateKey.

--- include/openssl/pem.h
+++ include/openssl/pem.h
@@ -347,6 +347,12 @@ DECLARE_PEM_rw(DHparams, DH)
 DECLARE_PEM_write(DHxparams, DH)
 # endif
 DECLARE_PEM_rw_cb(PrivateKey, EVP_PKEY)
+EVP_PKEY *PEM_read_bio_PrivateKey_ex(BIO *bp, EVP_PKEY **x, pem_password_cb *cb,
+                                     void *u, OPENSSL_CTX *libctx,
+                                     const char *propq);
+EVP_PKEY *PEM_read_PrivateKey_ex(FILE *fp, EVP_PKEY **x, pem_password_cb *cb,
+                                 void *u, OPENSSL_CTX *libctx,
+                                 const char *propq);
 DECLARE_PEM_rw(PUBKEY, EVP_PKEY)
 
 int PEM_write_bio_PrivateKey_traditional(BIO *bp, const EVP_PKEY *x,
