55164d68086e534e421ec4ac9753982114664358
Dr. Matthias St. Pierre <matthias.st.pierre@ncp-e.com>
2020-04-14 01:15:18 +0200

    fixup! Fix an assertion (and a comment) of evp_method_id()

--- crypto/evp/evp_fetch.c
+++ crypto/evp/evp_fetch.c
@@ -85,8 +85,8 @@ static OSSL_METHOD_STORE *get_evp_method_store(OPENSSL_CTX *libctx)
  */
 static uint32_t evp_method_id(unsigned int operation_id, int name_id)
 {
-    if (!ossl_assert(name_id < (1 << 24) && operation_id < (1 << 8))
-        || !ossl_assert(name_id > 0 && operation_id > 0))
+    if (!ossl_assert(name_id > 0 && name_id < (1 << 24))
+        || !ossl_assert(operation_id > 0 && operation_id < (1 << 8)))
         return 0;
     return ((name_id << 8) & 0xFFFFFF00) | (operation_id & 0x000000FF);
 }
