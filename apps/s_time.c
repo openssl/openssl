6643af1591a40a995115f6767a9c5e7f37fc5d64
Tomas Mraz <tmraz@fedoraproject.org>
2020-04-16 12:07:35 +0200

    s_time: Allow using -CAfile option as in other commands
    The s_time command in difference from all the other similar
    commands supported -cafile option instead of -CAfile.
    Add the -CAfile option and keep -cafile only for backwards
    compatibility.
    
    Fixes #11552

--- apps/s_time.c
+++ apps/s_time.c
@@ -62,6 +62,7 @@ const OPTIONS s_time_options[] = {
     {"key", OPT_KEY, '<', "File with key, PEM; default is -cert file"},
     {"CApath", OPT_CAPATH, '/', "PEM format directory of CA's"},
     {"cafile", OPT_CAFILE, '<', "PEM format file of CA's"},
+    {"CAfile", OPT_CAFILE, '<', "PEM format file of CA's"},
     {"no-CAfile", OPT_NOCAFILE, '-',
      "Do not load the default certificates file"},
     {"no-CApath", OPT_NOCAPATH, '-',
