7a92e804696fec77b9c07f03fbdf74a60b30e47a
Pauli <paul.dale@oracle.com>
2020-04-20 09:33:51 +1000

    gendsa: remove unnecessary OPENSSL_SUPPRESS_DEPRECATED definition
    Reviewed-by: Paul Yang <kaishen.yy@antfin.com>
    (Merged from https://github.com/openssl/openssl/pull/11570)

--- apps/gendsa.c
+++ apps/gendsa.c
@@ -7,9 +7,6 @@
  * https://www.openssl.org/source/license.html
  */
 
-/* We need to use some deprecated APIs */
-#define OPENSSL_SUPPRESS_DEPRECATED
-
 #include <openssl/opensslconf.h>
 
 #include <stdio.h>
