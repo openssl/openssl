a7be9c22511f0a18f20c923f01dfb4b06f00692b
Tomas Mraz <tmraz@fedoraproject.org>
2020-05-19 10:51:53 +0200

    Avoid potential overflow to the sign bit when shifting left 24 places
    Although there are platforms where int is 64 bit, 2GiB large BIGNUMs
    instead of 4GiB should be "big enough for everybody".

--- crypto/bn/bn_mpi.c
+++ crypto/bn/bn_mpi.c
@@ -45,7 +45,7 @@ BIGNUM *BN_mpi2bn(const unsigned char *d, int n, BIGNUM *ain)
     int neg = 0;
     BIGNUM *a = NULL;
 
-    if (n < 4) {
+    if (n < 4 || (d[0] & 0x80) != 0) {
         BNerr(BN_F_BN_MPI2BN, BN_R_INVALID_LENGTH);
         return NULL;
     }
