1349865a5621ce69f09ff4a0991afd74648df72b
Dr. David von Oheimb <David.von.Oheimb@siemens.com>
2020-06-05 16:34:51 +0200

    Silence gcc false positive warning on refdatalen in test/tls13encryptiontest.c
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Bernd Edlinger <bernd.edlinger@hotmail.de>
    (Merged from https://github.com/openssl/openssl/pull/12041)

--- test/tls13encryptiontest.c
+++ test/tls13encryptiontest.c
@@ -288,7 +288,7 @@ static int test_record(SSL3_RECORD *rec, RECORD_DATA *recd, int enc)
 {
     int ret = 0;
     unsigned char *refd;
-    size_t refdatalen;
+    size_t refdatalen = 0;
 
     if (enc)
         refd = multihexstr2buf(recd->ciphertext, &refdatalen);
