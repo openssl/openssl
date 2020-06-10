7f81aed4bccbeb0ad491b68dd601d576c4956c78
John Baldwin <jhb@FreeBSD.org>
2020-06-10 15:19:00 -0700

    Use the inherited 'bsd-gcc-shared' config on 32-bit x86 BSDs.
    BSD-generic32 already uses this for building shared libraries on other
    32-bit BSD platforms.  Commit b7efa56 collapsed various *BSD targets
    down to the BSD-generic ones and BSD-x86.  At the time only
    OpenBSD/i386 used `bsd-shared` while both FreeBSD and NetBSD used
    `bsd-gcc-shared`.  In practice, all of the BSDs are using either a
    GCC/ld.bfd toolchain or a clang/lld toolchain both of which are
    compatible with 'bsd-gcc-shared'.
    
    Retire 'bsd-shared' since this removes the last user.
    
    Fixes #12050.
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    Reviewed-by: Ben Kaduk <kaduk@mit.edu>
    Reviewed-by: Dmitry Belyavskiy <beldmit@gmail.com>
    (Merged from https://github.com/openssl/openssl/pull/12110)

--- Configurations/shared-info.pl
+++ Configurations/shared-info.pl
@@ -40,12 +40,6 @@ my %shared_info;
         };
     },
     'bsd-gcc-shared' => sub { return $shared_info{'linux-shared'}; },
-    'bsd-shared' => sub {
-        return $shared_info{'gnu-shared'} if detect_gnu_ld();
-        return {
-            shared_ldflag     => '-shared -nostdlib',
-        };
-    },
     'darwin-shared' => {
         module_ldflags        => '-bundle',
         shared_ldflag         => '-dynamiclib -current_version $(SHLIB_VERSION_NUMBER) -compatibility_version $(SHLIB_VERSION_NUMBER)',
