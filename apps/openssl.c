b75f08cb13ca2e03946e0beeaac2e0ffa0d2aae7
Shane Lontis <shane.lontis@oracle.com>
2020-06-18 09:03:32 +1000

    Fix segfault in openssl app called with no args.
    This is a result of removal of interactive mode.
    Redirected it to now use 'openssl help'.
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/12164)

--- apps/openssl.c
+++ apps/openssl.c
@@ -308,6 +308,13 @@ int help_main(int argc, char **argv)
     char *prog;
     HELP_CHOICE o;
     DISPLAY_COLUMNS dc;
+    char *new_argv[3];
+
+    if (argc == 0) {
+        new_argv[0] = "help";
+        new_argv[1] = NULL;
+        return do_cmd(prog_init(), 1, new_argv);
+    }
 
     prog = opt_init(argc, argv, help_options);
     while ((o = opt_next()) != OPT_hEOF) {
@@ -323,8 +330,6 @@ int help_main(int argc, char **argv)
     }
 
     if (opt_num_rest() == 1) {
-        char *new_argv[3];
-
         new_argv[0] = opt_rest()[0];
         new_argv[1] = "--help";
         new_argv[2] = NULL;
