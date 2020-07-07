bca2112c57ea4cc4b02e0c914c840c66037c09ad
Norman Ashley <nashley@cisco.com>
2020-07-06 21:47:49 -0400

    Update rsa_ameth.c
    Updated per comment.

--- crypto/rsa/rsa_ameth.c
+++ crypto/rsa/rsa_ameth.c
@@ -122,14 +122,11 @@ static int rsa_pub_cmp(const EVP_PKEY *a, const EVP_PKEY *b)
      * Don't check the public/private key, this is mostly for smart
      * cards.
      */
-    if (((a->type == EVP_PKEY_RSA)
-            && (RSA_flags(a->pkey.rsa) & RSA_METHOD_FLAG_NO_CHECK))
-            ||
-        ((b->type == EVP_PKEY_RSA)
-            && (RSA_flags(b->pkey.rsa) & RSA_METHOD_FLAG_NO_CHECK))
-       )
-        ; /* no-op */
-    else if (BN_cmp(b->pkey.rsa->n, a->pkey.rsa->n) != 0
+    if (((RSA_flags(a->pkey.rsa) & RSA_METHOD_FLAG_NO_CHECK))
+       || (RSA_flags(b->pkey.rsa) & RSA_METHOD_FLAG_NO_CHECK))
+        return 1;
+
+    if (BN_cmp(b->pkey.rsa->n, a->pkey.rsa->n) != 0
         || BN_cmp(b->pkey.rsa->e, a->pkey.rsa->e) != 0)
         return 0;
     return 1;
