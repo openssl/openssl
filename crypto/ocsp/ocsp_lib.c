7af71f72aaf2d95d776d61779a6401f66395f4c8
Norman Ashley <nashley@cisco.com>
2020-07-07 22:52:37 -0400

    Update ocsp_lib.c
    Initialize variables.

--- crypto/ocsp/ocsp_lib.c
+++ crypto/ocsp/ocsp_lib.c
@@ -47,21 +47,26 @@ OCSP_CERTID *OCSP_resp_cert_to_id(OCSP_BASICRESP *br,
                                   const X509 *subject,
                                   const X509 *issuer, int idx)
 {
-    const EVP_MD *dgst;
-    ASN1_OBJECT  *pmd;
+    const EVP_MD *dgst = NULL;
+    ASN1_OBJECT  *pmd = NULL;
     OCSP_CERTID  *cid;
     const OCSP_SINGLERESP *single;
 
     single = OCSP_resp_get0(br, idx);
-    if (single == NULL)
+    if (single == NULL) {
         return NULL;
+    }
 
     cid = (OCSP_CERTID *)OCSP_SINGLERESP_get0_id(single);
-    if (cid == NULL)
+    if (cid == NULL) {
         return NULL;
+    }
 
-    if (OCSP_id_get0_info(NULL, &pmd, NULL, NULL, cid))
+    if (OCSP_id_get0_info(NULL, &pmd, NULL, NULL, cid)) {
         dgst = EVP_get_digestbyobj(pmd);
+    } else {
+        return NULL;
+    }
 
     return (OCSP_cert_to_id(dgst, subject, issuer));
 }
