7b3464205a9f45448418f426d606ed5414b412ef
Nicola Tuveri <nic.tuv@gmail.com>
2020-07-16 03:23:26 +0300

    [ec] Do not default to OPENSSL_EC_NAMED_CURVE for curves without OID
    Some curves don't have an associated OID: for those we should not
    default to `OPENSSL_EC_NAMED_CURVE` encoding of parameters and instead
    set the ASN1 flag to `OPENSSL_EC_EXPLICIT_CURVE`.
    
    This is a follow-up to https://github.com/openssl/openssl/pull/12312
    
    (cherry picked from commit b0527a548bda76a25751a0e66a95e56b803c936c)

--- crypto/ec/ec_curve.c
+++ crypto/ec/ec_curve.c
@@ -3223,6 +3223,32 @@ static EC_GROUP *ec_group_new_from_data(OSSL_LIB_CTX *libctx,
             goto err;
         }
     }
+
+    if (EC_GROUP_get_asn1_flag(group) == OPENSSL_EC_NAMED_CURVE) {
+        /*
+         * Some curves don't have an associated OID: for those we should not
+         * default to `OPENSSL_EC_NAMED_CURVE` encoding of parameters and
+         * instead set the ASN1 flag to `OPENSSL_EC_EXPLICIT_CURVE`.
+         *
+         * Note that `OPENSSL_EC_NAMED_CURVE` is set as the default ASN1 flag on
+         * `EC_GROUP_new()`, when we don't have enough elements to determine if
+         * an OID for the curve name actually exists.
+         * We could implement this check on `EC_GROUP_set_curve_name()` but
+         * overloading the simple setter with this lookup could have a negative
+         * performance impact and unexpected consequences.
+         */
+        ASN1_OBJECT *asn1obj = OBJ_nid2obj(curve.nid);
+
+        if (asn1obj == NULL) {
+            ECerr(EC_F_EC_GROUP_NEW_FROM_DATA, ERR_R_OBJ_LIB);
+            goto err;
+        }
+        if (OBJ_length(asn1obj) == 0)
+            EC_GROUP_set_asn1_flag(group, OPENSSL_EC_EXPLICIT_CURVE);
+
+        ASN1_OBJECT_free(asn1obj);
+    }
+
     ok = 1;
  err:
     if (!ok) {
