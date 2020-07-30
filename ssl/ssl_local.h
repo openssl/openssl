7b365cf2b28bcd01231beac4ee514585c0164a18
Matt Caswell <matt@openssl.org>
2020-07-30 12:02:06 +0100

    Add an HMAC implementation that is TLS aware
    The TLS HMAC implementation should take care to calculate the MAC in
    constant time in the case of MAC-Then-Encrypt where we have a variable
    amount of padding.

--- ssl/ssl_local.h
+++ ssl/ssl_local.h
@@ -2761,8 +2761,7 @@ int ktls_configure_crypto(const SSL *s, const EVP_CIPHER *c, EVP_CIPHER_CTX *dd,
 
 /* s3_cbc.c */
 __owur char ssl3_cbc_record_digest_supported(const EVP_MD_CTX *ctx);
-__owur int ssl3_cbc_digest_record(SSL *s,
-                                  const EVP_MD_CTX *ctx,
+__owur int ssl3_cbc_digest_record(const EVP_MD *md,
                                   unsigned char *md_out,
                                   size_t *md_out_size,
                                   const unsigned char header[13],
