6557a251aca3ce4dc365131094d91630eb03313b
Robert Jędrzejczyk <robert@prog.olsztyn.pl>
2020-08-16 14:55:34 +0200

    Windows - Get ENV
    Get ENV value as UTF-8 encoded string instead raw string

--- crypto/getenv.c
+++ crypto/getenv.c
@@ -1,7 +1,7 @@
 /*
  * Copyright 2018 The OpenSSL Project Authors. All Rights Reserved.
  *
- * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * Licensed under the OpenSSL license (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
  * in the file LICENSE in the source distribution or at
  * https://www.openssl.org/source/license.html
@@ -13,9 +13,45 @@
 
 #include <stdlib.h>
 #include "internal/cryptlib.h"
+#include "e_os.h"
 
 char *ossl_safe_getenv(const char *name)
 {
+#if (defined(OPENSSL_SYS_WINDOWS)) && defined(CP_UTF8) && !defined(_WIN32_WCE)
+
+    if (GetEnvironmentVariableW(L"OPENSSL_WIN32_UTF8", NULL, 0) == 0)
+        return getenv(name);
+
+    /* convert name to wide string */
+    int rsize = mbstowcs(NULL, name, 0) + 1;
+    WCHAR * namew = _alloca(rsize * sizeof(WCHAR));
+    int fsize = mbstowcs(namew, name, rsize);
+
+    char *val = NULL;
+
+    /* determine value string size in wchars */
+    DWORD envlen = GetEnvironmentVariableW(namew, NULL, 0);    
+
+    if (envlen != 0) {
+        WCHAR *valw = _alloca(envlen * sizeof(WCHAR));
+        if (GetEnvironmentVariableW(namew, valw, envlen) < envlen) {
+            /* determine value string size in utf-8 */
+            int sz = WideCharToMultiByte(CP_UTF8, 0, valw, -1, 
+                       NULL, 0, NULL, NULL);
+            if (sz != 0) {
+                val = OPENSSL_malloc(sz);
+                /* convert value string from wide to utf-8 */
+                if (WideCharToMultiByte(CP_UTF8, 0, valw, -1, 
+                      val, sz, NULL, NULL) == 0) {
+                    OPENSSL_free(val);
+                    val = NULL;
+                }
+            }
+        }
+    }    
+    return val;
+#endif
+
 #if defined(__GLIBC__) && defined(__GLIBC_PREREQ)
 # if __GLIBC_PREREQ(2, 17)
 #  define SECURE_GETENV
