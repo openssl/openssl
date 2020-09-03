b931a5791182f7b717f2470b8652889bb984fd87
Ayush Sawal <ayush.sawal@chelsio.com>
2020-09-04 03:22:41 +0530

    Test: Added aes-gcm tests for afalg engine
    This patch does a known answer test for aes-gcm, using
    known test vectors.
    
    Signed-off-by: Ayush Sawal <ayush.sawal@chelsio.com>

--- test/afalgtest.c
+++ test/afalgtest.c
@@ -19,6 +19,7 @@
 #include <openssl/rand.h>
 #include "testutil.h"
 
+
 /* Use a buffer size which is not aligned to block size */
 #define BUFFER_SIZE     17
 
@@ -44,6 +45,170 @@ static ENGINE *e;
 #endif
 
 #ifndef OPENSSL_NO_AFALGENG
+
+#if defined(OPENSSL_SYS_UNIX)
+#include <unistd.h>
+#include <sys/socket.h>
+#include <linux/if_alg.h>
+#endif
+
+static const unsigned char gcm_key[] = {
+    0xee, 0xbc, 0x1f, 0x57, 0x48, 0x7f, 0x51, 0x92, 0x1c, 0x04, 0x65, 0x66,
+    0x5f, 0x8a, 0xe6, 0xd1, 0x65, 0x8b, 0xb2, 0x6d, 0xe6, 0xf8, 0xa0, 0x69,
+    0xa3, 0x52, 0x02, 0x93, 0xa5, 0x72, 0x07, 0x8f
+};
+static const unsigned char gcm_iv[] = {
+    0x99, 0xaa, 0x3e, 0x68, 0xed, 0x81, 0x73, 0xa0, 0xee, 0xd0, 0x66, 0x84
+};
+static const unsigned char gcm_pt[] = {
+    0xf5, 0x6e, 0x87, 0x05, 0x5b, 0xc3, 0x2d, 0x0e, 0xeb, 0x31, 0xb2, 0xea,
+    0xcc, 0x2b, 0xf2, 0xa5
+};
+static const unsigned char gcm_aad[] = {
+    0x4d, 0x23, 0xc3, 0xce, 0xc3, 0x34, 0xb4, 0x9b, 0xdb, 0x37, 0x0c, 0x43,
+    0x7f, 0xec, 0x78, 0xde
+};
+static const unsigned char gcm_ct_128[] = {
+    0xf5, 0xc9, 0x4f, 0x28, 0x9c, 0x59, 0xd0, 0x74, 0xb8, 0x43, 0x2e, 0xf8,
+    0xe4, 0xab, 0x0a, 0x72
+};
+static const unsigned char gcm_tag_128[] = {
+    0xdc, 0x63, 0x1e, 0xbe, 0xcf, 0x1e, 0x4f, 0x3a, 0x01, 0x62, 0xa3, 0x86,
+    0xd6, 0x0d, 0x4a, 0x1e
+};
+static const unsigned char gcm_ct_192[] = {
+    0x63, 0x87, 0x7f, 0xc0, 0xcf,0x0c, 0xb8, 0x6f, 0x21, 0x62, 0x1d, 0x2d,
+    0x8d, 0xcc, 0xc3, 0x7c
+};
+static const unsigned char gcm_tag_192[] = {
+    0x7a, 0x70, 0xba, 0x98, 0x9d, 0x67, 0x65, 0x1e, 0x55, 0xd0, 0x6a, 0xdf,
+    0xb1, 0x08, 0xfa, 0xf9
+};
+static const unsigned char gcm_ct_256[] = {
+    0xf7, 0x26, 0x44, 0x13, 0xa8, 0x4c, 0x0e, 0x7c, 0xd5, 0x36, 0x86, 0x7e,
+    0xb9, 0xf2, 0x17, 0x36
+};
+static const unsigned char gcm_tag_256[] = {
+    0x67, 0xba, 0x05, 0x10, 0x26, 0x2a, 0xe4, 0x87, 0xd7, 0x37, 0xee, 0x62,
+    0x98, 0xf7, 0x7e, 0x0c
+};
+
+static int chk_gcm_kernel_supp(void)
+{
+    struct sockaddr_alg sa;
+    int sock;
+    int r;
+
+    memset(&sa, 0, sizeof(sa));
+    sa.salg_family = AF_ALG;
+    OPENSSL_strlcpy((char *) sa.salg_type, "aead", sizeof(sa.salg_type));
+    OPENSSL_strlcpy((char *) sa.salg_name, "gcm(aes)", sizeof(sa.salg_name));
+    sock = socket(AF_ALG, SOCK_SEQPACKET, 0);
+    if (sock == -1)
+        return 0;
+    r = bind(sock, (struct sockaddr *)&sa, sizeof(sa));
+    if (r < 0)
+        return 0;
+    close(sock);
+
+    return 1;
+}
+
+static int do_encrypt(const EVP_CIPHER *cipher, unsigned char *ct, int *ct_len,
+                      unsigned char *tag, int *tag_len)
+{
+    int ret = 0;
+    EVP_CIPHER_CTX *ctx = NULL;
+    int outlen;
+    unsigned char outbuf[64];
+
+    *tag_len = 16;
+    ret = TEST_ptr(ctx = EVP_CIPHER_CTX_new())
+          && TEST_true(EVP_EncryptInit_ex(ctx, cipher, NULL, NULL,
+                                          NULL) > 0)
+          && TEST_true(EVP_EncryptInit_ex(ctx, NULL, NULL, gcm_key,
+                                          gcm_iv) > 0)
+          && TEST_true(EVP_EncryptUpdate(ctx, NULL, &outlen, gcm_aad,
+                                         sizeof(gcm_aad)) > 0)
+          && TEST_true(EVP_EncryptUpdate(ctx, ct, ct_len, gcm_pt,
+                                         sizeof(gcm_pt)) > 0)
+          && TEST_true(EVP_EncryptFinal_ex(ctx, outbuf, &outlen) > 0)
+          && TEST_true(EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_GET_TAG, 16,
+                                           tag) > 0);
+    EVP_CIPHER_CTX_free(ctx);
+    return ret;
+}
+
+static int do_decrypt(const EVP_CIPHER *cipher, const unsigned char *ct,
+                      int ct_len, const unsigned char *tag, int tag_len)
+{
+    int ret = 0;
+    EVP_CIPHER_CTX *ctx = NULL;
+    int outlen, ptlen;
+    unsigned char pt[32];
+    unsigned char outbuf[32];
+
+    ret = TEST_ptr(ctx = EVP_CIPHER_CTX_new())
+              && TEST_true(EVP_DecryptInit_ex(ctx, cipher, NULL,
+                                              NULL, NULL) > 0)
+              && TEST_true(EVP_DecryptInit_ex(ctx, NULL, NULL, gcm_key,
+                                              gcm_iv) > 0)
+              && TEST_true(EVP_DecryptUpdate(ctx, NULL, &outlen, gcm_aad,
+                                             sizeof(gcm_aad)) > 0)
+              && TEST_true(EVP_DecryptUpdate(ctx, pt, &ptlen, ct,
+                                             ct_len) > 0)
+              && TEST_true(EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_SET_TAG,
+                                               tag_len, (void *)tag) > 0)
+              && TEST_true(EVP_DecryptFinal_ex(ctx, outbuf, &outlen) > 0)
+              && TEST_mem_eq(gcm_pt, sizeof(gcm_pt), pt, ptlen);
+
+    EVP_CIPHER_CTX_free(ctx);
+    return ret;
+}
+
+static int test_afalg_aes_gcm(int keysize_idx)
+{
+    int ctlen = 0, taglen = 0, ret = 0, encl = 0, tagl = 0;
+    unsigned const char *enc_result = NULL;
+    unsigned const char *tag_result = NULL;
+    const EVP_CIPHER *cipher;
+    unsigned char tag[32];
+    unsigned char ct[32];
+
+    switch (keysize_idx) {
+        case 0:
+            cipher = EVP_aes_128_gcm();
+            enc_result = &gcm_ct_128[0];
+            tag_result = &gcm_tag_128[0];
+            encl = sizeof(gcm_ct_128);
+            tagl = sizeof(gcm_tag_128);
+            break;
+        case 1:
+            cipher = EVP_aes_192_gcm();
+            enc_result = &gcm_ct_192[0];
+            tag_result = &gcm_tag_192[0];
+            encl = sizeof(gcm_ct_192);
+            tagl = sizeof(gcm_tag_192);
+            break;
+        case 2:
+            cipher = EVP_aes_256_gcm();
+            enc_result = &gcm_ct_256[0];
+            tag_result = &gcm_tag_256[0];
+            encl = sizeof(gcm_ct_256);
+            tagl = sizeof(gcm_tag_256);
+            break;
+        default:
+            cipher = NULL;
+    }
+
+    ret = do_encrypt(cipher, ct, &ctlen, tag, &taglen)
+          && TEST_mem_eq(enc_result, encl, ct, ctlen)
+          && TEST_mem_eq(tag_result, tagl, tag, taglen)
+          && do_decrypt(cipher, ct, ctlen, tag, taglen);
+
+    return ret;
+
+}
 static int test_afalg_aes_cbc(int keysize_idx)
 {
     EVP_CIPHER_CTX *ctx;
@@ -137,6 +302,8 @@ int setup_tests(void)
     } else {
 # ifndef OPENSSL_NO_AFALGENG
         ADD_ALL_TESTS(test_afalg_aes_cbc, 3);
+        if (chk_gcm_kernel_supp())
+            ADD_ALL_TESTS(test_afalg_aes_gcm, 3);
 # endif
     }
 #endif
