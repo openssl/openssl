2f6dfe53c6a44f6754e79d9228fe6fc4831eb239
Pauli <paul.dale@oracle.com>
2020-09-09 08:25:06 +1000

    fixup! keymgmt: add FIPS error state handling

--- providers/implementations/keymgmt/kdf_legacy_kmgmt.c
+++ providers/implementations/keymgmt/kdf_legacy_kmgmt.c
@@ -67,6 +67,13 @@ int kdf_data_up_ref(KDF_DATA *kdfdata)
 {
     int ref = 0;
 
+    /* This is effectively doing a new operation on the KDF_DATA and should be
+     * adequately guarded again modules' error states.  However, both current
+     * calls here are guarded propery in exchange/kdf_exch.c.  Thus, it
+     * could be removed here.  The concern is that something in the future
+     * might call this function without adequate guards.  It's a cheap call,
+     * it seems best to leave it even though it is currently redundant.
+     */
     if (!ossl_prov_is_running())
         return 0;
 
