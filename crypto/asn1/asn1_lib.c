08e9684c53deab7d815be47bfdf0f324a0d10ad9
David Benjamin <davidben@google.com>
2020-09-18 15:21:43 -0400

    Deprecate ASN1_STRING_length_set in OpenSSL 3.0.
    Fixes #12885
    
    Reviewed-by: Kurt Roeckx <kurt@roeckx.be>
    GH: #12922

--- crypto/asn1/asn1_lib.c
+++ crypto/asn1/asn1_lib.c
@@ -387,10 +387,12 @@ int ASN1_STRING_length(const ASN1_STRING *x)
     return x->length;
 }
 
+#ifndef OPENSSL_NO_DEPRECATED_3_0
 void ASN1_STRING_length_set(ASN1_STRING *x, int len)
 {
     x->length = len;
 }
+#endif
 
 int ASN1_STRING_type(const ASN1_STRING *x)
 {
