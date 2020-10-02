7a23c23e4515d253d23b5c8258300873eca179c8
Akshit Akhoury <akshit.akhoury@citrix.com>
2020-10-02 22:28:36 +0530

    Changing X509at_get0_data_by_OBJ to expect const stack of X509_ATTRIBUTE
    CLA: trivial
    
    Reviewed-by: Kurt Roeckx <kurt@roeckx.be>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/13062)
    
    (cherry picked from commit 796948cd733d2bd0d8acbaf2354c718bcd4352a6)

--- crypto/x509/x509_att.c
+++ crypto/x509/x509_att.c
@@ -149,7 +149,7 @@ STACK_OF(X509_ATTRIBUTE) *X509at_add1_attr_by_txt(STACK_OF(X509_ATTRIBUTE)
     return ret;
 }
 
-void *X509at_get0_data_by_OBJ(STACK_OF(X509_ATTRIBUTE) *x,
+void *X509at_get0_data_by_OBJ(const STACK_OF(X509_ATTRIBUTE) *x,
                               const ASN1_OBJECT *obj, int lastpos, int type)
 {
     int i;
--- include/openssl/x509.h
+++ include/openssl/x509.h
@@ -933,7 +933,7 @@ STACK_OF(X509_ATTRIBUTE) *X509at_add1_attr_by_txt(STACK_OF(X509_ATTRIBUTE)
                                                   int type,
                                                   const unsigned char *bytes,
                                                   int len);
-void *X509at_get0_data_by_OBJ(STACK_OF(X509_ATTRIBUTE) *x,
+void *X509at_get0_data_by_OBJ(const STACK_OF(X509_ATTRIBUTE) *x,
                               const ASN1_OBJECT *obj, int lastpos, int type);
 X509_ATTRIBUTE *X509_ATTRIBUTE_create_by_NID(X509_ATTRIBUTE **attr, int nid,
                                              int atrtype, const void *data,
