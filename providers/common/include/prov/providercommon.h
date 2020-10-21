1c6b4dc5f07ade30dd833c644b9d89fe5b92dc47
Shane Lontis <shane.lontis@oracle.com>
2020-10-21 10:57:52 +1000

    Remove ossl_prov_util_nid_to_name()
    This removes a TODO.
    This function is not needed since any place that needs to do the
    conversion normally has a special case name2nid table.

--- providers/common/include/prov/providercommon.h
+++ providers/common/include/prov/providercommon.h
@@ -12,8 +12,6 @@
 
 const OSSL_CORE_HANDLE *FIPS_get_core_handle(OSSL_LIB_CTX *ctx);
 
-const char *ossl_prov_util_nid_to_name(int nid);
-
 int ossl_cipher_capable_aes_cbc_hmac_sha1(void);
 int ossl_cipher_capable_aes_cbc_hmac_sha256(void);
 
