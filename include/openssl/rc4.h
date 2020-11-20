53a14afee9a2a595fd567f9fefdbeae43fe02c75
Richard Levitte <levitte@openssl.org>
2020-11-20 10:08:27 +0100

    Switch deprecation method for RC4
    Reviewed-by: Tomas Mraz <tmraz@fedoraproject.org>
    (Merged from https://github.com/openssl/openssl/pull/13460)

--- include/openssl/rc4.h
+++ include/openssl/rc4.h
@@ -30,12 +30,14 @@ typedef struct rc4_key_st {
     RC4_INT data[256];
 } RC4_KEY;
 #  endif
-
-DEPRECATEDIN_3_0(const char *RC4_options(void))
-DEPRECATEDIN_3_0(void RC4_set_key(RC4_KEY *key, int len,
-                                  const unsigned char *data))
-DEPRECATEDIN_3_0(void RC4(RC4_KEY *key, size_t len, const unsigned char *indata,
-                          unsigned char *outdata))
+#  ifndef OPENSSL_NO_DEPRECATED_3_0
+OSSL_DEPRECATEDIN_3_0 const char *RC4_options(void);
+OSSL_DEPRECATEDIN_3_0 void RC4_set_key(RC4_KEY *key, int len,
+                                       const unsigned char *data);
+OSSL_DEPRECATEDIN_3_0 void RC4(RC4_KEY *key, size_t len,
+                               const unsigned char *indata,
+                               unsigned char *outdata);
+#  endif
 
 #  ifdef  __cplusplus
 }
