c37b94795730a857485c6cebac6402c03246dce5
Rich Salz <rsalz@akamai.com>
2021-01-05 20:26:05 -0500

    Add a local perl module to get year last changed
    This is used for generating a more-correct copyright statement
    for the "build_generated" targets.
    
    Fixes: #13765
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/13791)

--- crypto/bn/bn_prime.pl
+++ crypto/bn/bn_prime.pl
@@ -1,13 +1,17 @@
 #! /usr/bin/env perl
-# Copyright 1998-2020 The OpenSSL Project Authors. All Rights Reserved.
+# Copyright 1998-2021 The OpenSSL Project Authors. All Rights Reserved.
 #
 # Licensed under the Apache License 2.0 (the "License").  You may not use
 # this file except in compliance with the License.  You can obtain a copy
 # in the file LICENSE in the source distribution or at
 # https://www.openssl.org/source/license.html
+use FindBin;
+use lib "$FindBin::Bin/../../util/perl";
+use OpenSSL::copyright;
 
 # The year the output file is generated.
-my $YEAR = [localtime()]->[5] + 1900;
+my $YEAR = OpenSSL::copyright::year_of($0);
+
 print <<"EOF";
 /*
  * WARNING: do not edit!
