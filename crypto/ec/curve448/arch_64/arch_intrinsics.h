70fd5110261e9c663b2f6a6009514f72c303d85d
Amitay Isaacs <amitay@ozlabs.org>
2021-01-19 14:04:48 +1100

    curve448: Modernise reference 64-bit code
    Signed-off-by: Amitay Isaacs <amitay@ozlabs.org>
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/14784)

--- crypto/ec/curve448/arch_64/arch_intrinsics.h
+++ crypto/ec/curve448/arch_64/arch_intrinsics.h
@@ -10,22 +10,20 @@
  * Originally written by Mike Hamburg
  */
 
-#ifndef __ARCH_REF64_ARCH_INTRINSICS_H__
-# define __ARCH_REF64_ARCH_INTRINSICS_H__
+#ifndef OSSL_CRYPTO_EC_CURVE448_ARCH_64_INTRINSICS_H
+# define OSSL_CRYPTO_EC_CURVE448_ARCH_64_INTRINSICS_H
 
 # define ARCH_WORD_BITS 64
 
-static __inline__ __attribute((always_inline, unused))
-uint64_t word_is_zero(uint64_t a)
+static ossl_inline uint64_t word_is_zero(uint64_t a)
 {
     /* let's hope the compiler isn't clever enough to optimize this. */
     return (((__uint128_t) a) - 1) >> 64;
 }
 
-static __inline__ __attribute((always_inline, unused))
-__uint128_t widemul(uint64_t a, uint64_t b)
+static ossl_inline uint128_t widemul(uint64_t a, uint64_t b)
 {
-    return ((__uint128_t) a) * b;
+    return ((uint128_t) a) * b;
 }
 
-#endif                          /* ARCH_REF64_ARCH_INTRINSICS_H__ */
+#endif                          /* OSSL_CRYPTO_EC_CURVE448_ARCH_64_INTRINSICS_H */
