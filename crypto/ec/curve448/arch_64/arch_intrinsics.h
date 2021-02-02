374f4753375952ae5dd73efe32e486cd241c58a8
Amitay Isaacs <amitay@ozlabs.org>
2021-02-02 14:08:33 +1100

    fixup2
    Signed-off-by: Amitay Isaacs <amitay@ozlabs.org>

--- crypto/ec/curve448/arch_64/arch_intrinsics.h
+++ crypto/ec/curve448/arch_64/arch_intrinsics.h
@@ -13,11 +13,11 @@
 #ifndef OSSL_CRYPTO_EC_CURVE448_ARCH_64_INTRINSICS_H
 # define OSSL_CRYPTO_EC_CURVE448_ARCH_64_INTRINSICS_H
 
-#include "internal/constant_time.h"
+# include "internal/constant_time.h"
 
 # define ARCH_WORD_BITS 64
 
-#define word_is_zero(a)     constant_time_is_zero_64(a)
+# define word_is_zero(a)     constant_time_is_zero_64(a)
 
 static ossl_inline __uint128_t widemul(uint64_t a, uint64_t b)
 {
