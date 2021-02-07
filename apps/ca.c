e9415bf943afe701f097e5615ca37f693be1ad78
Armin Fuerst <armin@fuerst.priv.at>
2021-02-07 18:14:07 +0100

    Concept of moving logic for ca in libapps to have a possibility for automatic tests.

--- apps/ca.c
+++ apps/ca.c
@@ -33,6 +33,7 @@
 
 #include "apps.h"
 #include "progs.h"
+#include <ca_logic.h>
 
 #ifndef W_OK
 # define F_OK 0
@@ -129,7 +130,6 @@ static int do_body(X509 **xret, EVP_PKEY *pkey, X509 *x509,
                    CONF *conf, unsigned long certopt, unsigned long nameopt,
                    int default_op, int ext_copy, int selfsign);
 static int get_certificate_status(const char *ser_status, CA_DB *db);
-static int do_updatedb(CA_DB *db);
 static int check_time_format(const char *str);
 static int do_revoke(X509 *x509, CA_DB *db, REVINFO_TYPE rev_type,
                      const char *extval);
@@ -2268,56 +2268,6 @@ static int get_certificate_status(const char *serial, CA_DB *db)
         OPENSSL_free(row[i]);
     }
     return ok;
-}
-
-static int do_updatedb(CA_DB *db)
-{
-    ASN1_TIME *a_tm = NULL;
-    int i, cnt = 0;
-    char **rrow;
-
-    a_tm = ASN1_TIME_new();
-    if (a_tm == NULL)
-        return -1;
-
-    /* get actual time */
-    if (X509_gmtime_adj(a_tm, 0) == NULL) {
-        ASN1_TIME_free(a_tm);
-        return -1;
-    }
-
-    for (i = 0; i < sk_OPENSSL_PSTRING_num(db->db->data); i++) {
-        rrow = sk_OPENSSL_PSTRING_value(db->db->data, i);
-
-        if (rrow[DB_type][0] == DB_TYPE_VAL) {
-            /* ignore entries that are not valid */
-            ASN1_TIME *exp_date = NULL;
-
-            exp_date = ASN1_TIME_new();
-            if (exp_date == NULL) {
-                ASN1_TIME_free(a_tm);
-                return -1;
-            }
-
-            if (!ASN1_TIME_set_string(exp_date, rrow[DB_exp_date])) {
-                ASN1_TIME_free(a_tm);
-                ASN1_TIME_free(exp_date);
-                return -1;
-            }
-
-            if (ASN1_TIME_compare(exp_date, a_tm) <= 0) {
-                rrow[DB_type][0] = DB_TYPE_EXP;
-                rrow[DB_type][1] = '\0';
-                cnt++;
-
-                BIO_printf(bio_err, "%s=Expired\n", rrow[DB_serial]);
-            }
-            ASN1_TIME_free(exp_date);
-        }
-    }
-
-    ASN1_TIME_free(a_tm);
-    return cnt;
 }
 
 static const char *crl_reasons[] = {
