2caeb88e2dd0af630b18da6070ebd47dba74fb0c
Armin Fuerst <armin@fuerst.priv.at>
2021-02-13 18:38:16 +0100

    moved iso8601_utc_to_time_t to ca_logic to avoid forbidden nested functions error from CI
    (this function is still not intended to stay, that's just a temporary solution)

--- apps/lib/ca_logic.c
+++ apps/lib/ca_logic.c
@@ -59,3 +59,27 @@ int do_updatedb(CA_DB *db, time_t *now)
     return cnt;
 }
 
+time_t iso8601_utc_to_time_t(const char *dateStr)
+{
+    struct tm t;
+
+    int success = sscanf(dateStr, "%d-%d-%dT%d:%dZ",
+        &t.tm_year, &t.tm_mon, &t.tm_mday, &t.tm_hour, &t.tm_min);
+
+    if (success != 5) {
+        return 0;
+    }
+
+    t.tm_year = t.tm_year - 1900;
+    t.tm_mon = t.tm_mon - 1;
+    t.tm_sec = 0;
+    t.tm_wday = 0;
+    t.tm_yday = 0;
+    t.tm_isdst = 0;
+
+    time_t localTime = mktime(&t);
+    time_t utcTime = localTime - timezone;
+    return utcTime;
+}
+
+
--- test/ca_updatedb.c
+++ test/ca_updatedb.c
@@ -27,29 +27,6 @@ int main(int argc, char *argv[])
     int r;
     time_t *testdate;
 
-    time_t iso8601_utc_to_time_t(const char *dateStr)
-    {
-        struct tm t;
-    
-        int success = sscanf(dateStr, "%d-%d-%dT%d:%dZ",
-            &t.tm_year, &t.tm_mon, &t.tm_mday, &t.tm_hour, &t.tm_min);
-    
-        if (success != 5) {
-            return 0;
-        }
-    
-        t.tm_year = t.tm_year - 1900;
-        t.tm_mon = t.tm_mon - 1;
-        t.tm_sec = 0;
-        t.tm_wday = 0;
-        t.tm_yday = 0;
-        t.tm_isdst = 0;
-    
-        time_t localTime = mktime(&t);
-        time_t utcTime = localTime - timezone;
-        return utcTime;
-    }
-
     if (argc != 3) {
         fprintf(stderr, "Usage: %s indexfile testdate\n", argv[0]);
         fprintf(stderr, "       testdate format: YYYY-MM-DDThh:mm\n");
