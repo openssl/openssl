5e04e1d5f62db8ea4ecbf215ea840c8126f46e4d
Matt Caswell <matt@openssl.org>
2021-02-18 14:57:13 +0000

    Update copyright year

--- providers/implementations/kdfs/kbkdf.c
+++ providers/implementations/kdfs/kbkdf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2019-2021 The OpenSSL Project Authors. All Rights Reserved.
  * Copyright 2019 Red Hat, Inc.
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
