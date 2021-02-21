d9df865f72319abd6f7bffc2389804b4697f02a8
Armin Fuerst <armin@fuerst.priv.at>
2021-02-21 20:05:05 +0100

    obsoleted ca_logic by moving the remaining functions to a_time.c

--- crypto/asn1/a_time.c
+++ crypto/asn1/a_time.c
@@ -561,3 +561,61 @@ int ASN1_TIME_compare(const ASN1_TIME *a, const ASN1_TIME *b)
         return -1;
     return 0;
 }
+
+ASN1_TIME *asn1_string_to_ASN1_TIME(char *asn1_string)
+{
+    size_t len;
+    ASN1_TIME *tmps = NULL;
+    char *p;
+
+    len = strlen(asn1_string) + 1;
+    tmps = ASN1_STRING_new();
+    if (tmps == NULL)
+        return NULL;
+
+    if (!ASN1_STRING_set(tmps, NULL, len)) {
+        ASN1_STRING_free(tmps);
+        return NULL;
+    }
+
+    if (strlen(asn1_string) == 13)
+        tmps->type = V_ASN1_UTCTIME;
+    else
+        tmps->type = V_ASN1_GENERALIZEDTIME;
+    p = (char*)tmps->data;
+
+    tmps->length = BIO_snprintf(p, len, "%s", asn1_string);
+
+    return tmps;
+}
+
+time_t *asn1_string_to_time_t(char *asn1_string)
+{
+    ASN1_TIME *testdate_asn1 = NULL;
+    struct tm *testdate_tm = NULL;
+    time_t *testdatelocal = NULL;
+    time_t *testdateutc = NULL;
+
+    testdate_asn1 = asn1_string_to_ASN1_TIME(asn1_string);
+    if (testdate_asn1 == NULL)
+        return NULL;
+
+    testdate_tm = malloc(sizeof(*testdate_tm));
+
+    if (!(ASN1_TIME_to_tm(testdate_asn1, testdate_tm))) {
+        free(testdate_tm);
+        ASN1_STRING_free(testdate_asn1);
+        return NULL;
+    }
+
+    testdatelocal = malloc(sizeof(time_t));
+    *testdatelocal = mktime(testdate_tm);
+    free(testdate_tm);
+
+    testdateutc = malloc(sizeof(time_t));
+    *testdateutc = *testdatelocal - timezone;
+
+    free(testdatelocal);
+    ASN1_STRING_free(testdate_asn1);
+    return testdateutc;
+}
