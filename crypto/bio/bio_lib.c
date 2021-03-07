f111cd271ee5c4ec50223d1a17ae5ad25a6cd868
Armin Fuerst <armin@fuerst.priv.at>
2021-03-08 00:19:18 +0100

    reverted BIO_write

--- crypto/bio/bio_lib.c
+++ crypto/bio/bio_lib.c
@@ -348,12 +348,15 @@ static int bio_write_intern(BIO *b, const void *data, size_t dlen,
     return ret;
 }
 
-int BIO_write(BIO *b, const void *data, size_t dlen)
+int BIO_write(BIO *b, const void *data, int dlen)
 {
     size_t written;
     int ret;
 
-    ret = bio_write_intern(b, data, dlen, &written);
+    if (dlen < 0)
+        return 0;
+
+    ret = bio_write_intern(b, data, (size_t)dlen, &written);
 
     if (ret > 0) {
         /* *written should always be <= dlen */
