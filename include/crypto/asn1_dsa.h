2858156e0309031da8476e51fe76f0ce8c15010f
Shane Lontis <shane.lontis@oracle.com>
2021-03-09 09:59:13 +1000

    Add ossl_encode symbols
    Partial fix for #12964
    
    Reviewed-by: Paul Dale <pauli@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/14473)

--- include/crypto/asn1_dsa.h
+++ include/crypto/asn1_dsa.h
@@ -13,9 +13,9 @@
 
 #include "internal/packet.h"
 
-int encode_der_length(WPACKET *pkt, size_t cont_len);
-int encode_der_integer(WPACKET *pkt, const BIGNUM *n);
-int encode_der_dsa_sig(WPACKET *pkt, const BIGNUM *r, const BIGNUM *s);
+int ossl_encode_der_length(WPACKET *pkt, size_t cont_len);
+int ossl_encode_der_integer(WPACKET *pkt, const BIGNUM *n);
+int ossl_encode_der_dsa_sig(WPACKET *pkt, const BIGNUM *r, const BIGNUM *s);
 int ossl_decode_der_length(PACKET *pkt, PACKET *subpkt);
 int ossl_decode_der_integer(PACKET *pkt, BIGNUM *n);
 size_t ossl_decode_der_dsa_sig(BIGNUM *r, BIGNUM *s, const unsigned char **ppin,
