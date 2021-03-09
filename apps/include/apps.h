e5eb6907676d62547e4674acd946aed9a5cb9e37
Armin Fuerst <armin@fuerst.priv.at>
2021-03-09 16:56:48 +0100

    moved default_config_file

--- apps/include/apps.h
+++ apps/include/apps.h
@@ -46,7 +46,7 @@ void app_RAND_load_conf(CONF *c, const char *section);
 void app_RAND_write(void);
 int app_RAND_load(void);
 
-extern char *default_config_file; /* may be "" */
+//extern char *default_config_file; /* may be "" */
 extern BIO *bio_in;
 extern BIO *bio_out;
 extern BIO *bio_err;
