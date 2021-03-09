87638780631d1e70ff54cba00bfb8118d6b4f061
Shane Lontis <shane.lontis@oracle.com>
2021-03-09 11:47:48 +1000

    Add ossl_aria symbols
    Partial fix for #12964

--- providers/implementations/ciphers/cipher_aria_ccm_hw.c
+++ providers/implementations/ciphers/cipher_aria_ccm_hw.c
@@ -18,9 +18,9 @@ static int ccm_aria_initkey(PROV_CCM_CTX *ctx,
 {
     PROV_ARIA_CCM_CTX *actx = (PROV_ARIA_CCM_CTX *)ctx;
 
-    aria_set_encrypt_key(key, keylen * 8, &actx->ks.ks);
+    ossl_aria_set_encrypt_key(key, keylen * 8, &actx->ks.ks);
     CRYPTO_ccm128_init(&ctx->ccm_ctx, ctx->m, ctx->l, &actx->ks.ks,
-                       (block128_f)aria_encrypt);
+                       (block128_f)ossl_aria_encrypt);
     ctx->str = NULL;
     ctx->key_set = 1;
     return 1;
