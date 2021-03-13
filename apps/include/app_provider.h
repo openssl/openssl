80e34347fc5fa5b3c14861f31439b3a3ebfd9934
Armin Fuerst <armin@fuerst.priv.at>
2021-03-13 16:32:54 +0100

    Continue separation so it is obvious which parts must stay together

---
+++ apps/include/app_provider.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 1995-2021 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#ifndef OSSL_APPS_APP_PROVIDER_H
+#define OSSL_APPS_APP_PROVIDER_H
+
+void app_providers_cleanup(void);
+
+#endif                          /* ! OSSL_APPS_APPS_EXTRACTED_H */
