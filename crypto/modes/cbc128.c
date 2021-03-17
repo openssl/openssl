8129ac6ac4c0ca3a488c225cde580ede7dabe874
Pauli <ppzgs1@gmail.com>
2021-03-17 11:41:48 +1000

    modes: fix coverity 1449860: overlapping memory copy
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/14584)
    
    (cherry picked from commit 145f12d12dc83c737676883c625c2a95d34251ed)

--- crypto/modes/cbc128.c
+++ crypto/modes/cbc128.c
@@ -69,7 +69,8 @@ void CRYPTO_cbc128_encrypt(const unsigned char *in, unsigned char *out,
         in += 16;
         out += 16;
     }
-    memcpy(ivec, iv, 16);
+    if (ivec != iv)
+        memcpy(ivec, iv, 16);
 }
 
 void CRYPTO_cbc128_decrypt(const unsigned char *in, unsigned char *out,
