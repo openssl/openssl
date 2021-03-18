d33c2a3d8453a75509bcc8d2cf7d2dc2a3a518d0
Matt Caswell <matt@openssl.org>
2021-03-18 16:52:10 +0000

    Ensure buffer/length pairs are always in sync
    Following on from CVE-2021-3449 which was caused by a non-zero length
    associated with a NULL buffer, other buffer/length pairs are updated to
    ensure that they too are always in sync.
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Paul Dale <pauli@openssl.org>

--- ssl/statem/extensions.c
+++ ssl/statem/extensions.c
@@ -1149,6 +1149,7 @@ static int init_sig_algs_cert(SSL *s, unsigned int context)
     /* Clear any signature algorithms extension received */
     OPENSSL_free(s->s3->tmp.peer_cert_sigalgs);
     s->s3->tmp.peer_cert_sigalgs = NULL;
+    s->s3->tmp.peer_cert_sigalgslen = 0;
 
     return 1;
 }
