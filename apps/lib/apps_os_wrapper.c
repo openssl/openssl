466b8beac29dba1dfe6b2a9c1b81874a96562a65
Armin Fuerst <armin@fuerst.priv.at>
2021-03-28 10:21:04 +0200

    fix for AppVeyor result

--- apps/lib/apps_os_wrapper.c
+++ apps/lib/apps_os_wrapper.c
@@ -434,7 +434,7 @@ int app_open(const char *pathname, int flags, int mode)
     return open(pathname, flags, mode);
 }
 
-ssize_t app_read(int fd, void *buf, size_t count)
+ossl_ssize_t app_read(int fd, void *buf, size_t count)
 {
     return read(fd, buf, count);
 }
