8dc71c66e92ce348a71d450afeeff25313d24484
Armin Fuerst <armin@fuerst.priv.at>
2021-04-08 16:51:23 +0200

    ported changes from PR 14453

--- apps/include/apps_globals.h
+++ apps/include/apps_globals.h
@@ -31,6 +31,6 @@ const char *modestr(char mode, int format);
 const char *modeverb(char mode);
 int app_isdir(const char *);
 void app_bail_out(char *fmt, ...);
-void* app_malloc(size_t sz, const char *what);
+void *app_malloc(size_t sz, const char *what);
 
 #endif
