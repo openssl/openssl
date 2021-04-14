d7ccdff3164fed2fd14abdade2bac9dd5344ad9f
MichaM <contact-micha+github@posteo.de>
2021-04-14 23:45:05 +0200

    Fix typos
    CLA: trivial
    
    Signed-off-by: MichaM <contact-micha+github@posteo.de>

--- test/acvp_test.c
+++ test/acvp_test.c
@@ -206,7 +206,7 @@ err:
     return ret;
 }
 
-/* Extract r and s  from a ecdsa signature */
+/* Extract r and s  from an ecdsa signature */
 static int get_ecdsa_sig_rs_bytes(const unsigned char *sig, size_t sig_len,
                                   unsigned char **r, unsigned char **s,
                                   size_t *rlen, size_t *slen)
