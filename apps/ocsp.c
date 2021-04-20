1afeb96c5b8ef1825d41ad4bbce264802114733c
Rich Salz <rsalz@akamai.com>
2021-04-20 14:14:00 -0400

    Read a REQUEST not RESPONSE in ocsp responder
    Fixes: #13904

--- apps/ocsp.c
+++ apps/ocsp.c
@@ -1165,7 +1165,7 @@ static int do_responder(OCSP_REQUEST **preq, BIO **pcbio, BIO *acbio,
                         int timeout)
 {
 #ifndef OPENSSL_NO_SOCK
-    return http_server_get_asn1_req(ASN1_ITEM_rptr(OCSP_RESPONSE),
+    return http_server_get_asn1_req(ASN1_ITEM_rptr(OCSP_REQUEST),
                                     (ASN1_VALUE **)preq, NULL, pcbio, acbio,
                                     prog, 1 /* accept_get */, timeout);
 #else
