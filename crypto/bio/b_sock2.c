819ae43ec0cc475ce0f03622b4a5fef4a7fa1a1b
David Carlier <devnexen@gmail.com>
2021-04-24 16:13:26 +0100

    BIO_listen: disable setting ipv6_v6only on OpenBSD as it is a read only data and true
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Ben Kaduk <kaduk@mit.edu>
    (Merged from https://github.com/openssl/openssl/pull/15015)

--- crypto/bio/b_sock2.c
+++ crypto/bio/b_sock2.c
@@ -264,7 +264,8 @@ int BIO_listen(int sock, const BIO_ADDR *addr, int options)
         }
     }
 
-# ifdef IPV6_V6ONLY
+  /* On OpenBSD it is always ipv6 only with ipv6 sockets thus read-only */
+# if defined(IPV6_V6ONLY) && !defined(__OpenBSD__)
     if (BIO_ADDR_family(addr) == AF_INET6) {
         /*
          * Note: Windows default of IPV6_V6ONLY is ON, and Linux is OFF.
