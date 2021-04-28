14412688a10c640f4aa447def082d1c8f0dc9d4f
Matt Caswell <matt@openssl.org>
2021-04-28 16:38:32 +0100

    fixup! Add a test for the public core bio API

--- test/bio_core_test.c
+++ test/bio_core_test.c
@@ -54,7 +54,7 @@ const OSSL_DISPATCH biocbs[] = {
 
 static int test_bio_core(void)
 {
-    BIO *cbio = NULL, *cbiobad;
+    BIO *cbio = NULL, *cbiobad = NULL;
     OSSL_LIB_CTX *libctx = OSSL_LIB_CTX_new_from_dispatch(biocbs);
     int testresult = 0;
     OSSL_CORE_BIO corebio;
