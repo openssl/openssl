ddd3382bbf50d8a62a489d4eac3d87524454905d
Shane Lontis <shane.lontis@oracle.com>
2021-05-10 10:27:42 +1000

    Export/import flags for FFC params changed to seperate fields.
    An extra field got added to the ffc flags related to FIPS-186-2 key validation, but this field was
    not handled by the export/import since the flags were done as string combinations.
    To keep this consistent with other object flags they are now passed as seperate OSSL_PARAM fields.
    
    Fixes 'no-cached-fetch' build which uses export/import.

--- include/openssl/core_names.h
+++ include/openssl/core_names.h
@@ -294,12 +294,9 @@ extern "C" {
 #define OSSL_PKEY_PARAM_FFC_SEED            "seed"
 #define OSSL_PKEY_PARAM_FFC_COFACTOR        "j"
 #define OSSL_PKEY_PARAM_FFC_H               "hindex"
-#define OSSL_PKEY_PARAM_FFC_VALIDATE_TYPE   "valid-type"
-
-/* Diffie-Hellman/DSA Parameters parameter validation types */
-#define OSSL_FFC_PARAM_VALIDATE_PQ          "validate-pq"
-#define OSSL_FFC_PARAM_VALIDATE_G           "validate-g"
-#define OSSL_FFC_PARAM_VALIDATE_PQG         "validate-pqg"
+#define OSSL_PKEY_PARAM_FFC_VALIDATE_PQ     "validate-pq"
+#define OSSL_PKEY_PARAM_FFC_VALIDATE_G      "validate-g"
+#define OSSL_PKEY_PARAM_FFC_VALIDATE_LEGACY "validate-legacy"
 
 /* Diffie-Hellman params */
 #define OSSL_PKEY_PARAM_DH_GENERATOR        "safeprime-generator"
