c1aa719d4b5a770bd5fa0dd9cc4e5f8191a951eb
Matt Caswell <matt@openssl.org>
2021-05-14 15:33:40 +0100

    Fix a use-after-free in the child provider code
    If the child provider context data gets cleaned up before all usage of
    providers has finished then a use-after-free can occur. We change the
    priority of this data so that it gets freed later.
    
    Fixes #15284

--- include/internal/cryptlib.h
+++ include/internal/cryptlib.h
@@ -168,6 +168,7 @@ typedef struct ossl_ex_data_global_st {
 # define OSSL_LIB_CTX_CHILD_PROVIDER_INDEX          18
 # define OSSL_LIB_CTX_MAX_INDEXES                   19
 
+# define OSSL_LIB_CTX_METHOD_LOW_PRIORITY          -1
 # define OSSL_LIB_CTX_METHOD_DEFAULT_PRIORITY       0
 # define OSSL_LIB_CTX_METHOD_PRIORITY_1             1
 # define OSSL_LIB_CTX_METHOD_PRIORITY_2             2
