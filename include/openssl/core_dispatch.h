0e32becc1803bbdae9ecd178c2dd2ab702ea7718
Matt Caswell <matt@openssl.org>
2021-05-20 11:52:56 +0100

    Fix a memleak in the FIPS provider
    If the DRBG is used within the scope of the FIPS OSSL_provider_init
    function then it attempts to register a thread callback via c_thread_start.
    However the implementation of c_thread_start assumed that the provider's
    provctx was already present. However because OSSL_provider_init is still
    running it was actually NULL. This means the thread callback fail to work
    correctly and a memory leak resulted.
    
    Instead of having c_thread_start use the provctx as the callback argument
    we change the definition of c_thread_start to have an explicit callback
    argument to use.
    
    Reviewed-by: Paul Dale <pauli@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/15278)

--- include/openssl/core_dispatch.h
+++ include/openssl/core_dispatch.h
@@ -68,7 +68,8 @@ OSSL_CORE_MAKE_FUNC(int,core_get_params,(const OSSL_CORE_HANDLE *prov,
                                          OSSL_PARAM params[]))
 # define OSSL_FUNC_CORE_THREAD_START           3
 OSSL_CORE_MAKE_FUNC(int,core_thread_start,(const OSSL_CORE_HANDLE *prov,
-                                           OSSL_thread_stop_handler_fn handfn))
+                                           OSSL_thread_stop_handler_fn handfn,
+                                           void *arg))
 # define OSSL_FUNC_CORE_GET_LIBCTX             4
 OSSL_CORE_MAKE_FUNC(OPENSSL_CORE_CTX *,core_get_libctx,
                     (const OSSL_CORE_HANDLE *prov))
