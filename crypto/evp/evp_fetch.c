72f62f4408d2a4e75c58706d41775e4f8f7f0034
Pauli <pauli@openssl.org>
2021-05-24 09:35:08 +1000

    coverity 1484912: Null pointer dereferences (NULL_RETURNS)
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/15425)

--- crypto/evp/evp_fetch.c
+++ crypto/evp/evp_fetch.c
@@ -504,6 +504,9 @@ char *evp_get_global_properties_str(OSSL_LIB_CTX *libctx, int loadconfig)
     char *propstr = NULL;
     size_t sz;
 
+    if (plp == NULL)
+        return OPENSSL_strdup("");
+
     sz = ossl_property_list_to_string(libctx, *plp, NULL, 0);
     if (sz == 0) {
         ERR_raise(ERR_LIB_EVP, ERR_R_INTERNAL_ERROR);
