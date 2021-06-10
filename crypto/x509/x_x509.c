49a54634ec8cfff55f443bb458ab3f2d7791862d
Dr. David von Oheimb <David.von.Oheimb@siemens.com>
2021-06-10 19:36:35 +0200

    d2i_X509: revert calling X509v3_cache_extensions()
    Fixes #13754
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/15705)

--- crypto/x509/x_x509.c
+++ crypto/x509/x_x509.c
@@ -150,31 +150,8 @@ ASN1_SEQUENCE_ref(X509, x509_cb) = {
         ASN1_EMBED(X509, signature, ASN1_BIT_STRING)
 } ASN1_SEQUENCE_END_ref(X509, X509)
 
-IMPLEMENT_ASN1_ALLOC_FUNCTIONS_fname(X509, X509, X509)
+IMPLEMENT_ASN1_FUNCTIONS(X509)
 IMPLEMENT_ASN1_DUP_FUNCTION(X509)
-
-X509 *d2i_X509(X509 **a, const unsigned char **in, long len)
-{
-    X509 *cert = NULL;
-    int free_on_error = a != NULL && *a == NULL;
-
-    cert = (X509 *)ASN1_item_d2i((ASN1_VALUE **)a, in, len, (X509_it()));
-    /* Only cache the extensions if the cert object was passed in */
-    if (cert != NULL && a != NULL) { /* then cert == *a */
-        if (!ossl_x509v3_cache_extensions(cert)) {
-            if (free_on_error) {
-                *a = NULL;
-                X509_free(cert);
-            }
-            cert = NULL;
-        }
-    }
-    return cert;
-}
-int i2d_X509(const X509 *a, unsigned char **out)
-{
-    return ASN1_item_i2d((const ASN1_VALUE *)a, out, (X509_it()));
-}
 
 /*
  * This should only be used if the X509 object was embedded inside another
