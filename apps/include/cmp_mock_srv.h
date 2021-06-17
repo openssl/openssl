a4d96310f9c7ad5af6c7729da2745a0ca9ca3b05
Matt Caswell <matt@openssl.org>
2021-06-17 13:24:59 +0100

    Update copyright year
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/15801)

--- apps/include/cmp_mock_srv.h
+++ apps/include/cmp_mock_srv.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2020 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2018-2021 The OpenSSL Project Authors. All Rights Reserved.
  * Copyright Siemens AG 2018-2020
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
