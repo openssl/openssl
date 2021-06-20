1f4185f8d50c362969efad84ca4bcab8eaab5ae2
Loc Le <lequangloc@gmail.com>
2021-06-20 10:00:29 +0100

    remove the redundant assignment

--- crypto/ui/ui_lib.c
+++ crypto/ui/ui_lib.c
@@ -44,7 +44,6 @@ UI *UI_new_method(const UI_METHOD *method)
 
     if (!CRYPTO_new_ex_data(CRYPTO_EX_INDEX_UI, ret, &ret->ex_data)) {
         CRYPTO_THREAD_lock_free(ret->lock);
-	ret->lock = NULL;
         OPENSSL_free(ret);
         return NULL;
     }
