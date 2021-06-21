a7e996ab3d9b038c6b9e783e032f10791ddf6cbf
Matt Caswell <matt@openssl.org>
2021-06-21 15:37:48 +0100

    Don't skip the current provider in ossl_provider_register_child_cb
    This restriction was in place to avoid problems with recursive attempts
    to aquire the flag lock/store lock from within a provider's init function.
    Since those locks are no longer held when calling the init function there
    is no reason for the restriction any more.

--- crypto/provider_core.c
+++ crypto/provider_core.c
@@ -1550,13 +1550,7 @@ static int ossl_provider_register_child_cb(const OSSL_CORE_HANDLE *handle,
     max = sk_OSSL_PROVIDER_num(store->providers);
     for (i = 0; i < max; i++) {
         prov = sk_OSSL_PROVIDER_value(store->providers, i);
-        /*
-         * We require register_child_cb to be called during a provider init
-         * function. The currently initing provider will never be activated yet
-         * and we we should not attempt to aquire the flag_lock for it.
-         */
-        if (prov == thisprov)
-            continue;
+
         if (!CRYPTO_THREAD_read_lock(prov->flag_lock))
             break;
         /*
