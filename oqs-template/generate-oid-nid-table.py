1ef99fbe2c7f0f15f755e0618c541d80c1b70fc7
Michael Baentsch <info@baentsch.ch>
2021-08-03 17:44:36 +0200

    adding NIST round information [skip ci] (#325)

--- oqs-template/generate-oid-nid-table.py
+++ oqs-template/generate-oid-nid-table.py
@@ -19,19 +19,16 @@ args = parser.parse_args()
 ######################################
 
 liboqs_sig_docs_dir = os.path.join(args.liboqs_docs_dir, 'algorithms', 'sig')
-sig_to_impl_version = {}
+liboqs_sigs = {}
 for root, _, files in os.walk(liboqs_sig_docs_dir):
     for fil in files:
+      if fil.endswith(".yml"):
         with open(os.path.join(root, fil), mode='r', encoding='utf-8') as f:
-            alg_pretty_name = next(f).rstrip()
-            if alg_pretty_name.startswith("# "): alg_pretty_name = alg_pretty_name[1:].lstrip()
-            for line in f:
-                if line.startswith("- **Version**:"):
-                    sig_to_impl_version[alg_pretty_name] = line.split(":")[1].rstrip()
-                    break
+           algyml = yaml.safe_load(f.read())
+        liboqs_sigs[algyml['name']]=algyml
 
 table = [['Algorithm', 'Implementation Version',
-          'Claimed NIST Level', 'Code Point', 'OID']]
+          'NIST round', 'Claimed NIST Level', 'Code Point', 'OID']]
 claimed_nist_level = 0
 for sig in sorted(config['sigs'], key=lambda s: s['family']):
     for variant in sig['variants']:
@@ -47,13 +44,14 @@ for sig in sorted(config['sigs'], key=lambda s: s['family']):
         if sig['family'].startswith('SPHINCS'):
             sig['family'] = 'SPHINCS+'
 
-        table.append([variant['name'], sig_to_impl_version[sig['family']],
-                      claimed_nist_level, variant['code_point'],
+        table.append([variant['name'], liboqs_sigs[sig['family']]['spec-version'],
+                      liboqs_sigs[sig['family']]['nist-round'], claimed_nist_level, variant['code_point'],
                       variant['oid']])
 
         for hybrid in variant['mix_with']:
             table.append([variant['name'] + ' **hybrid with** ' + hybrid['name'],
-                          sig_to_impl_version[sig['family']],
+                          liboqs_sigs[sig['family']]['spec-version'],
+                          liboqs_sigs[sig['family']]['nist-round'],
                           claimed_nist_level,
                           hybrid['code_point'],
                           hybrid['oid']])
@@ -66,19 +64,18 @@ with open('oqs-sig-info.md', mode='w', encoding='utf-8') as f:
 ##################################
 
 liboqs_kem_docs_dir = os.path.join(args.liboqs_docs_dir, 'algorithms', 'kem')
-kem_to_impl_version = {}
+liboqs_kems = {}
 for root, _, files in os.walk(liboqs_kem_docs_dir):
     for fil in files:
+      if fil.endswith(".yml"):
         with open(os.path.join(root, fil), mode='r', encoding='utf-8') as f:
-            alg_pretty_name = next(f).rstrip()
-            if alg_pretty_name.startswith("# "): alg_pretty_name = alg_pretty_name[1:].lstrip()
-            for line in f:
-                if line.startswith("- **Version**:"):
-                    kem_to_impl_version[alg_pretty_name] = line.split(":")[1].rstrip()
-                    break
-kem_to_impl_version['SIDH'] = kem_to_impl_version['SIKE']
+           algyml = yaml.safe_load(f.read())
+        liboqs_kems[algyml['name']]=algyml
+liboqs_kems['SIDH']=liboqs_kems['SIKE']
+# TODO: Workaround for wrong upstream name for Kyber:
+liboqs_kems['CRYSTALS-Kyber']=liboqs_kems['Kyber']
 
-table_header = ['Family', 'Implementation Version', 'Variant', 'Claimed NIST Level',
+table_header = ['Family', 'Implementation Version', 'Variant', 'NIST round', 'Claimed NIST Level',
            'Code Point', 'Hybrid Elliptic Curve (if any)']
 table = []
 hybrid_elliptic_curve = ''
@@ -98,30 +95,30 @@ for kem in sorted(config['kems'], key=lambda k: k['family']):
     if 'implementation_version' in kem:
         implementation_version = kem['implementation_version']
     else:
-        implementation_version = kem_to_impl_version[kem['family']]
+        implementation_version = liboqs_kems[kem['family']]['spec-version']
 
     try: 
        table.append([kem['family'], implementation_version,
-                     kem['name_group'], claimed_nist_level, 
+                     kem['name_group'], liboqs_kems[kem['family']]['nist-round'], claimed_nist_level,
                      kem['nid'], ""])
        table.append([kem['family'], implementation_version,
-                     kem['name_group'], claimed_nist_level, 
+                     kem['name_group'], liboqs_kems[kem['family']]['nist-round'], claimed_nist_level,
                      kem['nid_hybrid'], hybrid_elliptic_curve])
-    except KeyError:
+    except KeyError as ke:
        # Non-existant NIDs mean this alg is not supported any more
        pass
 
     if 'extra_nids' in kem:
-        if 'current' in kem['extra_nids']:
+        if 'current' in kem['extra_nids']: # assume "current" NIDs to mean liboqs-driven NIST round information:
             for entry in kem['extra_nids']['current']:
                 table.append([kem['family'], implementation_version,
-                              kem['name_group'], claimed_nist_level, 
+                              kem['name_group'], liboqs_kems[kem['family']]['nist-round'], claimed_nist_level,
                               entry['nid'], 
                               entry['hybrid_group'] if 'hybrid_group' in entry else ""])
-        if 'old' in kem['extra_nids']:
+        if 'old' in kem['extra_nids']: # assume "old" submissions to mean NIST round 2:
             for entry in kem['extra_nids']['old']:
                 table.append([kem['family'], entry['implementation_version'],
-                              kem['name_group'], claimed_nist_level, 
+                              kem['name_group'], 2, claimed_nist_level,
                               entry['nid'],
                               entry['hybrid_group'] if 'hybrid_group' in entry else ""])
 
