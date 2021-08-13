6179f50454b9bfe8861b778f597f434d33804b06
Todd Short <tshort@akamai.com>
2021-08-13 09:59:59 -0400

    Fix potential double-free
    The `sk` variable is assigned to `s->session->peer_chain`.
    If `ssl3_digest_cached_records()` were to fail, then `sk` would still be
    non-NULL, and subsequently freed on the error return. When the session
    is freed, it will then attempt to free `s->session->peer_chain`,
    resulting in a double-free (of `sk`).
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/16309)

--- ssl/statem/statem_srvr.c
+++ ssl/statem/statem_srvr.c
@@ -3556,6 +3556,7 @@ MSG_PROCESS_RETURN tls_process_client_certificate(SSL *s, PACKET *pkt)
 
     sk_X509_pop_free(s->session->peer_chain, X509_free);
     s->session->peer_chain = sk;
+    sk = NULL;
 
     /*
      * Freeze the handshake buffer. For <TLS1.3 we do this after the CKE
@@ -3570,7 +3571,6 @@ MSG_PROCESS_RETURN tls_process_client_certificate(SSL *s, PACKET *pkt)
      * Inconsistency alert: cert_chain does *not* include the peer's own
      * certificate, while we do include it in statem_clnt.c
      */
-    sk = NULL;
 
     /* Save the current hash state for when we receive the CertificateVerify */
     if (SSL_IS_TLS13(s)) {
