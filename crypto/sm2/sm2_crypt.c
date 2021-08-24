6f2a7abe22de08bdaacf0025349b30d46826108a
Matt Caswell <matt@openssl.org>
2021-08-24 14:14:34 +0100

    Update copyright year
    Reviewed-by: Richard Levitte <levitte@openssl.org>

--- crypto/sm2/sm2_crypt.c
+++ crypto/sm2/sm2_crypt.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2017-2021 The OpenSSL Project Authors. All Rights Reserved.
  * Copyright 2017 Ribose Inc. All Rights Reserved.
  * Ported from Ribose contributions from Botan.
  *
