b5d7c5a0c443093790baae8d9061b140d5a65926
xtkoba <69125751+xtkoba@users.noreply.github.com>
2021-09-12 07:15:13 +0900

    Update threads_pthread.c

--- crypto/threads_pthread.c
+++ crypto/threads_pthread.c
@@ -216,9 +216,11 @@ int CRYPTO_atomic_or(uint64_t *val, uint64_t op, uint64_t *ret,
                      CRYPTO_RWLOCK *lock)
 {
 # if defined(__GNUC__) && defined(__ATOMIC_ACQ_REL)
-    if (__atomic_is_lock_free(sizeof(*val), (CRYPTO_ATOMIC_U64 *)val)) {
-        *ret = __atomic_or_fetch((CRYPTO_ATOMIC_U64 *)val, op, __ATOMIC_ACQ_REL);
-        return 1;
+    if (CRYPTO_ATOMIC_IS_ALIGNED_U64(val) &&
+        __atomic_is_lock_free(sizeof(*val), (CRYPTO_ATOMIC_U64 *)val)) {
+            *ret = __atomic_or_fetch((CRYPTO_ATOMIC_U64 *)val, op,
+                                     __ATOMIC_ACQ_REL);
+            return 1;
     }
 # elif defined(__sun) && (defined(__SunOS_5_10) || defined(__SunOS_5_11))
     /* This will work for all future Solaris versions. */
@@ -241,10 +243,12 @@ int CRYPTO_atomic_or(uint64_t *val, uint64_t op, uint64_t *ret,
 int CRYPTO_atomic_load(uint64_t *val, uint64_t *ret, CRYPTO_RWLOCK *lock)
 {
 # if defined(__GNUC__) && defined(__ATOMIC_ACQUIRE)
-    if (__atomic_is_lock_free(sizeof(*val), (CRYPTO_ATOMIC_U64 *)val)) {
-        __atomic_load((CRYPTO_ATOMIC_U64 *)val, (CRYPTO_ATOMIC_U64 *)ret,
-                      __ATOMIC_ACQUIRE);
-        return 1;
+    if (CRYPTO_ATOMIC_IS_ALIGNED_U64(val) &&
+        CRYPTO_ATOMIC_IS_ALIGNED_U64(ret) &&
+        __atomic_is_lock_free(sizeof(*val), (CRYPTO_ATOMIC_U64 *)val)) {
+            __atomic_load((CRYPTO_ATOMIC_U64 *)val, (CRYPTO_ATOMIC_U64 *)ret,
+                          __ATOMIC_ACQUIRE);
+            return 1;
     }
 # elif defined(__sun) && (defined(__SunOS_5_10) || defined(__SunOS_5_11))
     /* This will work for all future Solaris versions. */
