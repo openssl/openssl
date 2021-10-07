4feeaad736184e4f46cbbe13e21b97169b023523
Michael Baentsch <info@baentsch.ch>
2021-10-07 18:12:19 +0200

    addressing PR feedback

--- crypto/provider_core.c
+++ crypto/provider_core.c
@@ -1933,8 +1933,12 @@ static int core_obj_add_sigid(const OSSL_CORE_HANDLE *prov,
                               const char *pkey_name)
 {
     int sign_nid = OBJ_txt2nid(sign_name);
-    int digest_nid = digest_name?OBJ_txt2nid(digest_name):NID_undef;
+    int digest_nid = NID_undef;
     int pkey_nid = OBJ_txt2nid(pkey_name);
+
+    if (digest_name != NULL && digest_name[0] != '\0'
+        && (digest_nid = OBJ_txt2nid(digest_name)) == NID_undef)
+            return 0;
 
     if (sign_nid == NID_undef)
         return 0;
