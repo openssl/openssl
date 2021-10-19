37f1828d8701662c40cc98172001a533fe49764c
Daniel Hu <Daniel.Hu@arm.com>
2021-10-19 22:49:05 +0100

    SM4 optimization for ARM by HW instruction
    This patch implements the SM4 optimization for ARM processor,
    using SM4 HW instruction, which is an optional feature of
    crypto extension for aarch64 V8.
    
    Tested on some modern ARM micro-architectures with SM4 support, the
    performance uplift can be observed around 8X~40X over existing
    C implementation in openssl. Algorithms that can be parallelized
    (like CTR, ECB, CBC decryption) are on higher end, with algorithm
    like CBC encryption on lower end (due to inter-block dependency)
    
    Perf data on Yitian-710 2.75GHz hardware, before and after optimization:
    
    Before:
      type      16 bytes     64 bytes    256 bytes    1024 bytes   8192 bytes  16384 bytes
      SM4-CTR  105787.80k   107837.87k   108380.84k   108462.08k   108549.46k   108554.92k
      SM4-ECB  111924.58k   118173.76k   119776.00k   120093.70k   120264.02k   120274.94k
      SM4-CBC  106428.09k   109190.98k   109674.33k   109774.51k   109827.41k   109827.41k
    
    After (7.4x - 36.6x faster):
      type      16 bytes     64 bytes    256 bytes    1024 bytes   8192 bytes  16384 bytes
      SM4-CTR  781979.02k  2432994.28k  3437753.86k  3834177.88k  3963715.58k  3974556.33k
      SM4-ECB  937590.69k  2941689.02k  3945751.81k  4328655.87k  4459181.40k  4468692.31k
      SM4-CBC  890639.88k  1027746.58k  1050621.78k  1056696.66k  1058613.93k  1058701.31k
    
    Signed-off-by: Daniel Hu <Daniel.Hu@arm.com>
    
    Reviewed-by: Paul Dale <pauli@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/17455)
    
    (cherry picked from commit 15b7175f558bf9eb057ec3266685486f727dd70f)

---
+++ include/crypto/sm4_platform.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#ifndef OSSL_SM4_PLATFORM_H
+# define OSSL_SM4_PLATFORM_H
+# pragma once
+
+# if defined(OPENSSL_CPUID_OBJ)
+#  if (defined(__arm__) || defined(__arm) || defined(__aarch64__))
+#   include "arm_arch.h"
+#   if __ARM_MAX_ARCH__>=8
+#    define HWSM4_CAPABLE (OPENSSL_armcap_P & ARMV8_SM4)
+#    define HWSM4_set_encrypt_key sm4_v8_set_encrypt_key
+#    define HWSM4_set_decrypt_key sm4_v8_set_decrypt_key
+#    define HWSM4_encrypt sm4_v8_encrypt
+#    define HWSM4_decrypt sm4_v8_decrypt
+#    define HWSM4_cbc_encrypt sm4_v8_cbc_encrypt
+#    define HWSM4_ecb_encrypt sm4_v8_ecb_encrypt
+#    define HWSM4_ctr32_encrypt_blocks sm4_v8_ctr32_encrypt_blocks
+#   endif
+#  endif
+# endif /* OPENSSL_CPUID_OBJ */
+
+# if defined(HWSM4_CAPABLE)
+int HWSM4_set_encrypt_key(const unsigned char *userKey, SM4_KEY *key);
+int HWSM4_set_decrypt_key(const unsigned char *userKey, SM4_KEY *key);
+void HWSM4_encrypt(const unsigned char *in, unsigned char *out,
+                   const SM4_KEY *key);
+void HWSM4_decrypt(const unsigned char *in, unsigned char *out,
+                   const SM4_KEY *key);
+void HWSM4_cbc_encrypt(const unsigned char *in, unsigned char *out,
+                       size_t length, const SM4_KEY *key,
+                       unsigned char *ivec, const int enc);
+void HWSM4_ecb_encrypt(const unsigned char *in, unsigned char *out,
+                       size_t length, const SM4_KEY *key,
+                       const int enc);
+void HWSM4_ctr32_encrypt_blocks(const unsigned char *in, unsigned char *out,
+                                size_t len, const void *key,
+                                const unsigned char ivec[16]);
+# endif /* HWSM4_CAPABLE */
+
+#endif /* OSSL_SM4_PLATFORM_H */
