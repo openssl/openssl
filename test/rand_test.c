40cd60505e57dd1ea7e8fd7a36360ceac3683056
Pauli <pauli@openssl.org>
2021-10-25 19:06:26 +1000

    fixup! test: add unit test for test-rand RNG

--- test/rand_test.c
+++ test/rand_test.c
@@ -17,24 +17,32 @@ static int test_rand(void)
 {
     EVP_RAND_CTX *privctx;
     OSSL_PARAM params[2], *p = params;
-    unsigned char entropy[] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05 };
+    unsigned char entropy1[] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05 };
+    unsigned char entropy2[] = { 0xff, 0xfe, 0xfd };
     unsigned char outbuf[3];
 
     if (!TEST_ptr(privctx = RAND_get0_private(NULL)))
         return 0;
 
     *p++ = OSSL_PARAM_construct_octet_string(OSSL_RAND_PARAM_TEST_ENTROPY,
-                                             entropy, sizeof(entropy));
+                                             entropy1, sizeof(entropy1));
     *p = OSSL_PARAM_construct_end();
 
     if (!TEST_ptr(privctx = RAND_get0_private(NULL))
             || !TEST_true(EVP_RAND_CTX_set_params(privctx, params))
             || !TEST_int_gt(RAND_priv_bytes(outbuf, sizeof(outbuf)), 0)
-            || !TEST_mem_eq(outbuf, sizeof(outbuf), entropy, sizeof(outbuf))
+            || !TEST_mem_eq(outbuf, sizeof(outbuf), entropy1, sizeof(outbuf))
             || !TEST_int_le(RAND_priv_bytes(outbuf, sizeof(outbuf) + 1), 0)
             || !TEST_int_gt(RAND_priv_bytes(outbuf, sizeof(outbuf)), 0)
             || !TEST_mem_eq(outbuf, sizeof(outbuf),
-                            entropy + sizeof(outbuf), sizeof(outbuf)))
+                            entropy1 + sizeof(outbuf), sizeof(outbuf)))
+        return 0;
+
+    *params = OSSL_PARAM_construct_octet_string(OSSL_RAND_PARAM_TEST_ENTROPY,
+                                                entropy2, sizeof(entropy2));
+    if (!TEST_true(EVP_RAND_CTX_set_params(privctx, params))
+            || !TEST_int_gt(RAND_priv_bytes(outbuf, sizeof(outbuf)), 0)
+            || !TEST_mem_eq(outbuf, sizeof(outbuf), entropy2, sizeof(outbuf)))
         return 0;
     return 1;
 }
