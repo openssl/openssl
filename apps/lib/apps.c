b64194ae2d50d066a09d0cbe3131636f046efcc7
Rami Khaldi <rami.khaldi@fau.de>
2021-11-13 16:19:01 +0100

    Return a correct exit code when using the command line utility to check certificates and include the ip/email/host return code

--- apps/lib/apps.c
+++ apps/lib/apps.c
@@ -2106,29 +2106,37 @@ unsigned char *next_protos_parse(size_t *outlen, const char *in)
     return out;
 }
 
-void print_cert_checks(BIO *bio, X509 *x,
+int X509_check_cert(BIO *bio, X509 *x,
                        const char *checkhost,
-                       const char *checkemail, const char *checkip)
+                       const char *checkemail, const char *checkip, int print)
 {
+    int ret = 1;
+
     if (x == NULL)
-        return;
+        return 0;
+
     if (checkhost) {
-        BIO_printf(bio, "Hostname %s does%s match certificate\n",
-                   checkhost,
-                   X509_check_host(x, checkhost, 0, 0, NULL) == 1
-                       ? "" : " NOT");
+	    int valid_host = X509_check_host(x, checkhost, 0, 0, NULL);
+	    if (print)
+		BIO_printf(bio, "Hostname %s does%s match certificate\n", checkhost, valid_host == 1 ? "" : " NOT");
+	    ret = ret && valid_host;
     }
 
     if (checkemail) {
-        BIO_printf(bio, "Email %s does%s match certificate\n",
-                   checkemail, X509_check_email(x, checkemail, 0, 0)
-                   ? "" : " NOT");
+	    int valid_mail = X509_check_email(x, checkemail, 0, 0);
+	    if (print)
+		BIO_printf(bio, "Email %s does%s match certificate\n",   checkemail, valid_mail ? "" : " NOT");
+	    ret = ret && valid_mail;
     }
 
     if (checkip) {
-        BIO_printf(bio, "IP %s does%s match certificate\n",
-                   checkip, X509_check_ip_asc(x, checkip, 0) ? "" : " NOT");
+	   int valid_ip   =  X509_check_ip_asc(x, checkip, 0);
+	   if (print)
+		BIO_printf(bio, "IP %s does%s match certificate\n",      checkip,  valid_ip ? "" : " NOT");
+	   ret = ret && valid_ip;
     }
+
+    return ret;
 }
 
 static int do_pkey_ctx_init(EVP_PKEY_CTX *pkctx, STACK_OF(OPENSSL_STRING) *opts)
