05b19b9f09ed4726b860db28d0fae1328417cee7
Rami Khaldi <rami.khaldi@fau.de>
2021-11-13 16:19:01 +0100

    Return a correct exit code when using the command line utility to check certificates and include the ip/email/host return code CLA: trivial

--- apps/include/apps.h
+++ apps/include/apps.h
@@ -263,9 +263,9 @@ extern char *psk_key;
 
 unsigned char *next_protos_parse(size_t *outlen, const char *in);
 
-void print_cert_checks(BIO *bio, X509 *x,
+int X509_check_cert(BIO *bio, X509 *x,
                        const char *checkhost,
-                       const char *checkemail, const char *checkip);
+                       const char *checkemail, const char *checkip, int print);
 
 void store_setup_crl_download(X509_STORE *st);
 
