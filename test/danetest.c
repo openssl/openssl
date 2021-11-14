8b7c2c77df29de5dc2aae62fe32a8e7ba755ceb0
Peiwei Hu <jlu.hpw@foxmail.com>
2021-11-14 16:55:45 +0800

    BIO_gets: fix the incomplete return check
    Reviewed-by: Paul Dale <pauli@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/17028)
    
    (cherry picked from commit 7264068a15e7c4955efa25753430595a45caa16f)

--- test/danetest.c
+++ test/danetest.c
@@ -152,7 +152,7 @@ static char *read_to_eol(BIO *f)
     static char buf[4096];
     int n;
 
-    if (!BIO_gets(f, buf, sizeof(buf)))
+    if (BIO_gets(f, buf, sizeof(buf)) <= 0)
         return NULL;
 
     n = strlen(buf);
