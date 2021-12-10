d9ad5b16b32172df6f7d02cfb1c339cc85d0db01
Matt Caswell <matt@openssl.org>
2021-12-10 16:53:02 +0000

    Ensure that MDs created via EVP_MD_meth_new() go down the legacy route
    MDs created via EVP_MD_meth_new() are inherently legacy and therefore
    need to go down the legacy route when they are used.
    
    Reviewed-by: Dmitry Belyavskiy <beldmit@gmail.com>
    (Merged from https://github.com/openssl/openssl/pull/17255)

--- crypto/evp/digest.c
+++ crypto/evp/digest.c
@@ -208,7 +208,8 @@ static int evp_md_init_internal(EVP_MD_CTX *ctx, const EVP_MD *type,
             || tmpimpl != NULL
 # endif
 #endif
-            || (ctx->flags & EVP_MD_CTX_FLAG_NO_INIT) != 0) {
+            || (ctx->flags & EVP_MD_CTX_FLAG_NO_INIT) != 0
+            || type->origin == EVP_ORIG_METH) {
         if (ctx->digest == ctx->fetched_digest)
             ctx->digest = NULL;
         EVP_MD_free(ctx->fetched_digest);
