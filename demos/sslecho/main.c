167651e91af3cb5b6232b6185f05760457c429f5
bobwirka <bobwirka@yahoo.com>
2021-12-13 17:02:48 -0500

    Added asterisks to copyright in main.c.

--- demos/sslecho/main.c
+++ demos/sslecho/main.c
@@ -1,10 +1,10 @@
 /*
-    Copyright 20xx-20yy The OpenSSL Project Authors. All Rights Reserved.
-
-    Licensed under the Apache License 2.0 (the "License").  You may not use
-    this file except in compliance with the License.  You can obtain a copy
-    in the file LICENSE in the source distribution or at
-    https://www.openssl.org/source/license.html
+ *  Copyright 20xx-20yy The OpenSSL Project Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License 2.0 (the "License").  You may not use
+ *  this file except in compliance with the License.  You can obtain a copy
+ *  in the file LICENSE in the source distribution or at
+ *  https://www.openssl.org/source/license.html
  */
 
 #include <stdio.h>
