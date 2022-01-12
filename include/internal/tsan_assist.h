d1ec05915686019eec8fa8de9890292980fc5d6e
Pauli <pauli@openssl.org>
2022-01-12 13:26:38 +1100

    tsan: make detecting the need for locking when using tsan easier
    Reviewed-by: Bernd Edlinger <bernd.edlinger@hotmail.de>
    (Merged from https://github.com/openssl/openssl/pull/17479)

--- include/internal/tsan_assist.h
+++ include/internal/tsan_assist.h
@@ -130,7 +130,13 @@
 
 #ifndef TSAN_QUALIFIER
 
-# define TSAN_QUALIFIER volatile
+# ifdef OPENSSL_THREADS
+#  define TSAN_QUALIFIER volatile
+#  define TSAN_REQUIRES_LOCKING
+# else  /* OPENSSL_THREADS */
+#  define TSAN_QUALIFIER
+# endif /* OPENSSL_THREADS */
+
 # define tsan_load(ptr) (*(ptr))
 # define tsan_store(ptr, val) (*(ptr) = (val))
 # define tsan_counter(ptr) ((*(ptr))++)
