9913407882f31bcf4d73f533d8d93db2847bfa9b
Richard Levitte <levitte@openssl.org>
2022-01-22 14:31:57 +0100

    fixup! Add support for signed BIGNUMs in the OSSL_PARAM_BLD API

--- crypto/param_build.c
+++ crypto/param_build.c
@@ -232,7 +232,7 @@ int OSSL_PARAM_BLD_push_BN(OSSL_PARAM_BLD *bld, const char *key,
                            const BIGNUM *bn)
 {
     if (BN_is_negative(bn))
-        return push_BN(bld, key, bn, bn == NULL ? 0 : BN_num_bytes(bn),
+        return push_BN(bld, key, bn, bn == NULL ? 0 : BN_num_bytes(bn) + 1,
                        OSSL_PARAM_INTEGER);
     return push_BN(bld, key, bn, bn == NULL ? 0 : BN_num_bytes(bn),
                    OSSL_PARAM_UNSIGNED_INTEGER);
