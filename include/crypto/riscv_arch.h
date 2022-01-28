360f6dcc5aa1a86ec3ff9a94612b88e3d960ee2e
Henry Brausen <henry.brausen@vrull.eu>
2022-01-28 01:28:52 -0700

    Add basic RISC-V cpuid and OPENSSL_riscvcap
    RISC-V cpuid implementation allows bitmanip extensions Zb[abcs] to
    be enabled at runtime using OPENSSL_riscvcap environment variable.
    
    For example, to specify 64-bit RISC-V with the G,C,Zba,Zbb,Zbc
    extensions, one could write: OPENSSL_riscvcap="rv64gc_zba_zbb_zbc"
    
    Architecture string parsing is still very primitive, but can be
    expanded in the future. Currently, only bitmanip extensions Zba, Zbb,
    Zbc and Zbs are supported.
    
    Includes implementation of constant-time CRYPTO_memcmp in riscv64 asm,
    as well as OPENSSL_cleanse. Assembly implementations are written using
    perlasm.
    
    Reviewed-by: Philipp Tomsich <philipp.tomsich@vrull.eu>
    Signed-off-by: Henry Brausen <henry.brausen@vrull.eu>
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Paul Dale <pauli@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/17640)

---
+++ include/crypto/riscv_arch.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#ifndef OSSL_CRYPTO_RISCV_ARCH_H
+# define OSSL_CRYPTO_RISCV_ARCH_H
+
+# include <ctype.h>
+# include <stdint.h>
+
+# define RISCV_DEFINE_CAP(NAME, INDEX, BIT_INDEX) +1
+extern uint32_t OPENSSL_riscvcap_P[ ((
+# include "riscv_arch.def"
+) + sizeof(uint32_t) - 1) / sizeof(uint32_t) ];
+
+# ifdef OPENSSL_RISCVCAP_IMPL
+#  define RISCV_DEFINE_CAP(NAME, INDEX, BIT_INDEX) +1
+uint32_t OPENSSL_riscvcap_P[ ((
+#  include "riscv_arch.def"
+) + sizeof(uint32_t) - 1) / sizeof(uint32_t) ];
+# endif
+
+# define RISCV_DEFINE_CAP(NAME, INDEX, BIT_INDEX)                   \
+    static inline int RISCV_HAS_##NAME(void)                        \
+    {                                                               \
+        return (OPENSSL_riscvcap_P[INDEX] & (1 << BIT_INDEX)) != 0; \
+    }
+# include "riscv_arch.def"
+
+struct RISCV_capability_s {
+    const char *name;
+    size_t index;
+    size_t bit_offset;
+};
+
+# define RISCV_DEFINE_CAP(NAME, INDEX, BIT_INDEX) +1
+extern const struct RISCV_capability_s RISCV_capabilities[
+# include "riscv_arch.def"
+];
+
+# ifdef OPENSSL_RISCVCAP_IMPL
+#  define RISCV_DEFINE_CAP(NAME, INDEX, BIT_INDEX) \
+    { #NAME, INDEX, BIT_INDEX },
+const struct RISCV_capability_s RISCV_capabilities[] = {
+#  include "riscv_arch.def"
+};
+# endif
+
+# define RISCV_DEFINE_CAP(NAME, INDEX, BIT_INDEX) +1
+static const size_t kRISCVNumCaps =
+# include "riscv_arch.def"
+;
+
+#endif
