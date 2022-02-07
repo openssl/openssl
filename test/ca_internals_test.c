83741be778924cf89e8d03d0384352f51bc7b06a
Armin Fuerst <armin@fuerst.priv.at>
2022-02-07 18:49:28 +0100

    corrected typo in comment

--- test/ca_internals_test.c
+++ test/ca_internals_test.c
@@ -36,7 +36,7 @@ static int test_do_updatedb(void)
 
     /*
      * if the test will only work with 64bit time_t and
-     * the build only supports 32, assume the test as sucess 
+     * the build only supports 32, assume the test as success 
      */
     need64bit = (int)strtol(test_get_argument(3), NULL, 0);
     have64bit = sizeof(time_t) > sizeof(uint32_t);
