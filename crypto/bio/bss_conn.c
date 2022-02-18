4dea362c3fc59f15a4ec3cce3ed213f4f83b60fb
Henrik Holst <henrik.holst@millistream.com>
2022-02-18 16:53:16 +0100

    Changed to BIO_CTRL_SET_SEND_FLAGS

--- crypto/bio/bss_conn.c
+++ crypto/bio/bss_conn.c
@@ -559,7 +559,7 @@ static long conn_ctrl(BIO *b, int cmd, long num, void *ptr)
         BIO_clear_ktls_ctrl_msg_flag(b);
         ret = 0;
         break;
-    case BIO_CTRL_DGRAM_SET_SEND_FLAGS:
+    case BIO_CTRL_SET_SEND_FLAGS:
         b->send_flags = num;
         break;
 # endif
