5b56682df23303b13b08684fe3a8455a2dbe9f23
tangyiqun <tangyiqun@uniontech.com>
2022-03-09 10:43:49 +0800

    check return value of functions that call BIO_new()

--- fuzz/client.c
+++ fuzz/client.c
@@ -65,13 +65,21 @@ int FuzzerTestOneInput(const uint8_t *buf, size_t len)
 
     /* This only fuzzes the initial flow from the client so far. */
     ctx = SSL_CTX_new(SSLv23_method());
+    if(ctx == NULL)
+        goto end;
 
     client = SSL_new(ctx);
+    if(client == NULL)
+        goto end;
     OPENSSL_assert(SSL_set_min_proto_version(client, 0) == 1);
     OPENSSL_assert(SSL_set_cipher_list(client, "ALL:eNULL:@SECLEVEL=0") == 1);
     SSL_set_tlsext_host_name(client, "localhost");
     in = BIO_new(BIO_s_mem());
     out = BIO_new(BIO_s_mem());
+    if(in == NULL || out == NULL) {
+        BIO_printf(bio_err, "Unable to create BIO\n");
+	goto end;
+    }
     SSL_set_bio(client, in, out);
     SSL_set_connect_state(client);
     OPENSSL_assert((size_t)BIO_write(in, buf, len) == len);
@@ -84,9 +92,12 @@ int FuzzerTestOneInput(const uint8_t *buf, size_t len)
             }
         }
     }
+ end:
     SSL_free(client);
     ERR_clear_error();
     SSL_CTX_free(ctx);
+    BIO_free(in);
+    BIO_free(out);
 
     return 0;
 }
