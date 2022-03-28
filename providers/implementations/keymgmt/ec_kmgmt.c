2e0f4b48f3bda27bd3d163fe6840842a1829f0b4
Tomas Mraz <tomas@openssl.org>
2022-03-28 18:09:18 +0200

    ec_export: Other parameters are exportable with domain parameters
    Reviewed-by: Dmitry Belyavskiy <beldmit@gmail.com>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/17981)
    
    (cherry picked from commit e20af37d063514c27567c64e975fa5b3208707a9)

--- providers/implementations/keymgmt/ec_kmgmt.c
+++ providers/implementations/keymgmt/ec_kmgmt.c
@@ -470,9 +470,6 @@ int ec_export(void *keydata, int selection, OSSL_CALLBACK *param_cb,
     if ((selection & OSSL_KEYMGMT_SELECT_PRIVATE_KEY) != 0
             && (selection & OSSL_KEYMGMT_SELECT_PUBLIC_KEY) == 0)
         return 0;
-    if ((selection & OSSL_KEYMGMT_SELECT_OTHER_PARAMETERS) != 0
-            && (selection & OSSL_KEYMGMT_SELECT_KEYPAIR) == 0)
-        return 0;
 
     tmpl = OSSL_PARAM_BLD_new();
     if (tmpl == NULL)
