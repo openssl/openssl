209b89296ba5589d741c7ec740d7971ee3700b4f
Pauli <pauli@openssl.org>
2022-03-29 20:03:39 +1100

    fixup! Fix Coverity 1503096: out-of-bounds access

--- crypto/evp/evp_enc.c
+++ crypto/evp/evp_enc.c
@@ -354,8 +354,10 @@ static int evp_cipher_init_internal(EVP_CIPHER_CTX *ctx,
 
         case EVP_CIPH_CBC_MODE:
             n = EVP_CIPHER_CTX_get_iv_length(ctx);
-            if (n <= 0 || n > (int)sizeof(ctx->iv))
+            if (n <= 0 || n > (int)sizeof(ctx->iv)) {
+                ERR_raise(ERR_LIB_EVP, EVP_R_INVALID_KEY_LENGTH);
                 return 0;
+            }
             if (iv != NULL)
                 memcpy(ctx->oiv, iv, n);
             memcpy(ctx->iv, ctx->oiv, n);
@@ -366,8 +368,10 @@ static int evp_cipher_init_internal(EVP_CIPHER_CTX *ctx,
             /* Don't reuse IV for CTR mode */
             if (iv != NULL) {
                 n = EVP_CIPHER_CTX_get_iv_length(ctx);
-                if (n <= 0 || n > (int)sizeof(ctx->iv))
+                if (n <= 0 || n > (int)sizeof(ctx->iv)) {
+                    ERR_raise(ERR_LIB_EVP, EVP_R_INVALID_KEY_LENGTH);
                     return 0;
+                }
                 memcpy(ctx->iv, iv, n);
             }
             break;
