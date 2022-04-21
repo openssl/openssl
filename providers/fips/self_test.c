2dc3a4a4a57eca0d9bebd87234c7d682506188fc
Jon Spillett <jon.spillett@oracle.com>
2022-04-21 16:49:04 +1000

    Prefer GNU library initialization mechanism over platform one
    If GNU toolchain is used, use the __attribute__((constructor))
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/18147)

--- providers/fips/self_test.c
+++ providers/fips/self_test.c
@@ -103,6 +103,13 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
     }
     return TRUE;
 }
+
+#elif defined(__GNUC__)
+# undef DEP_INIT_ATTRIBUTE
+# undef DEP_FINI_ATTRIBUTE
+# define DEP_INIT_ATTRIBUTE static __attribute__((constructor))
+# define DEP_FINI_ATTRIBUTE static __attribute__((destructor))
+
 #elif defined(__sun)
 # pragma init(init)
 # pragma fini(cleanup)
@@ -124,12 +131,6 @@ void _cleanup(void)
 #elif defined(__hpux)
 # pragma init "init"
 # pragma fini "cleanup"
-
-#elif defined(__GNUC__)
-# undef DEP_INIT_ATTRIBUTE
-# undef DEP_FINI_ATTRIBUTE
-# define DEP_INIT_ATTRIBUTE static __attribute__((constructor))
-# define DEP_FINI_ATTRIBUTE static __attribute__((destructor))
 
 #elif defined(__TANDEM)
 /* Method automatically called by the NonStop OS when the DLL loads */
