bd1319e6604ec2f586b1c3f2161f5fd42a03a31b
Richard Levitte <levitte@openssl.org>
2022-04-22 11:00:36 +0200

    Don't empty the method store when flushing the query cache
    When evp_method_store_flush() flushed the query cache, it also freed
    all methods in the EVP method store, through an unfortunate call of
    ossl_method_store_flush_cache() with an argument saying that all
    methods should indeed be dropped.
    
    To undo some of the confusion, ossl_method_store_flush_cache() is
    renamed to ossl_method_store_cache_flush_all(), and limited to do
    only that.  Some if the items in the internal ALGORITHM structure are
    also renamed and commented to clarify what they are for.
    
    Fixes #18150

--- include/internal/property.h
+++ include/internal/property.h
@@ -77,7 +77,7 @@ int ossl_method_store_cache_set(OSSL_METHOD_STORE *store, OSSL_PROVIDER *prov,
                                 int (*method_up_ref)(void *),
                                 void (*method_destruct)(void *));
 
-__owur int ossl_method_store_flush_cache(OSSL_METHOD_STORE *store, int all);
+__owur int ossl_method_store_cache_flush_all(OSSL_METHOD_STORE *store);
 
 /* Merge two property queries together */
 OSSL_PROPERTY_LIST *ossl_property_merge(const OSSL_PROPERTY_LIST *a,
