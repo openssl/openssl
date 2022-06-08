8358ad79d6ce7689ef6adf7ecb651e67a11f9b1a
K1 <dongbeiouba@gmail.com>
2022-06-08 16:41:16 +0800

    Fix a mem leak in evp_pkey_export_to_provider
    If keymgmt is NULL, tmp_keymgmt is allocated and will not be freed.
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Paul Yang <kaishen.yy@antfin.com>
    Reviewed-by: Todd Short <todd.short@me.com>
    (Merged from https://github.com/openssl/openssl/pull/18499)
    
    (cherry picked from commit 115eb945acd9a27bf81c6c8923f43768f9e487a8)

--- crypto/evp/p_lib.c
+++ crypto/evp/p_lib.c
@@ -1853,7 +1853,7 @@ void *evp_pkey_export_to_provider(EVP_PKEY *pk, OSSL_LIB_CTX *libctx,
 
         if (ctx == NULL)
             goto end;
-        tmp_keymgmt = ctx->keymgmt;
+        allocated_keymgmt = tmp_keymgmt = ctx->keymgmt;
         ctx->keymgmt = NULL;
         EVP_PKEY_CTX_free(ctx);
     }
@@ -1966,8 +1966,10 @@ void *evp_pkey_export_to_provider(EVP_PKEY *pk, OSSL_LIB_CTX *libctx,
     if (keydata == NULL)
         tmp_keymgmt = NULL;
 
-    if (keymgmt != NULL)
+    if (keymgmt != NULL && tmp_keymgmt != NULL) {
         *keymgmt = tmp_keymgmt;
+        allocated_keymgmt = NULL;
+    }
 
     EVP_KEYMGMT_free(allocated_keymgmt);
     return keydata;
