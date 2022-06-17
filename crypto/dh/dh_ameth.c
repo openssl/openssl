46ac730eed88077a79f83e41cf6d8c5bc5911734
Pauli <pauli@openssl.org>
2022-06-17 11:02:36 +1000

    Coverity: fix 1506298: negative returns
    Reviewed-by: Shane Lontis <shane.lontis@oracle.com>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/18587)
    
    (cherry picked from commit be54ad88a67d2fba3b4fd51bef0fe7db0c01b99a)

--- crypto/dh/dh_ameth.c
+++ crypto/dh/dh_ameth.c
@@ -207,6 +207,11 @@ static int dh_priv_encode(PKCS8_PRIV_KEY_INFO *p8, const EVP_PKEY *pkey)
 
     ASN1_STRING_clear_free(prkey);
 
+    if (dplen <= 0) {
+        ERR_raise(ERR_LIB_DH, DH_R_BN_ERROR);
+        goto err;
+    }
+
     if (!PKCS8_pkey_set0(p8, OBJ_nid2obj(pkey->ameth->pkey_id), 0,
                          V_ASN1_SEQUENCE, params, dp, dplen)) {
         OPENSSL_clear_free(dp, dplen);
