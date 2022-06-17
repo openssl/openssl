713f6a14e2510b9de56559f2433da3179fdc9c84
Matt Caswell <matt@openssl.org>
2022-06-17 16:37:24 +0100

    Add a DTLS next epoch test
    Test that if we receive a packet from the next epoch, we can buffer it
    and still use it.
    
    Reviewed-by: Paul Dale <pauli@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/18601)
    
    (cherry picked from commit e1c153d31d4f913ebe2202a4bc20305919274d1f)

--- test/helpers/ssltestlib.h
+++ test/helpers/ssltestlib.h
@@ -49,6 +49,7 @@ void bio_s_always_retry_free(void);
 #define MEMPACKET_CTRL_GET_DROP_REC         (3 << 15)
 #define MEMPACKET_CTRL_SET_DUPLICATE_REC    (4 << 15)
 
+int mempacket_swap_epoch(BIO *bio);
 int mempacket_swap_recent(BIO *bio);
 int mempacket_test_inject(BIO *bio, const char *in, int inl, int pktnum,
                           int type);
