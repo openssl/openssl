7f77ecd2facbd9df62ed727fbd1f522376d44bd3
Matt Caswell <matt@openssl.org>
2022-06-21 14:39:48 +0100

    Prepare for 1.1.1q-dev
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    Release: yes

--- include/openssl/opensslv.h
+++ include/openssl/opensslv.h
@@ -39,8 +39,8 @@ extern "C" {
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-# define OPENSSL_VERSION_NUMBER  0x1010110fL
-# define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.1p  21 Jun 2022"
+# define OPENSSL_VERSION_NUMBER  0x10101110L
+# define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.1q-dev  xx XXX xxxx"
 
 /*-
  * The macros below are to be used for shared library (.so, .dll, ...)
