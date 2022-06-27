bf2f9e8b3117bbc2f4528bc91cf23b7ae689324c
slontis <shane.lontis@oracle.com>
2022-06-28 09:49:17 +1000

    Fix memory leak in cmp_calc_protection()
    Triggered by a  memory allocation failure.
    Detected by PR #18355
    
    Reviewed-by: Paul Dale <pauli@openssl.org>
    Reviewed-by: David von Oheimb <david.von.oheimb@siemens.com>
    (Merged from https://github.com/openssl/openssl/pull/18670)

--- crypto/cmp/cmp_protect.c
+++ crypto/cmp/cmp_protect.c
@@ -90,7 +90,7 @@ ASN1_BIT_STRING *ossl_cmp_calc_protection(const OSSL_CMP_CTX *ctx,
             goto end;
 
         if ((prot = ASN1_BIT_STRING_new()) == NULL)
-            return NULL;
+            goto end;
         /* OpenSSL defaults all bit strings to be encoded as ASN.1 NamedBitList */
         prot->flags &= ~(ASN1_STRING_FLAG_BITS_LEFT | 0x07);
         prot->flags |= ASN1_STRING_FLAG_BITS_LEFT;
