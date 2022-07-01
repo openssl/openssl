41dd20d830f1c24efa964b155874792b8bbdc461
Jiasheng Jiang <jiasheng@iscas.ac.cn>
2022-07-01 10:12:42 +0800

    fuzz/ct.c: Add check for BIO_new
    As the potential failure of the BIO_new,
    it should be better to check the return value.
    
    Signed-off-by: Jiasheng Jiang <jiasheng@iscas.ac.cn>

--- fuzz/ct.c
+++ fuzz/ct.c
@@ -32,7 +32,8 @@ int FuzzerTestOneInput(const uint8_t *buf, size_t len)
     STACK_OF(SCT) *scts = d2i_SCT_LIST(NULL, pp, len);
     if (scts != NULL) {
         BIO *bio = BIO_new(BIO_s_null());
-        SCT_LIST_print(scts, bio, 4, "\n", NULL);
+        if (bio != NULL)
+            SCT_LIST_print(scts, bio, 4, "\n", NULL);
         BIO_free(bio);
 
         if (i2d_SCT_LIST(scts, &der)) {
