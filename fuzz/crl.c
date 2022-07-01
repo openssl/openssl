8c4d849868357226a3948269b0e20fc80809427a
Jiasheng Jiang <jiasheng@iscas.ac.cn>
2022-07-01 10:08:40 +0800

    fuzz/crl.c: Add check for BIO_new
    As the potential failure of the BIO_new(),
    it shoule be better to check it.
    
    Signed-off-by: Jiasheng Jiang <jiasheng@iscas.ac.cn>

--- fuzz/crl.c
+++ fuzz/crl.c
@@ -29,7 +29,8 @@ int FuzzerTestOneInput(const uint8_t *buf, size_t len)
     X509_CRL *crl = d2i_X509_CRL(NULL, &p, len);
     if (crl != NULL) {
         BIO *bio = BIO_new(BIO_s_null());
-        X509_CRL_print(bio, crl);
+        if (bio != NULL)
+            X509_CRL_print(bio, crl);
         BIO_free(bio);
 
         i2d_X509_CRL(crl, &der);
