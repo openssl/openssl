55cb6360ff6abf880e68f53f7031ba7565ecf19e
Dmitry Belyavskiy <beldmit@gmail.com>
2022-08-01 12:33:39 +0200

    Avoid resource leaks in do_ssl3_write
    Thanks Matt for pointing on it

--- ssl/record/rec_layer_s3.c
+++ ssl/record/rec_layer_s3.c
@@ -876,7 +876,7 @@ int do_ssl3_write(SSL_CONNECTION *s, int type, const unsigned char *buf,
             eivlen = EVP_CIPHER_CTX_get_iv_length(s->enc_write_ctx);
             if (eivlen < 0) {
                 SSLfatal(s, SSL_AD_INTERNAL_ERROR, SSL_R_LIBRARY_BUG);
-                return -1;
+                goto err;
 	    }
             if (eivlen <= 1)
                 eivlen = 0;
