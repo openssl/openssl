0a5c7da05af21885cf5d776b85467461aed9b048
Pauli <pauli@openssl.org>
2022-08-16 11:10:54 +1000

    Coverity 1508534: misuse of time_t
    Avoid a problem when the lower 32 bits of time_t roll over by delaying the cast
    to integer until after the time delta has been computed.

--- ssl/statem/extensions_srvr.c
+++ ssl/statem/extensions_srvr.c
@@ -1102,7 +1102,7 @@ int tls_parse_ctos_psk(SSL_CONNECTION *s, PACKET *pkt, unsigned int context,
                 s->ext.early_data_ok = 1;
             s->ext.ticket_expected = 1;
         } else {
-            uint32_t ticket_age = 0, now, agesec, agems;
+            uint32_t ticket_age = 0, agesec, agems;
             int ret;
 
             /*
@@ -1142,8 +1142,7 @@ int tls_parse_ctos_psk(SSL_CONNECTION *s, PACKET *pkt, unsigned int context,
             }
 
             ticket_age = (uint32_t)ticket_agel;
-            now = (uint32_t)time(NULL);
-            agesec = now - (uint32_t)sess->time;
+            agesec = (uint32_t)(time(NULL) - sess->time);
             agems = agesec * (uint32_t)1000;
             ticket_age -= sess->ext.tick_age_add;
 
