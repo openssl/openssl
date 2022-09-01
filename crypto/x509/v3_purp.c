a400d55c7715a8960fb71b19d55c515e090ac372
Dr. David von Oheimb <David.von.Oheimb@siemens.com>
2022-09-01 18:08:43 +0200

    v3_purp.c: refactor to improve code commenting of ossl_x509v3_cache_extensions() and X509_check_akid()

--- crypto/x509/v3_purp.c
+++ crypto/x509/v3_purp.c
@@ -594,14 +594,24 @@ int ossl_x509v3_cache_extensions(X509 *x)
     if (x->akid == NULL && i != -1)
         x->ex_flags |= EXFLAG_INVALID;
 
-    /* Check if subject name matches issuer */
+    /* This is very similar to ossl_x509_likely_issued(x, x) == X509_V_OK */
     if (X509_NAME_cmp(X509_get_subject_name(x), X509_get_issuer_name(x)) == 0) {
-        x->ex_flags |= EXFLAG_SI; /* Cert is self-issued */
-        if (X509_check_akid(x, x->akid) == X509_V_OK /* SKID matches AKID */
-            /* .. and the signature alg matches the PUBKEY alg: */
-            && check_sig_alg_match(X509_get0_pubkey(x), x) == X509_V_OK)
-            x->ex_flags |= EXFLAG_SS; /* indicate self-signed */
-        /* This is very related to ossl_x509_likely_issued(x, x) == X509_V_OK */
+        x->ex_flags |= EXFLAG_SI; /* Certificate is self-issued: subject == issuer */
+        /*
+         * When the SKID is missing, which is rare for self-issued certs,
+         * we could afford doing the (accurate) actual self-signature check, but
+         * decided against it for efficiency reasons and according to RFC 5280,
+         * CA certs MUST have an SKID and non-root certs MUST have an AKID.
+         */
+        if (X509_check_akid(x, x->akid) == X509_V_OK
+            && check_sig_alg_match(X509_get0_pubkey(x), x) == X509_V_OK) {
+            /*
+             * Assume self-signed if the signature alg matches the pkey alg and
+             * AKID is missing or matches respective fields in the same cert
+             * Not checking if any given key usage extension allows signing.
+             */
+            x->ex_flags |= EXFLAG_SS;
+        }
     }
 
     /* Handle subject alternative names and various other extensions */
@@ -1037,6 +1047,12 @@ int ossl_x509_signing_allowed(const X509 *issuer, const X509 *subject)
     return X509_V_OK;
 }
 
+/*
+ * check if all sub-fields of the authority key identifier information akid,
+ * as far as present, match the respective subjectKeyIdentifier extension (if
+ * present in issuer), serialNumber field, and issuer fields of issuer.
+ * returns X509_V_OK also if akid is NULL because this means no restriction.
+ */
 int X509_check_akid(const X509 *issuer, const AUTHORITY_KEYID *akid)
 {
     if (akid == NULL)
