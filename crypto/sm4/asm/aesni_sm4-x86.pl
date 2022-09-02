203cc7fe937d0c28d8660609563e67e27807129b
Haoru Li <lihaoru@linux.alibaba.com>
2022-09-02 17:16:24 +0800

    Adding copyright descrypt

--- crypto/sm4/asm/aesni_sm4-x86.pl
+++ crypto/sm4/asm/aesni_sm4-x86.pl
@@ -1,3 +1,15 @@
+#! /usr/bin/env perl
+# Copyright 2022 The OpenSSL Project Authors. All Rights Reserved.
+#
+# Licensed under the Apache License 2.0 (the "License").  You may not use
+# this file except in compliance with the License.  You can obtain a copy
+# in the file LICENSE in the source distribution or at
+# https://www.openssl.org/source/license.html
+
+#
+# This module implements support for SM4 hw support on aarch64
+# Aug. 2022
+#
 
 # $output is the last argument if it looks like a file (it has an extension)
 # $flavour is the first argument if it doesn't look like a file
