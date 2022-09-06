81e7d57de90b750c219c8dacb6b8f2a71cce789e
Hugo Landau <hlandau@openssl.org>
2022-09-06 13:23:29 +0100

    QUIC Send Stream Management

--- ssl/quic/quic_ackm.c
+++ ssl/quic/quic_ackm.c
@@ -8,6 +8,7 @@
  */
 
 #include "internal/quic_ackm.h"
+#include "internal/uint_set.h"
 #include "internal/common.h"
 #include <assert.h>
 
@@ -373,45 +374,14 @@ tx_pkt_history_remove(struct tx_pkt_history_st *h, uint64_t pkt_num)
  * given PN until that PN becomes provably ACKed and we finally remove it from
  * our set (by bumping the watermark) as no longer being our concern.
  *
- * The data structure supports the following operations:
+ * The data structure used is the UINT_SET structure defined in uint_set.h,
+ * which is used as a PN set. We use the following operations of the structure:
  *
- *   Insert Range: Adds an inclusive range of packet numbers [start, end]
- *                 to the set. Equivalent to Insert for each number
- *                 in the range. (Used when we receive a new PN.)
+ *   Insert Range: Used when we receive a new PN.
  *
- *   Remove Range: Removes an inclusive range of packet numbers [start, end]
- *                 from the set. Not all of the range need already be in
- *                 the set, but any part of the range in the set is removed.
- *                 (Used when bumping the watermark.)
+ *   Remove Range: Used when bumping the watermark.
  *
- *   Query:        Is a PN in the data structure?
- *
- * The data structure can be iterated.
- *
- * For greater efficiency in tracking large numbers of contiguous PNs, we track
- * PN ranges rather than individual PNs. The data structure manages a list of PN
- * ranges [[start, end]...]. Internally this is implemented as a doubly linked
- * sorted list of range structures, which are automatically split and merged as
- * necessary.
- *
- * This data structure requires O(n) traversal of the list for insertion,
- * removal and query when we are not adding/removing ranges which are near the
- * beginning or end of the set of ranges. It is expected that the number of PN
- * ranges needed at any given time will generally be small and that most
- * operations will be close to the beginning or end of the range.
- *
- * Invariant: The data structure is always sorted in ascending order by PN.
- *
- * Invariant: No two adjacent ranges ever 'border' one another (have no
- *            numerical gap between them) as the data structure always ensures
- *            such ranges are merged.
- *
- * Invariant: No two ranges ever overlap.
- *
- * Invariant: No range [a, b] ever has a > b.
- *
- * Invariant: Since ranges are represented using inclusive bounds, no range
- *            item inside the data structure can represent a span of zero PNs.
+ *   Query:        Used to determine if a PN is in the set.
  *
  * **Possible duplicates.** A PN is considered a possible duplicate when either:
  *
@@ -435,344 +405,8 @@ tx_pkt_history_remove(struct tx_pkt_history_st *h, uint64_t pkt_num)
  * used to update the state of the RX side of the ACK manager by bumping the
  * watermark accordingly.
  */
-struct pn_set_item_st {
-    struct pn_set_item_st *prev, *next;
-    OSSL_QUIC_ACK_RANGE    range;
-};
-
-struct pn_set_st {
-    struct pn_set_item_st *head, *tail;
-
-    /* Number of ranges (not PNs) in the set. */
-    size_t                 num_ranges;
-};
-
-static void pn_set_init(struct pn_set_st *s)
-{
-    s->head = s->tail = NULL;
-    s->num_ranges = 0;
-}
-
-static void pn_set_destroy(struct pn_set_st *s)
-{
-    struct pn_set_item_st *x, *xnext;
-
-    for (x = s->head; x != NULL; x = xnext) {
-        xnext = x->next;
-        OPENSSL_free(x);
-    }
-}
-
-/* Possible merge of x, x->prev */
-static void pn_set_merge_adjacent(struct pn_set_st *s, struct pn_set_item_st *x)
-{
-    struct pn_set_item_st *xprev = x->prev;
-
-    if (xprev == NULL)
-        return;
-
-    if (x->range.start - 1 != xprev->range.end)
-        return;
-
-    x->range.start = xprev->range.start;
-    x->prev = xprev->prev;
-    if (x->prev != NULL)
-        x->prev->next = x;
-
-    if (s->head == xprev)
-        s->head = x;
-
-    OPENSSL_free(xprev);
-    --s->num_ranges;
-}
-
-/* Returns 1 if there exists a PN x which falls within both ranges a and b. */
-static int pn_range_overlaps(const OSSL_QUIC_ACK_RANGE *a,
-                             const OSSL_QUIC_ACK_RANGE *b)
-{
-    return ossl_quic_pn_min(a->end, b->end)
-        >= ossl_quic_pn_max(a->start, b->start);
-}
-
-/*
- * Insert a range into a PN set. Returns 0 on allocation failure, in which case
- * the PN set is in a valid but undefined state. Otherwise, returns 1. Ranges
- * can overlap existing ranges without limitation. If a range is a subset of
- * an existing range in the set, this is a no-op and returns 1.
- */
-static int pn_set_insert(struct pn_set_st *s, const OSSL_QUIC_ACK_RANGE *range)
-{
-    struct pn_set_item_st *x, *z, *xnext, *f, *fnext;
-    QUIC_PN start = range->start, end = range->end;
-
-    if (!ossl_assert(start <= end))
-        return 0;
-
-    if (s->head == NULL) {
-        /* Nothing in the set yet, so just add this range. */
-        x = OPENSSL_zalloc(sizeof(struct pn_set_item_st));
-        if (x == NULL)
-            return 0;
-
-        x->range.start = start;
-        x->range.end   = end;
-        s->head = s->tail = x;
-        ++s->num_ranges;
-        return 1;
-    }
-
-    if (start > s->tail->range.end) {
-        /*
-         * Range is after the latest range in the set, so append.
-         *
-         * Note: The case where the range is before the earliest range in the
-         * set is handled as a degenerate case of the final case below. See
-         * optimization note (*) below.
-         */
-        if (s->tail->range.end + 1 == start) {
-            s->tail->range.end = end;
-            return 1;
-        }
-
-        x = OPENSSL_zalloc(sizeof(struct pn_set_item_st));
-        if (x == NULL)
-            return 0;
-
-        x->range.start = start;
-        x->range.end   = end;
-        x->prev        = s->tail;
-        if (s->tail != NULL)
-            s->tail->next = x;
-        s->tail = x;
-        ++s->num_ranges;
-        return 1;
-    }
-
-    if (start <= s->head->range.start && end >= s->tail->range.end) {
-        /*
-         * New range dwarfs all ranges in our set.
-         *
-         * Free everything except the first range in the set, which we scavenge
-         * and reuse.
-         */
-        for (x = s->head->next; x != NULL; x = xnext) {
-            xnext = x->next;
-            OPENSSL_free(x);
-        }
-
-        s->head->range.start = start;
-        s->head->range.end   = end;
-        s->head->next = s->head->prev = NULL;
-        s->tail = s->head;
-        s->num_ranges = 1;
-        return 1;
-    }
-
-    /*
-     * Walk backwards since we will most often be inserting at the end. As an
-     * optimization, test the head node first and skip iterating over the
-     * entire list if we are inserting at the start. The assumption is that
-     * insertion at the start and end of the space will be the most common
-     * operations. (*)
-     */
-    z = end < s->head->range.start ? s->head : s->tail;
-
-    for (; z != NULL; z = z->prev) {
-        /* An existing range dwarfs our new range (optimisation). */
-        if (z->range.start <= start && z->range.end >= end)
-            return 1;
-
-        if (pn_range_overlaps(&z->range, range)) {
-            /*
-             * Our new range overlaps an existing range, or possibly several
-             * existing ranges.
-             */
-            struct pn_set_item_st *ovend = z;
-            OSSL_QUIC_ACK_RANGE t;
-            size_t n = 0;
-
-            t.end = ossl_quic_pn_max(end, z->range.end);
-
-            /* Get earliest overlapping range. */
-            for (; z->prev != NULL && pn_range_overlaps(&z->prev->range, range);
-                   z = z->prev);
-
-            t.start = ossl_quic_pn_min(start, z->range.start);
-
-            /* Replace sequence of nodes z..ovend with ovend only. */
-            ovend->range = t;
-            ovend->prev = z->prev;
-            if (z->prev != NULL)
-                z->prev->next = ovend;
-            if (s->head == z)
-                s->head = ovend;
-
-            /* Free now unused nodes. */
-            for (f = z; f != ovend; f = fnext, ++n) {
-                fnext = f->next;
-                OPENSSL_free(f);
-            }
-
-            s->num_ranges -= n;
-            break;
-        } else if (end < z->range.start
-                    && (z->prev == NULL || start > z->prev->range.end)) {
-            if (z->range.start == end + 1) {
-                /* We can extend the following range backwards. */
-                z->range.start = start;
-
-                /*
-                 * If this closes a gap we now need to merge
-                 * consecutive nodes.
-                 */
-                pn_set_merge_adjacent(s, z);
-            } else if (z->prev != NULL && z->prev->range.end + 1 == start) {
-                /* We can extend the preceding range forwards. */
-                z->prev->range.end = end;
-
-                /*
-                 * If this closes a gap we now need to merge
-                 * consecutive nodes.
-                 */
-                pn_set_merge_adjacent(s, z);
-            } else {
-                /*
-                 * The new interval is between intervals without overlapping or
-                 * touching them, so insert between, preserving sort.
-                 */
-                x = OPENSSL_zalloc(sizeof(struct pn_set_item_st));
-                if (x == NULL)
-                    return 0;
-
-                x->range.start = start;
-                x->range.end   = end;
-
-                x->next = z;
-                x->prev = z->prev;
-                if (x->prev != NULL)
-                    x->prev->next = x;
-                z->prev = x;
-                if (s->head == z)
-                    s->head = x;
-
-                ++s->num_ranges;
-            }
-            break;
-        }
-    }
-
-    return 1;
-}
-
-/*
- * Remove a range from the set. Returns 0 on allocation failure, in which case
- * the PN set is unchanged. Otherwise, returns 1. Ranges which are not already
- * in the set can be removed without issue. If a passed range is not in the PN
- * set at all, this is a no-op and returns 1.
- */
-static int pn_set_remove(struct pn_set_st *s, const OSSL_QUIC_ACK_RANGE *range)
-{
-    struct pn_set_item_st *z, *zprev, *y;
-    QUIC_PN start = range->start, end = range->end;
-
-    if (!ossl_assert(start <= end))
-        return 0;
-
-    /* Walk backwards since we will most often be removing at the end. */
-    for (z = s->tail; z != NULL; z = zprev) {
-        zprev = z->prev;
-
-        if (start > z->range.end)
-            /* No overlapping ranges can exist beyond this point, so stop. */
-            break;
-
-        if (start <= z->range.start && end >= z->range.end) {
-            /*
-             * The range being removed dwarfs this range, so it should be
-             * removed.
-             */
-            if (z->next != NULL)
-                z->next->prev = z->prev;
-            if (z->prev != NULL)
-                z->prev->next = z->next;
-            if (s->head == z)
-                s->head = z->next;
-            if (s->tail == z)
-                s->tail = z->prev;
-
-            OPENSSL_free(z);
-            --s->num_ranges;
-        } else if (start <= z->range.start) {
-            /*
-             * The range being removed includes start of this range, but does
-             * not cover the entire range (as this would be caught by the case
-             * above). Shorten the range.
-             */
-            assert(end < z->range.end);
-            z->range.start = end + 1;
-        } else if (end >= z->range.end) {
-            /*
-             * The range being removed includes the end of this range, but does
-             * not cover the entire range (as this would be caught by the case
-             * above). Shorten the range. We can also stop iterating.
-             */
-            assert(start > z->range.start);
-            assert(start > 0);
-            z->range.end = start - 1;
-            break;
-        } else if (start > z->range.start && end < z->range.end) {
-            /*
-             * The range being removed falls entirely in this range, so cut it
-             * into two. Cases where a zero-length range would be created are
-             * handled by the above cases.
-             */
-            y = OPENSSL_zalloc(sizeof(struct pn_set_item_st));
-            if (y == NULL)
-                return 0;
-
-            y->range.end   = z->range.end;
-            y->range.start = end + 1;
-            y->next = z->next;
-            y->prev = z;
-            if (y->next != NULL)
-                y->next->prev = y;
-
-            z->range.end = start - 1;
-            z->next = y;
-
-            if (s->tail == z)
-                s->tail = y;
-
-            ++s->num_ranges;
-            break;
-        } else {
-            /* Assert no partial overlap; all cases should be covered above. */
-            assert(!pn_range_overlaps(&z->range, range));
-        }
-    }
-
-     return 1;
-}
-
-/* Returns 1 iff the given PN is in the PN set. */
-static int pn_set_query(const struct pn_set_st *s, QUIC_PN pn)
-{
-    struct pn_set_item_st *x;
-
-    if (s->head == NULL)
-        return 0;
-
-    for (x = s->tail; x != NULL; x = x->prev)
-        if (x->range.start <= pn && x->range.end >= pn)
-            return 1;
-        else if (x->range.end < pn)
-            return 0;
-
-    return 0;
-}
-
 struct rx_pkt_history_st {
-    struct pn_set_st set;
+    UINT_SET set;
 
     /*
      * Invariant: PNs below this are not in the set.
@@ -786,13 +420,13 @@ static int rx_pkt_history_bump_watermark(struct rx_pkt_history_st *h,
 
 static void rx_pkt_history_init(struct rx_pkt_history_st *h)
 {
-    pn_set_init(&h->set);
+    ossl_uint_set_init(&h->set);
     h->watermark = 0;
 }
 
 static void rx_pkt_history_destroy(struct rx_pkt_history_st *h)
 {
-    pn_set_destroy(&h->set);
+    ossl_uint_set_destroy(&h->set);
 }
 
 /*
@@ -806,12 +440,12 @@ static void rx_pkt_history_trim_range_count(struct rx_pkt_history_st *h)
     QUIC_PN highest = QUIC_PN_INVALID;
 
     while (h->set.num_ranges > MAX_RX_ACK_RANGES) {
-        OSSL_QUIC_ACK_RANGE r = h->set.head->range;
+        UINT_RANGE r = h->set.head->range;
 
         highest = (highest == QUIC_PN_INVALID)
             ? r.end : ossl_quic_pn_max(highest, r.end);
 
-        pn_set_remove(&h->set, &r);
+        ossl_uint_set_remove(&h->set, &r);
     }
 
     /*
@@ -825,7 +459,7 @@ static void rx_pkt_history_trim_range_count(struct rx_pkt_history_st *h)
 static int rx_pkt_history_add_pn(struct rx_pkt_history_st *h,
                                  QUIC_PN pn)
 {
-    OSSL_QUIC_ACK_RANGE r;
+    UINT_RANGE r;
 
     r.start = pn;
     r.end   = pn;
@@ -833,7 +467,7 @@ static int rx_pkt_history_add_pn(struct rx_pkt_history_st *h,
     if (pn < h->watermark)
         return 1; /* consider this a success case */
 
-    if (pn_set_insert(&h->set, &r) != 1)
+    if (ossl_uint_set_insert(&h->set, &r) != 1)
         return 0;
 
     rx_pkt_history_trim_range_count(h);
@@ -843,7 +477,7 @@ static int rx_pkt_history_add_pn(struct rx_pkt_history_st *h,
 static int rx_pkt_history_bump_watermark(struct rx_pkt_history_st *h,
                                          QUIC_PN watermark)
 {
-    OSSL_QUIC_ACK_RANGE r;
+    UINT_RANGE r;
 
     if (watermark <= h->watermark)
         return 1;
@@ -851,7 +485,7 @@ static int rx_pkt_history_bump_watermark(struct rx_pkt_history_st *h,
     /* Remove existing PNs below the watermark. */
     r.start = 0;
     r.end   = watermark - 1;
-    if (pn_set_remove(&h->set, &r) != 1)
+    if (ossl_uint_set_remove(&h->set, &r) != 1)
         return 0;
 
     h->watermark = watermark;
@@ -1936,7 +1570,7 @@ static void ackm_fill_rx_ack_ranges(OSSL_ACKM *ackm, int pkt_space,
                                     OSSL_QUIC_FRAME_ACK *ack)
 {
     struct rx_pkt_history_st *h = get_rx_history(ackm, pkt_space);
-    struct pn_set_item_st *x;
+    UINT_SET_ITEM *x;
     size_t i = 0;
 
     /*
@@ -1945,8 +1579,10 @@ static void ackm_fill_rx_ack_ranges(OSSL_ACKM *ackm, int pkt_space,
      */
     for (x = h->set.tail;
          x != NULL && i < OSSL_NELEM(ackm->ack_ranges);
-         x = x->prev, ++i)
-        ackm->ack_ranges[pkt_space][i] = x->range;
+         x = x->prev, ++i) {
+        ackm->ack_ranges[pkt_space][i].start = x->range.start;
+        ackm->ack_ranges[pkt_space][i].end   = x->range.end;
+    }
 
     ack->ack_ranges     = ackm->ack_ranges[pkt_space];
     ack->num_ack_ranges = i;
@@ -1995,7 +1631,7 @@ int ossl_ackm_is_rx_pn_processable(OSSL_ACKM *ackm, QUIC_PN pn, int pkt_space)
 {
     struct rx_pkt_history_st *h = get_rx_history(ackm, pkt_space);
 
-    return pn >= h->watermark && pn_set_query(&h->set, pn) == 0;
+    return pn >= h->watermark && ossl_uint_set_query(&h->set, pn) == 0;
 }
 
 void ossl_ackm_set_loss_detection_deadline_callback(OSSL_ACKM *ackm,
