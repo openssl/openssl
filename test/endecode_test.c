c71a023bd45cb11abb87bd85d34b8fff09c677b0
Pauli <pauli@openssl.org>
2022-09-15 10:13:48 +1000

    endecode_test: fix correct FIPS provider version to skip tests

--- test/endecode_test.c
+++ test/endecode_test.c
@@ -47,7 +47,7 @@ OSSL_provider_init_fn ossl_legacy_provider_init;
 
 static int default_libctx = 1;
 static int is_fips = 0;
-static int is_fips_3_0_0 = 0;
+static int is_fips_3_0_2 = 0;
 
 static OSSL_LIB_CTX *testctx = NULL;
 static OSSL_LIB_CTX *keyctx = NULL;
@@ -175,7 +175,7 @@ static int test_encode_decode(const char *file, const int line,
                              output_type, output_structure, pass, pcipher)))
         goto end;
 
-    if ((flags & FLAG_FAIL_IF_FIPS) != 0 && is_fips && !is_fips_3_0_0) {
+    if ((flags & FLAG_FAIL_IF_FIPS) != 0 && is_fips && !is_fips_3_0_2) {
         if (TEST_false(decode_cb(file, line, (void **)&pkey2, encoded,
                                   encoded_len, output_type, output_structure,
                                   (flags & FLAG_DECODE_WITH_TYPE ? type : NULL),
@@ -1324,9 +1324,9 @@ int setup_tests(void)
             return 0;
     }
 
-    /* FIPS(3.0.0): provider imports explicit params but they won't work #17998 */
-    is_fips_3_0_0 = fips_provider_version_eq(testctx, 3, 0, 0);
-    if (is_fips_3_0_0 < 0)
+    /* FIPS(3.0.2): provider imports explicit params but they won't work #17998 */
+    is_fips_3_0_2 = provider_version_le(testctx, "fips", 3, 0, 2);
+    if (is_fips_3_0_2 < 0)
         return 0;
 
 #ifdef STATIC_LEGACY
