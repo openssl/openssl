3f2a93046f5da674c0246554dbfe6f926e54b30e
Dr. David von Oheimb <David.von.Oheimb@siemens.com>
2022-09-16 19:28:47 +0200

    fixup! CMS_decrypt*(): fix misconceptions and mem leak

--- crypto/cms/cms_env.c
+++ crypto/cms/cms_env.c
@@ -266,7 +266,7 @@ BIO *CMS_EnvelopedData_decrypt(CMS_EnvelopedData *env, BIO *detached_data,
                                      ASN1_STRING_get0_data(secret),
                                      ASN1_STRING_length(secret)) != 1)
         goto end;
-    res = CMS_decrypt(ci, secret == NULL ? pkey: NULL,
+    res = CMS_decrypt(ci, secret == NULL ? pkey : NULL,
                       secret == NULL ? cert : NULL, detached_data, bio, flags);
 
  end:
