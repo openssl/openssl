8ff66343bdf635ad574e9db4d4fca8f82c4bcc98
Todd Short <tshort@akamai.com>
2022-09-23 12:03:13 -0400

    Test TLS extension ordering
    Adding extensions is fragile, with the TLSEXT_TYPE entry needing to be
    located at TLSEXT_IDX in the array.
    
    This adds a test to ensure extensions are in the correct order.
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/19269)
    
    (cherry picked from commit ac44deaf00ad24fd18b9d74de4a23d98a2b75c8d)

--- ssl/statem/statem_local.h
+++ ssl/statem/statem_local.h
@@ -37,6 +37,11 @@
 /* Dummy message type */
 #define SSL3_MT_DUMMY   -1
 
+/* Invalid extension ID for non-supported extensions */
+#define TLSEXT_TYPE_invalid            0x10000
+#define TLSEXT_TYPE_out_of_range       0x10001
+unsigned int ossl_get_extension_type(size_t idx);
+
 extern const unsigned char hrrrandom[];
 
 /* Message processing return codes */
