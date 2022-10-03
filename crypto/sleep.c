0042d050425f0578495736a47ce8f976068a7d50
Richard Levitte <levitte@openssl.org>
2022-10-03 09:33:29 +0200

    fixup! Rename ossl_sleep() to OSSL_sleep() and make it public

--- crypto/sleep.c
+++ crypto/sleep.c
@@ -13,21 +13,25 @@
 /* system-specific variants defining OSSL_sleep() */
 #if defined(OPENSSL_SYS_UNIX) || defined(__DJGPP__)
 # include <unistd.h>
+
 void OSSL_sleep(unsigned long millis)
 {
 # ifdef OPENSSL_SYS_VXWORKS
     struct timespec ts;
+
     ts.tv_sec = (long int) (millis / 1000);
     ts.tv_nsec = (long int) (millis % 1000) * 1000000ul;
     nanosleep(&ts, NULL);
 # elif defined(__TANDEM)
 #  if !defined(_REENTRANT)
 #   include <cextdecs.h(PROCESS_DELAY_)>
+
     /* HPNS does not support usleep for non threaded apps */
     PROCESS_DELAY_(millis * 1000);
 #  elif defined(_SPT_MODEL_)
 #   include <spthread.h>
 #   include <spt_extensions.h>
+
     usleep(millis * 1000);
 #  else
     usleep(millis * 1000);
@@ -38,6 +42,7 @@ void OSSL_sleep(unsigned long millis)
 }
 #elif defined(_WIN32)
 # include <windows.h>
+
 void OSSL_sleep(unsigned long millis)
 {
     Sleep(millis);
@@ -45,9 +50,11 @@ void OSSL_sleep(unsigned long millis)
 #else
 /* Fallback to a busy wait */
 # include "internal/time.h"
+
 void OSSL_sleep(unsigned long millis)
 {
-    const OSSL_TIME finish = ossl_time_add(ossl_time_now(), ossl_ms2time(millis));
+    const OSSL_TIME finish
+        = ossl_time_add(ossl_time_now(), ossl_ms2time(millis));
 
     while (ossl_time_compare(ossl_time_now(), finish) < 0)
         /* busy wait */ ;
