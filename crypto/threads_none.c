894f2166ef2c16d8e4533e1c09e05ff31ea2f1d8
Tomas Mraz <tomas@openssl.org>
2022-10-03 15:23:05 +0200

    CRYPTO_THREAD_lock_new(): Avoid infinite recursion on allocation error
    Fixes #19334
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    Reviewed-by: Paul Dale <pauli@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/19335)

--- crypto/threads_none.c
+++ crypto/threads_none.c
@@ -21,10 +21,9 @@ CRYPTO_RWLOCK *CRYPTO_THREAD_lock_new(void)
 {
     CRYPTO_RWLOCK *lock;
 
-    if ((lock = OPENSSL_zalloc(sizeof(unsigned int))) == NULL) {
+    if ((lock = CRYPTO_zalloc(sizeof(unsigned int), NULL, 0)) == NULL)
         /* Don't set error, to avoid recursion blowup. */
         return NULL;
-    }
 
     *(unsigned int *)lock = 1;
 
