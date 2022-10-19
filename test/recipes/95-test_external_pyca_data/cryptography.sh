f531e4ae0c3f1ffbeaff4364ee3140662195adca
Paul Kehrer <paul.l.kehrer@gmail.com>
2022-10-19 09:07:32 -0500

    link the pyca tests against the correct openssl
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Paul Dale <pauli@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/19439)
    
    (cherry picked from commit 097752da99d9c27702e9e9d51609efedd3a4d0cf)

--- test/recipes/95-test_external_pyca_data/cryptography.sh
+++ test/recipes/95-test_external_pyca_data/cryptography.sh
@@ -39,22 +39,27 @@ cd $SRCTOP
 rm -rf venv-cryptography
 python -m venv venv-cryptography
 . ./venv-cryptography/bin/activate
+# Upgrade pip to always have latest
+pip install -U pip
 
 cd pyca-cryptography
 
-pip install .[test]
+echo "------------------------------------------------------------------"
+echo "Building cryptography and installing test requirements"
+echo "------------------------------------------------------------------"
+LDFLAGS="-L$O_LIB" CFLAGS="-I$O_BINC -I$O_SINC " pip install .[test]
 pip install -e vectors
 
 echo "------------------------------------------------------------------"
-echo "Building cryptography"
+echo "Print linked libraries"
 echo "------------------------------------------------------------------"
-CFLAGS="-I$O_BINC -I$O_SINC -L$O_LIB" pip install .
+ldd $(find ../venv-cryptography/lib/ -iname '*.so')
+
 
 echo "------------------------------------------------------------------"
 echo "Running tests"
 echo "------------------------------------------------------------------"
-
-CFLAGS="-I$O_BINC -I$O_SINC -L$O_LIB" pytest -n auto tests --wycheproof-root=../wycheproof
+pytest -n auto tests --wycheproof-root=../wycheproof
 
 cd ../
 deactivate
