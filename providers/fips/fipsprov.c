7b6102d5957b96a7c5968a210ef56592aa7d6468
Pauli <pauli@openssl.org>
2022-10-26 11:47:48 +1100

    Remove DES cipher from the FIPS provider
    Co-authored-by: Randall Steck <rsteck@thinqsoft.com>
    Co-authored-by: Mark J. Minnoch <mark@keypair.us>
    Co-authored-by: Steve Weymann <steve@keypair.us>

--- providers/fips/fipsprov.c
+++ providers/fips/fipsprov.c
@@ -318,10 +318,6 @@ static const OSSL_ALGORITHM_CAPABLE fips_ciphers[] = {
          ossl_cipher_capable_aes_cbc_hmac_sha256),
     ALGC(PROV_NAMES_AES_256_CBC_HMAC_SHA256, ossl_aes256cbc_hmac_sha256_functions,
          ossl_cipher_capable_aes_cbc_hmac_sha256),
-#ifndef OPENSSL_NO_DES
-    ALG(PROV_NAMES_DES_EDE3_ECB, ossl_tdes_ede3_ecb_functions),
-    ALG(PROV_NAMES_DES_EDE3_CBC, ossl_tdes_ede3_cbc_functions),
-#endif  /* OPENSSL_NO_DES */
     { { NULL, NULL, NULL }, NULL }
 };
 static OSSL_ALGORITHM exported_fips_ciphers[OSSL_NELEM(fips_ciphers)];
