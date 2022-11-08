30667f5c306dbc11ac0e6fddc7d26fd984d546ab
Bernd Edlinger <bernd.edlinger@hotmail.de>
2022-11-08 17:43:22 +0100

    Limit size of modulus for bn_mul_mont and BN_mod_exp_mont_consttime
    Otherwise the alloca can cause an exception.
    
    Issue reported by Jiayi Lin.
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Todd Short <todd.short@me.com>
    (Merged from https://github.com/openssl/openssl/pull/20005)

--- crypto/bn/bn_mont.c
+++ crypto/bn/bn_mont.c
@@ -42,7 +42,7 @@ int bn_mul_mont_fixed_top(BIGNUM *r, const BIGNUM *a, const BIGNUM *b,
     int num = mont->N.top;
 
 #if defined(OPENSSL_BN_ASM_MONT) && defined(MONT_WORD)
-    if (num > 1 && a->top == num && b->top == num) {
+    if (num > 1 && num <= BN_SOFT_LIMIT && a->top == num && b->top == num) {
         if (bn_wexpand(r, num) == NULL)
             return 0;
         if (bn_mul_mont(r->d, a->d, b->d, mont->N.d, mont->n0, num)) {
