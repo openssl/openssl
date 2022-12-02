bb26207fdf1faa13a971748b1dc2df908771843c
Peiwei Hu <jlu.hpw@foxmail.com>
2022-12-02 16:35:53 +0800

    Fix the check of BIO_set_write_buffer_size and BIO_set_read_buffer_size

--- ssl/ssl_lib.c
+++ ssl/ssl_lib.c
@@ -4816,7 +4816,7 @@ int ssl_init_wbio_buffer(SSL_CONNECTION *s)
     }
 
     bbio = BIO_new(BIO_f_buffer());
-    if (bbio == NULL || !BIO_set_read_buffer_size(bbio, 1)) {
+    if (bbio == NULL || BIO_set_read_buffer_size(bbio, 1) <= 0) {
         BIO_free(bbio);
         ERR_raise(ERR_LIB_SSL, ERR_R_BUF_LIB);
         return 0;
