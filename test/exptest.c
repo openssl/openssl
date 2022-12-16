ed8af0502167c80ba70522d88e719f874e24ebfd
Bernd Edlinger <bernd.edlinger@hotmail.de>
2022-12-16 19:30:29 +0100

    Fix a logic flaw in test_mod_exp_zero
    Due to the logic flaw, possible test failures
    in this test case might be ignored.
    
    Reviewed-by: Shane Lontis <shane.lontis@oracle.com>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/19929)
    
    (cherry picked from commit 42061268ee8f9ae0555d522870740fc91b744f4f)

--- test/exptest.c
+++ test/exptest.c
@@ -49,7 +49,7 @@ static int test_mod_exp_zero(void)
     BIGNUM *r = NULL;
     BN_ULONG one_word = 1;
     BN_CTX *ctx = BN_CTX_new();
-    int ret = 1, failed = 0;
+    int ret = 0, failed = 0;
 
     if (!TEST_ptr(m = BN_new())
         || !TEST_ptr(a = BN_new())
