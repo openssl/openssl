3b1c0c8f3cd66e80f81a9b7c9810bdada39363f2
Tomas Mraz <tomas@openssl.org>
2023-01-13 14:48:52 +0100

    pkey: Imply public check if -pubin is specified
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Paul Dale <pauli@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/20044)

--- apps/pkey.c
+++ apps/pkey.c
@@ -248,7 +248,7 @@ int pkey_main(int argc, char **argv)
             goto end;
         }
 
-        if (check)
+        if (check && !pubin)
             r = EVP_PKEY_check(ctx);
         else
             r = EVP_PKEY_public_check(ctx);
