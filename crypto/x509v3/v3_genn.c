2c6c9d439b484e1ba9830d8454a34fa4f80fdfe9
Hugo Landau <hlandau@openssl.org>
2023-01-17 17:45:42 +0000

    CVE-2023-0286: Fix GENERAL_NAME_cmp for x400Address (1.1.1)
    Reviewed-by: Paul Dale <pauli@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>

--- crypto/x509v3/v3_genn.c
+++ crypto/x509v3/v3_genn.c
@@ -98,7 +98,7 @@ int GENERAL_NAME_cmp(GENERAL_NAME *a, GENERAL_NAME *b)
         return -1;
     switch (a->type) {
     case GEN_X400:
-        result = ASN1_TYPE_cmp(a->d.x400Address, b->d.x400Address);
+        result = ASN1_STRING_cmp(a->d.x400Address, b->d.x400Address);
         break;
 
     case GEN_EDIPARTY:
