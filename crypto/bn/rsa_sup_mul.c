f683c59fe0be38b8cd2ee8b85cfe7be29d6b38d0
Daniel Krügler <daniel.kruegler@gmail.com>
2023-02-14 17:27:01 +0100

    #20280 Add missing preprocessor conditional to reenable Visual Studio 32-bit builds
    Signed-off-by: Daniel Krügler <daniel.kruegler@gmail.com>

--- crypto/bn/rsa_sup_mul.c
+++ crypto/bn/rsa_sup_mul.c
@@ -109,7 +109,7 @@ static ossl_inline void _mul_limb(limb_t *hi, limb_t *lo, limb_t a, limb_t b)
     *hi = t >> LIMB_BIT_SIZE;
     *lo = (limb_t)t;
 }
-#elif (BN_BYTES == 8) && (defined _MSC_VER)
+#elif (BN_BYTES == 8) && (defined _MSC_VER) && defined(_WIN64)
 # if defined(_M_X64)
 /*
  * on x86_64 (x64) we can use the _umul128 intrinsic to get one `mul`
