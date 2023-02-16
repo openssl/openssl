45ab74b73b71cad8078fd9d3f2b3990844018e39
hardik shah <hardik05@gmail.com>
2023-02-16 08:49:15 +0530

    changed based on reviews

--- fuzz/v3name.c
+++ fuzz/v3name.c
@@ -20,26 +20,22 @@ int FuzzerInitialize(int *argc, char ***argv)
 }
 
 int FuzzerTestOneInput(const uint8_t* data, size_t size){     
-    GENERAL_NAME *namesa = OPENSSL_malloc(size);
-    GENERAL_NAME *namesb = OPENSSL_malloc(size);
-
-    if (namesa == NULL || namesb == NULL)
-        return 0;
+    GENERAL_NAME *namesa;
+    GENERAL_NAME *namesb;
 
     const unsigned char *derp = data;
-
-        /*
-         * We create two versions of each GENERAL_NAME so that we ensure when
-         * we compare them they are always different pointers.
-         */
-        namesa = d2i_GENERAL_NAME(NULL, &derp, size);
-        derp = data;
-        namesb = d2i_GENERAL_NAME(NULL, &derp, size);        
-        GENERAL_NAME_cmp(namesa, namesb);
-        if (namesa != NULL)
-            GENERAL_NAME_free(namesa);
-        if (namesb != NULL)
-            GENERAL_NAME_free(namesb);
+    /*
+     * We create two versions of each GENERAL_NAME so that we ensure when
+     * we compare them they are always different pointers.
+     */
+    namesa = d2i_GENERAL_NAME(NULL, &derp, size);
+    derp = data;
+    namesb = d2i_GENERAL_NAME(NULL, &derp, size);        
+    GENERAL_NAME_cmp(namesa, namesb);
+    if (namesa != NULL)
+        GENERAL_NAME_free(namesa);
+    if (namesb != NULL)
+        GENERAL_NAME_free(namesb);
     return 0;
 }
 
