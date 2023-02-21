04ffefda9a7639bf2de27235ac549ef844aad2fc
Gang Chen <gang.c.chen@intel.com>
2023-02-21 14:54:46 +0800

    crypto/conf: gcc build warning fix
    Fix the gcc build warning from conf_sap.c:
    variable flags set but not used [-Wunused-but-set-variable]
    variable appname set but not used [-Wunused-but-set-variable]
    variable filename set but not used [-Wunused-but-set-variable]
    
    CLA: trivial
    
    Signed-off-by: Gang Chen <gang.c.chen@intel.com>

--- crypto/conf/conf_sap.c
+++ crypto/conf/conf_sap.c
@@ -43,16 +43,19 @@ void OPENSSL_config(const char *appname)
 int openssl_config_int(const OPENSSL_INIT_SETTINGS *settings)
 {
     int ret = 0;
+#if defined(OPENSSL_INIT_DEBUG) || !defined(OPENSSL_SYS_UEFI)
     const char *filename;
     const char *appname;
     unsigned long flags;
+#endif
 
     if (openssl_configured)
         return 1;
-
+#if defined(OPENSSL_INIT_DEBUG) || !defined(OPENSSL_SYS_UEFI)
     filename = settings ? settings->filename : NULL;
     appname = settings ? settings->appname : NULL;
     flags = settings ? settings->flags : DEFAULT_CONF_MFLAGS;
+#endif
 
 #ifdef OPENSSL_INIT_DEBUG
     fprintf(stderr, "OPENSSL_INIT: openssl_config_int(%s, %s, %lu)\n",
