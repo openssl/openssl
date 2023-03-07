843e4a183dfb1a268fbefcb40f7e93e69ef61eae
Matt Caswell <matt@openssl.org>
2023-03-07 16:52:55 +0000

    Ensure that EXFLAG_INVALID_POLICY is checked even in leaf certs
    Even though we check the leaf cert to confirm it is valid, we
    later ignored the invalid flag and did not notice that the leaf
    cert was bad.
    
    Fixes: CVE-2023-0465

--- crypto/x509/x509_vfy.c
+++ crypto/x509/x509_vfy.c
@@ -1673,13 +1673,15 @@ static int check_policy(X509_STORE_CTX *ctx)
         int i;
 
         /* Locate certificates with bad extensions and notify callback. */
-        for (i = 1; i < sk_X509_num(ctx->chain); i++) {
+        for (i = 0; i < sk_X509_num(ctx->chain); i++) {
             X509 *x = sk_X509_value(ctx->chain, i);
 
             CB_FAIL_IF((x->ex_flags & EXFLAG_INVALID_POLICY) != 0,
                        ctx, x, i, X509_V_ERR_INVALID_POLICY_EXTENSION);
         }
-        return 1;
+        /* Should not be able to get here */
+        ERR_raise(ERR_LIB_X509, ERR_R_INTERNAL_ERROR);
+        return 0;
     }
     if (ret == X509_PCY_TREE_FAILURE) {
         ctx->current_cert = NULL;
