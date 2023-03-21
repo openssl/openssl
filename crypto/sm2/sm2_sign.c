63541ba3b0232047e452ab5d90a4c33a09271cdb
slontis <shane.lontis@oracle.com>
2023-03-21 16:06:06 +1000

    Fix mem leak in ECDSA_sign().
    Similiar to the issue found in PR #20553 for DSA_sign().
    ECDSA_sign() leaked memory if the signature was NULL
    when i2d_ECDSA_SIG was called.
    
    Note that this does not affect the higher level EVP
    functions as they correctly handle NULL.
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Paul Dale <pauli@openssl.org>
    Reviewed-by: Todd Short <todd.short@me.com>
    (Merged from https://github.com/openssl/openssl/pull/20554)
    
    (cherry picked from commit 4befe81a99b89c52b749a87eece82c1cba4fab12)
    (cherry picked from commit 680b4be65eba2658c1d807dd9838ca88301bb7dd)

--- crypto/sm2/sm2_sign.c
+++ crypto/sm2/sm2_sign.c
@@ -454,7 +454,7 @@ int ossl_sm2_internal_sign(const unsigned char *dgst, int dgstlen,
         goto done;
     }
 
-    sigleni = i2d_ECDSA_SIG(s, &sig);
+    sigleni = i2d_ECDSA_SIG(s, sig != NULL ? &sig : NULL);
     if (sigleni < 0) {
        ERR_raise(ERR_LIB_SM2, ERR_R_INTERNAL_ERROR);
        goto done;
