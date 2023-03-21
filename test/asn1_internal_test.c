c6756e8ccbde7d35e3e0ec83f2cdbc2f9ebf06e3
Tomas Mraz <tomas@openssl.org>
2023-03-21 11:36:56 +0100

    OBJ_nid2obj(): Return UNDEF object instead of NULL for NID_undef
    Fixes a regression from 3.0 from the obj creation refactoring.
    
    Fixes #20555

--- test/asn1_internal_test.c
+++ test/asn1_internal_test.c
@@ -254,6 +254,16 @@ static int test_obj_create(void)
     return 1;
 }
 
+static int test_obj_nid_undef(void)
+{
+    if (!TEST_ptr(OBJ_nid2obj(NID_undef))
+        || !TEST_ptr(OBJ_nid2sn(NID_undef))
+        || !TEST_ptr(OBJ_nid2ln(NID_undef)))
+        return 0;
+
+    return 1;
+}
+
 int setup_tests(void)
 {
     ADD_TEST(test_tbl_standard);
@@ -261,5 +271,6 @@ int setup_tests(void)
     ADD_TEST(test_empty_nonoptional_content);
     ADD_TEST(test_unicode_range);
     ADD_TEST(test_obj_create);
+    ADD_TEST(test_obj_nid_undef);
     return 1;
 }
