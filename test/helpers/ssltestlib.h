7ff05cceee94f51f64987ca20e23deddb8fda2b2
Matt Caswell <matt@openssl.org>
2023-03-28 16:25:22 +0100

    Add a test for an app data record appearing before epoch change in DTLS
    We had a test for a handshake record appearing before epoch change, and
    a test for an app data record appearing before Finished - but not one for
    the app data record appearing before epoch change.
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Paul Dale <pauli@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/20637)

--- test/helpers/ssltestlib.h
+++ test/helpers/ssltestlib.h
@@ -50,7 +50,7 @@ void bio_s_always_retry_free(void);
 #define MEMPACKET_CTRL_SET_DUPLICATE_REC    (4 << 15)
 
 int mempacket_swap_epoch(BIO *bio);
-int mempacket_swap_recent(BIO *bio);
+int mempacket_move_packet(BIO *bio, int d, int s);
 int mempacket_test_inject(BIO *bio, const char *in, int inl, int pktnum,
                           int type);
 
