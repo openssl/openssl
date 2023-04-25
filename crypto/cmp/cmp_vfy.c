062dd298a1c4ea04a52a9b1b46b13718d6bcfbf8
Dr. David von Oheimb <David.von.Oheimb@siemens.com>
2023-04-25 19:26:36 +0200

    CMP client: fix checking new cert enrolled with oldcert and without private key
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Todd Short <todd.short@me.com>
    Reviewed-by: David von Oheimb <david.von.oheimb@siemens.com>
    (Merged from https://github.com/openssl/openssl/pull/20832)
    
    (cherry picked from commit e0f1ec3b2ec1b137695abc3199a62def5965351f)

--- crypto/cmp/cmp_vfy.c
+++ crypto/cmp/cmp_vfy.c
@@ -323,11 +323,11 @@ static int check_cert_path_3gpp(const OSSL_CMP_CTX *ctx,
          * verify that the newly enrolled certificate (which assumed rid ==
          * OSSL_CMP_CERTREQID) can also be validated with the same trusted store
          */
-        EVP_PKEY *pkey = OSSL_CMP_CTX_get0_newPkey(ctx, 1);
         OSSL_CMP_CERTRESPONSE *crep =
             ossl_cmp_certrepmessage_get0_certresponse(msg->body->value.ip,
                                                       OSSL_CMP_CERTREQID);
-        X509 *newcrt = ossl_cmp_certresponse_get1_cert(crep, ctx, pkey);
+        X509 *newcrt = ossl_cmp_certresponse_get1_cert(ctx, crep);
+
         /*
          * maybe better use get_cert_status() from cmp_client.c, which catches
          * errors
