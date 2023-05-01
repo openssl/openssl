8ddacec11481a37302c19f4454e23299af399f83
mlitre <martinlitre@mac.com>
2023-05-01 11:07:21 +0200

    Add negative integer check when using ASN1_BIT_STRING
    The negative integer check is done to prevent potential overflow.
    Fixes #20719.
    
    CLA: trivial
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Paul Dale <pauli@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/20862)
    
    (cherry picked from commit 1258a8e4361320cd3cfaf9ede692492ce01034c8)

--- crypto/asn1/a_bitstr.c
+++ crypto/asn1/a_bitstr.c
@@ -148,6 +148,9 @@ int ASN1_BIT_STRING_set_bit(ASN1_BIT_STRING *a, int n, int value)
     int w, v, iv;
     unsigned char *c;
 
+    if (n < 0)
+        return 0;
+
     w = n / 8;
     v = 1 << (7 - (n & 0x07));
     iv = ~v;
@@ -181,6 +184,9 @@ int ASN1_BIT_STRING_set_bit(ASN1_BIT_STRING *a, int n, int value)
 int ASN1_BIT_STRING_get_bit(const ASN1_BIT_STRING *a, int n)
 {
     int w, v;
+
+    if (n < 0)
+        return 0;
 
     w = n / 8;
     v = 1 << (7 - (n & 0x07));
