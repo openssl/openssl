8847320dd6a7ac5d5915f8aa538d720cd98f8946
Yuan, Shuai <shuai.yuan@intel.com>
2023-05-09 21:22:16 -0400

    Fixed EVP_PKEY_CTX_set_ec_paramgen_curve_nid() for SM2 in ENGINEs
    The CTRL translation is missing for SM2 key types.
    
    Fixes #20899
    Signed-off-by: Yuan, Shuai <shuai.yuan@intel.com>
    
    Reviewed-by: Shane Lontis <shane.lontis@oracle.com>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/20900)
    
    (cherry picked from commit 43d5dac9d00ac486823d949f85ee3ad650b62af8)

--- crypto/evp/ec_ctrl.c
+++ crypto/evp/ec_ctrl.c
@@ -283,7 +283,9 @@ int EVP_PKEY_CTX_get0_ecdh_kdf_ukm(EVP_PKEY_CTX *ctx, unsigned char **pukm)
  */
 int EVP_PKEY_CTX_set_ec_paramgen_curve_nid(EVP_PKEY_CTX *ctx, int nid)
 {
-    return EVP_PKEY_CTX_ctrl(ctx, EVP_PKEY_EC, EVP_PKEY_OP_TYPE_GEN,
+    int keytype = nid == EVP_PKEY_SM2 ? EVP_PKEY_SM2 : EVP_PKEY_EC;
+
+    return EVP_PKEY_CTX_ctrl(ctx, keytype, EVP_PKEY_OP_TYPE_GEN,
                              EVP_PKEY_CTRL_EC_PARAMGEN_CURVE_NID,
                              nid, NULL);
 }
