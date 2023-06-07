1b8f1c642fcbefb889d4d34355a5f23021ccdffa
Hugo Landau <hlandau@openssl.org>
2023-06-07 14:15:50 +0100

    QUIC: Multistream test fixes

--- test/quic_multistream_test.c
+++ test/quic_multistream_test.c
@@ -2467,13 +2467,20 @@ static const struct script_op script_28[] = {
     OP_C_CONNECT_WAIT       ()
     OP_C_SET_DEFAULT_STREAM_MODE(SSL_DEFAULT_STREAM_MODE_NONE)
 
-    OP_C_NEW_STREAM_UNI     (a, C_UNI_ID(0))
-    OP_C_WRITE              (a, "apple", 5)
+    OP_C_NEW_STREAM_BIDI    (a, C_BIDI_ID(0))
+    OP_C_WRITE              (a, "orange", 6)
 
-    OP_S_BIND_STREAM_ID     (a, C_UNI_ID(0))
-    OP_S_READ_EXPECT        (a, "apple", 5)
+    OP_S_BIND_STREAM_ID     (a, C_BIDI_ID(0))
+    OP_S_READ_EXPECT        (a, "orange", 6)
+
+    OP_C_NEW_STREAM_UNI     (b, C_UNI_ID(0))
+    OP_C_WRITE              (b, "apple", 5)
+
+    OP_S_BIND_STREAM_ID     (b, C_UNI_ID(0))
+    OP_S_READ_EXPECT        (b, "apple", 5)
 
     OP_SET_INJECT_WORD      (C_UNI_ID(0) + 1, OSSL_QUIC_FRAME_TYPE_RESET_STREAM)
+    OP_S_WRITE              (a, "fruit", 5)
 
     OP_C_EXPECT_CONN_CLOSE_INFO(QUIC_ERR_STREAM_STATE_ERROR,0,0)
 
@@ -2487,13 +2494,20 @@ static const struct script_op script_29[] = {
     OP_C_CONNECT_WAIT       ()
     OP_C_SET_DEFAULT_STREAM_MODE(SSL_DEFAULT_STREAM_MODE_NONE)
 
-    OP_C_NEW_STREAM_UNI     (a, C_UNI_ID(0))
-    OP_C_WRITE              (a, "apple", 5)
+    OP_C_NEW_STREAM_BIDI    (a, C_BIDI_ID(0))
+    OP_C_WRITE              (a, "orange", 6)
 
-    OP_S_BIND_STREAM_ID     (a, C_UNI_ID(0))
-    OP_S_READ_EXPECT        (a, "apple", 5)
+    OP_S_BIND_STREAM_ID     (a, C_BIDI_ID(0))
+    OP_S_READ_EXPECT        (a, "orange", 6)
+
+    OP_C_NEW_STREAM_UNI     (b, C_UNI_ID(0))
+    OP_C_WRITE              (b, "apple", 5)
+
+    OP_S_BIND_STREAM_ID     (b, C_UNI_ID(0))
+    OP_S_READ_EXPECT        (b, "apple", 5)
 
     OP_SET_INJECT_WORD      (C_UNI_ID(1) + 1, OSSL_QUIC_FRAME_TYPE_RESET_STREAM)
+    OP_S_WRITE              (a, "fruit", 5)
 
     OP_C_EXPECT_CONN_CLOSE_INFO(QUIC_ERR_STREAM_STATE_ERROR,0,0)
 
@@ -2849,7 +2863,7 @@ static int test_script(int idx)
 
 /* Dynamically generated tests. */
 static struct script_op dyn_frame_types_script[] = {
-    OP_S_SET_INJECT_PLAIN   (script_19_inject_plain)
+    OP_S_SET_INJECT_PLAIN   (script_20_inject_plain)
     OP_SET_INJECT_WORD      (0, 0) /* dynamic */
 
     OP_C_SET_ALPN           ("ossltest")
