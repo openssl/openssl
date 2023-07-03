9a64984aa99e6544453ceba4be57b4fd944f8dc4
Pauli <pauli@openssl.org>
2023-07-03 13:24:30 +1000

    fixup! fix memory allocation and reference counting issues

--- crypto/ec/ec_kmeth.c
+++ crypto/ec/ec_kmeth.c
@@ -86,16 +86,16 @@ EC_KEY *ossl_ec_key_new_method_int(OSSL_LIB_CTX *libctx, const char *propq,
     if (ret == NULL)
         return NULL;
 
+    if (!CRYPTO_NEW_REF(&ret->references, 1)) {
+        OPENSSL_free(ret);
+        return NULL;
+    }
+
     ret->libctx = libctx;
     if (propq != NULL) {
         ret->propq = OPENSSL_strdup(propq);
         if (ret->propq == NULL)
             goto err;
-    }
-
-    if (!CRYPTO_NEW_REF(&ret->references, 1)) {
-        OPENSSL_free(ret);
-        return NULL;
     }
 
     ret->meth = EC_KEY_get_default_method();
