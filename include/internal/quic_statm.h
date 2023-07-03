79a80f8b58bed854eb30a22e1643869dcc29e005
Hugo Landau <hlandau@openssl.org>
2023-07-03 15:45:25 +0100

    QUIC STATM: Move max_ack_delay tracking out of STATM
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Paul Dale <pauli@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/21349)

--- include/internal/quic_statm.h
+++ include/internal/quic_statm.h
@@ -16,13 +16,13 @@
 # ifndef OPENSSL_NO_QUIC
 
 typedef struct ossl_statm_st {
-    OSSL_TIME smoothed_rtt, latest_rtt, min_rtt, rtt_variance, max_ack_delay;
+    OSSL_TIME smoothed_rtt, latest_rtt, min_rtt, rtt_variance;
     char      have_first_sample;
 } OSSL_STATM;
 
 typedef struct ossl_rtt_info_st {
     /* As defined in RFC 9002. */
-    OSSL_TIME smoothed_rtt, latest_rtt, rtt_variance, min_rtt, max_ack_delay;
+    OSSL_TIME smoothed_rtt, latest_rtt, rtt_variance, min_rtt;
 } OSSL_RTT_INFO;
 
 int ossl_statm_init(OSSL_STATM *statm);
@@ -34,8 +34,6 @@ void ossl_statm_get_rtt_info(OSSL_STATM *statm, OSSL_RTT_INFO *rtt_info);
 void ossl_statm_update_rtt(OSSL_STATM *statm,
                            OSSL_TIME ack_delay,
                            OSSL_TIME override_latest_rtt);
-
-void ossl_statm_set_max_ack_delay(OSSL_STATM *statm, OSSL_TIME max_ack_delay);
 
 # endif
 
