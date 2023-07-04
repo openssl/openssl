658acd8144241e60e05b3658a40358619b926ffc
Jonathan M. Wilbur <jonathan@wilbur.space>
2023-07-04 18:35:34 +0000

    fix: duplicate extension registration
    Signed-off-by: Jonathan M. Wilbur <jonathan@wilbur.space>

--- crypto/x509/standard_exts.h
+++ crypto/x509/standard_exts.h
@@ -73,7 +73,6 @@ static const X509V3_EXT_METHOD *standard_exts[] = {
     &ossl_v3_issuer_sign_tool,
     &ossl_v3_tls_feature,
     &ossl_v3_ext_admission,
-    &ossl_v3_ext_admission,
     &ossl_v3_soa_identifier,
     &ossl_v3_indirect_issuer,
     &ossl_v3_no_assertion,
