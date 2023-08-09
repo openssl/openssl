f49b7f9fdc1eb8031beb6e1e273decf7e48af2c5
MscVZoltan <141750063+MscVZoltan@users.noreply.github.com>
2023-08-09 08:43:40 +0200

    Don't use C++ reserved word template for function arguments
    CLA: trivial

--- crypto/evp/keymgmt_meth.c
+++ crypto/evp/keymgmt_meth.c
@@ -346,7 +346,7 @@ void *evp_keymgmt_gen_init(const EVP_KEYMGMT *keymgmt, int selection,
 }
 
 int evp_keymgmt_gen_set_template(const EVP_KEYMGMT *keymgmt, void *genctx,
-                                 void *template)
+                                 void *templ)
 {
     /*
      * It's arguable if we actually should return success in this case, as
@@ -356,7 +356,7 @@ int evp_keymgmt_gen_set_template(const EVP_KEYMGMT *keymgmt, void *genctx,
      */
     if (keymgmt->gen_set_template == NULL)
         return 1;
-    return keymgmt->gen_set_template(genctx, template);
+    return keymgmt->gen_set_template(genctx, templ);
 }
 
 int evp_keymgmt_gen_set_params(const EVP_KEYMGMT *keymgmt, void *genctx,
