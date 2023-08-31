d5c3f4b2dba0202c589d1d733e88e392794dce41
Hugo Landau <hlandau@openssl.org>
2023-08-31 12:47:25 +0100

    Comment on internal flag
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/21905)

--- include/openssl/ssl3.h
+++ include/openssl/ssl3.h
@@ -307,6 +307,8 @@ extern "C" {
 /* Set if extended master secret extension required on renegotiation */
 # define TLS1_FLAGS_REQUIRED_EXTMS               0x1000
 
+/* 0x2000 is reserved for TLS1_FLAGS_QUIC (internal) */
+
 # define SSL3_MT_HELLO_REQUEST                   0
 # define SSL3_MT_CLIENT_HELLO                    1
 # define SSL3_MT_SERVER_HELLO                    2
