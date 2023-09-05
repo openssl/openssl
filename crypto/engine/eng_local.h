a14674076e3c1a9b5533cc2e376ffcc26b41ab58
Bernd Edlinger <bernd.edlinger@hotmail.de>
2023-09-05 16:59:45 +0200

    Fix engine cleanup error handling
    Error handling in engine_cleanup_add_first/last was
    broken and caused memory leaks.

--- crypto/engine/eng_local.h
+++ crypto/engine/eng_local.h
@@ -46,8 +46,8 @@ typedef struct st_engine_cleanup_item {
     ENGINE_CLEANUP_CB *cb;
 } ENGINE_CLEANUP_ITEM;
 DEFINE_STACK_OF(ENGINE_CLEANUP_ITEM)
-void engine_cleanup_add_first(ENGINE_CLEANUP_CB *cb);
-void engine_cleanup_add_last(ENGINE_CLEANUP_CB *cb);
+int engine_cleanup_add_first(ENGINE_CLEANUP_CB *cb);
+int engine_cleanup_add_last(ENGINE_CLEANUP_CB *cb);
 
 /* We need stacks of ENGINEs for use in eng_table.c */
 DEFINE_STACK_OF(ENGINE)
