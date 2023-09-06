a74dd1cd64860683db3035ff8bc0d12a5a483662
Bernd Edlinger <bernd.edlinger@hotmail.de>
2023-09-06 14:06:52 +0200

    Fix a possible memleak in SRP_VBASE_new
    In the error handling case the memory in
    vb->users_pwd was accidentally not released.
    
    Reviewed-by: Paul Dale <pauli@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/21981)
    
    (cherry picked from commit 68e95f7840d0d8ac4e5e03381cf9d305578dd1c7)

--- crypto/srp/srp_vfy.c
+++ crypto/srp/srp_vfy.c
@@ -276,6 +276,7 @@ SRP_VBASE *SRP_VBASE_new(char *seed_key)
         return NULL;
     if ((vb->users_pwd = sk_SRP_user_pwd_new_null()) == NULL
         || (vb->gN_cache = sk_SRP_gN_cache_new_null()) == NULL) {
+        sk_SRP_user_pwd_free(vb->users_pwd);
         OPENSSL_free(vb);
         return NULL;
     }
