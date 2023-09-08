a178fe87bedf2e0227002acf324a5e6674d8943c
Hugo Landau <hlandau@openssl.org>
2023-09-08 13:37:18 +0100

    QLOG: QUIC CHANNEL: Allow a log title to be specified

--- ssl/quic/quic_channel_local.h
+++ ssl/quic/quic_channel_local.h
@@ -437,6 +437,9 @@ struct quic_channel_st {
     /* Scratch area for use by RXDP to store decoded ACK ranges. */
     OSSL_QUIC_ACK_RANGE             *ack_range_scratch;
     size_t                          num_ack_range_scratch;
+
+    /* Title for QLOG purposes. We own this copy. */
+    char                            *quic_channel_local;
 };
 
 # endif
