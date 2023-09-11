bf80420813db85b874b7eecdf854a7afa4084d99
Matt Caswell <matt@openssl.org>
2023-09-11 15:10:22 +0100

    amend! Fix a failure in bio_dgram_test on the NonStop platform
    Fix a failure in bio_dgram_test on the NonStop platform
    
    The size of the datagram header is significantly larger that we might
    expect on NonStop (probably driven by sizeof(BIO_ADDR)). We adjust the
    size of the default buffer to take into account the header size and the
    mtu.
    
    Fixes #22013

--- test/bio_dgram_test.c
+++ test/bio_dgram_test.c
@@ -562,13 +562,10 @@ static int test_bio_dgram_pair(int idx)
      * The number of datagrams we can fit depends on the size of the default
      * write buffer size, the size of the datagram header and the size of the
      * payload data we send in each datagram. The max payload data is based on
-     * the mtu (1472). On most systems we expect the header size to be small,
-     * but on one platform (NonStop) we had a report of a very large header
-     * (2064 bytes) - probably driven by sizeof(BIO_ADDR). The default write
-     * buffer size is 17408. So in the very worst case we should be able to fit
-     * at least 4 datagrams into the write buffer.
+     * the mtu. The default write buffer size is 9 * (sizeof(header) + mtu) so
+     * we expect at least 9 maximally sized datagrams to fit in the buffer.
      */
-    if (!TEST_int_ge(i, 4))
+    if (!TEST_int_ge(i, 9))
         goto err;
 
     /* Check we read back the same data */
