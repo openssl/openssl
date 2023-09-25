d056e90ee58a039263b843e8fa330fa71b4d4835
Jerry Shih <bignose1007@gmail.com>
2023-09-25 09:14:48 +0800

    riscv: Provide vector crypto implementation of AES-GCM mode.
    To accelerate the performance of the AES-GCM mode, in this patch, we
    have the specialized multi-block implementations for AES-128-GCM,
    AES-192-GCM and AES-256-GCM.
    
    Signed-off-by: Phoebe Chen <phoebe.chen@sifive.com>
    Signed-off-by: Jerry Shih <jerry.shih@sifive.com>
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Paul Dale <pauli@openssl.org>
    Reviewed-by: Hugo Landau <hlandau@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/21923)

--- include/crypto/aes_platform.h
+++ include/crypto/aes_platform.h
@@ -475,6 +475,26 @@ void rv64i_zvkb_zvkned_ctr32_encrypt_blocks(const unsigned char *in,
                                             const void *key,
                                             const unsigned char ivec[16]);
 
+size_t rv64i_zvbb_zvkg_zvkned_aes_gcm_encrypt(const unsigned char *in,
+                                              unsigned char *out, size_t len,
+                                              const void *key,
+                                              unsigned char ivec[16], u64 *Xi);
+
+size_t rv64i_zvbb_zvkg_zvkned_aes_gcm_decrypt(const unsigned char *in,
+                                              unsigned char *out, size_t len,
+                                              const void *key,
+                                              unsigned char ivec[16], u64 *Xi);
+
+void gcm_ghash_rv64i_zvkg(u64 Xi[2], const u128 Htable[16], const u8 *inp,
+                          size_t len);
+
+#  define AES_GCM_ENC_BYTES 64
+#  define AES_GCM_DEC_BYTES 64
+#  define AES_gcm_encrypt rv64i_zvbb_zvkg_zvkned_aes_gcm_encrypt
+#  define AES_gcm_decrypt rv64i_zvbb_zvkg_zvkned_aes_gcm_decrypt
+#  define AES_GCM_ASM(ctx)    (ctx->ctr == rv64i_zvbb_zvkned_ctr32_encrypt_blocks && \
+                               ctx->gcm.funcs.ghash == gcm_ghash_rv64i_zvkg)
+
 # elif defined(OPENSSL_CPUID_OBJ) && defined(__riscv) && __riscv_xlen == 32
 /* RISC-V 32 support */
 #  include "riscv_arch.h"
