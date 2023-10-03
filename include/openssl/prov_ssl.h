1cdd859f69ff329e499d277ed68ed0136b8ed238
Frederik Wedel-Heinen <frederik.wedel-heinen@dencrypt.dk>
2023-10-03 09:39:47 +0200

    Adds initial dtls 1.3 structs and definitions
    Reviewed-by: Neil Horman <nhorman@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/22259)

--- include/openssl/prov_ssl.h
+++ include/openssl/prov_ssl.h
@@ -27,6 +27,7 @@ extern "C" {
 #define TLS1_3_VERSION 0x0304
 #define DTLS1_VERSION 0xFEFF
 #define DTLS1_2_VERSION 0xFEFD
+#define DTLS1_3_VERSION 0xFEFC
 #define DTLS1_BAD_VER 0x0100
 
 /* QUIC uses a 4 byte unsigned version number */
