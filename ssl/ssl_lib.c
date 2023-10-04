75a9bde61d7ab1a763d51d538a60e102fc4a4ee1
Frederik Wedel-Heinen <frederik.wedel-heinen@dencrypt.dk>
2023-10-04 11:37:10 +0200

    Add dtls1.3 to ssl_protocol_to_string()
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/22273)

--- ssl/ssl_lib.c
+++ ssl/ssl_lib.c
@@ -5322,6 +5322,9 @@ const char *ssl_protocol_to_string(int version)
     case DTLS1_2_VERSION:
         return "DTLSv1.2";
 
+    case DTLS1_3_VERSION:
+        return "DTLSv1.3";
+
     default:
         return "unknown";
     }
