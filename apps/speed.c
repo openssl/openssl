14b74b62925529c862acdf8ece6fd09504d76e33
Klavishnik <evg.shtanov@gmail.com>
2023-10-09 11:26:42 +0300

    Correction of code formatting

--- apps/speed.c
+++ apps/speed.c
@@ -4753,7 +4753,7 @@ static void multiblock_speed(const EVP_CIPHER *evp_cipher, int lengths_single,
                 aad[11] = (unsigned char)(len >> 8);
                 aad[12] = (unsigned char)(len);
                 pad = EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_TLS1_AAD,
-                                            EVP_AEAD_TLS1_AAD_LEN, aad);
+                                          EVP_AEAD_TLS1_AAD_LEN, aad);
                 ciph_success = EVP_Cipher(ctx, out, inp, len + pad);
             }
         }
