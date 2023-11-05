d761c55b364d112357304df29d90ae90d86b13af
Neil Horman <nhorman@openssl.org>
2023-11-05 17:51:38 -0500

    Augment rand argument parsing to allow scaling
    Instead of just accepting a number of bytes, allows openssl rand to
    accept a k|m|g suffix to scale to kbytes/mbytes/gbytes
    
    Fixes #22622

--- apps/rand.c
+++ apps/rand.c
@@ -25,7 +25,7 @@ typedef enum OPTION_choice {
 } OPTION_CHOICE;
 
 const OPTIONS rand_options[] = {
-    {OPT_HELP_STR, 1, '-', "Usage: %s [options] num\n"},
+    {OPT_HELP_STR, 1, '-', "Usage: %s [options] num[k|m|g|t]\n"},
 
     OPT_SECTION("General"),
     {"help", OPT_HELP, '-', "Display this summary"},
@@ -52,8 +52,10 @@ int rand_main(int argc, char **argv)
     BIO *out = NULL;
     char *outfile = NULL, *prog;
     OPTION_CHOICE o;
-    int format = FORMAT_BINARY, r, i, ret = 1, buflen = 131072;
+    int format = FORMAT_BINARY, r, i, ret = 1;
+    size_t buflen = (1 << 16); /* max rand chunk size is 2^16 bytes */
     long num = -1;
+    size_t scaled_num = 0;
     uint8_t *buf = NULL;
 
     prog = opt_init(argc, argv, rand_options);
@@ -95,8 +97,70 @@ int rand_main(int argc, char **argv)
     argc = opt_num_rest();
     argv = opt_rest();
     if (argc == 1) {
+        int factoridx = 0;
+        int shift = 0;
+        /*
+         * iterate over the value and check to see if there are
+         * any non-numerical chars
+         * A non digit suffix indicates we need to shift the
+         * number of requested bytes by a factor of:
+         * k = 1024^1 (1 << (10 * 1))
+         * m = 1024^2 (1 << (10 * 2))
+         * g = 1024^3 (1 << (10 * 3))
+         * t = 1024^4 (1 << (10 * 4))
+         * which can be achieved by bit-shifting the number
+         */
+        while (argv[0][factoridx]) {
+            if (!isdigit((int)(argv[0][factoridx]))) {
+                switch(argv[0][factoridx]) {
+                case 'k':
+                case 'K':
+                    shift = 10;
+                    break;
+                case 'm':
+                case 'M':
+                    shift = 20;
+                    break;
+                case 'g':
+                case 'G':
+                    shift = 30;
+                    break;
+                case 't':
+                case 'T':
+                    shift = 40;
+                    break;
+                default:
+                    BIO_printf(bio_err, "Invalid size suffix %s\n",
+                               &argv[0][factoridx]);
+                    goto opthelp;
+                }
+                break;
+            }
+            factoridx++;
+        }
+
+        if (factoridx && strlen(&argv[0][factoridx]) != 1) {
+            BIO_printf(bio_err, "Invalid size suffix %s\n", &argv[0][factoridx]);
+            goto opthelp;
+        }
+        /* Remove the suffix from the arg so that opt_long works */
+        if (shift != 0)
+            argv[0][factoridx] = '\0';
+
         if (!opt_long(argv[0], &num) || num <= 0)
             goto opthelp;
+
+        if (shift != 0) {
+            /* check for overflow */
+            if ((SIZE_MAX >> shift) < (size_t)num) {
+                BIO_printf(bio_err, "%lu bytes with provided suffix overflows\n",
+                           num);
+                goto opthelp;
+            }
+            scaled_num = num << shift;
+        } else {
+            scaled_num = num;
+        }
     } else if (!opt_check_rest_arg(NULL)) {
         goto opthelp;
     }
@@ -116,10 +180,10 @@ int rand_main(int argc, char **argv)
     }
 
     buf = app_malloc(buflen, "buffer for output file");
-    while (num > 0) {
+    while (scaled_num > 0) {
         long chunk;
 
-        chunk = (num > buflen) ? buflen : num;
+        chunk = (scaled_num > buflen) ? buflen : scaled_num;
         r = RAND_bytes(buf, chunk);
         if (r <= 0)
             goto end;
@@ -131,7 +195,7 @@ int rand_main(int argc, char **argv)
                 if (BIO_printf(out, "%02x", buf[i]) != 2)
                     goto end;
         }
-        num -= chunk;
+        scaled_num -= chunk;
     }
     if (format == FORMAT_TEXT)
         BIO_puts(out, "\n");
