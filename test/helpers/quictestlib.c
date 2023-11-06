5b659c1d6bfa67894c088b6c566bf3203600a54b
Neil Horman <nhorman@openssl.org>
2023-11-06 12:51:10 -0500

    fixup! add locking around fake_now

--- test/helpers/quictestlib.c
+++ test/helpers/quictestlib.c
@@ -74,6 +74,8 @@ struct qtest_fault {
 
 static void packet_plain_finish(void *arg);
 static void handshake_finish(void *arg);
+static OSSL_TIME qtest_get_time(void);
+static void qtest_reset_time(void);
 
 static int using_fake_time = 0;
 static OSSL_TIME fake_now;
@@ -81,13 +83,7 @@ static CRYPTO_RWLOCK *fake_now_lock = NULL;
 
 static OSSL_TIME fake_now_cb(void *arg)
 {
-    OSSL_TIME ret;
-    if (!CRYPTO_THREAD_read_lock(fake_now_lock))
-        ret.t = 0;
-    else
-        ret = fake_now;
-    CRYPTO_THREAD_unlock(fake_now_lock);
-    return ret;
+    return qtest_get_time();
 }
 
 static void noise_msg_callback(int write_p, int version, int content_type,
@@ -289,16 +285,14 @@ int qtest_create_quic_objects(OSSL_LIB_CTX *libctx, SSL_CTX *clientctx,
     if (serverctx != NULL && !TEST_true(SSL_CTX_up_ref(serverctx)))
         goto err;
     tserver_args.ctx = serverctx;
-    if (fake_now_lock == NULL)
+    if (fake_now_lock == NULL) {
         fake_now_lock = CRYPTO_THREAD_lock_new();
+        if (fake_now_lock == NULL)
+            goto err;
+    }
     if ((flags & QTEST_FLAG_FAKE_TIME) != 0) {
         using_fake_time = 1;
-        if (!CRYPTO_THREAD_write_lock(fake_now_lock))
-            goto err;
-        fake_now = ossl_time_zero();
-        CRYPTO_THREAD_unlock(fake_now_lock);
-        /* zero time can have a special meaning, bump it */
-        qtest_add_time(1);
+        qtest_reset_time();
         tserver_args.now_cb = fake_now_cb;
         (void)ossl_quic_conn_set_override_now_cb(*cssl, fake_now_cb, NULL);
     } else {
@@ -347,6 +341,27 @@ void qtest_add_time(uint64_t millis)
         return;
     fake_now = ossl_time_add(fake_now, ossl_ms2time(millis));
     CRYPTO_THREAD_unlock(fake_now_lock);
+}
+
+static OSSL_TIME qtest_get_time(void)
+{
+    OSSL_TIME ret;
+
+    if (!CRYPTO_THREAD_read_lock(fake_now_lock))
+        return ossl_time_zero();
+    ret = fake_now;
+    CRYPTO_THREAD_unlock(fake_now_lock);
+    return ret;
+}
+
+static void qtest_reset_time(void)
+{
+    if (!CRYPTO_THREAD_read_lock(fake_now_lock))
+        return;
+    fake_now = ossl_time_zero();
+    CRYPTO_THREAD_unlock(fake_now_lock);
+    /* zero time can have a special meaning, bump it */
+    qtest_add_time(1);
 }
 
 QTEST_FAULT *qtest_create_injector(QUIC_TSERVER *ts)
@@ -415,10 +430,7 @@ int qtest_wait_for_timeout(SSL *s, QUIC_TSERVER *qtserv)
     if (!SSL_get_event_timeout(s, &tv, &cinf))
         return 0;
     if (using_fake_time) {
-        if (!CRYPTO_THREAD_read_lock(fake_now_lock))
-            return 0;
-        now = fake_now;
-        CRYPTO_THREAD_unlock(fake_now_lock);
+        now = qtest_get_time();
     } else {
         now = ossl_time_now();
     }
@@ -428,10 +440,7 @@ int qtest_wait_for_timeout(SSL *s, QUIC_TSERVER *qtserv)
     if (ossl_time_is_infinite(mintimeout))
         return 0;
     if (using_fake_time) {
-        if (!CRYPTO_THREAD_write_lock(fake_now_lock))
-            return 0;
-        fake_now = ossl_time_add(now, mintimeout);
-        CRYPTO_THREAD_unlock(fake_now_lock);
+        qtest_add_time(ossl_time2ms(mintimeout));
     } else {
         OSSL_sleep(ossl_time2ms(mintimeout));
     }
