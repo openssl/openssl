044fd04cb4005dbb08c556dcf1e87fda9b7a45ba
Hugo Landau <hlandau@openssl.org>
2023-11-07 15:24:54 +0000

    QUIC SRTM: Fixes for clang
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/23022)

--- ssl/quic/quic_srtm.c
+++ ssl/quic/quic_srtm.c
@@ -478,8 +478,8 @@ static void check_mark(SRTM_ITEM *item, void *arg)
 {
     struct check_args *arg_ = arg;
     uint32_t token = arg_->token;
-    uint64_t prev_seq_num;
-    void *prev_opaque;
+    uint64_t prev_seq_num = 0;
+    void *prev_opaque = NULL;
     int have_prev = 0;
 
     assert(item != NULL);
