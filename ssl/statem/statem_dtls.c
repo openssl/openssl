ad98ea7c01721fa72a135d96e2746352a601eff0
Matt Caswell <matt@openssl.org>
2023-11-09 14:45:33 +0000

    Move freeing of an old enc_write_ctx/write_hash to dtls1_clear_sent_buffer
    When we are clearing the sent messages queue we should ensure we free any
    old enc_write_ctx/write_hash that are no longer in use. Previously this
    logic was in dtls1_hm_fragment_free() - but this can end up freeing the
    current enc_write_ctx/write_hash under certain error conditions.
    
    Fixes #22664

--- ssl/statem/statem_dtls.c
+++ ssl/statem/statem_dtls.c
@@ -95,11 +95,7 @@ void dtls1_hm_fragment_free(hm_fragment *frag)
 {
     if (!frag)
         return;
-    if (frag->msg_header.is_ccs) {
-        EVP_CIPHER_CTX_free(frag->msg_header.
-                            saved_retransmit_state.enc_write_ctx);
-        EVP_MD_CTX_free(frag->msg_header.saved_retransmit_state.write_hash);
-    }
+
     OPENSSL_free(frag->fragment);
     OPENSSL_free(frag->reassembly);
     OPENSSL_free(frag);
