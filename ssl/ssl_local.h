c8d4f8fca5182f02bc28c0b05461051f2365a18c
Frederik Wedel-Heinen <frederik.wedel-heinen@dencrypt.dk>
2023-12-04 14:27:23 +0100

    Fix description of version field of ssl connection struct
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/22937)

--- ssl/ssl_local.h
+++ ssl/ssl_local.h
@@ -1257,8 +1257,9 @@ struct ssl_connection_st {
     SSL *user_ssl;
 
     /*
-     * protocol version (one of SSL2_VERSION, SSL3_VERSION, TLS1_VERSION,
-     * DTLS1_VERSION)
+     * protocol version (one of SSL3_VERSION, TLS1_VERSION, TLS1_1_VERSION,
+     * TLS1_2_VERSION, TLS1_3_VERSION, DTLS1_VERSION, DTLS1_2_VERSION,
+     * DTLS1_3_VERSION)
      */
     int version;
     /*
