8fe0bf8fee2143487e6066fe72be15a17e3b584b
martin <rauch.martin@gmail.com>
2023-12-04 21:06:38 +0100

    Set parg to NULL SSL_get_tlsext_status_ocsp_resp

--- ssl/s3_lib.c
+++ ssl/s3_lib.c
@@ -3635,6 +3635,7 @@ long ssl3_ctrl(SSL *s, int cmd, long larg, void *parg)
         break;
 
     case SSL_CTRL_GET_TLSEXT_STATUS_REQ_OCSP_RESP:
+        *(unsigned char **)parg = NULL;
         ret = -1;
 
 #ifndef OPENSSL_NO_OCSP
@@ -3689,6 +3690,7 @@ long ssl3_ctrl(SSL *s, int cmd, long larg, void *parg)
         break;
 
     case SSL_CTRL_GET_TLSEXT_STATUS_REQ_OCSP_RESP_EX:
+        *(unsigned char **)parg = NULL;
         ret = -1;
 
 #ifndef OPENSSL_NO_OCSP
