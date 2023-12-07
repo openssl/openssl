f871e463f7a98f35ab35001edf0f03ce12acdf29
Max Ammann <max@maxammann.org>
2023-12-07 14:51:31 +0000

    Add license note

--- fuzz/provider.c
+++ fuzz/provider.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2023 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * https://www.openssl.org/source/license.html
+ * or in the file LICENSE in the source distribution.
+ */
 #include <string.h>
 #include "openssl/types.h"
 #include "openssl/crypto.h"
