d2eeaaaed2ae883fb7fe2a3a0408aa2437373fee
Vikas Verma <131862931+vikasverma4795@users.noreply.github.com>
2023-12-18 13:17:36 +0530

    CLA: trivial
    Fix uninitialized variables issue.
    character array is not initialized. It may contain garbage value.

--- crypto/evp/e_des.c
+++ crypto/evp/e_des.c
@@ -149,7 +149,7 @@ static int des_cfb1_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out,
                            const unsigned char *in, size_t inl)
 {
     size_t n, chunk = EVP_MAXCHUNK / 8;
-    unsigned char c[1], d[1];
+    unsigned char c[1] = {0}, d[1] = {0};
 
     if (inl < chunk)
         chunk = inl;
