d14d51ca024a7e3a874e8446f25e8bf8c2ae2918
Frederik Wedel-Heinen <frederik.wedel-heinen@dencrypt.dk>
2023-12-19 10:22:27 +0100

    Fix ssl_choose_server_version() logic

--- ssl/statem/statem_lib.c
+++ ssl/statem/statem_lib.c
@@ -2190,7 +2190,7 @@ int ssl_choose_server_version(SSL_CONNECTION *s, CLIENTHELLO_MSG *hello,
 
     if (suppversions->present) {
         unsigned int candidate_vers = 0;
-        unsigned int best_vers = 0;
+        unsigned int best_vers = SSL_CONNECTION_IS_DTLS(s) ? INT_MAX : 0;
         const SSL_METHOD *best_method = NULL;
         PACKET versionslist;
 
@@ -2213,11 +2213,6 @@ int ssl_choose_server_version(SSL_CONNECTION *s, CLIENTHELLO_MSG *hello,
         if (client_version <= SSL3_VERSION)
             return SSL_R_BAD_LEGACY_VERSION;
 
-        /* Initialise best_vers and best_method */
-        if (!PACKET_get_net_2(&versionslist, &best_vers)
-                || !ssl_version_supported(s, best_vers, &best_method)) {
-            return SSL_R_UNSUPPORTED_PROTOCOL;
-        }
         while (PACKET_get_net_2(&versionslist, &candidate_vers)) {
             if (ssl_version_cmp(s, candidate_vers, best_vers) <= 0)
                 continue;
@@ -2235,7 +2230,7 @@ int ssl_choose_server_version(SSL_CONNECTION *s, CLIENTHELLO_MSG *hello,
                  * This is after a HelloRetryRequest so we better check that we
                  * negotiated (D)TLSv1.3
                  */
-                if (best_vers != version1_3)
+                if (best_vers != TLS1_3_VERSION && best_vers != DTLS1_3_VERSION)
                     return SSL_R_UNSUPPORTED_PROTOCOL;
                 return 0;
             }
