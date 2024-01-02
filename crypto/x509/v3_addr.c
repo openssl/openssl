bdb3c6d6a2babb31bf145e3d0094e4b91b74c969
Neil Horman <nhorman@openssl.org>
2024-01-02 15:48:00 -0500

    Validate config options during x509 extension creation
    There are several points during x509 extension creation which rely on
    configuration options which may have been incorrectly parsed due to
    invalid settings.  Preform a value check for null in those locations to
    avoid various crashes/undefined behaviors
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/23183)
    
    (cherry picked from commit bac7e687d71b124b09ad6ad3e15be9b38c08a1ba)

--- crypto/x509/v3_addr.c
+++ crypto/x509/v3_addr.c
@@ -984,6 +984,10 @@ static void *v2i_IPAddrBlocks(const struct v3_ext_method *method,
          * the other input values.
          */
         if (safi != NULL) {
+            if (val->value == NULL) {
+                ERR_raise(ERR_LIB_X509V3, X509V3_R_MISSING_VALUE);
+                goto err;
+            }
             *safi = strtoul(val->value, &t, 0);
             t += strspn(t, " \t");
             if (*safi > 0xFF || *t++ != ':') {
