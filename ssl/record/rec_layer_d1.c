24ea63ce592359cf9bc4ca028ed90a75a5734601
Frederik Wedel-Heinen <frederik.wedel-heinen@dencrypt.dk>
2024-01-08 13:44:08 +0100

    Update epochs when changing key and cipher state for dtls 1.3
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/23229)

--- ssl/record/rec_layer_d1.c
+++ ssl/record/rec_layer_d1.c
@@ -502,7 +502,7 @@ start:
          * This may just be a stale retransmit. Also sanity check that we have
          * at least enough record bytes for a message header
          */
-        if (rr->epoch != sc->rlayer.d->r_epoch
+        if (rr->epoch != dtls1_get_epoch(sc, SSL3_CC_READ)
             || rr->length < DTLS1_HM_HEADER_LENGTH) {
             if (!ssl_release_record(sc, rr, 0))
                 return -1;
