9500d423beb79fd4d30be8b4cec676dd42557540
Frederik Wedel-Heinen <frederik.wedel-heinen@dencrypt.dk>
2024-01-17 14:29:17 +0100

    Fix sending session ids in DTLS-1.3
    DTLS 1.3 session id must not be sent by client unless
    it has a cached id. And DTLS 1.3 servers must not echo
    a session id from a client.
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/22366)

--- ssl/statem/statem_clnt.c
+++ ssl/statem/statem_clnt.c
@@ -1501,8 +1501,10 @@ __owur CON_FUNC_RETURN tls_construct_client_hello(SSL_CONNECTION *s, WPACKET *pk
             sess_id_len = 0;
     } else {
 #endif
-        if (s->new_session || s->session->ssl_version == TLS1_3_VERSION || s->session->ssl_version == DTLS1_3_VERSION) {
-            if ((s->version == TLS1_3_VERSION || s->version == DTLS1_3_VERSION)
+        if (s->new_session
+            || s->session->ssl_version == TLS1_3_VERSION
+            || s->session->ssl_version == DTLS1_3_VERSION) {
+            if (s->version == TLS1_3_VERSION
                 && (s->options & SSL_OP_ENABLE_MIDDLEBOX_COMPAT) != 0) {
                 sess_id_len = sizeof(s->tmp_session_id);
                 s->tmp_session_id_len = sess_id_len;
