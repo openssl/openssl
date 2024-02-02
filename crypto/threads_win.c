56103f01f0ff6a7045f130d899e5d43ac9ecd4ba
Neil Horman <nhorman@openssl.org>
2024-02-02 14:01:53 -0500

    fixup! Coverity issue 1591472

--- crypto/threads_win.c
+++ crypto/threads_win.c
@@ -120,6 +120,16 @@ static void free_rcu_thr_data(void *ptr)
     CRYPTO_THREAD_set_local(&rcu_thr_key, NULL);
 }
 
+#ifdef FIPS_MODULE
+static void ossl_rcu_init(void)
+{
+    int ossl_unused rc;
+    rc = CRYPTO_THREAD_init_local(&rcu_thr_key, NULL);
+    assert(rc == 1);
+    rc = ossl_init_thread_start(NULL, NULL, free_rcu_thr_data);
+    assert(rc == 1);
+}
+#else
 DEFINE_RUN_ONCE_STATIC(ossl_rcu_init)
 {
     if (!CRYPTO_THREAD_init_local(&rcu_thr_key, NULL)
@@ -128,6 +138,7 @@ DEFINE_RUN_ONCE_STATIC(ossl_rcu_init)
 
     return 1;
 }
+#endif
 
 static struct rcu_qp *allocate_new_qp_group(struct rcu_lock_st *lock,
                                             int count)
@@ -145,8 +156,13 @@ CRYPTO_RCU_LOCK *ossl_rcu_lock_new(int num_writers)
 {
     struct rcu_lock_st *new;
 
+#ifdef FIPS_MODULE
+    if (!CRYPTO_THREAD_run_once(&rcu_init_once, ossl_rcu_init))
+        return NULL;
+#else
     if (!RUN_ONCE(&rcu_init_once, ossl_rcu_init))
         return NULL;
+#endif
 
     if (num_writers < 1)
         num_writers = 1;
