0b3cd13845253011e6e0b6ada935a0b25c2e933b
Shakti Shah <shaktishah33@gmail.com>
2024-02-15 23:31:47 +0530

    Addressed review comments.

--- crypto/hpke/hpke.c
+++ crypto/hpke/hpke.c
@@ -1,6 +1,5 @@
 /*
- * Copyright 2000-2023 The OpenSSL Project Authors. All Rights Reserved.
- * Copyright Siemens AG 2018-2020
+ * Copyright 2022-2023 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
