370dba4e43bd4080526997e9652d855f6e847bd6
Tom Cosgrove <tom.cosgrove@arm.com>
2024-02-21 09:11:20 +0000

    Apply the AES-GCM unroll8 optimisation to Microsoft Azure Cobalt 100
    Performance improvements range from 18% to 32%.
    
    Change-Id: Ifb89eeac3c0625a582a25ff07cf7f9c9ec8f5ba6

--- crypto/arm_arch.h
+++ crypto/arm_arch.h
@@ -103,6 +103,7 @@ extern unsigned int OPENSSL_armv8_rsa_neonized;
 # define ARM_CPU_IMP_ARM           0x41
 # define HISI_CPU_IMP              0x48
 # define ARM_CPU_IMP_APPLE         0x61
+# define ARM_CPU_IMP_MICROSOFT     0x6D
 
 # define ARM_CPU_PART_CORTEX_A72   0xD08
 # define ARM_CPU_PART_N1           0xD0C
@@ -123,6 +124,8 @@ extern unsigned int OPENSSL_armv8_rsa_neonized;
 # define APPLE_CPU_PART_M2_AVALANCHE_PRO    0x035
 # define APPLE_CPU_PART_M2_BLIZZARD_MAX     0x038
 # define APPLE_CPU_PART_M2_AVALANCHE_MAX    0x039
+
+# define MICROSOFT_CPU_PART_COBALT_100      0xD49
 
 # define MIDR_PARTNUM_SHIFT       4
 # define MIDR_PARTNUM_MASK        (0xfffU << MIDR_PARTNUM_SHIFT)
