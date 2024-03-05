b5e076bee3c0445c108a6a35f077083ee42f9d80
Neil Horman <nhorman@openssl.org>
2024-03-05 10:56:35 -0500

    Gate setting of ipi_spec_dst on not building for freebsd
    some variants of FreeBSD (notably Dells OneFS) implement IP_PKTINFO
    partially, and as such the build breaks for those variants.
    specifically, it supports IP_PKTINFO, but the in_pktinfo struct has no
    defined ipi_spec_dst field.  Work around this by gating the setting of
    that variable on not building for FreeBSD
    
    Fixes #23739
    
    Reviewed-by: Hugo Landau <hlandau@openssl.org>
    Reviewed-by: Tim Hudson <tjh@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/23753)

--- crypto/bio/bss_dgram.c
+++ crypto/bio/bss_dgram.c
@@ -1174,7 +1174,7 @@ static int pack_local(BIO *b, MSGHDR_TYPE *mh, const BIO_ADDR *local) {
         cmsg->cmsg_type  = IP_PKTINFO;
 
         info = (struct in_pktinfo *)BIO_CMSG_DATA(cmsg);
-#   if !defined(OPENSSL_SYS_WINDOWS) && !defined(OPENSSL_SYS_CYGWIN)
+#   if !defined(OPENSSL_SYS_WINDOWS) && !defined(OPENSSL_SYS_CYGWIN) && !defined(__FreeBSD__)
         info->ipi_spec_dst      = local->s_in.sin_addr;
 #   endif
         info->ipi_addr.s_addr   = 0;
