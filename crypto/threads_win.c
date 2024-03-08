c31bd9e70e2440212447ea5dc70f06e0a0189647
Neil Horman <nhorman@openssl.org>
2024-03-08 11:58:07 -0500

    Add CRYPTO_atomic_store api
    Generally we can get away with just using CRYPTO_atomic_load to do
    stores by reversing the source and target variables, but doing so
    creates a problem for the thread sanitizer as CRYPTO_atomic_load hard
    codes an __ATOMIC_ACQUIRE constraint, which confuses tsan into thinking
    that loads and stores aren't properly ordered, leading to RAW/WAR
    hazzards getting reported.  Instead create a CRYPTO_atomic_store api
    that is identical to the load variant, save for the fact that the value
    is a unit64_t rather than a pointer that gets stored using an
    __ATOMIC_RELEASE constraint, satisfying tsan.

--- crypto/threads_win.c
+++ crypto/threads_win.c
@@ -608,6 +608,22 @@ int CRYPTO_atomic_load(uint64_t *val, uint64_t *ret, CRYPTO_RWLOCK *lock)
 #endif
 }
 
+int CRYPTO_atomic_store(uint64_t *dst, uint64_t val, CRYPTO_RWLOCK *lock)
+{
+#if (defined(NO_INTERLOCKEDOR64))
+    if (lock == NULL || !CRYPTO_THREAD_read_lock(lock))
+        return 0;
+    *dst = val;
+    if (!CRYPTO_THREAD_unlock(lock))
+        return 0;
+
+    return 1;
+#else
+    InterlockedExchange64(dst, val);
+    return 1;
+#endif
+}
+
 int CRYPTO_atomic_load_int(int *val, int *ret, CRYPTO_RWLOCK *lock)
 {
 #if (defined(NO_INTERLOCKEDOR64))
