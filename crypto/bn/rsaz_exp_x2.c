579a06cea3685e7f739cdd7e8cd3c3efa1c0d7b7
Adrien Zinger <zinger.ad@gmail.com>
2024-03-20 09:39:16 +0100

    Fix GCC compilation -Waggressive-loop-optimizations
    GCC 13.1.0 were reporting a compilation warning with -O2/3 and
    -Waggressive-loop-optimizations. GCC is raising an undefined behavior in the
    while loop. Replace the while loop with a memset call at the top of the
    function.
    
    Fixes #21088
    
    CLA: trivial
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Bernd Edlinger <bernd.edlinger@hotmail.de>
    Reviewed-by: Neil Horman <nhorman@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/23898)
    
    (cherry picked from commit c45ca0656f8d1fe43b8cf444c88d295a063341ca)

--- crypto/bn/rsaz_exp_x2.c
+++ crypto/bn/rsaz_exp_x2.c
@@ -576,11 +576,7 @@ static void to_words52(BN_ULONG *out, int out_len,
         out_len--;
     }
 
-    while (out_len > 0) {
-        *out = 0;
-        out_len--;
-        out++;
-    }
+    memset(out, 0, out_len * sizeof(BN_ULONG));
 }
 
 static ossl_inline void put_digit(uint8_t *out, int out_len, uint64_t digit)
