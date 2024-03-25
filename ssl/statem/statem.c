1409fbeb8e3dee7430d31a738d49c8c84cecd33d
Frederik Wedel-Heinen <frederik.wedel-heinen@dencrypt.dk>
2024-03-25 22:43:06 +0100

    Do DTLS13 and TLS13 connection version check in one macro
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/22366)

--- ssl/statem/statem.c
+++ ssl/statem/statem.c
@@ -401,7 +401,7 @@ static int state_machine(SSL_CONNECTION *s, int server)
 
         s->server = server;
         if (cb != NULL) {
-            if (SSL_IS_FIRST_HANDSHAKE(s) || !(SSL_CONNECTION_IS_TLS13(s) || SSL_CONNECTION_IS_DTLS13(s)))
+            if (SSL_IS_FIRST_HANDSHAKE(s) || !SSL_CONNECTION_IS_VERSION13(s))
                 cb(ussl, SSL_CB_HANDSHAKE_START, 1);
         }
 
