4e3c1e6206251c59855362d6d2edab4621c31dec
Neil Horman <nhorman@openssl.org>
2024-04-05 09:06:10 -0400

    Fix up path generation to use OPENSSL_MODULES
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/24025)

--- crypto/provider_core.c
+++ crypto/provider_core.c
@@ -573,16 +573,17 @@ OSSL_PROVIDER *ossl_provider_new(OSSL_LIB_CTX *libctx, const char *name,
 
     /* provider_new() generates an error, so no need here */
     prov = provider_new(name, template.init, template.parameters);
-    if (!ossl_provider_set_module_path(prov, template.path)) {
-        ossl_provider_free(prov);
-        return NULL;
-    }
 
     if (params != NULL) /* We copied the parameters, let's free them */
         sk_INFOPAIR_pop_free(template.parameters, infopair_free);
 
     if (prov == NULL)
         return NULL;
+
+    if (!ossl_provider_set_module_path(prov, template.path)) {
+        ossl_provider_free(prov);
+        return NULL;
+    }
 
     prov->libctx = libctx;
 #ifndef FIPS_MODULE
