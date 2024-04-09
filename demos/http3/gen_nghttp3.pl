7b46bac5d6956621c29b004c3e3600956ed37370
Neil Horman <nhorman@openssl.org>
2024-04-09 05:13:44 -0400

    add http3 demo to build
    http3 need a bit of extra work, as the nghttp3 library has to be built
    to link with the demo

---
+++ demos/http3/gen_nghttp3.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+#
+
+use File::Copy;
+use File::Path;
+use strict;
+use warnings;
+
+open STDOUT, '>&STDERR';
+
+chdir "demos/http3";
+rmtree("./nghttp3");
+system("git clone https://github.com/ngtcp2/nghttp3.git");
+
+chdir "nghttp3";
+mkdir "build";
+system("git submodule init");
+system("git submodule update");
+system("cmake -S . -B build");
+system("cmake --build build");
+
+my $libs="./build/lib/libnghttp*";
+
+for my $file (glob $libs) {
+    copy($file, "..");
+}
+
+chdir "../";
+
+exit(0);
