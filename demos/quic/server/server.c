1fe0af7076551fd5a6c1d2c0def59578d20743d7
Hugo Landau <hlandau@openssl.org>
2024-04-09 12:27:58 +0100

    Copyright header
    Reviewed-by: Neil Horman <nhorman@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/24037)

--- demos/quic/server/server.c
+++ demos/quic/server/server.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2024 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
 #include <openssl/err.h>
 #include <openssl/ssl.h>
 #include <openssl/quic.h>
