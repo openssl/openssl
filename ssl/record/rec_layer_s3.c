d146349171101dec3a876c13eb7a6dea32ba62ba
Matt Caswell <matt@openssl.org>
2024-04-24 11:33:41 +0100

    Set rlayer.packet to NULL after we've finished using it
    In order to ensure we do not have a UAF we reset the rlayer.packet pointer
    to NULL after we free it.
    
    CVE-2024-4741
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Neil Horman <nhorman@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/24395)

--- ssl/record/rec_layer_s3.c
+++ ssl/record/rec_layer_s3.c
@@ -230,6 +230,12 @@ int ssl3_read_n(SSL *s, size_t n, size_t max, int extend, int clearold,
         /* ... now we can act as if 'extend' was set */
     }
 
+    if (!ossl_assert(s->rlayer.packet != NULL)) {
+        /* does not happen */
+        SSLfatal(s, SSL_AD_INTERNAL_ERROR, ERR_R_INTERNAL_ERROR);
+        return -1;
+    }
+
     len = s->rlayer.packet_length;
     pkt = rb->buf + align;
     /*
