f42ecd469eda3174956c9bf5974c1022791dd3e8
Lukas Gerlach <s8lugerl@stud.uni-saarland.de>
2024-05-05 14:27:26 +0300

    include/internal/constant_time.h: add value_barrier_bn()
    A build breakage has been introduced on openssl-3.0 by commits
    38d6c2d5fd6c "Add value_barrier in MOD_EXP_CTIME_COPY_FROM_PREBUF",
    0c306f0ad0a8 "Add value_barrier in BN_consttime_swap", and a50aa72aba6c
    "Apply suggestion from @andrewkdinh" due to missing value_barrier_bn(),
    originally introduced in commit aaa1bda7187c "Optimizated calculation
    of shared power of 2 in bn_gcd".  Cherry-pick the part of the commit
    that contains the function definition.
    
    Fixes: 38d6c2d5fd6c "Add value_barrier in MOD_EXP_CTIME_COPY_FROM_PREBUF"
    Fixes: 0c306f0ad0a8 "Add value_barrier in BN_consttime_swap"
    Fixes: a50aa72aba6c "Apply suggestion from @andrewkdinh"
    References: aaa1bda7187c "Optimizated calculation of shared power of 2 in bn_gcd"
    Signed-off-by: Eugene Syromiatnikov <esyr@openssl.org>

--- include/internal/constant_time.h
+++ include/internal/constant_time.h
@@ -139,6 +139,17 @@ static ossl_inline uint64_t constant_time_lt_64(uint64_t a, uint64_t b)
 }
 
 #ifdef BN_ULONG
+static ossl_inline BN_ULONG value_barrier_bn(BN_ULONG a)
+{
+#if !defined(OPENSSL_NO_ASM) && defined(__GNUC__)
+    BN_ULONG r;
+    __asm__("" : "=r"(r) : "0"(a));
+#else
+    volatile BN_ULONG r = a;
+#endif
+    return r;
+}
+
 static ossl_inline BN_ULONG constant_time_msb_bn(BN_ULONG a)
 {
     return 0 - (a >> (sizeof(a) * 8 - 1));
