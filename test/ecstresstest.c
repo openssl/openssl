fdc508b54def312ba53f091cb7f17d7536d61957
Tomas Mraz <tomas@openssl.org>
2024-06-04 16:25:21 +0200

    Copyright year updates
    Reviewed-by: Neil Horman <nhorman@openssl.org>
    Release: yes

--- test/ecstresstest.c
+++ test/ecstresstest.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2021 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2017-2024 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License");
  * you may not use this file except in compliance with the License.
