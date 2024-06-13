13f94b22e486a7825017dc3871946042e973612c
MrRurikov <96385824+MrRurikov@users.noreply.github.com>
2024-06-13 13:23:31 +0300

    Append check for funcrion ossl_quic_rxfc_on_retire()
    Return value of function 'ossl_quic_rxfc_on_retire', called at quic_stream_map.c:760, is not checked, but it is usually checked for this function

--- ssl/quic/quic_stream_map.c
+++ ssl/quic/quic_stream_map.c
@@ -757,7 +757,8 @@ void ossl_quic_stream_map_remove_from_accept_queue(QUIC_STREAM_MAP *qsm,
     --qsm->num_accept;
 
     if ((max_streams_rxfc = qsm_get_max_streams_rxfc(qsm, s)) != NULL)
-        ossl_quic_rxfc_on_retire(max_streams_rxfc, 1, rtt);
+        if (!ossl_quic_rxfc_on_retire(max_streams_rxfc, 1, rtt)
+          return;
 }
 
 size_t ossl_quic_stream_map_get_accept_queue_len(QUIC_STREAM_MAP *qsm)
