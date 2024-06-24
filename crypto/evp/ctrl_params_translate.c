55c1458303c0fef88e4b2b35a090e9145f3e07eb
Tomas Mraz <tomas@openssl.org>
2024-06-24 11:25:12 +0200

    evp_pkey_ctx_setget_params_to_ctrl(): Always properly set ctx.action_type
    Fixes #24698
    
    Some applicable translations are bidirectional so they have
    NONE action_type. However we need to set the real action_type
    in the ctx.
    
    Reviewed-by: Neil Horman <nhorman@openssl.org>
    Reviewed-by: Paul Dale <ppzgs1@gmail.com>
    (Merged from https://github.com/openssl/openssl/pull/24709)

--- crypto/evp/ctrl_params_translate.c
+++ crypto/evp/ctrl_params_translate.c
@@ -2845,7 +2845,7 @@ static int evp_pkey_ctx_setget_params_to_ctrl(EVP_PKEY_CTX *pctx,
         fixup_args_fn *fixup = default_fixup_args;
         int ret;
 
-        tmpl.action_type = action_type;
+        ctx.action_type = tmpl.action_type = action_type;
         tmpl.keytype1 = tmpl.keytype2 = keytype;
         tmpl.optype = optype;
         tmpl.param_key = params->key;
@@ -2854,7 +2854,6 @@ static int evp_pkey_ctx_setget_params_to_ctrl(EVP_PKEY_CTX *pctx,
         if (translation != NULL) {
             if (translation->fixup_args != NULL)
                 fixup = translation->fixup_args;
-            ctx.action_type = translation->action_type;
             ctx.ctrl_cmd = translation->ctrl_num;
         }
         ctx.pctx = pctx;
