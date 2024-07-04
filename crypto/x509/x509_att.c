782f6375718884383980afec0c774b062360f9da
Dr. David von Oheimb <dev@ddvo.net>
2024-07-04 09:29:14 +0200

    constify ossl_x509at_add1_attr()

--- crypto/x509/x509_att.c
+++ crypto/x509/x509_att.c
@@ -80,7 +80,7 @@ X509_ATTRIBUTE *X509at_delete_attr(STACK_OF(X509_ATTRIBUTE) *x, int loc)
 }
 
 STACK_OF(X509_ATTRIBUTE) *ossl_x509at_add1_attr(STACK_OF(X509_ATTRIBUTE) **x,
-                                                X509_ATTRIBUTE *attr)
+                                                const X509_ATTRIBUTE *attr)
 {
     X509_ATTRIBUTE *new_attr = NULL;
     STACK_OF(X509_ATTRIBUTE) *sk = NULL;
