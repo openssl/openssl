a804648590252d9cd9c9c52c8a306450b2b65c27
Jonathan M. Wilbur <jonathan@wilbur.space>
2024-07-10 22:31:37 +0000

    doc: the basicAttConstraints X.509v3 extension

--- crypto/x509/v3_bacons.c
+++ crypto/x509/v3_bacons.c
@@ -12,6 +12,7 @@
 #include <openssl/conf.h>
 #include <openssl/x509v3.h>
 #include "x509_local.h"
+#include "ext_dat.h"
 
 static STACK_OF(CONF_VALUE) *i2v_OSSL_BASIC_ATTR_CONSTRAINTS(
     X509V3_EXT_METHOD *method,
