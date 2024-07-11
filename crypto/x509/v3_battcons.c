00804826f75f170383e296cfb8eeb151666e489b
Jonathan M. Wilbur <jonathan@wilbur.space>
2024-07-11 13:02:45 +0000

    fixup! feat: support the basicAttConstraints X.509v3 extension

--- crypto/x509/v3_battcons.c
+++ crypto/x509/v3_battcons.c
@@ -16,7 +16,7 @@
 
 static STACK_OF(CONF_VALUE) *i2v_OSSL_BASIC_ATTR_CONSTRAINTS(
     X509V3_EXT_METHOD *method,
-    OSSL_BASIC_ATTR_CONSTRAINTS *bcons,
+    OSSL_BASIC_ATTR_CONSTRAINTS *battcons,
     STACK_OF(CONF_VALUE)
     *extlist);
 
@@ -25,7 +25,7 @@ static OSSL_BASIC_ATTR_CONSTRAINTS *v2i_OSSL_BASIC_ATTR_CONSTRAINTS(
     X509V3_CTX *ctx,
     STACK_OF(CONF_VALUE) *values);
 
-const X509V3_EXT_METHOD ossl_v3_bacons = {
+const X509V3_EXT_METHOD ossl_v3_battcons = {
     NID_basic_att_constraints, 0,
     ASN1_ITEM_ref(OSSL_BASIC_ATTR_CONSTRAINTS),
     0, 0, 0, 0,
@@ -45,12 +45,12 @@ IMPLEMENT_ASN1_FUNCTIONS(OSSL_BASIC_ATTR_CONSTRAINTS)
 
 static STACK_OF(CONF_VALUE) *i2v_OSSL_BASIC_ATTR_CONSTRAINTS(
     X509V3_EXT_METHOD *method,
-    OSSL_BASIC_ATTR_CONSTRAINTS *bcons,
+    OSSL_BASIC_ATTR_CONSTRAINTS *battcons,
     STACK_OF(CONF_VALUE)
     *extlist)
 {
-    X509V3_add_value_bool("authority", bcons->authority, &extlist);
-    X509V3_add_value_int("pathlen", bcons->pathlen, &extlist);
+    X509V3_add_value_bool("authority", battcons->authority, &extlist);
+    X509V3_add_value_int("pathlen", battcons->pathlen, &extlist);
     return extlist;
 }
 
@@ -59,21 +59,21 @@ static OSSL_BASIC_ATTR_CONSTRAINTS *v2i_OSSL_BASIC_ATTR_CONSTRAINTS(
     X509V3_CTX *ctx,
     STACK_OF(CONF_VALUE) *values)
 {
-    OSSL_BASIC_ATTR_CONSTRAINTS *bcons = NULL;
+    OSSL_BASIC_ATTR_CONSTRAINTS *battcons = NULL;
     CONF_VALUE *val;
     int i;
 
-    if ((bcons = OSSL_BASIC_ATTR_CONSTRAINTS_new()) == NULL) {
+    if ((battcons = OSSL_BASIC_ATTR_CONSTRAINTS_new()) == NULL) {
         ERR_raise(ERR_LIB_X509V3, ERR_R_ASN1_LIB);
         return NULL;
     }
     for (i = 0; i < sk_CONF_VALUE_num(values); i++) {
         val = sk_CONF_VALUE_value(values, i);
         if (strcmp(val->name, "authority") == 0) {
-            if (!X509V3_get_value_bool(val, &bcons->authority))
+            if (!X509V3_get_value_bool(val, &battcons->authority))
                 goto err;
         } else if (strcmp(val->name, "pathlen") == 0) {
-            if (!X509V3_get_value_int(val, &bcons->pathlen))
+            if (!X509V3_get_value_int(val, &battcons->pathlen))
                 goto err;
         } else {
             ERR_raise(ERR_LIB_X509V3, X509V3_R_INVALID_NAME);
@@ -81,8 +81,8 @@ static OSSL_BASIC_ATTR_CONSTRAINTS *v2i_OSSL_BASIC_ATTR_CONSTRAINTS(
             goto err;
         }
     }
-    return bcons;
+    return battcons;
  err:
-    OSSL_BASIC_ATTR_CONSTRAINTS_free(bcons);
+    OSSL_BASIC_ATTR_CONSTRAINTS_free(battcons);
     return NULL;
 }
