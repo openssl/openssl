e8f1dac3234d738326ab3a8b5f6d3af01ef5d3e4
Pauli <ppzgs1@gmail.com>
2024-07-17 10:35:37 +1000

    fipsinstall: add no_short_mac option

--- util/mk-fipsmodule-cnf.pl
+++ util/mk-fipsmodule-cnf.pl
@@ -13,6 +13,7 @@ use Getopt::Long;
 my $conditional_errors = 1;
 my $security_checks = 1;
 my $ems_check = 1;
+my $no_short_mac = 1;
 my $drgb_no_trunc_dgst = 1;
 my $kdf_digest_check = 1;
 
@@ -47,6 +48,7 @@ activate = $activate
 conditional-errors = $conditional_errors
 security-checks = $security_checks
 tls1-prf-ems-check = $ems_check
+no-short-mac = $no_short_mac
 drbg-no-trunc-md = $drgb_no_trunc_dgst
 module-mac = $module_mac
 hkdf-digest-check = $kdf_digest_check
