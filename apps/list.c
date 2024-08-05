f4222b56884697ab96f2528ac3e9d000d109ce66
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2024-08-05 07:17:33 +0200

    optimize strlen test

--- apps/list.c
+++ apps/list.c
@@ -779,7 +779,7 @@ static void list_tls_signatures(void)
     int tls_sigalg_listed = 0;
     char *builtin_sigalgs = SSL_get1_builtin_sigalgs(app_get0_libctx());
 
-    if (builtin_sigalgs != NULL && strlen(builtin_sigalgs) > 0) {
+    if (builtin_sigalgs != NULL && builtin_sigalgs[0] != 0) {
         BIO_printf(bio_out, "%s", builtin_sigalgs);
         OPENSSL_free(builtin_sigalgs);
         tls_sigalg_listed = 1;
