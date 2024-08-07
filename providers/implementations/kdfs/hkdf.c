28585b7eeee3e8a44e2d47f05abe1a9b86ee19cd
slontis <shane.lontis@oracle.com>
2024-08-07 11:56:42 +1000

    fixup! Add FIPS KMAC key check

--- providers/implementations/kdfs/hkdf.c
+++ providers/implementations/kdfs/hkdf.c
@@ -521,7 +521,7 @@ static int HKDF_Extract(OSSL_LIB_CTX *libctx, const EVP_MD *evp_md,
     /* calc: PRK = HMAC-Hash(salt, IKM) */
     return
         EVP_Q_mac(libctx, "HMAC", NULL, EVP_MD_get0_name(evp_md), NULL, salt,
-                   salt_len, ikm, ikm_len, prk, EVP_MD_get_size(evp_md), NULL)
+                  salt_len, ikm, ikm_len, prk, EVP_MD_get_size(evp_md), NULL)
         != NULL;
 }
 
