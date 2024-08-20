254693280d1a93f5c1ab81f0f8d171b980ac0664
Jonathan M. Wilbur <jonathan@wilbur.space>
2024-08-20 23:24:01 +0000

    feat: add support for issuedOnBehalfOf X.509v3 extension
    Reviewed-by: Neil Horman <nhorman@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/25241)

--- crypto/x509/standard_exts.h
+++ crypto/x509/standard_exts.h
@@ -84,6 +84,7 @@ static const X509V3_EXT_METHOD *standard_exts[] = {
     &ossl_v3_acc_priv_policies,
     &ossl_v3_indirect_issuer,
     &ossl_v3_no_assertion,
+    &ossl_v3_issued_on_behalf_of,
     &ossl_v3_single_use,
     &ossl_v3_group_ac,
     &ossl_v3_holder_name_constraints,
