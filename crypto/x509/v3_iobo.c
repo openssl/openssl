712d89945fdd98d4ccbf86114413e2526e576cca
Jonathan M. Wilbur <jonathan@wilbur.space>
2024-08-21 11:36:25 +0000

    fixup! feat: add support for issuedOnBehalfOf X.509v3 extension

--- crypto/x509/v3_iobo.c
+++ crypto/x509/v3_iobo.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2021 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 1999-2024 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
@@ -10,9 +10,9 @@
 #include <openssl/x509v3.h>
 #include "ext_dat.h"
 
-static int i2r_IOBO(X509V3_EXT_METHOD *method,
-                    GENERAL_NAME *gn, BIO *out,
-                    int indent)
+static int i2r_ISSUED_ON_BEHALF_OF(X509V3_EXT_METHOD *method,
+                                   GENERAL_NAME *gn, BIO *out,
+                                   int indent)
 {
     if (BIO_printf(out, "%*s", indent, "") <= 0)
         return 0;
@@ -26,7 +26,7 @@ const X509V3_EXT_METHOD ossl_v3_issued_on_behalf_of = {
     0, 0, 0, 0,
     0, 0,
     0, 0,
-    (X509V3_EXT_I2R)i2r_IOBO,
+    (X509V3_EXT_I2R)i2r_ISSUED_ON_BEHALF_OF,
     0,
     NULL
 };
