d6e9dbeb37dc0c1ce90e64a54075f8c4e1633f36
Jonathan M. Wilbur <jonathan@wilbur.space>
2024-08-23 09:20:38 +0000

    fixup! fix: alias auditEntity OID

--- include/openssl/obj_mac.h
+++ include/openssl/obj_mac.h
@@ -1585,10 +1585,10 @@
 
 #define SN_ac_auditIdentity             "ac-auditIdentity"
 #define LN_ac_auditIdentity             "X509v3 Audit Identity"
-#define NID_ac_auditIdentity            1323
+#define NID_ac_auditIdentity            287
 #define OBJ_ac_auditIdentity            OBJ_id_pe,4L
 
-#define NID_ac_auditEntity              287
+#define NID_ac_auditEntity              1323
 #define OBJ_ac_auditEntity              OBJ_ac_auditIdentity
 
 #define SN_ac_targeting         "ac-targeting"
