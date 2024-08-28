lsingh4634426/OpenSSL-clone

Refactor OpenSSL 'EdDSA' EVP_SIGNATURE to allow use with EVP_PKEY functions
Add EVP_PKEY_{sign,verify}_message support for our Ed25519 and Ed448
implementations, including ph and ctx variants.

Tests are added with test_evp stanzas.

providers/fips/fipsprov.c
@@ -501,7 +501,14 @@ static const OSSL_ALGORITHM fips_signature[] = {
 # ifndef OPENSSL_NO_ECX
     { PROV_NAMES_ED25519, FIPS_UNAPPROVED_PROPERTIES,
       ossl_ed25519_signature_functions },
-    { PROV_NAMES_ED448, FIPS_UNAPPROVED_PROPERTIES, ossl_ed448_signature_functions },
+    { PROV_NAMES_ED25519ph, FIPS_UNAPPROVED_PROPERTIES,
+      ossl_ed25519ph_signature_functions },
+    { PROV_NAMES_ED25519ctx, FIPS_UNAPPROVED_PROPERTIES,
+      ossl_ed25519ctx_signature_functions },
+    { PROV_NAMES_ED448, FIPS_UNAPPROVED_PROPERTIES,
+      ossl_ed448_signature_functions },
+    { PROV_NAMES_ED448ph, FIPS_UNAPPROVED_PROPERTIES,
+      ossl_ed448ph_signature_functions },
 # endif
     { PROV_NAMES_ECDSA, FIPS_DEFAULT_PROPERTIES, ossl_ecdsa_signature_functions },
 #endif

openssl/openssl

providers/fips/fipsprov.c
@@ -441,7 +441,14 @@ static const OSSL_ALGORITHM fips_signature[] = {
 # ifndef OPENSSL_NO_ECX
     { PROV_NAMES_ED25519, FIPS_DEFAULT_PROPERTIES,
       ossl_ed25519_signature_functions },
-    { PROV_NAMES_ED448, FIPS_DEFAULT_PROPERTIES, ossl_ed448_signature_functions },
+    { PROV_NAMES_ED25519ph, FIPS_UNAPPROVED_PROPERTIES,
+      ossl_ed25519ph_signature_functions },
+    { PROV_NAMES_ED25519ctx, FIPS_UNAPPROVED_PROPERTIES,
+      ossl_ed25519ctx_signature_functions },
+    { PROV_NAMES_ED448, FIPS_DEFAULT_PROPERTIES,
+      ossl_ed448_signature_functions },
+    { PROV_NAMES_ED448ph, FIPS_UNAPPROVED_PROPERTIES,
+      ossl_ed448ph_signature_functions },
 # endif
     { PROV_NAMES_ECDSA, FIPS_DEFAULT_PROPERTIES, ossl_ecdsa_signature_functions },
 #endif