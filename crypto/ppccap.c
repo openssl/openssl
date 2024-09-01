32d28dd4c244a01244bee9a83b2695d112610234
Brad Smith <brad@comstyle.com>
2024-09-01 01:07:16 -0400

    Add support for elf_aux_info() on OpenBSD
    CLA: trivial

--- crypto/ppccap.c
+++ crypto/ppccap.c
@@ -99,9 +99,10 @@ size_t OPENSSL_instrument_bus2(unsigned int *out, size_t cnt, size_t max)
 # endif
 #endif
 
-#if defined(__FreeBSD__)
+#if defined(__FreeBSD__) || defined(__OpenBSD__)
 # include <sys/param.h>
-# if __FreeBSD_version >= 1200000
+# if (defined(__FreeBSD__) && __FreeBSD_version >= 1200000) || \
+    (defined(__OpenBSD__) && OpenBSD >= 202409)
 #  include <sys/auxv.h>
 #  define OSSL_IMPLEMENT_GETAUXVAL
 
