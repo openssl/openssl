f51816d2cc5049151a5bd43d7fe863aa055f5e4c
sftcd <stephen.farrell@cs.tcd.ie>
2024-09-11 00:28:32 +0100

    ECH CLI implementation
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/25420)

---
+++ ssl/ech/ech_helper.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2024 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#include <openssl/ssl.h>
+#include <openssl/ech.h>
+#include "../ssl_local.h"
+#include "ech_local.h"
+
+/* TODO(ECH): move code that's used by internals and test here */
