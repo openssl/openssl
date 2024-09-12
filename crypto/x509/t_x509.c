c154c5cb4d03d6b76fbfb0e9c5448eed96fdcf8a
Jonathan M. Wilbur <jonathan@wilbur.space>
2024-09-12 11:45:43 +0000

    fixup! feat: add ossl_serial_number_print

--- crypto/x509/t_x509.c
+++ crypto/x509/t_x509.c
@@ -86,7 +86,7 @@ int X509_print_ex(BIO *bp, X509 *x, unsigned long nmflags,
     if (!(cflag & X509_FLAG_NO_SERIAL)) {
         const ASN1_INTEGER *bs = X509_get0_serialNumber(x);
 
-        if (BIO_write(bp, "        Serial Number:", 22) <= 0)
+        if (BIO_write(bp, "        Serial Number: ", 23) <= 0)
             goto err;
         if (ossl_serial_number_print(bp, bs, 12) != 0)
             goto err;
@@ -528,7 +528,7 @@ int ossl_serial_number_print(BIO *out, const ASN1_INTEGER* bs, int indent)
             ul = l;
             neg = "";
         }
-        if (BIO_printf(out, " %s%lu (%s0x%lx)", neg, ul, neg, ul) <= 0)
+        if (BIO_printf(out, "%s%lu (%s0x%lx)", neg, ul, neg, ul) <= 0)
             return -1;
     } else { /* Reading a long int failed: just print hex. */
         neg = (bs->type == V_ASN1_NEG_INTEGER) ? " (Negative)" : "";
