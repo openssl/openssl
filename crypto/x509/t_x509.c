b65312674f72629fa9a948d920c008dce1bdd0a9
Jonathan M. Wilbur <jonathan@wilbur.space>
2024-09-12 10:52:20 +0000

    feat: add ossl_serial_number_print

--- crypto/x509/t_x509.c
+++ crypto/x509/t_x509.c
@@ -531,3 +531,42 @@ int X509_STORE_CTX_print_verify_cb(int ok, X509_STORE_CTX *ctx)
 
     return ok;
 }
+
+/* Prints serial numbers in decimal and hexadecimal. The indent argument is only
+used if the serial number is too large to fit in a long int. */
+int ossl_serial_number_print(BIO *out, const ASN1_INTEGER* bs, int indent)
+{
+    int i;
+    long l;
+    unsigned long ul;
+    const char *neg;
+
+    if (bs->length <= (int)sizeof(long)) {
+        ERR_set_mark();
+        l = ASN1_INTEGER_get(bs);
+        ERR_pop_to_mark();
+    } else {
+        l = -1;
+    }
+    if (l != -1) { /* Reading a long int succeeded: print decimal and hex. */
+        if (bs->type == V_ASN1_NEG_INTEGER) {
+            ul = 0 - (unsigned long)l;
+            neg = "-";
+        } else {
+            ul = l;
+            neg = "";
+        }
+        if (BIO_printf(out, " %s%lu (%s0x%lx)", neg, ul, neg, ul) <= 0)
+            return -1;
+    } else { /* Reading a long int failed: just print hex. */
+        neg = (bs->type == V_ASN1_NEG_INTEGER) ? " (Negative)" : "";
+        if (BIO_printf(out, "\n%*s%s", indent, "", neg) <= 0)
+            return -1;
+
+        for (i = 0; i < bs->length; i++) {
+            if (BIO_printf(out, "%02x%c", bs->data[i], ':') <= 0)
+                return -1;
+        }
+    }
+    return 0;
+}
