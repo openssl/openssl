40d1bc5287ae0c1d87a518e4e5a0d7d1bea4f6f6
Andrew Dinh <andrewd@openssl.org>
2024-09-16 03:26:47 +0800

    Adds a new demo blocking QUIC server for use with the existing demo QUIC clients
    Remove util/quicserver.c
    
    Reviewed-by: Saša Nedvědický <sashan@openssl.org>
    Reviewed-by: Neil Horman <nhorman@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/25465)

--- demos/guide/quic-client-block.c
+++ demos/guide/quic-client-block.c
@@ -243,7 +243,8 @@ int main(int argc, char *argv[])
         printf("Failed to write hostname in HTTP request\n");
         goto end;
     }
-    if (!SSL_write_ex(ssl, request_end, strlen(request_end), &written)) {
+    if (!SSL_write_ex2(ssl, request_end, strlen(request_end),
+                       SSL_WRITE_FLAG_CONCLUDE, &written)) {
         printf("Failed to write end of HTTP request\n");
         goto end;
     }
