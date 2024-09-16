a57c6f84920bff522bca5fede73f1a3f132d7cff
Matt Caswell <matt@openssl.org>
2024-09-16 12:00:32 +0100

    Don't restrict what EVP_PKEY_Q_keygen can be used for
    The EVP_PKEY_Q_keygen function contains a list of algorithm type names
    and fails if the requested name is not in the list. This prevents the use
    of this function for externally supplied key type names.
    
    We should just assume that any unrecognised key type name does not require
    a parameter.
    
    Reviewed-by: Dmitry Belyavskiy <beldmit@gmail.com>
    Reviewed-by: Tim Hudson <tjh@openssl.org>
    Reviewed-by: Paul Dale <ppzgs1@gmail.com>
    (Merged from https://github.com/openssl/openssl/pull/25468)

--- crypto/evp/evp_lib.c
+++ crypto/evp/evp_lib.c
@@ -1236,17 +1236,10 @@ EVP_PKEY *EVP_PKEY_Q_keygen(OSSL_LIB_CTX *libctx, const char *propq,
         name = va_arg(args, char *);
         params[0] = OSSL_PARAM_construct_utf8_string(OSSL_PKEY_PARAM_GROUP_NAME,
                                                      name, 0);
-    } else if (OPENSSL_strcasecmp(type, "ED25519") != 0
-               && OPENSSL_strcasecmp(type, "X25519") != 0
-               && OPENSSL_strcasecmp(type, "ED448") != 0
-               && OPENSSL_strcasecmp(type, "X448") != 0
-               && OPENSSL_strcasecmp(type, "SM2") != 0) {
-        ERR_raise(ERR_LIB_EVP, ERR_R_PASSED_INVALID_ARGUMENT);
-        goto end;
     }
+
     ret = evp_pkey_keygen(libctx, type, propq, params);
 
- end:
     va_end(args);
     return ret;
 }
