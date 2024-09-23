776efcde346c0aa69628bf56c6971cb4030dfefd
Dimitri John Ledkov <dimitri.ledkov@surgut.co.uk>
2024-09-23 12:57:22 +0100

    fips: prohibit SHA1 in DH & ECDH exchange
    See Section 5 Key Agreement Using Diffie-Hellman and MQV of [NIST SP 800-131Ar2](https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-131Ar2.pdf).
    
    Strengths less than 112bits is disallowed, thus eliminating SHA1.
    
    Skip cms test case that requires use of SHA1 with X9.42 DH.

--- providers/common/securitycheck_fips.c
+++ providers/common/securitycheck_fips.c
@@ -82,7 +82,8 @@ int ossl_fips_ind_digest_check(OSSL_FIPS_IND *ind, int id,
                                OSSL_LIB_CTX *libctx,
                                const EVP_MD *md, const char *desc)
 {
-    int approved = (ossl_digest_get_approved_nid(md) != NID_undef);
+    int nid = ossl_digest_get_approved_nid(md);
+    int approved = (nid != NID_undef && nid != NID_sha1);
 
     if (!approved) {
         if (!ossl_FIPS_IND_on_unapproved(ind, id, libctx, desc, "Digest",
