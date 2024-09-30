c64558ede85ed296753b42f74478b99bd89e7c34
slontis <shane.lontis@oracle.com>
2024-10-01 02:38:58 +1000

    Add LMS public key decoder.
    This uses OSSL_DECODER_CTX_new_for_pkey().
    "XDR" can be specified for the input type, and the key type is "LMS"
    
    Reviewed-by: Viktor Dukhovni <viktor@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Paul Dale <ppzgs1@gmail.com>
    (Merged from https://github.com/openssl/openssl/pull/27885)

--- crypto/lms/lms_pubkey_decode.c
+++ crypto/lms/lms_pubkey_decode.c
@@ -11,6 +11,27 @@
 #include "crypto/lms.h"
 #include "crypto/lms_util.h"
 
+/**
+ * @brief Calculate the size of a public key in XDR format.
+ *
+ * @param data A byte array of XDR data for a LMS public key.
+ *             The first 4 bytes are looked at.
+ * @param datalen The size of |data|.
+ * @returns The calculated size, or 0 on error.
+ */
+size_t ossl_lms_pubkey_length(const unsigned char *data, size_t datalen)
+{
+    PACKET pkt;
+    uint32_t lms_type;
+    const LMS_PARAMS *params;
+
+    if (!PACKET_buf_init(&pkt, data, datalen)
+            || !PACKET_get_4_len(&pkt, &lms_type)
+            || (params = ossl_lms_params_get(lms_type)) == NULL)
+        return 0;
+    return LMS_SIZE_LMS_TYPE + LMS_SIZE_OTS_TYPE + LMS_SIZE_I + params->n;
+}
+
 /*
  * @brief Decode LMS public key data in XDR format into a LMS_KEY object.
  *
@@ -25,18 +46,22 @@
  * @return 1 on success or 0 otherwise.
  */
 static
-int lms_pubkey_from_pkt(PACKET *pkt, LMS_KEY *lmskey)
+int lms_pubkey_from_pkt(LMS_KEY *lmskey, const unsigned char *pubkeydata,
+                        size_t publen)
 {
     uint32_t lms_type;
     uint32_t ots_type;
     LMS_PUB_KEY *key = &lmskey->pub;
+    PACKET pkt;
 
-    key->encoded = (unsigned char *)pkt->curr;
-    if (!PACKET_get_4_len(pkt, &lms_type))
+    if (!PACKET_buf_init(&pkt, pubkeydata, publen))
+        goto err;
+    key->encoded = (unsigned char *)pkt.curr;
+    if (!PACKET_get_4_len(&pkt, &lms_type))
         goto err;
     lmskey->lms_params = ossl_lms_params_get(lms_type);
     if (lmskey->lms_params == NULL
-            || !PACKET_get_4_len(pkt, &ots_type))
+            || !PACKET_get_4_len(&pkt, &ots_type))
         goto err;
     lmskey->ots_params = ossl_lm_ots_params_get(ots_type);
     if (lmskey->ots_params == NULL)
@@ -44,11 +69,13 @@ int lms_pubkey_from_pkt(PACKET *pkt, LMS_KEY *lmskey)
 
     /* The digest used must be the same */
     if (HASH_NOT_MATCHED(lmskey->ots_params, lmskey->lms_params)
-            || !PACKET_get_bytes_shallow(pkt, &lmskey->Id, LMS_SIZE_I)
-            || !PACKET_get_bytes_shallow(pkt, &key->K, lmskey->lms_params->n))
+            || !PACKET_get_bytes(&pkt, (const unsigned char **)&lmskey->Id,
+                                 LMS_SIZE_I)
+            || !PACKET_get_bytes(&pkt, (const unsigned char **)&key->K,
+                                 lmskey->lms_params->n))
         goto err;
-    key->encodedlen = pkt->curr - key->encoded;
-    return 1;
+    key->encodedlen = pkt.curr - key->encoded;
+    return PACKET_remaining(&pkt) == 0;
 err:
     return 0;
 }
@@ -64,29 +91,22 @@ err:
  * @returns 1 on success, or 0 otherwise. 0 is returned if either |pub| is
  * invalid or |publen| is not the correct size (i.e. trailing data is not allowed)
  */
-static
-int lms_pubkey_decode(const unsigned char *pub, size_t publen, LMS_KEY *lmskey)
+int ossl_lms_pubkey_decode(const unsigned char *pub, size_t publen,
+                           LMS_KEY *lmskey)
 {
-    PACKET pkt;
     LMS_PUB_KEY *pkey = &lmskey->pub;
 
     if (pkey->encoded != NULL && pkey->encodedlen != publen) {
-        if (pkey->allocated) {
-            OPENSSL_free(pkey->encoded);
-            pkey->allocated = 0;
-        }
+        OPENSSL_free(pkey->encoded);
         pkey->encodedlen = 0;
     }
     pkey->encoded = OPENSSL_memdup(pub, publen);
     if (pkey->encoded == NULL)
         return 0;
 
-    if (!PACKET_buf_init(&pkt, pkey->encoded, publen)
-            || !lms_pubkey_from_pkt(&pkt, lmskey)
-            || (PACKET_remaining(&pkt) > 0))
+    if (!lms_pubkey_from_pkt(lmskey, pkey->encoded, publen))
         goto err;
     pkey->encodedlen = publen;
-    pkey->allocated = 1;
     return 1;
 err:
     OPENSSL_free(pkey->encoded);
@@ -107,9 +127,9 @@ int ossl_lms_pubkey_from_params(const OSSL_PARAM params[], LMS_KEY *lmskey)
 
     p = OSSL_PARAM_locate_const(params, OSSL_PKEY_PARAM_ENCODED_PUBLIC_KEY);
     if (p != NULL) {
-        if (p->data == NULL || p->data_type != OSSL_PARAM_OCTET_STRING)
-            return 0;
-        if (!lms_pubkey_decode(p->data, p->data_size, lmskey))
+        if (p->data == NULL
+                || p->data_type != OSSL_PARAM_OCTET_STRING
+                || !ossl_lms_pubkey_decode(p->data, p->data_size, lmskey))
             return 0;
     }
     return 1;
--- providers/implementations/include/prov/implementations.h
+++ providers/implementations/include/prov/implementations.h
@@ -854,6 +854,8 @@ extern const OSSL_DISPATCH ossl_pem_to_der_decoder_functions[];
 extern const OSSL_DISPATCH ossl_file_store_functions[];
 extern const OSSL_DISPATCH ossl_winstore_store_functions[];
 
+extern const OSSL_DISPATCH ossl_xdr_to_lms_decoder_functions[];
+
 extern const OSSL_DISPATCH ossl_PrivateKeyInfo_der_to_ml_dsa_44_decoder_functions[];
 extern const OSSL_DISPATCH ossl_SubjectPublicKeyInfo_der_to_ml_dsa_44_decoder_functions[];
 
