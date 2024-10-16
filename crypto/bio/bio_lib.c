4e581b03189cb2da1948b50d242f1d00623ca3c7
Neil Horman <nhorman@openssl.org>
2024-10-16 14:34:08 -0400

    Fix potential use-after-free in REF_PRINT_COUNT
    We use REF_PRINT_COUNT to dump out the value of various reference
    counters in our code
    
    However, we commonly use this macro after an increment or decrement.  On
    increment its fine, but on decrement its not, because the macro
    dereferences the object holding the counter value, which may be freed by
    another thread, as we've given up our ref count to it prior to using the
    macro.
    
    The rule is that we can't reference memory for an object once we've
    released our reference, so lets fix this by altering REF_PRINT_COUNT to
    accept the value returned by CRYPTO_[UP|DOWN]_REF instead.  The
    eliminates the need to dereference the memory the object points to an
    allows us to use the call after we release our reference count
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/25664)
    
    (cherry picked from commit dc10ffc2834e0d2f5ebc1c3e29bd97f1f43a0ead)

--- crypto/bio/bio_lib.c
+++ crypto/bio/bio_lib.c
@@ -126,7 +126,7 @@ int BIO_free(BIO *a)
     if (CRYPTO_DOWN_REF(&a->references, &ret) <= 0)
         return 0;
 
-    REF_PRINT_COUNT("BIO", a);
+    REF_PRINT_COUNT("BIO", ret, a);
     if (ret > 0)
         return 1;
     REF_ASSERT_ISNT(ret < 0);
@@ -191,7 +191,7 @@ int BIO_up_ref(BIO *a)
     if (CRYPTO_UP_REF(&a->references, &i) <= 0)
         return 0;
 
-    REF_PRINT_COUNT("BIO", a);
+    REF_PRINT_COUNT("BIO", i, a);
     REF_ASSERT_ISNT(i < 2);
     return i > 1;
 }
