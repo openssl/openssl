8e5afd7bd8434981066073ce357ddadbf8fef1de
sashan <anedvedicky@gmail.com>
2024-10-24 17:38:19 +0200

    fixup! This is the patch which comes from PR #25751 by Zhiguo Zhou from intel. this is is a draft PR which I need to try to build by OpenSSL CI.
    - enable clang 16

--- crypto/bn/asm/rsaz-3k-avxifma.pl
+++ crypto/bn/asm/rsaz-3k-avxifma.pl
@@ -32,7 +32,12 @@ if (`$ENV{CC} -Wa,-v -c -o /dev/null -x assembler /dev/null 2>&1`
         =~ /GNU assembler version ([2-9]\.[0-9]+)/) {
     $avxifma = ($1>=2.40);
 }
-$avxifma = 1;
+
+if (!$avxifma && `$ENV{CC} -v 2>&1`
+    =~ /\s*((?:clang|LLVM) version|.*based on LLVM) ([0-9]+)\.([0-9]+)\.([0-9]+)?/) {
+    my $ver = $2 + $3/100.0 + $4/10000.0; # 3.1.0->3.01, 3.10.1->3.1001
+    $avxifma = ($ver>=16.0);
+}
 
 open OUT,"| \"$^X\" \"$xlate\" $flavour \"$output\""
     or die "can't call $xlate: $!";
