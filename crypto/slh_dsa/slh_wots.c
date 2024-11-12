006b56816aaf21b2e005a3893280af152841aa2d
slontis <shane.lontis@oracle.com>
2024-11-12 18:35:10 +1100

    Update SLH-DSA code to use PACKET and WPACKET.
    Reviewed-by: Paul Dale <ppzgs1@gmail.com>
    Reviewed-by: Viktor Dukhovni <viktor@openssl.org>
    Reviewed-by: Tim Hudson <tjh@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/25882)

--- crypto/slh_dsa/slh_wots.c
+++ crypto/slh_dsa/slh_wots.c
@@ -82,32 +82,43 @@ static ossl_inline void compute_checksum_nibbles(const uint8_t *in, size_t in_le
  *
  * @param ctx Contains SLH_DSA algorithm functions and constants.
  * @param in An input string of |n| bytes
- * @param n The size of |in| and |pk_seed|_
  * @param start_index The chaining start index
  * @param steps The number of iterations starting from |start_index|
  *              Note |start_index| + |steps| < w
  *              (where w = 16 indicates the length of the hash chains)
+ * @param pk_seed A public key seed (which is added to the hash)
  * @param adrs An ADRS object which has a type of WOTS_HASH, and has a layer
  *             address, tree address, key pair address and chain address
- * @param pk_seed A public key seed (which is added to the hash)
+ * @params wpkt A WPACKET object to write the hash chain to (n bytes are written)
  * @returns 1 on success, or 0 on error.
  */
 static int slh_wots_chain(SLH_DSA_CTX *ctx, const uint8_t *in,
                           uint8_t start_index, uint8_t steps,
-                          const uint8_t *pk_seed, uint8_t *adrs, uint8_t *out)
+                          const uint8_t *pk_seed, uint8_t *adrs, WPACKET *wpkt)
 {
     SLH_HASH_FUNC_DECLARE(ctx, hashf, hctx);
     SLH_ADRS_FUNC_DECLARE(ctx, adrsf);
     SLH_HASH_FN_DECLARE(hashf, F);
     SLH_ADRS_FN_DECLARE(adrsf, set_hash_address);
-    size_t j, end_index = start_index + steps;
+    size_t j = start_index, end_index;
     size_t n = ctx->params->n;
+    uint8_t *tmp; /* Pointer into the |wpkt| buffer */
+    size_t tmp_len = n;
 
-    memcpy(out, in, n);
+    if (steps == 0)
+        return WPACKET_memcpy(wpkt, in, n);
 
-    for (j = start_index; j < end_index; ++j) {
+    if (!WPACKET_allocate_bytes(wpkt, tmp_len, &tmp))
+        return 0;
+
+    set_hash_address(adrs, j++);
+    if (!F(hctx, pk_seed, adrs, in, n, tmp, tmp_len))
+        return 0;
+
+    end_index = start_index + steps;
+    for (; j < end_index; ++j) {
         set_hash_address(adrs, j);
-        if (!F(hctx, pk_seed, adrs, out, n, out))
+        if (!F(hctx, pk_seed, adrs, tmp, n, tmp, tmp_len))
             return 0;
     }
     return 1;
@@ -123,11 +134,12 @@ static int slh_wots_chain(SLH_DSA_CTX *ctx, const uint8_t *in,
  * @param adrs An ADRS object containing the layer address, tree address and
  *             keypair address of the WOTS+ public key to generate.
  * @param pk_out The generated public key of size |n|
+ * @param pk_out_len The maximum size of |pk_out|
  * @returns 1 on success, or 0 on error.
  */
 int ossl_slh_wots_pk_gen(SLH_DSA_CTX *ctx,
                          const uint8_t *sk_seed, const uint8_t *pk_seed,
-                         SLH_ADRS adrs, uint8_t *pk_out)
+                         SLH_ADRS adrs, uint8_t *pk_out, size_t pk_out_len)
 {
     int ret = 0;
     SLH_HASH_FUNC_DECLARE(ctx, hashf, hctx);
@@ -136,33 +148,38 @@ int ossl_slh_wots_pk_gen(SLH_DSA_CTX *ctx,
     SLH_ADRS_FN_DECLARE(adrsf, set_chain_address);
     SLH_ADRS_DECLARE(sk_adrs);
     SLH_ADRS_DECLARE(wots_pk_adrs);
-    size_t i, len = 0;
     size_t n = ctx->params->n;
-    uint8_t tmp[SLH_WOTS_LEN_MAX * SLH_MAX_N], *ptmp = tmp;
-    uint8_t sk[32];
+    size_t i, len = SLH_WOTS_LEN(n); /*2 * n + 3 */
+    uint8_t sk[SLH_MAX_N];
+    uint8_t tmp[SLH_WOTS_LEN_MAX * SLH_MAX_N];
+    WPACKET pkt, *tmp_wpkt = &pkt; /* Points to the |tmp| buffer */
+    size_t tmp_len = 0;
 
+    if (!WPACKET_init_static_len(tmp_wpkt, tmp, sizeof(tmp), 0))
+        return 0;
     adrsf->copy(sk_adrs, adrs);
     adrsf->set_type_and_clear(sk_adrs, SLH_ADRS_TYPE_WOTS_PRF);
     adrsf->copy_keypair_address(sk_adrs, adrs);
 
-    len = SLH_WOTS_LEN(n); /* See Section 5 intro */
-    for (i = 0; i < len; ++i) {
+    for (i = 0; i < len; ++i) { /* len = 2n + 3 */
         set_chain_address(sk_adrs, i);
-        if (!PRF(hctx, pk_seed, sk_seed, sk_adrs, sk))
+        if (!PRF(hctx, pk_seed, sk_seed, sk_adrs, sk, sizeof(sk)))
             goto end;
 
         set_chain_address(adrs, i);
-        if (!slh_wots_chain(ctx, sk, 0, NIBBLE_MASK, pk_seed, adrs, ptmp))
+        if (!slh_wots_chain(ctx, sk, 0, NIBBLE_MASK, pk_seed, adrs, tmp_wpkt))
             goto end;
-        ptmp += n;
     }
 
-    len = ptmp - tmp; /* should be n * (2 * n + 3) */
+    if (!WPACKET_get_total_written(tmp_wpkt, &tmp_len)) /* should be n * (2 * n + 3) */
+        goto end;
     adrsf->copy(wots_pk_adrs, adrs);
     adrsf->set_type_and_clear(wots_pk_adrs, SLH_ADRS_TYPE_WOTS_PK);
     adrsf->copy_keypair_address(wots_pk_adrs, adrs);
-    ret = hashf->T(hctx, pk_seed, wots_pk_adrs, tmp, len, pk_out);
+    ret = hashf->T(hctx, pk_seed, wots_pk_adrs, tmp, tmp_len,
+                   pk_out, pk_out_len);
 end:
+    WPACKET_finish(tmp_wpkt);
     OPENSSL_cleanse(tmp, sizeof(tmp));
     OPENSSL_cleanse(sk, n);
     return ret;
@@ -172,6 +189,8 @@ end:
  * @brief WOTS+ Signature generation
  * See FIPS 205 Section 5.2 Algorithm 7
  *
+ * The returned signature size is len * |n| bytes (where len = 2 * |n| + 3).
+ *
  * @param ctx Contains SLH_DSA algorithm functions and constants.
  * @param msg An input message of size |n| bytes.
  *            The message is either an XMSS or FORS public key
@@ -179,14 +198,12 @@ end:
  * @param pk_seed The public key seed  of size |n| bytes
  * @param adrs An address containing the layer address, tree address and key
  *             pair address. The size is either 32 or 22 bytes.
- * @param sig The returned signature.
- * @param sig_len The size of |sig| which should be len * |n| bytes.
- *                (where len = 2 * |n| + 3)
+ * @param sig_wpkt A WPACKET object to write the signature to.
  * @returns 1 on success, or 0 on error.
  */
 int ossl_slh_wots_sign(SLH_DSA_CTX *ctx, const uint8_t *msg,
                        const uint8_t *sk_seed, const uint8_t *pk_seed,
-                       SLH_ADRS adrs, uint8_t *sig, size_t sig_len)
+                       SLH_ADRS adrs, WPACKET *sig_wpkt)
 {
     int ret = 0;
     SLH_HASH_FUNC_DECLARE(ctx, hashf, hctx);
@@ -198,7 +215,6 @@ int ossl_slh_wots_sign(SLH_DSA_CTX *ctx, const uint8_t *msg,
     uint8_t sk[SLH_MAX_N];
     size_t i, len1, len;
     size_t n = ctx->params->n;
-    uint8_t *psig = sig;
 
     len1 = SLH_WOTS_LEN1(n); /* 2 * n is for the message length in nibbles */
     len = len1 + SLH_WOTS_LEN2;  /* 2 * n + 3 (3 checksum nibbles) */
@@ -218,19 +234,16 @@ int ossl_slh_wots_sign(SLH_DSA_CTX *ctx, const uint8_t *msg,
     for (i = 0; i < len; ++i) {
         set_chain_address(sk_adrs, i);
         /* compute chain i secret */
-        if (!PRF(hctx, pk_seed, sk_seed, sk_adrs, sk))
+        if (!PRF(hctx, pk_seed, sk_seed, sk_adrs, sk, sizeof(sk)))
             goto err;
         set_chain_address(adrs, i);
         /* compute chain i signature */
         if (!slh_wots_chain(ctx, sk, 0, msg_and_csum_nibbles[i],
-                            pk_seed, adrs, psig))
+                            pk_seed, adrs, sig_wpkt))
             goto err;
-        psig += n;
     }
-    assert(sig_len == (size_t)(psig - sig));
     ret = 1;
 err:
-    OPENSSL_cleanse(sk, n);
     return ret;
 }
 
@@ -238,30 +251,40 @@ err:
  * @brief Compute a candidate WOTS+ public key from a message and signature
  * See FIPS 205 Section 5.3 Algorithm 8
  *
+ * The size of the signature is len * |n| bytes (where len = 2 * |n| + 3).
+ *
  * @param ctx Contains SLH_DSA algorithm functions and constants.
- * @param sig A WOTS+ signature of size len * |n| bytes. (where len = 2 * |n| + 3)
+ * @param sig_rpkt A PACKET object to read a WOTS+ signature from
  * @param msg A message of size |n| bytes.
  * @param pk_seed The public key seed of size |n|.
  * @param adrs An ADRS object containing the layer address, tree address and
  *             key pair address that of the WOTS+ key used to sign the message.
  * @param pk_out The returned public key candidate of size |n|
+ * @param pk_out_len The maximum size of |pk_out|
  * @returns 1 on success, or 0 on error.
  */
 int ossl_slh_wots_pk_from_sig(SLH_DSA_CTX *ctx,
-                              const uint8_t *sig, const uint8_t *msg,
+                              PACKET *sig_rpkt, const uint8_t *msg,
                               const uint8_t *pk_seed, uint8_t *adrs,
-                              uint8_t *pk_out)
+                              uint8_t *pk_out, size_t pk_out_len)
 {
+    int ret = 0;
     SLH_HASH_FUNC_DECLARE(ctx, hashf, hctx);
     SLH_ADRS_FUNC_DECLARE(ctx, adrsf);
     SLH_ADRS_FN_DECLARE(adrsf, set_chain_address);
     SLH_ADRS_DECLARE(wots_pk_adrs);
     uint8_t msg_and_csum_nibbles[SLH_WOTS_LEN_MAX];
-    uint8_t tmp[SLH_WOTS_LEN_MAX * SLH_MAX_N], *ptmp = tmp;
     size_t i, len1, len, n = ctx->params->n;
+    const uint8_t *sig_i;  /* Pointer into |pkt_sig| buffer */
+    uint8_t tmp[SLH_WOTS_LEN_MAX * SLH_MAX_N];
+    WPACKET pkt, *tmp_pkt = &pkt;
+    size_t tmp_len = 0;
 
     len1 = SLH_WOTS_LEN1(n);
-    len = len1 + SLH_WOTS_LEN2;
+    len = len1 + SLH_WOTS_LEN2; /* 2n + 3 */
+
+    if (!WPACKET_init_static_len(tmp_pkt, tmp, sizeof(tmp), 0))
+        return 0;
 
     slh_bytes_to_nibbles(msg, n, msg_and_csum_nibbles);
     compute_checksum_nibbles(msg_and_csum_nibbles, len1, msg_and_csum_nibbles + len1);
@@ -269,16 +292,22 @@ int ossl_slh_wots_pk_from_sig(SLH_DSA_CTX *ctx,
     /* Compute the end nodes for each of the chains */
     for (i = 0; i < len; ++i) {
         set_chain_address(adrs, i);
-        if (!slh_wots_chain(ctx, sig, msg_and_csum_nibbles[i],
-                            NIBBLE_MASK - msg_and_csum_nibbles[i],
-                            pk_seed, adrs, ptmp))
-            return 0;
-        sig += n;
-        ptmp += n;
+        if (!PACKET_get_bytes(sig_rpkt, &sig_i, n)
+                || !slh_wots_chain(ctx, sig_i, msg_and_csum_nibbles[i],
+                                   NIBBLE_MASK - msg_and_csum_nibbles[i],
+                                   pk_seed, adrs, tmp_pkt))
+            goto err;
     }
     /* compress the computed public key value */
     adrsf->copy(wots_pk_adrs, adrs);
     adrsf->set_type_and_clear(wots_pk_adrs, SLH_ADRS_TYPE_WOTS_PK);
     adrsf->copy_keypair_address(wots_pk_adrs, adrs);
-    return hashf->T(hctx, pk_seed, wots_pk_adrs, tmp, ptmp - tmp, pk_out);
+    if (!WPACKET_get_total_written(tmp_pkt, &tmp_len))
+        goto err;
+    ret = hashf->T(hctx, pk_seed, wots_pk_adrs, tmp, tmp_len,
+                    pk_out, pk_out_len);
+ err:
+    if (!WPACKET_finish(tmp_pkt))
+        ret = 0;
+    return ret;
 }
