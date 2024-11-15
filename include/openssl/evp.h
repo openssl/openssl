9f69cb00e91e76117266ef3cff815fb93381765c
Dmitry Belyavskiy <beldmit@gmail.com>
2024-11-15 17:51:10 +0100

    Implement EVP_MAC_init_SKEY
    Signed-off-by: Dmitry Belyavskiy <beldmit@gmail.com>
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- include/openssl/evp.h
+++ include/openssl/evp.h
@@ -1260,6 +1260,7 @@ unsigned char *EVP_Q_mac(OSSL_LIB_CTX *libctx, const char *name, const char *pro
                          unsigned char *out, size_t outsize, size_t *outlen);
 int EVP_MAC_init(EVP_MAC_CTX *ctx, const unsigned char *key, size_t keylen,
                  const OSSL_PARAM params[]);
+int EVP_MAC_init_SKEY(EVP_MAC_CTX *ctx, EVP_SKEY *skey, const OSSL_PARAM params[]);
 int EVP_MAC_update(EVP_MAC_CTX *ctx, const unsigned char *data, size_t datalen);
 int EVP_MAC_final(EVP_MAC_CTX *ctx,
                   unsigned char *out, size_t *outl, size_t outsize);
