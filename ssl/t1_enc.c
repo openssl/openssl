674174ca89c7edd2b6913d5bb2013e076974106f
sftcd <stephen.farrell@cs.tcd.ie>
2024-11-20 14:10:30 +0000

    ECH client side

--- ssl/t1_enc.c
+++ ssl/t1_enc.c
@@ -577,6 +577,10 @@ int tls1_alert_code(int code)
         return SSL_AD_HANDSHAKE_FAILURE;
     case TLS13_AD_MISSING_EXTENSION:
         return SSL_AD_HANDSHAKE_FAILURE;
+#ifndef OPENSSL_NO_ECH
+    case SSL_AD_ECH_REQUIRED:
+        return TLS1_AD_ECH_REQUIRED;
+#endif
     default:
         return -1;
     }
