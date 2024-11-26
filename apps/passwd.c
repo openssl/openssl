b1f7e6d8bb4259dd33a72b8918c50f7b74d059d6
Bartel Artem <artem.bartel@gmail.com>
2024-11-26 11:04:43 +0300

    checking '<=' instead of '=='.

--- apps/passwd.c
+++ apps/passwd.c
@@ -369,7 +369,7 @@ static char *md5crypt(const char *passwd, const char *magic, const char *salt)
     if (magic_len > 0)
         salt_out += 2 + magic_len;
 
-    assert(salt_len == 8);
+    assert(salt_len <= 8);
 
     md = EVP_MD_CTX_new();
     if (md == NULL
