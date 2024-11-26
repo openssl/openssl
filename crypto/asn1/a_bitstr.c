1e8c54398da344e98067062d2897eabbd68f4386
Andrey Tsygunka <aitsygunka@yandex.ru>
2024-11-26 10:53:31 +0300

    Fix heap-buffer-overflow in ossl_i2c_ASN1_BIT_STRING, when data contains only zero values.
    CLA: trivial
    
    Signed-off-by: Andrey Tsygunka <aitsygunka@yandex.ru>

--- crypto/asn1/a_bitstr.c
+++ crypto/asn1/a_bitstr.c
@@ -36,6 +36,10 @@ int ossl_i2c_ASN1_BIT_STRING(ASN1_BIT_STRING *a, unsigned char **pp)
                 if (a->data[len - 1])
                     break;
             }
+
+            if (len == 0)
+                return -1;
+
             j = a->data[len - 1];
             if (j & 0x01)
                 bits = 0;
