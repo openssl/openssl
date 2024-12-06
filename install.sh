55cf109f7e1beb3b4b64a8d090987a58c680f9ce
minghui.zhang <minghui.zhang@spacemit.com>
2024-12-06 14:49:40 +0800

    add libcrypto.so libcrypto.so.3 libssl.so libssl.so.3
    Change-Id: I821dba4484deb3f52894a0277ff6485e3b0225df
    (cherry picked from commit 03e3eaf75a8307f3cfab0947ad4398640e6757dd)

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 TARGET_DIR=$1
-mkdir -p $TARGET_DIR/usr/bin
-cp -rf apps/openssl  $TARGET_DIR/usr/bin
+mkdir -p $TARGET_DIR
+cp -rf apps/openssl  $TARGET_DIR/usr/bin/openssl
+cp -rf  libssl.so $TARGET_DIR/usr/lib/
+cp -rf  libssl.so.3 $TARGET_DIR/usr/lib/
+cp -rf  libcrypto.so $TARGET_DIR/usr/lib/
+cp -rf  libcrypto.so.3 $TARGET_DIR/usr/lib/
