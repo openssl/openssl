47bb9b414d064b6ddec9ee61155fa3933636f6af
Frederik Wedel-Heinen <frederik.wedel-heinen@dencrypt.dk>
2024-12-11 14:09:30 +0100

    fixup! Reduce the number of mallocs in dtls1_new() by allocating message queues together with the d1 struct.

--- ssl/d1_lib.c
+++ ssl/d1_lib.c
@@ -90,13 +90,10 @@ int dtls1_new(SSL *ssl)
 
     if (!ssl3_new(ssl))
         return 0;
-    if ((d1 = OPENSSL_zalloc(sizeof(*d1) + 2 * sizeof(struct pqueue_st))) == NULL) {
+    if ((d1 = OPENSSL_zalloc(sizeof(*d1))) == NULL) {
         ssl3_free(ssl);
         return 0;
     }
-
-    d1->buffered_messages = (pqueue *)(d1 + 1);
-    d1->sent_messages = d1->buffered_messages + 1;
 
     if (s->server)
         d1->cookie_len = sizeof(s->d1->cookie);
@@ -123,8 +120,9 @@ void dtls1_clear_received_buffer(SSL_CONNECTION *s)
 {
     pitem *item = NULL;
     hm_fragment *frag = NULL;
+    pqueue *buffered_messages = &s->d1->buffered_messages;
 
-    while ((item = pqueue_pop(s->d1->rcvd_messages)) != NULL) {
+    while ((item = pqueue_pop(buffered_messages)) != NULL) {
         frag = (hm_fragment *)item->data;
         dtls1_hm_fragment_free(frag);
         pitem_free(item);
@@ -134,8 +132,9 @@ void dtls1_clear_received_buffer(SSL_CONNECTION *s)
 void dtls1_clear_sent_buffer(SSL_CONNECTION *s)
 {
     pitem *item = NULL;
+    pqueue *sent_messages = &s->d1->sent_messages;
 
-    while ((item = pqueue_pop(s->d1->sent_messages)) != NULL) {
+    while ((item = pqueue_pop(sent_messages)) != NULL) {
         dtls_sent_msg *sent_msg = (dtls_sent_msg *)item->data;
 
         if (sent_msg->record_type == SSL3_RT_CHANGE_CIPHER_SPEC
@@ -169,8 +168,6 @@ void dtls1_free(SSL *ssl)
 
 int dtls1_clear(SSL *ssl)
 {
-    pqueue *rcvd_messages;
-    pqueue *sent_messages;
     size_t mtu;
     size_t link_mtu;
 
@@ -184,8 +181,6 @@ int dtls1_clear(SSL *ssl)
     if (s->d1) {
         DTLS_timer_cb timer_cb = s->d1->timer_cb;
 
-        rcvd_messages = s->d1->rcvd_messages;
-        sent_messages = s->d1->sent_messages;
         mtu = s->d1->mtu;
         link_mtu = s->d1->link_mtu;
 
@@ -204,9 +199,6 @@ int dtls1_clear(SSL *ssl)
             s->d1->mtu = mtu;
             s->d1->link_mtu = link_mtu;
         }
-
-        s->d1->rcvd_messages = rcvd_messages;
-        s->d1->sent_messages = sent_messages;
     }
 
     if (!ssl3_clear(ssl))
--- ssl/statem/statem_dtls.c
+++ ssl/statem/statem_dtls.c
@@ -502,7 +502,7 @@ static int dtls1_retrieve_buffered_fragment(SSL_CONNECTION *s, size_t *len)
     int ret;
     int chretran = 0;
 
-    iter = pqueue_iterator(s->d1->rcvd_messages);
+    iter = pqueue_iterator(&s->d1->rcvd_messages);
     do {
         item = pqueue_next(&iter);
         if (item == NULL)
@@ -523,7 +523,7 @@ static int dtls1_retrieve_buffered_fragment(SSL_CONNECTION *s, size_t *len)
                  * It is safe to pop this message from the queue even though
                  * we have an active iterator
                  */
-                pqueue_pop(s->d1->rcvd_messages);
+                pqueue_pop(&s->d1->rcvd_messages);
                 dtls1_hm_fragment_free(frag);
                 pitem_free(item);
                 item = NULL;
@@ -543,7 +543,7 @@ static int dtls1_retrieve_buffered_fragment(SSL_CONNECTION *s, size_t *len)
                         * We have fragments for both a ClientHello without
                         * cookie and one with. Ditch the one without.
                         */
-                        pqueue_pop(s->d1->rcvd_messages);
+                        pqueue_pop(&s->d1->rcvd_messages);
                         dtls1_hm_fragment_free(frag);
                         pitem_free(item);
                         item = next;
@@ -564,7 +564,7 @@ static int dtls1_retrieve_buffered_fragment(SSL_CONNECTION *s, size_t *len)
 
     if (s->d1->handshake_read_seq == frag->msg_header.seq || chretran) {
         size_t frag_len = frag->msg_header.frag_len;
-        pqueue_pop(s->d1->rcvd_messages);
+        pqueue_pop(&s->d1->rcvd_messages);
 
         /* Calls SSLfatal() as required */
         ret = dtls1_preprocess_fragment(s, &frag->msg_header);
@@ -625,7 +625,7 @@ static int dtls1_reassemble_fragment(SSL_CONNECTION *s,
     memset(seq64be, 0, sizeof(seq64be));
     seq64be[6] = (unsigned char)(msg_hdr->seq >> 8);
     seq64be[7] = (unsigned char)msg_hdr->seq;
-    item = pqueue_find(s->d1->rcvd_messages, seq64be);
+    item = pqueue_find(&s->d1->rcvd_messages, seq64be);
 
     if (item == NULL) {
         frag = dtls1_hm_fragment_new(msg_hdr->msg_len, 1);
@@ -687,7 +687,7 @@ static int dtls1_reassemble_fragment(SSL_CONNECTION *s,
         if (item == NULL)
             goto err;
 
-        item = pqueue_insert(s->d1->rcvd_messages, item);
+        item = pqueue_insert(&s->d1->rcvd_messages, item);
         /*
          * pqueue_insert fails iff a duplicate item is inserted. However,
          * |item| cannot be a duplicate. If it were, |pqueue_find|, above,
@@ -724,7 +724,7 @@ static int dtls1_process_out_of_seq_message(SSL_CONNECTION *s,
     memset(seq64be, 0, sizeof(seq64be));
     seq64be[6] = (unsigned char)(msg_hdr->seq >> 8);
     seq64be[7] = (unsigned char)msg_hdr->seq;
-    item = pqueue_find(s->d1->rcvd_messages, seq64be);
+    item = pqueue_find(&s->d1->rcvd_messages, seq64be);
 
     /*
      * If we already have an entry and this one is a fragment, don't discard
@@ -782,7 +782,7 @@ static int dtls1_process_out_of_seq_message(SSL_CONNECTION *s,
         if (item == NULL)
             goto err;
 
-        item = pqueue_insert(s->d1->rcvd_messages, item);
+        item = pqueue_insert(&s->d1->rcvd_messages, item);
         /*
          * pqueue_insert fails iff a duplicate item is inserted. However,
          * |item| cannot be a duplicate. If it were, |pqueue_find|, above,
@@ -1119,7 +1119,7 @@ int dtls1_get_queue_priority(unsigned short seq, int record_type)
 
 int dtls1_retransmit_sent_messages(SSL_CONNECTION *s)
 {
-    piterator iter = pqueue_iterator(s->d1->sent_messages);
+    piterator iter = pqueue_iterator(&s->d1->sent_messages);
     pitem *item;
     int found = 0;
 
@@ -1188,7 +1188,7 @@ int dtls1_buffer_sent_message(SSL_CONNECTION *s, int record_type)
         return 0;
     }
 
-    pqueue_insert(s->d1->sent_messages, item);
+    pqueue_insert(&s->d1->sent_messages, item);
     return 1;
 }
 
@@ -1207,7 +1207,7 @@ int dtls1_retransmit_message(SSL_CONNECTION *s, unsigned short seq, int *found)
     seq64be[6] = (unsigned char)(seq >> 8);
     seq64be[7] = (unsigned char)seq;
 
-    item = pqueue_find(s->d1->sent_messages, seq64be);
+    item = pqueue_find(&s->d1->sent_messages, seq64be);
     if (item == NULL) {
         SSLfatal(s, SSL_AD_INTERNAL_ERROR, ERR_R_INTERNAL_ERROR);
         *found = 0;
