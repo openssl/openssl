dad175ee67dcae07ccff5a3a491db30a189f1a56
Simo Sorce <simo@redhat.com>
2025-01-10 17:20:59 -0500

    Add EVP_KDF_CTX_set_SKEY
    This allows to use a SKEY as input to a KDF derive operation.
    
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- include/openssl/core_dispatch.h
+++ include/openssl/core_dispatch.h
@@ -477,7 +477,8 @@ OSSL_CORE_MAKE_FUNC(int, mac_init_skey, (void *mctx, void *key, const OSSL_PARAM
 # define OSSL_FUNC_KDF_GET_PARAMS                    9
 # define OSSL_FUNC_KDF_GET_CTX_PARAMS               10
 # define OSSL_FUNC_KDF_SET_CTX_PARAMS               11
-# define OSSL_FUNC_KDF_DERIVE_SKEY                  12
+# define OSSL_FUNC_KDF_SET_SKEY                     12
+# define OSSL_FUNC_KDF_DERIVE_SKEY                  13
 
 OSSL_CORE_MAKE_FUNC(void *, kdf_newctx, (void *provctx))
 OSSL_CORE_MAKE_FUNC(void *, kdf_dupctx, (void *src))
@@ -495,7 +496,10 @@ OSSL_CORE_MAKE_FUNC(int, kdf_get_ctx_params,
                     (void *kctx, OSSL_PARAM params[]))
 OSSL_CORE_MAKE_FUNC(int, kdf_set_ctx_params,
                     (void *kctx, const OSSL_PARAM params[]))
-OSSL_CORE_MAKE_FUNC(void *, kdf_derive_skey, (void *kctx, const OSSL_PARAM params[]))
+OSSL_CORE_MAKE_FUNC(int, kdf_set_skey,
+                    (void *kctx, void *skeydata, const char *paramname))
+OSSL_CORE_MAKE_FUNC(void *, kdf_derive_skey,
+                    (void *kctx, size_t keylen, const OSSL_PARAM params[]))
 
 /* RAND */
 
