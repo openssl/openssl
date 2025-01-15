a5b5bb1761edc14a7276fd690004c72db504823e
Tom Cosgrove <tom.cosgrove@arm.com>
2025-01-15 16:20:16 +0000

    Add support for DIT in AArch64
    The Arm v8.4-A architecture added a data independent timing flag (DIT)
    which tells the CPU to avoid making optimisations that may change
    instruction timing based on the value of the data they operate on.
    Enabling this is crucial for constant-time cryptographic code, but it
    may reduce performance, so it needs to only be set at as low a level in
    the code as possible (but above cryptographic operations whose timing
    is important).
    
    This change enables DIT on AArch64, with a macro OSSL_NO_DIT that may be
    used to disable it.
    
    A macro OSSL_ENABLE_DIT_FOR_SCOPE is provided, which will turn DIT on
    (if it is not already) and arrange for it to be returned to its previous
    state when the current scope (function) exits.
    
    DIT is only enabled in the EVP interfaces.

--- crypto/evp/evp_enc.c
+++ crypto/evp/evp_enc.c
@@ -21,6 +21,7 @@
 #include "internal/common.h"
 #include "internal/safe_math.h"
 #include "crypto/evp.h"
+#include "crypto/aarch64_dit.h"
 #include "evp_local.h"
 
 OSSL_SAFE_MATH_SIGNED(int, int)
@@ -744,6 +745,8 @@ int EVP_EncryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl,
     size_t soutl, inl_ = (size_t)inl;
     int blocksize;
 
+    OSSL_ENABLE_DIT_FOR_SCOPE
+
     if (ossl_likely(outl != NULL)) {
         *outl = 0;
     } else {
@@ -805,6 +808,8 @@ int EVP_EncryptFinal_ex(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl)
     size_t soutl;
     int blocksize;
 
+    OSSL_ENABLE_DIT_FOR_SCOPE
+
     if (outl != NULL) {
         *outl = 0;
     } else {
@@ -854,6 +859,8 @@ int EVP_DecryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl,
     int ret;
     size_t soutl, inl_ = (size_t)inl;
     int blocksize;
+
+    OSSL_ENABLE_DIT_FOR_SCOPE
 
     if (ossl_likely(outl != NULL)) {
         *outl = 0;
@@ -910,6 +917,8 @@ int EVP_DecryptFinal_ex(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl)
     size_t soutl;
     int ret;
     int blocksize;
+
+    OSSL_ENABLE_DIT_FOR_SCOPE
 
     if (outl != NULL) {
         *outl = 0;
