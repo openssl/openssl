66dd711ebc0cae9caa5a2bddea36aabe4034d72b
Alexey Moksyakov <yavtuk@yandex.ru>
2025-01-20 18:30:12 +0300

    x10 load_sbox

--- crypto/sm4/asm/vpsm4_ex-armv8.pl
+++ crypto/sm4/asm/vpsm4_ex-armv8.pl
@@ -475,13 +475,13 @@ sub load_sbox () {
 	my $data = shift;
 
 $code.=<<___;
-	adrp $xtmp1, _${prefix}_consts
-	ldr $MaskQ, [$xtmp1, #:lo12:.Lsbox_magic]
-	ldr $TAHMatQ, [$xtmp1, #:lo12:.Lsbox_magic+16]
-	ldr $TALMatQ, [$xtmp1, #:lo12:.Lsbox_magic+32]
-	ldr $ATAHMatQ, [$xtmp1, #:lo12:.Lsbox_magic+48]
-	ldr $ATALMatQ, [$xtmp1, #:lo12:.Lsbox_magic+64]
-	ldr $ANDMaskQ, [$xtmp1, #:lo12:.Lsbox_magic+80]
+	adrp x10, _${prefix}_consts
+	ldr $MaskQ, [x10, #:lo12:.Lsbox_magic]
+	ldr $TAHMatQ, [x10, #:lo12:.Lsbox_magic+16]
+	ldr $TALMatQ, [x10, #:lo12:.Lsbox_magic+32]
+	ldr $ATAHMatQ, [x10, #:lo12:.Lsbox_magic+48]
+	ldr $ATALMatQ, [x10, #:lo12:.Lsbox_magic+64]
+	ldr $ANDMaskQ, [x10, #:lo12:.Lsbox_magic+80]
 ___
 }
 
@@ -526,8 +526,8 @@ sub compute_tweak_vec() {
 	my $std = shift;
 	&rbit(@vtmp[2],$src,$std);
 $code.=<<___;
-	adrp $xtmp2, _${prefix}_consts
-	ldr  @qtmp[0], [$xtmp2, #:lo12:.Lxts_magic]
+	adrp x10, _${prefix}_consts
+	ldr  @qtmp[0], [x10, #:lo12:.Lxts_magic]
 	shl  $des.16b, @vtmp[2].16b, #1
 	ext  @vtmp[1].16b, @vtmp[2].16b, @vtmp[2].16b,#15
 	ushr @vtmp[1].16b, @vtmp[1].16b, #7
