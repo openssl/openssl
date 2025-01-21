b999ea6bc43ef6c53d464f364b7799ac9049ddd9
Bernd Edlinger <bernd.edlinger@hotmail.de>
2025-01-21 16:12:18 +0100

    Give DTLS tests more time to complete
    Increase the timeout for DTLS tests to 10 seconds.
    But do that only for DTLS as this would waste time
    for other tests, most of the TLS tests do not need
    this at all.
    
    Fixes #26491
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/26502)

--- util/perl/TLSProxy/Proxy.pm
+++ util/perl/TLSProxy/Proxy.pm
@@ -293,7 +293,7 @@ sub start
     open(my $savedin, "<&STDIN");
 
     # Temporarily replace STDIN so that sink process can inherit it...
-    open(STDIN, "$^X -e 'sleep(1)' |");
+    open(STDIN, "$^X -e 'sleep(10)' |") if $self->{isdtls};
     $pid = open(STDIN, "$execcmd 2>&1 |") or die "Failed to $execcmd: $!\n";
     $self->{real_serverpid} = $pid;
 
