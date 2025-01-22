09879c5c1c0d55ef13ae1ac3de74aa20e742ebfb
Rajeev Ranjan <ranjan.rajeev@siemens.com>
2025-01-22 16:25:45 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! CMP: add documentation

--- crypto/crmf/crmf_lib.c
+++ crypto/crmf/crmf_lib.c
@@ -716,15 +716,14 @@ EVP_PKEY *OSSL_CRMF_ENCRYPTEDKEY_get1_pkey(const OSSL_CRMF_ENCRYPTEDKEY *encrypt
     if (sd == NULL)
         goto end;
 
-    purpose_id = X509_PURPOSE_get_unused_id(libctx);
-
-    if (X509_PURPOSE_get_by_sname(SN_cmKGA) < 0
-        && !X509_PURPOSE_add(purpose_id, X509_TRUST_COMPAT, 0, check_cmKGA,
-                             LN_cmKGA, SN_cmKGA, NULL)) {
-        ERR_raise(ERR_LIB_CRMF, CRMF_R_ERROR_SETTING_PURPOSE);
-        goto end;
+    if ((purpose_id = X509_PURPOSE_get_by_sname(SN_cmKGA)) < 0) {
+        purpose_id = X509_PURPOSE_get_unused_id(libctx);
+        if (!X509_PURPOSE_add(purpose_id, X509_TRUST_COMPAT, 0, check_cmKGA,
+                              LN_cmKGA, SN_cmKGA, NULL)) {
+            ERR_raise(ERR_LIB_CRMF, CRMF_R_ERROR_SETTING_PURPOSE);
+            goto end;
+        }
     }
-
     if ((vpm = X509_STORE_get0_param(ts)) == NULL)
         goto end;
 
