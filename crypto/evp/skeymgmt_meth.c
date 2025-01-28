de5c8c64932b2b2f5af7f01cc320d45178d5ab4e
Simo Sorce <simo@redhat.com>
2025-01-28 13:06:28 -0500

    Add skeymgmt helper to fetch from a provider
    This allows to fetch efficiently directly from the same provider that can
    handle the EVP_SKEY at hand.
    
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- crypto/evp/skeymgmt_meth.c
+++ crypto/evp/skeymgmt_meth.c
@@ -122,6 +122,18 @@ static void *skeymgmt_from_algorithm(int name_id,
     return skeymgmt;
 }
 
+EVP_SKEYMGMT *evp_skeymgmt_fetch_from_prov(OSSL_PROVIDER *prov,
+                                           const char *name,
+                                           const char *properties)
+{
+    return evp_generic_fetch_from_prov(prov,
+                                       OSSL_OP_SKEYMGMT,
+                                       name, properties,
+                                       skeymgmt_from_algorithm,
+                                       (int (*)(void *))EVP_SKEYMGMT_up_ref,
+                                       (void (*)(void *))EVP_SKEYMGMT_free);
+}
+
 EVP_SKEYMGMT *EVP_SKEYMGMT_fetch(OSSL_LIB_CTX *ctx, const char *algorithm,
                                  const char *properties)
 {
