a60caee146114bacd3935ae47ef5dc1f0e942824
Neil Horman <nhorman@openssl.org>
2025-01-29 14:19:35 -0500

    Move quictestlib TODO to QUIC FUTURE
    Improving handling of packets in tserver doesn't currently make sense,
    as we're planning on eliminating it soon.  Move this TODO to QUIC FUTURE
    
    Fixes openssl/project#1070
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Saša Nedvědický <sashan@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/26593)

--- test/helpers/quictestlib.c
+++ test/helpers/quictestlib.c
@@ -264,13 +264,7 @@ int qtest_create_quic_objects(OSSL_LIB_CTX *libctx, SSL_CTX *clientctx,
                                       0, &now_cb), 1))
                 goto err;
         }
-        /*
-         * TODO(QUIC SERVER):
-         *    Currently the simplistic handler of the quic tserver cannot cope
-         *    with noise introduced in the first packet received from the
-         *    client. This needs to be removed once we have proper server side
-         *    handling.
-         */
+
         (void)BIO_ctrl(sbio, BIO_CTRL_NOISE_BACK_OFF, 0, NULL);
 
         (*fault)->noiseargs.cbio = cbio;
