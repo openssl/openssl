d24638e19d617a0149875704f5374322c0f26590
Neil Horman <nhorman@openssl.org>
2025-01-29 14:44:13 -0500

    Move handling of connection close frames to QUIC FUTURE
    There is a corner case in handling connection close frames for which RFC
    guidance is unclear.  Given that, move addressing it to QUIC FUTURE
    
    Fixes openssl/project#1075
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Saša Nedvědický <sashan@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/26593)

--- ssl/quic/quic_txp.c
+++ ssl/quic/quic_txp.c
@@ -1469,7 +1469,8 @@ static int txp_should_try_staging(OSSL_QUIC_TX_PACKETISER *txp,
      * This is not a major concern for clients, since if a client has a 1-RTT EL
      * provisioned the server is guaranteed to also have a 1-RTT EL provisioned.
      *
-     * TODO(QUIC SERVER): Revisit this when server support is added.
+     * TODO(QUIC FUTURE): Revisit this when when have reached a decision on how
+     * best to implement this
      */
     if (*conn_close_enc_level > enc_level
         && *conn_close_enc_level != QUIC_ENC_LEVEL_1RTT)
