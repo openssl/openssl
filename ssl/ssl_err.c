0d2122d8d5486fc61c0b043366ee09da5b0edbc3
Neil Horman <nhorman@openssl.org>
2025-02-14 15:46:55 -0500

    fixup! QUIC APL: Rework QCTX to support listener calls

--- ssl/ssl_err.c
+++ ssl/ssl_err.c
@@ -250,7 +250,7 @@ static const ERR_STRING_DATA SSL_str_reasons[] = {
     {ERR_PACK(ERR_LIB_SSL, 0, SSL_R_LENGTH_TOO_SHORT), "length too short"},
     {ERR_PACK(ERR_LIB_SSL, 0, SSL_R_LIBRARY_BUG), "library bug"},
     {ERR_PACK(ERR_LIB_SSL, 0, SSL_R_LIBRARY_HAS_NO_CIPHERS),
-    "library has no ciphers"},
+     "library has no ciphers"},
     {ERR_PACK(ERR_LIB_SSL, 0, SSL_R_LISTENER_USE_ONLY), "listener use only"},
     {ERR_PACK(ERR_LIB_SSL, 0, SSL_R_MAXIMUM_ENCRYPTED_PKTS_REACHED),
      "maximum encrypted pkts reached"},
