1e70fd4c50273d9b9fe0385e726c56779c099fb7
Viktor Dukhovni <openssl-users@dukhovni.org>
2025-02-22 22:28:57 +1100

    fixup! More seed and private key checks for ML-DSA
    Drop duplicate declarations

--- include/crypto/ml_dsa.h
+++ include/crypto/ml_dsa.h
@@ -106,10 +106,6 @@ __owur int ossl_ml_dsa_key_public_from_private(ML_DSA_KEY *key);
 __owur int ossl_ml_dsa_pk_decode(ML_DSA_KEY *key, const uint8_t *in, size_t in_len);
 __owur int ossl_ml_dsa_sk_decode(ML_DSA_KEY *key, const uint8_t *in, size_t in_len);
 
-__owur int ossl_ml_dsa_key_public_from_private(ML_DSA_KEY *key);
-__owur int ossl_ml_dsa_pk_decode(ML_DSA_KEY *key, const uint8_t *in, size_t in_len);
-__owur int ossl_ml_dsa_sk_decode(ML_DSA_KEY *key, const uint8_t *in, size_t in_len);
-
 __owur int ossl_ml_dsa_sign(const ML_DSA_KEY *priv, int msg_is_mu,
                             const uint8_t *msg, size_t msg_len,
                             const uint8_t *context, size_t context_len,
