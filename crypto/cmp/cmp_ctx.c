7c567e022cfba5c27cb7190074c5ae765fc5508c
Dr. David von Oheimb <dev@ddvo.net>
2025-02-26 18:49:58 +0100

    OSSL_CMP_CTX_{set,get}_option(): improve error diagnostics

--- crypto/cmp/cmp_ctx.c
+++ crypto/cmp/cmp_ctx.c
@@ -968,7 +968,7 @@ DEFINE_set1_ASN1_OCTET_STRING(OSSL_CMP_CTX, transactionID)
         ctx->revocationReason = val;
         break;
     default:
-        ERR_raise(ERR_LIB_CMP, CMP_R_INVALID_OPTION);
+        ERR_raise_data(ERR_LIB_CMP, CMP_R_INVALID_OPTION, "%d", opt);
         return 0;
     }
 
@@ -1030,7 +1030,7 @@ int OSSL_CMP_CTX_get_option(const OSSL_CMP_CTX *ctx, int opt)
     case OSSL_CMP_OPT_REVOCATION_REASON:
         return ctx->revocationReason;
     default:
-        ERR_raise(ERR_LIB_CMP, CMP_R_INVALID_OPTION);
+        ERR_raise_data(ERR_LIB_CMP, CMP_R_INVALID_OPTION, "%d", opt);
         return -1;
     }
 }
