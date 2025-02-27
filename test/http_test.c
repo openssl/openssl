285ff8d5ed5c138cd73adbc43e3b67778fd3eae1
Jean-Frederic Clere <jfclere@gmail.com>
2025-02-27 17:35:05 +0100

    fix snprintf by BIO_snprintf

--- test/http_test.c
+++ test/http_test.c
@@ -159,7 +159,7 @@ static int test_http_method(int do_get, int do_txt, int suggested_status)
     int res = 0;
     int real_server = do_txt && 0; /* remove "&& 0" for using real server */
 
-    snprintf(path, sizeof(path), "/%d%s", suggested_status,
+    BIO_snprintf(path, sizeof(path), "/%d%s", suggested_status,
              do_get > 1 ? "/will-be-redirected" : RPATH);
     if (do_txt) {
         content_type = "text/plain";
