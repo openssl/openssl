24a907f6d08e2623bfe3b1550a19354bc54ff8c4
Adrian Stanciu <adrian.stanciu@intel.com>
2025-02-27 10:59:36 +0200

    Extract AES CFB implementation to cipher_aes_cfb*

--- providers/implementations/ciphers/cipher_aes.h
+++ providers/implementations/ciphers/cipher_aes.h
@@ -51,11 +51,7 @@ typedef struct prov_aes_ctx_st {
 } PROV_AES_CTX;
 
 #define ossl_prov_cipher_hw_aes_ofb ossl_prov_cipher_hw_aes_ofb128
-#define ossl_prov_cipher_hw_aes_cfb ossl_prov_cipher_hw_aes_cfb128
 const PROV_CIPHER_HW *ossl_prov_cipher_hw_aes_ecb(size_t keybits);
 const PROV_CIPHER_HW *ossl_prov_cipher_hw_aes_cbc(size_t keybits);
 const PROV_CIPHER_HW *ossl_prov_cipher_hw_aes_ofb128(size_t keybits);
-const PROV_CIPHER_HW *ossl_prov_cipher_hw_aes_cfb128(size_t keybits);
-const PROV_CIPHER_HW *ossl_prov_cipher_hw_aes_cfb1(size_t keybits);
-const PROV_CIPHER_HW *ossl_prov_cipher_hw_aes_cfb8(size_t keybits);
 const PROV_CIPHER_HW *ossl_prov_cipher_hw_aes_ctr(size_t keybits);
