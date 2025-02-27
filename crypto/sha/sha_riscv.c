a41f9135f0824a0ff81e2e6b59158ee5dd529662
Julian Zhu <julian.oerv@isrc.iscas.ac.cn>
2025-02-27 17:10:18 +0800

    RISC-V: Provide optimized SHA-512 implementation using Zbb extension
    Signed-off-by: Julian Zhu <julian.oerv@isrc.iscas.ac.cn>
    
    Reviewed-by: Paul Dale <ppzgs1@gmail.com>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/27161)

--- crypto/sha/sha_riscv.c
+++ crypto/sha/sha_riscv.c
@@ -30,6 +30,7 @@ void sha256_block_data_order(SHA256_CTX *ctx, const void *in, size_t num)
 }
 
 void sha512_block_data_order_zvkb_zvknhb(void *ctx, const void *in, size_t num);
+void sha512_block_data_order_zbb(void *ctx, const void *in, size_t num);
 void sha512_block_data_order_c(void *ctx, const void *in, size_t num);
 void sha512_block_data_order(SHA512_CTX *ctx, const void *in, size_t num);
 
@@ -37,6 +38,8 @@ void sha512_block_data_order(SHA512_CTX *ctx, const void *in, size_t num)
 {
     if (RISCV_HAS_ZVKB_AND_ZVKNHB() && riscv_vlen() >= 128) {
         sha512_block_data_order_zvkb_zvknhb(ctx, in, num);
+    } else if (RISCV_HAS_ZBB()) {
+        sha512_block_data_order_zbb(ctx, in, num);
     } else {
         sha512_block_data_order_c(ctx, in, num);
     }
