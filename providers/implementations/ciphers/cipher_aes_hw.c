f0c0d2979c2a5f461aa54ddcd4622440bd4ed2a2
Adrian Stanciu <adrian.stanciu@intel.com>
2025-02-27 10:59:36 +0200

    Extract AES CFB implementation to cipher_aes_cfb*

--- providers/implementations/ciphers/cipher_aes_hw.c
+++ providers/implementations/ciphers/cipher_aes_hw.c
@@ -146,7 +146,7 @@ const PROV_CIPHER_HW *ossl_prov_cipher_hw_aes_##mode(size_t keybits)           \
 # include "cipher_aes_hw_rv64i.inc"
 #elif defined(OPENSSL_CPUID_OBJ) && defined(__riscv) && __riscv_xlen == 32
 # include "cipher_aes_hw_rv32i.inc"
-#elif defined (ARMv8_HWAES_CAPABLE)
+#elif defined(ARMv8_HWAES_CAPABLE)
 # include "cipher_aes_hw_armv8.inc"
 #else
 /* The generic case */
@@ -157,7 +157,4 @@ const PROV_CIPHER_HW *ossl_prov_cipher_hw_aes_##mode(size_t keybits)           \
 PROV_CIPHER_HW_aes_mode(cbc)
 PROV_CIPHER_HW_aes_mode(ecb)
 PROV_CIPHER_HW_aes_mode(ofb128)
-PROV_CIPHER_HW_aes_mode(cfb128)
-PROV_CIPHER_HW_aes_mode(cfb1)
-PROV_CIPHER_HW_aes_mode(cfb8)
 PROV_CIPHER_HW_aes_mode(ctr)
