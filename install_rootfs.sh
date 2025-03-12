8c2e8b60baf87a341457d132734bdf01e9893c9c
Chen Linfei <linfei.chen@spacemit.com>
2025-03-12 14:07:19 +0800

    fix install_rootfs.sh failed
    Change-Id: I27d9aa733b00c162e9dc0026b5426fdc4b1a5c4e
    (cherry picked from commit a52d60479b763628977166f4a3db9e848469c0cf)

--- install_rootfs.sh
+++ install_rootfs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 TARGET_DIR=$1
-mkdir -p $TARGET_DIR
+mkdir -p $TARGET_DIR/usr/bin
+mkdir -p $TARGET_DIR/usr/lib
 cp -rf apps/openssl  $TARGET_DIR/usr/bin/openssl
 cp -rf  libssl.so $TARGET_DIR/usr/lib/
 cp -rf  libssl.so.3 $TARGET_DIR/usr/lib/
