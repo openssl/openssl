19bd73327c7f5b6740a00b3164a423a61681f074
Nicolas Blais-Miko <n.blaismiko@gmail.com>
2025-03-22 08:14:50 -0400

    Fix silent error in EVP_CIPHER_CTX_get_updated_iv.
    Added new params API function OSSL_PARAM_set_octet_string_or_ptr to only
    call the correct setter for OSSL_CIPHER_PARAM_IV and OSSL_CIPHER_PARAM_UPDATED_IV.
    Both OSSL_PARAM_set_octet_string and OSSL_PARAM_set_octet_ptr could be called with
    only one expected to succeed. This would put a silent error on the error stack when
    calling EVP_CIPHER_CTX_get_updated_iv.
    
    Fixes #27117
    
    Reviewed-by: Paul Dale <ppzgs1@gmail.com>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/27120)

--- providers/implementations/ciphers/ciphercommon_gcm.c
+++ providers/implementations/ciphers/ciphercommon_gcm.c
@@ -187,8 +187,7 @@ int ossl_gcm_get_ctx_params(void *vctx, OSSL_PARAM params[])
                 ERR_raise(ERR_LIB_PROV, PROV_R_INVALID_IV_LENGTH);
                 return 0;
             }
-            if (!OSSL_PARAM_set_octet_string(p, ctx->iv, ctx->ivlen)
-                && !OSSL_PARAM_set_octet_ptr(p, &ctx->iv, ctx->ivlen)) {
+            if (!OSSL_PARAM_set_octet_string_or_ptr(p, ctx->iv, ctx->ivlen)) {
                 ERR_raise(ERR_LIB_PROV, PROV_R_FAILED_TO_SET_PARAMETER);
                 return 0;
             }
@@ -201,8 +200,7 @@ int ossl_gcm_get_ctx_params(void *vctx, OSSL_PARAM params[])
                 ERR_raise(ERR_LIB_PROV, PROV_R_INVALID_IV_LENGTH);
                 return 0;
             }
-            if (!OSSL_PARAM_set_octet_string(p, ctx->iv, ctx->ivlen)
-                && !OSSL_PARAM_set_octet_ptr(p, &ctx->iv, ctx->ivlen)) {
+            if (!OSSL_PARAM_set_octet_string_or_ptr(p, ctx->iv, ctx->ivlen)) {
                 ERR_raise(ERR_LIB_PROV, PROV_R_FAILED_TO_SET_PARAMETER);
                 return 0;
             }
