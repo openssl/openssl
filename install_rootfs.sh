69616844312aee84d6c2002b3a6195aa2fc6817c
Chen Linfei <linfei.chen@spacemit.com>
2025-03-30 14:56:20 +0800

    fix install_rootfs.sh failed
    Change-Id: Ic1dc44a75fbddbad720ef12a6019676005de7996
    (cherry picked from commit 47bf7256dcc89009feaaa2035f3ad02fab935d2d)

--- install_rootfs.sh
+++ install_rootfs.sh
@@ -9,5 +9,4 @@ cp -rf libssl.so.3 $TARGET_DIR/usr/lib/
 cp -rf libcrypto.so $TARGET_DIR/usr/lib/
 cp -rf libcrypto.so.3 $TARGET_DIR/usr/lib/
 cp -rf _run_stress.sh $TARGET_DIR/usr/bin/openssl_stress
-cp -rf check_run_stress_error.sh $TARGET_DIR/usr/bin/openssl_stress
 cp -rf run_stress.sh $TARGET_DIR/usr/bin/openssl_stress
