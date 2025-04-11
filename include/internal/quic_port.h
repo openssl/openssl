047ef2fb884a6f587b3e63104cdcead01ae9e706
Neil Horman <nhorman@openssl.org>
2025-04-11 14:52:42 -0400

    Add flag to determine how quic accepts connections
    The flag defaults to 0 (unknown), and gets set to
    1 (using SSL_accept_ex), or -1 (using SSL_accpet_connection)

--- include/internal/quic_port.h
+++ include/internal/quic_port.h
@@ -159,6 +159,11 @@ size_t ossl_quic_port_get_num_incoming_channels(const QUIC_PORT *port);
 /* Sets if incoming connections should currently be allowed. */
 void ossl_quic_port_set_allow_incoming(QUIC_PORT *port, int allow_incoming);
 
+/* Sets flag to indicate we are using SSL_listen_ex to get connections */
+void ossl_quic_port_set_using_peeloff(QUIC_PORT *port, int using_peeloff);
+
+int ossl_quic_port_get_using_peeloff(QUIC_PORT *port);
+
 /* Returns 1 if we are using addressed mode on the read side. */
 int ossl_quic_port_is_addressed_r(const QUIC_PORT *port);
 
