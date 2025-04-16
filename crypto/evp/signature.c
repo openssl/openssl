59d581fa4fba84a5eb473853e8f9f070791a7c38
Dr. David von Oheimb <dev@ddvo.net>
2025-04-16 06:20:23 +0200

    crypto/evp/signature.c: add checks for consistent presence of 'update' and 'final' functions

--- crypto/evp/signature.c
+++ crypto/evp/signature.c
@@ -421,6 +421,35 @@ static void *evp_signature_from_algorithm(int name_id,
         goto err;
     }
 
+    if ((signature->sign_message_update == NULL) !=
+        (signature->sign_message_final == NULL)) {
+        ERR_raise_data(ERR_LIB_EVP, EVP_R_INVALID_PROVIDER_FUNCTIONS,
+                       "inconsistent presence of %s message signing update and final:%s",
+                       signature->type_name, desc);
+        goto err;
+    }
+    if ((signature->verify_message_update == NULL) !=
+        (signature->verify_message_final == NULL)) {
+        ERR_raise_data(ERR_LIB_EVP, EVP_R_INVALID_PROVIDER_FUNCTIONS,
+                       "inconsistent presence of %s message verification update and final:%s",
+                       signature->type_name, desc);
+        goto err;
+    }
+    if ((signature->digest_sign_update == NULL) !=
+        (signature->digest_sign_final == NULL)) {
+        ERR_raise_data(ERR_LIB_EVP, EVP_R_INVALID_PROVIDER_FUNCTIONS,
+                       "inconsistent presence of %s digest signing update and final:%s",
+                       signature->type_name, desc);
+        goto err;
+    }
+    if ((signature->digest_verify_update == NULL) !=
+        (signature->digest_verify_final == NULL)) {
+        ERR_raise_data(ERR_LIB_EVP, EVP_R_INVALID_PROVIDER_FUNCTIONS,
+                       "inconsistent presence of %s digest verification update and final:%s",
+                       signature->type_name, desc);
+        goto err;
+    }
+
     return signature;
  err:
     EVP_SIGNATURE_free(signature);
