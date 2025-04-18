5bb2abc26af2e043e34079807166c16e1f5dea08
sashan <anedvedicky@gmail.com>
2025-04-18 12:09:52 +0200

    ossl_json_f64() seems to be unused, remove it to avoid libm dependency
    Reviewed-by: Paul Dale <ppzgs1@gmail.com>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/27434)
    
    (cherry picked from commit 0e418628998d0337599643c1e4054c60e5f21ec4)

--- ssl/quic/json_enc.c
+++ ssl/quic/json_enc.c
@@ -11,7 +11,6 @@
 #include "internal/nelem.h"
 #include "internal/numbers.h"
 #include <string.h>
-#include <math.h>
 
 /*
  * wbuf
@@ -593,33 +592,6 @@ void ossl_json_i64(OSSL_JSON_ENC *json, int64_t value)
 
     if (quote && !ossl_json_in_error(json))
         json_write_char(json, '"');
-}
-
-/* Encode a JSON number from a 64-bit floating point value. */
-void ossl_json_f64(OSSL_JSON_ENC *json, double value)
-{
-    char buf[32];
-
-    if (!json_pre_item(json))
-        return;
-
-#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
-    {
-        int checks = isnan(value);
-# if !defined(OPENSSL_SYS_VMS)
-        checks |= isinf(value);
-# endif
-
-        if (checks) {
-            json_raise_error(json);
-            return;
-        }
-    }
-#endif
-
-    BIO_snprintf(buf, sizeof(buf), "%1.17g", value);
-    json_write_str(json, buf);
-    json_post_item(json);
 }
 
 /*
