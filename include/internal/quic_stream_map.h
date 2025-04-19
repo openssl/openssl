5c8b78c5c074f83d402657f8d199d5f674a016a2
sashan <anedvedicky@gmail.com>
2025-04-19 15:26:20 +0200

    let underlying stream object know connection is being closed
    the proposed change attempts to fix following edge-case.
    
    local QUIC connection end-point receives connection close
    notification from remote peer while QUIC streams still exists.
    In my opinion QUIC stack should signal error condition to
    all existing streams as soon as there connection is going to
    be closed.
    
    Change in this PR makes that working for non-blocking I/O
    which uses SSL_poll(). Whenever the SSL_poll() triggers _EC
    event on QUIC connection it will also make sure the error
    will be triggered on all stream objects which belong to
    connection.
    
    There two events which notify application about QUIC
    connection termination:
      - SSL_POLL_EVENT_EC - comes first and tells connection is about
        to terminate
      - SSL_POLL_EVENT_ECD - comes second after this the connection is gone
    
    The errors on streams are signaled with SSL_POLL_EVENT_EC. The application
    is told the error on stream objects is permanent. Application should just free
    stream objects.

--- include/internal/quic_stream_map.h
+++ include/internal/quic_stream_map.h
@@ -314,6 +314,10 @@ struct quic_stream_st {
     unsigned int    ready_for_gc            : 1;
     /* Set to 1 if this is currently counted in the shutdown flush stream count. */
     unsigned int    shutdown_flush          : 1;
+    /* set when underlying connection signals EC, the connection is being closed
+     * by removte peer, while there are still streams hanging around
+     */
+    unsigned int    conn_tearing_down       : 1;
 };
 
 #define QUIC_STREAM_INITIATOR_CLIENT        0
