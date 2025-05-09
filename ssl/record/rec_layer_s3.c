86e06fd775114e0a2fa8583e794007069099d674
Matt Caswell <matt@openssl.org>
2025-05-09 09:58:15 +0100

    Fix DTLS handling when receiving a no_renegotiation alert
    no_renegotiation is a warning alert sent from the server when it is not
    prepared to accept a renegotiation attempt. In TLS we abort the connection
    when we receive one of these - which is a reasonable response. However,
    in DTLS we incorrectly ignore this and keep trying to renegotiate.
    
    We bring the DTLS handling of a no_renegotiation alert into line with
    how TLS handles this. In versions prior to 3.2 handling of a warning
    alert in DTLS was mishandled resulting in a failure of the connection,
    which ends up being the right thing to do "by accident" in the case of
    "no_renegotiation". From 3.2 this mishandling was fixed, but exposed this
    latent bug.
    
    Fixes #27419
    
    Reviewed-by: Frederik Wedel-Heinen <fwh.openssl@gmail.com>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/27591)
    
    (cherry picked from commit e5feca0659ef6119f6cedfab1b6af034735723ff)

--- ssl/record/rec_layer_s3.c
+++ ssl/record/rec_layer_s3.c
@@ -1613,10 +1613,10 @@ int ssl3_read_bytes(SSL *s, int type, int *recvd_type, unsigned char *buf,
             /*
              * This is a warning but we receive it if we requested
              * renegotiation and the peer denied it. Terminate with a fatal
-             * alert because if application tried to renegotiate it
+             * alert because if the application tried to renegotiate it
              * presumably had a good reason and expects it to succeed. In
-             * future we might have a renegotiation where we don't care if
-             * the peer refused it where we carry on.
+             * the future we might have a renegotiation where we don't care
+             * if the peer refused it where we carry on.
              */
             SSLfatal(s, SSL_AD_HANDSHAKE_FAILURE, SSL_R_NO_RENEGOTIATION);
             return -1;
