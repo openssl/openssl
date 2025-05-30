80a968fa763647fdc20b70fbcb25bfc6c6ad02f9
Norbert Pocs <norbertp@openssl.org>
2025-05-30 10:26:21 +0200

    DRAFT: deprecate tests
    Signed-off-by: Norbert Pocs <norbertp@openssl.org>

--- test/helpers/ssl_test_ctx.c
+++ test/helpers/ssl_test_ctx.c
@@ -519,15 +519,21 @@ const char *ssl_max_fragment_len_name(int MFL_mode)
 __owur static int parse_expected_key_type(int *ptype, const char *value)
 {
     int nid;
+#ifndef OPENSSL_NO_DEPRECATED_3_6
     const EVP_PKEY_ASN1_METHOD *ameth;
+#endif
 
     if (value == NULL)
         return 0;
+#ifndef OPENSSL_NO_DEPRECATED_3_6
     ameth = EVP_PKEY_asn1_find_str(NULL, value, -1);
     if (ameth != NULL)
         EVP_PKEY_asn1_get0_info(&nid, NULL, NULL, NULL, NULL, ameth);
     else
         nid = OBJ_sn2nid(value);
+#else
+    nid = OBJ_sn2nid(value);
+#endif
     if (nid == NID_undef)
         nid = OBJ_ln2nid(value);
 #ifndef OPENSSL_NO_EC
