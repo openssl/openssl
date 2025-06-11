768703277f6aec1536a15344a3fb243c16927d5e
Matt Caswell <matt@openssl.org>
2025-06-11 09:50:16 +0100

    Allow our *_gen_cleanup functions to tolerate a NULL ctx
    Our *_gen_cleanup functions are essentially "free" functions. Our
    free functions tolerate NULL being passed. We are being inconsistent with
    our *_gen_cleanup functions. Some of them tolerate NULL and others do not.
    
    We should consistently tolerate NULL.
    
    See also #27795
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Tim Hudson <tjh@openssl.org>
    Reviewed-by: Paul Dale <ppzgs1@gmail.com>
    Reviewed-by: Neil Horman <nhorman@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/27813)
    
    (cherry picked from commit 743bae4a225b9df1d11446e5f0620270b10c835a)

--- providers/implementations/keymgmt/ecx_kmgmt.c
+++ providers/implementations/keymgmt/ecx_kmgmt.c
@@ -696,6 +696,9 @@ static void ecx_gen_cleanup(void *genctx)
 {
     struct ecx_gen_ctx *gctx = genctx;
 
+    if (gctx == NULL)
+        return;
+
     OPENSSL_free(gctx->propq);
     OPENSSL_free(gctx);
 }
