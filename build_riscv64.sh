a872c57b0955e777e6873cdbe72c3d42cdd07488
minghui.zhang <minghui.zhang@spacemit.com>
2025-06-20 14:02:05 +0800

    modified build_riscv64.sh .gitignore install_rootfs.sh to make sure that build files are organized
    Change-Id: I718f280c0bcb69941a3fdaf52c5c651e90967097
    (cherry picked from commit 81dc05133ba984a8cdb73bd314b4d06fadc9a4bc)

--- build_riscv64.sh
+++ build_riscv64.sh
@@ -1,3 +1,6 @@
 make clean
-./Configure linux64-riscv64
+rm -rf build 
+mkdir -p build
+./Configure linux64-riscv64 --prefix=$(pwd)/build
 make CC=riscv64-unknown-linux-gnu-gcc CXX=riscv64-unknown-linux-gnu-g++  -j9
+make install 
--- install_rootfs.sh
+++ install_rootfs.sh
@@ -3,10 +3,10 @@ TARGET_DIR=$1
 mkdir -p $TARGET_DIR/usr/bin
 mkdir -p $TARGET_DIR/usr/bin/openssl_stress
 mkdir -p $TARGET_DIR/usr/lib
-cp -rf apps/openssl  $TARGET_DIR/usr/bin/openssl
-cp -rf libssl.so $TARGET_DIR/usr/lib/
-cp -rf libssl.so.3 $TARGET_DIR/usr/lib/
-cp -rf libcrypto.so $TARGET_DIR/usr/lib/
-cp -rf libcrypto.so.3 $TARGET_DIR/usr/lib/
+cp -rf build/bin/openssl  $TARGET_DIR/usr/bin/openssl
+cp -rf build/lib/libssl.so $TARGET_DIR/usr/lib/
+cp -rf build/lib/libssl.so.3 $TARGET_DIR/usr/lib/
+cp -rf build/lib/libcrypto.so $TARGET_DIR/usr/lib/
+cp -rf build/lib/libcrypto.so.3 $TARGET_DIR/usr/lib/
 cp -rf _run_stress.sh $TARGET_DIR/usr/bin/openssl_stress
 cp -rf run_stress.sh $TARGET_DIR/usr/bin/openssl_stress
