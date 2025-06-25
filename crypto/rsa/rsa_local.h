902568bbd98a61d03495498b56e95e67e42cc71b
Neil Horman <nhorman@openssl.org>
2025-06-25 14:26:24 -0400

    Remove need for BN_BLINDING lock
    Issue https://github.com/openssl/project/issues/1245 has identified that
    we encounter a significant amount of time waiting to acquire the
    BN_BLINDING_lock when running our handshake perf test with 10 threads
    using an rsa key.  Specifically, with 10 threads we spend about 19327731
    usecs just waiting.  So it would be great if we could eliminate the need
    to get the write lock here.
    
    Currently, the need for the lock is based off the fact that each rsa key
    has only a single blinding pointer, for which exclusive access is
    needed, with an attempt to use a fallback mt_blinding pointer in the
    shared case.  If a key is shared by many threads, then we find ourselves
    needing to maniuplate this lock quite frequently if we are doing lots of
    ssl connections.
    
    To address this, I've come up with this approach.  It replaces the
    blinding pointer with a pointer to a sparse array.  The sparse array is
    then indexed by thread id.  This allows us to do two things:
    
    When getting the blinding, we only need to take the read lock in the
    common case when looking up this threads blinding structure.  Only in
    the first lookup for any thread do we need to take the write side lock
    when updating the table, and only then for a very brief critical section
    (i.e. we don't need to hold the lock when allocating/setting the
    blinding up via RSA_setup_blinding
    
    This trades off some extra memory usage for the above significant
    reduction in execution time.
    
    it also allows us to simplify the blinding code quite a bit by
    eliminating the need to handle shared blindings because blindings are
    never shared anymore
    
    Fixes openssl/project#1245
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/27913)

--- crypto/rsa/rsa_local.h
+++ crypto/rsa/rsa_local.h
@@ -93,8 +93,7 @@ struct rsa_st {
     BN_MONT_CTX *_method_mod_n;
     BN_MONT_CTX *_method_mod_p;
     BN_MONT_CTX *_method_mod_q;
-    BN_BLINDING *blinding;
-    BN_BLINDING *mt_blinding;
+    void *blindings_sa;
     CRYPTO_RWLOCK *lock;
 
     int dirty_cnt;
@@ -196,5 +195,7 @@ int ossl_rsa_fips186_4_gen_prob_primes(RSA *rsa, RSA_ACVP_TEST *test,
 int ossl_rsa_padding_add_PKCS1_type_2_ex(OSSL_LIB_CTX *libctx, unsigned char *to,
                                          int tlen, const unsigned char *from,
                                          int flen);
+void ossl_rsa_free_blinding(RSA *rsa);
+void *ossl_rsa_alloc_blinding(void);
 
 #endif /* OSSL_CRYPTO_RSA_LOCAL_H */
