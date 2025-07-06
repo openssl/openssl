f1e3cd0620103a8c5177e4832a6d2c4ee020011a
Andrew Dinh <andrewd@openssl.org>
2025-07-06 23:33:14 +0700

    fixup! Address Coverity issues 1655235 and 1655233

--- crypto/bio/bss_mem.c
+++ crypto/bio/bss_mem.c
@@ -283,7 +283,7 @@ static long mem_ctrl(BIO *b, int cmd, long num, void *ptr)
         off = (ossl_ssize_t)num;
         /* FALLTHRU */
     case BIO_C_FILE_TELL:
-        ret = off;
+        ret = (long)off;
         break;
     case BIO_CTRL_EOF:
         ret = (long)(bm->length == 0);
