cde6b765b31b8e72f32a292046e2aede9c1198c2
Eugene Syromiatnikov <esyr@openssl.org>
2025-07-07 15:15:44 +0200

    crypto/threads_lock_contention: condition file suffix on FIPS_MODULE and not fopen() call
    Signed-off-by: Eugene Syromiatnikov <esyr@openssl.org>

--- crypto/threads_pthread.c
+++ crypto/threads_pthread.c
@@ -637,13 +637,14 @@ struct stack_traces {
     struct stack_info stacks[STACKS_COUNT];
 };
 
+#  ifdef FIPS_MODULE
+#   define FIPS_SFX "-fips"
+#  else
+#   define FIPS_SFX ""
+#  endif
 static void init_contention_fp_once(void)
 {
-#  ifdef FIPS_MODULE
-    contention_fp = fopen("lock-contention-log-fips.txt", "w");
-#  else
-    contention_fp = fopen("lock-contention-log.txt", "w");
-#  endif
+    contention_fp = fopen("lock-contention-log" FIPS_SFX ".txt", "w");
     if (contention_fp == NULL)
         fprintf(stderr, "Contention log file could not be opened, log will not be recorded\n");
 
