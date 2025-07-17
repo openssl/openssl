fa9b7b930e3e59f5b30de0e8a6755bfaafdd5c49
Eugene Syromiatnikov <esyr@openssl.org>
2025-07-17 03:32:02 +0200

    Add array memory allocation routines
    Such routines allow alleviating the need to perform explicit integer
    overflow check during allocation size calculation and generally make
    the allocations more semantic (as they signify that a collection
    of NUM items, each occupying SIZE bytes is being allocated), which paves
    the road for additional correctness checks in the future.
    
    Signed-off-by: Eugene Syromiatnikov <esyr@openssl.org>
    
    Reviewed-by: Saša Nedvědický <sashan@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Paul Dale <ppzgs1@gmail.com>
    Reviewed-by: Neil Horman <nhorman@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/28059)

--- include/internal/mem_alloc_utils.h
+++ include/internal/mem_alloc_utils.h
@@ -14,10 +14,17 @@
 #ifndef OSSL_INTERNAL_CHECK_SIZE_OVERFLOW_H
 # define OSSL_INTERNAL_CHECK_SIZE_OVERFLOW_H
 
+# include <limits.h>
+# include <stdbool.h>
+# include <stdint.h>
+
 # include "internal/common.h"
+# include "internal/safe_math.h"
 
 # include <openssl/cryptoerr.h>
 # include <openssl/err.h>
+
+OSSL_SAFE_MATH_UNSIGNED(size_t, size_t)
 
 /*
  * A helper routine to report memory allocation errors.
@@ -41,10 +48,38 @@ ossl_report_alloc_err_ex(const char * const file, const int line,
 }
 
 /* Report a memory allocation failure. */
-static inline void
+static ossl_inline ossl_unused void
 ossl_report_alloc_err(const char * const file, const int line)
 {
     ossl_report_alloc_err_ex(file, line, ERR_R_MALLOC_FAILURE);
+}
+
+/* Report an integer overflow during allocation size calculation. */
+static ossl_inline ossl_unused void
+ossl_report_alloc_err_of(const char * const file, const int line)
+{
+    ossl_report_alloc_err_ex(file, line, CRYPTO_R_INTEGER_OVERFLOW);
+}
+
+/*
+ * Check the result of num and size multiplication for overflow
+ * and set error if it is the case;  return true if there was no overflow,
+ * false if there was.
+ */
+static ossl_inline ossl_unused bool
+ossl_size_mul(const size_t num, const size_t size, size_t *bytes,
+              const char * const file, const int line)
+{
+    int err = 0;
+    *bytes = safe_mul_size_t(num, size, &err);
+
+    if (ossl_unlikely(err != 0)) {
+        ossl_report_alloc_err_of(file, line);
+
+        return false;
+    }
+
+    return true;
 }
 
 #endif /* OSSL_INTERNAL_CHECK_SIZE_OVERFLOW_H */
