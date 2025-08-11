fc563b4d48e2229f6d52a95ff32a527fa983ef68
Leon Timmermans <fawaka@gmail.com>
2025-08-11 22:25:59 +0200

    Allow get_params to return length of the AES-GCM tag parameter
    Previously, EVP_CIPHER_CTX_get_params would not report the length of the
    tag parameter when called with a NULL data pointer. This change makes the
    function behave as documented.
    
    Reviewed-by: Dmitry Belyavskiy <beldmit@gmail.com>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/28232)

--- providers/implementations/ciphers/ciphercommon_gcm.c
+++ providers/implementations/ciphers/ciphercommon_gcm.c
@@ -216,13 +216,15 @@ int ossl_gcm_get_ctx_params(void *vctx, OSSL_PARAM params[])
 
     if (p.tag != NULL) {
         sz = p.tag->data_size;
-        if (sz == 0
-            || sz > EVP_GCM_TLS_TAG_LEN
-            || !ctx->enc
-            || ctx->taglen == UNINITIALISED_SIZET) {
+        if (!ctx->enc || ctx->taglen == UNINITIALISED_SIZET) {
             ERR_raise(ERR_LIB_PROV, PROV_R_INVALID_TAG);
             return 0;
         }
+        if (p.tag->data != NULL && (sz > EVP_GCM_TLS_TAG_LEN || sz == 0)) {
+            ERR_raise(ERR_LIB_PROV, PROV_R_INVALID_TAG);
+            return 0;
+        }
+
         if (!OSSL_PARAM_set_octet_string(p.tag, ctx->buf, sz)) {
             ERR_raise(ERR_LIB_PROV, PROV_R_FAILED_TO_SET_PARAMETER);
             return 0;
