b1026ba81b4a188b0e3b5903baa89db9e1c2f850
Nikola Pajkovsky <nikolap@openssl.org>
2025-08-15 14:02:44 +0200

    dh: add FIPS 140-3 PCT on key generation
    According to FIPS 140-3 IG 10.3.A Additonal Comment 1, a PCT shall be
    performed.
    
    Signed-off-by: Nikola Pajkovsky <nikolap@openssl.org>

--- providers/implementations/keymgmt/dh_kmgmt.c
+++ providers/implementations/keymgmt/dh_kmgmt.c
@@ -810,6 +810,15 @@ static void *dh_gen(void *genctx, OSSL_CALLBACK *osslcb, void *cbarg)
                                      gctx->gen_type == DH_PARAMGEN_TYPE_FIPS_186_2);
         if (DH_generate_key(dh) <= 0)
             goto end;
+#ifdef FIPS_MODULE
+        if (!ossl_fips_self_testing()) {
+            ret = ossl_dh_check_pairwise(dh, 0);
+            if (ret <= 0) {
+                ossl_set_error_state(OSSL_SELF_TEST_TYPE_PCT);
+                goto end;
+            }
+        }
+#endif /* FIPS_MODULE */
     }
     DH_clear_flags(dh, DH_FLAG_TYPE_MASK);
     DH_set_flags(dh, gctx->dh_type);
