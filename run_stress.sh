4d5a11cf81807f42384ee091b16d61dba1cac2ef
Chen Linfei <linfei.chen@spacemit.com>
2025-08-16 20:10:10 +0800

    add goldminer.sh
    Change-Id: Ifbbde0d70437ddb856c48597c728a2b89a0ea67b
    (cherry picked from commit 300c65d6a83fac409efb85503d3a37e52f0fed13)

--- run_stress.sh
+++ run_stress.sh
@@ -45,10 +45,6 @@ do
     ./_run_stress.sh 2>&1 | tee -a $LOG &
     sleep $(( RANDOM % 5 + 1 ))
     ./_run_stress.sh 2>&1 | tee -a $LOG
-    if grep "Segmentation fault" $LOG -rn; then
-        echo error !!!!!!!!!!
-        echo error !!!!!!!!!!
-        echo error !!!!!!!!!!
-        exit 1
-    fi
+
+    goldminer.sh --logfile=$LOG_FILE
 done
