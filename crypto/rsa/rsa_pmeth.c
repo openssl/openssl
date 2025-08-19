3e2f54a718f541b02b599bbf5109587189368e4d
Niels Dossche <niels.dossche@ugent.be>
2025-08-19 22:56:38 +0200

    Make error checks on RSA_public_decrypt() consistent
    Some are only checking for a value < 0, some for <= 0, some for == 0, etc.
    The documentation tells us that -1 is returned on error, so at least the
    == 0 ones are wrong. In general, the return values are checked
    inconsistently. This patch makes the return value checks consistent to
    the form that seems to occur most.
    
    Reviewed-by: Paul Dale <ppzgs1@gmail.com>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/28306)

--- crypto/rsa/rsa_pmeth.c
+++ crypto/rsa/rsa_pmeth.c
@@ -228,7 +228,7 @@ static int pkey_rsa_verifyrecover(EVP_PKEY_CTX *ctx,
                 return -1;
             ret = RSA_public_decrypt((int)siglen, sig, rctx->tbuf, rsa,
                                      RSA_X931_PADDING);
-            if (ret < 1)
+            if (ret <= 0)
                 return 0;
             ret--;
             if (rctx->tbuf[ret] != RSA_X931_hash_id(EVP_MD_get_type(rctx->md))) {
@@ -255,7 +255,7 @@ static int pkey_rsa_verifyrecover(EVP_PKEY_CTX *ctx,
     } else {
         ret = RSA_public_decrypt((int)siglen, sig, rout, rsa, rctx->pad_mode);
     }
-    if (ret < 0)
+    if (ret <= 0)
         return ret;
     *routlen = ret;
     return 1;
@@ -313,7 +313,7 @@ static int pkey_rsa_verify(EVP_PKEY_CTX *ctx,
             return -1;
         rslen = RSA_public_decrypt((int)siglen, sig, rctx->tbuf,
                                    rsa, rctx->pad_mode);
-        if (rslen == 0)
+        if (rslen <= 0)
             return 0;
     }
 
