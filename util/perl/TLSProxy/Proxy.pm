9c1a476fcc52b57d3ff40c0ccfc74cc5836018b9
Neil Horman <nhorman@openssl.org>
2025-09-04 14:04:55 -0400

    Convert TLSProxy to use new provider
    replace use of ossltest engine with provider in TLSProxy and update all
    dependent tests

--- util/perl/TLSProxy/Proxy.pm
+++ util/perl/TLSProxy/Proxy.pm
@@ -318,7 +318,7 @@ sub start
     }
 
     my $execcmd = $self->execute
-        ." s_server -no_comp -engine ossltest -state"
+        ." s_server -no_comp -provider=p_ossltest -provider=default -propquery ?provider=p_ossltest -state"
         #In TLSv1.3 we issue two session tickets. The default session id
         #callback gets confused because the ossltest engine causes the same
         #session id to be created twice due to the changed random number
@@ -423,8 +423,8 @@ sub clientstart
     if ($self->execute) {
         my $pid;
         my $execcmd = $self->execute
-             ." s_client -engine ossltest"
-             ." -connect $self->{proxy_addr}:$self->{proxy_port}";
+             ." s_client -provider=p_ossltest -provider=default -propquery ?provider=p_ossltest"
+             ." -keylogfile ./keys.log -connect $self->{proxy_addr}:$self->{proxy_port}";
         if ($self->{isdtls}) {
             $execcmd .= " -dtls -max_protocol DTLSv1.2"
                         # TLSProxy does not support message fragmentation. So
