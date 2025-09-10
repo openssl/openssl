openssl/openssl

commit 2631a941469864a35258130082096876e7243225

OSSL_sleep(): Calling sleep() function if sleepTime > 1sec
On some systems usleep() func does not support time >1sec.

Reviewed-by: Matt Caswell <matt@openssl.org>
Reviewed-by: Richard Levitte <levitte@openssl.org>
Reviewed-by: Tomas Mraz <tomas@openssl.org>
(Merged from https://github.com/openssl/openssl/pull/21010)

crypto/sleep.c
@@ -12,6 +12,7 @@
 
 /* system-specific variants defining OSSL_sleep() */
 #if defined(OPENSSL_SYS_UNIX) || defined(__DJGPP__)
+#include <unistd.h>
 
 void OSSL_sleep(uint64_t millis)
 {
@@ -36,7 +37,11 @@ void OSSL_sleep(uint64_t millis)
     usleep(millis * 1000);
 #  endif
 # else
-    usleep(millis * 1000);
+    unsigned int s = (unsigned int)(millis / 1000);
+    unsigned int us = (unsigned int)((millis % 1000) * 1000);
+
+    sleep(s);
+    usleep(us);
 # endif
 }
 #elif defined(_WIN32) && !defined(OPENSSL_SYS_UEFI)

commit 890c346eb7e2dcb1aadb9065972296d46f016c6c

crypto/sleep.c: avoid integer overflov when usleep() is used
Clamt the sleep time in seconds to UINT_MAX if the requested millis
is UINT_MAX * 1000 or greater.

Fixes: 2631a9414698 "OSSL_sleep(): Calling sleep() function if sleepTime > 1sec"
Signed-off-by: Eugene Syromiatnikov <esyr@openssl.org>

crypto/sleep.c
@@ -29,8 +29,10 @@
 #  include <unistd.h>
 static void ossl_sleep_millis(uint64_t millis)
 {
-    unsigned int s = (unsigned int)(millis / 1000);
-    unsigned int us = (unsigned int)((millis % 1000) * 1000);
+    unsigned int s = millis < 1000LLU * UINT_MAX ? (unsigned int)(millis / 1000)
+                                                 : UINT_MAX;
+    unsigned int us = millis < 1000LLU * UINT_MAX
+        ? (unsigned int)((millis % 1000) * 1000) : 0;
 
     if (s > 0)
         sleep(s);