5dec881a0daf49992e1387c87eafdf347808ba4e
Nikola Pajkovsky <nikolap@openssl.org>
2025-09-18 11:13:45 +0200

    x509store: reduce lock contention in X509_STORE
    X509_STORE was using STACK_OF(X509_OBJECT) which is not ideal structure. The
    better solution is to use hashmap. The performance gains come from the fact that
    sorting was removed and therefore read lock is just enough for looking up
    objects/cert/crls from hashmap.
    
    The change in X509_STORE_get0_objects() is a bit controversial. The function
    argument which is const has been cast out to non-const because function have to
    modify the X509_STORE, hence the caller has to take write lock
    (X509_STORE_lock).
    
    Note, hashmap maps X509_NAME to STACK_OF(X509_OBJECT), and the stack is never
    sorted which may lead to performace impact if stack contains a huge of objects.
    
    Before the change
    
    | Threads |   mean/us |  var/us |
    |---------+-----------+---------|
    |       1 |  2.434803 | .034190 |
    |       2 |  3.033588 | .247471 |
    |       4 |  6.551132 | .150209 |
    |       6 | 12.548113 | .258445 |
    |       8 | 17.566257 | .168508 |
    |      10 | 22.782846 | .182674 |
    |      12 | 27.928990 | .426779 |
    |      14 | 32.844572 | .307754 |
    |      16 | 37.816247 | .660630 |
    |      18 | 42.662465 | .434926 |
    
    After the change
    
    | Threads |  mean/us |  var/us |
    |---------+----------+---------|
    |       1 | 2.385398 | .015329 |
    |       2 | 2.775794 | .172223 |
    |       4 | 3.071882 | .126400 |
    |       6 | 3.174147 | .139685 |
    |       8 | 3.479235 | .297154 |
    |      10 | 4.206260 | .149006 |
    |      12 | 5.044039 | .194108 |
    |      14 | 5.890640 | .185817 |
    |      16 | 6.447808 | .256179 |
    |      18 | 7.489261 | .149204 |
    
    Resolves: https://github.com/openssl/project/issues/1275
    Signed-off-by: Nikola Pajkovsky <nikolap@openssl.org>

--- crypto/x509/by_dir.c
+++ crypto/x509/by_dir.c
@@ -232,7 +232,7 @@ static int get_cert_by_subject_ex(X509_LOOKUP *xl, X509_LOOKUP_TYPE type,
     int i, j, k;
     unsigned long h;
     BUF_MEM *b = NULL;
-    X509_OBJECT stmp, *tmp;
+    X509_OBJECT stmp, *tmp = NULL;
     const char *postfix = "";
 
     if (name == NULL)
@@ -348,11 +348,16 @@ static int get_cert_by_subject_ex(X509_LOOKUP *xl, X509_LOOKUP_TYPE type,
          *       sorted and sorting the would result in O(n^2 log n) complexity.
          */
         if (k > 0) {
-            if (!X509_STORE_lock(xl->store_ctx))
+            STACK_OF(X509_OBJECT) *objs;
+
+            if (ossl_x509_store_read_lock(xl->store_ctx) == 0)
                 goto finish;
-            j = sk_X509_OBJECT_find(xl->store_ctx->objs, &stmp);
-            tmp = sk_X509_OBJECT_value(xl->store_ctx->objs, j);
-            X509_STORE_unlock(xl->store_ctx);
+            objs = ossl_x509_store_ht_get_by_name(xl->store_ctx, name);
+            if (objs != NULL) {
+                j = sk_X509_OBJECT_find(objs, &stmp);
+                tmp = sk_X509_OBJECT_value(objs, j);
+            }
+            ossl_x509_store_read_unlock(xl->store_ctx);
         } else {
             tmp = NULL;
         }
@@ -419,14 +424,6 @@ static int get_cert_by_subject_ex(X509_LOOKUP *xl, X509_LOOKUP_TYPE type,
         }
     }
  finish:
-    /* If we changed anything, resort the objects for faster lookup */
-    if (X509_STORE_lock(xl->store_ctx)) {
-        if (!sk_X509_OBJECT_is_sorted(xl->store_ctx->objs)) {
-            sk_X509_OBJECT_sort(xl->store_ctx->objs);
-        }
-        X509_STORE_unlock(xl->store_ctx);
-    }
-
     BUF_MEM_free(b);
     return ok;
 }
