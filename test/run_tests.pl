76c33a2c21838c6e05d67e25f2bfc660f286cc0e
Neil Horman <nhorman@openssl.org>
2025-09-25 11:37:52 -0400

    Additonal removals of engine references in tests/*
    Spotted by @andrewkdinh, some extra notes about/useages of engines that
    are now vestigial.
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/28525)

--- test/run_tests.pl
+++ test/run_tests.pl
@@ -70,7 +70,6 @@ if (!defined($jobs)) {
 $ENV{OPENSSL_CONF} = rel2abs(catfile($srctop, "apps", "openssl.cnf"));
 $ENV{OPENSSL_CONF_INCLUDE} = rel2abs(catdir($bldtop, "test"));
 $ENV{OPENSSL_MODULES} = rel2abs(catdir($bldtop, "providers"));
-$ENV{OPENSSL_ENGINES} = rel2abs(catdir($bldtop, "engines"));
 $ENV{CTLOG_FILE} = rel2abs(catfile($srctop, "test", "ct", "log_list.cnf"));
 
 # On platforms that support this, this will ensure malloc returns data that is
