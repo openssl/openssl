cb77529d835fff7e8a13d1757a283f0b91031a0d
Erik Bråthen Solem <erikbsolem@hotmail.com>
2025-10-02 03:51:45 +0200

    Assembly generation scripts: set $win64=1 for flavour "win64"

--- crypto/aes/asm/aes-cfb-avx512.pl
+++ crypto/aes/asm/aes-cfb-avx512.pl
@@ -12,7 +12,8 @@
 $output = $#ARGV >= 0 && $ARGV[$#ARGV] =~ m|\.\w+$| ? pop : undef;
 $flavour = $#ARGV >= 0 && $ARGV[0] !~ m|\.| ? shift : undef;
 
-$win64=0; $win64=1 if ($flavour =~ /[nm]asm|mingw64/ || $output =~ /\.asm$/);
+$win64=0;
+$win64=1 if ($flavour =~ /[nm]asm|mingw64|win64/ || $output =~ /\.asm$/);
 
 $avx512vaes=0; # will be non-zero if tooling supports Intel AVX-512 and VAES
 
