c951fcba796aebab20378cf7b86885e226684e6f
Bob Beck <beck@openssl.org>
2025-10-06 05:03:41 -0600

    Bring in boundary test from #28584
    Will add further unit tests for the cert validity check routine

--- test/certs/setup.sh
+++ test/certs/setup.sh
@@ -158,6 +158,7 @@ openssl x509 -in sca-cert.pem -trustout \
 ./mkcert.sh genee server.example ee-key ee-cert ca-key ca-cert
 # ee variants: expired, issuer-key2, issuer-name2, bad-pathlen
 ./mkcert.sh genee server.example ee-key ee-expired ca-key ca-cert -days -1
+./mkcert.sh genee server.example ee-key ee-expired2 ca-key ca-cert -days 3650
 ./mkcert.sh genee server.example ee-key ee-cert2 ca-key2 ca-cert2
 ./mkcert.sh genee server.example ee-key ee-name2 ca-key ca-name2
 ./mkcert.sh genee server.example ee-key ee-pathlen ca-key ca-cert \
