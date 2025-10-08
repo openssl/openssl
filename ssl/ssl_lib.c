b2a3a3faf84a47e1495d8d9915a53846cc6d6577
Joshua Rogers <MegaManSec@users.noreply.github.com>
2025-10-09 06:14:15 +0800

    ssl: fix OOB write in SSL_get_shared_ciphers when no shared ciphers
    When no cipher names are appended, p remains at buf and the unconditional
    p[-1] = '\0' underflows. Only NUL-terminate if at least one cipher was written;
    otherwise return an empty string safely.
    
    Signed-off-by: Joshua Rogers <MegaManSec@users.noreply.github.com>
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/28785)
    
    (cherry picked from commit 680491a2a7403fc6e5e1759e0eabeceeacaf37f9)

--- ssl/ssl_lib.c
+++ ssl/ssl_lib.c
@@ -3393,18 +3393,20 @@ char *SSL_get_shared_ciphers(const SSL *s, char *buf, int size)
         if (sk_SSL_CIPHER_find(srvrsk, c) < 0)
             continue;
 
-        n = OPENSSL_strnlen(c->name, size);
-        if (n >= size) {
-            if (p != buf)
-                --p;
-            *p = '\0';
-            return buf;
-        }
+        n = (int)OPENSSL_strnlen(c->name, size);
+        if (n >= size)
+            break;
+
         memcpy(p, c->name, n);
         p += n;
         *(p++) = ':';
         size -= n + 1;
     }
+
+    /* No overlap */
+    if (p == buf)
+        return NULL;
+
     p[-1] = '\0';
     return buf;
 }
