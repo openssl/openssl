78a46ac75a9417b84a88f2922530af3842a14dee
Richard Levitte <levitte@openssl.org>
2025-10-22 15:50:41 +0200

    OSSL_FN: Add internal construction and introspection functions
    These functions will be useful with other test programs without having
    to include crypto/fn/fn_local.h, making them closer to real world use.
    
    This also introduces OSSL_FN errors
    
    Related-to: doc/designs/fixed-size-large-numbers.md
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/29028)

---
+++ crypto/fn/fn_intern.c
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2025 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#include "internal/cryptlib.h"
+#include "crypto/fn_intern.h"
+#include "crypto/fnerr.h"
+#include "fn_local.h"
+#include <openssl/err.h>
+
+int ossl_fn_set_words(OSSL_FN *f, const OSSL_FN_ULONG *words, size_t limbs)
+{
+    if (ossl_unlikely(f == NULL)) {
+        ERR_raise(ERR_LIB_OSSL_FN, ERR_R_PASSED_NULL_PARAMETER);
+        return 0;
+    }
+
+    if ((size_t)f->dsize < limbs) {
+        ERR_raise(ERR_LIB_OSSL_FN, OSSL_FN_R_RESULT_ARG_TOO_SMALL);
+        return 0;
+    }
+
+    memcpy(f->d, words, sizeof(OSSL_FN_ULONG) * limbs);
+    memset(f->d + limbs, 0, sizeof(OSSL_FN_ULONG) * (f->dsize - limbs));
+    return 1;
+}
+
+const OSSL_FN_ULONG *ossl_fn_get_words(OSSL_FN *f)
+{
+    if (ossl_unlikely(f == NULL)) {
+        ERR_raise(ERR_LIB_OSSL_FN, ERR_R_PASSED_NULL_PARAMETER);
+        return NULL;
+    }
+
+    return f->d;
+}
+
+size_t ossl_fn_get_dsize(OSSL_FN *f)
+{
+    return f->dsize;
+}
+
+bool ossl_fn_is_dynamically_allocated(OSSL_FN *f)
+{
+    return f->is_dynamically_allocated;
+}
+
+bool ossl_fn_is_securely_allocated(OSSL_FN *f)
+{
+    return f->is_securely_allocated;
+}
