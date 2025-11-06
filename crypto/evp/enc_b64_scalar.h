a3026c24bde38bcb20b6401030136cec1c37d9ca
Nick Nuon <myjpntrip@gmail.com>
2025-11-06 10:35:52 -0500

    formatting

--- crypto/evp/enc_b64_scalar.h
+++ crypto/evp/enc_b64_scalar.h
@@ -1,8 +1,5 @@
-#ifndef OSSL_CRYPTO_EVP_B64_SCALAR_H
-# define OSSL_CRYPTO_EVP_B64_SCALAR_H
+#define OSSL_CRYPTO_EVP_B64_SCALAR_H
 #include <openssl/evp.h>
 
 int evp_encodeblock_int(EVP_ENCODE_CTX *ctx, unsigned char *t,
                         const unsigned char *f, int dlen, int *wrap_cnt);
-
-#endif
