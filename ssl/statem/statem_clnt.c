0be606b4d5d44f44638c10630b805e1a94765989
Ryan Hooper <ryhooper@cisco.com>
2025-11-13 17:35:37 -0500

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! Fixing comment out tests for DTLS1.3

--- ssl/statem/statem_clnt.c
+++ ssl/statem/statem_clnt.c
@@ -495,6 +495,10 @@ static WRITE_TRAN ossl_statem_client13_write_transition(SSL_CONNECTION *s)
         /* Fall through */
 
     case TLS_ST_CW_END_OF_EARLY_DATA:
+        if (SSL_CONNECTION_IS_DTLS13(s)) {
+            /* If we are done with early data we need to clean up Epoch 1 messages sent*/
+            dtls1_clear_sent_buffer(s, 0);
+        }
     case TLS_ST_CW_CHANGE:
         if (s->s3.tmp.cert_req == 0)
             st->hand_state = TLS_ST_CW_FINISHED;
