b3c941ea3fc10b1f9b27d58b12b24a24788cfa85
Helen Zhang <helzhang@cisco.com>
2025-11-20 15:17:12 +0000

    fixup! add snmkkdf implementation

--- providers/implementations/kdfs/snmpkdf.c
+++ providers/implementations/kdfs/snmpkdf.c
@@ -151,7 +151,9 @@ static int kdf_snmpkdf_set_ctx_params(void *vctx, const OSSL_PARAM params[])
     struct snmp_set_ctx_params_st p;
     KDF_SNMPKDF *ctx = vctx;
     OSSL_LIB_CTX *libctx = PROV_LIBCTX_OF(ctx->provctx);
+#ifdef FIPS_MODULE
     const EVP_MD *md = NULL;
+#endif
 
     if (params == NULL)
         return 1;
@@ -162,8 +164,8 @@ static int kdf_snmpkdf_set_ctx_params(void *vctx, const OSSL_PARAM params[])
     if (p.digest != NULL) {
         if (!ossl_prov_digest_load(&ctx->digest, p.digest, p.propq, NULL, libctx))
             return 0;
-        md = ossl_prov_digest_md(&ctx->digest);
 #ifdef FIPS_MODULE
+        md = ossl_prov_digest_md(&ctx->digest);
         if (!EVP_MD_is_a(md, SN_sha1))
             return 0;
 #endif
