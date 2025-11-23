dac8f26411fefacaa3f2efee84d9bcf1ba82ffe8
Timothy Copeland <tacopeland@proton.me>
2025-11-23 22:53:58 +1100

    crypto/x509/x509_vfy.c: tell verify callback of failure to get CRL

--- crypto/x509/x509_vfy.c
+++ crypto/x509/x509_vfy.c
@@ -1362,6 +1362,7 @@ static int check_cert_crl(X509_STORE_CTX *ctx)
                 ctx->current_crl_score = get_crl_score(ctx, &crl_issuer,
                                                        &reasons, crl, x);
             } else {
+                ok = verify_cb_crl(ctx, X509_V_ERR_UNABLE_TO_GET_CRL);
                 goto done;
             }
             ctx->current_issuer = crl_issuer;
