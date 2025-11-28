73d2a5b684f63fa1b7392993738b6de80ed062cb
Bob Beck <beck@openssl.org>
2025-11-28 13:03:53 -0700

    apply clang-format to all .c and .h files

--- providers/implementations/include/prov/implementations.h
+++ providers/implementations/include/prov/implementations.h
@@ -197,7 +197,7 @@ extern const OSSL_DISPATCH ossl_rc2128ofb128_functions[];
 #ifndef OPENSSL_NO_DES
 extern const OSSL_DISPATCH ossl_tdes_ede3_ecb_functions[];
 extern const OSSL_DISPATCH ossl_tdes_ede3_cbc_functions[];
-# ifndef FIPS_MODULE
+#ifndef FIPS_MODULE
 extern const OSSL_DISPATCH ossl_tdes_ede3_ofb_functions[];
 extern const OSSL_DISPATCH ossl_tdes_ede3_cfb_functions[];
 extern const OSSL_DISPATCH ossl_tdes_ede3_cfb8_functions[];
@@ -217,23 +217,22 @@ extern const OSSL_DISPATCH ossl_des_ofb64_functions[];
 extern const OSSL_DISPATCH ossl_des_cfb64_functions[];
 extern const OSSL_DISPATCH ossl_des_cfb1_functions[];
 extern const OSSL_DISPATCH ossl_des_cfb8_functions[];
-# endif /* FIPS_MODULE */
+#endif /* FIPS_MODULE */
 #endif /* OPENSSL_NO_DES */
 
 #ifndef OPENSSL_NO_RC4
 extern const OSSL_DISPATCH ossl_rc440_functions[];
 extern const OSSL_DISPATCH ossl_rc4128_functions[];
-# ifndef OPENSSL_NO_MD5
+#ifndef OPENSSL_NO_MD5
 extern const OSSL_DISPATCH ossl_rc4_hmac_ossl_md5_functions[];
-# endif /* OPENSSL_NO_MD5 */
+#endif /* OPENSSL_NO_MD5 */
 #endif /* OPENSSL_NO_RC4 */
 #ifndef OPENSSL_NO_CHACHA
 extern const OSSL_DISPATCH ossl_chacha20_functions[];
-# ifndef OPENSSL_NO_POLY1305
+#ifndef OPENSSL_NO_POLY1305
 extern const OSSL_DISPATCH ossl_chacha20_ossl_poly1305_functions[];
-# endif /* OPENSSL_NO_POLY1305 */
+#endif /* OPENSSL_NO_POLY1305 */
 #endif /* OPENSSL_NO_CHACHA */
-
 
 #ifndef OPENSSL_NO_SIV
 extern const OSSL_DISPATCH ossl_aes128siv_functions[];
