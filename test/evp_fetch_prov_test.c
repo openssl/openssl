bb745a44744854563dc7124c1e94d1b9b1ed17a3
Andrew Dinh <andrewd@openssl.org>
2025-12-10 09:17:43 +0000

    Fix temporary freeze implementation

--- test/evp_fetch_prov_test.c
+++ test/evp_fetch_prov_test.c
@@ -224,29 +224,38 @@ static int test_EVP_MD_fetch_freeze(void)
 {
     EVP_MD *md = NULL;
     int ret = 0;
+    OSSL_LIB_CTX *ctx = OSSL_LIB_CTX_new();
 
-    if (!TEST_ptr(md = EVP_MD_fetch(NULL, "SHA256", NULL))
+    if (!TEST_ptr(ctx)
+        || !TEST_ptr(md = EVP_MD_fetch(ctx, "SHA256", NULL))
         || !TEST_true(test_md(md))
         || !TEST_int_ne(md->origin, EVP_ORIG_FROZEN))
         goto err;
-
     EVP_MD_free(md);
     md = NULL;
 
-    if (!TEST_int_eq(OSSL_LIB_CTX_freeze(NULL, "?fips=true"), 1)
-        || !TEST_ptr(md = EVP_MD_fetch(NULL, "SHA256", NULL))
+    if (!TEST_int_eq(OSSL_LIB_CTX_freeze(ctx, "?fips=true"), 1)
+        || !TEST_ptr(md = EVP_MD_fetch(ctx, "SHA256", NULL))
         || !TEST_true(test_md(md))
-        || !TEST_int_eq(md->origin, EVP_ORIG_FROZEN)
-        || !TEST_ptr(md = EVP_MD_fetch(NULL, "SHA256", "?fips=true"))
+        || !TEST_int_eq(md->origin, EVP_ORIG_FROZEN))
+        goto err;
+    /* Technically, frozen version doesn't need to be freed */
+    EVP_MD_free(md);
+
+    if (!TEST_ptr(md = EVP_MD_fetch(ctx, "SHA256", "?fips=true"))
         || !TEST_true(test_md(md))
-        || !TEST_int_eq(md->origin, EVP_ORIG_FROZEN)
-        || !TEST_ptr(md = EVP_MD_fetch(NULL, "SHA256", "?provider=default"))
+        || !TEST_int_eq(md->origin, EVP_ORIG_FROZEN))
+        goto err;
+    EVP_MD_free(md);
+
+    if (!TEST_ptr(md = EVP_MD_fetch(ctx, "SHA256", "?provider=default"))
         || !TEST_true(test_md(md))
         || !TEST_int_ne(md->origin, EVP_ORIG_FROZEN))
         goto err;
 
     ret = 1;
- err:
+err:
+    OSSL_LIB_CTX_free(ctx);
     EVP_MD_free(md);
     return ret;
 }
