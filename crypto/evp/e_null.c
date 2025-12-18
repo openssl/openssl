1b03a1814b36cd6ded01306b7356655bec80a357
Matt Caswell <matt@openssl.org>
2025-12-18 15:59:26 +0000

    Remove dead EVP_enc_null() code

--- crypto/evp/e_null.c
+++ crypto/evp/e_null.c
@@ -13,16 +13,12 @@
 #include <openssl/objects.h>
 #include "crypto/evp.h"
 
-static int null_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key,
-    const unsigned char *iv, int enc);
-static int null_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out,
-    const unsigned char *in, size_t inl);
 static const EVP_CIPHER n_cipher = {
     NID_undef,
     1, 0, 0, 0,
     EVP_ORIG_GLOBAL,
-    null_init_key,
-    null_cipher,
+    NULL,
+    NULL,
     NULL,
     0,
     NULL,
@@ -34,18 +30,4 @@ static const EVP_CIPHER n_cipher = {
 const EVP_CIPHER *EVP_enc_null(void)
 {
     return &n_cipher;
-}
-
-static int null_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key,
-    const unsigned char *iv, int enc)
-{
-    return 1;
-}
-
-static int null_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out,
-    const unsigned char *in, size_t inl)
-{
-    if (in != out)
-        memcpy(out, in, inl);
-    return 1;
 }
