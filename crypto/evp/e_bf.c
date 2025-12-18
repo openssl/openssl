32eaa748a3020896d3ac7b76c75f56ffd8112e56
Matt Caswell <matt@openssl.org>
2025-12-18 14:08:29 +0000

    Cleanup block cipher macros in include/crypto/evp.h
    Remove some unneeded deadcode and fix the formatting
    
    Also fix all users of those macros to avoid compilation warnings
    
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Neil Horman <nhorman@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/29446)

--- crypto/evp/e_bf.c
+++ crypto/evp/e_bf.c
@@ -7,12 +7,6 @@
  * https://www.openssl.org/source/license.html
  */
 
-/*
- * BF low level APIs are deprecated for public use, but still ok for internal
- * use.
- */
-#include "internal/deprecated.h"
-
 #include <stdio.h>
 #include "internal/cryptlib.h"
 #ifndef OPENSSL_NO_BF
@@ -22,28 +16,7 @@
 #include <openssl/blowfish.h>
 #include "evp_local.h"
 
-static int bf_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key,
-    const unsigned char *iv, int enc);
-
-typedef struct {
-    BF_KEY ks;
-} EVP_BF_KEY;
-
-#define data(ctx) EVP_C_DATA(EVP_BF_KEY, ctx)
-
 IMPLEMENT_BLOCK_CIPHER(bf, ks, BF, EVP_BF_KEY, NID_bf, 8, 16, 8, 64,
-    EVP_CIPH_VARIABLE_LENGTH, bf_init_key, NULL,
-    EVP_CIPHER_set_asn1_iv, EVP_CIPHER_get_asn1_iv, NULL)
-
-static int bf_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key,
-    const unsigned char *iv, int enc)
-{
-    int len = EVP_CIPHER_CTX_get_key_length(ctx);
-
-    if (len < 0)
-        return 0;
-    BF_set_key(&data(ctx)->ks, len, key);
-    return 1;
-}
-
+    EVP_CIPH_VARIABLE_LENGTH, NULL, NULL,
+    NULL, NULL, NULL)
 #endif
--- crypto/evp/e_xcbc_d.c
+++ crypto/evp/e_xcbc_d.c
@@ -7,12 +7,6 @@
  * https://www.openssl.org/source/license.html
  */
 
-/*
- * DES low level APIs are deprecated for public use, but still ok for internal
- * use.
- */
-#include "internal/deprecated.h"
-
 #include <stdio.h>
 #include "internal/cryptlib.h"
 
@@ -24,30 +18,17 @@
 #include <openssl/des.h>
 #include "evp_local.h"
 
-static int desx_cbc_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key,
-    const unsigned char *iv, int enc);
-static int desx_cbc_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out,
-    const unsigned char *in, size_t inl);
-
-typedef struct {
-    DES_key_schedule ks; /* key schedule */
-    DES_cblock inw;
-    DES_cblock outw;
-} DESX_CBC_KEY;
-
-#define data(ctx) EVP_C_DATA(DESX_CBC_KEY, ctx)
-
 static const EVP_CIPHER d_xcbc_cipher = {
     NID_desx_cbc,
     8, 24, 8,
     EVP_CIPH_CBC_MODE,
     EVP_ORIG_GLOBAL,
-    desx_cbc_init_key,
-    desx_cbc_cipher,
     NULL,
-    sizeof(DESX_CBC_KEY),
-    EVP_CIPHER_set_asn1_iv,
-    EVP_CIPHER_get_asn1_iv,
+    NULL,
+    NULL,
+    0,
+    NULL,
+    NULL,
     NULL,
     NULL
 };
@@ -55,37 +36,5 @@ static const EVP_CIPHER d_xcbc_cipher = {
 const EVP_CIPHER *EVP_desx_cbc(void)
 {
     return &d_xcbc_cipher;
-}
-
-static int desx_cbc_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key,
-    const unsigned char *iv, int enc)
-{
-    DES_cblock *deskey = (DES_cblock *)key;
-
-    DES_set_key_unchecked(deskey, &data(ctx)->ks);
-    memcpy(&data(ctx)->inw[0], &key[8], 8);
-    memcpy(&data(ctx)->outw[0], &key[16], 8);
-
-    return 1;
-}
-
-static int desx_cbc_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out,
-    const unsigned char *in, size_t inl)
-{
-    while (inl >= EVP_MAXCHUNK) {
-        DES_xcbc_encrypt(in, out, (long)EVP_MAXCHUNK, &data(ctx)->ks,
-            (DES_cblock *)ctx->iv,
-            &data(ctx)->inw, &data(ctx)->outw,
-            EVP_CIPHER_CTX_is_encrypting(ctx));
-        inl -= EVP_MAXCHUNK;
-        in += EVP_MAXCHUNK;
-        out += EVP_MAXCHUNK;
-    }
-    if (inl)
-        DES_xcbc_encrypt(in, out, (long)inl, &data(ctx)->ks,
-            (DES_cblock *)ctx->iv,
-            &data(ctx)->inw, &data(ctx)->outw,
-            EVP_CIPHER_CTX_is_encrypting(ctx));
-    return 1;
 }
 #endif
