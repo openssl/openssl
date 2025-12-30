a2467a53556bd261e7a4561a0c40b45243d6ebc6
xxcui <xxcui@linux.alibaba.com>
2025-12-30 09:49:38 +0800

    Fix sha3 compile issue

--- crypto/sha/keccak1600_riscv.c
+++ crypto/sha/keccak1600_riscv.c
@@ -19,6 +19,9 @@ size_t SHA3_absorb_c(uint64_t A[5][5], const unsigned char *inp, size_t len,
     size_t r);
 void SHA3_squeeze_c(uint64_t A[5][5], unsigned char *out, size_t len, size_t r, int next);
 #endif
+size_t SHA3_absorb(uint64_t A[5][5], const unsigned char *inp, size_t len,
+    size_t r);
+void SHA3_squeeze(uint64_t A[5][5], unsigned char *out, size_t len, size_t r, int next);
 
 size_t SHA3_absorb_v_zbb(uint64_t A[5][5], const unsigned char *inp, size_t len,
     size_t r);
