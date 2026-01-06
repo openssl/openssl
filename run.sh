29cb1f66165af8c2f4607a7abbf1b0cc19d534a9
zhangminghui <minghui.zhang@spacemit.com>
2026-01-06 17:48:32 +0800

    add single_core test to run.sh
    Change-Id: I501da04946badc0ca9431dc99ddfbdef99ff1930
    (cherry picked from commit ac40a3aab8821dcbf7caa4a6da1adf4816f7d491)

--- run.sh
+++ run.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+##Single-core test
+#LOG_SINGLE_FILE=openssl_single.log
+#echo run openssl_single
+#echo export OPENSSL_riscvcap=ZBA_ZBB_ZBC_ZBS_V_ZVBB_ZVBC_ZVKB_ZVKG_ZVKNED_ZVKNHA_ZVKNHB_ZVKSED_ZVKSH
+#export OPENSSL_riscvcap=ZBA_ZBB_ZBC_ZBS_V_ZVBB_ZVBC_ZVKB_ZVKG_ZVKNED_ZVKNHA_ZVKNHB_ZVKSED_ZVKSH
+#echo openssl speed
+#./openssl speed 2>&1 | tee -a $LOG_SINGLE_FILE
+#echo export OPENSSL_riscvcap=
+#export OPENSSL_riscvcap=
+#echo openssl speed
+#./openssl speed 2>&1 | tee -a $LOG_SINGLE_FILE
+#
+#goldminer.sh --logfile=$LOG_SINGLE__FILE
 
+##Multi-core test
 LOG_FILE=openssl.log
-
 echo run openssl
 echo export OPENSSL_riscvcap=ZBA_ZBB_ZBC_ZBS_V_ZVBB_ZVBC_ZVKB_ZVKG_ZVKNED_ZVKNHA_ZVKNHB_ZVKSED_ZVKSH
 export OPENSSL_riscvcap=ZBA_ZBB_ZBC_ZBS_V_ZVBB_ZVBC_ZVKB_ZVKG_ZVKNED_ZVKNHA_ZVKNHB_ZVKSED_ZVKSH
