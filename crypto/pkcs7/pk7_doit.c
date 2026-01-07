7bbca05be55b129651d9df4bdb92becc45002c12
Bob Beck <beck@openssl.org>
2026-01-07 11:29:48 -0700

    Ensure ASN1 types are checked before use.
    Some of these were fixed by LibreSSL in commit https://github.com/openbsd/src/commit/aa1f637d454961d22117b4353f98253e984b3ba8
    this fix includes the other fixes in that commit, as well as fixes for others found by a scan
    for a similar unvalidated access paradigm in the tree.
    
    Reviewed-by: Kurt Roeckx <kurt@roeckx.be>
    Reviewed-by: Shane Lontis <shane.lontis@oracle.com>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/29582)

--- crypto/pkcs7/pk7_doit.c
+++ crypto/pkcs7/pk7_doit.c
@@ -1178,6 +1178,8 @@ ASN1_OCTET_STRING *PKCS7_digest_from_attributes(STACK_OF(X509_ATTRIBUTE) *sk)
     ASN1_TYPE *astype;
     if ((astype = get_attribute(sk, NID_pkcs9_messageDigest)) == NULL)
         return NULL;
+    if (astype->type != V_ASN1_OCTET_STRING)
+        return NULL;
     return astype->value.octet_string;
 }
 
