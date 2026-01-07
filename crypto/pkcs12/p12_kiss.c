bd2f9bb8a9ebb2d7d5886041ad7629d18da9987d
Bob Beck <beck@openssl.org>
2026-01-07 11:29:48 -0700

    Ensure ASN1 types are checked before use.
    Some of these were fixed by LibreSSL in commit https://github.com/openbsd/src/commit/aa1f637d454961d22117b4353f98253e984b3ba8
    this fix includes the other fixes in that commit, as well as fixes for others found by a scan
    for a similar unvalidated access paradigm in the tree.

--- crypto/pkcs12/p12_kiss.c
+++ crypto/pkcs12/p12_kiss.c
@@ -196,11 +196,15 @@ static int parse_bag(PKCS12_SAFEBAG *bag, const char *pass, int passlen,
     ASN1_BMPSTRING *fname = NULL;
     ASN1_OCTET_STRING *lkid = NULL;
 
-    if ((attrib = PKCS12_SAFEBAG_get0_attr(bag, NID_friendlyName)))
-        fname = attrib->value.bmpstring;
+    if ((attrib = PKCS12_SAFEBAG_get0_attr(bag, NID_friendlyName))) {
+        if (attrib->type == V_ASN1_BMPSTRING)
+            fname = attrib->value.bmpstring;
+    }
 
-    if ((attrib = PKCS12_SAFEBAG_get0_attr(bag, NID_localKeyID)))
-        lkid = attrib->value.octet_string;
+    if ((attrib = PKCS12_SAFEBAG_get0_attr(bag, NID_localKeyID))) {
+        if (attrib->type == V_ASN1_OCTET_STRING)
+            lkid = attrib->value.octet_string;
+    }
 
     switch (PKCS12_SAFEBAG_get_nid(bag)) {
     case NID_keyBag:
