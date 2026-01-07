8d29d2941c1cb547134ef47831d2faed670a416f
Bob Beck <beck@openssl.org>
2026-01-07 16:21:10 -0700

    Go for a ride in my Volkswagen for valgrind
    This pile on to @nhorman's #29573 so that we see
    the lovely false leaks in valgrind for checking
    our forthcoming suppression file.

--- test/testutil/main.c
+++ test/testutil/main.c
@@ -12,6 +12,19 @@
 #include "output.h"
 #include "tu_local.h"
 
+#if defined __has_include
+/* Any compiler you're going to run valgrind on has this */
+#if __has_include(<valgrind/valgrind.h>)
+#include <valgrind/valgrind.h>
+#define OPENSSL_VALGRIND_H_INCLUDED
+#endif
+#endif /* defined(__has_include) */
+
+/*
+ * At some point we should consider looking at this function with a view to
+ * moving most/all of this into onfree handlers in OSSL_LIB_CTX.
+ */
+
 int main(int argc, char *argv[])
 {
     int ret = EXIT_FAILURE;
@@ -40,6 +53,19 @@ int main(int argc, char *argv[])
 end:
     ret = pulldown_test_framework(ret);
     test_close_streams();
+#if defined(OPENSSL_VALGRIND_H_INCLUDED) && defined(RUNNING_ON_VALGRIND)
+    /*
+     * Somewhat paradoxically, we do *NOT* want to clean up normally
+     * when running tests using valgrind in order to test the suppression
+     * file which we will ship with the distribution.
+     *
+     * If for any reason you actually wish to run the test under valgrind
+     * but still call cleanup, just run it without OSSL_USE_VALGRIND set.
+     */
+    if (RUNNING_ON_VALGRIND && getenv(OSSL_USE_VALGRIND) != NULL)
+        return ret;
+#endif /* defined(OPENSSL_VALGRIND_H_INCLUDED) && defined(RUNNING_ON_VALGRIND) */
+
     OPENSSL_cleanup();
     return ret;
 }
