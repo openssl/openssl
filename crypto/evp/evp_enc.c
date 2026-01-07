aa70893e25aee2d199540f958fb0f76c8f851978
Andrew Dinh <andrewd@openssl.org>
2026-01-08 01:10:20 +0900

    Add freeze functionality to EVP_CIPHER_fetch
    Add unit test to check functionality
    
    Reviewed-by: Neil Horman <nhorman@openssl.org>
    Reviewed-by: Paul Dale <paul.dale@oracle.com>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    MergeDate: Wed Feb 18 10:13:45 2026
    (Merged from https://github.com/openssl/openssl/pull/30002)

--- crypto/evp/evp_enc.c
+++ crypto/evp/evp_enc.c
@@ -2044,11 +2044,13 @@ int evp_cipher_fetch_all(OSSL_LIB_CTX *ctx)
 EVP_CIPHER *EVP_CIPHER_fetch(OSSL_LIB_CTX *ctx, const char *algorithm,
     const char *properties)
 {
-    EVP_CIPHER *cipher = evp_generic_fetch(ctx, OSSL_OP_CIPHER, algorithm, properties,
-        evp_cipher_from_algorithm, evp_cipher_up_ref,
-        evp_cipher_free, NULL, NULL);
-
-    return cipher;
+    return evp_generic_fetch(ctx, OSSL_OP_CIPHER,
+        algorithm, properties,
+        evp_cipher_from_algorithm,
+        evp_cipher_up_ref,
+        evp_cipher_free,
+        evp_cipher_dup_frozen,
+        evp_cipher_frozen_free);
 }
 
 EVP_CIPHER *evp_cipher_fetch_from_prov(OSSL_PROVIDER *prov,
