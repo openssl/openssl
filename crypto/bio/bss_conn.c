827f7fdd8aec17f61717ef065ad0c0705dde81e6
Joshua Rogers <MegaManSec@users.noreply.github.com>
2026-01-10 19:55:15 +1100

    Clear addr_iter on reset in bss_conn
    Signed-off-by: Joshua Rogers <MegaManSec@users.noreply.github.com>
    
    Reviewed-by: Frederik Wedel-Heinen <fwh.openssl@gmail.com>
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/28915)
    
    (cherry picked from commit c64912cd5956e90746e668509278e0b8224f7a2a)

--- crypto/bio/bss_conn.c
+++ crypto/bio/bss_conn.c
@@ -393,6 +393,7 @@ static long conn_ctrl(BIO *b, int cmd, long num, void *ptr)
         conn_close_socket(b);
         BIO_ADDRINFO_free(data->addr_first);
         data->addr_first = NULL;
+        data->addr_iter = NULL;
         b->flags = 0;
         break;
     case BIO_C_DO_STATE_MACHINE:
