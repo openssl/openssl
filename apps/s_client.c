6a1c59bb1d567d071132b3f6121e687ee2631189
Philip Prindeville <philipp@redfish-solutions.com>
2026-01-20 17:51:03 -0700

    fixup! Minor cleanup of I/O of apps
    undo clang-format -i indent change

--- apps/s_client.c
+++ apps/s_client.c
@@ -3041,13 +3041,13 @@ re_start:
             }
 #endif
 
-                /*
-                 * Note: under VMS with SOCKETSHR the second parameter is
-                 * currently of type (int *) whereas under other systems it is
-                 * (void *) if you don't have a cast it will choke the compiler:
-                 * if you do have a cast then you can either go for (int *) or
-                 * (void *).
-                 */
+            /*
+             * Note: under VMS with SOCKETSHR the second parameter is
+             * currently of type (int *) whereas under other systems it is
+             * (void *) if you don't have a cast it will choke the compiler:
+             * if you do have a cast then you can either go for (int *) or
+             * (void *).
+             */
 #if defined(OPENSSL_SYS_WINDOWS) || defined(OPENSSL_SYS_MSDOS)
             /*
              * Under Windows/DOS we make the assumption that we can always
