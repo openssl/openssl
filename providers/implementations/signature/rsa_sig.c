d379cfd1ba30c5efcc427da939a8fcd7d331b74f
Tomas Mraz <tomas@openssl.org>
2026-01-21 19:10:28 +0100

    rsa_sig.c: Properly duplicate the sig member
    Otherwise UAF and doublefree appears when the duplicate
    is freed.
    
    Reviewed-by: Richard Levitte <levitte@openssl.org>
    Reviewed-by: Eugene Syromiatnikov <esyr@openssl.org>
    Reviewed-by: Paul Dale <paul.dale@oracle.com>
    MergeDate: Fri Jan 23 10:40:03 2026
    (Merged from https://github.com/openssl/openssl/pull/29707)

--- providers/implementations/signature/rsa_sig.c
+++ providers/implementations/signature/rsa_sig.c
@@ -1021,6 +1021,7 @@ static void *rsa_dupctx(void *vprsactx)
     dstctx->mdctx = NULL;
     dstctx->tbuf = NULL;
     dstctx->propq = NULL;
+    dstctx->sig = NULL;
 
     if (srcctx->rsa != NULL && !RSA_up_ref(srcctx->rsa))
         goto err;
@@ -1044,6 +1045,12 @@ static void *rsa_dupctx(void *vprsactx)
     if (srcctx->propq != NULL) {
         dstctx->propq = OPENSSL_strdup(srcctx->propq);
         if (dstctx->propq == NULL)
+            goto err;
+    }
+
+    if (srcctx->sig != NULL) {
+        dstctx->sig = OPENSSL_memdup(srcctx->sig, srcctx->siglen);
+        if (dstctx->sig == NULL)
             goto err;
     }
 
