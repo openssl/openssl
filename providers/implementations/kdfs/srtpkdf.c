openssl/openssl

commit fe67753da40968765166f13f128ae7a3dc54081b

Add SRTPKDF implementation
      In compliance with RFC 3711, Section 4.3.3

Reviewed-by: Paul Dale <paul.dale@oracle.com>
Reviewed-by: Shane Lontis <shane.lontis@oracle.com>
MergeDate: Fri Jan 23 10:19:32 2026
(Merged from https://github.com/openssl/openssl/pull/29435)

providers/implementations/kdfs/srtpkdf.c
@@ -1,0 +1,445 @@
+/*
+ * Copyright 2026 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <openssl/evp.h>
+#include <openssl/kdf.h>
+#include <openssl/bn.h>
+#include <openssl/core_names.h>
+#include <openssl/proverr.h>
+#include "internal/cryptlib.h"
+#include "internal/numbers.h"
+#include "crypto/evp.h"
+#include "prov/provider_ctx.h"
+#include "prov/providercommon.h"
+#include "prov/implementations.h"
+#include "prov/provider_util.h"
+#include "providers/implementations/kdfs/srtpkdf.inc"
+
+#define KDF_SRTP_AUTH_KEY_LEN 20
+#define KDF_SRTP_SALT_KEY_LEN 14
+#define KDF_SRTCP_AUTH_KEY_LEN KDF_SRTP_AUTH_KEY_LEN
+#define KDF_SRTCP_SALT_KEY_LEN KDF_SRTP_SALT_KEY_LEN
+#define KDF_SRTP_SALT_LEN 14
+#define KDF_SRTP_KDR_LEN 6
+#define KDF_SRTP_IDX_LEN 6
+#define KDF_SRTCP_IDX_LEN 4
+#define KDF_SRTP_IV_LEN 16
+#define KDF_SRTP_MAX_KDR 24
+#define KDF_SRTP_MAX_LABEL 7
+#define KDF_SRTP_MAX_SALT_LEN (KDF_SRTP_SALT_LEN + 2)
+
+/* See RFC 3711, Section 4.3.3 */
+static OSSL_FUNC_kdf_newctx_fn kdf_srtpkdf_new;
+static OSSL_FUNC_kdf_dupctx_fn kdf_srtpkdf_dup;
+static OSSL_FUNC_kdf_freectx_fn kdf_srtpkdf_free;
+static OSSL_FUNC_kdf_reset_fn kdf_srtpkdf_reset;
+static OSSL_FUNC_kdf_derive_fn kdf_srtpkdf_derive;
+static OSSL_FUNC_kdf_settable_ctx_params_fn kdf_srtpkdf_settable_ctx_params;
+static OSSL_FUNC_kdf_set_ctx_params_fn kdf_srtpkdf_set_ctx_params;
+static OSSL_FUNC_kdf_gettable_ctx_params_fn kdf_srtpkdf_gettable_ctx_params;
+static OSSL_FUNC_kdf_get_ctx_params_fn kdf_srtpkdf_get_ctx_params;
+
+static int SRTPKDF(OSSL_LIB_CTX *provctx, const EVP_CIPHER *cipher,
+    const unsigned char *mkey, const unsigned char *msalt, const unsigned char *index,
+    const uint32_t kdr, const uint32_t kdr_n,
+    const uint32_t label, unsigned char *obuffer, const size_t keylen);
+
+typedef struct {
+    /* Warning: Any changes to this structure may require you to update kdf_srtpkdf_dup */
+    void *provctx;
+    PROV_CIPHER cipher;
+    unsigned char *key;
+    size_t key_len;
+    unsigned char *salt;
+    size_t salt_len;
+    unsigned char *index;
+    size_t index_len;
+    uint32_t kdr;
+    uint32_t kdr_n; /* 2 ** kdr_n = kdr */
+    uint32_t label;
+} KDF_SRTPKDF;
+
+static void *kdf_srtpkdf_new(void *provctx)
+{
+    KDF_SRTPKDF *ctx;
+
+    if (!ossl_prov_is_running())
+        return NULL;
+
+    if ((ctx = OPENSSL_zalloc(sizeof(*ctx))) != NULL)
+        ctx->provctx = provctx;
+    return ctx;
+}
+
+static void *kdf_srtpkdf_dup(void *vsrc)
+{
+    const KDF_SRTPKDF *src = (const KDF_SRTPKDF *)vsrc;
+    KDF_SRTPKDF *dest;
+
+    dest = kdf_srtpkdf_new(src->provctx);
+    if (dest != NULL) {
+        if (!ossl_prov_memdup(src->key, src->key_len,
+                &dest->key, &dest->key_len)
+            || !ossl_prov_memdup(src->salt, src->salt_len,
+                &dest->salt, &dest->salt_len)
+            || !ossl_prov_memdup(src->index, src->index_len,
+                &dest->index, &dest->index_len)
+            || !ossl_prov_cipher_copy(&dest->cipher, &src->cipher))
+            goto err;
+        dest->kdr = src->kdr;
+        dest->kdr_n = src->kdr_n;
+        dest->label = src->label;
+    }
+    return dest;
+
+err:
+    kdf_srtpkdf_free(dest);
+    return NULL;
+}
+
+static void kdf_srtpkdf_free(void *vctx)
+{
+    KDF_SRTPKDF *ctx = (KDF_SRTPKDF *)vctx;
+
+    if (ctx != NULL) {
+        kdf_srtpkdf_reset(ctx);
+        OPENSSL_free(ctx);
+    }
+}
+
+static void kdf_srtpkdf_reset(void *vctx)
+{
+    KDF_SRTPKDF *ctx = (KDF_SRTPKDF *)vctx;
+    void *provctx = ctx->provctx;
+
+    ossl_prov_cipher_reset(&ctx->cipher);
+    OPENSSL_clear_free(ctx->key, ctx->key_len);
+    OPENSSL_clear_free(ctx->index, ctx->index_len);
+    OPENSSL_clear_free(ctx->salt, ctx->salt_len);
+    memset(ctx, 0, sizeof(*ctx));
+    ctx->provctx = provctx;
+}
+
+static int srtpkdf_set_membuf(unsigned char **dst, size_t *dst_len,
+    const OSSL_PARAM *p)
+{
+    OPENSSL_clear_free(*dst, *dst_len);
+    *dst = NULL;
+    *dst_len = 0;
+    return OSSL_PARAM_get_octet_string(p, (void **)dst, 0, dst_len);
+}
+
+static int is_power_of_two(uint32_t x, uint32_t *n)
+{
+    /* Check if we've been given an exact power of two */
+    if (x == 0 || (x & (x - 1)) != 0) {
+        *n = 0;
+        return 0;
+    }
+    /* Count the number of trailing bits in the passed value */
+#ifdef __GNUC__
+    *n = __builtin_ctz(x);
+#else
+    {
+        uint32_t count = 0;
+        while ((x & 1) == 0) {
+            count++;
+            x >>= 1;
+        }
+        *n = count;
+    }
+#endif
+    return 1;
+}
+
+static int kdf_srtpkdf_derive(void *vctx, unsigned char *key, size_t keylen,
+    const OSSL_PARAM params[])
+{
+    KDF_SRTPKDF *ctx = (KDF_SRTPKDF *)vctx;
+    const EVP_CIPHER *cipher;
+    OSSL_LIB_CTX *libctx = PROV_LIBCTX_OF(ctx->provctx);
+
+    if (!ossl_prov_is_running() || !kdf_srtpkdf_set_ctx_params(ctx, params))
+        return 0;
+
+    cipher = ossl_prov_cipher_cipher(&ctx->cipher);
+    if (cipher == NULL) {
+        ERR_raise(ERR_LIB_PROV, PROV_R_MISSING_CIPHER);
+        return 0;
+    }
+    if (ctx->key == NULL) {
+        ERR_raise(ERR_LIB_PROV, PROV_R_MISSING_KEY);
+        return 0;
+    }
+    if (ctx->salt == NULL) {
+        ERR_raise(ERR_LIB_PROV, PROV_R_MISSING_SALT);
+        return 0;
+    }
+    if (ctx->kdr > 0) {
+        uint32_t n = 0;
+        if (!is_power_of_two(ctx->kdr, &n)
+            || n > KDF_SRTP_MAX_KDR) {
+            ERR_raise(ERR_LIB_PROV, PROV_R_INVALID_KDR);
+            return 0;
+        }
+        ctx->kdr_n = n;
+    }
+    if (ctx->label > KDF_SRTP_MAX_LABEL) {
+        ERR_raise(ERR_LIB_PROV, PROV_R_INVALID_LABEL);
+        return 0;
+    }
+
+    return SRTPKDF(libctx, cipher, ctx->key, ctx->salt, ctx->index,
+        ctx->kdr, ctx->kdr_n, ctx->label, key, keylen);
+}
+
+static int kdf_srtpkdf_set_ctx_params(void *vctx, const OSSL_PARAM params[])
+{
+    struct srtp_set_ctx_params_st p;
+    KDF_SRTPKDF *ctx = vctx;
+    OSSL_LIB_CTX *libctx = PROV_LIBCTX_OF(ctx->provctx);
+    const EVP_CIPHER *cipher;
+
+    if (params == NULL)
+        return 1;
+
+    if (ctx == NULL || !srtp_set_ctx_params_decoder(params, &p))
+        return 0;
+
+    if ((p.cipher != NULL)
+        && !ossl_prov_cipher_load(&ctx->cipher, p.cipher, p.propq, libctx))
+        return 0;
+
+    cipher = ossl_prov_cipher_cipher(&ctx->cipher);
+    if (cipher == NULL)
+        return 0;
+    if (!EVP_CIPHER_is_a(cipher, "AES-128-CTR") && !EVP_CIPHER_is_a(cipher, "AES-192-CTR")
+        && !EVP_CIPHER_is_a(cipher, "AES-256-CTR"))
+        return 0;
+
+    if ((p.key != NULL)
+        && !srtpkdf_set_membuf(&ctx->key, &ctx->key_len, p.key))
+        return 0;
+
+    if ((p.salt != NULL)
+        && !srtpkdf_set_membuf(&ctx->salt, &ctx->salt_len, p.salt))
+        return 0;
+
+    if ((p.index != NULL)
+        && !srtpkdf_set_membuf(&ctx->index, &ctx->index_len, p.index))
+        return 0;
+
+    if (p.kdr != NULL) {
+        if (!OSSL_PARAM_get_uint32(p.kdr, &ctx->kdr))
+            return 0;
+    }
+
+    if (p.label != NULL) {
+        if (!OSSL_PARAM_get_uint32(p.label, &ctx->label))
+            return 0;
+    }
+
+    return 1;
+}
+
+static const OSSL_PARAM *kdf_srtpkdf_settable_ctx_params(ossl_unused void *ctx,
+    ossl_unused void *p_ctx)
+{
+    return srtp_set_ctx_params_list;
+}
+
+static int kdf_srtpkdf_get_ctx_params(void *vctx, OSSL_PARAM params[])
+{
+    struct srtp_get_ctx_params_st p;
+    KDF_SRTPKDF *ctx = vctx;
+
+    if (ctx == NULL || !srtp_get_ctx_params_decoder(params, &p))
+        return 0;
+
+    if (p.size != NULL) {
+        size_t sz = EVP_CIPHER_key_length(ossl_prov_cipher_cipher(&ctx->cipher));
+
+        if (!OSSL_PARAM_set_size_t(p.size, sz))
+            return 0;
+    }
+    return 1;
+}
+
+static const OSSL_PARAM *kdf_srtpkdf_gettable_ctx_params(ossl_unused void *ctx,
+    ossl_unused void *p_ctx)
+{
+    return srtp_get_ctx_params_list;
+}
+
+const OSSL_DISPATCH ossl_kdf_srtpkdf_functions[] = {
+    { OSSL_FUNC_KDF_NEWCTX, (void (*)(void))kdf_srtpkdf_new },
+    { OSSL_FUNC_KDF_DUPCTX, (void (*)(void))kdf_srtpkdf_dup },
+    { OSSL_FUNC_KDF_FREECTX, (void (*)(void))kdf_srtpkdf_free },
+    { OSSL_FUNC_KDF_RESET, (void (*)(void))kdf_srtpkdf_reset },
+    { OSSL_FUNC_KDF_DERIVE, (void (*)(void))kdf_srtpkdf_derive },
+    { OSSL_FUNC_KDF_SETTABLE_CTX_PARAMS,
+        (void (*)(void))kdf_srtpkdf_settable_ctx_params },
+    { OSSL_FUNC_KDF_SET_CTX_PARAMS,
+        (void (*)(void))kdf_srtpkdf_set_ctx_params },
+    { OSSL_FUNC_KDF_GETTABLE_CTX_PARAMS,
+        (void (*)(void))kdf_srtpkdf_gettable_ctx_params },
+    { OSSL_FUNC_KDF_GET_CTX_PARAMS,
+        (void (*)(void))kdf_srtpkdf_get_ctx_params },
+    { 0, NULL }
+};
+
+/*
+ * SRTPKDF - In compliance with SP800-135 and RFC3711, calculate
+ *           various keys defined by label using a master key,
+ *           master salt, kdr(if non-zero) and index.
+ *
+ * Denote the cryptographic key (encryption key, cipher salt or
+ * authentication key(HMAC key), etc) to be derived as K. The
+ * length of K is denoted by L. Below is a description of the KDF.
+ *
+ * master_salt: a random non-salt value.
+ * kdr: the key derivation rate. kdr is a number from the set
+ *   factor of 2.
+ * index: a 48-bit value in RTP or a 32-bit value in RTCP.
+ *   See Sections 3.2.1 and 4.3.2 of RFC 3711 for details.
+ * A function, DIV, is defined as followed:
+ *   a and x are non-negative integers.
+ *   a DIV x =  a | x (a DIV x) is represented as a bit string whose
+ *   length (in bits) is the same as a.
+ * label: an 8-bit value represented by two hexadecimal numbers from
+ *   the set of {0x00,0x01, 0x02, 0x03, 0x04, 0x05}.
+ *   https://www.ietf.org/archive/id/draft-ietf-avtcore-srtp-encrypted-header-ext-01.html
+ *   The values 06 and 07 are used.
+ * key_id = label || (index DIV kdr)
+ *
+ * Input:
+ *   cipher - AES cipher
+ *   mkey - pointer to master key
+ *   msalt - pointer to master salt
+ *   idx - pointer to index
+ *   kdr - key derivation rate
+ *   kdr_n - power of kdr (2**kdr_n = kdr)
+ *   label - 8-bit label
+ *   obuffer - buffer for output
+ *   keylen - length of output buffer
+ * Output:
+ *   obuffer - filled with derived key
+ *   return - 1 on pass, 0 fail
+ */
+int SRTPKDF(OSSL_LIB_CTX *provctx, const EVP_CIPHER *cipher,
+    const unsigned char *mkey, const unsigned char *msalt, const unsigned char *index,
+    const uint32_t kdr, const uint32_t kdr_n,
+    const uint32_t label, unsigned char *obuffer, const size_t keylen)
+{
+    EVP_CIPHER_CTX *ctx = NULL;
+    int outl, i, index_len = 0, o_len = 0, salt_len = 0;
+    unsigned char buf[EVP_MAX_KEY_LENGTH];
+    unsigned char iv[KDF_SRTP_IV_LEN];
+    unsigned char local_salt[KDF_SRTP_MAX_SALT_LEN];
+    unsigned char master_salt[KDF_SRTP_MAX_SALT_LEN];
+    BIGNUM *bn_index = NULL, *bn_salt = NULL;
+    int ret, iv_len = KDF_SRTP_IV_LEN, rv = 0;
+
+    salt_len = KDF_SRTP_SALT_LEN;
+
+    /* get label-specific lengths */
+    switch (label) {
+    case 0:
+        index_len = KDF_SRTP_IDX_LEN;
+        o_len = EVP_CIPHER_key_length(cipher);
+        break;
+    case 1:
+        index_len = KDF_SRTP_IDX_LEN;
+        o_len = KDF_SRTP_AUTH_KEY_LEN;
+        break;
+    case 2:
+        index_len = KDF_SRTP_IDX_LEN;
+        o_len = KDF_SRTP_SALT_KEY_LEN;
+        break;
+    case 3:
+        index_len = KDF_SRTCP_IDX_LEN;
+        o_len = EVP_CIPHER_key_length(cipher);
+        break;
+    case 4:
+        index_len = KDF_SRTCP_IDX_LEN;
+        o_len = KDF_SRTCP_AUTH_KEY_LEN;
+        break;
+    case 5:
+        index_len = KDF_SRTCP_IDX_LEN;
+        o_len = KDF_SRTCP_SALT_KEY_LEN;
+        break;
+    case 6:
+        index_len = KDF_SRTP_IDX_LEN;
+        o_len = EVP_CIPHER_key_length(cipher);
+        break;
+    case 7:
+        index_len = KDF_SRTP_IDX_LEN;
+        o_len = KDF_SRTP_SALT_KEY_LEN;
+        break;
+    default:
+        return rv;
+    }
+
+    if ((obuffer == NULL) || (keylen > INT_MAX) || (o_len > (int)keylen))
+        return rv;
+
+    /* set up a couple of work areas for the final logic on the salt */
+    memset(iv, 0, KDF_SRTP_IV_LEN);
+    memset(master_salt, 0, KDF_SRTP_MAX_SALT_LEN);
+    memcpy(master_salt, msalt, salt_len);
+
+    /* gather some bignums for some math */
+    bn_index = BN_new();
+    bn_salt = BN_new();
+    if ((bn_index == NULL) || (bn_salt == NULL)) {
+        BN_free(bn_index);
+        BN_free(bn_salt);
+        return rv;
+    }
+
+    /* if index is NULL or kdr=0, then index and kdr are not in play */
+    if ((index != NULL) && (kdr > 0)) {
+        if (!BN_bin2bn(index, index_len, bn_index))
+            goto err;
+
+        ret = BN_rshift(bn_salt, bn_index, kdr_n);
+        if (!ret)
+            goto err;
+        iv_len = BN_bn2bin(bn_salt, iv);
+        for (i = 1; i <= iv_len; i++)
+            master_salt[salt_len - i] ^= iv[iv_len - i];
+    }
+
+    /* take the munged up salt from above and add the label */
+    memset(local_salt, 0, KDF_SRTP_MAX_SALT_LEN);
+    memcpy(local_salt, master_salt, salt_len);
+    local_salt[((KDF_SRTP_SALT_LEN - 1) - index_len)] ^= label;
+
+    /* perform the AES encryption on the master key and derived salt */
+    memset(buf, 0, o_len);
+    if (!(ctx = EVP_CIPHER_CTX_new())
+        || (EVP_EncryptInit_ex(ctx, cipher, NULL, mkey, local_salt) <= 0)
+        || (EVP_CIPHER_CTX_set_padding(ctx, 0) <= 0)
+        || (EVP_EncryptUpdate(ctx, (unsigned char *)obuffer, &outl, buf, o_len) <= 0)
+        || (EVP_EncryptFinal_ex(ctx, (unsigned char *)obuffer, &outl) <= 0))
+        goto err;
+
+    rv = 1;
+err:
+    EVP_CIPHER_CTX_free(ctx);
+    OPENSSL_cleanse(iv, KDF_SRTP_IV_LEN);
+    OPENSSL_cleanse(local_salt, KDF_SRTP_MAX_SALT_LEN);
+    OPENSSL_cleanse(master_salt, KDF_SRTP_IV_LEN);
+    BN_clear_free(bn_index);
+    BN_clear_free(bn_salt);
+    return rv;
+}

.github/workflows/run-checker-daily.yml
@@ -105,6 +105,7 @@ jobs:
           no-sm4,
           no-snmpkdf,
           no-sock,
+          no-srtpkdf,
           no-sse2,
           no-sshkdf,
           no-sskdf,

.gitignore
@@ -165,6 +165,7 @@ providers/implementations/digests/sha3_prov.inc
 providers/implementations/digests/ml_dsa_mu_prov.inc
 providers/implementations/include/prov/blake2_params.inc
 providers/implementations/kdfs/snmpkdf.inc
+providers/implementations/kdfs/srtpkdf.inc
 providers/implementations/macs/cmac_prov.inc
 providers/implementations/macs/gmac_prov.inc
 providers/implementations/macs/hmac_prov.inc

Configure
@@ -535,6 +535,7 @@ my @disablables = (
     "sock",
     "srp",
     "srtp",
+    "srtpkdf",
     "sse2",
     "sshkdf",
     "sskdf",
@@ -655,7 +656,7 @@ my @disable_cascades = (
                              "pvkkdf", "rc2", "rc4", "rmd160",
                              "scrypt", "seed", "siphash", "siv",
                              "slh-dsa", "sm3", "sm4", "snmpkdf",
-                             "srp", "srtp", "sshkdf", "sskdf",
+                             "srp", "srtp", "srtpkdf", "sshkdf", "sskdf",
                              "ssl-trace",
                              "tfo",
                              "ts", "ui-console", "whirlpool",

build.info
@@ -68,6 +68,7 @@ DEPEND[]=include/openssl/asn1.h \
          providers/implementations/kdfs/pvkkdf.inc \
          providers/implementations/kdfs/scrypt.inc \
          providers/implementations/kdfs/snmpkdf.inc \
+         providers/implementations/kdfs/srtpkdf.inc \
          providers/implementations/kdfs/sshkdf.inc \
          providers/implementations/kdfs/sskdf.inc \
          providers/implementations/kdfs/tls1_prf.inc \
@@ -189,6 +190,7 @@ DEPEND[providers/implementations/asymciphers/rsa_enc.inc \
        providers/implementations/kdfs/pvkkdf.inc \
        providers/implementations/kdfs/scrypt.inc \
        providers/implementations/kdfs/snmpkdf.inc \
+       providers/implementations/kdfs/srtpkdf.inc \
        providers/implementations/kdfs/sshkdf.inc \
        providers/implementations/kdfs/sskdf.inc \
        providers/implementations/kdfs/tls1_prf.inc \
@@ -301,6 +303,8 @@ GENERATE[providers/implementations/kdfs/scrypt.inc]=\
     providers/implementations/kdfs/scrypt.inc.in
 GENERATE[providers/implementations/kdfs/snmpkdf.inc]=\
     providers/implementations/kdfs/snmpkdf.inc.in
+GENERATE[providers/implementations/kdfs/srtpkdf.inc]=\
+    providers/implementations/kdfs/srtpkdf.inc.in
 GENERATE[providers/implementations/kdfs/sshkdf.inc]=\
     providers/implementations/kdfs/sshkdf.inc.in
 GENERATE[providers/implementations/kdfs/sskdf.inc]=\

doc/build.info
@@ -4629,6 +4629,10 @@ DEPEND[html/man7/EVP_KDF-SNMPKDF.html]=man7/EVP_KDF-SNMPKDF.pod
 GENERATE[html/man7/EVP_KDF-SNMPKDF.html]=man7/EVP_KDF-SNMPKDF.pod
 DEPEND[man/man7/EVP_KDF-SNMPKDF.7]=man7/EVP_KDF-SNMPKDF.pod
 GENERATE[man/man7/EVP_KDF-SNMPKDF.7]=man7/EVP_KDF-SNMPKDF.pod
+DEPEND[html/man7/EVP_KDF-SRTPKDF.html]=man7/EVP_KDF-SRTPKDF.pod
+GENERATE[html/man7/EVP_KDF-SRTPKDF.html]=man7/EVP_KDF-SRTPKDF.pod
+DEPEND[man/man7/EVP_KDF-SRTPKDF.7]=man7/EVP_KDF-SRTPKDF.pod
+GENERATE[man/man7/EVP_KDF-SRTPKDF.7]=man7/EVP_KDF-SRTPKDF.pod
 DEPEND[html/man7/EVP_KDF-SS.html]=man7/EVP_KDF-SS.pod
 GENERATE[html/man7/EVP_KDF-SS.html]=man7/EVP_KDF-SS.pod
 DEPEND[man/man7/EVP_KDF-SS.7]=man7/EVP_KDF-SS.pod
@@ -5204,6 +5208,7 @@ html/man7/EVP_KDF-PKCS12KDF.html \
 html/man7/EVP_KDF-PVKKDF.html \
 html/man7/EVP_KDF-SCRYPT.html \
 html/man7/EVP_KDF-SNMPKDF.html \
+html/man7/EVP_KDF-SRTPKDF.html \
 html/man7/EVP_KDF-SS.html \
 html/man7/EVP_KDF-SSHKDF.html \
 html/man7/EVP_KDF-TLS13_KDF.html \
@@ -5366,6 +5371,7 @@ man/man7/EVP_KDF-PKCS12KDF.7 \
 man/man7/EVP_KDF-PVKKDF.7 \
 man/man7/EVP_KDF-SCRYPT.7 \
 man/man7/EVP_KDF-SNMPKDF.7 \
+man/man7/EVP_KDF-SRTPKDF.7 \
 man/man7/EVP_KDF-SS.7 \
 man/man7/EVP_KDF-SSHKDF.7 \
 man/man7/EVP_KDF-TLS13_KDF.7 \

doc/man1/openssl-kdf.pod.in
@@ -141,7 +141,7 @@ This option is identical to the B<-mac> option.
 
 Specifies the name of a supported KDF algorithm which will be used.
 The supported algorithms names include TLS1-PRF, HKDF, SSKDF, PBKDF2,
-SNMPKDF, SSHKDF, X942KDF-ASN1, X942KDF-CONCAT, X963KDF and SCRYPT.
+SNMPKDF, SRTPKDF, SSHKDF, X942KDF-ASN1, X942KDF-CONCAT, X963KDF and SCRYPT.
 
 =back
 
@@ -181,6 +181,14 @@ Use SNMPKDF to create a hex-encoded derived key from an engine ID, hash and pass
                 -kdfopt pass:IFUcNbMl \
                 -kdfopt hexeid:800002b805123456789abcdef0123456789abcdef0123456789abcdef0123456 SNMPKDF
 
+Use SRTPKDF to create a SRTP authentication derived key from a cipher, mkey, msalt,
+    kdr, index and label:
+
+    openssl kdf -keylen 20 -kdfopt cipher:AES-128-CTR \
+                -kdfopt key:E1F97A0D3E018BE0D64FA32C06DE4139 \
+                -kdfopt salt:0EC675AD498AFEEBB6960B3AABE6 \
+                -kdfopt index:000000000000 -kdfopt label:1 SRTPKDF
+
 Use SSHKDF to create a hex-encoded derived key from a secret key, hash and session_id:
 
     openssl kdf -keylen 16 -kdfopt digest:SHA2-256 \
@@ -215,6 +223,7 @@ L<EVP_KDF-TLS1_PRF(7)>,
 L<EVP_KDF-PBKDF2(7)>,
 L<EVP_KDF-HKDF(7)>,
 L<EVP_KDF-SNMPKDF(7)>,
+L<EVP_KDF-SRTPKDF(7)>,
 L<EVP_KDF-SS(7)>,
 L<EVP_KDF-SSHKDF(7)>,
 L<EVP_KDF-X942-ASN1(7)>,

doc/man7/EVP_KDF-SRTPKDF.pod
@@ -1,0 +1,197 @@
+=pod
+
+=head1 NAME
+
+EVP_KDF-SRTPKDF - The SRTP EVP_KDF implementation
+
+=head1 DESCRIPTION
+
+Support for computing the B<SRTP> KDF through the B<EVP_KDF> API.
+
+The EVP_KDF-SRTP algorithm implements the SRTP key derivation function.
+SRTP follows the specification in RFC 3711 Section 4.3.3, where various
+cryptographic keys (encryption, authentication, and salt keys) are derived
+from a master key and master salt using AES encryption with specific labels.
+
+The output keys are used for SRTP and SRTCP packet protection.
+
+=head2 Identity
+
+"SRTP" is the name for this implementation; it can be used with the
+EVP_KDF_fetch() function.
+
+=head2 Supported parameters
+
+The supported parameters are:
+
+=over 4
+
+=item "properties" (B<OSSL_KDF_PARAM_PROPERTIES>) <UTF8 string>
+
+=item "cipher" (B<OSSL_KDF_PARAM_CIPHER>) <UTF8 string>
+
+This parameter sets the cipher to be used for the key derivation.
+Typically "AES-128-CTR" or "AES-256-CTR" is used.
+
+=item "key" (B<OSSL_KDF_PARAM_KEY>) <octet string>
+
+This parameter sets the master key value. This is typically 16 bytes
+for AES-128 or 32 bytes for AES-256.
+
+=item "salt" (B<OSSL_KDF_PARAM_SALT>) <octet string>
+
+This parameter sets the master salt value. This is typically 14 bytes
+as specified in RFC 3711.
+
+=item "kdr" (B<OSSL_KDF_PARAM_SRTPKDF_KDR>) <unsigned integer>
+
+This parameter sets the key derivation rate (KDR). The KDR controls
+how often keys are rederived. If not set or set to zero, no key
+rederivation is performed. The KDR value is power of 2 (range 2^0 to 2^24).
+
+=item "index" (B<OSSL_KDF_PARAM_SRTPKDF_INDEX>) <octet string>
+
+This parameter sets the index value used in key derivation. For RTP
+packets, this is typically a 48-bit (6 byte) value. For RTCP packets,
+this is typically a 32-bit (4 byte) value. If not set, defaults to zero.
+
+=item "label" (B<OSSL_KDF_PARAM_SRTPKDF_LABEL>) <unsigned integer>
+
+This parameter sets the label that identifies the type of key to derive.
+Valid values are:
+
+=over 4
+
+=item 0 - SRTP encryption key
+
+=item 1 - SRTP authentication key
+
+=item 2 - SRTP salt key
+
+=item 3 - SRTCP encryption key
+
+=item 4 - SRTCP authentication key
+
+=item 5 - SRTCP salt key
+
+=item 6 - SRTP encryption key (alternative)
+
+=item 7 - SRTP salt key (alternative)
+
+=back
+
+=back
+
+=head1 NOTES
+
+A context for SRTP can be obtained by calling:
+
+ EVP_KDF *kdf = EVP_KDF_fetch(NULL, "SRTP", NULL);
+ EVP_KDF_CTX *kctx = EVP_KDF_CTX_new(kdf);
+
+The output length of the SRTP KDF operation is determined by the label:
+
+=over 4
+
+=item Labels 0, 3, 6: Output length equals the cipher key length
+
+=item Labels 1, 4: Output length is 20 bytes (160 bits)
+
+=item Labels 2, 5, 7: Output length is 14 bytes (112 bits)
+
+=back
+
+=head1 EXAMPLES
+
+This example derives an SRTP encryption key (label 0) using AES-128-CTR
+with a 16-byte master key and 14-byte master salt:
+
+ EVP_KDF *kdf;
+ EVP_KDF_CTX *kctx;
+ unsigned char out[16];
+ unsigned char master_key[16] = { /* master key bytes */ };
+ unsigned char master_salt[14] = { /* master salt bytes */ };
+ uint32_t label = 0;
+ OSSL_PARAM params[5], *p = params;
+
+ kdf = EVP_KDF_fetch(NULL, "SRTP", NULL);
+ kctx = EVP_KDF_CTX_new(kdf);
+ EVP_KDF_free(kdf);
+
+ *p++ = OSSL_PARAM_construct_utf8_string(OSSL_KDF_PARAM_CIPHER,
+                                         "AES-128-CTR", 0);
+ *p++ = OSSL_PARAM_construct_octet_string(OSSL_KDF_PARAM_KEY,
+                                          master_key, sizeof(master_key));
+ *p++ = OSSL_PARAM_construct_octet_string(OSSL_KDF_PARAM_SALT,
+                                          master_salt, sizeof(master_salt));
+ *p++ = OSSL_PARAM_construct_uint32(OSSL_KDF_PARAM_SRTPKDF_LABEL, &label);
+ *p = OSSL_PARAM_construct_end();
+
+ if (EVP_KDF_derive(kctx, out, sizeof(out), params) <= 0) {
+     error("EVP_KDF_derive");
+ }
+
+ EVP_KDF_CTX_free(kctx);
+
+This example derives an SRTP authentication key (label 1) with key derivation
+rate and index:
+
+ EVP_KDF *kdf;
+ EVP_KDF_CTX *kctx;
+ unsigned char out[20];
+ unsigned char master_key[16] = { /* master key bytes */ };
+ unsigned char master_salt[14] = { /* master salt bytes */ };
+ uint32_t kdr = 0x1000; /* KDR */
+ unsigned char index[6] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 }; /* index */
+ uint32_t label = 1;
+ OSSL_PARAM params[7], *p = params;
+
+ kdf = EVP_KDF_fetch(NULL, "SRTP", NULL);
+ kctx = EVP_KDF_CTX_new(kdf);
+ EVP_KDF_free(kdf);
+
+ *p++ = OSSL_PARAM_construct_utf8_string(OSSL_KDF_PARAM_CIPHER,
+                                         "AES-128-CTR", 0);
+ *p++ = OSSL_PARAM_construct_octet_string(OSSL_KDF_PARAM_KEY,
+                                          master_key, sizeof(master_key));
+ *p++ = OSSL_PARAM_construct_octet_string(OSSL_KDF_PARAM_SALT,
+                                          master_salt, sizeof(master_salt));
+ *p++ = OSSL_PARAM_construct_uint32(OSSL_KDF_PARAM_SRTPKDF_KDR, &kdr);
+ *p++ = OSSL_PARAM_construct_octet_string(OSSL_KDF_PARAM_SRTPKDF_INDEX,
+                                          index, sizeof(index));
+ *p++ = OSSL_PARAM_construct_uint32(OSSL_KDF_PARAM_SRTPKDF_LABEL, &label);
+ *p = OSSL_PARAM_construct_end();
+
+ if (EVP_KDF_derive(kctx, out, sizeof(out), params) <= 0) {
+     error("EVP_KDF_derive");
+ }
+
+ EVP_KDF_CTX_free(kctx);
+
+=head1 CONFORMING TO
+
+RFC 3711 Section 4.3.3 (SRTP Key Derivation)
+
+=head1 SEE ALSO
+
+L<EVP_KDF(3)>,
+L<EVP_KDF_CTX_new(3)>,
+L<EVP_KDF_CTX_free(3)>,
+L<EVP_KDF_CTX_set_params(3)>,
+L<EVP_KDF_derive(3)>,
+L<EVP_KDF(3)/PARAMETERS>
+
+=head1 HISTORY
+
+The SRTPKDF was added in OpenSSL 4.0.0.
+
+=head1 COPYRIGHT
+
+Copyright 2026 The OpenSSL Project Authors. All Rights Reserved.
+
+Licensed under the Apache License 2.0 (the "License").  You may not use
+this file except in compliance with the License.  You can obtain a copy
+in the file LICENSE in the source distribution or at
+L<https://www.openssl.org/source/license.html>.
+
+=cut

commit d13c44b0931e5feb4291a3640c9178e03075aa17

srtpkdf.c: avoid ctx NULL dereference kdf_srtpkdf_set_ctx_params()
ctx is dereferenced before NULL check to obtain libctx.  Fix it
by moving the dereference after the NULL check.

Resolves: https://scan5.scan.coverity.com/#/project-view/65248/10222?selectedIssue=1680648
Fixes: fe67753da4096 "Add SRTPKDF implementation"
Signed-off-by: Eugene Syromiatnikov <esyr@openssl.org>

providers/implementations/kdfs/srtpkdf.c
@@ -206,7 +206,7 @@ static int kdf_srtpkdf_set_ctx_params(void *vctx, const OSSL_PARAM params[])
 {
     struct srtp_set_ctx_params_st p;
     KDF_SRTPKDF *ctx = vctx;
-    OSSL_LIB_CTX *libctx = PROV_LIBCTX_OF(ctx->provctx);
+    OSSL_LIB_CTX *libctx;
     const EVP_CIPHER *cipher;
 
     if (params == NULL)
@@ -214,6 +214,8 @@ static int kdf_srtpkdf_set_ctx_params(void *vctx, const OSSL_PARAM params[])
 
     if (ctx == NULL || !srtp_set_ctx_params_decoder(params, &p))
         return 0;
+
+    libctx = PROV_LIBCTX_OF(ctx->provctx);
 
     if ((p.cipher != NULL)
         && !ossl_prov_cipher_load(&ctx->cipher, p.cipher, p.propq, libctx))