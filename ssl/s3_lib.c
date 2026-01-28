c3f9f957d1214eedde30384a6b25096654651843
Eugene Syromiatnikov <esyr@openssl.org>
2026-01-28 16:48:03 +0100

    Apply clang-format to the source tree
    Ensure that all the source files are formatted with the current
    .clang-format configuration, to avoid spurious clang-format checker
    errors when arbitrary files are touched.
    
    Signed-off-by: Eugene Syromiatnikov <esyr@openssl.org>

--- ssl/s3_lib.c
+++ ssl/s3_lib.c
@@ -4156,7 +4156,7 @@ long ssl3_callback_ctrl(SSL *s, int cmd, void (*fp)(void))
     switch (cmd) {
 #if !defined(OPENSSL_NO_DEPRECATED_3_0)
     case SSL_CTRL_SET_TMP_DH_CB:
-        s->cert->dh_tmp_cb = (DH * (*)(SSL *, int, int)) fp;
+        s->cert->dh_tmp_cb = (DH *(*)(SSL *, int, int))fp;
         ret = 1;
         break;
 #endif
@@ -4404,7 +4404,7 @@ long ssl3_ctx_callback_ctrl(SSL_CTX *ctx, int cmd, void (*fp)(void))
     switch (cmd) {
 #if !defined(OPENSSL_NO_DEPRECATED_3_0)
     case SSL_CTRL_SET_TMP_DH_CB: {
-        ctx->cert->dh_tmp_cb = (DH * (*)(SSL *, int, int)) fp;
+        ctx->cert->dh_tmp_cb = (DH *(*)(SSL *, int, int))fp;
     } break;
 #endif
     case SSL_CTRL_SET_TLSEXT_SERVERNAME_CB:
