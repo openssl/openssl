e94626d06c3eb718f8d91644fc111bc2594f72de
Neil Horman <nhorman@openssl.org>
2026-02-02 10:24:56 -0500

    Update ossl-http3-demo to support multiple requests
    In order to use our http3 demo to do interop testing, said demo needs to
    be able to handle multiple requests and responses written to specific
    output files.
    
    Add that code here, allowing us to specify optionally a list of requests
    on the command line to send to the server, as well as a download
    directory, so that requests made get written locally to the same name as
    the request in the specified download directory.
    
    while we're at it, also clean up the code infrastructure to use SSL_poll
    to do read-ready checking, rather than iterating/mutating the internal
    hash table, which is questionable to do (i.e. we shouldn't be removing
    elements from the hash table while iterating over it).

--- demos/http3/ossl-nghttp3-demo.c
+++ demos/http3/ossl-nghttp3-demo.c
@@ -14,6 +14,19 @@
 
 static int done;
 
+/**
+ * @brief variable to record output basename path
+ */
+static char *dlpath = NULL;
+
+/**
+ * @brief struct to hold user data for async http3 writes
+ */
+struct stream_user_data {
+    char *outpath;
+    FILE *fp;
+};
+
 static void make_nv(nghttp3_nv *nv, const char *name, const char *value)
 {
     nv->name = (uint8_t *)name;
@@ -54,58 +67,88 @@ static int on_end_headers(nghttp3_conn *h3conn, int64_t stream_id,
 
 static int on_recv_data(nghttp3_conn *h3conn, int64_t stream_id,
     const uint8_t *data, size_t datalen,
-    void *conn_user_data, void *stream_user_data)
+    void *conn_user_data, void *user_data)
 {
+    FILE *outfp;
     size_t wr;
+    struct stream_user_data *sdata = OSSL_DEMO_H3_STREAM_get_user_data((const OSSL_DEMO_H3_STREAM *)user_data);
 
-    /* HTTP response body data - write it to stdout. */
+    if (dlpath == NULL) {
+        outfp = stdout;
+    } else {
+        if (sdata->fp == NULL) {
+            sdata->fp = fopen(sdata->outpath, "w+");
+        }
+        if (sdata->fp == NULL) {
+            fprintf(stderr, "Failed to open %s for writing\n", sdata->outpath);
+            return 1;
+        }
+        outfp = sdata->fp;
+    }
+    /* HTTP response body data - write it. */
     while (datalen > 0) {
-        wr = fwrite(data, 1, datalen, stdout);
-        if (ferror(stdout))
+        fprintf(stderr, "writing %lu bytes to %s\n", datalen, sdata->outpath);
+        wr = fwrite(data, 1, datalen, outfp);
+        if (ferror(outfp))
             return 1;
 
         data += wr;
         datalen -= wr;
     }
-
     return 0;
 }
 
 static int on_end_stream(nghttp3_conn *h3conn, int64_t stream_id,
-    void *conn_user_data, void *stream_user_data)
+    void *conn_user_data, void *user_data)
 {
     /* HTTP transaction is done - set done flag so that we stop looping. */
     done = 1;
     return 0;
 }
 
-static int try_conn(OSSL_DEMO_H3_CONN *conn, const char *bare_hostname)
+static int try_conn(OSSL_DEMO_H3_CONN *conn, const char *bare_hostname, const char *path)
 {
     nghttp3_nv nva[16];
     size_t num_nv = 0;
+    struct stream_user_data *sdata;
+    size_t needed_size;
 
     /* Build HTTP headers. */
     make_nv(&nva[num_nv++], ":method", "GET");
     make_nv(&nva[num_nv++], ":scheme", "https");
     make_nv(&nva[num_nv++], ":authority", bare_hostname);
-    make_nv(&nva[num_nv++], ":path", "/");
+    make_nv(&nva[num_nv++], ":path", path);
     make_nv(&nva[num_nv++], "user-agent", "OpenSSL-Demo/nghttp3");
 
+    needed_size = sizeof(struct stream_user_data);
+    needed_size += snprintf(NULL, 0, "%s/%s", dlpath, path);
+
+    sdata = malloc(needed_size + 1);
+    if (sdata == NULL)
+        return 0;
+    sdata->outpath = (char *)(sdata + 1);
+    sprintf(sdata->outpath, "%s/%s", dlpath, path);
+    sdata->fp = NULL;
+    fprintf(stderr, "Requesting %s\n", sdata->outpath);
     /* Submit request. */
-    if (!OSSL_DEMO_H3_CONN_submit_request(conn, nva, num_nv, NULL, NULL)) {
-        ERR_raise_data(ERR_LIB_USER, ERR_R_OPERATION_FAIL,
-            "cannot submit HTTP/3 request");
+    if (!OSSL_DEMO_H3_CONN_submit_request(conn, nva, num_nv, NULL, sdata)) {
+        fprintf(stderr, "Cannot submit HTTP/3 request\n");
         return 0;
     }
 
     /* Wait for request to complete. */
     done = 0;
-    while (!done)
+    while (!done) {
         if (!OSSL_DEMO_H3_CONN_handle_events(conn)) {
-            ERR_raise_data(ERR_LIB_USER, ERR_R_OPERATION_FAIL,
-                "cannot handle events");
+            fprintf(stderr, "Cannot handle events\n");
             return 0;
         }
+    }
+    if (sdata->fp != NULL) {
+        fclose(sdata->fp);
+        fprintf(stderr, "Closing local FILE pointer for %s\n", sdata->outpath);
+    }
+    free(sdata);
     return 1;
 }
 
@@ -120,11 +163,51 @@ int main(int argc, char **argv)
     char *hostname, *service;
     BIO_ADDRINFO *bai = NULL;
     const BIO_ADDRINFO *bai_walk;
+    FILE *req_fp = NULL;
+    size_t req_count = 0;
+    char **req_array = NULL;
+    size_t i;
+    char buffer[PATH_MAX];
 
     /* Check arguments. */
     if (argc < 2) {
-        fprintf(stderr, "usage: %s <host:port>\n", argv[0]);
+        fprintf(stderr, "usage: %s <host:port> [requestfile.txt <download_dir>]\n", argv[0]);
         goto err;
+    }
+
+    /*
+     * If we have more than two arguments, then we are accepting both a request file
+     * which is a newline separated list of paths to request from the server
+     * as well as a download location relative to the current working directory
+     */
+    if (argc >= 4) {
+        dlpath = argv[3];
+        fprintf(stderr, "setting download path to %s, and reading %s\n", dlpath, argv[2]);
+
+        /*
+         * Read in our request file, one path per line
+         */
+        req_fp = fopen(argv[2], "r");
+        if (req_fp == NULL) {
+            fprintf(stderr, "Unable to open request file\n");
+            goto err;
+        }
+        while (!feof(req_fp)) {
+            req_count++;
+            req_array = realloc(req_array, sizeof(char *) * req_count);
+            req_array[req_count - 1] = NULL;
+            if (fscanf(req_fp, "%s", buffer) != 1) {
+                req_count--;
+                if (feof(req_fp))
+                    break;
+                fprintf(stderr, "Failed to read request file at index %lu\n", req_count);
+                fclose(req_fp);
+                goto err;
+            }
+            req_array[req_count - 1] = calloc(1, strlen(buffer) + 1);
+            memcpy(req_array[req_count - 1], buffer, strlen(buffer));
+        }
+        fclose(req_fp);
     }
 
     addr = argv[1];
@@ -156,7 +239,7 @@ int main(int argc, char **argv)
     if ((ctx = SSL_CTX_new(OSSL_QUIC_client_method())) == NULL)
         goto err;
 
-    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, NULL);
 
     if (SSL_CTX_set_default_verify_paths(ctx) == 0)
         goto err;
@@ -183,23 +266,26 @@ int main(int argc, char **argv)
         conn = OSSL_DEMO_H3_CONN_new_for_addr(ctx, bai_walk, hostname,
             &callbacks, NULL, NULL);
         if (conn != NULL) {
-            if (try_conn(conn, addr) == 0) {
-                /*
-                 * Failure, try the next address.
-                 */
-                OSSL_DEMO_H3_CONN_free(conn);
-                conn = NULL;
-                ret = 1;
-            } else {
-                /*
-                 * Success, done.
-                 */
-                ret = 0;
-                break;
+            for (i = 0; i < req_count; i++) {
+                if (try_conn(conn, addr, req_array[i]) == 0) {
+                    /*
+                     * Failure, bail out.
+                     */
+                    OSSL_DEMO_H3_CONN_free(conn);
+                    conn = NULL;
+                    ret = 1;
+                    goto err;
+                }
             }
+            fprintf(stderr, "all requests complete\n");
+            ret = 0;
         }
     }
 err:
+    for (i = 0; i < req_count; i++)
+        free(req_array[i]);
+    free(req_array);
+
     if (ret != 0)
         ERR_print_errors_fp(stderr);
 
