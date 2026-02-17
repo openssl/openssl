9adf61690dfb89c08f26d66a7b2684f36db1244e
Simo Sorce <simo@redhat.com>
2026-02-17 11:10:21 -0500

    fixup! fixup! Annotate benign race in FIPS deferred self test

--- include/internal/threads_common.h
+++ include/internal/threads_common.h
@@ -18,8 +18,8 @@
 
 #if defined(__SANITIZE_THREAD__)
 #include <sanitizer/tsan_interface.h>
-extern void AnnotateBenignRaceSized(const char* f, int l,
-    const volatile void* mem, unsigned int size, const char* desc);
+extern void AnnotateBenignRaceSized(const char *f, int l,
+    const volatile void *mem, unsigned int size, const char *desc);
 #define TSAN_BENIGN(x, desc) \
     AnnotateBenignRaceSized(__FILE__, __LINE__, &(x), xizeof(x), desc);
 #else
