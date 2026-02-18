3032abdcd27f50a340603449df142d52bd9afd87
Norbert Pocs <norbertp@openssl.org>
2026-02-18 09:59:04 +0100

    Fix coverity issue 1681421
    Check the return value of ASN1_parse_dump
    
    Fixes: 6b167313f422 "Attribute certificate printing functions"
    
    Signed-off-by: Norbert Pocs <norbertp@openssl.org>

--- crypto/x509/t_acert.c
+++ crypto/x509/t_acert.c
@@ -62,8 +62,10 @@ static int print_attribute(BIO *bp, X509_ATTRIBUTE *a)
         case V_ASN1_SEQUENCE:
             if (BIO_puts(bp, "\n") <= 0)
                 goto err;
-            ASN1_parse_dump(bp, at->value.sequence->data,
-                at->value.sequence->length, i, 1);
+            if (ASN1_parse_dump(bp, at->value.sequence->data,
+                    at->value.sequence->length, i, 1)
+                <= 0)
+                goto err;
             break;
         default:
             if (BIO_printf(bp, "unable to print attribute of type 0x%X\n",
