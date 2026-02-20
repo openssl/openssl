080f9c9ecf0a6287773e69e0ec2191be844a6f91
Dr. David von Oheimb <dev@ddvo.net>
2026-02-20 10:41:12 +0100

    fixup! X509V3_set_nconf(): Improve error handling using this function, mostly in apps/

--- crypto/x509/v3_conf.c
+++ crypto/x509/v3_conf.c
@@ -446,6 +446,8 @@ static X509V3_CONF_METHOD nconf_method = {
 
 void X509V3_set_nconf(X509V3_CTX *ctx, CONF *conf)
 {
+    if (ctx == NULL)
+        return;
     ctx->db_meth = &nconf_method;
     ctx->db = conf;
 }
