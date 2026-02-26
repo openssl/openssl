3c5141b42877bb25f69ec230742c5548abd4daef
slontis <shane.lontis@oracle.com>
2026-02-26 20:52:56 +1100

    Fix Provider compatibility cross test when using the 4.0 FIPS provider
    OpenSSL 4.0 adds support for using EVP_DigestVerify for LMS.
    Since OpenSSL 3.6 tests if this fails, it must be updated to check
    if the OpenSSL 4.0 fips provider is being used.
    
    Related to fixing CI failure in PR #29381
    
    Reviewed-by: Viktor Dukhovni <viktor@openssl.org>
    Reviewed-by: Paul Dale <paul.dale@oracle.com>
    Reviewed-by: Matt Caswell <matt@openssl.org>
    MergeDate: Fri Feb 27 14:37:41 2026
    (Merged from https://github.com/openssl/openssl/pull/30191)

--- test/lms_test.c
+++ test/lms_test.c
@@ -281,15 +281,21 @@ static int lms_digest_verify_fail_test(void)
     LMS_ACVP_TEST_DATA *td = &lms_testdata[0];
     EVP_PKEY *pub = NULL;
     EVP_MD_CTX *vctx = NULL;
+    int expected = 0;
 
     if (!TEST_ptr(pub = lms_pubkey_from_data(td->pub, td->publen)))
         return 0;
     if (!TEST_ptr(vctx = EVP_MD_CTX_new()))
         goto err;
+
+    /* Prior to OpenSSL 4.0, EVP_DigestVerify is not supported for LMS */
+    if (OSSL_PROVIDER_available(libctx, "fips")
+        && fips_provider_version_match(libctx, ">=4.0.0"))
+        expected = 1;
     /* Only one shot mode is supported, streaming fails to initialise */
     if (!TEST_int_eq(EVP_DigestVerifyInit_ex(vctx, NULL, NULL, libctx, NULL,
                          pub, NULL),
-            0))
+            expected))
         goto err;
     ret = 1;
 err:
