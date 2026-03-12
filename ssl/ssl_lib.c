50260442757d1ca106f1fa8abd2ab2a39830231c
Ryan Hooper <ryhooper@cisco.com>
2026-03-12 13:51:31 -0400

    DTLS 1.3 remove the last TODO in Proxy.pm and Clean up Double Free
    When removing the last todo in proxy it encountered a double free.
    The changes are to address that double free.
    
    Fixes: openssl/project#1795

--- ssl/ssl_lib.c
+++ ssl/ssl_lib.c
@@ -1462,6 +1462,32 @@ void ossl_ssl_connection_free(SSL *ssl)
         return;
 
     /*
+     * For DTLS, the sent_messages queue may hold saved_retransmit_state
+     * entries pointing to the current s->rlayer.wrl. RECORD_LAYER_clear
+     * is about to free and NULL out that pointer. If we let
+     * dtls1_clear_sent_buffer (called later via ssl_deinit) compare
+     * saved.wrl against a now-NULL s->rlayer.wrl, it will incorrectly
+     * attempt to free already-freed memory (double-free). Preemptively
+     * null out any saved_retransmit_state entry that references the
+     * current wrl so that RECORD_LAYER_clear owns that free exclusively.
+     * Entries with a different (older) saved wrl pointer are unaffected
+     * and will be freed correctly by dtls1_clear_sent_buffer.
+     */
+    if (SSL_CONNECTION_IS_DTLS(s) && s->d1 != NULL && s->rlayer.wrl != NULL) {
+        pitem *item;
+        piterator iter = pqueue_iterator(&s->d1->sent_messages);
+
+        while ((item = pqueue_next(&iter)) != NULL) {
+            dtls_sent_msg *sent_msg = (dtls_sent_msg *)item->data;
+
+            if (sent_msg->saved_retransmit_state.wrl == s->rlayer.wrl) {
+                sent_msg->saved_retransmit_state.wrl = NULL;
+                sent_msg->saved_retransmit_state.wrlmethod = NULL;
+            }
+        }
+    }
+
+    /*
      * Ignore return values. This could result in user callbacks being called
      * e.g. for the QUIC TLS record layer. So we do this early before we have
      * freed other things.
