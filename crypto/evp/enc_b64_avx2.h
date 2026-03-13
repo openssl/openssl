33705366699eb2b33126b907670bef9bd90e3a87
slontis <shane.lontis@oracle.com>
2026-03-13 15:13:40 +1100

    Fix windows build failure for arm64ec Fixes #30361
    The new b64 encoder code uses __m256 which is not currently supported in ARM64EC code,
    since it does not natively support x64-specific instruction sets like AVX.
    Disable the fast AVX path if arm64EC is used.

--- crypto/evp/enc_b64_avx2.h
+++ crypto/evp/enc_b64_avx2.h
@@ -4,9 +4,11 @@
 #include <openssl/evp.h>
 
 #if defined(__x86_64) || defined(__x86_64__) || defined(_M_AMD64) || defined(_M_X64)
+#if !defined(_M_ARM64EC)
 int encode_base64_avx2(EVP_ENCODE_CTX *ctx,
     unsigned char *out, const unsigned char *src, int srclen,
     int newlines, int *wrap_cnt);
+#endif /* !defined(_M_ARM64EC) */
 #endif
 
 #endif
