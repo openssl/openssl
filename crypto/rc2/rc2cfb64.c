807cd427ce9db8e5a87a6b41b645a562db2d8b91
frostb1ten <68353531+frostb1ten@users.noreply.github.com>
2026-03-16 05:07:12 -0500

    Mask *num on entry in deprecated low-level OFB/CFB implementations

--- crypto/rc2/rc2cfb64.c
+++ crypto/rc2/rc2cfb64.c
@@ -27,7 +27,7 @@ void RC2_cfb64_encrypt(const unsigned char *in, unsigned char *out,
     int *num, int encrypt)
 {
     register unsigned long v0, v1, t;
-    register int n = *num;
+    register int n = *num & 0x07;
     register long l = length;
     unsigned long ti[2];
     unsigned char *iv, c, cc;
