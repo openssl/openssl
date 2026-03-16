88a22717cb9ba8572b0e90b8ed4d4072cece7dfd
frostb1ten <68353531+frostb1ten@users.noreply.github.com>
2026-03-16 05:07:12 -0500

    Mask *num on entry in deprecated low-level OFB/CFB implementations
    Reviewed-by: Tomas Mraz <tomas@openssl.foundation>
    Reviewed-by: Paul Dale <paul.dale@oracle.com>
    MergeDate: Wed Mar 18 07:16:23 2026
    (Merged from https://github.com/openssl/openssl/pull/30447)

--- crypto/rc5/rc5cfb64.c
+++ crypto/rc5/rc5cfb64.c
@@ -27,7 +27,7 @@ void RC5_32_cfb64_encrypt(const unsigned char *in, unsigned char *out,
     unsigned char *ivec, int *num, int encrypt)
 {
     register unsigned long v0, v1, t;
-    register int n = *num;
+    register int n = *num & 0x07;
     register long l = length;
     unsigned long ti[2];
     unsigned char *iv, c, cc;
