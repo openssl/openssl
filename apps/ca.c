937c2a47cbe322e4ae1d70c4f3cd7c3d7be3ce6d
Alexandr Nedvedicky <sashan@openssl.org>
2026-03-17 09:24:16 +0100

    Fix coverity issue CID 1688667
    Coverity complains with error impossible_and:
        The and condition type == 22 && type == 19
        can never be true because type cannot be equal
        to two different values at the same time.
    
    Resolves: https://scan5.scan.coverity.com/#/project-view/62622/10222?selectedIssue=1688667

--- apps/ca.c
+++ apps/ca.c
@@ -1525,7 +1525,7 @@ static int do_body(X509 **xret, EVP_PKEY *pkey, X509 *x509,
         j = ASN1_PRINTABLE_type(ASN1_STRING_get0_data(str),
             ASN1_STRING_length(str));
         if ((type == V_ASN1_T61STRING && j != V_ASN1_T61STRING)
-            || (type == V_ASN1_IA5STRING && type == V_ASN1_PRINTABLESTRING)) {
+            || (type == V_ASN1_IA5STRING && j != V_ASN1_IA5STRING)) {
             BIO_puts(bio_err,
                 "\nThe string contains characters that are illegal for the"
                 " ASN.1 type\n");
