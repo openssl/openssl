openssl/openssl

commit 35dc6c353bfeac6f94ba294cc336e1455d6b4453

QUIC: Make more transport parameters configurable
The following QUIC transport parameters are now configurable via
SSL_get_value_uint() / SSL_set_value_uint():

max_udp_payload_size
initial_max_data
initial_max_stream_data_{uni, bidi_local, bidi_remote}
initial_max_streams_{uni, bidi}
ack_delay_exponent
max_ack_delay
disable_active_migration
active_connection_id_limit

Reviewed-by: Neil Horman <nhorman@openssl.org>
Reviewed-by: Saša Nedvědický <sashan@openssl.org>
MergeDate: Tue Mar 17 13:15:29 2026
(Merged from https://github.com/openssl/openssl/pull/29664)

include/internal/quic_types.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2025 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2022-2026 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
@@ -96,15 +96,16 @@ int ossl_quic_gen_rand_conn_id(OSSL_LIB_CTX *libctx, size_t len,
 
 #define QUIC_MIN_INITIAL_DGRAM_LEN 1200
 
+#define QUIC_MAX_MAX_UDP_PAYLOAD_SIZE 65527 /* RFC 9000 s. 18.2 */
+#define QUIC_DEFAULT_MAX_UDP_PAYLOAD_SIZE QUIC_MAX_MAX_UDP_PAYLOAD_SIZE
+
 #define QUIC_DEFAULT_ACK_DELAY_EXP 3
 #define QUIC_MAX_ACK_DELAY_EXP 20
 
 #define QUIC_DEFAULT_MAX_ACK_DELAY 25
+#define QUIC_MAX_MAX_ACK_DELAY 16384
 
 #define QUIC_MIN_ACTIVE_CONN_ID_LIMIT 2
-
-/* Arbitrary choice of default idle timeout (not an RFC value). */
-#define QUIC_DEFAULT_IDLE_TIMEOUT 30000
 
 #define QUIC_STATELESS_RESET_TOKEN_LEN 16
 

doc/man3/SSL_get_value_uint.pod
@@ -12,6 +12,9 @@ SSL_VALUE_CLASS_FEATURE_REQUEST, SSL_VALUE_CLASS_FEATURE_PEER_REQUEST,
 SSL_VALUE_CLASS_FEATURE_NEGOTIATED, SSL_VALUE_QUIC_STREAM_BIDI_LOCAL_AVAIL,
 SSL_VALUE_QUIC_STREAM_BIDI_REMOTE_AVAIL, SSL_VALUE_QUIC_STREAM_UNI_LOCAL_AVAIL,
 SSL_VALUE_QUIC_STREAM_UNI_REMOTE_AVAIL, SSL_VALUE_QUIC_IDLE_TIMEOUT,
+SSL_VALUE_QUIC_UDP_PAYLOAD_SIZE_MAX, SSL_VALUE_QUIC_WINDOWCON,
+SSL_VALUE_QUIC_WINDOWBSTR, SSL_VALUE_QUIC_WINDOWUSTR,
+SSL_VALUE_QUIC_ACK_DELAY_EXPONENT, SSL_VALUE_QUIC_ACK_DELAY_MAX,
 SSL_VALUE_EVENT_HANDLING_MODE,
 SSL_VALUE_EVENT_HANDLING_MODE_INHERIT,
 SSL_VALUE_EVENT_HANDLING_MODE_EXPLICIT,
@@ -45,6 +48,12 @@ manage negotiable features and configuration values for an SSL object
  #define SSL_VALUE_QUIC_STREAM_UNI_LOCAL_AVAIL
  #define SSL_VALUE_QUIC_STREAM_UNI_REMOTE_AVAIL
  #define SSL_VALUE_QUIC_IDLE_TIMEOUT
+ #define SSL_VALUE_QUIC_UDP_PAYLOAD_SIZE_MAX
+ #define SSL_VALUE_QUIC_WINDOWCON
+ #define SSL_VALUE_QUIC_WINDOWBSTR
+ #define SSL_VALUE_QUIC_WINDOWUSTR
+ #define SSL_VALUE_QUIC_ACK_DELAY_EXPONENT
+ #define SSL_VALUE_QUIC_ACK_DELAY_MAX
 
  #define SSL_VALUE_EVENT_HANDLING_MODE
  #define SSL_VALUE_EVENT_HANDLING_MODE_INHERIT
@@ -158,7 +167,7 @@ documentation for a specific value specifies otherwise.
 
 =over 4
 
-=item B<SSL_VALUE_QUIC_IDLE_TIMEOUT> (connection object)
+=item B<SSL_VALUE_QUIC_IDLE_TIMEOUT> (connection/listener object)
 
 Negotiated feature value. This configures the desired QUIC idle timeout in
 milliseconds, where 0 represents a lack of an idle timeout. This feature can
@@ -167,6 +176,65 @@ changed.
 
 This release of OpenSSL uses a default value of 30 seconds. This default value
 may change between releases of OpenSSL.
+
+=item B<SSL_VALUE_QUIC_UDP_PAYLOAD_SIZE_MAX> (connection/listener object)
+
+Feature (peer) request value. This configures the maximum UDP payload size in
+bytes, corresponding to the QUIC max_udp_payload_size transport parameter
+as defined in RFC 9000, Section 18.2. It limits the size of UDP datagrams the
+local QUIC stack is willing to receive from the peer. It does not change the
+maximum datagram payload size used on the transmit path. This feature can only
+be configured prior to connection establishment and cannot be subsequently
+changed. This value will be sent to the peer in the transport parameters.
+
+This release of OpenSSL uses a default value of 1200 bytes. This default value
+may change between releases of OpenSSL.
+
+=item B<SSL_VALUE_QUIC_WINDOWCON> (connection/listener object)
+
+Feature (peer) request value. This configures the initial QUIC connection-level
+flow control receive window in bytes. This feature can only be configured prior
+to connection establishment and cannot be subsequently changed. This value will
+be sent to the peer in the transport parameters.
+
+This release of OpenSSL uses a default value of 768 * 1024 bytes. This default
+value may change between releases of OpenSSL.
+
+=item B<SSL_VALUE_QUIC_WINDOWBSTR> (connection/listener object)
+
+Feature (peer) request value. This sets the initial receive window size, in
+bytes, for QUIC stream flow control on a bidirectional stream created by the
+local endpoint. This feature can only be configured prior to connection
+establishment and cannot be subsequently changed. This value will be sent to the
+peer in the transport parameters.
+
+This release of OpenSSL uses a default value of 512 * 1024 bytes. This default
+value may change between releases of OpenSSL.
+
+=item B<SSL_VALUE_QUIC_WINDOWUSTR> (connection/listener object)
+
+As above, but configures the initial receive window size for QUIC stream flow
+control on a unidirectional stream created by the remote endpoint.
+
+=item B<SSL_VALUE_QUIC_ACK_DELAY_EXPONENT> (connection/listener object)
+
+Feature (peer) request value. This configures the ACK delay exponent. This
+feature can only be configured prior to connection establishment and cannot be
+subsequently changed. This value will be sent to the peer in the transport
+parameters.
+
+This release of OpenSSL uses a default value of 3. This default value may change
+between releases of OpenSSL.
+
+=item B<SSL_VALUE_QUIC_ACK_DELAY_MAX> (connection/listener object)
+
+Feature (peer) request value. This configures the maximum ACK delay in
+milliseconds. This feature can only be configured prior to connection
+establishment and cannot be subsequently changed. This value will be sent to the
+peer in the transport parameters.
+
+This release of OpenSSL uses a default value of 25 milliseconds. This default
+value may change between releases of OpenSSL.
 
 =item B<SSL_VALUE_QUIC_STREAM_BIDI_LOCAL_AVAIL> (connection object)
 
@@ -339,11 +407,16 @@ L<SSL_set_default_stream_mode(3)>, L<SSL_set_incoming_stream_policy(3)>
 
 =head1 HISTORY
 
-These functions were added in OpenSSL 3.3.
+The values SSL_VALUE_QUIC_UDP_PAYLOAD_SIZE_MAX, SSL_VALUE_QUIC_WINDOWCON,
+SSL_VALUE_QUIC_WINDOWBSTR, SSL_VALUE_QUIC_WINDOWUSTR,
+SSL_VALUE_QUIC_ACK_DELAY_EXPONENT, and SSL_VALUE_QUIC_ACK_DELAY_MAX
+were added in OpenSSL 4.1.
+
+The remaining functions and values described here were all added in OpenSSL 3.3.
 
 =head1 COPYRIGHT
 
-Copyright 2002-2024 The OpenSSL Project Authors. All Rights Reserved.
+Copyright 2002-2026 The OpenSSL Project Authors. All Rights Reserved.
 
 Licensed under the Apache License 2.0 (the "License").  You may not use
 this file except in compliance with the License.  You can obtain a copy

include/internal/quic_channel.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2025 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2022-2026 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
@@ -128,6 +128,20 @@ typedef struct quic_channel_args_st {
 
     /* Title to use for the qlog session, or NULL. */
     const char *qlog_title;
+
+    /* Transport parameter values for the channel. */
+    uint64_t max_idle_timeout;
+    uint64_t max_udp_payload_size;
+    uint64_t init_max_data;
+    uint64_t init_max_stream_data_bidi_local;
+    uint64_t init_max_stream_data_bidi_remote;
+    uint64_t init_max_stream_data_uni;
+    uint64_t init_max_streams_bidi;
+    uint64_t init_max_streams_uni;
+    uint64_t max_ack_delay;
+    uint64_t active_conn_id_limit;
+    unsigned char ack_delay_exponent;
+    unsigned char disable_active_migration;
 } QUIC_CHANNEL_ARGS;
 
 /* Represents the cause for a connection's termination. */
@@ -461,13 +475,69 @@ uint64_t ossl_quic_channel_get_remote_stream_count_avail(const QUIC_CHANNEL *ch,
 int ossl_quic_channel_have_generated_transport_params(const QUIC_CHANNEL *ch);
 
 /* Configures the idle timeout to request from peer (milliseconds, 0=no timeout). */
-void ossl_quic_channel_set_max_idle_timeout_request(QUIC_CHANNEL *ch, uint64_t ms);
-/* Get the configured idle timeout to request from peer. */
+int ossl_quic_channel_set_max_idle_timeout_request(QUIC_CHANNEL *ch, uint64_t ms);
+/* Gets the configured idle timeout to request from peer. */
 uint64_t ossl_quic_channel_get_max_idle_timeout_request(const QUIC_CHANNEL *ch);
-/* Get the idle timeout requested by the peer. */
+/* Gets the idle timeout requested by the peer. */
 uint64_t ossl_quic_channel_get_max_idle_timeout_peer_request(const QUIC_CHANNEL *ch);
-/* Get the idle timeout actually negotiated. */
+/* Gets the idle timeout actually negotiated. */
 uint64_t ossl_quic_channel_get_max_idle_timeout_actual(const QUIC_CHANNEL *ch);
+
+/* Configures the maximum UDP payload size to advertise to the peer (bytes). */
+int ossl_quic_channel_set_max_udp_payload_size_request(QUIC_CHANNEL *ch, uint64_t size);
+/* Gets the configured maximum UDP payload size to advertise to the peer. */
+uint64_t ossl_quic_channel_get_max_udp_payload_size_request(const QUIC_CHANNEL *ch);
+/* Gets the maximum UDP payload size advertised by the peer. */
+uint64_t ossl_quic_channel_get_max_udp_payload_size_peer_request(const QUIC_CHANNEL *ch);
+
+/* Configures the maximum data to advertise to the peer (bytes). */
+int ossl_quic_channel_set_max_data_request(QUIC_CHANNEL *ch, uint64_t max_data);
+/* Gets the configured maximum data to advertise to the peer. */
+uint64_t ossl_quic_channel_get_max_data_request(const QUIC_CHANNEL *ch);
+/* Gets the maximum data advertised by the peer. */
+uint64_t ossl_quic_channel_get_max_data_peer_request(const QUIC_CHANNEL *ch);
+
+/* Configures the maximum stream data for a bidi/uni remote/local stream to advertise to the peer (bytes). */
+int ossl_quic_channel_set_max_stream_data_request(QUIC_CHANNEL *ch, uint64_t max_data, int is_uni, int is_remote);
+/* Gets the configured maximum stream data for a bidi/uni remote/local stream to advertise to the peer. */
+uint64_t ossl_quic_channel_get_max_stream_data_request(const QUIC_CHANNEL *ch, int is_uni, int is_remote);
+/* Gets the maximum stream data for a bidi/uni remote/local stream advertised by the peer. */
+uint64_t ossl_quic_channel_get_max_stream_data_peer_request(const QUIC_CHANNEL *ch, int is_uni, int is_remote);
+
+/* Configures the maximum bidi/uni streams to advertise to the peer. */
+int ossl_quic_channel_set_max_streams_request(QUIC_CHANNEL *ch, uint64_t max_streams, int is_uni);
+/* Gets the configured maximum bidi/uni streams to advertise to the peer. */
+uint64_t ossl_quic_channel_get_max_streams_request(const QUIC_CHANNEL *ch, int is_uni);
+/* Gets the maximum bidi/uni streams advertised by the peer. */
+uint64_t ossl_quic_channel_get_max_streams_peer_request(const QUIC_CHANNEL *ch, int is_uni);
+
+/* Configures the ACK delay exponent to advertise to the peer. */
+int ossl_quic_channel_set_ack_delay_exponent_request(QUIC_CHANNEL *ch, uint64_t exp);
+/* Gets the configured ACK delay exponent to advertise to the peer. */
+uint64_t ossl_quic_channel_get_ack_delay_exponent_request(const QUIC_CHANNEL *ch);
+/* Gets the ACK delay exponent advertised by the peer. */
+uint64_t ossl_quic_channel_get_ack_delay_exponent_peer_request(const QUIC_CHANNEL *ch);
+
+/* Configures the maximum ACK delay to advertise to the peer (milliseconds). */
+int ossl_quic_channel_set_max_ack_delay_request(QUIC_CHANNEL *ch, uint64_t ms);
+/* Gets the configured maximum ACK delay to advertise to the peer. */
+uint64_t ossl_quic_channel_get_max_ack_delay_request(const QUIC_CHANNEL *ch);
+/* Gets the maximum ACK delay advertised by the peer. */
+uint64_t ossl_quic_channel_get_max_ack_delay_peer_request(const QUIC_CHANNEL *ch);
+
+/* Configures the disable active migration flag to advertise to the peer. */
+int ossl_quic_channel_set_disable_active_migration_request(QUIC_CHANNEL *ch, uint64_t disable);
+/* Gets the configured disable active migration flag to advertise to the peer. */
+uint64_t ossl_quic_channel_get_disable_active_migration_request(const QUIC_CHANNEL *ch);
+/* Gets the disable active migration flag advertised by the peer. */
+uint64_t ossl_quic_channel_get_disable_active_migration_peer_request(const QUIC_CHANNEL *ch);
+
+/* Configures the active connection ID limit to advertise to the peer. */
+int ossl_quic_channel_set_active_conn_id_limit_request(QUIC_CHANNEL *ch, uint64_t limit);
+/* Gets the configured active connection ID limit to advertise to the peer. */
+uint64_t ossl_quic_channel_get_active_conn_id_limit_request(const QUIC_CHANNEL *ch);
+/* Gets the active connection ID limit advertised by the peer. */
+uint64_t ossl_quic_channel_get_active_conn_id_limit_peer_request(const QUIC_CHANNEL *ch);
 
 int ossl_quic_bind_channel(QUIC_CHANNEL *ch, const BIO_ADDR *peer,
     const QUIC_CONN_ID *scid, const QUIC_CONN_ID *dcid,

include/internal/quic_port.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2025 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2023-2026 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
@@ -139,6 +139,51 @@ OSSL_TIME ossl_quic_port_get_time(QUIC_PORT *port);
 
 int ossl_quic_port_get_rx_short_dcid_len(const QUIC_PORT *port);
 int ossl_quic_port_get_tx_init_dcid_len(const QUIC_PORT *port);
+
+/* Configures the idle timeout to request from peer (milliseconds, 0=no timeout). */
+void ossl_quic_port_set_max_idle_timeout(QUIC_PORT *port, uint64_t ms);
+/* Gets the configured idle timeout to request from peer. */
+uint64_t ossl_quic_port_get_max_idle_timeout(const QUIC_PORT *port);
+
+/* Configures the maximum UDP payload size to advertise to the peer (bytes). */
+void ossl_quic_port_set_max_udp_payload_size(QUIC_PORT *port, uint64_t size);
+/* Gets the configured maximum UDP payload size to advertise to the peer. */
+uint64_t ossl_quic_port_get_max_udp_payload_size(const QUIC_PORT *port);
+
+/* Configures the maximum data to advertise to the peer (bytes). */
+void ossl_quic_port_set_init_max_data(QUIC_PORT *port, uint64_t max_data);
+/* Gets the configured maximum data to advertise to the peer. */
+uint64_t ossl_quic_port_get_init_max_data(const QUIC_PORT *port);
+
+/* Configures the maximum stream data for a bidi/uni remote/local stream to advertise to the peer (bytes). */
+void ossl_quic_port_set_init_max_stream_data(QUIC_PORT *port, uint64_t max_data, int is_uni, int is_remote);
+/* Gets the configured maximum stream data for a bidi/uni remote/local stream to advertise to the peer. */
+uint64_t ossl_quic_port_get_init_max_stream_data(const QUIC_PORT *port, int is_uni, int is_remote);
+
+/* Configures the maximum bidi/uni streams to advertise to the peer. */
+void ossl_quic_port_set_init_max_streams(QUIC_PORT *port, uint64_t max_streams, int is_uni);
+/* Gets the configured maximum bidi/uni streams to advertise to the peer. */
+uint64_t ossl_quic_port_get_init_max_streams(const QUIC_PORT *port, int is_uni);
+
+/* Configures the ACK delay exponent to advertise to the peer. */
+void ossl_quic_port_set_ack_delay_exponent(QUIC_PORT *port, uint64_t exp);
+/* Gets the configured ACK delay exponent to advertise to the peer. */
+uint64_t ossl_quic_port_get_ack_delay_exponent(const QUIC_PORT *port);
+
+/* Configures the maximum ACK delay to advertise to the peer (milliseconds). */
+void ossl_quic_port_set_max_ack_delay(QUIC_PORT *port, uint64_t ms);
+/* Gets the configured maximum ACK delay to advertise to the peer. */
+uint64_t ossl_quic_port_get_max_ack_delay(const QUIC_PORT *port);
+
+/* Configures the disable active migration flag to advertise to the peer. */
+void ossl_quic_port_set_disable_active_migration(QUIC_PORT *port, uint64_t disable);
+/* Gets the configured disable active migration flag to advertise to the peer. */
+uint64_t ossl_quic_port_get_disable_active_migration(const QUIC_PORT *port);
+
+/* Configures the active connection ID limit to advertise to the peer. */
+void ossl_quic_port_set_active_conn_id_limit(QUIC_PORT *port, uint64_t limit);
+/* Gets the configured active connection ID limit to advertise to the peer. */
+uint64_t ossl_quic_port_get_active_conn_id_limit(const QUIC_PORT *port);
 
 /* Returns 1 if the port is running/healthy, 0 if it has failed. */
 int ossl_quic_port_is_running(const QUIC_PORT *port);

include/internal/quic_record_tx.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2024 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2022-2026 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
@@ -305,6 +305,9 @@ size_t ossl_qtx_get_unflushed_pkt_count(OSSL_QTX *qtx);
  * for ensuring the lifetime of the BIO exceeds the lifetime of the QTX.
  */
 void ossl_qtx_set_bio(OSSL_QTX *qtx, BIO *bio);
+
+/* Changes the MTU in bytes we use to send datagrams. */
+int ossl_qtx_set_mtu(OSSL_QTX *qtx, unsigned int mtu);
 
 /* Changes the MDPL. */
 int ossl_qtx_set_mdpl(OSSL_QTX *qtx, size_t mdpl);

include/internal/quic_txp.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2025 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2022-2026 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
@@ -154,6 +154,10 @@ int ossl_quic_tx_packetiser_set_cur_scid(OSSL_QUIC_TX_PACKETISER *txp,
  */
 int ossl_quic_tx_packetiser_set_peer(OSSL_QUIC_TX_PACKETISER *txp,
     const BIO_ADDR *peer);
+
+/* Change the ACK delay exponent the TXP uses to encode ACK frames. */
+int ossl_quic_tx_packetiser_set_ack_delay_exponent(OSSL_QUIC_TX_PACKETISER *txp,
+    uint32_t exp);
 
 /*
  * Change the QLOG instance retrieval function in use after instantiation.

include/openssl/ssl.h.in
@@ -2437,6 +2437,12 @@ __owur int SSL_get_conn_close_info(SSL *ssl,
 #define SSL_VALUE_STREAM_WRITE_BUF_SIZE 7
 #define SSL_VALUE_STREAM_WRITE_BUF_USED 8
 #define SSL_VALUE_STREAM_WRITE_BUF_AVAIL 9
+#define SSL_VALUE_QUIC_UDP_PAYLOAD_SIZE_MAX 10
+#define SSL_VALUE_QUIC_WINDOWCON 11
+#define SSL_VALUE_QUIC_WINDOWBSTR 12
+#define SSL_VALUE_QUIC_WINDOWUSTR 13
+#define SSL_VALUE_QUIC_ACK_DELAY_EXPONENT 14
+#define SSL_VALUE_QUIC_ACK_DELAY_MAX 15
 
 #define SSL_VALUE_EVENT_HANDLING_MODE_INHERIT 0
 #define SSL_VALUE_EVENT_HANDLING_MODE_IMPLICIT 1

commit c97a6f5723cc8de2ad41769c026bbc47e47d9d39

quic: fix off-by-one in QUIC_MAX_MAX_ACK_DELAY
Should be 2^14-1 (16383) per RFC 9000 s. 18.2, not 2^14 (16384).

Fixes: 35dc6c353bfe ("QUIC: Make more transport parameters configurable")
Signed-off-by: Abhinav Agarwal <abhinavagarwal1996@gmail.com>

include/internal/quic_types.h
@@ -103,7 +103,7 @@ int ossl_quic_gen_rand_conn_id(OSSL_LIB_CTX *libctx, size_t len,
 #define QUIC_MAX_ACK_DELAY_EXP 20
 
 #define QUIC_DEFAULT_MAX_ACK_DELAY 25
-#define QUIC_MAX_MAX_ACK_DELAY 16384
+#define QUIC_MAX_MAX_ACK_DELAY 16383 /* RFC 9000 s. 18.2 */
 
 #define QUIC_MIN_ACTIVE_CONN_ID_LIMIT 2
 