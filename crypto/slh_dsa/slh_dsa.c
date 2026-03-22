openssl/openssl

commit 2f9e152d86a750753ea729f8c9d79d764a73f20c

Add SLH_DSA signature verification.
This uses a SLH_DSA_CTX that is passed to most functions.
It contains information related to a parameter set (such as constants,
hash functions, prefetched EVP_MD/EVP_MAC objects, as well as ADDRESS
functions). This context is seperated from the SLH_DSA_KEY since
multiple signature operations could be performed using the same keys.
This only implements functions required for SLH-DSA-SHA2-128s

Reviewed-by: Paul Dale <ppzgs1@gmail.com>
Reviewed-by: Viktor Dukhovni <viktor@openssl.org>
Reviewed-by: Tim Hudson <tjh@openssl.org>
(Merged from https://github.com/openssl/openssl/pull/25882)

crypto/slh_dsa/slh_dsa.c
@@ -1,0 +1,169 @@
+/*
+ * Copyright 2024 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+#include <assert.h>
+#include <stddef.h>
+#include <string.h>
+#include "slh_dsa_local.h"
+#include "slh_dsa_key.h"
+
+#define SLH_MAX_M 49
+
+/* (n + SLH_SIG_FORS_LEN(k, a, n) + SLH_SIG_HT_LEN(n, hm, d)) */
+#define SLH_SIG_RANDOM_LEN(n)      (n)
+#define SLH_SIG_FORS_LEN(k, a, n)  (n) * ((k) * (1 + (a)))
+#define SLH_SIG_HT_LEN(h, d, n)    (n) * ((h) + (d) * SLH_WOTS_LEN(n))
+
+static void get_tree_ids(const uint8_t *digest, const SLH_DSA_PARAMS *params,
+                         uint64_t *tree_id, uint32_t *leaf_id);
+
+static int slh_verify_internal(SLH_DSA_CTX *ctx, const SLH_DSA_KEY *pub,
+                               const uint8_t *msg, size_t msg_len,
+                               const uint8_t *sig, size_t sig_len)
+{
+    SLH_HASH_FUNC_DECLARE(ctx, hashf, hctx);
+    SLH_ADRS_FUNC_DECLARE(ctx, adrsf);
+    SLH_ADRS_DECLARE(adrs);
+    uint8_t mdigest[SLH_MAX_M];
+    uint8_t pk_fors[SLH_MAX_N];
+    uint64_t tree_id;
+    uint32_t leaf_id;
+    const SLH_DSA_PARAMS *params = ctx->params;
+    uint32_t n = params->n;
+    size_t r_len = SLH_SIG_RANDOM_LEN(n);
+    size_t sig_fors_len = SLH_SIG_FORS_LEN(params->k, params->a, n);
+    size_t sig_ht_len = SLH_SIG_HT_LEN(params->h, params->d, n);
+    const uint8_t *r, *sig_fors, *sig_ht, *md, *pk_seed, *pk_root;
+
+    if (sig_len != (r_len + sig_fors_len + sig_ht_len))
+        return 0;
+    /* Exit if public key is not set */
+    if (pub->key_len == 0)
+        return 0;
+
+    adrsf->zero(adrs);
+
+    r = sig;
+    sig_fors = r + r_len;
+    sig_ht = sig_fors + sig_fors_len;
+
+    pk_seed = SLH_DSA_PK_SEED(pub);
+    pk_root = SLH_DSA_PK_ROOT(pub);
+
+    hashf->H_MSG(hctx, r, pk_seed, pk_root, msg, msg_len, mdigest);
+    md = mdigest;
+    get_tree_ids(mdigest, params, &tree_id, &leaf_id);
+
+    adrsf->set_tree_address(adrs, tree_id);
+    adrsf->set_type_and_clear(adrs, SLH_ADRS_TYPE_FORS_TREE);
+    adrsf->set_keypair_address(adrs, leaf_id);
+    ossl_slh_fors_pk_from_sig(ctx, sig_fors, md, pk_seed, adrs, pk_fors);
+    return ossl_slh_ht_verify(ctx, pk_fors, sig_ht, pk_seed, tree_id, leaf_id, pk_root);
+}
+
+/*
+ * Pure signatures M' function
+ * ctx is the empty string by default.
+ */
+static uint8_t *msg_encode(const uint8_t *msg, size_t msg_len,
+                           const uint8_t *ctx, size_t ctx_len, int encode,
+                           uint8_t *tmp, size_t tmp_len, size_t *out_len)
+{
+    uint8_t *encoded = NULL;
+    size_t encoded_len;
+
+    if (encode == 0) {
+        /* Raw message */
+        *out_len = msg_len;
+        return (uint8_t *)msg;
+    }
+    if (ctx_len > SLH_DSA_MAX_CONTEXT_STRING_LEN)
+        return 0;
+
+    /* Pure encoding */
+    encoded_len = 1 + 1 + ctx_len + msg_len;
+    *out_len = encoded_len;
+    if (encoded_len <= tmp_len) {
+        encoded = tmp;
+    } else {
+        encoded = OPENSSL_zalloc(encoded_len);
+        if (encoded == NULL)
+            return NULL;
+    }
+    encoded[0] = 0;
+    encoded[1] = (uint8_t)ctx_len;
+    memcpy(&encoded[2], ctx, ctx_len);
+    memcpy(&encoded[2 + ctx_len], msg, msg_len);
+    return encoded;
+}
+
+int ossl_slh_dsa_verify(SLH_DSA_CTX *slh_ctx, const SLH_DSA_KEY *pub,
+                        const uint8_t *msg, size_t msg_len,
+                        const uint8_t *ctx, size_t ctx_len, int encode,
+                        const uint8_t *sig, size_t sig_len)
+{
+    uint8_t *m;
+    size_t m_len;
+    uint8_t m_tmp[1024];
+    int ret = 0;
+
+    m = msg_encode(msg, msg_len, ctx, ctx_len, encode, m_tmp, sizeof(m_tmp),
+                   &m_len);
+    if (m == NULL)
+        return 0;
+
+    ret = slh_verify_internal(slh_ctx, pub, m, m_len, sig, sig_len);
+    if (m != msg && m != m_tmp)
+        OPENSSL_free(m);
+    return ret;
+}
+
+/* FIPS 205 Algorithm 2 toInt(X, n) */
+static uint64_t bytes_to_u64_be(const uint8_t *in, size_t in_len)
+{
+    size_t i;
+    uint64_t total = 0;
+
+    for (i = 0; i < in_len; i++)
+        total = (total << 8) + *in++;
+    return total;
+}
+
+/*
+ * See Algorithm 19 Steps 7..10 (also Algorithm 20 Step 10..13).
+ * Converts digested bytes into a tree index, and leaf index within the tree.
+ * The sizes are determined by the |params| parameter set.
+ */
+static void get_tree_ids(const uint8_t *digest, const SLH_DSA_PARAMS *params,
+                         uint64_t *tree_id, uint32_t *leaf_id)
+{
+    const uint8_t *tree_id_bytes, *leaf_id_bytes;
+    uint32_t md_len, tree_id_len, leaf_id_len;
+    uint64_t tree_id_mask, leaf_id_mask;
+
+    md_len = ((params->k * params->a + 7) >> 3); /* 21..40 bytes */
+    tree_id_len = ((params->h - params->hm + 7) >> 3); /* 7 or 8 bytes */
+    leaf_id_len = ((params->hm + 7) >> 3); /* 1 or 2 bytes */
+
+    tree_id_bytes = digest + md_len;
+    leaf_id_bytes = tree_id_bytes + tree_id_len;
+
+    assert((md_len + tree_id_len + leaf_id_len) == params->m);
+    /*
+     * In order to calculate A mod (2^X) where X is in the range of (54..64)
+     * This is equivalent to A & (2^x - 1) which is just a sequence of X ones
+     * that must fit into a 64 bit value.
+     * e.g when X = 64 it would be A & (0xFFFF_FFFF_FFFF_FFFF)
+     *     when X = 54 it would be A & (0x3F_FFFF_FFFF_FFFF)
+     * i.e. A & (0xFFFF_FFFF_FFFF_FFFF >> (64 - X))
+     */
+    tree_id_mask = ((uint64_t)-1) >> (64 - (params->h - params->hm));
+    leaf_id_mask = (1 << params->hm) - 1; /* max value is 0x1FF when hm = 9 */
+    *tree_id = bytes_to_u64_be(tree_id_bytes, tree_id_len) & tree_id_mask;
+    *leaf_id = (uint32_t)(bytes_to_u64_be(leaf_id_bytes, leaf_id_len) & leaf_id_mask);
+}

crypto/slh_dsa/build.info
@@ -1,6 +1,7 @@
 LIBS=../../libcrypto
 
-$COMMON=slh_dsa_key.c slh_params.c
+$COMMON=slh_adrs.c slh_dsa.c slh_dsa_ctx.c slh_dsa_key.c slh_fors.c slh_hash.c \
+        slh_hypertree.c slh_params.c slh_wots.c slh_xmss.c
 
 IF[{- !$disabled{'slh_dsa'} -}]
   SOURCE[../../libcrypto]=$COMMON

crypto/slh_dsa/slh_adrs.c
@@ -1,0 +1,132 @@
+/*
+ * Copyright 2024 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+#include <string.h>
+#include "slh_adrs.h"
+
+/* See FIPS 205 - Section 4.3 Table 1  Uncompressed Addresses */
+#define SLH_ADRS_OFF_LAYER_ADR      0
+#define SLH_ADRS_OFF_TREE_ADR       4
+#define SLH_ADRS_OFF_TYPE          16
+#define SLH_ADRS_OFF_KEYPAIR_ADDR  20
+#define SLH_ADRS_OFF_CHAIN_ADDR    24
+#define SLH_ADRS_OFF_HASH_ADDR     28
+#define SLH_ADRS_OFF_TREE_INDEX    SLH_ADRS_OFF_HASH_ADDR
+#define SLH_ADRS_SIZE_TYPE          4
+/* Number of bytes after type to clear */
+#define SLH_ADRS_SIZE_TYPECLEAR    SLH_ADRS_SIZE - (SLH_ADRS_OFF_TYPE + SLH_ADRS_SIZE_TYPE)
+#define SLH_ADRS_SIZE_KEYPAIR_ADDR 4
+
+/* See FIPS 205 - Section 11.2 Table 3 Compressed Addresses */
+#define SLH_ADRSC_OFF_LAYER_ADR     0
+#define SLH_ADRSC_OFF_TREE_ADR      1
+#define SLH_ADRSC_OFF_TYPE          9
+#define SLH_ADRSC_OFF_KEYPAIR_ADDR  10
+#define SLH_ADRSC_OFF_CHAIN_ADDR    14
+#define SLH_ADRSC_OFF_HASH_ADDR     18
+#define SLH_ADRSC_OFF_TREE_INDEX    SLH_ADRSC_OFF_HASH_ADDR
+#define SLH_ADRSC_SIZE_TYPE         1
+#define SLH_ADRSC_SIZE_TYPECLEAR    SLH_ADRS_SIZE_TYPECLEAR
+#define SLH_ADRSC_SIZE_KEYPAIR_ADDR SLH_ADRS_SIZE_KEYPAIR_ADDR
+
+#define slh_adrsc_set_tree_height slh_adrsc_set_chain_address
+#define slh_adrsc_set_tree_index slh_adrsc_set_hash_address
+
+static OSSL_SLH_ADRS_FUNC_set_layer_address slh_adrsc_set_layer_address;
+static OSSL_SLH_ADRS_FUNC_set_tree_address slh_adrsc_set_tree_address;
+static OSSL_SLH_ADRS_FUNC_set_type_and_clear slh_adrsc_set_type_and_clear;
+static OSSL_SLH_ADRS_FUNC_set_keypair_address slh_adrsc_set_keypair_address;
+static OSSL_SLH_ADRS_FUNC_copy_keypair_address slh_adrsc_copy_keypair_address;
+static OSSL_SLH_ADRS_FUNC_set_chain_address slh_adrsc_set_chain_address;
+static OSSL_SLH_ADRS_FUNC_set_hash_address slh_adrsc_set_hash_address;
+static OSSL_SLH_ADRS_FUNC_zero slh_adrsc_zero;
+static OSSL_SLH_ADRS_FUNC_copy slh_adrsc_copy;
+
+/* Variants of the FIPS 205 Algorithm 3 toByte(x, n) for 32 and 64 bit integers */
+
+/* Convert a 32 bit value |in| to 4 bytes |out| in big endian format */
+static ossl_inline void U32TOSTR(unsigned char *out, uint32_t in)
+{
+    out[3] = (unsigned char)((in) & 0xff);
+    out[2] = (unsigned char)((in >> 8) & 0xff);
+    out[1] = (unsigned char)((in >> 16) & 0xff);
+    out[0] = (unsigned char)((in >> 24) & 0xff);
+}
+
+/* Convert a 64 bit value |in| to 8 bytes |out| in big endian format */
+static ossl_inline void U64TOSTR(unsigned char *out, uint64_t in)
+{
+    out[7] = (unsigned char)((in) & 0xff);
+    out[6] = (unsigned char)((in >> 8) & 0xff);
+    out[5] = (unsigned char)((in >> 16) & 0xff);
+    out[4] = (unsigned char)((in >> 24) & 0xff);
+    out[3] = (unsigned char)((in >> 32) & 0xff);
+    out[2] = (unsigned char)((in >> 40) & 0xff);
+    out[1] = (unsigned char)((in >> 48) & 0xff);
+    out[0] = (unsigned char)((in >> 56) & 0xff);
+}
+
+/* Compressed versions of ADRS functions See Table 3 */
+static void slh_adrsc_set_layer_address(SLH_ADRS adrsc, uint32_t layer)
+{
+    adrsc[SLH_ADRSC_OFF_LAYER_ADR] = (uint8_t)layer;
+}
+static void slh_adrsc_set_tree_address(SLH_ADRS adrsc, uint64_t in)
+{
+    U64TOSTR(adrsc + SLH_ADRSC_OFF_TREE_ADR, in);
+}
+static void slh_adrsc_set_type_and_clear(SLH_ADRS adrsc, uint32_t type)
+{
+    adrsc[SLH_ADRSC_OFF_TYPE] = (uint8_t)type;
+    memset(adrsc + SLH_ADRSC_OFF_TYPE + SLH_ADRSC_SIZE_TYPE, 0, SLH_ADRSC_SIZE_TYPECLEAR);
+}
+static void slh_adrsc_set_keypair_address(SLH_ADRS adrsc, uint32_t in)
+{
+    U32TOSTR(adrsc + SLH_ADRSC_OFF_KEYPAIR_ADDR, in);
+}
+static void slh_adrsc_copy_keypair_address(SLH_ADRS dst, const SLH_ADRS src)
+{
+    memcpy(dst + SLH_ADRSC_OFF_KEYPAIR_ADDR, src + SLH_ADRSC_OFF_KEYPAIR_ADDR,
+           SLH_ADRSC_SIZE_KEYPAIR_ADDR);
+}
+static void slh_adrsc_set_chain_address(SLH_ADRS adrsc, uint32_t in)
+{
+    U32TOSTR(adrsc + SLH_ADRSC_OFF_CHAIN_ADDR, in);
+}
+static void slh_adrsc_set_hash_address(SLH_ADRS adrsc, uint32_t in)
+{
+    U32TOSTR(adrsc + SLH_ADRSC_OFF_HASH_ADDR, in);
+}
+static void slh_adrsc_zero(SLH_ADRS adrsc)
+{
+    memset(adrsc, 0, SLH_ADRSC_SIZE);
+}
+static void slh_adrsc_copy(SLH_ADRS dst, const SLH_ADRS src)
+{
+    memcpy(dst, src, SLH_ADRSC_SIZE);
+}
+
+const SLH_ADRS_FUNC *ossl_slh_get_adrs_fn(int is_compressed)
+{
+    static const SLH_ADRS_FUNC methods[] = {
+        {
+            slh_adrsc_set_layer_address,
+            slh_adrsc_set_tree_address,
+            slh_adrsc_set_type_and_clear,
+            slh_adrsc_set_keypair_address,
+            slh_adrsc_copy_keypair_address,
+            slh_adrsc_set_chain_address,
+            slh_adrsc_set_tree_height,
+            slh_adrsc_set_hash_address,
+            slh_adrsc_set_tree_index,
+            slh_adrsc_zero,
+            slh_adrsc_copy,
+        }
+    };
+    return &methods[0];
+}

crypto/slh_dsa/slh_adrs.h
@@ -1,0 +1,73 @@
+/*
+ * Copyright 2024 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#include <openssl/e_os2.h>
+
+/*
+ * An Address object is used to store a blob of data that is used by hash
+ * functions. It stores information related to the type of operation, as well as
+ * information related to tree addresses and heights.
+ * SHAKE based algorithms use 32 bytes for this object, whereas SHA2 based
+ * algorithms use a compressed format of 22 bytes. For this reason there are
+ * different method tables to support the different formats.
+ * FIPS 205 Section 4.2 describes the SHAKE related functions.
+ * The compressed format is discussed in Section 11.2.
+ */
+
+#define SLH_ADRS_SIZE     32 /* size of the ADRS blob */
+#define SLH_ADRSC_SIZE    22 /* size of a compact ADRS blob */
+#define SLH_ADRS_SIZE_MAX SLH_ADRS_SIZE
+
+/* 7 Different types of addresses */
+#define SLH_ADRS_TYPE_WOTS_HASH  0
+#define SLH_ADRS_TYPE_WOTS_PK    1
+#define SLH_ADRS_TYPE_TREE       2
+#define SLH_ADRS_TYPE_FORS_TREE  3
+#define SLH_ADRS_TYPE_FORS_ROOTS 4
+#define SLH_ADRS_TYPE_WOTS_PRF   5
+#define SLH_ADRS_TYPE_FORS_PRF   6
+
+typedef uint8_t *SLH_ADRS;
+
+#define SLH_ADRS_DECLARE(a) uint8_t a[SLH_ADRS_SIZE_MAX]
+#define SLH_ADRS_FUNC_DECLARE(ctx, adrsf) \
+    const SLH_ADRS_FUNC *adrsf = ctx->adrs_func
+#define SLH_ADRS_FN_DECLARE(adrsf, t) OSSL_SLH_ADRS_FUNC_##t *t = adrsf->t
+
+typedef void (OSSL_SLH_ADRS_FUNC_zero)(SLH_ADRS adrs);
+typedef void (OSSL_SLH_ADRS_FUNC_copy)(SLH_ADRS dst, const SLH_ADRS src);
+typedef void (OSSL_SLH_ADRS_FUNC_copy_keypair_address)(SLH_ADRS dst, const SLH_ADRS src);
+/*
+ * Note that the tree address is actually 12 bytes in uncompressed format,
+ * but we only use 8 bytes
+ */
+typedef void (OSSL_SLH_ADRS_FUNC_set_tree_address)(SLH_ADRS adrs, uint64_t in);
+typedef void (OSSL_SLH_ADRS_FUNC_set_layer_address)(SLH_ADRS adrs, uint32_t layer);
+typedef void (OSSL_SLH_ADRS_FUNC_set_type_and_clear)(SLH_ADRS adrs, uint32_t type);
+typedef void (OSSL_SLH_ADRS_FUNC_set_keypair_address)(SLH_ADRS adrs, uint32_t in);
+typedef void (OSSL_SLH_ADRS_FUNC_set_chain_address)(SLH_ADRS adrs, uint32_t in);
+typedef void (OSSL_SLH_ADRS_FUNC_set_tree_height)(SLH_ADRS adrs, uint32_t in);
+typedef void (OSSL_SLH_ADRS_FUNC_set_hash_address)(SLH_ADRS adrs, uint32_t in);
+typedef void (OSSL_SLH_ADRS_FUNC_set_tree_index)(SLH_ADRS adrs, uint32_t in);
+
+typedef struct slh_adrs_func_st {
+    OSSL_SLH_ADRS_FUNC_set_layer_address *set_layer_address;
+    OSSL_SLH_ADRS_FUNC_set_tree_address *set_tree_address;
+    OSSL_SLH_ADRS_FUNC_set_type_and_clear *set_type_and_clear;
+    OSSL_SLH_ADRS_FUNC_set_keypair_address *set_keypair_address;
+    OSSL_SLH_ADRS_FUNC_copy_keypair_address *copy_keypair_address;
+    OSSL_SLH_ADRS_FUNC_set_chain_address *set_chain_address;
+    OSSL_SLH_ADRS_FUNC_set_tree_height *set_tree_height;
+    OSSL_SLH_ADRS_FUNC_set_hash_address *set_hash_address;
+    OSSL_SLH_ADRS_FUNC_set_tree_index *set_tree_index;
+    OSSL_SLH_ADRS_FUNC_zero *zero;
+    OSSL_SLH_ADRS_FUNC_copy *copy;
+} SLH_ADRS_FUNC;
+
+const SLH_ADRS_FUNC *ossl_slh_get_adrs_fn(int is_compressed);

crypto/slh_dsa/slh_dsa_ctx.c
@@ -1,0 +1,46 @@
+/*
+ * Copyright 2024 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+#include <stddef.h>
+#include <openssl/crypto.h>
+#include "slh_dsa_local.h"
+
+/*
+ * @param
+ */
+SLH_DSA_CTX *ossl_slh_dsa_ctx_new(const char *alg,
+                                  OSSL_LIB_CTX *lib_ctx, const char *propq)
+{
+    SLH_DSA_CTX *ret = OPENSSL_zalloc(sizeof(*ret));
+
+    if (ret != NULL) {
+        const SLH_DSA_PARAMS *params = ossl_slh_dsa_params_get(alg);
+
+        if (params == NULL)
+            goto err;
+        ret->params = params;
+        ret->hash_func = ossl_slh_get_hash_fn(params->is_shake);
+        ret->adrs_func = ossl_slh_get_adrs_fn(params->is_shake == 0);
+
+        if (!ossl_slh_hash_ctx_init(&ret->hash_ctx, lib_ctx, propq,
+                                    params->is_shake,
+                                    params->security_category,
+                                    params->n, params->m))
+            goto err;
+    }
+    return ret;
+ err:
+    OPENSSL_free(ret);
+    return NULL;
+}
+
+void ossl_slh_dsa_ctx_free(SLH_DSA_CTX *ctx)
+{
+    ossl_slh_hash_ctx_cleanup(&ctx->hash_ctx);
+    OPENSSL_free(ctx);
+}

crypto/slh_dsa/slh_dsa_key.c
@@ -140,3 +140,8 @@ int ossl_slh_dsa_key_fromdata(SLH_DSA_KEY *key, const OSSL_PARAM params[])
     key->key_len = 0;
     return 0;
 }
+
+int ossl_slh_dsa_key_type_matches(SLH_DSA_CTX *ctx, const SLH_DSA_KEY *key)
+{
+    return (key->params == ctx->params);
+}

crypto/slh_dsa/slh_dsa_key.h
@@ -11,6 +11,8 @@
 #include "internal/refcount.h"
 
 #define SLH_DSA_MAX_KEYLEN 32 * 2 /* 2 * n */
+#define SLH_DSA_PK_SEED(key) (key->pub)
+#define SLH_DSA_PK_ROOT(key)  (key->pub + key->params->n)
 
 struct slh_dsa_key_st {
     uint8_t pub[SLH_DSA_MAX_KEYLEN];

crypto/slh_dsa/slh_dsa_local.h
@@ -8,4 +8,58 @@
  */
 
 #include "crypto/slh_dsa.h"
+#include "slh_hash.h"
 #include "slh_params.h"
+
+/*
+ * Maximum size of the security parameter |n| in FIPS 205 Section 11. Table 2.
+ * This indicates the length in bytes of a message that can be signed.
+ * It is the size used by WOTS+ public and private key elements as well as
+ * signature elements.
+ */
+#define SLH_MAX_N                   32
+/*
+ * For the given standard w=16 for all parameter sets.
+ * A n byte message is converted into 2 * n base 16 Integers followed
+ * by 3 Integers for the checksum of these values.
+ */
+#define SLH_WOTS_LEN(n)             (2 * (n) + 3)
+
+/*
+ * FIPS 205 SLH_DSA algorithms have many different parameters which includes:
+ *   - A set of constants (Section 11. contains 12 parameter sets)
+ *     such as tree heights and security parameters associated with a algorithm
+ *     name such as SLH-DSA-SHA2-128s.
+ *   - ADRS functions (such as set_layer_address() in Section 4.3 & 11.2)
+ *   - Hash Functions (such as H_MSG() & PRF()) See Sections 11.1, 11.2.1 & 11.2.2.
+ *
+ *   - OpenSSL also uses an SLH_HASH_CTX to pass pre-fetched EVP related objects
+ *     to the Hash functions.
+ *
+ * SLH_DSA_CTX is a container to hold all of these objects. This object is
+ * resolved early and is then passed to most SLH_DSA related functions.
+ */
+struct slh_dsa_ctx_st {
+    const SLH_DSA_PARAMS *params;
+    const SLH_ADRS_FUNC *adrs_func;
+    const SLH_HASH_FUNC *hash_func;
+    SLH_HASH_CTX hash_ctx;
+};
+
+void ossl_slh_wots_pk_from_sig(SLH_DSA_CTX *ctx,
+                               const uint8_t *sig, const uint8_t *msg,
+                               const uint8_t *pk_seed, uint8_t *adrs,
+                               uint8_t *pk_out);
+
+void ossl_slh_xmss_pk_from_sig(SLH_DSA_CTX *ctx, uint32_t node_id,
+                               const uint8_t *sig, const uint8_t *msg,
+                               const uint8_t *pk_seed, SLH_ADRS adrs,
+                               uint8_t *pk_out);
+
+int ossl_slh_ht_verify(SLH_DSA_CTX *ctx, const uint8_t *msg, const uint8_t *sig,
+                       const uint8_t *pk_seed, uint64_t tree_id, uint32_t leaf_id,
+                       const uint8_t *pk_root);
+
+void ossl_slh_fors_pk_from_sig(SLH_DSA_CTX *ctx, const uint8_t *sig,
+                               const uint8_t *md, const uint8_t *pk_seed,
+                               SLH_ADRS adrs, uint8_t *pk_out);

commit a24fbf403a891ec026847f70bef1fd582b1e9601

SLH-DSA: Fix Integer overflow in msg_encode leading to buffer overflow
Reported by Zehua Qiao and me@snkth.com

An encode message buffer M = 00 || CXT_LEN || CTX || MSG was being
allocated followed by memcpy's into the buffer for CTX and MSG.
If len(MSG) was close to size_t the allocated buffer would be
overwritten.

The fix uses WPACKET to perform the message encoding M = 00 || CXT_LEN || CTX || MSG

Although ML_DSA does a similiar operation, SLH-DSA has to buffer the
encoding because the encoded message is processed multiple times for
PRF_MSG and H_MSG. FOr ML_DSA the encoded message can just be hashed.

Fixes: 2f9e152d86a7 "Add SLH_DSA signature verification."

Reviewed-by: Tomas Mraz <tomas@openssl.foundation>
Reviewed-by: Matt Caswell <matt@openssl.foundation>
Reviewed-by: Eugene Syromiatnikov <esyr@openssl.org>
MergeDate: Sun Mar 22 00:15:47 2026
(Merged from https://github.com/openssl/openssl/pull/30477)

crypto/slh_dsa/slh_dsa.c
@@ -232,6 +232,7 @@ static uint8_t *msg_encode(const uint8_t *msg, size_t msg_len,
     const uint8_t *ctx, size_t ctx_len, int encode,
     uint8_t *tmp, size_t tmp_len, size_t *out_len)
 {
+    WPACKET pkt;
     uint8_t *encoded = NULL;
     size_t encoded_len;
 
@@ -240,11 +241,14 @@ static uint8_t *msg_encode(const uint8_t *msg, size_t msg_len,
         *out_len = msg_len;
         return (uint8_t *)msg;
     }
+
     if (ctx_len > SLH_DSA_MAX_CONTEXT_STRING_LEN)
         return NULL;
 
     /* Pure encoding */
     encoded_len = 1 + 1 + ctx_len + msg_len;
+    if (encoded_len < msg_len) /* Check for overflow */
+        return NULL;
     *out_len = encoded_len;
     if (encoded_len <= tmp_len) {
         encoded = tmp;
@@ -253,10 +257,17 @@ static uint8_t *msg_encode(const uint8_t *msg, size_t msg_len,
         if (encoded == NULL)
             return NULL;
     }
-    encoded[0] = 0;
-    encoded[1] = (uint8_t)ctx_len;
-    memcpy(&encoded[2], ctx, ctx_len);
-    memcpy(&encoded[2 + ctx_len], msg, msg_len);
+    if (!WPACKET_init_static_len(&pkt, encoded, encoded_len, 0)
+        || !WPACKET_put_bytes_u8(&pkt, 0)
+        || !WPACKET_put_bytes_u8(&pkt, (uint8_t)ctx_len)
+        || !WPACKET_memcpy(&pkt, ctx, ctx_len)
+        || !WPACKET_memcpy(&pkt, msg, msg_len)
+        || !WPACKET_finish(&pkt)) {
+        if (encoded != tmp)
+            OPENSSL_free(encoded);
+        encoded = NULL;
+        WPACKET_cleanup(&pkt);
+    }
     return encoded;
 }
 