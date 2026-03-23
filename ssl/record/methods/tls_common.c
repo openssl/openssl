7e6e87f06343304417d11f3a9135ad60a8351f06
Alan Somers <asomers@gmail.com>
2026-03-23 14:54:09 -0600

    Check for EOF before error, but only if ret == 0
    CLA: trivial

--- ssl/record/methods/tls_common.c
+++ ssl/record/methods/tls_common.c
@@ -404,9 +404,11 @@ int tls_default_read_n(OSSL_RECORD_LAYER *rl, size_t n, size_t max, int extend,
         clear_sys_error();
         if (bio != NULL) {
             ret = BIO_read(bio, pkt + len + left, (int)(max - left));
-            if (ret >= 0) {
+            if (ret > 0) {
                 bioread = ret;
                 ret = OSSL_RECORD_RETURN_SUCCESS;
+	    } else if (ret == 0 && BIO_eof(bio)) {
+                ret = OSSL_RECORD_RETURN_EOF;
             } else if (BIO_should_retry(bio)) {
                 if (rl->prev != NULL) {
                     /*
