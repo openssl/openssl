27a18eeafc4c08bde4715796065b8f88129393cc
Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>
2026-03-25 05:15:30 +0530

    Add negative length validation in EVP_EncryptUpdate and EVP_DecryptUpdate
    Added input length validation checks to prevent potential security issues
    when negative values are passed to EVP_EncryptUpdate and EVP_DecryptUpdate.
    These functions cast inl (int) to size_t without validation, which could lead
    to unexpectedly large buffer allocation attempts or unintended behavior with
    negative inputs.
    
    Validation is performed early in both functions to ensure only valid,
    non-negative lengths are processed. Error is reported via EVP_R_INVALID_LENGTH.
    
    Fixes: https://github.com/openssl/openssl/issues/30486
    
    Reviewed-by: Paul Dale <paul.dale@oracle.com>
    Reviewed-by: Tomas Mraz <tomas@openssl.foundation>
    Reviewed-by: Eugene Syromiatnikov <esyr@openssl.org>
    MergeDate: Tue Mar 31 02:10:52 2026
    (Merged from https://github.com/openssl/openssl/pull/30560)
    
    (cherry picked from commit d29c165122bd480ca736a3eeb21d88a6b433ead3)

--- crypto/evp/evp_enc.c
+++ crypto/evp/evp_enc.c
@@ -651,6 +651,11 @@ int EVP_EncryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl,
     size_t soutl, inl_ = (size_t)inl;
     int blocksize;
 
+    if (inl < 0) {
+        ERR_raise(ERR_LIB_EVP, EVP_R_INVALID_LENGTH);
+        return 0;
+    }
+
     if (outl != NULL) {
         *outl = 0;
     } else {
@@ -799,6 +804,11 @@ int EVP_DecryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl,
     unsigned int b;
     size_t soutl, inl_ = (size_t)inl;
     int blocksize;
+
+    if (inl < 0) {
+        ERR_raise(ERR_LIB_EVP, EVP_R_INVALID_LENGTH);
+        return 0;
+    }
 
     if (outl != NULL) {
         *outl = 0;
