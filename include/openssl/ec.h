2d4200a9fe7ed080a431800c717f45194260f533
Igor Ustinov <igus@openssl.foundation>
2026-03-27 09:30:29 +0100

    Added the EVP_EC_affine2oct() function, which converts affine coordinates of an EC point to an octet string conforming to Sec. 2.3.4 of the SECG SEC 1 ("Elliptic Curve Cryptography") standard.
    Fixes #16270

--- include/openssl/ec.h
+++ include/openssl/ec.h
@@ -1550,8 +1550,6 @@ OSSL_DEPRECATEDIN_3_0 void EC_KEY_METHOD_get_verify(const EC_KEY_METHOD *meth,
         EC_KEY *eckey));
 #endif /* OPENSSL_NO_DEPRECATED_3_0 */
 
-#define EVP_EC_gen(curve) \
-    EVP_PKEY_Q_keygen(NULL, NULL, "EC", (char *)(strstr(curve, "")))
 /* strstr is used to enable type checking for the variadic string arg */
 #define ECParameters_dup(x) ASN1_dup_of(EC_KEY, i2d_ECParameters, \
     d2i_ECParameters, x)
