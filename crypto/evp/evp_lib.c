19815f200f2ddc4846e48dd3f01d66164faedf23
Igor Ustinov <igus@openssl.foundation>
2026-04-14 16:39:38 +0200

    The EVP_PKEY_Q_keygen function now explicitly handles the situation when NULL is passed instead of the curve name.

--- crypto/evp/evp_lib.c
+++ crypto/evp/evp_lib.c
@@ -1029,6 +1029,10 @@ EVP_PKEY *EVP_PKEY_Q_keygen(OSSL_LIB_CTX *libctx, const char *propq,
         params[0] = OSSL_PARAM_construct_size_t(OSSL_PKEY_PARAM_RSA_BITS, &bits);
     } else if (OPENSSL_strcasecmp(type, "EC") == 0) {
         name = va_arg(args, char *);
+        if (name == NULL) {
+            ERR_raise(ERR_LIB_EVP, ERR_R_PASSED_NULL_PARAMETER);
+            return NULL;
+        }
         params[0] = OSSL_PARAM_construct_utf8_string(OSSL_PKEY_PARAM_GROUP_NAME,
             name, 0);
     }
