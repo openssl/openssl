openssl/openssl

commit b27b31b62846d21a915acfd45c92ba82d3cd5666

Extend the provider MAC bridge for SIPHASH
The previous commits added support for HMAC into the provider MAC bridge.
We now extend that for SIPHASH too.

Reviewed-by: Paul Dale <paul.dale@oracle.com>
(Merged from https://github.com/openssl/openssl/pull/12637)

providers/defltprov.c
@@ -365,6 +365,7 @@ static const OSSL_ALGORITHM deflt_signature[] = {
     { "ECDSA", "provider=default", ecdsa_signature_functions },
 #endif
     { "HMAC", "provider=default", mac_hmac_signature_functions },
+    { "SIPHASH", "provider=default", mac_siphash_signature_functions },
     { NULL, NULL, NULL }
 };
 
@@ -394,6 +395,7 @@ static const OSSL_ALGORITHM deflt_keymgmt[] = {
     { "HKDF", "provider=default", kdf_keymgmt_functions },
     { "SCRYPT:id-scrypt", "provider=default", kdf_keymgmt_functions },
     { "HMAC", "provider=default", mac_keymgmt_functions },
+    { "SIPHASH", "provider=default", mac_keymgmt_functions },
     { NULL, NULL, NULL }
 };
 

providers/implementations/include/prov/implementations.h
@@ -296,6 +296,7 @@ extern const OSSL_DISPATCH ed25519_signature_functions[];
 extern const OSSL_DISPATCH ed448_signature_functions[];
 extern const OSSL_DISPATCH ecdsa_signature_functions[];
 extern const OSSL_DISPATCH mac_hmac_signature_functions[];
+extern const OSSL_DISPATCH mac_siphash_signature_functions[];
 
 /* Asym Cipher */
 extern const OSSL_DISPATCH rsa_asym_cipher_functions[];

crypto/evp/pmeth_lib.c
@@ -151,7 +151,6 @@ static int is_legacy_alg(int id, const char *keytype)
      */
     case EVP_PKEY_SM2:
     case EVP_PKEY_CMAC:
-    case EVP_PKEY_SIPHASH:
     case EVP_PKEY_POLY1305:
         return 1;
     default:

providers/implementations/signature/mac_legacy.c
@@ -70,6 +70,7 @@ static void *mac_newctx(void *provctx, const char *propq, const char *macname)
     }
 
 MAC_NEWCTX(hmac, "HMAC")
+MAC_NEWCTX(siphash, "SIPHASH")
 
 static int mac_digest_sign_init(void *vpmacctx, const char *mdname, void *vkey)
 {
@@ -177,3 +178,4 @@ static void *mac_dupctx(void *vpmacctx)
     };
 
 MAC_SIGNATURE_FUNCTIONS(hmac)
+MAC_SIGNATURE_FUNCTIONS(siphash)

commit a19df28d3f43290a23404aefde3c9dd633121107

fix: added missing conditional macro when disabling the SipHash algorithm
Original-Merge-Date: Fri Mar 13 15:50:39 2026
Original-PR: https://github.com/openssl/openssl/pull/30212
Original-Commit: 6c1fead89023 "fix: added missing conditional macro when disabling the SipHash algorithm"
Resolves: https://github.com/openssl/openssl/issues/29514
Fixes: b27b31b62846 "Extend the provider MAC bridge for SIPHASH"

apps/list.c
@@ -1434,6 +1434,9 @@ static void list_disabled(void)
 #ifdef OPENSSL_NO_DSA
     BIO_puts(bio_out, "DSA\n");
 #endif
+#ifdef OPENSSL_NO_SIPHASH
+    BIO_puts(bio_out, "SIPHASH\n");
+#endif
 #if defined(OPENSSL_NO_DTLS)
     BIO_puts(bio_out, "DTLS\n");
 #endif

providers/defltprov.c
@@ -500,8 +500,10 @@ static const OSSL_ALGORITHM deflt_signature[] = {
     { PROV_NAMES_ML_DSA_87, "provider=default", ossl_ml_dsa_87_signature_functions },
 #endif
     { PROV_NAMES_HMAC, "provider=default", ossl_mac_legacy_hmac_signature_functions },
+#ifndef OPENSSL_NO_SIPHASH
     { PROV_NAMES_SIPHASH, "provider=default",
         ossl_mac_legacy_siphash_signature_functions },
+#endif
 #ifndef OPENSSL_NO_POLY1305
     { PROV_NAMES_POLY1305, "provider=default",
         ossl_mac_legacy_poly1305_signature_functions },
@@ -622,8 +624,10 @@ static const OSSL_ALGORITHM deflt_keymgmt[] = {
         PROV_DESCS_SCRYPT_SIGN },
     { PROV_NAMES_HMAC, "provider=default", ossl_mac_legacy_keymgmt_functions,
         PROV_DESCS_HMAC_SIGN },
+#ifndef OPENSSL_NO_SIPHASH
     { PROV_NAMES_SIPHASH, "provider=default", ossl_mac_legacy_keymgmt_functions,
         PROV_DESCS_SIPHASH_SIGN },
+#endif
 #ifndef OPENSSL_NO_POLY1305
     { PROV_NAMES_POLY1305, "provider=default", ossl_mac_legacy_keymgmt_functions,
         PROV_DESCS_POLY1305_SIGN },

providers/implementations/include/prov/implementations.h
@@ -276,7 +276,9 @@ extern const OSSL_DISPATCH ossl_kmac256_internal_functions[];
 #endif
 extern const OSSL_DISPATCH ossl_kmac128_functions[];
 extern const OSSL_DISPATCH ossl_kmac256_functions[];
+#ifndef OPENSSL_NO_SIPHASH
 extern const OSSL_DISPATCH ossl_siphash_functions[];
+#endif
 extern const OSSL_DISPATCH ossl_poly1305_functions[];
 
 /* KDFs / PRFs */