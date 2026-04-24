89fdb42721364d46f5e272b9777558865167ab59
xxcui <xxcui@linux.alibaba.com>
2026-04-24 10:35:33 +0800

    riscv/sha3: code repair according to reviewer's comments
      - Remove redundant .balign after .p2align in KeccakF1600
      - Guard SHA3_absorb_zbb with 8-byte alignment check on inp to prevent unaligned ld traps; unaligned input falls back to C
      - Update copyright year to 2026

--- crypto/sha/asm/keccak1600-riscv64-zbb.pl
+++ crypto/sha/asm/keccak1600-riscv64-zbb.pl
@@ -1,10 +1,10 @@
 #! /usr/bin/env perl
-# Copyright 2025 The OpenSSL Project Authors. All Rights Reserved.
+# Copyright 2026 The OpenSSL Project Authors. All Rights Reserved.
 #
 # Licensed under the Apache License 2.0 (the "License").  You may not use
 # this file except in compliance with the License.  You can obtain a copy
 # in the file LICENSE in the source distribution or at
-# https:#www.openssl.org/source/license.html
+# https://www.openssl.org/source/license.html
 
 use strict;
 use warnings;
@@ -334,7 +334,6 @@ KeccakF1600_int:
 ###############################################################################
 .p2align    5
 .type KeccakF1600, \@function
-.balign 16
 KeccakF1600:
     addi sp, sp, -192
     sd $T1, 64(sp)
--- crypto/sha/keccak1600_riscv.c
+++ crypto/sha/keccak1600_riscv.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2025 The OpenSSL Project Authors. All Rights Reserved.
+ * Copyright 2023-2026 The OpenSSL Project Authors. All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
@@ -12,6 +12,8 @@
 
 #include <openssl/opensslconf.h>
 #include "crypto/riscv_arch.h"
+
+#define IS_ALIGNED_64(ptr) (((uintptr_t)(ptr) & 7) == 0)
 
 #ifdef INCLUDE_C_KECCAK1600
 /* The fallback implementation for `SHA3_absorb` and `SHA3_squeeze`. */
@@ -30,7 +32,7 @@ void SHA3_squeeze_zbb(uint64_t A[5][5], unsigned char *out, size_t len, size_t r
 size_t SHA3_absorb(uint64_t A[5][5], const unsigned char *inp, size_t len,
     size_t r)
 {
-    if (RISCV_HAS_ZBB()) {
+    if (RISCV_HAS_ZBB() && IS_ALIGNED_64(inp)) {
         return SHA3_absorb_zbb(A, inp, len, r);
     } else {
         return SHA3_absorb_c(A, inp, len, r);
