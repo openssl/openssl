b65b3dd3dbb45d0448c9852df3e432dc19e883bf
Abel Tom <abeltom.kernel@gmail.com>
2026-05-06 05:19:20 +0200

    Fixes #30979: Added `BN_CTX_end` before free in sm2_sign and sm2_crypt.
    Added `BN_CTX_end` call before `BN_CTX_free` to keep the pattern
    consistent with functions like `sm2_sig_verify`, `sm2_sig_gen`,
    for instance.
    
    Added missing `BN_CTX_start()` and `BN_CTX_end()` calls in
    `ossl_sm2_compute_z_digest`. Fixed formatting.
    
    Fixes: 3d328a445c2a "Add SM2 signature and ECIES schemes"
    
    Reviewed-by: Eugene Syromiatnikov <esyr@openssl.org>
    Reviewed-by: Matt Caswell <matt@openssl.foundation>
    MergeDate: Wed May 13 07:25:15 2026
    (Merged from https://github.com/openssl/openssl/pull/31069)

--- crypto/sm2/sm2_sign.c
+++ crypto/sm2/sm2_sign.c
@@ -60,6 +60,7 @@ int ossl_sm2_compute_z_digest(uint8_t *out,
         goto done;
     }
 
+    BN_CTX_start(ctx);
     p = BN_CTX_get(ctx);
     a = BN_CTX_get(ctx);
     b = BN_CTX_get(ctx);
@@ -141,6 +142,7 @@ int ossl_sm2_compute_z_digest(uint8_t *out,
 
 done:
     OPENSSL_free(buf);
+    BN_CTX_end(ctx);
     BN_CTX_free(ctx);
     EVP_MD_CTX_free(hash);
     return rc;
@@ -322,6 +324,7 @@ done:
         BN_free(s);
     }
 
+    BN_CTX_end(ctx);
     BN_CTX_free(ctx);
     EC_POINT_free(kG);
     return sig;
@@ -405,8 +408,8 @@ static int sm2_sig_verify(const EC_KEY *key, const ECDSA_SIG *sig,
         ret = 1;
 
 done:
-    BN_CTX_end(ctx);
     EC_POINT_free(pt);
+    BN_CTX_end(ctx);
     BN_CTX_free(ctx);
     return ret;
 }
