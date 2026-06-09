openssl/openssl

commit 6446e0c3c8286428374ec738ffdc859802bd3c92

Extend OID config module format.

crypto/asn1/asn_moid.c
@@ -3,7 +3,7 @@
  * project 2001.
  */
 /* ====================================================================
- * Copyright (c) 2001 The OpenSSL Project.  All rights reserved.
+ * Copyright (c) 2001-2004 The OpenSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -57,6 +57,7 @@
  */
 
 #include <stdio.h>
+#include <ctype.h>
 #include <openssl/crypto.h>
 #include "cryptlib.h"
 #include <openssl/conf.h>
@@ -64,6 +65,8 @@
 #include <openssl/x509.h>
 
 /* Simple ASN1 OID module: add all objects in a given section */
+
+static int do_create(char *value, char *name);
 
 static int oid_module_init(CONF_IMODULE *md, const CONF *cnf)
 	{
@@ -80,7 +83,7 @@ static int oid_module_init(CONF_IMODULE *md, const CONF *cnf)
 	for(i = 0; i < sk_CONF_VALUE_num(sktmp); i++)
 		{
 		oval = sk_CONF_VALUE_value(sktmp, i);
-		if(OBJ_create(oval->value, oval->name, oval->name) == NID_undef)
+		if(!do_create(oval->value, oval->name))
 			{
 			ASN1err(ASN1_F_OID_MODULE_INIT, ASN1_R_ADDING_OBJECT);
 			return 0;
@@ -98,3 +101,60 @@ void ASN1_add_oid_module(void)
 	{
 	CONF_module_add("oid_section", oid_module_init, oid_module_finish);
 	}
+
+/* Create an OID based on a name value pair. Accept two formats.
+ * shortname = 1.2.3.4
+ * shortname = some long name, 1.2.3.4
+ */
+
+
+static int do_create(char *value, char *name)
+	{
+	int nid;
+	ASN1_OBJECT *oid;
+	char *ln, *ostr, *p, *lntmp;
+	p = strrchr(value, ',');
+	if (!p)
+		{
+		ln = name;
+		ostr = value;
+		}
+	else
+		{
+		ln = NULL;
+		ostr = p + 1;
+		if (!*ostr)
+			return 0;
+		while(isspace((unsigned char)*ostr)) ostr++;
+		}
+
+	nid = OBJ_create(ostr, name, ln);
+
+	if (nid == NID_undef)
+		return 0;
+
+	if (p)
+		{
+		ln = value;
+		while(isspace((unsigned char)*ln)) ln++;
+		p--;
+		while(isspace((unsigned char)*p))
+			{
+			if (p == ln)
+				return 0;
+			p--;
+			}
+		p++;
+		lntmp = OPENSSL_malloc((p - ln) + 1);
+		if (lntmp == NULL)
+			return 0;
+		memcpy(lntmp, ln, p - ln);
+		lntmp[p - ln + 1] = 0;
+		oid = OBJ_nid2obj(nid);
+		oid->ln = lntmp;
+		}
+
+	return 1;
+	}
+		
+		

doc/apps/config.pod
@@ -105,6 +105,11 @@ as any compliant applications. For example:
  some_new_oid = 1.2.3.4
  some_other_oid = 1.2.3.5
 
+In OpenSSL 0.9.8 it is also possible to set the value to the long name followed
+by a comma and the numerical OID form. For example:
+
+ shortName = some object long name, 1.2.3.4
+
 =head2 ENGINE CONFIGURATION MODULE
 
 This ENGINE configuration module has the name B<engines>. The value of this

commit 32145557613fd2818b23fe4f112bb7c41f0bc460

asn1: prevent OOB read in do_create()
Avoid pointer underflow when trimming trailing whitespace from
the OID long name in do_create(). An all-whitespace long name
could cause the backward scan to move before the start of the
buffer, resulting in an out-of-bounds read.

Reject empty long names after whitespace trimming.

Added edge-case tests to validate the change.

Fixes #31261

Fixes: 6446e0c3c828 "Extend OID config module format."

crypto/asn1/asn_moid.c
@@ -81,10 +81,12 @@ static int do_create(const char *value, const char *name)
         while (ossl_isspace(*ln))
             ln++;
         p--;
-        while (ossl_isspace(*p)) {
-            if (p == ln)
+        while (p >= ln && ossl_isspace(*p)) {
+            if (ln == p)
                 return 0;
             p--;
+            if (p < ln)
+                return 0;
         }
         p++;
         if ((lntmp = OPENSSL_malloc((p - ln) + 1)) == NULL)

test/recipes/04-test_asn1_parse.t
@@ -12,7 +12,7 @@ use OpenSSL::Test::Utils;
 
 setup("test_asn1_parse");
 
-plan tests => 4;
+plan tests => 8;
 
 $ENV{OPENSSL_CONF} = srctop_file("test", "test_asn1_parse.cnf");
 
@@ -24,6 +24,18 @@ ok(run(app(([ 'openssl', 'asn1parse',
 
 ok(run(app(([ 'openssl', 'asn1parse',
               '-genstr', 'OID:1.2.3.4.3']))));
+
+ok(run(app(([ 'openssl', 'asn1parse',
+              '-genstr', 'OID:1.2.3.4.4']))));
+
+ok(run(app(([ 'openssl', 'asn1parse',
+              '-genstr', 'OID:1.2.3.4.5']))));
+
+ok(run(app(([ 'openssl', 'asn1parse',
+              '-genstr', 'OID:1.2.3.4.6']))));
+
+ok(run(app(([ 'openssl', 'asn1parse',
+              '-genstr', 'OID:1.2.3.4.7']))));
 
 ok(run(app(([ 'openssl', 'asn1parse',
               '-genconf', srctop_file("test", "test_asn1_genconf.cnf")]))));

test/test_asn1_parse.cnf
@@ -10,3 +10,7 @@ oid_section = oids
 testoid1 = 1.2.3.4.1
 testoid2 = A Very Long OID Name, 1.2.3.4.2
 testoid3 = ,1.2.3.4.3
+testoid4 =         OID Name     ,1.2.3.4.4
+testoid5 = "         ",1.2.3.4.5
+testoid6 = "      e    ",1.2.3.4.6
+testoid7 =           ,1.2.3.4.7