d4e6ee3b05a062950bf7fb5c6174760a7040735f
Ingo Franzki <ifranzki@linux.ibm.com>
2026-06-12 15:30:54 +0200

    s390x: Don't ignore errors from s390x_mod_exp_hw() and s390x_crt()
    Currently errors from s390x_mod_exp_hw() and s390x_crt() are silently
    ignored and the software path is used as fallback.
    
    Change this to only take the software path if s390x_mod_exp_hw() and
    s390x_crt() returns 0 to indicate that it does not support the RSA
    acceleration. In case of errors, return them to the caller. Errors could
    be memory allocation failures or errors during BIGNUM calls. Those should
    not be ignored, but reported as failure.
    
    Note that it can happen that the ioctl's fail, but this is not to be
    reported as error. Those are situations where for example no suitable
    crypto adapter is available, or the file descriptor has been closed
    by a sandbox. Those situations disable the RSA acceleration for further
    RSA requests, but the current operation should still be performed via the
    software fallback. For cases where the RSA key size is too large for
    acceleration, the operation must also be performed via the software
    fallback, and not reported as an error.
    
    This also fixes failures of the test_rsa_pkcs1_mfail test case that found
    the memory allocation failures that got ignored.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- crypto/rsa/rsa_ossl.c
+++ crypto/rsa/rsa_ossl.c
@@ -1184,9 +1184,12 @@ static int rsa_ossl_finish(RSA *rsa)
 static int rsa_ossl_s390x_mod_exp(BIGNUM *r0, const BIGNUM *i, RSA *rsa,
     BN_CTX *ctx)
 {
+    int rc;
+
     if (rsa->version != RSA_ASN1_VERSION_MULTI) {
-        if (s390x_crt(r0, i, rsa->p, rsa->q, rsa->dmp1, rsa->dmq1, rsa->iqmp) == 1)
-            return 1;
+        rc = s390x_crt(r0, i, rsa->p, rsa->q, rsa->dmp1, rsa->dmq1, rsa->iqmp);
+        if (rc >= 0)
+            return rc;
     }
     return rsa_ossl_mod_exp(r0, i, rsa, ctx);
 }
