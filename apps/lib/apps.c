05901611486e3d046ba88cee219181e9aa724732
Bob Beck <beck@openssl.org>
2026-06-16 15:43:30 -0600

    Simplify key arg processing using asprintf()
    This removes the need for a potentially dying app_malloc call in here.

--- apps/lib/apps.c
+++ apps/lib/apps.c
@@ -3930,9 +3930,8 @@ char *process_additional_mac_key_arguments(const char *arg)
             BIO_printf(bio_err, "No environment variable %s\n", arg);
             return NULL;
         }
-        outbuf = app_malloc(strlen("key:") + strlen(val) + 1, "MACOPT KEYENV");
-        strcpy(outbuf, "key:");
-        strcat(outbuf, val);
+        if (OPENSSL_asprintf(&outbuf, "key:%s", val) < 0)
+            outbuf = NULL;
         return outbuf;
     }
 
@@ -3946,9 +3945,8 @@ char *process_additional_mac_key_arguments(const char *arg)
             BIO_printf(bio_err, "No environment variable %s\n", arg);
             return NULL;
         }
-        outbuf = app_malloc(strlen("hexkey:") + strlen(val) + 1, "MACOPT KEYENVHEX");
-        strcpy(outbuf, "hexkey:");
-        strcat(outbuf, val);
+        if (OPENSSL_asprintf(&outbuf, "hexkey:%s", val) < 0)
+            outbuf = NULL;
         return outbuf;
     }
 
@@ -4000,9 +3998,8 @@ char *process_additional_mac_key_arguments(const char *arg)
             clear_free(inbuf);
             return NULL;
         }
-        outbuf = app_malloc(strlen("key:") + strlen(inbuf) + 1, "MACOPT KEYSTDIN");
-        strcpy(outbuf, "key:");
-        strcat(outbuf, inbuf);
+        if (OPENSSL_asprintf(&outbuf, "key:%s", inbuf) < 0)
+            outbuf = NULL;
         clear_free(inbuf);
         return outbuf;
     }
