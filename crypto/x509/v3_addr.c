7814075f3b614f886b33199bf6528187f11531db
Nihal Kotte <ryoyamaha@Nihals-MacBook.local>
2026-06-18 18:53:13 +0530

    Fixes issues pointed out by @esyr and @jogme

--- crypto/x509/v3_addr.c
+++ crypto/x509/v3_addr.c
@@ -95,13 +95,21 @@ unsigned int X509v3_addr_get_afi(const IPAddressFamily *f)
     return (f->addressFamily->data[0] << 8) | f->addressFamily->data[1];
 }
 
+/*
+ * Get SAFI from an IPAdressFamily.
+ */
 unsigned int X509v3_addr_get_safi(const IPAddressFamily *f)
 {
     if (f == NULL
         || f->addressFamily == NULL
         || f->addressFamily->data == NULL
-        || f->addressFamily->length < 3)
+        || f->addressFamily->length < 2){
         return 0;
+    }
+
+    if (f->addressFamily->length == 2){
+        return 1;
+    }
 
     return (f->addressFamily->data[2]);
 }
