42d343f4a4b6dbf604696309f92b65dcef1e9f69
Gowtham Suresh Kumar <gowtham.sureshkumar@arm.com>
2026-07-30 16:10:34 +0100

    Add support for Arm Neoverse V3AE platform
    Add cpu MIDR and ensure relevant optimisations are enabled.
    
    Backport of commit 32ff0eb from master branch.
    
    Signed-off-by: Paul Elliott <paul.elliott@arm.com>
    Signed-off-by: Gowtham Suresh Kumar <gowtham.sureshkumar@arm.com>

--- crypto/arm_arch.h
+++ crypto/arm_arch.h
@@ -106,6 +106,7 @@ extern unsigned int OPENSSL_armv8_rsa_neonized;
 #define HISI_CPU_PART_KP920 0xD01
 #define ARM_CPU_PART_V2 0xD4F
 #define ARM_CPU_PART_N3 0xD8E
+#define ARM_CPU_PART_V3_AE 0xD83
 #define ARM_CPU_PART_V3 0xD84
 
 #define APPLE_CPU_PART_M1_ICESTORM 0x022
