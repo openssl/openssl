9b7acda79a97d20fa7df45b6f9b1f4993f483252
Alexandr Nedvedicky <sashan@openssl.org>
2026-07-31 15:18:17 +0200

    fixup! fixup! fixup! fixup! fixup! Update doc/internal/man7/ossl_rbtree.pod

--- test/ossl_rbtree_test.c
+++ test/ossl_rbtree_test.c
@@ -203,7 +203,7 @@ static int test_rbt_dup_insert(void)
         return 0;
 
     for (i = 0; i < 26; i++) {
-        OSSL_RBT_INIT_RBE(&insert_rbt);
+        OSSL_RBT_INIT_RBE(ossl_rbt, &insert_rbt);
         insert_rbt.rbt_data = test_data[i];
         conflict_rbt = OSSL_RBT_INSERT(ossl_rbt, &rbt_head, &insert_rbt);
         if (!TEST_ptr(conflict_rbt)) {
