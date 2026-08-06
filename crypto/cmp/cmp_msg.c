684e94c08f685b49b7d82fe3af6f617d0a95925c
Bob Beck <beck@openssl.org>
2026-08-05 18:53:19 -0600

    Move CMS sign-then-encrypt for central key generation into cmp
    Recompose it from the public CMS_sign_ex(), CMS_encrypt_ex() and
    ASN.1 routines instead of reaching into CMS_ContentInfo internals.
    Remove ossl_cms_sign_encrypt() and internal/cms.h from libcrypto,
    which existed only for this single caller. The encoded result is
    unchanged.

--- crypto/cmp/cmp_msg.c
+++ crypto/cmp/cmp_msg.c
@@ -13,7 +13,7 @@
 
 #include "cmp_local.h"
 
-#include <internal/cms.h> /* for ossl_cms_sign_encrypt() */
+#include <openssl/cms.h>
 
 OSSL_CMP_MSG *OSSL_CMP_MSG_new(OSSL_LIB_CTX *libctx, const char *propq)
 {
@@ -464,6 +464,69 @@ err:
 }
 
 #ifndef OPENSSL_NO_CMS
+/* return a pointer to the content octets of a CMS_ContentInfo encoding */
+static const unsigned char *cms_get_content_der(const unsigned char *der,
+    long der_len, long *content_len)
+{
+    const unsigned char *p = der;
+    long len;
+    int tag, xclass;
+    ASN1_OBJECT *obj = NULL;
+
+    if ((ASN1_get_object(&p, &len, &tag, &xclass, der_len) & 0x80) != 0
+        || tag != V_ASN1_SEQUENCE)
+        return NULL;
+    if ((obj = d2i_ASN1_OBJECT(NULL, &p, len)) == NULL)
+        return NULL;
+    ASN1_OBJECT_free(obj);
+    len = (long)(der + der_len - p);
+    if ((ASN1_get_object(&p, &len, &tag, &xclass, len) & 0x80) != 0
+        || tag != 0 || xclass != V_ASN1_CONTEXT_SPECIFIC)
+        return NULL;
+    *content_len = len;
+    return p;
+}
+
+static CMS_EnvelopedData *cms_sign_encrypt(BIO *data, X509 *sign_cert,
+    STACK_OF(X509) *certs, EVP_PKEY *sign_key, unsigned int sign_flags,
+    STACK_OF(X509) *enc_recip, const EVP_CIPHER *cipher,
+    unsigned int enc_flags, OSSL_LIB_CTX *libctx, const char *propq)
+{
+    CMS_EnvelopedData *evd = NULL;
+    CMS_ContentInfo *signcms = NULL, *evpcms = NULL;
+    unsigned char *sder = NULL, *eder = NULL;
+    const unsigned char *content;
+    long content_len;
+    BIO *signbio = NULL;
+    int len;
+
+    signcms = CMS_sign_ex(sign_cert, sign_key, certs, data, sign_flags,
+        libctx, propq);
+    if (signcms == NULL)
+        goto err;
+    if ((len = i2d_CMS_ContentInfo(signcms, &sder)) < 0
+        || (content = cms_get_content_der(sder, len, &content_len)) == NULL
+        || (signbio = BIO_new_mem_buf(content, (int)content_len)) == NULL)
+        goto err;
+    evpcms = CMS_encrypt_ex(enc_recip, signbio, cipher, enc_flags,
+        libctx, propq);
+    if (evpcms == NULL)
+        goto err;
+    if ((len = i2d_CMS_ContentInfo(evpcms, &eder)) < 0
+        || (content = cms_get_content_der(eder, len, &content_len)) == NULL)
+        goto err;
+    evd = (CMS_EnvelopedData *)ASN1_item_d2i(NULL, &content, content_len,
+        ASN1_ITEM_rptr(CMS_EnvelopedData));
+
+err:
+    OPENSSL_free(sder);
+    OPENSSL_free(eder);
+    BIO_free(signbio);
+    CMS_ContentInfo_free(signcms);
+    CMS_ContentInfo_free(evpcms);
+    return evd;
+}
+
 static OSSL_CRMF_ENCRYPTEDKEY *enc_privkey(OSSL_CMP_CTX *ctx, const EVP_PKEY *pkey)
 {
     OSSL_CRMF_ENCRYPTEDKEY *ek = NULL;
@@ -482,7 +545,7 @@ static OSSL_CRMF_ENCRYPTEDKEY *enc_privkey(OSSL_CMP_CTX *ctx, const EVP_PKEY *pk
         goto err;
     ossl_cmp_set_own_chain(ctx);
     cipher = EVP_CIPHER_fetch(ctx->libctx, SN_aes_256_cbc, ctx->propq);
-    envData = ossl_cms_sign_encrypt(privbio, ctx->cert, ctx->chain, ctx->pkey, CMS_BINARY,
+    envData = cms_sign_encrypt(privbio, ctx->cert, ctx->chain, ctx->pkey, CMS_BINARY,
         encryption_recips, cipher, CMS_BINARY,
         ctx->libctx, ctx->propq);
     EVP_CIPHER_free(cipher);
