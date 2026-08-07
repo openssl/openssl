4574f82ebf3caf3777718dba06928d006a08fa6b
Bob Beck <beck@openssl.org>
2026-08-07 11:43:25 -0600

    Move the CMS sources to a top-level cms directory
    Now that they are disentangled from libcrypto, move them out of the
    crypto directory, for possible later flensing from the libcrypto whale.

--- test/cmsapitest.c
+++ test/cmsapitest.c
@@ -13,7 +13,7 @@
 #include <openssl/cms.h>
 #include <openssl/bio.h>
 #include <openssl/x509.h>
-#include "../crypto/cms/cms_local.h" /* for d.signedData and d.envelopedData */
+#include "../cms/cms_local.h" /* for d.signedData and d.envelopedData */
 
 #include "testutil.h"
 
