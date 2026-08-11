50944b598f67a2668b55cac0f07bd524f9e9034e
Neil Horman <nhorman@openssl.org>
2026-08-11 16:04:05 -0400

    fixup! Test EVP_PKEY_get_size for zero return

--- test/evp_extra_test.c
+++ test/evp_extra_test.c
@@ -8920,7 +8920,7 @@ static int test_rsasve_degenerate_ciphertext(int idx)
         goto err;
 
     ctlen = secretlen = (size_t)EVP_PKEY_get_size(rsakey);
-    if (!TEST_int_gt(ctlen, 0))
+    if (!TEST_size_t_gt(ctlen, 0))
         goto err;
     if (!TEST_ptr(ct = OPENSSL_zalloc(ctlen))
         || !TEST_ptr(secret = OPENSSL_malloc(secretlen)))
