e33b9558f5505f6ab57039727486e91aa94c34e8
Andrew Dinh <andrewd@openssl.org>
2026-08-20 17:02:48 +0700

    Fix tick race in radix fault-injection scripts
    Guard any `OP_SET_INJECT_WORD`+`OP_WRITE` with 2 new functions `OP_ENGINE_TICK_ENABLE`/`OP_ENGINE_TICK_DISABLE` that enable/disable ticking in the QUIC engine. Otherwise, the radix test interpreter can tick between the 2 ops and cause the peer to close the connection prematurely

--- test/radix/quic_tests.c
+++ test/radix/quic_tests.c
@@ -2976,9 +2976,10 @@ DEF_SCRIPT(script_46, "Fault injection - ACK - malformed initial range")
     OP_ACCEPT_STREAM_WAIT(S, Sa, 0);
     OP_READ_EXPECT(Sa, "apple", 5);
 
+    OP_ENGINE_TICK_DISABLE(S);
     OP_SET_INJECT_WORD(1, 0);
-
     OP_WRITE(Sa, "Strawberry", 10);
+    OP_ENGINE_TICK_ENABLE(S);
 
     OP_EXPECT_CONN_CLOSE_INFO(C, OSSL_QUIC_ERR_FRAME_ENCODING_ERROR, 0, 0);
 }
@@ -2994,9 +2995,10 @@ DEF_SCRIPT(script_47, "Fault injection - ACK - malformed subsequent range")
     OP_ACCEPT_STREAM_WAIT(S, Sa, 0);
     OP_READ_EXPECT(Sa, "apple", 5);
 
+    OP_ENGINE_TICK_DISABLE(S);
     OP_SET_INJECT_WORD(2, 0);
-
     OP_WRITE(Sa, "Strawberry", 10);
+    OP_ENGINE_TICK_ENABLE(S);
 
     OP_EXPECT_CONN_CLOSE_INFO(C, OSSL_QUIC_ERR_FRAME_ENCODING_ERROR, 0, 0);
 }
@@ -3012,9 +3014,10 @@ DEF_SCRIPT(script_48, "Fault injection - ACK - malformed subsequent range")
     OP_ACCEPT_STREAM_WAIT(S, Sa, 0);
     OP_READ_EXPECT(Sa, "apple", 5);
 
+    OP_ENGINE_TICK_DISABLE(S);
     OP_SET_INJECT_WORD(3, 0);
-
     OP_WRITE(Sa, "Strawberry", 10);
+    OP_ENGINE_TICK_ENABLE(S);
 
     OP_EXPECT_CONN_CLOSE_INFO(C, OSSL_QUIC_ERR_FRAME_ENCODING_ERROR, 0, 0);
 }
