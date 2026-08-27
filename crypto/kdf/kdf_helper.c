ec0b1ceb1e853380730b8ce634f2958afee42450
slontis <shane.lontis@oracle.com>
2026-08-27 15:08:25 +1000

    fixup! ARGON2: Add a helper API that uses non OSSL_PARAM options for common parameters.

--- crypto/kdf/kdf_helper.c
+++ crypto/kdf/kdf_helper.c
@@ -19,14 +19,13 @@ int EVP_KDF_argon2(uint8_t *out, size_t outlen,
     const uint8_t *pass, size_t passlen,
     const uint8_t *salt, size_t saltlen,
     uint32_t lanes, uint32_t memorycost, uint32_t iterations,
-    OSSL_LIB_CTX *libctx, const OSSL_PARAM *optionals)
+    OSSL_LIB_CTX *libctx, const char *propq,
+    const OSSL_PARAM *optionals)
 {
     int ret = 0;
     EVP_KDF_CTX *kctx = NULL;
     EVP_KDF *kdf = NULL;
     OSSL_PARAM params[7], *p = params;
-    const OSSL_PARAM *propq_param;
-    const char *propq = NULL;
     const char *alg;
 
     switch (alg_type) {
@@ -43,29 +42,24 @@ int EVP_KDF_argon2(uint8_t *out, size_t outlen,
         ERR_raise(ERR_LIB_EVP, EVP_R_BAD_ALGORITHM_NAME);
         return 0;
     }
-    if (pass == NULL || passlen == 0) {
-        ERR_raise_data(ERR_LIB_EVP, EVP_R_INVALID_VALUE, "Password required");
-        return 0;
-    }
     if (salt == NULL || saltlen == 0) {
         ERR_raise_data(ERR_LIB_EVP, EVP_R_INVALID_VALUE, "Salt required");
         return 0;
     }
 
-    propq_param = OSSL_PARAM_locate_const(optionals, OSSL_ALG_PARAM_PROPERTIES);
-    if (propq_param != NULL && !OSSL_PARAM_get_utf8_string_ptr(propq_param, &propq))
-        return 0;
-
     kdf = EVP_KDF_fetch(libctx, alg, propq);
     kctx = EVP_KDF_CTX_new(kdf);
     if (kctx == NULL)
         goto err;
-
-    *p++ = OSSL_PARAM_construct_octet_string(OSSL_KDF_PARAM_PASSWORD, (uint8_t *)pass, passlen);
+    if (pass != NULL && passlen > 0)
+        *p++ = OSSL_PARAM_construct_octet_string(OSSL_KDF_PARAM_PASSWORD, (uint8_t *)pass, passlen);
     *p++ = OSSL_PARAM_construct_octet_string(OSSL_KDF_PARAM_SALT, (uint8_t *)salt, saltlen);
     *p++ = OSSL_PARAM_construct_uint32(OSSL_KDF_PARAM_ARGON2_LANES, &lanes);
     *p++ = OSSL_PARAM_construct_uint32(OSSL_KDF_PARAM_ARGON2_MEMCOST, &memorycost);
     *p++ = OSSL_PARAM_construct_uint32(OSSL_KDF_PARAM_ITER, &iterations);
+    /* If there is no optional propq set it to the propq used by the fetch */
+    if (propq != NULL && OSSL_PARAM_locate_const(optionals, OSSL_KDF_PARAM_PROPERTIES) == NULL)
+        *p++ = OSSL_PARAM_construct_utf8_string(OSSL_KDF_PARAM_PROPERTIES, (char *)propq, 0);
     *p = OSSL_PARAM_construct_end();
 
     if (optionals != NULL && !EVP_KDF_CTX_set_params(kctx, optionals)) {
