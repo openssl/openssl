0d1b400a1166415f9d479ff11b3893a036445993
Richard Levitte <levitte@openssl.foundation>
2026-09-09 09:33:34 +0200

    RSA: Convert rsa_get_public_exp() to OSSL_FN
    The d -> e recovery (e = d^-1 mod (p-1)(q-1)) used by
    RSA_setup_blinding() when no public exponent is set now runs on
    OSSL_FN views: the (p-1)(q-1) product and the modular inverse.
    
    Resolves: https://github.com/openssl/project/issues/2045
    Assisted-by: Pi:moonshotai/kimi-k3
    Signed-off-by: Richard Levitte <levitte@openssl.foundation>

--- crypto/rsa/rsa_crpt.c
+++ crypto/rsa/rsa_crpt.c
@@ -17,6 +17,8 @@
 #include <openssl/crypto.h>
 #include "internal/cryptlib.h"
 #include "crypto/bn.h"
+#include "crypto/fn.h"
+#include "crypto/fn_intern.h"
 #include <openssl/rand.h>
 #include "rsa_local.h"
 
@@ -74,30 +76,72 @@ int RSA_blinding_on(RSA *rsa, BN_CTX *ctx)
 }
 
 static BIGNUM *rsa_get_public_exp(const BIGNUM *d, const BIGNUM *p,
-    const BIGNUM *q, BN_CTX *ctx)
+    const BIGNUM *q)
 {
-    BIGNUM *ret = NULL, *r0, *r1, *r2;
+    /* e = d^-1 mod (p-1)(q-1), computed on OSSL_FN views */
+    BIGNUM *ret = NULL;
+    OSSL_FN_CTX *fn_ctx = NULL;
+    OSSL_FN *r0 = NULL, *r1 = NULL, *r2 = NULL, *fn_e = NULL;
+    const OSSL_FN *fn_d, *fn_p, *fn_q;
+    size_t pl, ql, nl, fn_size;
+    int e_bits;
 
     if (d == NULL || p == NULL || q == NULL)
         return NULL;
 
-    BN_CTX_start(ctx);
-    r0 = BN_CTX_get(ctx);
-    r1 = BN_CTX_get(ctx);
-    r2 = BN_CTX_get(ctx);
-    if (r2 == NULL)
+    fn_d = bn_get_ossl_fn(d);
+    fn_p = bn_get_ossl_fn(p);
+    fn_q = bn_get_ossl_fn(q);
+    if (fn_d == NULL || fn_p == NULL || fn_q == NULL)
+        return NULL;
+    pl = ossl_fn_get_dsize((OSSL_FN *)fn_p);
+    ql = ossl_fn_get_dsize((OSSL_FN *)fn_q);
+    nl = pl + ql;
+
+    r0 = OSSL_FN_secure_new_limbs(nl);
+    r1 = OSSL_FN_secure_new_limbs(pl);
+    r2 = OSSL_FN_secure_new_limbs(ql);
+    if (r0 == NULL || r1 == NULL || r2 == NULL)
         goto err;
 
-    if (!BN_sub(r1, p, BN_value_one()))
-        goto err;
-    if (!BN_sub(r2, q, BN_value_one()))
-        goto err;
-    if (!BN_mul(r0, r1, r2, ctx))
+    if (!OSSL_FN_copy(r1, fn_p) || !OSSL_FN_sub_word(r1, 1)
+        || !OSSL_FN_copy(r2, fn_q) || !OSSL_FN_sub_word(r2, 1))
         goto err;
 
-    ret = BN_mod_inverse(NULL, d, r0, ctx);
+    /* The mul and the mod_inverse run sequentially in one arena. */
+    fn_size = ossl_fn_ctx_max_size(OSSL_FN_mul_ctx_size(r0, r1, r2),
+        OSSL_FN_mod_inverse_ctx_size(r0, fn_d, r0));
+    if (fn_size == 0)
+        goto err;
+    fn_ctx = OSSL_FN_CTX_secure_new_size(NULL, fn_size);
+    if (fn_ctx == NULL)
+        goto err;
+
+    if (!OSSL_FN_mul(r0, r1, r2, fn_ctx))
+        goto err;
+
+    ret = BN_new();
+    if (ret == NULL)
+        goto err;
+    fn_e = bn_acquire_ossl_fn(ret, (int)nl);
+    if (fn_e == NULL) {
+        BN_free(ret);
+        ret = NULL;
+        goto err;
+    }
+    if (!OSSL_FN_mod_inverse(fn_e, fn_d, r0, fn_ctx)) {
+        BN_free(ret);
+        ret = NULL;
+        goto err;
+    }
+    e_bits = (int)OSSL_FN_num_bits(fn_e);
+    bn_release(ret, e_bits > 0 ? (e_bits + BN_BITS2 - 1) / BN_BITS2 : 1);
+
 err:
-    BN_CTX_end(ctx);
+    OSSL_FN_CTX_free(fn_ctx);
+    OSSL_FN_clear_free(r0);
+    OSSL_FN_clear_free(r1);
+    OSSL_FN_clear_free(r2);
     return ret;
 }
 
@@ -122,7 +166,7 @@ BN_BLINDING *RSA_setup_blinding(RSA *rsa, BN_CTX *in_ctx)
     }
 
     if (rsa->e == NULL) {
-        e = rsa_get_public_exp(rsa->d, rsa->p, rsa->q, ctx);
+        e = rsa_get_public_exp(rsa->d, rsa->p, rsa->q);
         if (e == NULL) {
             ERR_raise(ERR_LIB_RSA, RSA_R_NO_PUBLIC_EXPONENT);
             goto err;
