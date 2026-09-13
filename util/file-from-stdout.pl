0979d5b56bfe4e1e2add6d8302e355943f99dab3
Neil Horman <nhorman@openssl.org>
2026-09-13 17:40:54 -0400

    Make syystem exec directly rather than calling /bin/sh
    When perl uses system() to call an external binary, if the argument is a
    single array, perl uses /bin/sh and passes the entire argument as the
    command line (i.e. /bin/sh -c @ARGV)
    
    This is normally fine, but on platforms in which /bin/sh is an older
    shell, @ARGV can easily overflow the command line space (e.g. on
    nonstop).
    
    Instead, shift off the first argument, then call system(argv[0], @ARGV).
    This forces perl to call exec directly, rather than using the shell,
    avoiding that.
    
    Fixes #32778

--- util/file-from-stdout.pl
+++ util/file-from-stdout.pl
@@ -42,12 +42,13 @@ END {
     unlink $temp if defined $temp && -e $temp;
 }
 
+my $exec = shift @ARGV;
 open my $saved, ">&", \*STDOUT
     or die "Can't save stdout, $!\n";
 open STDOUT, ">&", $out
     or die "Can't redirect stdout to $temp, $!\n";
 
-my $status = system @ARGV;
+my $status = system($exec, @ARGV);
 my $why = $!;
 
 open STDOUT, ">&", $saved
