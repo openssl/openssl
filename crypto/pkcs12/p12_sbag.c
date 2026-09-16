c086ca4dcbb1eceb40ad60caefde36baeddaaf40
Dmitry Belyavskiy <beldmit@gmail.com>
2026-09-16 15:37:12 +0200

    fixup! Provide metadata for symmetric keys when parsing pkcs#12 files

--- crypto/pkcs12/p12_sbag.c
+++ crypto/pkcs12/p12_sbag.c
@@ -271,7 +271,7 @@ EVP_SKEY *PKCS8_PRIV_KEY_INFO_get1_skey(const PKCS8_PRIV_KEY_INFO *p8inf,
 
 err:
     OSSL_PARAM_BLD_free(bld);
-    OSSL_PARAM_free(params);
+    OSSL_PARAM_clear_free(params);
     OPENSSL_free(oid_der);
     OPENSSL_free(params_der);
     return skey;
