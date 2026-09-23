7b4f2934daf54e63a693ea8122007e47cea7b565
slontis <shane.lontis@oracle.com>
2026-09-23 12:38:15 +1000

    fixup! paramnames: Add support for constant FIPS indicators

--- util/perl/OpenSSL/paramnames.pm
+++ util/perl/OpenSSL/paramnames.pm
@@ -18,7 +18,6 @@ our @EXPORT_OK = qw(generate_public_macros
 
 my $case_sensitive = 1;
 my $need_break = 0;
-my $invalid_param = "invalid param";
 my $default_indicator = "fips indicator";
 
 my %params = (
@@ -174,16 +173,7 @@ my %params = (
     'OSSL_DIGEST_PARAM_SIZE' =>         "size",         # size_t
     'OSSL_DIGEST_PARAM_XOF' =>          "xof",          # int, 0 or 1
     'OSSL_DIGEST_PARAM_ALGID_ABSENT' => "algid-absent", # int, 0 or 1
-    'OSSL_DIGEST_PARAM_FUNCTION_NAME' =>    "function-name", # utf8 string
-    'OSSL_DIGEST_PARAM_CUSTOMIZATION' =>    "customization", # utf8 string
     'OSSL_DIGEST_PARAM_FIPS_APPROVED_INDICATOR' => '*OSSL_ALG_PARAM_FIPS_APPROVED_INDICATOR',
-    'OSSL_DIGEST_PARAM_PROPERTIES' => '*OSSL_ALG_PARAM_PROPERTIES',# utf8 string
-
-# external mu digest parameters
-    'OSSL_DIGEST_PARAM_MU_PUB_KEY' =>        "pub",                        # octet string
-    'OSSL_DIGEST_PARAM_MU_CONTEXT_STRING' => "context-string",             # octet string
-    'OSSL_DIGEST_PARAM_MU_DIGEST' =>         '*OSSL_ALG_PARAM_DIGEST',     # utf8 string
-    'OSSL_DIGEST_PARAM_MU_PROPERTIES' =>     '*OSSL_ALG_PARAM_PROPERTIES', # utf8 string
 
 # MAC parameters
     'OSSL_MAC_PARAM_KEY' =>            "key",           # octet string
@@ -692,11 +682,7 @@ sub trie_matched {
   my $indent1 = shift;
   my $indent2 = shift;
 
-  if ($field eq $invalid_param) {
-    printf "%sERR_raise_data(ERR_LIB_PROV, ERR_R_UNSUPPORTED,\n", $indent1;
-    printf "%s               \"param %%s is unsupported\", s);\n", $indent1;
-    printf "%sreturn 0;\n", $indent1;
-  } elsif ($field eq $default_indicator) {
+  if ($field eq $default_indicator) {
     printf "%sif (!OSSL_PARAM_set_int((OSSL_PARAM *)p, 1))\n", $indent1;
     printf "%sreturn 0;\n", $indent2;
   } elsif (defined($num)) {
@@ -867,10 +853,7 @@ sub output_param_decoder {
 
         $prms{$pname} = $pident;
 
-        if ($pident eq $invalid_param) {
-            # Skip error cases in parameter list
-            next;
-        } elsif ($pident eq $default_indicator) {
+        if ($pident eq $default_indicator) {
             # These are only relevant for the FIPS provider
             $ifdefs{$pident} = ' defined(FIPS_MODULE)';
         }
@@ -905,8 +888,8 @@ sub output_param_decoder {
     my %done_prms = ();
     my $have_struct_field = 0;
     foreach my $pident (sort values %prms) {
-        if ($pident eq $invalid_param || $pident eq $default_indicator) {
-            # Skip error cases and default indicators in structure
+        if ($pident eq $default_indicator) {
+            # Skip default indicators in structure
             next;
         }
         if (not defined $done_prms{$pident}) {
